18d159c44196a660f008a6686cd59afb0eea5473
Boots Boogie <87733534+bootsboogie@users.noreply.github.com>
2021-09-03 09:00:14 -0600

    Make tests alittle better

--- src/context/WalletProvider/WalletProvider.test.tsx
+++ src/context/WalletProvider/WalletProvider.test.tsx
@@ -3,8 +3,6 @@ import { WebUSBKeepKeyAdapter } from '@shapeshiftoss/hdwallet-keepkey-webusb'
 import { act, renderHook } from '@testing-library/react-hooks'
 
 import { SUPPORTED_WALLETS } from './config'
-// import * as config from './config'
-// import { SUPPORTED_WALLETS } from './config'
 import { useWallet, WalletActions, WalletProvider } from './WalletProvider'
 
 jest.mock('@shapeshiftoss/hdwallet-keepkey-webusb', () => ({
@@ -13,6 +11,10 @@ jest.mock('@shapeshiftoss/hdwallet-keepkey-webusb', () => ({
   }
 }))
 
+const walletInfoPayload = {
+  name: SUPPORTED_WALLETS.native.name,
+  icon: SUPPORTED_WALLETS.native.icon
+}
 const setup = () => {
   // @ts-ignore
   WebUSBKeepKeyAdapter.useKeyring.mockImplementation(() => ({
@@ -45,19 +47,18 @@ describe('WalletProvider', () => {
       expect(result.current.state.wallet).toBeTruthy()
     })
 
-    it('can SET_WALLET_MODAL state to open and close', async () => {
+    it('can SET_WALLET_INFO', async () => {
       const { result, waitForValueToChange } = setup()
       await waitForValueToChange(() => result.current.state.adapters)
 
-      expect(result.current.state.modal).toBe(false)
+      expect(result.current.state.walletInfo).toBe(null)
       act(() => {
-        result.current.dispatch({ type: WalletActions.SET_WALLET_MODAL, payload: true })
+        result.current.dispatch({
+          type: WalletActions.SET_WALLET_INFO,
+          payload: walletInfoPayload
+        })
       })
-      expect(result.current.state.modal).toBe(true)
-      act(() => {
-        result.current.dispatch({ type: WalletActions.SET_WALLET_MODAL, payload: false })
-      })
-      expect(result.current.state.modal).toBe(false)
+      expect(result.current.state.walletInfo).toEqual(walletInfoPayload)
     })
 
     it('can SET_IS_CONNECTED', async () => {
@@ -74,6 +75,21 @@ describe('WalletProvider', () => {
       })
       expect(result.current.state.isConnected).toBe(false)
     })
+
+    it('can SET_WALLET_MODAL state to open and close', async () => {
+      const { result, waitForValueToChange } = setup()
+      await waitForValueToChange(() => result.current.state.adapters)
+
+      expect(result.current.state.modal).toBe(false)
+      act(() => {
+        result.current.dispatch({ type: WalletActions.SET_WALLET_MODAL, payload: true })
+      })
+      expect(result.current.state.modal).toBe(true)
+      act(() => {
+        result.current.dispatch({ type: WalletActions.SET_WALLET_MODAL, payload: false })
+      })
+      expect(result.current.state.modal).toBe(false)
+    })
   })
 
   describe('disconnect', () => {
@@ -81,6 +97,10 @@ describe('WalletProvider', () => {
       const walletDisconnect = jest.fn()
       const { result, waitForValueToChange } = setup()
       await waitForValueToChange(() => result.current.state.adapters)
+
+      expect(result.current.state.wallet).toBe(null)
+      expect(result.current.state.walletInfo).toBe(null)
+      expect(result.current.state.isConnected).toBe(false)
 
       act(() => {
         result.current.dispatch({
@@ -90,17 +110,19 @@ describe('WalletProvider', () => {
         result.current.dispatch({ type: WalletActions.SET_IS_CONNECTED, payload: true })
         result.current.dispatch({
           type: WalletActions.SET_WALLET_INFO,
-          payload: { name: SUPPORTED_WALLETS.native.name, icon: SUPPORTED_WALLETS.native.icon }
+          payload: walletInfoPayload
         })
       })
 
       expect(result.current.state.wallet).toBeTruthy()
+      expect(result.current.state.walletInfo).toBeTruthy()
       expect(result.current.state.isConnected).toBe(true)
 
       act(() => {
         result.current.disconnect()
       })
 
+      expect(walletDisconnect).toHaveBeenCalledTimes(1)
       expect(result.current.state.wallet).toBe(null)
       expect(result.current.state.walletInfo).toBe(null)
       expect(result.current.state.isConnected).toBe(false)

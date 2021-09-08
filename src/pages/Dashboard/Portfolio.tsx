d8b1b4ae00d2d6fde1ac63fd82f4c1cf93777f0a
Sean Luther <elmutt@gmail.com>
2021-09-08 16:19:31 -0600

    another step towards seperating asset service and market service functionality

--- src/pages/Dashboard/Portfolio.tsx
+++ src/pages/Dashboard/Portfolio.tsx
@@ -13,14 +13,15 @@ import { AssetList } from './components/AssetList/AssetList'
 // TODO: Combined Portfolio Asset Chart
 const asset = {
   icon: 'https://static.coincap.io/assets/icons/256/btc.png',
-  name: 'Bitcoin',
+  displayName: 'Bitcoin',
   network: 'bitcoin',
   symbol: 'BTC',
   price: '1000',
   marketCap: '1000',
   volume: '1000',
   changePercent24Hr: 25,
-  description: 'loremIpsum'
+  description: 'loremIpsum',
+  name: 'bitcoin'
 }
 
 export const Portfolio = () => {

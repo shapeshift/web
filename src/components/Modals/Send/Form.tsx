f0f48439451a0c043786991174b9f4b46d004d00
technojak <87927541+technojak@users.noreply.github.com>
2021-09-10 10:56:54 -0600

    Add useFormSend tests

--- src/components/Modals/Send/Form.tsx
+++ src/components/Modals/Send/Form.tsx
@@ -26,7 +26,7 @@ import { Details } from './views/Details'
 import { QrCodeScanner } from './views/QrCodeScanner'
 
 // @TODO Determine if we should use symbol for display purposes or some other identifier for display
-type SendInput = {
+export type SendInput = {
   address: string
   asset: any
   feeType: FeeDataKey
@@ -46,16 +46,70 @@ type SendFormProps = {
   asset: AssetMarketData
 }
 
-export const Form = ({ asset: initalAsset }: SendFormProps) => {
-  const location = useLocation()
-  const history = useHistory()
+const useFormSend = () => {
   const toast = useToast()
-  const { send } = useModal()
+  const translate = useTranslate()
   const chainAdapter = useChainAdapters()
+  const { send } = useModal()
   const {
     state: { wallet }
   } = useWallet()
-  const translate = useTranslate()
+
+  const handleSend = async (data: SendInput) => {
+    if (wallet) {
+      try {
+        const path = "m/44'/60'/0'/0/0" // TODO (technojak) get path and asset precision from asset-service
+        const adapter = chainAdapter.byChain(ChainIdentifier.Ethereum)
+        const value = bnOrZero(data.crypto.amount)
+          .times(bnOrZero(10).exponentiatedBy(data.asset.decimals || 18))
+          .toFixed(0)
+        const { txToSign } = await adapter.buildSendTransaction({
+          to: data.address,
+          value,
+          erc20ContractAddress: data.asset.contractAddress,
+          wallet,
+          path,
+          fee: data.estimatedFees[data.feeType].feeUnitPrice,
+          limit: data.estimatedFees[data.feeType].feeUnits
+        })
+        const signedTx = await adapter.signTransaction({ txToSign, wallet })
+        const tx = await adapter.broadcastTransaction(signedTx)
+
+        toast({
+          title: translate('modals.send.sent', { asset: data.asset.name }),
+          description: translate('modals.send.youHaveSent', {
+            amount: data.crypto.amount,
+            symbol: data.crypto.symbol,
+            tx
+          }),
+          status: 'success',
+          duration: 9000,
+          isClosable: true,
+          position: 'top-right'
+        })
+      } catch (error) {
+        toast({
+          title: translate('modals.send.errorTite'),
+          description: translate('modals.send.errorDescription', { asset: data.asset.name }),
+          status: 'error',
+          duration: 9000,
+          isClosable: true,
+          position: 'top-right'
+        })
+      } finally {
+        send.close()
+      }
+    }
+  }
+  return {
+    handleSend
+  }
+}
+
+export const Form = ({ asset: initalAsset }: SendFormProps) => {
+  const location = useLocation()
+  const history = useHistory()
+  const { handleSend } = useFormSend()
 
   const methods = useForm<SendInput>({
     mode: 'onChange',
@@ -78,43 +132,6 @@ export const Form = ({ asset: initalAsset }: SendFormProps) => {
     /** @todo wire up asset select */
     // methods.setValue('asset', asset)
     history.push(SendRoutes.Details)
-  }
-
-  const handleSend = async (data: SendInput) => {
-    if (wallet) {
-      try {
-        const path = "m/44'/60'/0'/0/0" // TODO (technojak) get path and asset precision from asset-service
-        const adapter = chainAdapter.byChain(ChainIdentifier.Ethereum)
-        const value = bnOrZero(data.crypto.amount)
-          .times(bnOrZero(10).exponentiatedBy(data.asset.decimals || 18))
-          .toFixed(0)
-        const { txToSign } = await adapter.buildSendTransaction({
-          to: data.address,
-          value,
-          erc20ContractAddress: data.asset.contractAddress,
-          wallet,
-          path,
-          fee: data.estimatedFees[data.feeType].feeUnitPrice,
-          limit: data.estimatedFees[data.feeType].feeUnits
-        })
-        const signedTx = await adapter.signTransaction({ txToSign, wallet })
-        await adapter.broadcastTransaction(signedTx)
-        send.close()
-        toast({
-          title: translate('modals.send.sent', { asset: data.asset.name }),
-          description: translate('modals.send.youHaveSent', {
-            amount: data.crypto.amount,
-            symbol: data.crypto.symbol
-          }),
-          status: 'success',
-          duration: 9000,
-          isClosable: true,
-          position: 'top-right'
-        })
-      } catch (error) {
-        console.error(error)
-      }
-    }
   }
 
   const checkKeyDown = (event: React.KeyboardEvent<HTMLFormElement>) => {
---
+++ src/components/Modals/Send/hooks/useFormSend/useFormSend.tsx
@@ -0,0 +1,71 @@
+import { useToast } from '@chakra-ui/react'
+import { ChainIdentifier } from '@shapeshiftoss/chain-adapters'
+import { useTranslate } from 'react-polyglot'
+import { useChainAdapters } from 'context/ChainAdaptersProvider/ChainAdaptersProvider'
+import { useModal } from 'context/ModalProvider/ModalProvider'
+import { useWallet } from 'context/WalletProvider/WalletProvider'
+import { bnOrZero } from 'lib/bignumber/bignumber'
+
+import { SendInput } from '../../Form'
+
+export const useFormSend = () => {
+  const toast = useToast()
+  const translate = useTranslate()
+  const chainAdapter = useChainAdapters()
+  const { send } = useModal()
+  const {
+    state: { wallet }
+  } = useWallet()
+
+  const handleSend = async (data: SendInput) => {
+    if (wallet) {
+      try {
+        const path = "m/44'/60'/0'/0/0" // TODO (technojak) get path and asset precision from asset-service
+        const adapter = chainAdapter.byChain(ChainIdentifier.Ethereum)
+        const value = bnOrZero(data.crypto.amount)
+          .times(bnOrZero(10).exponentiatedBy(data.asset.decimals || 18))
+          .toFixed(0)
+
+        const { txToSign } = await adapter.buildSendTransaction({
+          to: data.address,
+          value,
+          erc20ContractAddress: data.asset.contractAddress,
+          wallet,
+          path,
+          fee: data.estimatedFees[data.feeType].feeUnitPrice,
+          limit: data.estimatedFees[data.feeType].feeUnits
+        })
+
+        const signedTx = await adapter.signTransaction({ txToSign, wallet })
+
+        await adapter.broadcastTransaction(signedTx)
+
+        toast({
+          title: translate('modals.send.sent', { asset: data.asset.name }),
+          description: translate('modals.send.youHaveSent', {
+            amount: data.crypto.amount,
+            symbol: data.crypto.symbol
+          }),
+          status: 'success',
+          duration: 9000,
+          isClosable: true,
+          position: 'top-right'
+        })
+      } catch (error) {
+        toast({
+          title: translate('modals.send.sent'),
+          description: translate('modals.send.somethingWentWrong'),
+          status: 'error',
+          duration: 9000,
+          isClosable: true,
+          position: 'top-right'
+        })
+      } finally {
+        send.close()
+      }
+    }
+  }
+  return {
+    handleSend
+  }
+}

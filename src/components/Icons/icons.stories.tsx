736f25fa9ba61956fbf430b5468031f98fc82d90
Boots Boogie <87733534+bootsboogie@users.noreply.github.com>
2021-09-15 09:40:18 -0600

    clean up icons story

--- src/components/Icons/icons.stories.tsx
+++ src/components/Icons/icons.stories.tsx
@@ -21,21 +21,21 @@ import { TwitterIcon } from './TwitterIcon'
 import { YouTubeIcon } from './YouTubeIcon'
 
 const icons = [
-  { icon: AssetsIcon, name: 'AssetsIcon' },
-  { icon: CircleIcon, name: 'CircleIcon' },
-  { icon: DashboardIcon, name: 'DashboardIcon' },
-  { icon: DiscordIcon, name: 'DiscordIcon' },
-  { icon: EarnIcon, name: 'EarnIcon' },
-  { icon: FacebookIcon, name: 'FacebookIcon' },
-  { icon: FoxIcon, name: 'FoxIcon' },
-  { icon: InstagramIcon, name: 'InstagramIcon' },
-  { icon: KeepKeyIcon, name: 'KeepKeyIcon' },
-  { icon: MediumIcon, name: 'MediumIcon' },
-  { icon: QRCodeIcon, name: 'QRCodeIcon' },
-  { icon: ShapeShiftVertical, name: 'ShapeShiftVertical' },
-  { icon: TelegramIcon, name: 'TelegramIcon' },
-  { icon: TwitterIcon, name: 'TwitterIcon' },
-  { icon: YouTubeIcon, name: 'YouTubeIcon' }
+  AssetsIcon,
+  CircleIcon,
+  DashboardIcon,
+  DiscordIcon,
+  EarnIcon,
+  FacebookIcon,
+  FoxIcon,
+  InstagramIcon,
+  KeepKeyIcon,
+  MediumIcon,
+  QRCodeIcon,
+  ShapeShiftVertical,
+  TelegramIcon,
+  TwitterIcon,
+  YouTubeIcon
 ]
 
 export default {
@@ -51,11 +51,11 @@ export default {
 
 export const Basic = () => (
   <HStack spacing='24px' wrap='wrap'>
-    {icons.map(({ icon, name }) => {
+    {icons.map(icon => {
       return (
         <Center flexDir='column' py={4}>
           {createElement(icon, { w: '50px', h: '50px', mb: 2 })}
-          <RawText>{name}</RawText>
+          <RawText>{`${icon.displayName}`}</RawText>
         </Center>
       )
     })}

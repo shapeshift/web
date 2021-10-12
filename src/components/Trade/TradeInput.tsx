e4ce019b389cb15e0516338784144f369b8d667e
Jacob Cooley <10103480+toshiSat@users.noreply.github.com>
2021-10-12 16:31:13 -0600

    autofix

--- src/components/Trade/TradeInput.tsx
+++ src/components/Trade/TradeInput.tsx
@@ -17,10 +17,9 @@ import { RawText, Text } from 'components/Text'
 import { TokenButton } from 'components/TokenRow/TokenButton'
 import { TokenRow } from 'components/TokenRow/TokenRow'
 import { useLocaleFormatter } from 'hooks/useLocaleFormatter/useLocaleFormatter'
+import { TradeActions, useSwapper } from 'hooks/useSwapper/useSwapper'
 import { bn } from 'lib/bignumber/bignumber'
 import { firstNonZeroDecimal } from 'lib/math'
-
-import { TradeActions, useSwapper } from 'hooks/useSwapper/useSwapper'
 
 const FiatInput = (props: InputProps) => (
   <Input

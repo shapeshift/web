433135e5a8734be4a440aea12a62d94bede506ad
Boots Boogie <87733534+bootsboogie@users.noreply.github.com>
2021-10-12 15:42:00 -0600

    use lib/assetService

--- src/components/AssetSearch/AssetSearch.tsx
+++ src/components/AssetSearch/AssetSearch.tsx
@@ -4,7 +4,7 @@ import { Asset, NetworkTypes } from '@shapeshiftoss/types'
 import sortBy from 'lodash/sortBy'
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { useAssets } from 'context/AssetProvider/AssetProvider'
+import { getAssetService, service as assetService } from 'lib/assetService'
 
 import { AssetList } from './AssetList'
 import { filterAssetsBySearchTerm } from './helpers/filterAssetsBySearchTerm/filterAssetsBySearchTerm'
@@ -26,11 +26,10 @@ export const AssetSearch = ({ onClick }: AssetSearchProps) => {
   const searchString = watch('search')
   const searching = useMemo(() => searchString.length > 0, [searchString])
 
-  const assetService = useAssets()
-
   const fetchTokens = useCallback(async () => {
     try {
-      const data = assetService.byNetwork(NetworkTypes.MAINNET)
+      if (!assetService) await getAssetService()
+      const data = assetService?.byNetwork(NetworkTypes.MAINNET)
       const sorted = sortBy(data, ['name', 'symbol'])
       setSortedAssets(sorted)
     } catch (e) {

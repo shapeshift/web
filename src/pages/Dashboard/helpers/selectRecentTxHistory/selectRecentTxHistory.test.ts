d3e9c1b62857e17ea876aa8d48695e9a90c14ca2
Boots Boogie <87733534+bootsboogie@users.noreply.github.com>
2021-10-22 15:14:09 -0600

    Add tests for selectRecentTxHistory

---
+++ src/pages/Dashboard/helpers/selectRecentTxHistory/selectRecentTxHistory.test.ts
@@ -0,0 +1,41 @@
+import { ChainTypes } from '@shapeshiftoss/types'
+import { first } from 'lodash'
+import { BtcSend, EthReceive, EthSend } from 'jest/mocks/txs'
+
+import { selectRecentTxHistory } from './selectRecentTxHistory'
+
+const firstTx = { ...EthSend, blockTime: 1634917507 }
+const secondTx = { ...EthReceive, blockTime: 1634053165 }
+const thirdTx = { ...BtcSend, blockTime: 1633633106 }
+const fourthTx = { ...EthReceive, blockTime: 1633630546 }
+const fifthTx = { ...BtcSend, blockTime: 1633625920 }
+
+describe('selectRecentTxHistory', () => {
+  it('returns all txHistory for multiple chains sorted by blockTime', () => {
+    const store = {
+      txHistory: {
+        [ChainTypes.Ethereum]: [fourthTx, secondTx, firstTx],
+        [ChainTypes.Bitcoin]: [fifthTx, thirdTx]
+      },
+      assets: {}
+    }
+    const txs = selectRecentTxHistory(store)
+    expect(txs[0].blockTime).toBe(firstTx.blockTime)
+    expect(txs[1].blockTime).toBe(secondTx.blockTime)
+    expect(txs[2].blockTime).toBe(thirdTx.blockTime)
+    expect(txs[3].blockTime).toBe(fourthTx.blockTime)
+    expect(txs[4].blockTime).toBe(fifthTx.blockTime)
+  })
+
+  it('returns only the first 10', () => {
+    const store = {
+      txHistory: {
+        [ChainTypes.Ethereum]: [fourthTx, secondTx, firstTx, EthSend, EthSend, EthReceive],
+        [ChainTypes.Bitcoin]: [fifthTx, thirdTx, BtcSend, BtcSend, BtcSend, BtcSend]
+      },
+      assets: {}
+    }
+    const txs = selectRecentTxHistory(store)
+    expect(txs.length).toBe(10)
+  })
+})

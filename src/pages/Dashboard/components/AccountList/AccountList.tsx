d9498fc36bcb9d55e4c4e50de26c8422576ebda3
Boots Boogie <87733534+bootsboogie@users.noreply.github.com>
2021-10-27 10:58:44 -0600

    Fix account types

--- src/pages/Dashboard/components/AccountList/AccountList.tsx
+++ src/pages/Dashboard/components/AccountList/AccountList.tsx
@@ -6,33 +6,35 @@ type AccountListProps = {
   accounts: Record<string, chainAdapters.Account<ChainTypes>>
 }
 
-function isAccountOfChainType<T extends ChainTypes>(
-  chainType: T,
-  x: chainAdapters.Account<ChainTypes>
-): x is chainAdapters.Account<T> {
-  return x.chain === chainType
-}
-
 export const AccountList = ({ accounts }: AccountListProps) => (
   <>
-    {Object.values(accounts).map(account => {
-      if (isAccountOfChainType(ChainTypes.Ethereum, account)) {
-        return (
-          <>
-            <AccountRow key={account.chain} balance={account.balance} chain={account.chain} />
-            {account.chainSpecific.tokens &&
-              account.chainSpecific.tokens.map(tokenAccount => (
-                <AccountRow
-                  key={`${account.chain}-${tokenAccount.contract}`}
-                  balance={tokenAccount.balance}
-                  chain={account.chain}
-                  tokenId={tokenAccount.contract}
-                />
-              ))}
-          </>
-        )
-      } else {
-        return <AccountRow key={account.chain} balance={account.balance} chain={account.chain} />
+    {Object.values(accounts).map(genericAccount => {
+      switch (genericAccount.chain) {
+        case ChainTypes.Ethereum: {
+          const account = genericAccount as chainAdapters.Account<ChainTypes.Ethereum>
+          return (
+            <>
+              <AccountRow key={account.chain} balance={account.balance} chain={account.chain} />
+              {account.chainSpecific.tokens &&
+                account.chainSpecific.tokens.map(tokenAccount => (
+                  <AccountRow
+                    key={`${account.chain}-${tokenAccount.contract}`}
+                    balance={tokenAccount.balance}
+                    chain={account.chain}
+                    tokenId={tokenAccount.contract}
+                  />
+                ))}
+            </>
+          )
+        }
+        case ChainTypes.Bitcoin: {
+          const account = genericAccount as chainAdapters.Account<ChainTypes.Bitcoin>
+          return <AccountRow key={account.chain} balance={account.balance} chain={account.chain} />
+        }
+        default: {
+          console.error(`AccountList: unknown chain ${genericAccount.chain}`)
+          return null
+        }
       }
     })}
   </>

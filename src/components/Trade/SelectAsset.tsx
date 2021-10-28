d56c8711c6525b47bc7920d69c6918da36d4eb73
Jacob Cooley <10103480+toshiSat@users.noreply.github.com>
2021-10-28 13:35:43 -0600

    Undo test changes

--- src/components/Trade/SelectAsset.tsx
+++ src/components/Trade/SelectAsset.tsx
@@ -1,6 +1,5 @@
 import { Box } from '@chakra-ui/react'
 import { Asset } from '@shapeshiftoss/types'
-import { useEffect } from 'react'
 import { RouteProps } from 'react-router-dom'
 import { AssetSearch } from 'components/AssetSearch/AssetSearch'
 import { SlideTransition } from 'components/SlideTransition'
@@ -8,9 +7,6 @@ import { SlideTransition } from 'components/SlideTransition'
 type SelectAssetProps = { onClick: (asset: Asset) => void } & RouteProps
 
 export const SelectAsset = ({ onClick }: SelectAssetProps) => {
-  useEffect(() => {
-    console.log('render')
-  }, [])
   return (
     <SlideTransition>
       <Box height='400px' p={0} display='flex' flexDir='column'>

dea85a2ada585683b83188a07a2b0fc038d0d5ba
jdambron <dreamlike3d@gmail.com>
2021-10-28 18:06:43 +0200

    fix: improve testing method and make tests independent

--- src/hooks/useLocalStorage/useLocalStorage.test.ts
+++ src/hooks/useLocalStorage/useLocalStorage.test.ts
@@ -2,28 +2,23 @@ import { act, renderHook } from '@testing-library/react-hooks'
 
 import { useLocalStorage } from './useLocalStorage'
 
-beforeAll(() => {
-  const { result } = renderHook(() => useLocalStorage<Record<string, string>>('testString', null))
-  const [, setLocalValue] = result.current
-  act(() => setLocalValue(null))
-})
-
-afterAll(() => {
+afterEach(() => {
   const { result } = renderHook(() => useLocalStorage<Record<string, string>>('testString', null))
   const [, setLocalValue] = result.current
   act(() => setLocalValue(null))
 })
 
 describe('useLocalStorage hook', () => {
-  it('should store values', () => {
+  it('should be null if not set', () => {
+    expect(localStorage.getItem('testString')).toBe(null)
+  })
+
+  it('should store string values', () => {
     const { result } = renderHook(() => useLocalStorage<Record<string, string>>('testString', null))
     const [, setLocalValue] = result.current
     act(() => setLocalValue({ testValue: 'This is a test' }))
-    expect(result.current[0]).toStrictEqual({ testValue: 'This is a test' })
-  })
-
-  it('should retrieve a stored value', () => {
-    const { result } = renderHook(() => useLocalStorage<Record<string, string>>('testString', null))
-    expect(result.current[0]).toStrictEqual({ testValue: 'This is a test' })
+    expect(JSON.parse(localStorage.getItem('testString') || '{}')).toStrictEqual({
+      testValue: 'This is a test'
+    })
   })
 })

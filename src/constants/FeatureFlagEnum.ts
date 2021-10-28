1e37ead257323febba52b078a5953eccdf13502a
ReallyBeard <89934888+reallybeard@users.noreply.github.com>
2021-10-28 14:41:30 -0500

    Update FeatureFlagEnum.ts

--- src/constants/FeatureFlagEnum.ts
+++ src/constants/FeatureFlagEnum.ts
@@ -7,6 +7,6 @@ export type Flag = typeof FeatureFlagEnum[keyof typeof FeatureFlagEnum]
 
 export const FeatureFlagEnum = {
   Test: FlagValue.On,
-  Yearn: FlagValue.On,
+  Yearn: FlagValue.Off,
   Osmosis: FlagValue.Off
 } as const

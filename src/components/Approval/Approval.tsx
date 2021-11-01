d394aa05e1f9a5fb53577072f70f7397228b5e88
DaoDev <88341957+DaoDev44@users.noreply.github.com>
2021-11-01 13:16:58 -0600

    change lodash get import

--- src/components/Approval/Approval.tsx
+++ src/components/Approval/Approval.tsx
@@ -1,5 +1,5 @@
 import { Button, Divider, Flex, Image, Link, SkeletonCircle } from '@chakra-ui/react'
-import { get } from 'lodash'
+import get from 'lodash/get'
 import { useEffect, useRef, useState } from 'react'
 import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 import { useFormContext } from 'react-hook-form'

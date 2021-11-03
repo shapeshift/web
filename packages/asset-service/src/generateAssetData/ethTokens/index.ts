9b796ef386666674456706d190abf7562883a112
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2021-11-03 15:26:24 -0600

    feat: asset service caip19s (#184)
    * feat: new types for market cap
    
    * feat: market service getByMarketCap
    
    * feat: add default overrideable args to getByMarketCap
    
    * test: stub new tests
    
    * test: getByMarketCap default args test
    
    * test: getByMarketCap override args test
    
    * docs: update asset service docs
    
    * feat: generateMarketCapData script and yarn command
    
    * chore: generated market cap data
    
    * chore: comment how to consume if rate limited
    
    * feat: can handle partial rate limiting better
    
    * Revert "docs: update asset service docs"
    
    This reverts commit a7445f7d9c516a628deb436dc5fbe47f5155f716.
    
    * chore: lint fix
    
    * feat: getMarketCap with caip19 ids
    
    * chore: tidy generate script
    
    * test: don't mutate args and fix tests
    
    * feat: change response type to be object keyed by caips
    
    * feat: don't leak coingecko id through to client
    
    * test: add types to test
    
    * chore: fix package generate script and run generate
    
    * feat: asset service add caip19s to generated data
    
    * chore: add missing dev dep to swapper and ignore swappercli from build
    
    * fix: correct types for uniswap endpoint
    
    * chore: move asset service to dev dep in swapper

--- packages/asset-service/src/generateAssetData/ethTokens/index.ts
+++ packages/asset-service/src/generateAssetData/ethTokens/index.ts
@@ -1,10 +1,12 @@
-import { ContractTypes, TokenAsset } from '@shapeshiftoss/types'
+import { caip19 } from '@shapeshiftoss/caip'
+import { ChainTypes, ContractTypes, NetworkTypes, TokenAsset } from '@shapeshiftoss/types'
 import axios from 'axios'
 import lodash from 'lodash'
 
 import { tokensToOverride } from './overrides'
 
-type CoingeckoTokenData = {
+type UniswapToken = {
+  chainId: number
   address: string
   name: string
   symbol: string
@@ -12,12 +14,24 @@ type CoingeckoTokenData = {
   logoURI: string
 }
 
+type UniswapTokenData = {
+  name: string
+  logoURI: string
+  keywords: string[]
+  timestamp: string
+  tokens: UniswapToken[]
+}
+
 export async function getTokens(): Promise<TokenAsset[]> {
-  const { data: uniswapTokenData } = await axios.get(
+  const { data: uniswapTokenData } = await axios.get<UniswapTokenData>(
     'https://tokens.coingecko.com/uniswap/all.json'
   )
 
-  const tokens = uniswapTokenData.tokens.map((token: CoingeckoTokenData) => {
+  const chain = ChainTypes.Ethereum
+  const network = NetworkTypes.MAINNET
+  const contractType = ContractTypes.ERC20
+
+  const tokens = uniswapTokenData.tokens.map((token) => {
     const overrideToken: TokenAsset | undefined = lodash.find(
       tokensToOverride,
       (override: TokenAsset) => override.tokenId === token.address
@@ -25,20 +39,22 @@ export async function getTokens(): Promise<TokenAsset[]> {
 
     if (overrideToken) return overrideToken
 
-    return {
+    const tokenId = token.address.toLowerCase()
+
+    const result: TokenAsset = {
+      caip19: caip19.toCAIP19({ chain, network, contractType, tokenId }),
       name: token.name,
       precision: token.decimals,
-      tokenId: token.address.toLowerCase(),
+      tokenId,
       contractType: ContractTypes.ERC20,
       color: '#FFFFFF', // TODO
       secondaryColor: '#FFFFFF', // TODO
       icon: token.logoURI,
-      explorer: 'https://etherscan.io',
-      explorerTxLink: 'https://etherscan.io/tx/',
       sendSupport: true,
       receiveSupport: true,
       symbol: token.symbol
     }
+    return result
   })
 
   return tokens

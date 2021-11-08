43caba572ef4a6c735ae5cfc6d727fec4229e81f
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2021-11-08 15:59:48 -0700

    Update to use btc websocket and update existing transactions instead of add duplicate

--- src/state/slices/txHistorySlice/txHistorySlice.ts
+++ src/state/slices/txHistorySlice/txHistorySlice.ts
@@ -5,13 +5,26 @@ export type Tx = chainAdapters.SubscribeTxsMessage<ChainTypes>
 
 const initialState = {} as { [key: string]: Tx[] }
 
+const updateSlice = (txs: Array<Tx>, tx: Tx): Array<Tx> => {
+  if (!txs) return [tx]
+
+  const index = txs.findIndex(t => t.txid === tx.txid)
+
+  if (txs[index]) {
+    txs[index] = tx
+    return txs
+  }
+
+  return [tx, ...txs]
+}
+
 export const txHistory = createSlice({
   name: 'txHistory',
   initialState,
   reducers: {
     onMessage(state, { payload }: { payload: { message: Tx } }) {
       const chain = payload.message.chain
-      state[chain] = state[chain] ? [...state[chain], payload.message] : [payload.message]
+      state[chain] = updateSlice(state[chain], payload.message)
     }
   }
 })

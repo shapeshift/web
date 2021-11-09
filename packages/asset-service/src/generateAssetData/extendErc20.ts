1ea0ac42962ec6c09a1fca5fa891f0753367e4a4
theoboldfrazier <88169003+theoboldfrazier@users.noreply.github.com>
2021-11-09 08:56:39 -0700

    feat: add yearn erc20 assets to asset service (#196)
    * feat: add yearn vault erc-20 assets to asset data
    
    * moved yearn vault type into types
    
    * created erc20 extend file
    
    * removed export
    
    * Updated based on code review
    
    * Fixed compiler with typecast
    
    * switched typecast to empty array if undefined
    
    * removed unused var
    
    * Fixed typo in filename.
    
    * Updated logic for creating generated assets

---
+++ packages/asset-service/src/generateAssetData/extendErc20.ts
@@ -0,0 +1,61 @@
+import { caip19 } from '@shapeshiftoss/caip'
+import { ChainTypes, ContractTypes, NetworkTypes, TokenAsset } from '@shapeshiftoss/types'
+import axios, { AxiosInstance, AxiosResponse } from 'axios'
+const axiosInstance: AxiosInstance = axios.create()
+
+type YearnApiVault = {
+  inception: number
+  address: string
+  symbol: string
+  name: string
+  display_name: string
+  icon: string
+  token: {
+    name: string
+    symbol: string
+    address: string
+    decimals: number
+    display_name: string
+    icon: string
+  }
+  tvl: {
+    total_assets: number
+    price: number
+    tvl: number
+  }
+  apy: {
+    net_apy: number
+  }
+  endorsed: boolean
+  version: string
+  decimals: number
+  type: string
+  emergency_shutdown: boolean
+}
+
+export const extendErc20 = async (): Promise<TokenAsset[]> => {
+  const response: AxiosResponse = await axiosInstance.get<YearnApiVault[]>(
+    `https://api.yearn.finance/v1/chains/1/vaults/all`
+  )
+  const yearnVaults: YearnApiVault[] = response?.data
+  return yearnVaults.map((vault: YearnApiVault) => {
+    return {
+      color: '#FFFFFF',
+      contractType: ContractTypes.ERC20,
+      icon: vault.icon,
+      name: vault.name,
+      precision: vault.decimals,
+      receiveSupport: true,
+      secondaryColor: '#FFFFFF',
+      sendSupport: true,
+      symbol: vault.symbol,
+      tokenId: vault.address,
+      caip19: caip19.toCAIP19({
+        chain: ChainTypes.Ethereum,
+        network: NetworkTypes.MAINNET,
+        tokenId: vault.address,
+        contractType: ContractTypes.ERC20
+      })
+    }
+  })
+}
--- packages/asset-service/src/generateAssetData/generateAssetData.ts
+++ packages/asset-service/src/generateAssetData/generateAssetData.ts
@@ -3,13 +3,20 @@ import fs from 'fs'
 
 import { baseAssets } from './baseAssets'
 import { getTokens } from './ethTokens'
+import { extendErc20 } from './extendErc20'
 
 const generateAssetData = async () => {
   const generatedAssetData = await Promise.all(
     baseAssets.map(async (baseAsset) => {
       if (baseAsset.chain === ChainTypes.Ethereum && baseAsset.network === NetworkTypes.MAINNET) {
-        const ethTokens = await getTokens()
-        const baseAssetWithTokens: BaseAsset = { ...baseAsset, tokens: ethTokens }
+        const [ethTokens, extendedERC20Tokens] = await Promise.all([
+          await getTokens(),
+          await extendErc20()
+        ])
+        const baseAssetWithTokens: BaseAsset = {
+          ...baseAsset,
+          tokens: ethTokens.concat(extendedERC20Tokens)
+        }
         return baseAssetWithTokens
       } else {
         return baseAsset

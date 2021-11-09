d7943975c35d7ed1d07c1aeeca838feb7c3e8026
theoboldfrazier <theobold.frazier@gmail.com>
2021-11-09 16:16:52 -0700

    fixed lint

--- src/state/slices/assetsSlice/assetsSlice.ts
+++ src/state/slices/assetsSlice/assetsSlice.ts
@@ -21,7 +21,10 @@ export const fetchAsset = createAsyncThunk(
       const service = await getAssetService()
 
       const assetData = service?.byTokenId({ chain, network, tokenId })
-      const description = await service?.description({ chain, tokenData: tokenId ? assetData : null })
+      const description = await service?.description({
+        chain,
+        tokenData: tokenId ? assetData : null
+      })
       if (!assetData) return {}
       if (!description) return { [tokenId || chain]: assetData }
       return { [tokenId || chain]: { ...assetData, description } }

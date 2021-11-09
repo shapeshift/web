57e4b7ba442c698e4a333a22296c47f8b7fa527f
pastaghost <62026038+pastaghost@users.noreply.github.com>
2021-11-09 10:21:34 -0700

    feat: add portis wallet support (#301)
    * initial metamask ui
    
    * start adding connect UI
    
    * import hdwallet-metamask
    
    * pair and get address working
    
    * metamask paired, eth address, and tx history
    
    * receive modal works
    
    * rm unused
    
    * Update MetaMaskIcon
    
    * Fix lint errors
    
    * Bump HDWallet dependency version
    
    * update deps
    
    * metamask sends  work
    
    * fix typo
    
    * chore: bump hdwallet release
    
    * chore: lock file cleanup
    
    * chore: rm unused translations
    
    * chore: fix lint and build errors
    
    * chore: update lib dependencies
    
    * fix: add caip-19 field to asset definitions
    
    * feat: wip - add preliminary portis support
    
    * fix: use createIcon for portis/metamask icons
    
    * fix: initialize portis wallet with dapp ID
    
    * fix: spelling correction
    
    * Refactor usePortisSuccess hook
    
    * fix: apply suggestions from code review
    
    * fix: empty commit to retrigger ci
    
    * fix: run linter
    
    Co-authored-by: Mike <mvp>

---
+++ src/context/WalletProvider/Portis/hooks/usePortisSuccess/usePortisSuccess.ts
@@ -0,0 +1,36 @@
+import { useEffect } from 'react'
+import { KeyManager, SUPPORTED_WALLETS } from 'context/WalletProvider/config'
+import { useWallet, WalletActions } from 'context/WalletProvider/WalletProvider'
+import { useStateIfMounted } from 'hooks/useStateIfMounted/useStateIfMounted'
+
+export const usePortisSuccess = () => {
+  const [isSuccessful, setIsSuccessful] = useStateIfMounted<boolean | null>(null)
+  const { state, dispatch } = useWallet()
+
+  useEffect(() => {
+    ;(async () => {
+      try {
+        // TODO handle else
+        if (state.adapters?.has(KeyManager.Portis)) {
+          const wallet = (await state.adapters.get(KeyManager.Portis)?.pairDevice()) ?? null
+          if (!wallet) {
+            throw new Error('Failed to initialize Portis wallet')
+          }
+          const { name, icon } = SUPPORTED_WALLETS[KeyManager.Portis]
+          dispatch({
+            type: WalletActions.SET_WALLET,
+            payload: { wallet, name, icon, deviceId: 'test' }
+          })
+          setIsSuccessful(true)
+          dispatch({ type: WalletActions.SET_IS_CONNECTED, payload: true })
+        }
+      } catch (error) {
+        console.error('Failed to load device', error)
+        setIsSuccessful(false)
+      }
+    })()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  })
+
+  return { isSuccessful }
+}

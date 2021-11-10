41195c4b618054fe08ff8c4f07502f18ec2e41b4
theoboldfrazier <theobold.frazier@gmail.com>
2021-11-10 11:48:29 -0700

    chore: updated description call and bumped asset-serivce

--- src/state/slices/assetsSlice/assetsSlice.ts
+++ src/state/slices/assetsSlice/assetsSlice.ts
@@ -21,10 +21,7 @@ export const fetchAsset = createAsyncThunk(
       const service = await getAssetService()
 
       const assetData = service?.byTokenId({ chain, network, tokenId })
-      const description = await service?.description({
-        chain,
-        tokenData: tokenId ? (assetData as TokenAsset) : undefined
-      })
+      const description = await service?.description({ asset: assetData })
       if (!assetData) return {}
       if (!description) return { [tokenId || chain]: assetData }
       return { [tokenId || chain]: { ...assetData, description } }

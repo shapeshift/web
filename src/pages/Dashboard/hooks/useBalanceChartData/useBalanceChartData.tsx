45bc9f48ed2cb6eea266b672879c7dc5766aad9a
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2021-11-11 11:01:41 -0700

    wip: putting txs in buckets, need market data in store next

--- src/pages/Dashboard/hooks/useBalanceChartData/useBalanceChartData.tsx
+++ src/pages/Dashboard/hooks/useBalanceChartData/useBalanceChartData.tsx
@@ -1,7 +1,11 @@
 import { HistoryData, HistoryTimeframe } from '@shapeshiftoss/types'
 import dayjs from 'dayjs'
+import { fill, head, reduce, reverse } from 'lodash'
 import { useEffect, useState } from 'react'
-import { usePortfolio } from 'pages/Dashboard/contexts/PortfolioContext'
+import { useSelector } from 'react-redux'
+import { bn } from 'lib/bignumber/bignumber'
+import { selectRecentTxHistory } from 'pages/Dashboard/helpers/selectRecentTxHistory/selectRecentTxHistory'
+import { Tx } from 'state/slices/txHistorySlice/txHistorySlice'
 
 type UseBalanceChartDataReturn = {
   balanceChartData: Array<HistoryData>
@@ -12,22 +16,108 @@ type UseBalanceChartDataArgs = {
   timeframe: HistoryTimeframe
 }
 
+type Bucket = {
+  start: dayjs.Dayjs
+  end: dayjs.Dayjs
+  txs: Tx[]
+}
+
+type BucketMeta = {
+  count: number
+  duration: number
+  unit: dayjs.ManipulateType
+}
+
+type MakeBucketsReturn = {
+  buckets: Array<Bucket>
+  meta: BucketMeta
+}
+
+export const makeBuckets = (timeframe: HistoryTimeframe): MakeBucketsReturn => {
+  const makeReducer = (duration: number, unit: dayjs.ManipulateType) => {
+    const now = dayjs()
+    return (acc: Array<Bucket>, _cur: unknown, idx: number) => {
+      const end = now.subtract(idx, unit)
+      const start = end.subtract(duration, unit).add(1, 'second')
+      const txs: Tx[] = []
+      const bucket = { start, end, txs }
+      acc.push(bucket)
+      return acc
+    }
+  }
+
+  const timeframeMap = {
+    [HistoryTimeframe.HOUR]: { count: 60, duration: 1, unit: 'minute' },
+    [HistoryTimeframe.DAY]: { count: 95, duration: 15, unit: 'minutes' },
+    [HistoryTimeframe.WEEK]: { count: 84, duration: 2, unit: 'hours' },
+    [HistoryTimeframe.MONTH]: { count: 90, duration: 8, unit: 'hours' },
+    [HistoryTimeframe.YEAR]: { count: 52, duration: 1, unit: 'week' },
+    [HistoryTimeframe.ALL]: { count: 60, duration: 1, unit: 'weeks' }
+  }
+
+  const meta = timeframeMap[timeframe]
+  const { count, duration, unit } = meta
+  const buckets = reverse(reduce(fill(Array(count), 0), makeReducer(duration, unit), []))
+  return { buckets, meta }
+}
+
 export const useBalanceChartData = (args: UseBalanceChartDataArgs): UseBalanceChartDataReturn => {
-  // const { timeframe } = args
-  const [balanceChartLoading, setBalanceChartLoading] = useState(true)
-  const { /* balances, */ loading: portfolioLoading } = usePortfolio()
+  const { timeframe } = args
+  const [balanceChartLoading /*, setBalanceChartLoading */] = useState(true)
+  const [balanceChartData, setBalanceChartData] = useState<HistoryData[]>([])
+  // const { balances, loading: portfolioLoading } = usePortfolio()
+  const txs = useSelector(selectRecentTxHistory)
+
+  // useEffect(() => {
+  //   if (portfolioLoading) return
+  //   ;(async () => {
+  //     try {
+  //       const promises = await Promise.all(
+  //         Object.values(balances).map(account => {
+  //           const { chain } = account
+  //           const tokenId = account.contract
+  //           const args = { chain, timeframe, tokenId }
+  //           return getPriceHistory(args)
+  //         })
+  //       )
+  //     } catch (e) {
+  //       console.error('useBalanceChartData market data error', e)
+  //     } finally {
+  //       setBalanceChartLoading(false)
+  //     }
+  //   })()
+  // }, [timeframe, balances, setBalanceChartLoading, portfolioLoading])
 
   useEffect(() => {
-    if (portfolioLoading) return
-    setBalanceChartLoading(false)
-  }, [setBalanceChartLoading, portfolioLoading])
+    const { buckets, meta } = makeBuckets(timeframe)
+    const start = head(buckets)!.start
+    const seenTxs = new Map()
+    // txs are potentially a lot longer than buckets (~100), iterate the long list once
+    const bucketedTxs = txs.reduce((acc, tx) => {
+      // if we've seen this tx already ignore it
+      if (seenTxs.has(tx.blockHash)) return acc
+      const txDayjs = dayjs(tx.blockTime * 1000)
+      // if the tx is before the time domain ignore it, it can't be past the end (now)
+      if (txDayjs.isBefore(start)) return acc
+      const { unit } = meta
+      // the number of time units from start to this tx
+      const bucketIndex = txDayjs.diff(start, unit as dayjs.OpUnitType)
+      // add to the correct bucket
+      acc[bucketIndex].txs.push(tx)
+      // mark as seen
+      seenTxs.set(tx.blockHash, true)
+      return acc
+    }, buckets)
 
-  const balanceChartData: Array<HistoryData> = [
-    { date: dayjs().subtract(3, 'weeks').toISOString(), price: 10 },
-    { date: dayjs().subtract(2, 'weeks').toISOString(), price: 32 },
-    { date: dayjs().subtract(1, 'weeks').toISOString(), price: 50 },
-    { date: dayjs().subtract(1, 'day').toISOString(), price: 5 }
-  ]
+    console.info(bucketedTxs)
+
+    // return fake data
+    const fakeData: Array<HistoryData> = buckets.map(bucket => ({
+      price: bn(Math.random()).times(420.69).decimalPlaces(2).toNumber(),
+      date: bucket.end.toISOString()
+    }))
+    setBalanceChartData(fakeData)
+  }, [txs, timeframe, setBalanceChartData])
 
   const result = { balanceChartData, balanceChartLoading }
   return result

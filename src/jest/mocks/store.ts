a9606282e10bb3b47e7bca91e95057a95a322552
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2021-11-11 19:26:13 -0700

    test: munge tests because bounty

--- src/jest/mocks/store.ts
+++ src/jest/mocks/store.ts
@@ -1,9 +1,18 @@
+import { HistoryTimeframe } from '@shapeshiftoss/types'
 import { TxHistory } from 'state/slices/txHistorySlice/txHistorySlice'
 
 export const mockStore = {
   assets: {},
   marketData: {
     marketData: {},
+    priceHistory: {
+      [HistoryTimeframe.DAY]: {},
+      [HistoryTimeframe.HOUR]: {},
+      [HistoryTimeframe.WEEK]: {},
+      [HistoryTimeframe.MONTH]: {},
+      [HistoryTimeframe.YEAR]: {},
+      [HistoryTimeframe.ALL]: {}
+    },
     loading: false
   },
   txHistory: {} as TxHistory,

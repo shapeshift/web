fa8c3a2adc38297bca47ed0aec50c23a79d65bc3
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2021-11-17 12:02:20 -0700

    feat: useWalletsSupportsChain hook, hide send/receive, account type buttons, and tx history on bitcoin if metamask or other non btc wallet connected

---
+++ src/hooks/useWalletSupportsChain/useWalletSupportsChain.ts
@@ -0,0 +1,25 @@
+import { caip2 } from '@shapeshiftoss/caip'
+import { HDWallet, supportsBTC, supportsETH } from '@shapeshiftoss/hdwallet-core'
+import { Asset, ChainTypes, NetworkTypes } from '@shapeshiftoss/types'
+
+type UseWalletSupportsChainArgs = { asset: Asset; wallet: HDWallet | null }
+type UseWalletSupportsChain = (args: UseWalletSupportsChainArgs) => boolean
+
+export const useWalletSupportsChain: UseWalletSupportsChain = ({ asset, wallet }) => {
+  if (!wallet) return false
+  const ethCAIP2 = caip2.toCAIP2({ chain: ChainTypes.Ethereum, network: NetworkTypes.MAINNET })
+  const btcCAIP2 = caip2.toCAIP2({ chain: ChainTypes.Bitcoin, network: NetworkTypes.MAINNET })
+  const assetCAIP2 = caip2.toCAIP2({ chain: asset.chain, network: asset.network })
+  switch (assetCAIP2) {
+    case ethCAIP2: {
+      return supportsETH(wallet)
+    }
+    case btcCAIP2: {
+      return supportsBTC(wallet)
+    }
+    default: {
+      console.error(`useWalletSupportsChain: unknown caip2 ${assetCAIP2}`)
+      return false
+    }
+  }
+}

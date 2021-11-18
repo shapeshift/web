ea94484290763fbc43ba2872288c7e3ef09f1ecc
ReallyBeard <89934888+reallybeard@users.noreply.github.com>
2021-11-18 17:35:21 -0600

    Update NativeLoad.tsx

--- src/context/WalletProvider/NativeWallet/components/NativeLoad.tsx
+++ src/context/WalletProvider/NativeWallet/components/NativeLoad.tsx
@@ -101,10 +101,9 @@ export const NativeLoad = () => {
           {wallets.map((wallet, i) => {
             return (
               <Row
-                key={i}
+                key={wallet.id}
                 mx={-4}
                 py={2}
-                key={wallet.id}
                 alignItems='center'
                 justifyContent='space-between'
                 variant='btn-ghost'

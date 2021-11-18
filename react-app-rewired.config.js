37100262682392b1f017ef9586ef9646bd0b21ac
Reid Rankin <reidrankin@gmail.com>
2021-11-17 19:51:34 -0500

    feat: non-csp security headers

--- react-app-rewired.config.js
+++ react-app-rewired.config.js
@@ -5,7 +5,13 @@ module.exports = {
   devServer: configFunction => {
     return (proxy, allowedHost) => {
       const config = configFunction(proxy, allowedHost)
-      config.headers = {}
+      config.headers = {
+        'Cross-Origin-Opener-Policy': 'same-origin',
+        'Permissions-Policy': 'document-domain=()',
+        'Referrer-Policy': 'no-referrer',
+        'X-Content-Type-Options': 'nosniff',
+        'X-Frame-Options': 'DENY'
+      }
       return config
     }
   }

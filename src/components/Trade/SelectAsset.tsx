30fcdd6b6f3d40acaa47cf29601b8c46bf90e6f1
DaoDev <88341957+DaoDev44@users.noreply.github.com>
2021-11-18 16:41:02 -0700

    use caip2 for asset filter

--- src/components/Trade/SelectAsset.tsx
+++ src/components/Trade/SelectAsset.tsx
@@ -1,5 +1,6 @@
 import { Box } from '@chakra-ui/react'
-import { Asset } from '@shapeshiftoss/types'
+import { caip2 } from '@shapeshiftoss/caip'
+import { Asset, ChainTypes, NetworkTypes } from '@shapeshiftoss/types'
 import { RouteProps } from 'react-router-dom'
 import { AssetSearch } from 'components/AssetSearch/AssetSearch'
 import { SlideTransition } from 'components/SlideTransition'
@@ -8,8 +9,11 @@ type SelectAssetProps = { onClick: (asset: Asset) => void } & RouteProps
 
 export const SelectAsset = ({ onClick }: SelectAssetProps) => {
   // Filters the asset search to only show eth/erc20 assets
+  const ethCAIP2 = caip2.toCAIP2({ chain: ChainTypes.Ethereum, network: NetworkTypes.MAINNET })
   const filterByCaip19 = (assets: Asset[]): Asset[] => {
-    return assets.filter((asset: Asset) => asset.caip19.includes('eip155:1'))
+    return assets.filter(
+      ({ chain, network }: Asset) => caip2.toCAIP2({ chain, network }) === ethCAIP2
+    )
   }
 
   return (

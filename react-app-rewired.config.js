2df62ed4f8cd088e06f1d82a565c14b9bf4a31d0
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2021-11-18 19:28:45 -0700

    chore: fix jest test locally

--- react-app-rewired.config.js
+++ react-app-rewired.config.js
@@ -2,7 +2,7 @@
  * React App Rewired Config
  */
 const headers = require('./headers')
-process.env.REACT_APP_CSP_META = headers.cspMeta ?? ''
+process.env.REACT_APP_CSP_META = headers.cspMeta || ''
 
 module.exports = {
   devServer: configFunction => {

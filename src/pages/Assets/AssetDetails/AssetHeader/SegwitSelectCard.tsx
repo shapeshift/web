86a9c9b6509b4bede47c276db097bbd450101e35
Julien Dambron <dreamlike3d@gmail.com>
2021-11-19 15:52:23 +0100

    feat: handle btc formats via a dropdown

--- src/pages/Assets/AssetDetails/AssetHeader/SegwitSelectCard.tsx
+++ src/pages/Assets/AssetDetails/AssetHeader/SegwitSelectCard.tsx
@@ -1,13 +1,14 @@
-import { Button } from '@chakra-ui/react'
-import { ChainTypes } from '@shapeshiftoss/types'
+import { Select } from '@chakra-ui/react'
+import { ChainTypes, UtxoAccountType } from '@shapeshiftoss/types'
+import { useTranslate } from 'react-polyglot'
 import { useDispatch, useSelector } from 'react-redux'
 import { Card } from 'components/Card/Card'
-import { Text } from 'components/Text'
 import { ReduxState } from 'state/reducer'
 import { preferences, supportedAccountTypes } from 'state/slices/preferencesSlice/preferencesSlice'
 
 export const SegwitSelectCard = ({ chain }: { chain: ChainTypes }) => {
   const dispatch = useDispatch()
+  const translate = useTranslate()
   const accountTypes = supportedAccountTypes[chain]
   const currentAccountType = useSelector(
     (state: ReduxState) => state.preferences.accountTypes[chain]
@@ -15,24 +16,31 @@ export const SegwitSelectCard = ({ chain }: { chain: ChainTypes }) => {
 
   return (
     <Card.Body hidden={!accountTypes}>
-      {accountTypes?.map((accountType, i) => (
-        <Button
-          key={i}
-          size='sm'
-          colorScheme={currentAccountType === accountType ? 'white' : 'blue'}
-          variant='ghost'
-          onClick={() =>
-            dispatch(
-              preferences.actions.setAccountType({
-                key: chain,
-                value: accountType
-              })
-            )
-          }
-        >
-          <Text translation={`assets.assetDetails.assetHeader.${accountType}`} />
-        </Button>
-      ))}
+      <Select
+        size='sm'
+        value={currentAccountType}
+        onChange={option =>
+          dispatch(
+            preferences.actions.setAccountType({
+              key: chain,
+              value: option.target.value
+            })
+          )
+        }
+      >
+        {accountTypes?.map(accountType => (
+          <option
+            value={accountType}
+            title={
+              accountType === UtxoAccountType.SegwitNative
+                ? translate('assets.assetDetails.assetHeader.SegwitNativeTooltip')
+                : undefined
+            }
+          >
+            {translate(`assets.assetDetails.assetHeader.${accountType}`)}
+          </option>
+        ))}
+      </Select>
     </Card.Body>
   )
 }

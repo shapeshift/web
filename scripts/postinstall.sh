bda7428b5ff322abc579dabe5d5b376730b97d33
Chris Thompson <chris@thompson-web.org>
2021-11-30 16:04:11 -0700

    fix: postinstall script doesn't work correctly on macOS

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-LOCK_HASH=$(sha1sum yarn.lock)
+# Different command for macOS and Linux
+COMMAND="sha1sum"
+# Check if we're running macOS
+if hash shasum 2> /dev/null; then
+  COMMAND="shasum"
+fi
+
+LOCK_HASH=$($COMMAND yarn.lock)
 yarn minify
-LOCK_HASH_AFTER=$(sha1sum yarn.lock)
+LOCK_HASH_AFTER=$($COMMAND yarn.lock)
 if [ "$LOCK_HASH" != "$LOCK_HASH_AFTER" ]; then
   yarn
 fi

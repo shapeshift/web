91252577d88351b0c2b39bf348aff2527169ee1d
Chris Thompson <chris@thompson-web.org>
2021-11-30 16:19:17 -0700

    chore: parse just the sha hash

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Different command for macOS and Linux
-COMMAND="shasum"
+COMMAND="shasum -a 1"
 # Fallback if we don't have "shasum" installed
 if hash sha1sum 2> /dev/null; then
   COMMAND="sha1sum"
 fi
 
-LOCK_HASH=$($COMMAND yarn.lock)
+LOCK_HASH=$($COMMAND yarn.lock | cut -d ' ' -f 1)
 yarn minify
-LOCK_HASH_AFTER=$($COMMAND yarn.lock)
+LOCK_HASH_AFTER=$($COMMAND yarn.lock | cut -d ' ' -f 1)
 
 echo "[$LOCK_HASH] vs [$LOCK_HASH_AFTER]"
 if [ "$LOCK_HASH" != "$LOCK_HASH_AFTER" ]; then

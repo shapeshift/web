22c485435b97c3a06b1e30fcb12579920ced5743
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2021-11-30 17:40:28 -0700

    chore: remove unused fns

--- src/state/slices/txHistorySlice/txHistorySlice.ts
+++ src/state/slices/txHistorySlice/txHistorySlice.ts
@@ -2,10 +2,9 @@ import { createSlice } from '@reduxjs/toolkit'
 import { CAIP2, CAIP19 } from '@shapeshiftoss/caip'
 import { chainAdapters, ChainTypes, UtxoAccountType } from '@shapeshiftoss/types'
 import filter from 'lodash/filter'
-import isEqual from 'lodash/isEqual'
 import orderBy from 'lodash/orderBy'
 import values from 'lodash/values'
-import { createSelector, createSelectorCreator, defaultMemoize } from 'reselect'
+import { createSelector } from 'reselect'
 import { caip2FromTx, caip19FromTx } from 'lib/txs'
 import { ReduxState } from 'state/reducer'
 
@@ -105,26 +104,16 @@ export const selectTxHistoryByFilter = createSelector(
   }
 )
 
-const createDeepEqualSelector = createSelectorCreator(defaultMemoize, isEqual)
-
-const _selectLastNTxIds = createSelector(
-  (state: ReduxState) => state.txHistory.ids, // ids
-  (_state: ReduxState, count: number) => count, // count
+export const selectLastNTxIds = createSelector(
+  (state: ReduxState) => state.txHistory.ids,
+  (_state: ReduxState, count: number) => count,
   (ids, count) => ids.slice(0, count)
 )
-
-export const selectLastNTxIds = createDeepEqualSelector(_selectLastNTxIds, ids => ids)
 
 export const selectTxById = createSelector(
   (state: ReduxState) => state.txHistory.byId,
   (_state: ReduxState, txId: string) => txId,
   (txsById, txId) => txsById[txId]
-)
-
-export const selectTxIdsByCAIP19 = createSelector(
-  (state: ReduxState) => state.txHistory.ids,
-  (_state: ReduxState, caip19: string) => caip19,
-  (ids, caip19) => ids.filter(id => id.includes(caip19))
 )
 
 export const selectTxIdsByFilter = createSelector(

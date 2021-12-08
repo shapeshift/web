957166bba6523e4992d97c89c266d895823f331e
Chris Thompson <1958266+cjthompson@users.noreply.github.com>
2021-12-08 11:49:35 -0700

    chore: update the postinstall script to work with macOS (#522)
    * fix: postinstall script doesn't work correctly on macOS
    
    * chore: add debug out
    
    * chore: default to use shasum instead of sha1sum
    
    * chore: parse just the sha hash

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-LOCK_HASH=$(sha1sum yarn.lock)
+# Different command for macOS and Linux
+COMMAND="shasum -a 1"
+# Fallback if we don't have "shasum" installed
+if hash sha1sum 2> /dev/null; then
+  COMMAND="sha1sum"
+fi
+
+LOCK_HASH=$($COMMAND yarn.lock | cut -d ' ' -f 1)
 yarn minify
-LOCK_HASH_AFTER=$(sha1sum yarn.lock)
+LOCK_HASH_AFTER=$($COMMAND yarn.lock | cut -d ' ' -f 1)
+
+echo "[$LOCK_HASH] vs [$LOCK_HASH_AFTER]"
 if [ "$LOCK_HASH" != "$LOCK_HASH_AFTER" ]; then
   yarn
 fi

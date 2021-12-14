dc9ed06df74046e1176b060ac831c08d732f197c
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2021-12-15 09:33:06 +1100

    feat: fetch market data for assets outside top 1000 when portfolio is loaded

--- src/context/PortfolioProvider/PortfolioContext.tsx
+++ src/context/PortfolioProvider/PortfolioContext.tsx
@@ -1,23 +1,52 @@
 import isEmpty from 'lodash/isEmpty'
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useAccountSpecifiers } from 'hooks/useAccountSpecifiers/useAccountSpecifiers'
 import { useGetAssetsQuery } from 'state/slices/assetsSlice/assetsSlice'
-import { useFindAllQuery } from 'state/slices/marketDataSlice/marketDataSlice'
-import { portfolioApi } from 'state/slices/portfolioSlice/portfolioSlice'
+import {
+  marketApi,
+  selectMarketData,
+  useFindAllQuery
+} from 'state/slices/marketDataSlice/marketDataSlice'
+import { portfolioApi, selectPortfolioAssetIds } from 'state/slices/portfolioSlice/portfolioSlice'
 
 // TODO(0xdef1cafe): make this a data provider
 export const PortfolioProvider = ({ children }: { children: React.ReactNode }) => {
   const dispatch = useDispatch()
-  useGetAssetsQuery() // load all assets
-  useFindAllQuery() // load top 1000 assets market data
-  const pubkeys = useAccountSpecifiers()
+  // immediately load all assets, before the wallet is even connected,
+  // so the app is functional and ready
+  useGetAssetsQuery()
 
+  // load top 1000 assets market data
+  // this is needed to sort assets by market cap
+  // and covers most assets users will have
+  useFindAllQuery()
+  const accountSpecifiers = useAccountSpecifiers()
+
+  // once the wallet is connected, reach out to unchained to fetch
+  // accounts for each chain/account specifier combination
   useEffect(() => {
-    if (isEmpty(pubkeys)) return
+    if (isEmpty(accountSpecifiers)) return
     // fetch each account
-    pubkeys.forEach(pubkey => dispatch(portfolioApi.endpoints.getAccount.initiate(pubkey)))
-  }, [dispatch, pubkeys])
+    accountSpecifiers.forEach(accountSpecifier =>
+      dispatch(portfolioApi.endpoints.getAccount.initiate(accountSpecifier))
+    )
+  }, [dispatch, accountSpecifiers])
+
+  // we only prefetch market data for the top 1000 assets
+  // once the portfolio has loaded, check we have market data
+  // for more obscure assets, if we don't have it, fetch it
+  const portfolioAssetIds = useSelector(selectPortfolioAssetIds)
+  const marketData = useSelector(selectMarketData)
+  useEffect(() => {
+    if (!portfolioAssetIds.length) return
+    portfolioAssetIds.forEach(assetId => {
+      if (!marketData[assetId]) {
+        debugger
+        dispatch(marketApi.endpoints.findByCaip19.initiate(assetId))
+      }
+    })
+  }, [portfolioAssetIds, marketData, dispatch])
 
   return <>{children}</>
 }

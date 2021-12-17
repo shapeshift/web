21e87cd50498e1189f5554cbf9edd1c0059e29d9
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2021-12-17 16:15:21 +1100

    chore: comment util

--- src/state/slices/txHistorySlice/utils.ts
+++ src/state/slices/txHistorySlice/utils.ts
@@ -3,8 +3,11 @@ import { CAIP19 } from '@shapeshiftoss/caip'
 import { Tx } from './txHistorySlice'
 
 export const getRelatedAssetIds = (tx: Tx): CAIP19[] => {
+  // we only want unique ids
   const relatedAssets = new Set<CAIP19>()
+  // we want tokens to display on the fee asset
   if (tx.fee?.caip19) relatedAssets.add(tx.fee?.caip19)
+  // all related transfers in a tx
   tx.transfers.forEach(transfer => relatedAssets.add(transfer.caip19))
   return Array.from(relatedAssets)
 }

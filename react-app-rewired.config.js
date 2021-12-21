4df2deb4f2f8ceae9662d7ed3828a4776b43eefc
Reid Rankin <reidrankin@gmail.com>
2021-12-20 21:26:52 -0500

    chore: upgrade to create-react-app 5

--- react-app-rewired.config.js
+++ react-app-rewired.config.js
@@ -1,9 +1,14 @@
 /**
  * React App Rewired Config
  */
+const _ = require('lodash')
+const path = require('path')
+const webpack = require('webpack')
+
 const headers = require('./headers')
 process.env.REACT_APP_CSP_META = headers.cspMeta ?? ''
 
+// This ensures environment variables will be enumerated in order, which affects the build products.
 const reactEnvEntries = Object.entries(process.env)
   .filter(([k]) => k.startsWith('REACT_APP'))
   .sort((a, b) => {
@@ -15,37 +20,78 @@ reactEnvEntries.forEach(([k]) => delete process.env[k])
 reactEnvEntries.forEach(([k, v]) => (process.env[k] = v))
 
 module.exports = {
-  webpack: config => {
-    // Cloudflare Pages has a max asset size of 25 MiB. Without limiting the chunk size,
-    // generated chunks or source maps may exceed this limit. 8 MiB seems to keep the max
-    // gzipped chunk size ~1MiB and the max source map size ~12MiB, which avoids tripping
-    // CRA's "The bundle size is significantly larger than recommended" warning.
-    //
-    // This file isn't transpiled, and might be run in node 14, which doesn't support ??=,
-    // ??, or even ||=.
-    config.optimization = config.optimization || {}
-    config.optimization.splitChunks = config.optimization.splitChunks || {}
-    config.optimization.splitChunks.maxSize = 8 * 1024 * 1024
+  webpack: (config, mode) => {
+    const isProduction = mode === 'production'
+    const isDevelopment = mode === 'development'
 
-    // This will be unnessecary in react-scripts ^4.1
-    // ref: https://github.com/facebook/create-react-app/issues/8096#issuecomment-751894155
-    // ref: https://github.com/facebook/create-react-app/pull/10004#issuecomment-871945665
-    // ref: https://github.com/facebook/create-react-app/issues/7135#issuecomment-497102755
-    switch (process.env.NODE_ENV) {
-      case 'production': {
-        config.plugins = config.plugins.filter(plugin => {
-          return plugin.constructor.name !== 'ForkTsCheckerWebpackPlugin'
-        })
-        break
+    // Initialize top-level arrays just in case they're missing for some reason.
+    _.merge(config, {
+      plugins: [],
+      ignoreWarnings: [],
+    })
+
+    // Webpack 5 no longer bundles polyfills for default Node modules, but we depend on some
+    // packages that need them to work.
+    //
+    // (If https://github.com/facebook/create-react-app/issues/11756 ever gets officially fixed,
+    // we should probably align with whatever solution is chosen.)
+    _.merge(config, {
+      resolve: {
+        fallback: {
+          crypto: require.resolve('crypto-browserify'),
+          http: require.resolve('stream-http'),
+          https: require.resolve('https-browserify'),
+          stream: require.resolve('stream-browserify'),
+          zlib: require.resolve('browserify-zlib'),
+        }
+      },
+      // Also provide polyfills for some Node globals.
+      plugins: [...config.plugins, new webpack.ProvidePlugin({
+        Buffer: ['buffer/', 'Buffer'],
+        process: ['process/browser.js'],
+      })]
+    })
+
+    // Cloudflare Pages has a max asset size of 25 MiB. Without limiting the chunk size,
+    // generated chunks or source maps may exceed this limit. 6 MiB seems to keep the max
+    // gzipped chunk size ~1MiB and the max source map size ~8MiB, which avoids tripping
+    // CRA's "The bundle size is significantly larger than recommended" warning.
+    _.merge(config, isProduction ? {
+      optimization: {
+        splitChunks: {
+          chunks: 'all',
+          name: isDevelopment ? undefined : false, // _.merge() ignores undefined
+          maxSize: 6 * 1024 * 1024,
+        },
+        // This uses numerically-ascending chunk IDs with no gaps, a la Webpack 4. Webpack 5
+        // numbers chunks differently, and it's not obvious that they're deterministic. If
+        // we can determine that chunk ids are deterministic without this option, it can go.
+        chunkIds: 'natural',
       }
-      default: {
-        const forkTsCheckerWebpackPlugin = config.plugins.find(plugin => {
-          return plugin.constructor.name === 'ForkTsCheckerWebpackPlugin'
-        })
-        forkTsCheckerWebpackPlugin.memoryLimit = 4096
-        break
+    } : undefined)
+
+    // Webpack uses MD4 by default, but SHA-256 can be verified with standard tooling.
+    _.merge(config, {
+      output: {
+        hashFunction: 'sha256',
       }
-    }
+    })
+
+    // Ignore warnings raised by source-map-loader. Some third party packages ship misconfigured
+    // sourcemap paths and cause many spurious warnings.
+    //
+    // Removable when https://github.com/facebook/create-react-app/pull/11752 is merged upstream.
+    _.merge(config, {
+      ignoreWarnings: [...config.ignoreWarnings, 
+        function ignoreSourceMapLoaderWarnings(warning) {
+          return (
+            warning.module?.resource?.includes?.('node_modules') &&
+            warning.details?.includes?.('source-map-loader')
+          );
+        }
+      ],
+    })
+
     return config
   },
   devServer: configFunction => {

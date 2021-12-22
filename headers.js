1b1dcbfafa59173b084cdf8e047fc67ac3251d51
Reid Rankin <reidrankin@gmail.com>
2021-12-21 23:31:35 -0500

    fix: add some missing CSP lines (#608)

--- headers.js
+++ headers.js
@@ -21,6 +21,14 @@ const cspMeta = Object.entries({
     'https://api.thegraph.com/subgraphs/name/salazarguille/yearn-vaults-v2-subgraph-mainnet',
     // @yfi/sdk@1.0.12: https://github.com/yearn/yearn-sdk/blob/0a85ae7be734ba594b8b7e4a290e631610a3b399/src/services/assets.ts#L7
     'https://raw.githubusercontent.com/yearn/yearn-assets/',
+    // @yfi/sdk@1.0.12: https://github.com/yearn/yearn-sdk/blob/0a85ae7be734ba594b8b7e4a290e631610a3b399/src/services/assets.ts#L17
+    'https://raw.githack.com/trustwallet/assets/',
+    // @yfi/sdk@1.0.12: https://github.com/yearn/yearn-sdk/blob/0a85ae7be734ba594b8b7e4a290e631610a3b399/src/services/assets.ts#L13
+    'https://api.github.com/repos/yearn/yearn-assets/',
+    // @shapeshiftoss/caip@1.7.0: https://github.com/shapeshift/lib/blob/5a378b186bf943c9f5e5342e1333b9fbc7c0deaf/packages/caip/src/adapters/coincap/index.ts#L5
+    'https://api.coincap.io/v2/assets',
+    // @shapeshiftoss/market-service@1.7.0: https://github.com/shapeshift/lib/blob/9123527ebbcf0fd62a619ab2824d970123bd5ac2/packages/market-service/src/coincap/coincap.ts#L21
+    'https://api.coincap.io/v2/assets/',
     process.env.REACT_APP_ETHEREUM_NODE_URL,
     process.env.REACT_APP_UNCHAINED_ETHEREUM_HTTP_URL,
     process.env.REACT_APP_UNCHAINED_ETHEREUM_WS_URL,

abadf41f82df3dcffcaaface95c07e12ff3b97d9
Reid Rankin <reidrankin@gmail.com>
2021-12-23 15:26:30 -0500

    Revert "feat: load env values from separate json file (#610)"
    This reverts commit 4ba8e3fb344ffd1023053e81b017e84e704fe661.

--- react-app-rewired.config.js
+++ react-app-rewired.config.js
@@ -1,13 +1,23 @@
 /**
  * React App Rewired Config
  */
-const stableStringify = require('fast-json-stable-stringify')
 const _ = require('lodash')
 const path = require('path')
 const webpack = require('webpack')
 
 const headers = require('./headers')
 process.env.REACT_APP_CSP_META = headers.cspMeta ?? ''
+
+// This ensures environment variables will be enumerated in order, which affects the build products.
+const reactEnvEntries = Object.entries(process.env)
+  .filter(([k]) => k.startsWith('REACT_APP'))
+  .sort((a, b) => {
+    if (a[0] < b[0]) return -1
+    if (a[0] > b[0]) return 1
+    return 0
+  })
+reactEnvEntries.forEach(([k]) => delete process.env[k])
+reactEnvEntries.forEach(([k, v]) => (process.env[k] = v))
 
 module.exports = {
   webpack: (config, mode) => {
@@ -80,81 +90,6 @@ module.exports = {
           );
         }
       ],
-    })
-
-    // Collect env vars that would have been injected via DefinePlugin. We will emit them
-    // as dynamically-loaded JSON later instead of find/replacing the string 'process.env'.
-    // This ensures that the minified Webpack output will be the same no matter the build
-    // options being used.
-    const env = Object.fromEntries(
-      Object.entries(
-        config.plugins
-          .filter((plugin) => plugin.constructor.name === "DefinePlugin")
-          .reduceRight((x) => x).definitions?.["process.env"] || {}
-      ).filter(([k, v])=> v).map(([k, v]) => [k, JSON.parse(v)])
-    );
-    // Update the Webpack config to emit the collected env vars as `env.json` and load them
-    // dynamically instead of baking them into each chunk that might reference them.
-    _.merge(config, {
-      plugins: [...config.plugins.map(plugin => {
-        switch (plugin.constructor.name) {
-          case 'DefinePlugin': {
-            // Remove the 'process.env' entry from DefinePlugin; this will cause `process.env` to
-            // resolve via the ProvidePlugin `process` global.
-            delete plugin.definitions['process.env']
-            return plugin
-          }
-          case 'ProvidePlugin': {
-            // This is a thin wrapper around the process/browser.js module, which inserts the
-            // contents of `env.json` into `process.env` and freezes the object to prevent
-            // attacks on any code that might expect its environment variables to be immutable.
-            return new webpack.ProvidePlugin(Object.assign({}, plugin.definitions, {
-              process: [ path.join(__dirname, 'src/env/process.js') ],
-            }))
-          }
-          default: return plugin
-        }
-      })],
-      module: {
-        rules: [...config.module?.rules, {
-          // This rule causes the (placeholder) contents of `src/env/env.json` to be thrown away
-          // and replaced with `stableStringify(env)`, which is then written out to `build/env.json`.
-          //
-          // Note that simply adding this rule doesn't force `env.json` to be generated. That happens
-          // because `src/env/process.js` `require()`s it, and that module is provided via ProvidePlugin
-          // above. If nothing ever uses that `process` global, both `src/env/process.js` and `env.json`
-          // will be omitted from the build.
-          resource: path.join(__dirname, 'src/env/env.json'),
-          // Webpack loads resources by reading them from disk and feeding them through a series
-          // of loaders. It then emits them by feeding the result to a generator.
-          //
-          // The `val-loader` plugin is a customizable loader which `require()`s `executableFile`,
-          // expecting a single exported function which it uses to transform the on-disk module data.
-          // The stub loader, `src/env/loader.js`, simply takes a `code` function as an option and
-          // and replaces the module data with its result.
-          //
-          // Webpack requires both the module being loaded and the stub loader to exist as actual files
-          // on the filesystem, but this setup allows the actual content of a module to be generated
-          // at compile time.
-          use: [{
-            loader: 'val-loader',
-            options: {
-              executableFile: require.resolve(path.join(__dirname, 'src/env/loader.js')),
-              code: () => stableStringify(env),
-            },
-          }],
-          // The type ['asset/resource'](https://webpack.js.org/guides/asset-modules/#resource-assets)
-          // tells Webpack to a special generator that will emit the module as a separate file instead
-          // of inlining its contents into another chunk, as well as skip the normal plugin processing
-          // and minification steps and just write the raw as-loaded contents. The `generator.filename`
-          // option overrides the default output path so that the file ends up at `build/env.json`
-          // instead of the default `build/static/media/env.[hash].json`.
-          type: 'asset/resource',
-          generator: {
-            filename: '[base]',
-          },
-        }]
-      },
     })
 
     return config
--- src/env/process.js
+++
@@ -1,6 +0,0 @@
-// This is a thin wrapper around the process/browser.js module which assigns the contents of
-// env.json to process.env and freezes things to prevent shenanigans. See react-app-rewired.config.js
-// for details.
-const process = require('process/browser.js')
-Object.freeze(Object.assign(process.env, require('./env.json')))
-module.exports = Object.freeze(process)

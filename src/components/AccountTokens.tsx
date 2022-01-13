3de1193d23e3f3195af69e670c42c66fb77f960b
reallybeard <89934888+reallybeard@users.noreply.github.com>
2022-01-13 11:46:56 -0600

    add the show more tokens toggle

--- src/components/AccountTokens.tsx
+++ src/components/AccountTokens.tsx
@@ -1,5 +1,8 @@
-import { Stack } from '@chakra-ui/react'
+import { Button, Collapse, Stack, useDisclosure } from '@chakra-ui/react'
 import { CAIP19 } from '@shapeshiftoss/caip'
+import { useMemo } from 'react'
+import { FaArrowCircleDown } from 'react-icons/fa'
+import { useTranslate } from 'react-polyglot'
 import { generatePath } from 'react-router'
 import { Text } from 'components/Text'
 import { selectAssetByCAIP19 } from 'state/slices/assetsSlice/assetsSlice'
@@ -19,9 +22,22 @@ export const AccountTokens = ({
   caip19: CAIP19
   accountId: AccountSpecifier
 }) => {
+  const { isOpen, onToggle } = useDisclosure()
+  const translate = useTranslate()
   const accounts = useAppSelector(state => selectPortfolioAccountById(state, accountId))
   const asset = useAppSelector(state => selectAssetByCAIP19(state, caip19))
-  if (asset.tokenId) return null
+
+  const featuredAssets = useMemo(() => {
+    return accounts && accounts.length > 5 ? accounts.slice(0, 5) : accounts
+  }, [accounts])
+
+  const moreAssets = useMemo(() => {
+    return accounts && accounts.length > 5 ? accounts.slice(5) : null
+  }, [accounts])
+
+  // @TODO: This filters for ETH to not show tokens component on tokens
+  if (asset.tokenId || !accounts) return null
+
   return (
     <Card>
       <Card.Header>
@@ -31,9 +47,9 @@ export const AccountTokens = ({
       </Card.Header>
       <Card.Body pt={0}>
         <Stack mx={-4}>
-          {accounts &&
-            accounts.length > 0 &&
-            accounts.map(account => {
+          {featuredAssets &&
+            featuredAssets.length > 0 &&
+            featuredAssets.map(account => {
               const path = generatePath('/accounts/:accountId/:assetId', {
                 accountId,
                 assetId: account
@@ -41,7 +57,35 @@ export const AccountTokens = ({
               return <AccountRow CAIP19={account} allocationValue={10} key={account} to={path} />
             })}
         </Stack>
+        {moreAssets && moreAssets.length > 0 && (
+          <Collapse in={isOpen}>
+            <Stack mx={-4}>
+              {moreAssets.map(account => {
+                const path = generatePath('/accounts/:accountId/:assetId', {
+                  accountId,
+                  assetId: account
+                })
+                return <AccountRow CAIP19={account} allocationValue={10} key={account} to={path} />
+              })}
+            </Stack>
+          </Collapse>
+        )}
       </Card.Body>
+      {moreAssets && moreAssets.length > 0 && (
+        <Card.Footer p={0}>
+          <Button
+            variant='link'
+            p={4}
+            borderTopRadius='none'
+            colorScheme='blue'
+            onClick={onToggle}
+            isFullWidth
+            rightIcon={<FaArrowCircleDown />}
+          >
+            {translate(`assets.assetCards.${isOpen ? 'hideTokens' : 'showTokens'}`)}
+          </Button>
+        </Card.Footer>
+      )}
     </Card>
   )
 }

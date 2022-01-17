7acc6f043d93495a66709f1491372b3d81629167
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2022-01-17 13:13:31 -0700

    chore: remove debugging

--- src/context/TransactionsProvider/TransactionsProvider.tsx
+++ src/context/TransactionsProvider/TransactionsProvider.tsx
@@ -47,9 +47,6 @@ export const TransactionsProvider = ({ children }: TransactionsProviderProps): J
                 const caip10 = `${msg.caip2}:${msg.address}`
                 const state = store.getState()
                 const accountId = selectAccountIdByAddress(state, caip10)
-                if (msg.txid.startsWith('8d')) {
-                  console.info(msg)
-                }
                 dispatch(
                   txHistory.actions.onMessage({
                     message: { ...msg, accountType },

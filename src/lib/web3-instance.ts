b3d50b85eb0a8131679b4bd78966898a4ed6b408
Alexandre Gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-01-22 02:25:13 +0300

    the fix that fixes all failing tests

--- src/lib/web3-instance.ts
+++ src/lib/web3-instance.ts
@@ -3,9 +3,8 @@ import { getWeb3Provider } from 'lib/web3-provider'
 
 let maybeWeb3Instance: Web3 | null
 
-const web3Provider = getWeb3Provider()
-
 export const getWeb3Instance = (): Web3 => {
+  const web3Provider = getWeb3Provider()
   if (!maybeWeb3Instance) {
     maybeWeb3Instance = new Web3(web3Provider)
     return maybeWeb3Instance!

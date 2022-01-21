1367d8ed10cd602a3c010288f25ea8902312dd05
theoboldfrazier <theobold.frazier@gmail.com>
2022-01-21 12:41:28 -0700

    Dropped down the max allowance for yearn deposits to 96 bits to comply with limits on uniswap and compound

--- src/constants/allowance.ts
+++ src/constants/allowance.ts
@@ -1,1 +1,3 @@
-export const MAX_ALLOWANCE = '0xfffffffffffffffffffffffffffffffffffffffffffffff'
+// Need to limit the max allowance to 96 bits because contracts like Uniswap and Compound have a
+// 96 bit max allowance.
+export const MAX_ALLOWANCE = '0xffffffffffffffffffffffff'

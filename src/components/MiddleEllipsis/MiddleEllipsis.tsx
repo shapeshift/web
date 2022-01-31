d65ecb15164d965b9d08958262a7725a83d4c0e8
Amitoj Singh <amitojsingh366@gmail.com>
2022-01-31 21:05:08 +0300

    refactor: middle ellipsis use last 4 characters

--- src/components/MiddleEllipsis/MiddleEllipsis.tsx
+++ src/components/MiddleEllipsis/MiddleEllipsis.tsx
@@ -14,8 +14,8 @@ export const MiddleEllipsis = ({ address, ...rest }: MiddleEllipsisProps) => {
 }
 
 export function shortenAddress(address: string, chars = 4): string {
-  if (!address.length < 10) {
+  if (address.length < 10) {
     return address
   }
-  return `${address.substring(0, chars + 2)}...${address.substring(42 - chars)}`
+  return `${address.substring(0, chars + 2)}...${address.slice(-4)}`
 }

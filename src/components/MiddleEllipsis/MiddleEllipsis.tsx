fc58a2ef6308bb2794e10e1782197439b0860c90
Amitoj Singh <amitojsingh366@gmail.com>
2022-01-31 20:57:43 +0300

    refactor: middle ellipsis return string if invalid address

--- src/components/MiddleEllipsis/MiddleEllipsis.tsx
+++ src/components/MiddleEllipsis/MiddleEllipsis.tsx
@@ -17,7 +17,7 @@ export const MiddleEllipsis = ({ address, ...rest }: MiddleEllipsisProps) => {
 export function shortenAddress(address: string, chars = 4): string {
   const parsed = isAddress(address)
   if (!parsed) {
-    throw Error(`Invalid 'address' parameter '${address}'.`)
+    return address
   }
   return `${address.substring(0, chars + 2)}...${address.substring(42 - chars)}`
 }

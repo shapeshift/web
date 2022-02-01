050d32fb81a1310000129d04179f180085f94484
theoboldfrazier <theobold.frazier@gmail.com>
2022-02-01 14:17:09 -0700

    filtering out vaults that have disabled deposits and including vault version number in the name

--- src/features/defi/providers/yearn/api/vaults.ts
+++ src/features/defi/providers/yearn/api/vaults.ts
@@ -1,3 +1,4 @@
+import { bnOrZero } from '@shapeshiftoss/chain-adapters'
 import { ChainTypes } from '@shapeshiftoss/types'
 import { Vault } from '@yfi/sdk'
 import {
@@ -21,15 +22,22 @@ export type SupportedYearnVault = {
 export const getSupportedVaults = async (): Promise<SupportedYearnVault[]> => {
   const vaults = await yearnSdk.vaults.get()
 
-  return vaults.map((vault: Vault) => {
-    return {
-      vaultAddress: toLower(vault.address),
-      name: vault.name,
-      symbol: vault.symbol,
-      tokenAddress: toLower(vault.token),
-      chain: ChainTypes.Ethereum,
-      provider: DefiProvider.Yearn,
-      type: DefiType.Vault
-    }
-  })
+  return vaults
+    .filter((vault: Vault) => {
+      // Currently filtering out all vaults where deposits are disabled or the deposit limit is 0.
+      // TODO: Update modal so that vaults with disabled deposit only show if a user has a balance
+      // and only display the option to withdraw from the vault.
+      return !vault.metadata.depositsDisabled && bnOrZero(vault.metadata.depositLimit).gt(0)
+    })
+    .map((vault: Vault) => {
+      return {
+        vaultAddress: toLower(vault.address),
+        name: `${vault.name} ${vault.version}`,
+        symbol: vault.symbol,
+        tokenAddress: toLower(vault.token),
+        chain: ChainTypes.Ethereum,
+        provider: DefiProvider.Yearn,
+        type: DefiType.Vault
+      }
+    })
 }

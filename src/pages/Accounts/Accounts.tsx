4d36e4b20a358f6490fdceaaba2c67f0302f33f4
Amitoj Singh <amitojsingh366@gmail.com>
2022-02-02 21:04:48 +0300

    chore: send user back to prev page if they disconnect on accounts page

--- src/pages/Accounts/Accounts.tsx
+++ src/pages/Accounts/Accounts.tsx
@@ -1,13 +1,26 @@
 import { Flex, Heading, Stack } from '@chakra-ui/react'
+import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router'
 import { Page } from 'components/Layout/Page'
 import { Text } from 'components/Text'
+import { useWallet } from 'context/WalletProvider/WalletProvider'
 import { selectPortfolioAccountIdsSortedFiat } from 'state/slices/portfolioSlice/selectors'
 
 import { AccountRowWithTokens } from './AccountRowWithTokens'
 
 export const Accounts = () => {
   const sortedAccountIds = useSelector(selectPortfolioAccountIdsSortedFiat)
+  const {
+    state: { isConnected }
+  } = useWallet()
+
+  const history = useHistory()
+
+  useEffect(() => {
+    if (!isConnected) history.goBack()
+  }, [history, isConnected])
+
   return (
     <Page style={{ flex: 1 }}>
       <Flex
@@ -23,9 +36,10 @@ export const Accounts = () => {
           <Heading>
             <Text translation='accounts.accounts' />
           </Heading>
-          {sortedAccountIds.map(accountId => (
-            <AccountRowWithTokens accountId={accountId} key={accountId} />
-          ))}
+          {isConnected &&
+            sortedAccountIds.map(accountId => (
+              <AccountRowWithTokens accountId={accountId} key={accountId} />
+            ))}
         </Stack>
       </Flex>
     </Page>

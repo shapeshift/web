aef53b9c3dc75fa064d194caae2fd183fd8098d6
Amitoj Singh <amitojsingh366@gmail.com>
2022-02-02 11:38:40 +0300

    refactor: remove wallet checks from private route

--- src/Routes/PrivateRoute.tsx
+++ src/Routes/PrivateRoute.tsx
@@ -1,26 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { getConfig } from 'config'
-import { Redirect, Route, RouteProps } from 'react-router-dom'
+import { Route, RouteProps } from 'react-router-dom'
 
-type PrivateRouteProps = {
-  hasWallet: boolean
-} & RouteProps
-
-const HIDE_SPLASH = getConfig().REACT_APP_HIDE_SPLASH
-
-export const PrivateRoute = ({ hasWallet, ...rest }: PrivateRouteProps) => {
-  const { location } = rest
-
-  // return hasWallet || HIDE_SPLASH ? (
-  //   <Route {...rest} />
-  // ) : (
-  //   <Redirect
-  //     to={{
-  //       pathname: '/connect-wallet',
-  //       search: `returnUrl=${location?.pathname ?? '/dashboard'}`
-  //     }}
-  //   />
-  // )
-
-  return <Route {...rest} />
+export const PrivateRoute = ({ ...props }: RouteProps) => {
+  return <Route {...props} />
 }

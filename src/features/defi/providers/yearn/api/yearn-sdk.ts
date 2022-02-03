7947c84c9f419019ed9aeeb46fc7e14b58dd2c53
theoboldfrazier <theobold.frazier@gmail.com>
2022-02-03 15:14:32 -0700

    updated to remove yearn-api and import it from @shapeshiftoss/investor-yearn

--- src/features/defi/providers/yearn/api/yearn-sdk.ts
+++
@@ -1,7 +0,0 @@
-import { JsonRpcProvider } from '@ethersproject/providers'
-import { Yearn } from '@yfi/sdk'
-
-// YearnVaultMarketCapService deps
-const network = 1 // 1 for mainnet
-const provider = new JsonRpcProvider(process.env.REACT_APP_UNCHAINED_ETHEREUM_HTTP_URL)
-export const yearnSdk = new Yearn(network, { provider, disableAllowlist: true })

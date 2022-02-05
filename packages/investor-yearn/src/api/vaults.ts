4fb3f1a6a6a0d2bdcb0ab19e1fa9a94817ce72e2
Amitoj Singh <35400192+amitojsingh366@users.noreply.github.com>
2022-02-06 02:41:40 +0300

    feat: expired prop on yearn vaults (#344)
    * feat: add expired prop on vaults
    
    * feat: add expired prop on vaults
    
    * refactor: don't coerce vault.metadata.depositsDisabled

--- packages/investor-yearn/src/api/vaults.ts
+++ packages/investor-yearn/src/api/vaults.ts
@@ -14,27 +14,21 @@ export type SupportedYearnVault = {
   chain: ChainTypes
   provider: string
   type: string
+  expired: boolean
 }
 
 export const getSupportedVaults = async (): Promise<SupportedYearnVault[]> => {
   const vaults = await yearnSdk.vaults.get()
-
-  return vaults
-    .filter((vault: Vault) => {
-      // Currently filtering out all vaults where deposits are disabled or the deposit limit is 0.
-      // TODO: Update modal so that vaults with disabled deposit only show if a user has a balance
-      // and only display the option to withdraw from the vault.
-      return !vault.metadata.depositsDisabled && bnOrZero(vault.metadata.depositLimit).gt(0)
-    })
-    .map((vault: Vault) => {
-      return {
-        vaultAddress: toLower(vault.address),
-        name: `${vault.name} ${vault.version}`,
-        symbol: vault.symbol,
-        tokenAddress: toLower(vault.token),
-        chain: ChainTypes.Ethereum,
-        provider: DefiProvider.Yearn,
-        type: DefiType.Vault
-      }
-    })
+  return vaults.map((vault: Vault) => {
+    return {
+      vaultAddress: toLower(vault.address),
+      name: `${vault.name} ${vault.version}`,
+      symbol: vault.symbol,
+      tokenAddress: toLower(vault.token),
+      chain: ChainTypes.Ethereum,
+      provider: DefiProvider.Yearn,
+      type: DefiType.Vault,
+      expired: vault.metadata.depositsDisabled || bnOrZero(vault.metadata.depositLimit).lte(0)
+    }
+  })
 }

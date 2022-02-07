462a765aa2b97071efe0109fc6e2fd10d55b57e3
Raphael Beer <raphael.beer@protonmail.com>
2022-02-07 08:21:47 +0100

    fix: linux-brave-version not executable

--- cypress/scripts/linux-brave-version.sh
+++ cypress/scripts/linux-brave-version.sh
@@ -2,11 +2,10 @@
 
 BRAVE_PATH=$(which brave-browser)
 BRAVE_CODE=$?
-BRAVE_VERSION=$($BRAVE_PATH --version 2> /dev/null)
 
+# STDOUT will be like "Brave Browser 77.0.69.135"
 if [ $BRAVE_CODE -eq 0 ]; then
   echo "$BRAVE_PATH"
-  echo "${BRAVE_VERSION/Brave Browser/}"
   exit 0
 else
   exit 1

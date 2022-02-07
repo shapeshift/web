18cab24c583d8c3af9a9f0cdc94a1a9a1c193d08
LPX <info@shapeshift.one>
2022-02-07 21:45:46 +0900

    feat: set default language to client browsers'
    quick and dirty solution until product team is ready

--- src/assets/translations/index.ts
+++ src/assets/translations/index.ts
@@ -1,3 +1,9 @@
 import en from './messages-en.json'
+import es from './messages-es.json'
+import fr from './messages-fr.json'
+// import ko from './messages-ko.json' KO locale almost ready
+import pt from './messages-pt.json'
+import ru from './messages-ru.json'
+import zh from './messages-ru.json'
 
-export const translations: any = { en }
+export const translations: any = { en, es, fr, pt, ru, zh }

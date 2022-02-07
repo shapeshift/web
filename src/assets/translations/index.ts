e0d2e2ab3d28fe737dfbd31d82ef623d59e34686
LPX <info@shapeshift.one>
2022-02-08 04:40:01 +0900

    fix typo, removing KO locale for now

--- src/assets/translations/index.ts
+++ src/assets/translations/index.ts
@@ -1,9 +1,8 @@
 import en from './messages-en.json'
 import es from './messages-es.json'
 import fr from './messages-fr.json'
-// import ko from './messages-ko.json' KO locale almost ready
 import pt from './messages-pt.json'
 import ru from './messages-ru.json'
-import zh from './messages-ru.json'
+import zh from './messages-zh.json'
 
 export const translations: any = { en, es, fr, pt, ru, zh }

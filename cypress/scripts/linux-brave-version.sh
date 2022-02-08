3fa38f68cdc4551e819c6f71c6df3b46409fbd96
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2022-02-08 18:26:43 +1100

    fix: improve brave linux path

--- cypress/scripts/linux-brave-version.sh
+++ cypress/scripts/linux-brave-version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # TODO - replace `which` with `/usr/bin/which` and test on Linux
-BRAVE_PATH=$(which brave-browser)
+BRAVE_PATH=$(/usr/bin/which brave-browser)
 BRAVE_CODE=$?
 
 if [ $BRAVE_CODE -eq 0 ]; then

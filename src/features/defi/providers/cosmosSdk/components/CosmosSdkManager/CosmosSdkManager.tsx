f3af68068a0ba18e5b1ffadc04bf85aed613dace
Alexandre Gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-02-08 22:08:34 +0300

    fix: typecheck

--- src/features/defi/providers/cosmosSdk/components/CosmosSdkManager/CosmosSdkManager.tsx
+++ src/features/defi/providers/cosmosSdk/components/CosmosSdkManager/CosmosSdkManager.tsx
@@ -10,7 +10,8 @@ type CosmosSdkManagerProps = {
 }
 
 // TODO: Add proper routing with MemoryRouter, this just handles the "Get Started" modal route for now
-export const CosmosSdkManager = ({ assetId }: CosmosSdkManagerProps) => {
+// TODO: Do not use monkey default assetId
+export const CosmosSdkManager = ({ assetId = 'cosmoshub-3/slip44:118' }: CosmosSdkManagerProps) => {
   const params = useParams<DefiParams>()
 
   if (params.action === DefiAction.GetStarted) return <GetStarted assetId={assetId} />

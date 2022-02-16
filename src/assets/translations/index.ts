f99e2faaa9ef42177e44e315d856229e1f5907c9
LPX <info@shapeshift.one>
2022-02-16 15:59:05 +0900

    feat: indonesian locale

--- src/assets/translations/index.ts
+++ src/assets/translations/index.ts
@@ -1,9 +1,10 @@
 import en from './messages-en.json'
 import es from './messages-es.json'
 import fr from './messages-fr.json'
+import id from './messages-id.json'
 import ko from './messages-ko.json'
 import pt from './messages-pt.json'
 import ru from './messages-ru.json'
 import zh from './messages-zh.json'
 
-export const translations: any = { en, es, fr, ko, pt, ru, zh }
+export const translations: any = { en, es, fr, id, ko, pt, ru, zh }

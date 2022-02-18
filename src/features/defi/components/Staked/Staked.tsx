ae43d80ae996f5d9bd3009dd6e36267c1c74b6b4
Alexandre Gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-02-18 14:24:56 +0300

    fix: make tsc happy

--- src/features/defi/components/Staked/Staked.tsx
+++ src/features/defi/components/Staked/Staked.tsx
@@ -56,7 +56,7 @@ export const Staked = ({ assetId }: StakedProps) => {
             <Flex width='50%' height='20px'>
               <Text translation={'defi.rewards'} fontSize='16px' />
             </Flex>
-            <Flex direction='column' width='50%' alignItems='flex-end' width='100%'>
+            <Flex direction='column' alignItems='flex-end' width='100%'>
               <Text translation={'$42,4242,42'} fontWeight='semibold' color='green.500' />
               <Text translation={'708.00 OSMO'} color='gray.500' />
             </Flex>

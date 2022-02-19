ae1352521129d42ed0345ad0e767257fee18d932
Alexandre Gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-02-19 15:27:59 +0300

    fix: move cosmos components to src/plugins/cosmos/components

--- src/context/ModalProvider/ModalProvider.tsx
+++ src/context/ModalProvider/ModalProvider.tsx
@@ -1,6 +1,6 @@
-import { GetStartedModal } from 'features/cosmos/components/modals/GetStarted/GetStarted'
 import merge from 'lodash/merge'
 import noop from 'lodash/noop'
+import { GetStartedModal } from 'plugins/cosmos/components/modals/GetStarted/GetStarted'
 import React, { useContext, useMemo, useReducer } from 'react'
 import { PassphraseModal } from 'components/Modals/KeyManagement/KeepKey/Passphrase'
 import { PinModal } from 'components/Modals/KeyManagement/KeepKey/Pin'

48b06cc7a677974c020c4bcf738bc34cac44f751
Chris Thompson <chris@thompson-web.org>
2022-02-22 15:07:01 -0700

    chore: add env variables for Cosmos backend

--- src/config.ts
+++ src/config.ts
@@ -8,12 +8,24 @@ const { cleanEnv, str, url } = envalid
 // add validators for each .env variable
 // note env vars must be prefixed with REACT_APP_
 const validators = {
-  REACT_APP_UNCHAINED_ETHEREUM_HTTP_URL: url(),
-  REACT_APP_UNCHAINED_ETHEREUM_WS_URL: url(),
-  REACT_APP_UNCHAINED_BITCOIN_HTTP_URL: url(),
-  REACT_APP_UNCHAINED_BITCOIN_WS_URL: url(),
+  REACT_APP_UNCHAINED_ETHEREUM_HTTP_URL: url({
+    devDefault: 'https://dev-api.ethereum.shapeshift.com'
+  }),
+  REACT_APP_UNCHAINED_ETHEREUM_WS_URL: url({
+    devDefault: 'wss://dev-api.ethereum.shapeshift.com'
+  }),
+  REACT_APP_UNCHAINED_BITCOIN_HTTP_URL: url({
+    devDefault: 'https://dev-api.bitcoin.shapeshift.com'
+  }),
+  REACT_APP_UNCHAINED_BITCOIN_WS_URL: url({
+    devDefault: 'wss://dev-api.bitcoin.shapeshift.com'
+  }),
+  REACT_APP_UNCHAINED_COSMOS_HTTP_URL: url({
+    devDefault: 'http://api.cosmos.localhost'
+  }),
+  REACT_APP_UNCHAINED_COSMOS_WS_URL: url({ devDefault: 'ws://api.cosmos.localhost' }),
   REACT_APP_ETHEREUM_NODE_URL: url(),
-  REACT_APP_PORTIS_DAPP_ID: str(),
+  REACT_APP_PORTIS_DAPP_ID: str({ devDefault: 'fakePortisId' }),
   REACT_APP_HIDE_SPLASH: bool({ default: false }),
   REACT_APP_FEATURE_YEARN: bool({ default: true }),
   REACT_APP_FEATURE_COSMOS_INVESTOR: bool({ default: false }),
@@ -24,6 +36,7 @@ const validators = {
 function reporter<T>({ errors }: envalid.ReporterOptions<T>) {
   Object.entries(errors).forEach(([envVar, err]) => {
     if (!err) return
+    console.error(err)
     console.error(
       envVar,
       'missing from config. Check sample.env and add it to your local .env and add a validator in config.ts'
@@ -31,6 +44,4 @@ function reporter<T>({ errors }: envalid.ReporterOptions<T>) {
   })
 }
 
-const getConfig = () => cleanEnv(env, validators, { reporter })
-
-export { getConfig }
+export const getConfig = () => cleanEnv(env, validators, { reporter })

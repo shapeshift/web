8325b3bac2f8edf45f218ef33f15b219298057e3
StackedQ <stackedq@gmail.com>
2022-02-23 17:47:15 +0330

    fix(useSendDetails): update errored state properly on input change (#1052)
    * fix: wrong button title on send modal
    
    * fix: root cause found and fixed.
    
    * refactor: applying requested changes

--- src/components/Modals/Send/hooks/useSendDetails/useSendDetails.tsx
+++ src/components/Modals/Send/hooks/useSendDetails/useSendDetails.tsx
@@ -274,11 +274,18 @@ export const useSendDetails = (): UseSendDetailsReturnType => {
           fieldName !== SendFormFields.FiatAmount
             ? SendFormFields.FiatAmount
             : SendFormFields.CryptoAmount
+        if (inputValue === '') {
+          // Don't show an error message when the input is empty
+          setValue(SendFormFields.AmountFieldError, '')
+          setLoading(false)
+          // Set value of the other input to an empty string as well
+          setValue(key, '')
+          return
+        }
         const amount =
           fieldName === SendFormFields.FiatAmount
             ? bnOrZero(bn(inputValue).div(price)).toString()
             : bnOrZero(bn(inputValue).times(price)).toString()
-
         setValue(key, amount)
 
         let estimatedFees
@@ -307,6 +314,9 @@ export const useSendDetails = (): UseSendDetailsReturnType => {
             'modals.send.errors.notEnoughNativeToken',
             { asset: feeAsset.symbol }
           ])
+        } else {
+          // Remove existing error messages because the send amount is valid
+          setValue(SendFormFields.AmountFieldError, '')
         }
         setLoading(false)
       },

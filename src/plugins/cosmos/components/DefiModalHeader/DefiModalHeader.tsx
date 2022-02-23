e22521102e767406bca821d3dad025ec31bcfc39
Alexandre GOMES <17035424+gomesalexandre@users.noreply.github.com>
2022-02-23 10:52:07 +0300

    feat: cosmos 'Learn More' modal (#994)
    - add <LearnMore /> modal
    - add <CarouselDots /> component
    - use standard useModal() provider for cosmos modal

---
+++ src/plugins/cosmos/components/DefiModalHeader/DefiModalHeader.tsx
@@ -0,0 +1,29 @@
+import { Box } from '@chakra-ui/layout'
+import { Image } from '@chakra-ui/react'
+import { Text } from 'components/Text'
+
+type DefiModalHeaderProps = {
+  headerImageSrc: string
+  headerImageMaxWidth: number
+  headerText: string | [string, Record<string, string>]
+}
+
+export const DefiModalHeader = ({
+  headerImageSrc,
+  headerText,
+  headerImageMaxWidth
+}: DefiModalHeaderProps) => (
+  <>
+    <Box textAlign='center'>
+      <Image
+        src={headerImageSrc}
+        width='100%'
+        minWidth='68px'
+        maxWidth={{ base: `${headerImageMaxWidth / 2}px`, sm: `${headerImageMaxWidth}px` }}
+      />
+    </Box>
+    <Box textAlign='center' my='24px'>
+      <Text translation={headerText} fontSize='18px' fontWeight='bold' />
+    </Box>
+  </>
+)

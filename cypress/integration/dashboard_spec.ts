bf61482a1164ffeb76ed5106bc3933de7b5215dd
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2022-03-01 15:44:54 +1100

    chore: uncomment some tests

--- cypress/integration/dashboard_spec.ts
+++ cypress/integration/dashboard_spec.ts
@@ -1,5 +1,5 @@
-// const baseUrl = Cypress.config().baseUrl
-// const linkContract = Cypress.env('linkContract')
+const baseUrl = Cypress.config().baseUrl
+const linkContract = Cypress.env('linkContract')
 
 beforeEach(() => {
   // Intercept all account requests relating to our test wallet
@@ -13,16 +13,15 @@ describe('The Dashboard', () => {
     cy.login()
   })
 
-  // TODO - Disabled temporarily to get into CI
-  // it('nav bar works', () => {
-  //   // A proxy to understand if the Dashboard has initialised
-  //   cy.getBySel('account-row').should('have.length', 7)
-  //
-  //   cy.navigateToAccounts()
-  //   cy.navigateToAssets()
-  //   cy.navigateToDefi()
-  //   cy.navigateToDashboard()
-  // })
+  it('nav bar works', () => {
+    // A proxy to understand if the Dashboard has initialised
+    cy.getBySel('account-row').should('have.length', 7)
+
+    cy.navigateToAccounts()
+    cy.navigateToAssets()
+    cy.navigateToDefi()
+    cy.navigateToDashboard()
+  })
 
   it('displays the expected account rows', () => {
     cy.getBySel('account-row').should('have.length', 7)
@@ -36,51 +35,49 @@ describe('The Dashboard', () => {
   })
 
   it('supports trades', () => {
+    cy.getBySel('account-row-asset-name-ETH').click()
     cy.getBySel('token-row-sell').find('input').type('89')
-    // Increase the timeout to reduce the risk of flaking during quote request
     cy.getBySel('trade-preview-button').should('be.disabled')
-    // cy.getBySel('trade-rate-quote').should('have.text', '1 ETH = 7,673.93 FOX')
+    // cy.getBySel('trade-rate-quote').should('have.text', '1 ETH = 7,673.93 FOX') // TODO - mock response and test here
     cy.getBySel('swap-assets-button').click()
-    // cy.getBySel('trade-rate-quote').should('have.text', '1 FOX = 0.00011 ETH')
+    // cy.getBySel('trade-rate-quote').should('have.text', '1 FOX = 0.00011 ETH') // TODO - mock response and test here
     cy.getBySel('trade-preview-button').should('be.disabled')
     cy.getBySel('token-row-sell-max-button').click()
-    // This can't be clicked until max sell is calculated, and it can take some time to compute on slower hardware.
-    cy.getBySel('trade-preview-button').should('be.not.disabled').click()
+    cy.getBySel('trade-preview-button').should('have.text', 'Not enough ETH to cover gas')
     // TODO - We are now at the approval screen - test the rest of the flow
   })
 
-  // TODO - Disabled temporarily to get into CI
-  // it('supports send transaction setup', () => {
-  //   cy.navigateToDashboard()
-  //   cy.getBySel('account-row-asset-crypto-LINK').click()
-  //   cy.url().should('equal', `${baseUrl}assets/${linkContract}`)
-  //   cy.getBySel('asset-action-send').click()
-  //   cy.getBySel('send-address-input').type('0xabadbabe')
-  //   cy.getBySel('send-address-next-button')
-  //     .should('be.disabled')
-  //     .should('have.text', 'Invalid Address')
-  //   cy.getBySel('send-address-input').clear()
-  //   cy.getBySel('send-address-input').type('0xAceBabe64807cb045505b268ef253D8fC2FeF5Bc')
-  //   cy.getBySel('send-address-next-button')
-  //     .should('not.be.disabled')
-  //     .should('have.text', 'Next')
-  //     .click()
-  //   cy.getBySel('account-card-asset-name-label').should('have.text', 'Chainlink')
-  //   cy.getBySel('account-card-crypto-label').should('exist')
-  //   cy.getBySel('send-modal-next-button').should('be.disabled')
-  //   cy.getBySel('send-modal-crypto-input').type('10')
-  //   cy.getBySel('send-modal-next-button').should('have.text', 'Not enough ETH to cover gas')
-  //   // TODO - Add more test data and finish this flow
-  //   cy.backdropDismiss()
-  // })
-  //
-  // it('supports receive transaction setup', () => {
-  //   cy.navigateToDashboard()
-  //   cy.getBySel('account-row-asset-crypto-LINK').click()
-  //   cy.url().should('equal', `${baseUrl}assets/${linkContract}`)
-  //   cy.getBySel('asset-action-receive').click()
-  //   cy.getBySel('receive-qr-code').should('exist')
-  //   cy.getBySel('receive-address-label').should('have.text', '0xfDCa...fCde')
-  //   cy.backdropDismiss()
-  // })
+  it('supports send transaction setup', () => {
+    cy.navigateToDashboard()
+    cy.getBySel('account-row-asset-crypto-LINK').click()
+    cy.url().should('equal', `${baseUrl}assets/${linkContract}`)
+    cy.getBySel('asset-action-send').click()
+    cy.getBySel('send-address-input').type('0xabadbabe')
+    cy.getBySel('send-address-next-button')
+      .should('be.disabled')
+      .should('have.text', 'Invalid Address')
+    cy.getBySel('send-address-input').clear()
+    cy.getBySel('send-address-input').type('0xAceBabe64807cb045505b268ef253D8fC2FeF5Bc').click()
+    cy.getBySel('send-address-next-button')
+      .should('not.be.disabled')
+      .should('have.text', 'Next')
+      .click()
+    cy.getBySel('account-card-asset-name-label').should('have.text', 'Chainlink')
+    cy.getBySel('account-card-crypto-label').should('exist')
+    cy.getBySel('send-modal-next-button').should('be.disabled')
+    cy.getBySel('send-modal-crypto-input').type('10')
+    cy.getBySel('send-modal-next-button').should('have.text', 'Not enough ETH to cover gas')
+    // TODO - Add more test data and finish this flow
+    cy.backdropDismiss()
+  })
+
+  it('supports receive transaction setup', () => {
+    cy.navigateToDashboard()
+    cy.getBySel('account-row-asset-crypto-LINK').click()
+    cy.url().should('equal', `${baseUrl}assets/${linkContract}`)
+    cy.getBySel('asset-action-receive').click()
+    cy.getBySel('receive-qr-code').should('exist')
+    cy.getBySel('receive-address-label').should('have.text', '0xfDCa...fCde')
+    cy.backdropDismiss()
+  })
 })

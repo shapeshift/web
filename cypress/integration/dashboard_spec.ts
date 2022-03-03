a17841a83c47cdf3ee0a8556192a7349d4259d1d
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2022-03-04 07:50:34 +1100

    chore: skip flakey tests instead of commenting them

--- cypress/integration/dashboard_spec.ts
+++ cypress/integration/dashboard_spec.ts
@@ -1,5 +1,5 @@
-// const baseUrl = Cypress.config().baseUrl
-// const linkContract = Cypress.env('linkContract')
+const baseUrl = Cypress.config().baseUrl
+const linkContract = Cypress.env('linkContract')
 
 beforeEach(() => {
   // Intercept all account requests relating to our test wallet
@@ -34,6 +34,7 @@ describe('The Dashboard', () => {
     // TODO - Mock API response and test account row allocation
   })
 
+  // Flakey - fix and re-add
   it('supports trades', () => {
     cy.getBySel('account-row-asset-name-ETH').click()
     cy.getBySel('token-row-sell').find('input').type('89')
@@ -47,37 +48,38 @@ describe('The Dashboard', () => {
     // TODO - We are now at the approval screen - test the rest of the flow
   })
 
-  // it('supports send transaction setup', () => {
-  //   cy.navigateToDashboard()
-  //   cy.getBySel('account-row-asset-crypto-LINK').click()
-  //   cy.url().should('equal', `${baseUrl}assets/${linkContract}`)
-  //   cy.getBySel('asset-action-send').click()
-  //   cy.getBySel('send-address-input').type('0xabadbabe')
-  //   cy.getBySel('send-address-next-button')
-  //     .should('be.disabled')
-  //     .should('have.text', 'Invalid Address')
-  //   cy.getBySel('send-address-input').clear()
-  //   cy.getBySel('send-address-input').type('0xAceBabe64807cb045505b268ef253D8fC2FeF5Bc').click()
-  //   cy.getBySel('send-address-next-button')
-  //     .should('not.be.disabled')
-  //     .should('have.text', 'Next')
-  //     .click()
-  //   cy.getBySel('account-card-asset-name-label').should('have.text', 'Chainlink')
-  //   cy.getBySel('account-card-crypto-label').should('exist')
-  //   cy.getBySel('send-modal-next-button').should('be.disabled')
-  //   cy.getBySel('send-modal-crypto-input').type('10')
-  //   cy.getBySel('send-modal-next-button').should('have.text', 'Not enough ETH to cover gas')
-  //   // TODO - Add more test data and finish this flow
-  //   cy.backdropDismiss()
-  // })
-  //
-  // it('supports receive transaction setup', () => {
-  //   cy.navigateToDashboard()
-  //   cy.getBySel('account-row-asset-crypto-LINK').click()
-  //   cy.url().should('equal', `${baseUrl}assets/${linkContract}`)
-  //   cy.getBySel('asset-action-receive').click()
-  //   cy.getBySel('receive-qr-code').should('exist')
-  //   cy.getBySel('receive-address-label').should('have.text', '0xfDCa...fCde')
-  //   cy.backdropDismiss()
-  // })
+  // Flakey - fix and re-add
+  it.skip('supports send transaction setup', () => {
+    cy.navigateToDashboard()
+    cy.getBySel('account-row-asset-crypto-LINK').click()
+    cy.url().should('equal', `${baseUrl}assets/${linkContract}`)
+    cy.getBySel('asset-action-send').click()
+    cy.getBySel('send-address-input').type('0xabadbabe')
+    cy.getBySel('send-address-next-button')
+      .should('be.disabled')
+      .should('have.text', 'Invalid Address')
+    cy.getBySel('send-address-input').clear()
+    cy.getBySel('send-address-input').type('0xAceBabe64807cb045505b268ef253D8fC2FeF5Bc').click()
+    cy.getBySel('send-address-next-button')
+      .should('not.be.disabled')
+      .should('have.text', 'Next')
+      .click()
+    cy.getBySel('account-card-asset-name-label').should('have.text', 'Chainlink')
+    cy.getBySel('account-card-crypto-label').should('exist')
+    cy.getBySel('send-modal-next-button').should('be.disabled')
+    cy.getBySel('send-modal-crypto-input').type('10')
+    cy.getBySel('send-modal-next-button').should('have.text', 'Not enough ETH to cover gas')
+    // TODO - Add more test data and finish this flow
+    cy.backdropDismiss()
+  })
+
+  it.skip('supports receive transaction setup', () => {
+    cy.navigateToDashboard()
+    cy.getBySel('account-row-asset-crypto-LINK').click()
+    cy.url().should('equal', `${baseUrl}assets/${linkContract}`)
+    cy.getBySel('asset-action-receive').click()
+    cy.getBySel('receive-qr-code').should('exist')
+    cy.getBySel('receive-address-label').should('have.text', '0xfDCa...fCde')
+    cy.backdropDismiss()
+  })
 })

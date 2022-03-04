681b5fe2faca94a7efb9bdfefd3808ffc0388073
reallybeard <89934888+reallybeard@users.noreply.github.com>
2022-03-04 14:06:44 -0600

    feat: flag ui (#1133)
    * add a flag ui
    
    * Update Flags.tsx
    
    * Update Flags.tsx

---
+++ src/pages/Flags/Flags.tsx
@@ -0,0 +1,38 @@
+import { Heading, Stack, StackDivider } from '@chakra-ui/react'
+import { Route } from 'Routes/helpers'
+import { Card } from 'components/Card/Card'
+import { Main } from 'components/Layout/Main'
+import { RawText } from 'components/Text'
+
+import { FlagRow } from './FlagRow'
+
+type FlagsPageProps = {
+  route: Route
+}
+
+const FlagHeader = () => {
+  return (
+    <Stack pb={4}>
+      <Heading>Flags</Heading>
+      <RawText color='gray.500'>Turn on and off flags, by toggling the switch.</RawText>
+    </Stack>
+  )
+}
+
+const example = ['one', 'two', 'three']
+
+export const Flags = ({ route }: FlagsPageProps) => {
+  return (
+    <Main route={route} titleComponent={<FlagHeader />}>
+      <Card>
+        <Card.Body>
+          <Stack divider={<StackDivider />}>
+            {example.map(flag => (
+              <FlagRow flag={flag} />
+            ))}
+          </Stack>
+        </Card.Body>
+      </Card>
+    </Main>
+  )
+}

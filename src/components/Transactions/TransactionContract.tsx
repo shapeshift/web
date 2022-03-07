cdbb2316af4687ba629a026002c1a7b86b35e252
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2022-03-08 09:58:43 +1100

    fix: check for undefined feeAsset (#1138)
    * fix: check for undefined feeAsset
    
    * chore: use explicit types
    
    * chore: update selectAssetByCAIP19 type
    
    * refactor: update logic
    
    * fix: check for fee & feeAsset
    
    * Update src/components/Transactions/TransactionContract.tsx
    
    Co-authored-by: Chris Thompson <1958266+cjthompson@users.noreply.github.com>
    
    Co-authored-by: Chris Thompson <1958266+cjthompson@users.noreply.github.com>

--- src/components/Transactions/TransactionContract.tsx
+++ src/components/Transactions/TransactionContract.tsx
@@ -130,21 +130,23 @@ export const TransactionContract = ({ txDetails }: { txDetails: TxDetails }) => 
             </Row.Value>
           </Row>
 
-          <Row variant='vertical' hidden={!(txDetails.tx?.fee && txDetails.feeAsset)}>
-            <Row.Label>
-              <Text translation='transactionRow.fee' />
-            </Row.Label>
-            <Row.Value>
-              <Amount.Crypto
-                value={fromBaseUnit(
-                  txDetails.tx?.fee?.value ?? '0',
-                  txDetails.feeAsset?.precision ?? 18
-                )}
-                symbol={txDetails.feeAsset.symbol}
-                maximumFractionDigits={6}
-              />
-            </Row.Value>
-          </Row>
+          {txDetails.tx?.fee && txDetails.feeAsset && (
+            <Row variant='vertical'>
+              <Row.Label>
+                <Text translation='transactionRow.fee' />
+              </Row.Label>
+              <Row.Value>
+                <Amount.Crypto
+                  value={fromBaseUnit(
+                    txDetails.tx?.fee?.value,
+                    txDetails.feeAsset?.precision ?? 18
+                  )}
+                  symbol={txDetails.feeAsset.symbol}
+                  maximumFractionDigits={6}
+                />
+              </Row.Value>
+            </Row>
+          )}
           <TransactionStatus txStatus={txDetails.tx.status} />
           {toAddress && (
             <Row variant='vertical'>

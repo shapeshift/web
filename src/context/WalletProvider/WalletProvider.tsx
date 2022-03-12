2889c97e6e3ed06c60d7f51429e8bf0e90d2ee74
Amitoj Singh <amitojsingh366@gmail.com>
2022-03-13 00:31:08 +0300

    fix: lint

--- src/context/WalletProvider/WalletProvider.tsx
+++ src/context/WalletProvider/WalletProvider.tsx
@@ -108,15 +108,15 @@ function playSound(type: any) {
 export type ActionTypes =
   | { type: WalletActions.SET_ADAPTERS; payload: Adapters }
   | {
-    type: WalletActions.SET_WALLET
-    payload: {
-      wallet: HDWallet | null
-      name: string
-      icon: ComponentWithAs<'svg', IconProps>
-      deviceId: string
-      meta?: { label: string }
+      type: WalletActions.SET_WALLET
+      payload: {
+        wallet: HDWallet | null
+        name: string
+        icon: ComponentWithAs<'svg', IconProps>
+        deviceId: string
+        meta?: { label: string }
+      }
     }
-  }
   | { type: WalletActions.SET_IS_CONNECTED; payload: boolean }
   | { type: WalletActions.SET_CONNECTOR_TYPE; payload: KeyManager }
   | { type: WalletActions.SET_INITIAL_ROUTE; payload: string }
@@ -193,7 +193,7 @@ export const WalletProvider = ({ children }: { children: React.ReactNode }): JSX
 
   useEffect(() => {
     if (state.keyring) {
-      ; (async () => {
+      ;(async () => {
         const adapters: Adapters = new Map()
         let options: undefined | { portisAppId: string }
         for (const wallet of Object.values(KeyManager)) {
@@ -254,7 +254,7 @@ export const WalletProvider = ({ children }: { children: React.ReactNode }): JSX
       }
     })
 
-    ipcRenderer.on('playSound', (event, data) => { })
+    ipcRenderer.on('playSound', (event, data) => {})
 
     ipcRenderer.on('attach', (event, data) => {
       dispatch({ type: WalletActions.SET_KEEPKEY_STATE, payload: data.state })
@@ -442,7 +442,7 @@ export const WalletProvider = ({ children }: { children: React.ReactNode }): JSX
 
     //END HDwallet API
 
-    ipcRenderer.on('setDevice', (event, data) => { })
+    ipcRenderer.on('setDevice', (event, data) => {})
 
     ipcRenderer.on('signTx', async (event: any, data: any) => {
       let unsignedTx = data.payload.data
@@ -531,7 +531,7 @@ export const WalletProvider = ({ children }: { children: React.ReactNode }): JSX
     const localWalletType = getLocalWalletType()
     const localWalletDeviceId = getLocalWalletDeviceId()
     if (localWalletType && localWalletDeviceId && state.adapters) {
-      ; (async () => {
+      ;(async () => {
         if (state.adapters?.has(localWalletType)) {
           switch (localWalletType) {
             case KeyManager.Native:

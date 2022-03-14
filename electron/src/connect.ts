99c969b2562f11aaeebcdc1339e3eedc536a694a
highlander <bithighlander@gmail.com>
2022-03-14 15:11:40 -0600

    sessionProposial begin

--- electron/src/connect.ts
+++ electron/src/connect.ts
@@ -45,6 +45,103 @@ export const COSMOS_SIGNING_METHODS = {
     COSMOS_SIGN_AMINO: 'cosmos_signAmino'
 }
 
+/*
+
+example event
+{
+   relay: { protocol: 'waku' },
+   topic: '0537892ecb22ca40b14e6fcd6f8190a110fa9a2782c993397821c36ec26c4322',
+   proposer: {
+     publicKey: 'f961e93e195d1b118bffef3ad5ff4aaf2aa80bbf8168ba3fbd73445d81832561',
+     controller: false,
+     metadata: {
+       description: 'React App for WalletConnect',
+       url: 'https://react-app.walletconnect.com',
+       icons: '[array]',
+       name: 'React App'
+     }
+   },
+   signal: {
+     method: 'pairing',
+     params: {
+       topic: '95adfef496af30fe59aea86aca1ee3ccfaefe8390fc6b25b1850643d606b3532'
+     }
+   },
+   permissions: {
+     blockchain: { chains: '[array]' },
+     jsonrpc: { methods: '[array]' },
+     notifications: { types: '[array]' }
+   },
+   ttl: 604800
+ }
+
+
+ */
+
+export async function approveWalletConnect(event: any, payload: any) {
+    let tag = " | pairWalletConnect | "
+    try {
+        let proposal = {
+            "relay":{
+                "protocol":"waku"
+            },
+            "topic":"827c48aeaad46ed796b0ca52825373e9893f66c7735bd3e1cb4b44d56f4e4308",
+            "proposer":{
+                "publicKey":"c4dc6e2b7ffedef5370ceb1edf79e5cdc9c3100941e9e5590e67e5178d6fd958",
+                "controller":false,
+                "metadata":{
+                    "description":"React App for WalletConnect",
+                    "url":"https://react-app.walletconnect.com",
+                    "icons":[
+                        "https://react-app.walletconnect.com/favicon.ico"
+                    ],
+                    "name":"React App"
+                }
+            },
+            "signal":{
+                "method":"pairing",
+                "params":{
+                    "topic":"4ee2bb7aa62095430ef208742dac19097070d142d4ab8c4fd882d4312bbbd669"
+                }
+            },
+            "permissions":{
+                "blockchain":{
+                    "chains":[
+                        "eip155:1"
+                    ]
+                },
+                "jsonrpc":{
+                    "methods":[
+                        "eth_sendTransaction",
+                        "eth_signTransaction",
+                        "eth_sign",
+                        "personal_sign",
+                        "eth_signTypedData"
+                    ]
+                },
+                "notifications":{
+                    "types":[
+
+                    ]
+                }
+            },
+            "ttl":604800
+        }
+
+        //
+        const accounts = ['0xBlaBlaTesting']
+        const response = {
+            state: {
+                accounts
+            }
+        }
+        await walletConnectClient.approve({ proposal, response })
+    } catch (e) {
+        log.error(e)
+    }
+}
+
+
 export async function pairWalletConnect(event: any, payload: any) {
     let tag = " | pairWalletConnect | "
     try {
@@ -79,10 +176,62 @@ export async function createWalletConnectClient(event: IpcMainEvent) {
     //Wallet Connect Events
     //ref https://github.com/WalletConnect/web-examples/blob/main/wallets/react-wallet-v2/src/hooks/useWalletConnectEventsManager.ts
 
+
+    /*
+        Example
+        {
+            "relay":{
+                "protocol":"waku"
+            },
+            "topic":"827c48aeaad46ed796b0ca52825373e9893f66c7735bd3e1cb4b44d56f4e4308",
+            "proposer":{
+                "publicKey":"c4dc6e2b7ffedef5370ceb1edf79e5cdc9c3100941e9e5590e67e5178d6fd958",
+                "controller":false,
+                "metadata":{
+                    "description":"React App for WalletConnect",
+                    "url":"https://react-app.walletconnect.com",
+                    "icons":[
+                        "https://react-app.walletconnect.com/favicon.ico"
+                    ],
+                    "name":"React App"
+                }
+            },
+            "signal":{
+                "method":"pairing",
+                "params":{
+                    "topic":"4ee2bb7aa62095430ef208742dac19097070d142d4ab8c4fd882d4312bbbd669"
+                }
+            },
+            "permissions":{
+                "blockchain":{
+                    "chains":[
+                        "eip155:1"
+                    ]
+                },
+                "jsonrpc":{
+                    "methods":[
+                        "eth_sendTransaction",
+                        "eth_signTransaction",
+                        "eth_sign",
+                        "personal_sign",
+                        "eth_signTypedData"
+                    ]
+                },
+                "notifications":{
+                    "types":[
+
+                    ]
+                }
+            },
+            "ttl":604800
+        }
+
+     */
     let onSessionProposal = function (params: any) {
         let tag = " | onSessionProposal | "
         try {
             log.info(tag, "params: ", params)
+            log.info(tag, "params: ",JSON.stringify(params))
 
             //TODO do something
             event.sender.send("@app/onSessionProposal", {});
@@ -96,6 +245,7 @@ export async function createWalletConnectClient(event: IpcMainEvent) {
         let tag = " | onSessionCreated | "
         try {
             log.info(tag, "params: ", params)
+            log.info(tag, "params: ",JSON.stringify(params))
 
             //TODO save dapp into pairing
             event.sender.send("@app/onSessionCreated", {});

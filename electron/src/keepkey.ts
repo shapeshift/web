d3f18c4b6bc30487dcc4657fdf692745cde21736
highlander <bithighlander@gmail.com>
2022-03-17 15:06:32 -0600

    simplify firmware updating

--- electron/src/keepkey.ts
+++ electron/src/keepkey.ts
@@ -89,30 +89,33 @@ export const update_keepkey_status = async function () {
         }
         log.info(tag, "resultInit: ", resultInit)
 
-        let allDevices = usb.getDeviceList()
-        log.info(tag, "allDevices: ", allDevices)
-
-        let resultWebUsb = usb.findByIds(11044, 2)
-        if (resultWebUsb) {
-            log.info(tag, "KeepKey connected in webusb!")
-            //TODO only trigger if firmware modal open
-            windows?.mainWindow?.webContents.send('onCompleteFirmwareUpload', {})
-        }
-
-        let resultPreWebUsb = usb.findByIds(11044, 1)
-        if (resultPreWebUsb) {
-            log.info(tag, "update required! (resultPreWebUsb)")
-            log.info(tag, "update required! (resultPreWebUsb): ",resultInit.bootloaderVersion)
-            //update firmware next
-            if (resultInit.bootloaderVersion === "v1.1.0") {
-                windows?.mainWindow?.webContents.send('openFirmwareUpdate', { })
-            }
-        }
-
-        let resultUpdater = usb.findByIds(11044, 1)
-        if (resultUpdater) {
-            // log.info(tag, "UPDATER MODE DETECTED!")
-        }
+        // let allDevices = usb.getDeviceList()
+        // log.info(tag, "allDevices: ", allDevices)
+        //
+        // let resultWebUsb = usb.findByIds(11044, 2)
+        // if (resultWebUsb) {
+        //     log.info(tag, "KeepKey connected in webusb!")
+        //     //TODO only trigger if firmware modal open
+        //     windows?.mainWindow?.webContents.send('onCompleteFirmwareUpload', {})
+        // }
+        //
+        // let resultPreWebUsb = usb.findByIds(11044, 1)
+        // if (resultPreWebUsb) {
+        //     log.info(tag, "update required! (resultPreWebUsb)")
+        //     log.info(tag, "update required! (resultPreWebUsb): ",resultInit.bootloaderVersion)
+        //     //update firmware next
+        //     if (resultInit.bootloaderVersion === "v1.1.0") {
+        //         windows?.mainWindow?.webContents.send('openFirmwareUpdate', { })
+        //     } else {
+        //         log.info(tag, "update required! (resultPreWebUsb): Update bootloader!")
+        //         windows?.mainWindow?.webContents.send('openBootloaderUpdate', { })
+        //     }
+        // }
+        //
+        // let resultUpdater = usb.findByIds(11044, 1)
+        // if (resultUpdater) {
+        //     // log.info(tag, "UPDATER MODE DETECTED!")
+        // }
     } catch (e) {
         log.error(tag,e)
     }

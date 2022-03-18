dd396dc8e9732d2b06c32b3b78ba54c37603f10c
LPX <info@shapeshift.one>
2022-03-18 10:21:08 +0900

    empty locale for visual help during translation

--- src/assets/translations/index.ts
+++ src/assets/translations/index.ts
@@ -1,5 +1,6 @@
 /* eslint-disable simple-import-sort/imports */
 import en from './main.json'
+import nd from './dev/main.json'
 import es from './es/main.json'
 import fr from './fr/main.json'
 import id from './id/main.json'
@@ -8,4 +9,4 @@ import pt from './pt/main.json'
 import ru from './ru/main.json'
 import zh from './zh/main.json'
 
-export const translations: any = { en, es, fr, id, ko, pt, ru, zh }
+export const translations: any = { en, nd, es, fr, id, ko, pt, ru, zh }

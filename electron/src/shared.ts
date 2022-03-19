75f2b22967a3a5b5755f77c21aa6a49edb9cdd60
highlander <bithighlander@gmail.com>
2022-03-19 17:51:16 -0600

    begin state engine

--- electron/src/shared.ts
+++ electron/src/shared.ts
@@ -1,10 +1,30 @@
 import { IpcMainEvent } from "electron";
 
+/*
+
+  KeepKey Status codes
+  state : status
+  ---------------
+     -1 : error
+      0 : preInit
+      1 : no devices
+      2 : Bootloader mode
+      3 : Bootloader out of date
+      4 : updating bootloader
+      5 : Firmware out of date
+      6 : updating firmware
+      7 : device connected
+      8 : bridge online
+
+ */
+
 export const shared: {
     USER: userType,
     SIGNED_TX: any,
     eventIPC: IpcMainEvent | null,
     KEEPKEY_FEATURES: Record<string, unknown>
+    KEEPKEY_STATE: number
+    KEEPKEY_STATUS: string
 } = {
     USER: {
         online: false,
@@ -13,7 +33,9 @@ export const shared: {
     },
     SIGNED_TX: null,
     eventIPC: null,
-    KEEPKEY_FEATURES: {}
+    KEEPKEY_FEATURES: {},
+    KEEPKEY_STATE: 0,
+    KEEPKEY_STATUS: "preInit"
 }
 
 export type userType = {

4f02e5e5a00fe239329781e99a3fad1a761dd45e
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2022-03-22 17:30:42 +1100

    feat: better handle translations in radio buttons

--- src/components/Radio/Radio.tsx
+++ src/components/Radio/Radio.tsx
@@ -7,12 +7,15 @@ import {
   useRadioGroup,
   UseRadioProps
 } from '@chakra-ui/react'
+import { ThemeTypings } from '@chakra-ui/styled-system'
 import { HistoryTimeframe } from '@shapeshiftoss/types'
+import Polyglot, { InterpolationOptions } from 'node-polyglot'
 import { memo } from 'react'
+import { useTranslate } from 'react-polyglot'
 import { Text } from 'components/Text'
 
 interface RadioCardProps extends UseRadioProps {
-  label: string
+  label: string | [string, number | Polyglot.InterpolationOptions]
 }
 
 const RadioCard = memo((props: RadioCardProps) => {
@@ -20,9 +23,12 @@ const RadioCard = memo((props: RadioCardProps) => {
   const { getInputProps, getCheckboxProps } = useRadio({ id, ...props })
   const input = getInputProps()
   const checkbox = getCheckboxProps()
+  const translate: (phrase: string, options?: number | InterpolationOptions) => string =
+    useTranslate()
+  const ariaLabel = typeof props.label === 'string' ? props.label : translate(...props.label)
   return (
     <>
-      <Button aria-label={props.label} as='label' htmlFor={input.id} cursor='pointer' {...checkbox}>
+      <Button aria-label={ariaLabel} as='label' htmlFor={input.id} cursor='pointer' {...checkbox}>
         <Text translation={props.label} />
       </Button>
       <input {...input} />
@@ -30,13 +36,18 @@ const RadioCard = memo((props: RadioCardProps) => {
   )
 })
 
+export interface RadioOption<T> {
+  label: string | [string, number | Polyglot.InterpolationOptions]
+  value: T
+}
+
 export interface RadioProps<T> {
   name?: string
   defaultValue?: T
-  options: { label: string; value: T }[]
+  options: RadioOption<T>[]
   onChange: (value: T) => void
   variant?: string
-  colorScheme?: string
+  colorScheme?: ThemeTypings['colorSchemes']
   buttonGroupProps?: ButtonGroupProps
 }
 

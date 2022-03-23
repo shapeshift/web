cb2c3b494fd7b106c8c4e543ab52c51e396fa186
DaoDev44 <88341957+DaoDev44@users.noreply.github.com>
2022-03-23 17:56:56 -0600

    chore: add error handling (#468)
    * add error handling
    
    * title
    
    * fix balance
    
    * use String instead of toString
    
    * use bn helper functions isFinite and lte
    
    * use bignumber toString
    
    * add value to error handling
    
    * remove String

--- packages/chain-adapters/src/cosmossdk/cosmos/CosmosChainAdapter.ts
+++ packages/chain-adapters/src/cosmossdk/cosmos/CosmosChainAdapter.ts
@@ -99,7 +99,15 @@ export class ChainAdapter
       const account = await this.getAccount(from)
 
       if (sendMax) {
-        tx.value = bnOrZero(account.balance).minus(gas).toString()
+        try {
+          const val = bnOrZero(account.balance).minus(gas)
+          if (val.isFinite() || val.lte(0)) {
+            throw new Error(`CosmosChainAdapter: transaction value is invalid: ${val.toString()}`)
+          }
+          tx.value = val.toString()
+        } catch (error) {
+          return ErrorHandler(error)
+        }
       }
 
       const utx: CosmosTx = {

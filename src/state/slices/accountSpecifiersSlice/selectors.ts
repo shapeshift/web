dde0481fe191ba61ef06afd936fdcd8879561703
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-03-24 17:50:46 +0100

    fix: lowercase account specifier strings

--- src/state/slices/accountSpecifiersSlice/selectors.ts
+++ src/state/slices/accountSpecifiersSlice/selectors.ts
@@ -6,5 +6,5 @@ export const selectAccountSpecifiers = (state: ReduxState) =>
 // returns an array of the full `caip2:pubkeyish` type, as used in URLs for account pages
 export const selectAccountSpecifierStrings = (state: ReduxState) =>
   state.accountSpecifiers.accountSpecifiers.map(accountSpecifier =>
-    Object.entries(accountSpecifier)[0].join(':')
+    Object.entries(accountSpecifier)[0].join(':').toLowerCase()
   )

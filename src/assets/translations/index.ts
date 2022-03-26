5186fe5d892a60461650fbdafff8911e840ffc7a
LPX <info@shapeshift.one>
2022-03-26 11:23:58 +0900

    Update index.ts

--- src/assets/translations/index.ts
+++ src/assets/translations/index.ts
@@ -1,6 +1,4 @@
-/* eslint-disable simple-import-sort/imports */
 import en from './main.json'
-import nd from './dev/main.json'
 import es from './es/main.json'
 import fr from './fr/main.json'
 import id from './id/main.json'
@@ -9,4 +7,4 @@ import pt from './pt/main.json'
 import ru from './ru/main.json'
 import zh from './zh/main.json'
 
-export const translations: any = { en, nd, es, fr, id, ko, pt, ru, zh }
+export const translations: any = { en, es, fr, id, ko, pt, ru, zh }

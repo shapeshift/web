3c0d783d646c55c96bfe08f5ef2e990c83a505c5
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-03-30 11:14:50 +0200

    chore: remove createDeepOutputSelector on primitive returned

--- src/state/slices/stakingDataSlice/selectors.ts
+++ src/state/slices/stakingDataSlice/selectors.ts
@@ -43,7 +43,7 @@ export const selectStakingDatabyAccountSpecifier = createSelector(
   }
 )
 
-export const selectDelegationCryptoAmountByDenom = createDeepEqualOutputSelector(
+export const selectDelegationCryptoAmountByDenom = createSelector(
   selectStakingDatabyAccountSpecifier,
   selectValidatorAddress,
   selectDenom,

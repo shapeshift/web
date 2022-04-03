0f690d29718203a9ec63fa4cc6487ae89ed77b03
Trần Trung Tín <trungtindev@gmail.com>
2022-04-03 14:53:11 +0700

    useHasAppUpdated: use then/catch instead of IIFE

--- src/hooks/useHasAppUpdated/useHasAppUpdated.ts
+++ src/hooks/useHasAppUpdated/useHasAppUpdated.ts
@@ -23,57 +23,65 @@ export const useHasAppUpdated = () => {
   const envUrl = '/env.json'
 
   const storeAndCompareAsset = async () => {
-    // we don't care about updates locally obv
-    if (window.location.hostname === 'localhost') return
+    try {
+      // we don't care about updates locally obv
+      if (window.location.hostname === 'localhost') return
 
-    // the changed manifest, will be null if there's no change
-    const changedManifest = await (async () => {
-      const manifestMainJs = await fetchAsset(assetManifestUrl)
-      if (!manifestMainJs) {
-        console.error(`useHasAppUpdated: can't find main.js in asset-manifest.json`)
+      // the changed manifest, will be null if there's no change
+      const changedManifest = await fetchAsset(assetManifestUrl)
+        .then(manifestMainJs => {
+          if (!manifestMainJs) {
+            console.error(`useHasAppUpdated: can't find main.js in asset-manifest.json`)
+          }
+          if (!initialManifestMainJs) {
+            // first run
+            if (manifestMainJs) setManifestMainJs(manifestMainJs)
+          } else {
+            // subsequent runs
+            if (manifestMainJs && !isEqual(manifestMainJs, initialManifestMainJs)) {
+              return manifestMainJs
+            }
+          }
+          return null
+        })
+        .catch(e => {
+          console.error(`useHasAppUpdated: error comparing ${assetManifestUrl} for changes`, e)
+        })
+
+      const changedEnv = await fetchAsset(envUrl)
+        .then(env => {
+          if (!env) {
+            console.error(`useHasAppUpdated: can't find env in env.json`)
+          }
+          if (!initialEnv) {
+            // first run
+            if (env) setEnv(env)
+          } else {
+            // subsequent runs
+            if (env && !isEqual(env, initialEnv)) {
+              return env
+            }
+          }
+          return null
+        })
+        .catch(e => {
+          console.error(`useHasAppUpdated: error comparing ${envUrl} for changes`, e)
+        })
+
+      if (changedManifest != null || changedEnv != null) {
+        console.info(
+          !changedManifest != null
+            ? `useHasAppUpdated: app updated, manifest: ${JSON.stringify(
+                changedManifest
+              )}, initial: ${JSON.stringify(initialManifestMainJs)}`
+            : `useHasAppUpdated: app updated due to changing env, env: ${JSON.stringify(
+                changedEnv
+              )}, initial: ${JSON.stringify(initialEnv)}`
+        )
+        setHasUpdated(true)
       }
-
-      if (!initialManifestMainJs) {
-        // first run
-        if (manifestMainJs) setManifestMainJs(manifestMainJs)
-      } else {
-        // subsequent runs
-        if (manifestMainJs && !isEqual(manifestMainJs, initialManifestMainJs)) {
-          return manifestMainJs
-        }
-      }
-      return null
-    })()
-
-    const changedEnv = await (async () => {
-      const env = await fetchAsset(envUrl)
-      if (!env) {
-        console.error(`useHasAppUpdated: can't find env in env.json`)
-      }
-
-      if (!initialEnv) {
-        // first run
-        if (env) setEnv(env)
-      } else {
-        // subsequent runs
-        if (env && !isEqual(env, initialEnv)) {
-          return env
-        }
-      }
-      return null
-    })()
-
-    if (changedManifest != null || changedEnv != null) {
-      console.info(
-        !changedManifest != null
-          ? `useHasAppUpdated: app updated, manifest: ${JSON.stringify(
-              changedManifest
-            )}, initial: ${JSON.stringify(initialManifestMainJs)}`
-          : `useHasAppUpdated: app updated due to changing env, env: ${JSON.stringify(
-              changedEnv
-            )}, initial: ${JSON.stringify(initialEnv)}`
-      )
-      setHasUpdated(true)
+    } catch (e) {
+      console.error(`useHasAppUpdated: error comparing assets`, e)
     }
   }
 

de1a1336b952ccc79b7ea76a878a1805201f89af
Sean Luther <elmutt@gmail.com>
2022-04-04 12:30:36 -0600

    fix bad boolean logic

--- src/hooks/useBalanceChartData/useBalanceChartData.ts
+++ src/hooks/useBalanceChartData/useBalanceChartData.ts
@@ -253,7 +253,7 @@ export const calculateBucketPrices: CalculateBucketPrices = args => {
       tx.transfers.forEach(transfer => {
         const asset = transfer.caip19
 
-        if (!(assetIds.includes(asset) && skipBucketBalanceChange)) return
+        if (!assetIds.includes(asset) || !skipBucketBalanceChange) return
 
         const bucketValue = bnOrZero(bucket.balance.crypto[asset])
         const transferValue = bnOrZero(transfer.value)

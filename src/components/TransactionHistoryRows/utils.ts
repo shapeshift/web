7828b046b320a12f114bd0d50a05063976854e45
stackedQ <stackedQ@gmail.com>
2022-04-04 16:42:02 +0430

    feat: fallback values for unknown asset.

--- src/components/TransactionHistoryRows/utils.ts
+++ src/components/TransactionHistoryRows/utils.ts
@@ -6,34 +6,39 @@ export enum AssetTypes {
   Fee = 'fee'
 }
 
+const fallbackValues = {
+  symbol: ' N/A',
+  precision: 18
+}
+
 export const parseRelevantAssetFromTx = (txDetails: TxDetails, assetType: AssetTypes | null) => {
   switch (assetType) {
     case AssetTypes.Source:
       return {
-        symbol: txDetails.sellAsset?.symbol ?? '',
+        symbol: txDetails.sellAsset?.symbol ?? fallbackValues.symbol,
         amount: txDetails.sellTransfer?.value ?? '0',
-        precision: txDetails.sellAsset?.precision ?? 0,
+        precision: txDetails.sellAsset?.precision ?? fallbackValues.precision,
         currentPrice: txDetails.sourceMarketData?.price
       }
     case AssetTypes.Destination:
       return {
-        symbol: txDetails.buyAsset?.symbol ?? '',
+        symbol: txDetails.buyAsset?.symbol ?? fallbackValues.symbol,
         amount: txDetails.buyTransfer?.value ?? '0',
-        precision: txDetails.buyAsset?.precision ?? 0,
+        precision: txDetails.buyAsset?.precision ?? fallbackValues.precision,
         currentPrice: txDetails.destinationMarketData?.price
       }
     case AssetTypes.Fee:
       return {
-        symbol: txDetails.feeAsset?.symbol ?? '',
+        symbol: txDetails.feeAsset?.symbol ?? fallbackValues.symbol,
         amount: txDetails.tx.fee?.value ?? '0',
-        precision: txDetails.feeAsset?.precision ?? 0,
+        precision: txDetails.feeAsset?.precision ?? fallbackValues.precision,
         currentPrice: txDetails.feeMarketData?.price
       }
     default:
       return {
-        symbol: txDetails.symbol,
+        symbol: txDetails.symbol ?? fallbackValues.symbol,
         amount: txDetails.value ?? '0',
-        precision: txDetails.precision,
+        precision: txDetails.precision ?? fallbackValues.precision,
         currentPrice: undefined
       }
   }

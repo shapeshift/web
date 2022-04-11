fdc05f8618da272d36f013a6571735770ffe0d4e
MrNerdHair <mrnerdhair@gmail.com>
2022-04-11 15:18:37 -0400

    chore: remove unused env vars

--- src/Routes/PrivateRoute.tsx
+++ src/Routes/PrivateRoute.tsx
@@ -1,16 +1,13 @@
-import { getConfig } from 'config'
 import { Redirect, Route, RouteProps } from 'react-router-dom'
 
 type PrivateRouteProps = {
   hasWallet: boolean
 } & RouteProps
 
-const HIDE_SPLASH = getConfig().REACT_APP_HIDE_SPLASH
-
 export const PrivateRoute = ({ hasWallet, ...rest }: PrivateRouteProps) => {
   const { location } = rest
 
-  return hasWallet || HIDE_SPLASH ? (
+  return hasWallet ? (
     <Route {...rest} />
   ) : (
     <Redirect
--- src/config.ts
+++ src/config.ts
@@ -16,7 +16,6 @@ const validators = {
   REACT_APP_UNCHAINED_COSMOS_WS_URL: url(),
   REACT_APP_ETHEREUM_NODE_URL: url(),
   REACT_APP_PORTIS_DAPP_ID: str({ devDefault: 'fakePortisId' }),
-  REACT_APP_HIDE_SPLASH: bool({ default: false }),
   REACT_APP_GEM_COINIFY_SUPPORTED_COINS: url(),
   REACT_APP_GEM_WYRE_SUPPORTED_COINS: url(),
   REACT_APP_GEM_ASSET_LOGO: url(),

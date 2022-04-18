4ad7708942085fe38e207f43940e0abe4c5a670e
Chris Thompson <1958266+cjthompson@users.noreply.github.com>
2022-04-18 10:56:32 -0600

    fix: change calls to "getSupportedChains()" to use "PluginManager.supportedChains" (#1482)
    "supportedChains" is a reactive state variable so it will cause re-renders when it changes
    
    using `adapters.getSupportedChains()` isn't reactive.

--- src/context/TransactionsProvider/TransactionsProvider.tsx
+++ src/context/TransactionsProvider/TransactionsProvider.tsx
@@ -3,7 +3,7 @@ import { utxoAccountParams } from '@shapeshiftoss/chain-adapters'
 import isEmpty from 'lodash/isEmpty'
 import React, { useCallback, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { useChainAdapters } from 'context/PluginProvider/PluginProvider'
+import { usePlugins } from 'context/PluginProvider/PluginProvider'
 import { useWallet } from 'hooks/useWallet/useWallet'
 import { walletSupportChain } from 'hooks/useWalletSupportsChain/useWalletSupportsChain'
 import { AccountSpecifierMap } from 'state/slices/accountSpecifiersSlice/accountSpecifiersSlice'
@@ -29,7 +29,7 @@ export const TransactionsProvider = ({ children }: TransactionsProviderProps): J
   const {
     state: { wallet, walletInfo },
   } = useWallet()
-  const chainAdapter = useChainAdapters()
+  const { chainAdapterManager, supportedChains } = usePlugins()
   const assets = useSelector(selectAssets)
   const portfolioAssetIds = useSelector(selectPortfolioAssetIds)
   const accountSpecifiers = useSelector(selectAccountSpecifiers)
@@ -50,10 +50,9 @@ export const TransactionsProvider = ({ children }: TransactionsProviderProps): J
   useEffect(() => {
     if (!wallet) return
     if (isEmpty(assets)) return
-    const supportedChains = chainAdapter.getSupportedChains()
     ;(async () => {
       for (const chain of supportedChains) {
-        const adapter = chainAdapter.byChain(chain)
+        const adapter = chainAdapterManager.byChain(chain)
         const chainId = adapter.getCaip2()
         if (!walletSupportChain({ chainId, wallet })) continue
 
@@ -123,7 +122,7 @@ export const TransactionsProvider = ({ children }: TransactionsProviderProps): J
       dispatch(txHistory.actions.clear())
       supportedChains.forEach(chain => {
         try {
-          const adapter = chainAdapter.byChain(chain)
+          const adapter = chainAdapterManager.byChain(chain)
           adapter.unsubscribeTxs()
         } catch (e) {
           console.error('TransactionsProvider: Error unsubscribing from transaction history', e)
@@ -135,7 +134,8 @@ export const TransactionsProvider = ({ children }: TransactionsProviderProps): J
     dispatch,
     walletInfo?.deviceId,
     wallet,
-    chainAdapter,
+    chainAdapterManager,
+    supportedChains,
     accountSpecifiers,
     getAccountSpecifiersByChainId,
     portfolioAssetIds,

943b4aa8a89d75dd48c69159b1d6db9f6336bd6e
Amin Limbada <aminlimbada94@gmail.com>
2022-04-19 22:08:37 +0400

    chore: fix path after rebase

--- src/context/WalletProvider/config.ts
+++ src/context/WalletProvider/config.ts
@@ -34,9 +34,9 @@ import { NativeConfig } from './NativeWallet/config'
 import { PortisConnect } from './Portis/components/Connect'
 import { PortisFailure } from './Portis/components/Failure'
 import { PortisConfig } from './Portis/config'
-import { XDeFiConnect } from './XDeFi/components/Connect'
-import { XDeFiFailure } from './XDeFi/components/Failure'
-import { XDEFIConfig } from './XDeFi/config'
+import { XDEFIConnect } from './XDEFI/components/Connect'
+import { XDEFIFailure } from './XDEFI/components/Failure'
+import { XDEFIConfig } from './XDEFI/config'
 
 export interface SupportedWalletInfo {
   adapter: any
@@ -99,9 +99,9 @@ export const SUPPORTED_WALLETS: Record<KeyManager, SupportedWalletInfo> = {
   [KeyManager.XDefi]: {
     ...XDEFIConfig,
     routes: [
-      { path: '/xdefi/connect', component: XDeFiConnect },
+      { path: '/xdefi/connect', component: XDEFIConnect },
 
-      { path: '/xdefi/failure', component: XDeFiFailure },
+      { path: '/xdefi/failure', component: XDEFIFailure },
     ],
   }
 }

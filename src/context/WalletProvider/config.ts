93dbf707c3247539078f80967867a52f7caba3ea
NeOMakinG <github@szczupak.fr>
2022-04-19 17:07:51 +0200

    feat: add legacy login form to the wallet modal (#1503)
    * feat: add legacy login form to wallet modal
    
    * fix: add feature flag and improve form
    
    * fix: naming nitpicks
    
    * fix: wrong translation and unneeded returns

--- src/context/WalletProvider/config.ts
+++ src/context/WalletProvider/config.ts
@@ -15,6 +15,7 @@ import { MetaMaskConnect } from './MetaMask/components/Connect'
 import { MetaMaskFailure } from './MetaMask/components/Failure'
 import { MetaMaskConfig } from './MetaMask/config'
 import { EnterPassword } from './NativeWallet/components/EnterPassword'
+import { LegacyLogin } from './NativeWallet/components/LegacyLogin'
 import { NativeCreate } from './NativeWallet/components/NativeCreate'
 import { NativeImport } from './NativeWallet/components/NativeImport'
 import { NativeLoad } from './NativeWallet/components/NativeLoad'
@@ -48,6 +49,7 @@ export const SUPPORTED_WALLETS: Record<KeyManager, SupportedWalletInfo> = {
       { path: '/native/create-test', component: NativeTestPhrase },
       { path: '/native/success', component: NativeSuccess },
       { path: '/native/enter-password', component: EnterPassword },
+      { path: '/native/legacy/login', component: LegacyLogin },
     ],
   },
   [KeyManager.KeepKey]: {

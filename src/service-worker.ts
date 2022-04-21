087d393a6b25ca939c628445338af99aa8b5e1a6
Chris Thompson <chris@thompson-web.org>
2022-04-21 16:02:59 -0600

    feat: add service-worker
    closes #1545

---
+++ src/service-worker.ts
@@ -0,0 +1,37 @@
+/// <reference lib="webworker" />
+declare const self: ServiceWorkerGlobalScope &
+  typeof globalThis & { __WB_MANIFEST: { revision: string | null; url: string } }
+
+export {}
+
+// create-react-app calls workbox-webpack-plugin.InjectManifest by default
+// and will throw an error if __WEB_MANIFEST isn't referenced in the service worker file
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const manifest = self.__WB_MANIFEST
+
+self.addEventListener('activate', () => self.clients.claim())
+
+self.addEventListener('install', event => {
+  console.info('ServiceWorker:install', event)
+  event.waitUntil(
+    (async () => {
+      try {
+        console.info('ServiceWorker:install:skipWaiting')
+        await self.skipWaiting()
+      } catch (e) {
+        console.error('ServiceWorker:install:Error', e)
+      }
+    })(),
+  )
+})
+
+self.addEventListener('message', event => {
+  console.info('ServiceWorker:message', event)
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting()
+  }
+})
+
+self.addEventListener('fetch', event => {
+  console.info('ServiceWorker:fetch', event)
+})

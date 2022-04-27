9905d78d0c880e69be6bb12eaf7faa722c95489c
hpratt <hpratt111@live.com>
2022-04-27 10:16:09 -0400

    add messages for missing or errored price history and market data

---
+++ src/components/MissingDataFeedback/Message.tsx
@@ -0,0 +1,22 @@
+import { Alert, Box } from '@chakra-ui/react'
+import { FaInfoCircle } from 'react-icons/fa'
+import { IconCircle } from 'components/IconCircle'
+import { Text } from 'components/Text'
+import React from 'react'
+
+export const MissingDataMessage: React.FC<{ tkey: 'assetUnavailable' | 'assetErrored' | 'priceHistoryLookupErrored' | 'priceHistoryUnavailable' | 'balanceHistoryErrored' | 'balanceHistoryUnavailable' | 'loading' }> = ({ tkey }) => {
+  return (
+    <Box p={8}>
+      <Alert status='info' variant='subtle' borderRadius='lg' pl={2}>
+        <IconCircle boxSize={8} color='blue.300' background='transparent'>
+          <FaInfoCircle />
+        </IconCircle>
+        <Text
+          color='blue.300'
+          translation={`assets.assetDetails.assetHeader.${tkey}`}
+          fontWeight='semibold'
+        />
+      </Alert>
+    </Box>
+  )
+}

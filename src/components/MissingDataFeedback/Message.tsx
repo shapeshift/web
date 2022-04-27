a7837b3c83e5ec38baa8734d2a9fe59cb7a26402
hpratt <hpratt111@live.com>
2022-04-27 10:28:05 -0400

    run linter

--- src/components/MissingDataFeedback/Message.tsx
+++ src/components/MissingDataFeedback/Message.tsx
@@ -1,10 +1,19 @@
 import { Alert, Box } from '@chakra-ui/react'
+import React from 'react'
 import { FaInfoCircle } from 'react-icons/fa'
 import { IconCircle } from 'components/IconCircle'
 import { Text } from 'components/Text'
-import React from 'react'
 
-export const MissingDataMessage: React.FC<{ tkey: 'assetUnavailable' | 'assetErrored' | 'priceHistoryLookupErrored' | 'priceHistoryUnavailable' | 'balanceHistoryErrored' | 'balanceHistoryUnavailable' | 'loading' }> = ({ tkey }) => {
+export const MissingDataMessage: React.FC<{
+  tkey:
+    | 'assetUnavailable'
+    | 'assetErrored'
+    | 'priceHistoryLookupErrored'
+    | 'priceHistoryUnavailable'
+    | 'balanceHistoryErrored'
+    | 'balanceHistoryUnavailable'
+    | 'loading'
+}> = ({ tkey }) => {
   return (
     <Box p={8}>
       <Alert status='info' variant='subtle' borderRadius='lg' pl={2}>

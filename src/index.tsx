f6515552449cabe9abf5ab129038779974d6c837
MrNerdHair <mrnerdhair@gmail.com>
2022-04-28 18:56:57 -0400

    chore: remove existing serviceworker code

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,6 @@ import { reportWebVitals } from 'lib/reportWebVitals'
 
 import { App } from './App'
 import { AppProviders } from './AppProviders'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,8 +17,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 )
-
-serviceWorkerRegistration.register()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

e895c5ed55a659d6e46df52a6d29ebb84f3823ff
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2022-04-29 15:28:04 -0600

    chore: remove more dead shit

--- src/pages/Assets/Asset.tsx
+++ src/pages/Assets/Asset.tsx
@@ -1,13 +1,9 @@
-import { Flex } from '@chakra-ui/react'
 import type { ChainId } from '@shapeshiftoss/caip'
 import { useParams } from 'react-router-dom'
 import { Route } from 'Routes/helpers'
 import { AssetAccountDetails } from 'components/AssetAccountDetails'
-import { Page } from 'components/Layout/Page'
 import { selectAssetByCAIP19 } from 'state/slices/selectors'
 import { useAppSelector } from 'state/store'
-
-import { LoadingAsset } from './LoadingAsset'
 
 export type MatchParams = {
   chainId: ChainId
@@ -18,14 +14,5 @@ export const Asset = ({ route }: { route?: Route }) => {
   const params = useParams<MatchParams>()
   const assetId = `${params.chainId}/${params.assetSubId}`
   const asset = useAppSelector(state => selectAssetByCAIP19(state, assetId))
-
-  if (!asset)
-    return (
-      <Page>
-        <Flex role='main' flex={1} height='100%'>
-          <LoadingAsset />
-        </Flex>
-      </Page>
-    )
   return <AssetAccountDetails assetId={asset.caip19} route={route} />
 }

d960d59ee05c7f4bba1b1fd8ecc2d2dc6113c90b
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2022-04-29 14:11:33 -0600

    fix: remove redundant checks

--- src/components/AssetAccountDetails.tsx
+++ src/components/AssetAccountDetails.tsx
@@ -4,8 +4,6 @@ import { Route } from 'Routes/helpers'
 import { AssetTransactionHistory } from 'components/TransactionHistory/AssetTransactionHistory'
 import { TradeCard } from 'pages/Dashboard/TradeCard'
 import { AccountSpecifier } from 'state/slices/accountSpecifiersSlice/accountSpecifiersSlice'
-import { selectMarketDataAvailableByAssetId } from 'state/slices/selectors'
-import { useAppSelector } from 'state/store'
 
 import { AccountAssets } from './AccountAssets/AccountAssets'
 import { AssetAccounts } from './AssetAccounts/AssetAccounts'
@@ -24,9 +22,6 @@ type AssetDetailsProps = {
 }
 
 export const AssetAccountDetails = ({ assetId: caip19, accountId }: AssetDetailsProps) => {
-  const isMarketDataAvailable = useAppSelector(state =>
-    selectMarketDataAvailableByAssetId(state, caip19),
-  )
   return (
     <Main titleComponent={<AssetHeader assetId={caip19} accountId={accountId} />}>
       <Stack
@@ -45,7 +40,7 @@ export const AssetAccountDetails = ({ assetId: caip19, accountId }: AssetDetails
         </Stack>
         <Stack flex='1 1 0%' width='full' maxWidth={{ base: 'full', xl: 'sm' }} spacing={4}>
           <TradeCard defaultBuyAssetId={caip19} />
-          {isMarketDataAvailable && <AssetMarketData assetId={caip19} />}
+          <AssetMarketData assetId={caip19} />
           <AssetDescription assetId={caip19} />
         </Stack>
       </Stack>

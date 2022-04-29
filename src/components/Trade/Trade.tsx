b430ab91a969ee674d2b60635f635f766d1c7278
Sean Luther <elmutt@gmail.com>
2022-04-29 13:55:36 -0600

    fix tests

--- src/components/Trade/Trade.tsx
+++ src/components/Trade/Trade.tsx
@@ -1,5 +1,5 @@
 import { AssetId } from '@shapeshiftoss/caip'
-import { Asset, chainAdapters, ChainTypes, Quote, SwapperType } from '@shapeshiftoss/types'
+import { Asset, chainAdapters, ChainTypes, Quote } from '@shapeshiftoss/types'
 import { FormProvider, useForm } from 'react-hook-form'
 import { MemoryRouter, Route, Switch } from 'react-router-dom'
 import { TradeActions } from 'components/Trade/hooks/useSwapper/useSwapper'

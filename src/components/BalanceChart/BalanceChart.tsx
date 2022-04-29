74e6c0b2da59018b869bc894bfaa25c6b29c68be
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2022-04-29 14:15:40 -0600

    chore: revert unnecessary changes to balance chart

--- src/components/BalanceChart/BalanceChart.tsx
+++ src/components/BalanceChart/BalanceChart.tsx
@@ -3,12 +3,9 @@ import { HistoryTimeframe } from '@shapeshiftoss/types'
 import { useEffect } from 'react'
 import { Card } from 'components/Card/Card'
 import { Graph } from 'components/Graph/Graph'
-import { InformationalAlert } from 'components/InformationalAlert/InformationalAlert'
 import { useBalanceChartData } from 'hooks/useBalanceChartData/useBalanceChartData'
 import { calculatePercentChange } from 'lib/charts'
 import { AccountSpecifier } from 'state/slices/accountSpecifiersSlice/accountSpecifiersSlice'
-import { selectBalanceHistoryAvailableByAssetsAndTimeframe } from 'state/slices/selectors'
-import { useAppSelector } from 'state/store'
 
 type BalanceChartArgs = {
   assetIds: AssetId[]
@@ -31,23 +28,12 @@ export const BalanceChart: React.FC<BalanceChartArgs> = ({
     timeframe,
   })
 
-  const isAtLeastOnePriceHistoryAvailable = useAppSelector(state =>
-    selectBalanceHistoryAvailableByAssetsAndTimeframe(state, assetIds, timeframe),
-  )
-
   useEffect(
     () => setPercentChange(calculatePercentChange(balanceChartData)),
     [balanceChartData, setPercentChange],
   )
 
   const color = percentChange > 0 ? 'green.500' : 'red.500'
-
-  if (!balanceChartDataLoading && !isAtLeastOnePriceHistoryAvailable)
-    return (
-      <Card.Body p={0}>
-        <InformationalAlert translation='assets.assetDetails.assetHeader.assetUnavailable' />
-      </Card.Body>
-    )
 
   return (
     <Card.Body p={0} height='350px'>

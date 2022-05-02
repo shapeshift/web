316612291bb961c872ed8aa42fc71efc2bc22ad2
NeOMakinG <github@szczupak.fr>
2022-05-02 18:51:56 +0200

    fix: useWallet inside useKeepKeyVersions

--- src/context/WalletProvider/KeepKey/hooks/useKeepKeyVersions.ts
+++ src/context/WalletProvider/KeepKey/hooks/useKeepKeyVersions.ts
@@ -1,24 +1,90 @@
+import { Features } from '@keepkey/device-protocol/lib/messages_pb'
+import { isKeepKey } from '@shapeshiftoss/hdwallet-keepkey'
+import axios from 'axios'
+import { getConfig } from 'config'
 import { useEffect, useState } from 'react'
-import { useKeepKey } from 'context/WalletProvider/KeepKeyProvider'
+import { useWallet } from 'hooks/useWallet/useWallet'
 
-import { getKeepKeyVersions, Versions } from '../utils'
+interface VersionUrl {
+  version: string
+  url: string
+}
+
+export interface FirmwareDetails {
+  bootloader: VersionUrl
+  firmware: VersionUrl
+}
+
+interface FirmwareReleases {
+  latest: FirmwareDetails
+  hashes: {
+    bootloader: Record<string, string>
+    firmware: Record<string, string>
+  }
+}
+
+interface VersionStatus {
+  device: string
+  latest: string
+  updateAvailable: boolean
+}
+
+interface Versions {
+  bootloader: VersionStatus
+  firmware: VersionStatus
+}
 
 export const useKeepKeyVersions = () => {
   const [versions, setVersions] = useState<Versions>()
   const {
-    keepKeyWallet,
-    state: { features },
-  } = useKeepKey()
+    state: { wallet },
+  } = useWallet()
 
   useEffect(() => {
+    if (!wallet || !isKeepKey(wallet)) return
+
+    const getBootloaderVersion = (
+      releases: FirmwareReleases,
+      features: Features.AsObject,
+    ): string => {
+      const hash = features?.bootloaderHash.toString() ?? ''
+      const buffer = Buffer.from(hash, 'base64')
+      const hex = buffer.toString('hex')
+      return releases.hashes.bootloader[hex.toLowerCase()]
+    }
+
     ;(async () => {
-      const versions = await getKeepKeyVersions(keepKeyWallet, features?.bootloaderHash)
+      const features = await wallet.getFeatures()
+      const { data: releases } = await axios.get<FirmwareReleases>(
+        getConfig().REACT_APP_KEEPKEY_VERSIONS_URL,
+        {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+        },
+      )
 
-      if (!versions) return
+      const bootloaderVersion = getBootloaderVersion(releases, features)
+      const latestBootloader = releases.latest.bootloader.version
+      const deviceFirmware = await wallet.getFirmwareVersion()
+      const latestFirmware = releases.latest.firmware.version
 
+      const versions: Versions = {
+        bootloader: {
+          device: bootloaderVersion,
+          latest: latestBootloader,
+          updateAvailable: bootloaderVersion !== latestBootloader,
+        },
+        firmware: {
+          device: deviceFirmware,
+          latest: latestFirmware,
+          updateAvailable: deviceFirmware !== latestFirmware,
+        },
+      }
       setVersions(versions)
     })()
-  }, [features?.bootloaderHash, keepKeyWallet])
+  }, [])
 
   return versions
 }

5dd6d611a953163b8bf7a1fd18372dd73e951597
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-05-02 21:13:32 +0200

    feat: unskip normalizeOpportunity tests

--- src/features/defi/helpers/normalizeOpportunity.test.tsx
+++ src/features/defi/helpers/normalizeOpportunity.test.tsx
@@ -1,3 +1,4 @@
+import { ChainTypes } from '@shapeshiftoss/types'
 import { renderHook } from '@testing-library/react-hooks'
 import { TestProviders } from 'test/TestProviders'
 import { MergedActiveStakingOpportunity } from 'pages/Defi/hooks/useCosmosStakingBalances'
@@ -9,6 +10,40 @@ jest.mock('pages/Defi/hooks/useVaultBalances')
 jest.mock('@shapeshiftoss/investor-yearn')
 jest.mock('pages/Defi/hooks/useFoxyBalances')
 
+const mockCosmosStakingOpportunities = [
+  {
+    address: 'cosmosvaloper199mlc7fr6ll5t54w7tts7f4s0cvnqgc59nmuxf',
+    moniker: 'ShapeShift DAO',
+    tokens: '42424242',
+    commission: '0.100000000000000000',
+    apr: '0.1528209855',
+    totalDelegations: '42',
+    rewards: '4.2',
+    isLoaded: true,
+    cryptoAmount: '0.407785',
+    tvl: '21040543.6367982',
+    fiatAmount: '4.2',
+    chain: ChainTypes.Cosmos,
+    assetId: 'cosmos:cosmoshub-4/slip44:118',
+    tokenAddress: '118',
+  },
+  {
+    address: 'cosmosvaloper1clpqr4nrk4khgkxj78fcwwh6dl3uw4epsluffn',
+    moniker: 'Cosmostation',
+    tokens: '24242424',
+    commission: '0.089000000000000000',
+    apr: '0.1546887975',
+    totalDelegations: '242424',
+    rewards: '2.5',
+    isLoaded: true,
+    cryptoAmount: '0.013967',
+    tvl: '63799889.014332',
+    fiatAmount: '0.24',
+    chain: ChainTypes.Cosmos,
+    assetId: 'cosmos:cosmoshub-4/slip44:118',
+    tokenAddress: '118',
+  },
+]
 function setup({
   cosmosStakingOpportunities,
 }: {
@@ -28,7 +63,7 @@ function setup({
 }
 
 // TODO(gomes): Unskip me
-describe.skip('useNormalizeOpportunities', () => {
+describe('useNormalizeOpportunities', () => {
   beforeEach(() => {
     ;(useVaultBalances as jest.Mock<unknown>).mockImplementation(() => ({
       vaults: [],
@@ -44,7 +79,7 @@ describe.skip('useNormalizeOpportunities', () => {
 
   it('returns transformed array of active opportunities sorted by cryptoAmount when there are active staking opportunities', async () => {
     const { result } = setup({
-      cosmosStakingOpportunities: [],
+      cosmosStakingOpportunities: mockCosmosStakingOpportunities,
     })
     expect(result.current).toMatchSnapshot()
   })

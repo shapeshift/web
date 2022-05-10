fabd6e34ea42c1f65d32e4b272eef34b3f286c9d
chxash <chxash@users.noreply.github.com>
2022-05-10 09:13:35 +0200

    fix: review comment

--- src/hooks/useEvent/useEvent.tsx
+++ src/hooks/useEvent/useEvent.tsx
@@ -2,6 +2,12 @@ import { useCallback, useLayoutEffect, useRef } from 'react'
 
 type AnyFunction = (...args: any[]) => any
 
+/**
+ * A Hook to define an event handler with an always-stable function identity.
+ * RFC : https://github.com/reactjs/rfcs/blob/useevent/text/0000-useevent.md
+ * Original implementation : https://gist.github.com/diegohaz/695097a06f038a707c3a1b11e4e40195
+ * @param callback
+ */
 export function useEvent<T extends AnyFunction>(callback?: T) {
   const ref = useRef<AnyFunction | undefined>(() => {
     throw new Error('Cannot call an event handler while rendering.')

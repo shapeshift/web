447e0770a776a28d4569376d84ac26ddfc45bb63
chxash <chxash@users.noreply.github.com>
2022-05-13 19:02:15 +0200

    feat: add fox page as plugin (#1782)
    * feat: fox page plugin
    
    * feat: add fox page link to side bar

---
+++ src/plugins/foxPage/foxPage.tsx
@@ -0,0 +1,5 @@
+import { RawText } from 'components/Text'
+
+export const FoxPage = () => {
+  return <RawText>TODO Fox Page</RawText>
+}
---
+++ src/plugins/foxPage/index.tsx
@@ -0,0 +1,25 @@
+import { Plugins } from 'plugins'
+import { FoxIcon } from 'components/Icons/FoxIcon'
+
+import { FoxPage } from './foxPage'
+
+export function register(): Plugins {
+  return [
+    [
+      'foxPage',
+      {
+        name: 'foxPage',
+        featureFlag: 'FoxPage',
+        icon: <FoxIcon />,
+        routes: [
+          {
+            path: '/fox',
+            label: 'navBar.foxToken',
+            main: () => <FoxPage></FoxPage>,
+            icon: <FoxIcon />,
+          },
+        ],
+      },
+    ],
+  ]
+}

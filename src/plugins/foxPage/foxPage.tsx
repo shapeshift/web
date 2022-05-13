20b9cf12952771ff1a053920dca21992133e945a
chxash <chxash@users.noreply.github.com>
2022-05-13 14:55:55 +0200

    feat: implements fox page routing

--- src/plugins/foxPage/foxPage.tsx
+++ src/plugins/foxPage/foxPage.tsx
@@ -1,5 +1,17 @@
+import { useState } from 'react'
 import { RawText } from 'components/Text'
 
-export const FoxPage = () => {
-  return <RawText>TODO Fox Page</RawText>
+export enum FoxPageTab {
+  Fox = 'Fox',
+  Foxy = 'Foxy',
+}
+
+export type FoxPageProps = {
+  initialTab?: FoxPageTab
+}
+
+export const FoxPage = (props: FoxPageProps) => {
+  const [tab] = useState(props.initialTab ?? FoxPageTab.Fox)
+
+  return <RawText>TODO Fox Page // current Tab : {tab}</RawText>
 }
--- src/plugins/foxPage/index.tsx
+++ src/plugins/foxPage/index.tsx
@@ -1,7 +1,7 @@
 import { Plugins } from 'plugins'
 import { FoxIcon } from 'components/Icons/FoxIcon'
 
-import { FoxPage } from './foxPage'
+import { FoxPage, FoxPageTab } from './foxPage'
 
 export function register(): Plugins {
   return [
@@ -17,6 +17,23 @@ export function register(): Plugins {
             label: 'navBar.foxToken',
             main: () => <FoxPage></FoxPage>,
             icon: <FoxIcon />,
+            routes: [
+              {
+                path: '/fox',
+                label: 'navBar.foxToken',
+                main: () => <FoxPage></FoxPage>,
+              },
+              {
+                path: '/foxy',
+                label: 'navBar.foxToken',
+                main: () => <FoxPage initialTab={FoxPageTab.Foxy}></FoxPage>,
+              },
+              {
+                path: '/onefox',
+                label: 'navBar.foxToken',
+                main: () => <FoxPage initialTab={FoxPageTab.Foxy}></FoxPage>,
+              },
+            ],
           },
         ],
       },

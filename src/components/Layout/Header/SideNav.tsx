cdbbee57bbdc614dcaa70c51d9a39a2b7b363fdd
0xf11nix <101975263+0xf11nix@users.noreply.github.com>
2022-05-15 00:08:55 +1000

    chore: dynamically calculate top on SideNav

--- src/components/Layout/Header/SideNav.tsx
+++ src/components/Layout/Header/SideNav.tsx
@@ -1,11 +1,19 @@
 import { chakra, useColorModeValue } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+import { useWallet } from 'hooks/useWallet/useWallet'
 
 import { SideNavContent } from './SideNavContent'
 
 export const SideNav = () => {
   const bg = useColorModeValue('white', 'gray.850')
   const borderColor = useColorModeValue('gray.100', 'gray.750')
-
+  const {
+    state: { walletInfo },
+  } = useWallet()
+  const [top, setTop] = useState('4.5rem')
+  useEffect(() => {
+    setTop(walletInfo?.deviceId === 'DemoWallet' ? '7rem' : '4.5rem')
+  }, [walletInfo?.deviceId])
   return (
     <>
       <chakra.header
@@ -15,9 +23,9 @@ export const SideNav = () => {
         borderColor={borderColor}
         left='0'
         right='0'
-        height='calc(100vh - 4.5rem)'
+        height={`calc(100vh - ${top})`}
         position='sticky'
-        top='4.5rem'
+        top={top}
         maxWidth='xs'
         flex={{ base: 'inherit', '2xl': '1 1 0%' }}
         display={{ base: 'none', md: 'flex' }}

1516c76001e8fac1b3731e67fdfc9dafd94f0ff6
NeOMakinG <valentin.szczupak@prestashop.com>
2022-05-15 14:50:30 +0200

    fix: use chakra tab

---
+++ src/plugins/foxPage/components/FoxTab.stories.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable import/no-anonymous-default-export */
+/* eslint-disable import/no-default-export */
+
+import { SimpleGrid, TabList, Tabs } from '@chakra-ui/react'
+import { Story } from '@storybook/react'
+import { fox } from 'test/mocks/assets'
+
+import { FoxTab } from './FoxTab'
+import { Total } from './Total'
+
+export default {
+  title: 'Plugins/FoxPage/Tab',
+  component: FoxTab,
+}
+
+export const FoxPageTabs: Story = () => (
+  <Tabs variant='unstyled'>
+    <TabList>
+      <SimpleGrid
+        gridTemplateColumns={{ base: 'repeat(1, 1fr)', lg: 'repeat(4, 1fr)' }}
+        gridGap={4}
+        mb={4}
+        width='full'
+      >
+        <Total
+          fiatAmount={'6000'}
+          icons={[
+            'https://assets.coincap.io/assets/icons/fox@2x.png',
+            'https://raw.githubusercontent.com/shapeshift/lib/main/packages/asset-service/src/generateAssetData/ethTokens/icons/foxy-icon.png',
+            'https://rawcdn.githack.com/trustwallet/assets/master/blockchains/ethereum/assets/0x03352D267951E96c6F7235037C5DFD2AB1466232/logo.png',
+          ]}
+        />
+        <FoxTab
+          assetIcon={fox.icon}
+          assetSymbol={fox.symbol}
+          isActive={true}
+          cryptoAmount={'3000'}
+          fiatAmount={'1000'}
+        />
+        <FoxTab
+          assetIcon={fox.icon}
+          assetSymbol={fox.symbol}
+          cryptoAmount={'3000'}
+          fiatAmount={'1000'}
+        />
+        <FoxTab
+          assetIcon={fox.icon}
+          assetSymbol={fox.symbol}
+          cryptoAmount={'3000'}
+          fiatAmount={'1000'}
+        />
+      </SimpleGrid>
+    </TabList>
+  </Tabs>
+)
---
+++ src/plugins/foxPage/components/FoxTab.tsx
@@ -0,0 +1,49 @@
+import { Box } from '@chakra-ui/layout'
+import { Link, SkeletonText, Tab, useColorModeValue } from '@chakra-ui/react'
+import { Amount } from 'components/Amount/Amount'
+import { AssetIcon } from 'components/AssetIcon'
+import { Card } from 'components/Card/Card'
+
+type FoxTabProps = {
+  assetIcon: string
+  assetSymbol: string
+  fiatAmount: string
+  cryptoAmount: string
+  isSelected?: Boolean
+}
+
+export const FoxTab = ({ assetIcon, assetSymbol, fiatAmount, cryptoAmount }: FoxTabProps) => {
+  const bgHover = useColorModeValue('gray.100', 'gray.750')
+
+  return (
+    <Tab
+      _selected={{ bg: bgHover, borderColor: 'primary', borderWidth: '2px' }}
+      borderRadius='xl'
+      borderColor={bgHover}
+      borderWidth={'1px'}
+      bg={'none'}
+      _hover={{ textDecoration: 'none', bg: bgHover }}
+      textAlign='left'
+      p={0}
+    >
+      <Card display='block' bg='none' border='none' boxShadow='none' p={0} width='full'>
+        <Card.Body p={4}>
+          <Box mb={6}>
+            <AssetIcon src={assetIcon} boxSize='8' zIndex={2} />
+          </Box>
+          <SkeletonText isLoaded={true} noOfLines={2}>
+            <Amount.Crypto
+              color='inherit'
+              value={cryptoAmount}
+              symbol={assetSymbol}
+              lineHeight={1.2}
+              fontSize={'2xl'}
+              fontWeight='semibold'
+            />
+            <Amount.Fiat color='gray.500' value={fiatAmount} lineHeight={1.2} />
+          </SkeletonText>
+        </Card.Body>
+      </Card>
+    </Tab>
+  )
+}

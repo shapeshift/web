4a2dfc35e415fb7218780409c20b7f5fbd13dd3b
NeOMakinG <valentin.szczupak@prestashop.com>
2022-05-17 21:29:49 +0200

    fix: avoid displaying asset description collapse button if the description is small (#1811)
    * fix: avoid displaying description collapse button if the description is small
    
    * fix: trigger condition every time description change

--- src/components/AssetHeader/AssetDescription.tsx
+++ src/components/AssetHeader/AssetDescription.tsx
@@ -1,6 +1,6 @@
-import { Button, Collapse, Skeleton, SkeletonText } from '@chakra-ui/react'
+import { Box, Button, Collapse, Skeleton, SkeletonText } from '@chakra-ui/react'
 import { AssetId } from '@shapeshiftoss/caip'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useTranslate } from 'react-polyglot'
 import { Card } from 'components/Card/Card'
 import { ParsedHtml } from 'components/ParsedHtml/ParsedHtml'
@@ -14,15 +14,29 @@ type AssetDescriptionProps = {
   assetId: AssetId
 }
 
+const DESCRIPTION_DEFAULT_HEIGHT = 75
+
 export const AssetDescription = ({ assetId }: AssetDescriptionProps) => {
   const translate = useTranslate()
   const [showDescription, setShowDescription] = useState(false)
+  const [shouldDisplayToggleButton, setShouldDisplayToggleButton] = useState(true)
+  const descriptionEl = useRef<HTMLDivElement | null>(null)
   const handleToggle = () => setShowDescription(!showDescription)
   const asset = useAppSelector(state => selectAssetById(state, assetId))
   const { name, description, isTrustedDescription } = asset || {}
   const query = useGetAssetDescriptionQuery(assetId)
   const isLoaded = !query.isLoading
+
+  // If the height of the description is higher than the collapse element when it's closed
+  // we should display the Show More button
+  useEffect(() => {
+    setShouldDisplayToggleButton(
+      (descriptionEl?.current?.clientHeight ?? 0) > DESCRIPTION_DEFAULT_HEIGHT,
+    )
+  }, [descriptionEl?.current?.clientHeight, description])
+
   if (!description || !isLoaded) return null
+
   return (
     <Card>
       <Card.Footer>
@@ -31,21 +45,25 @@ export const AssetDescription = ({ assetId }: AssetDescriptionProps) => {
             {translate('assets.assetDetails.assetHeader.aboutAsset', { asset: name })}
           </Card.Heading>
         </Skeleton>
-        <Collapse startingHeight={70} in={showDescription}>
-          <SkeletonText isLoaded={isLoaded} noOfLines={4} spacing={2} skeletonHeight='20px'>
-            {isTrustedDescription && (
-              <ParsedHtml color='gray.500' innerHtml={markdownLinkToHTML(description)} />
-            )}
-            {!isTrustedDescription && (
-              <SanitizedHtml color='gray.500' dirtyHtml={markdownLinkToHTML(description)} />
-            )}
-          </SkeletonText>
+        <Collapse startingHeight={DESCRIPTION_DEFAULT_HEIGHT} in={showDescription}>
+          <Box ref={descriptionEl}>
+            <SkeletonText isLoaded={isLoaded} noOfLines={4} spacing={2} skeletonHeight='20px'>
+              {isTrustedDescription && (
+                <ParsedHtml color='gray.500' innerHtml={markdownLinkToHTML(description)} />
+              )}
+              {!isTrustedDescription && (
+                <SanitizedHtml color='gray.500' dirtyHtml={markdownLinkToHTML(description)} />
+              )}
+            </SkeletonText>
+          </Box>
         </Collapse>
-        <Button size='sm' onClick={handleToggle} mt='1rem'>
-          {showDescription
-            ? translate('assets.assetDetails.assetDescription.showLess')
-            : translate('assets.assetDetails.assetDescription.showMore')}
-        </Button>
+        {shouldDisplayToggleButton && (
+          <Button size='sm' onClick={handleToggle} mt='1rem'>
+            {showDescription
+              ? translate('assets.assetDetails.assetDescription.showLess')
+              : translate('assets.assetDetails.assetDescription.showMore')}
+          </Button>
+        )}
       </Card.Footer>
     </Card>
   )

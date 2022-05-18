e25821aaaabe0d66e5c1097a760d913d2adeb4fc
LPX <info@shapeshift.one>
2022-05-19 06:54:35 +0900

    typescriptwoes

---
+++ src/assets/translations/en/index.ts
@@ -0,0 +1,13 @@
+import * as path from 'path'
+let context = require.context('./', false)
+let files: any = {}
+
+context.keys().forEach((filename: string) => {
+  if (filename.endsWith('.json')) {
+    const locale: string = path.basename(filename, '.json')
+    const fileContext: string = context(filename)
+    files[locale] = fileContext
+  }
+})
+
+export { files as en }

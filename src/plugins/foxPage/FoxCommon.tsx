3a2a75f5a47eab4c7e6ad06228e2d089cacf477f
NeOMakinG <valentin.szczupak@prestashop.com>
2022-05-23 17:25:55 +0200

    fix: refactor chunks

--- src/plugins/foxPage/FoxCommon.tsx
+++ src/plugins/foxPage/FoxCommon.tsx
@@ -1,75 +1,79 @@
 export enum OpportunityTypes {
-  LiquidityPool = 'liquidityPool',
+  LiquidityPool = 'liquidityPools',
   Farming = 'farming',
   BorrowingAndLending = 'borrowingAndLending',
 }
 
 export type ExternalOpportunity = {
-  type: OpportunityTypes
   title: string
   apy: string
   link: string
   icons: string[]
 }
 
-export const OpportunityTypeTitles = {
-  [OpportunityTypes.LiquidityPool]: 'plugins.foxPage.liquidityPools',
-  [OpportunityTypes.Farming]: 'plugins.foxPage.farming',
-  [OpportunityTypes.BorrowingAndLending]: 'plugins.foxPage.borrowingAndLending',
+export type OpportunitiesChunk = {
+  type: OpportunityTypes
+  title: string
+  opportunities: ExternalOpportunity[]
 }
 
-export type FoxOpportunities = [ExternalOpportunity[], ExternalOpportunity[], ExternalOpportunity[]]
-
 // @TODO: When wiring this up, move this to foxPage.ts as icons and APY should be fetched from dynamic datas
-export const otherOpportunitiesChunks: FoxOpportunities = [
-  [
-    {
-      type: OpportunityTypes.LiquidityPool,
-      title: 'Uniswap',
-      apy: '0.1161',
-      link: '#',
-      icons: ['https://assets.coincap.io/assets/icons/fox@2x.png'],
-    },
-    {
-      type: OpportunityTypes.LiquidityPool,
-      title: 'SushiSwap',
-      apy: '0.1161',
-      link: '#',
-      icons: ['https://assets.coincap.io/assets/icons/fox@2x.png'],
-    },
-  ],
-  [
-    {
-      type: OpportunityTypes.Farming,
-      title: 'Fox Farm',
-      apy: '0.1161',
-      link: '#',
-      icons: [
-        'https://assets.coincap.io/assets/icons/fox@2x.png',
-        'https://assets.coincap.io/assets/icons/fox@2x.png',
-      ],
-    },
-  ],
-  [
-    {
-      type: OpportunityTypes.BorrowingAndLending,
-      title: 'ETH-FOX',
-      apy: '0.1161',
-      link: '#',
-      icons: [
-        'https://assets.coincap.io/assets/icons/fox@2x.png',
-        'https://assets.coincap.io/assets/icons/fox@2x.png',
-      ],
-    },
-    {
-      type: OpportunityTypes.BorrowingAndLending,
-      title: 'Rari Pool 79',
-      apy: '0.1161',
-      link: '#',
-      icons: [
-        'https://assets.coincap.io/assets/icons/fox@2x.png',
-        'https://assets.coincap.io/assets/icons/fox@2x.png',
-      ],
-    },
-  ],
+export const otherOpportunitiesChunks: OpportunitiesChunk[] = [
+  {
+    type: OpportunityTypes.LiquidityPool,
+    title: 'plugins.foxPage.liquidityPools',
+    opportunities: [
+      {
+        title: 'Uniswap',
+        apy: '0.1161',
+        link: '#',
+        icons: ['https://assets.coincap.io/assets/icons/fox@2x.png'],
+      },
+      {
+        title: 'SushiSwap',
+        apy: '0.1161',
+        link: '#',
+        icons: ['https://assets.coincap.io/assets/icons/fox@2x.png'],
+      },
+    ],
+  },
+  {
+    type: OpportunityTypes.Farming,
+    title: 'plugins.foxPage.farming',
+    opportunities: [
+      {
+        title: 'Fox Farm',
+        apy: '0.1161',
+        link: '#',
+        icons: [
+          'https://assets.coincap.io/assets/icons/fox@2x.png',
+          'https://assets.coincap.io/assets/icons/fox@2x.png',
+        ],
+      },
+    ],
+  },
+  {
+    type: OpportunityTypes.BorrowingAndLending,
+    title: 'plugins.foxPage.borrowingAndLending',
+    opportunities: [
+      {
+        title: 'ETH-FOX',
+        apy: '0.1161',
+        link: '#',
+        icons: [
+          'https://assets.coincap.io/assets/icons/fox@2x.png',
+          'https://assets.coincap.io/assets/icons/fox@2x.png',
+        ],
+      },
+      {
+        title: 'Rari Pool 79',
+        apy: '0.1161',
+        link: '#',
+        icons: [
+          'https://assets.coincap.io/assets/icons/fox@2x.png',
+          'https://assets.coincap.io/assets/icons/fox@2x.png',
+        ],
+      },
+    ],
+  },
 ]
--- src/plugins/foxPage/components/OtherOpportunities/OtherOpportunities.tsx
+++ src/plugins/foxPage/components/OtherOpportunities/OtherOpportunities.tsx
@@ -4,7 +4,7 @@ import { useTranslate } from 'react-polyglot'
 import { Card } from 'components/Card/Card'
 import { Text } from 'components/Text/Text'
 
-import { OpportunityTypeTitles, otherOpportunitiesChunks } from '../../FoxCommon'
+import { otherOpportunitiesChunks } from '../../FoxCommon'
 import { FoxOtherOpportunityPanel } from './FoxOtherOpportunityPanel'
 
 type OtherOpportunitiesProps = {
@@ -26,13 +26,14 @@ export const OtherOpportunities: React.FC<OtherOpportunitiesProps> = ({ descript
       </Card.Header>
       <Card.Body pb={0}>
         <Accordion mx={-6} defaultIndex={[0]} allowToggle allowMultiple>
-          {otherOpportunitiesChunks.map(opportunities => {
+          {otherOpportunitiesChunks.map(opportunitiesChunk => {
+            const { opportunities } = opportunitiesChunk
             if (!opportunities.length) return null
 
             return (
               <FoxOtherOpportunityPanel
                 opportunities={opportunities}
-                title={OpportunityTypeTitles[opportunities[0].type]}
+                title={opportunitiesChunk.title}
               />
             )
           })}

5f2cb3b04c0e1488b6c7e176b4eef7b5fa1f0811
GMSteuart <gmsteuart@gmail.com>
2022-05-24 23:28:38 -0500

    fix: headers for macos/ios safari

--- headers.js
+++ headers.js
@@ -6,6 +6,9 @@ const cspMeta = Object.entries({
   'connect-src': [
     "'self'",
     'data:',
+    // Fixes local (dev) websocket requests in Safari on macOS/iOS
+    ...(process.env.NODE_ENV === 'development' ? ['ws://localhost:3000/ws',
+    `ws://${process.env.REACT_APP_LOCAL_IP}:3000/ws`] : []),
     // @shapeshiftoss/swapper@1.15.0: https://github.com/shapeshift/lib/blob/f833ac7f8c70dee801eaa24525336ca6992e5903/packages/swapper/src/swappers/zrx/utils/zrxService.ts#L4
     'https://api.0x.org',
     // @shapeshiftoss/chain-adapters@1.22.1: https://github.com/shapeshift/lib/blob/476550629be9485bfc089decc4df85456968464a/packages/chain-adapters/src/ethereum/EthereumChainAdapter.ts#L226

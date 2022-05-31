ff032a853cd2310cb7552c7c6c2a3fa41ccb94b7
Nenba Jonathan <jonathan.nenba@propellerhead.com>
2022-05-31 15:23:26 +0100

    chore: fix the unit test

--- src/components/AssetSearch/helpers/enrichAsset/enrichAsset.test.ts
+++ src/components/AssetSearch/helpers/enrichAsset/enrichAsset.test.ts
@@ -50,17 +50,6 @@ const assets: Asset[] = [
 
 const rowData: AccountRowData[] = [
   {
-    name: 'Dai',
-    icon: 'https://assets.coingecko.com/coins/images/9956/thumb/dai-multi-collateral-mcd.png?1574218774',
-    symbol: 'DAI',
-    fiatAmount: '29',
-    cryptoAmount: '9.00022389',
-    assetId: 'eip155:1/erc20:0x6b175474e89094c44da98b954eedeac495271d0f',
-    allocation: 2939,
-    price: '1',
-    priceChange: 2,
-  },
-  {
     assetId: 'eip155:1/erc20:0x79be75ffc64dd58e66787e4eae470c8a1fd08ba4',
     name: 'Aave AMM DAI',
     icon: 'https://assets.coingecko.com/coins/images/17197/thumb/aAMMDAI_2x.png?1626940032',
@@ -71,17 +60,28 @@ const rowData: AccountRowData[] = [
     fiatAmount: '10',
     cryptoAmount: '9.00022389',
   },
+  {
+    name: 'Dai',
+    icon: 'https://assets.coingecko.com/coins/images/9956/thumb/dai-multi-collateral-mcd.png?1574218774',
+    symbol: 'DAI',
+    fiatAmount: '29',
+    cryptoAmount: '9.00022389',
+    assetId: 'eip155:1/erc20:0x6b175474e89094c44da98b954eedeac495271d0f',
+    allocation: 2939,
+    price: '1',
+    priceChange: 2,
+  },
 ]
 
 const marketData: { [x: string]: MarketData | undefined } = {
-  'eip155:1/erc20:0x6b175474e89094c44da98b954eedeac495271d0f': {
-    marketCap: '39939393',
+  'eip155:1/erc20:0x79be75ffc64dd58e66787e4eae470c8a1fd08ba4': {
+    marketCap: '20392093290',
     price: '1',
     volume: '',
     changePercent24Hr: 3,
   },
-  'eip155:1/erc20:0x79be75ffc64dd58e66787e4eae470c8a1fd08ba4': {
-    marketCap: '20392093290',
+  'eip155:1/erc20:0x6b175474e89094c44da98b954eedeac495271d0f': {
+    marketCap: '39939393',
     price: '1',
     volume: '',
     changePercent24Hr: 3,
@@ -93,22 +93,20 @@ describe('enrichAsset', () => {
     const returnedAssets = enrichAsset(assets, rowData, marketData)
     // check that all the element in the array have a valid cryptoAmount and marketCap property
 
-    const firstAsset = returnedAssets[0] as any
-    const secondAsset = returnedAssets[1] as any
-    expect(Number(firstAsset.cryptoAmount)).toEqual(Number(rowData[1].cryptoAmount))
+    const firstAsset = returnedAssets[assets[0].assetId] as any
+    const secondAsset = returnedAssets[assets[1].assetId] as any
+    expect(Number(firstAsset.cryptoAmount)).toEqual(Number(rowData[0].cryptoAmount))
 
-    expect(Number(secondAsset.cryptoAmount)).toEqual(Number(rowData[0].cryptoAmount))
+    expect(Number(secondAsset.cryptoAmount)).toEqual(Number(rowData[1].cryptoAmount))
   })
 
   it('check that all the element in the array have a valid marketCap property', () => {
     const returnedAssets = enrichAsset(assets, rowData, marketData)
 
-    const firstAsset = returnedAssets[0] as any
-    const secondAsset = returnedAssets[1] as any
-    expect(Number(firstAsset.marketCap)).toEqual(Number(marketData[firstAsset.assetId]?.marketCap))
+    const firstAsset = returnedAssets[assets[0].assetId]
+    const secondAsset = returnedAssets[assets[1].assetId]
+    expect(Number(firstAsset.marketCap)).toEqual(Number(marketData[assets[0].assetId]?.marketCap))
 
-    expect(Number(secondAsset.marketCap)).toEqual(
-      Number(marketData[secondAsset.assetId]?.marketCap),
-    )
+    expect(Number(secondAsset.marketCap)).toEqual(Number(marketData[assets[1].assetId]?.marketCap))
   })
 })

15b10f9487b9255fbe69e35c1f700800056283a0
Nenba Jonathan <jonathan.nenba@propellerhead.com>
2022-05-31 15:51:08 +0100

    chore: update the function and remove the unused.

--- src/components/AssetSearch/AssetList.tsx
+++ src/components/AssetSearch/AssetList.tsx
@@ -1,17 +1,18 @@
 import { ListProps } from '@chakra-ui/react'
-import { Asset } from '@shapeshiftoss/types'
+import { Asset, MarketData } from '@shapeshiftoss/types'
+import sortBy from 'lodash/sortBy'
 import { useEffect, useMemo } from 'react'
 import { useRouteMatch } from 'react-router-dom'
 import AutoSizer from 'react-virtualized-auto-sizer'
 import { FixedSizeList } from 'react-window'
 import { Text } from 'components/Text'
 import { useRefCallback } from 'hooks/useRefCallback/useRefCallback'
+import { bnOrZero } from 'lib/bignumber/bignumber'
+import { AccountRowData } from 'state/slices/portfolioSlice/selectors'
 import { selectMarketData, selectPortfolioAccountRows } from 'state/slices/selectors'
 import { useAppSelector } from 'state/store'
 
 import { AssetRow } from './AssetRow'
-import { enrichAsset } from './helpers/enrichAsset/enrichAsset'
-import { sortAssetsByAccountAndMarketCap } from './helpers/sortAssetsByAccountAndMarketCap/sortAssetsByAccountAndMarketCap'
 
 type AssetListProps = {
   handleClick: (asset: Asset) => void
@@ -23,17 +24,69 @@ type ItemData<T> = {
   handleClick: T
 }
 
+const sortAssetsByAccountAndMarketCap = (
+  assets: Asset[],
+  enrichedData: Record<string, { fiatAmount: string; cryptoAmount: string; marketCap: string }>,
+): Asset[] => {
+  return sortBy(assets, [
+    asset => {
+      return Number(enrichedData[asset.assetId].fiatAmount)
+    },
+    asset => {
+      return Number(enrichedData[asset.assetId].marketCap)
+    },
+  ]).reverse()
+}
+
+/**
+ * This function create a data object(cryptoAmount, fiatAmount, marketCap) from the Asset, portfolioAcountRows and marketData for each asset for facilitate sorting.
+ * @param assets
+ * @param portfolioAcountRows
+ * @param marketData
+ * @returns Record<string, { fiatAmount: string; cryptoAmount: string; marketCap: string }>
+ */
+export const createEnrichData = (
+  assets: Asset[],
+  portfolioAcountRows: AccountRowData[],
+  marketData: {
+    [x: string]: MarketData | undefined
+  },
+): Record<string, { fiatAmount: string; cryptoAmount: string; marketCap: string }> => {
+  const result = {} as Record<
+    string,
+    { fiatAmount: string; cryptoAmount: string; marketCap: string }
+  >
+  assets.forEach(asset => {
+    const fiatAmount = portfolioAcountRows.find(
+      portfolioAccountRow => portfolioAccountRow.assetId === asset.assetId,
+    )?.fiatAmount
+
+    const cryptoAmount = portfolioAcountRows.find(
+      portfolioAccountRow => portfolioAccountRow.assetId === asset.assetId,
+    )?.cryptoAmount
+
+    const assetMarketData = marketData[asset.assetId]
+
+    result[asset.assetId] = {
+      fiatAmount: bnOrZero(fiatAmount).toString(),
+      cryptoAmount: bnOrZero(cryptoAmount).toString(),
+      marketCap: bnOrZero(assetMarketData?.marketCap).toString(),
+    }
+  })
+  return result
+}
+
 export const AssetList = ({ assets, handleClick }: AssetListProps) => {
   const portfolioAcountRows = useAppSelector(state => selectPortfolioAccountRows(state))
   const marketData = useAppSelector(state => selectMarketData(state))
-  const enrichedAssetData = useMemo(
-    () => enrichAsset(assets, portfolioAcountRows, marketData),
+  const enrichedData = useMemo(
+    () => createEnrichData(assets, portfolioAcountRows, marketData),
     [assets, marketData, portfolioAcountRows],
   )
 
   const sortedAssets = useMemo(
-    () => sortAssetsByAccountAndMarketCap(assets, enrichedAssetData),
-    [assets, enrichedAssetData],
+    () => sortAssetsByAccountAndMarketCap(assets, enrichedData),
+    [assets, enrichedData],
   )
 
   type HandleClick = ReturnType<typeof handleClick>

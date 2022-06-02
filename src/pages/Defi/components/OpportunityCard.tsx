028c6ac71978132eb0531631368963a031e475c5
Nenba Jonathan <jonathan.nenba@propellerhead.com>
2022-06-02 16:24:18 +0100

    chore: udpate the foxAssetId

--- src/pages/Defi/components/OpportunityCard.tsx
+++ src/pages/Defi/components/OpportunityCard.tsx
@@ -30,7 +30,7 @@ type OpportunityCardProps = {
   isLoaded?: boolean
 } & EarnOpportunityType
 
-export const FoxyAssetId = 'eip155:1/erc20:0xdc49108ce5c57bc3408c3a5e95f3d864ec386ed3'
+export const FoxAssetId = 'eip155:1/erc20:0xc770eefad204b5180df6a14ee197d99d808ee52d'
 export const FoxyIconUrl =
   'https://raw.githubusercontent.com/shapeshift/lib/main/packages/asset-service/src/generateAssetData/ethTokens/icons/foxy-icon.png'
 

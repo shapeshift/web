e55b1391765aa82442fc7b84fcde7ab485cfa7ff
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2022-06-03 17:57:23 -0600

    feat: override icons for defi opportunities

--- src/pages/Defi/components/OpportunityCardList.tsx
+++ src/pages/Defi/components/OpportunityCardList.tsx
@@ -7,26 +7,12 @@ import { Card } from 'components/Card/Card'
 import { IconCircle } from 'components/IconCircle'
 import { Text } from 'components/Text'
 import { bnOrZero } from 'lib/bignumber/bignumber'
-import { selectAssets } from 'state/slices/selectors'
-import { useAppSelector } from 'state/store'
 
 import { UseEarnBalancesReturn } from '../hooks/useEarnBalances'
 import { OpportunityCard } from './OpportunityCard'
 
-export const FoxAssetId = 'eip155:1/erc20:0xc770eefad204b5180df6a14ee197d99d808ee52d'
-export const FoxyAssetId = 'eip155:1/erc20:0xdc49108ce5c57bc3408c3a5e95f3d864ec386ed3'
-
 export const OpportunityCardList = ({ balances }: { balances: UseEarnBalancesReturn }) => {
   const activeOpportunities = balances.opportunities.filter(o => bnOrZero(o.cryptoAmount).gt(0))
-  const assets = useAppSelector(selectAssets)
-
-  const selectProperOpportunityIcon = (assetId: string): string => {
-    const overrideAssetIds: { [index: string]: string } = {
-      [FoxAssetId]: FoxyAssetId,
-    }
-    const overrideAssetId = overrideAssetIds[assetId] ?? assetId
-    return assets[overrideAssetId].icon
-  }
 
   return (
     <Box mb={6}>
@@ -54,13 +40,7 @@ export const OpportunityCardList = ({ balances }: { balances: UseEarnBalancesRet
         gridGap={6}
       >
         {activeOpportunities.map(opportunity => {
-          return (
-            <OpportunityCard
-              key={opportunity.assetId}
-              selectProperIcon={selectProperOpportunityIcon}
-              {...opportunity}
-            />
-          )
+          return <OpportunityCard key={opportunity.assetId} {...opportunity} />
         })}
       </SimpleGrid>
       {activeOpportunities.length === 0 && (

0cdf07feb7aba4633c85878f4951d715c590dc1f
pastaghost <pastaghost@2520.io>
2022-06-03 15:35:20 -0600

    fix: add mobileEnabled field to WalletConnect config

--- src/context/WalletProvider/WalletConnect/config.ts
+++ src/context/WalletProvider/WalletConnect/config.ts
@@ -3,6 +3,7 @@ import { WalletConnectIcon } from 'components/Icons/WalletConnectIcon'
 
 export const WalletConnectConfig = {
   adapter: WalletConnectAdapter,
+  mobileEnabled: true,
   icon: WalletConnectIcon,
   name: 'WalletConnect',
 }

1ad7b195ea085f5ca01155dbc0823d7940e489e7
pastaghost <pastaghost@2520.io>
2022-06-03 15:33:23 -0600

    fix: add walletconnect csp entries

---
+++ headers/csps/wallets/walletconnect.ts
@@ -0,0 +1,8 @@
+import type { Csp } from '../../types'
+
+export const csp: Csp = {
+  'connect-src': [
+    'wss://*.bridge.walletconnect.org/',
+    'https://registry.walletconnect.com/api/v2/wallets',
+  ],
+}

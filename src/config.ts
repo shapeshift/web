32673a8247d8a4abea1602203c14764291a18271
Grant Steuart <gmsteuart@gmail.com>
2022-06-03 16:57:28 -0500

    feat: keplr wallet (#1707)
    * feat: init keplr wallet
    
    * feat: add KeplrIcon svg
    
    * feat: keplr connect option
    
    * fix: add listener for keplr_keystorechange event
    
    * chore: update hdwallet dependencies
    
    * fix: minor fixes
    
    * chore: run linter
    
    * fix: apply code review suggestion
    
    * chore: update lockfile
    
    * fix: fix keplr window object error
    
    * chore: run linter
    
    * fix: utilize feature flag
    
    * fix: move keplr window interface to external types file
    
    Co-authored-by: pastaghost <pastaghost@2520.io>
    Co-authored-by: pastaghost <62026038+pastaghost@users.noreply.github.com>
    Co-authored-by: 0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>

--- src/config.ts
+++ src/config.ts
@@ -41,6 +41,7 @@ const validators = {
   REACT_APP_FEATURE_TALLYHO_WALLET: bool({ default: false }),
   REACT_APP_FEATURE_BANXA_RAMP: bool({ default: false }),
   REACT_APP_FEATURE_FOX_PAGE: bool({ default: false }),
+  REACT_APP_FEATURE_KEPLR_WALLET: bool({ default: false }),
   REACT_APP_TOKEMAK_STATS_URL: url({ default: 'https://stats.tokemaklabs.com/' }),
   REACT_APP_COINGECKO_API_KEY: str({ default: '' }), // not required, we can fall back to the free tier
 }

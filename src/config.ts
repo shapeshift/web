c44086c75a38ac70cf7e3c0da313c965744a9588
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2022-06-04 09:08:29 -0600

    chore: add alchemy API key to sample and config

--- src/config.ts
+++ src/config.ts
@@ -44,6 +44,7 @@ const validators = {
   REACT_APP_FEATURE_KEPLR_WALLET: bool({ default: false }),
   REACT_APP_TOKEMAK_STATS_URL: url({ default: 'https://stats.tokemaklabs.com/' }),
   REACT_APP_COINGECKO_API_KEY: str({ default: '' }), // not required, we can fall back to the free tier
+  REACT_APP_ALCHEMY_API_KEY: str(),
 }
 
 function reporter<T>({ errors }: envalid.ReporterOptions<T>) {

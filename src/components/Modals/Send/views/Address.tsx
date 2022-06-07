8f1ea3753a08bf86d3751161785e7163033cb555
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2022-06-06 23:05:52 -0600

    fix: need address to continue

--- src/components/Modals/Send/views/Address.tsx
+++ src/components/Modals/Send/views/Address.tsx
@@ -77,13 +77,13 @@ export const Address = () => {
                   setIsValidatingInput(true)
                   // this does not throw, everything inside is handled
                   const { address, vanityAddress } = await parseAddressInput({ chainId, value })
-                  // console.info('value', value)
-                  // console.info('address', address)
-                  // console.info('vanityAddress', vanityAddress)
+                  console.info('value', value)
+                  console.info('address', address)
+                  console.info('vanityAddress', vanityAddress)
                   setIsValidatingInput(false)
                   address && setValue(SendFormFields.Address, address)
                   vanityAddress && setValue(SendFormFields.VanityDomain, vanityAddress)
-                  return address || vanityAddress ? true : 'common.invalidAddress'
+                  return address ? true : 'common.invalidAddress'
                 },
               },
             }}

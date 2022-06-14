534fd9de7ac9fec0c87d5584a92d74b460c21d3f
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2022-06-14 14:39:04 +1000

    fix: asset search active highlighting

--- src/components/AssetSearch/AssetRow.tsx
+++ src/components/AssetSearch/AssetRow.tsx
@@ -1,31 +1,25 @@
 import { Box, Button, Text, useColorModeValue } from '@chakra-ui/react'
-import { fromAssetId } from '@shapeshiftoss/caip'
 import { Asset } from '@shapeshiftoss/types'
-import { useRouteMatch } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { ListChildComponentProps } from 'react-window'
 import { AssetIcon } from 'components/AssetIcon'
+import { MatchParams } from 'pages/Accounts/Account'
 
 export const AssetRow: React.FC<ListChildComponentProps> = ({ data, index, style }) => {
-  const match = useRouteMatch<{ address: string; network: string }>()
+  const { assetId: searchedAssetId } = useParams<MatchParams>()
+  const parsedAssetId = searchedAssetId ? decodeURIComponent(searchedAssetId) : undefined
   const color = useColorModeValue('gray.500', 'whiteAlpha.500')
   const asset: Asset = data.items[index]
   if (!asset) return null
 
   const { handleClick } = data
-  const { assetReference } = fromAssetId(asset.assetId)
-
-  const active =
-    !match?.params?.address && !assetReference
-      ? match?.params?.network === asset?.chain
-      : match?.params?.address === assetReference
-
-  if (!asset) return null
+  const isActive = asset.assetId === parsedAssetId
 
   return (
     <Button
       variant='ghost'
       onClick={() => handleClick(asset)}
-      isActive={active}
+      isActive={isActive}
       justifyContent='flex-start'
       style={style}
       _focus={{

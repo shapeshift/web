6dd5269ffdc1bc8c3c351f061ad08a0c97129dce
Sean Luther <elmutt@gmail.com>
2022-06-16 16:35:23 -0600

    fix env

--- react-app-rewired/headers/csps/defi/swappers/Thor.ts
+++ react-app-rewired/headers/csps/defi/swappers/Thor.ts
@@ -2,5 +2,5 @@ import type { Csp } from '../../../types'
 
 export const csp: Csp = {
   // removes `/v2` from midgard url
-  'connect-src': [process.env.REACT_APP_MIDGARD_URL!.slice(0, -3)],
+  'connect-src': [String(process.env.REACT_APP_MIDGARD_URL).slice(0, -3)],
 }

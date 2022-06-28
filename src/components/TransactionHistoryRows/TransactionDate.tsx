61f8197cd7a15764975a7e2b25eeaae0e22a8b52
NeOMakinG <valentin.szczupak@prestashop.com>
2022-06-28 20:54:52 +0200

    fix: add ru, id and ch translations and translate dates

--- src/components/TransactionHistoryRows/TransactionDate.tsx
+++ src/components/TransactionHistoryRows/TransactionDate.tsx
@@ -1,15 +1,31 @@
 import dayjs from 'dayjs'
 import { RawText } from 'components/Text'
+import { selectSelectedLocale } from 'state/slices/selectors'
+import { useAppSelector } from 'state/store'
 
-export const TransactionDate = ({ blockTime }: { blockTime: number }) => (
-  <RawText
-    fontWeight='bold'
-    fontSize='sm'
-    color='gray.700'
-    lineHeight='taller'
-    whiteSpace='nowrap'
-    px={4}
-  >
-    {dayjs(blockTime * 1000).format('MMMM DD, YYYY')}
-  </RawText>
-)
+export const TransactionDate = ({ blockTime }: { blockTime: number }) => {
+  const selectedLocale = useAppSelector(selectSelectedLocale)
+
+  const transactionDate = () => {
+    require(`dayjs/locale/${selectedLocale}.js`)
+    const localizedFormat = require('dayjs/plugin/localizedFormat')
+    dayjs.extend(localizedFormat)
+
+    return dayjs(blockTime * 1000)
+      .locale(selectedLocale)
+      .format('ll')
+  }
+
+  return (
+    <RawText
+      fontWeight='bold'
+      fontSize='sm'
+      color='gray.700'
+      lineHeight='taller'
+      whiteSpace='nowrap'
+      px={4}
+    >
+      {transactionDate()}
+    </RawText>
+  )
+}

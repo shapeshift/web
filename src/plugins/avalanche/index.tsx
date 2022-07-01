2196941c84210ddc83585d78f937d3acd8b4881e
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2022-07-01 15:12:25 +1000

    feat: add avalanche plugin wip

---
+++ src/plugins/avalanche/index.tsx
@@ -0,0 +1,41 @@
+import { ChainId } from '@shapeshiftoss/caip'
+import { avalanche, ChainAdapter } from '@shapeshiftoss/chain-adapters'
+import { KnownChainIds } from '@shapeshiftoss/types'
+import * as unchained from '@shapeshiftoss/unchained-client'
+import { getConfig } from 'config'
+import { Plugins } from 'plugins'
+
+export function register(): Plugins {
+  return [
+    [
+      'avalancheChainAdapter',
+      {
+        name: 'avalancheChainAdapter',
+        featureFlag: 'Avalanche',
+        providers: {
+          chainAdapters: [
+            [
+              KnownChainIds.EthereumMainnet,
+              () => {
+                const http = new unchained.ethereum.V1Api(
+                  new unchained.ethereum.Configuration({
+                    basePath: getConfig().REACT_APP_UNCHAINED_AVALANCHE_HTTP_URL,
+                  }),
+                )
+
+                const ws = new unchained.ws.Client<unchained.ethereum.EthereumTx>(
+                  getConfig().REACT_APP_UNCHAINED_AVALANCHE_WS_URL,
+                )
+
+                return new avalanche.ChainAdapter({
+                  providers: { http, ws },
+                  rpcUrl: getConfig().REACT_APP_AVALANCHE_NODE_URL,
+                }) as unknown as ChainAdapter<ChainId> // FIXME: this is silly
+              },
+            ],
+          ],
+        },
+      },
+    ],
+  ]
+}

c13dd2377a429c61143f302b5b9b5991ef4b6b78
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-07-02 15:00:23 +0200

    feat: lighter refactor

--- src/plugins/foxPage/hooks/useFarmingApr.ts
+++ src/plugins/foxPage/hooks/useFarmingApr.ts
@@ -1,76 +1,18 @@
-import { Contract } from '@ethersproject/contracts'
-import { Fetcher, Token } from '@uniswap/sdk'
-import IUniswapV2Pair from '@uniswap/v2-core/build/IUniswapV2Pair.json'
-import { useEffect, useMemo, useState } from 'react'
-import { bnOrZero } from 'lib/bignumber/bignumber'
+import { useEffect, useState } from 'react'
 
-import {
-  FOX_TOKEN_CONTRACT_ADDRESS,
-  UNISWAP_V2_WETH_FOX_FARMING_REWARDS_ADDRESS,
-  UNISWAP_V2_WETH_FOX_POOL_ADDRESS,
-  WETH_TOKEN_CONTRACT_ADDRESS,
-} from '../const'
-import farmingAbi from '../farmingAbi.json'
-import { getEthersProvider, rewardRatePerToken } from '../utils'
-import { useCurrentBlockNumber } from './useCurrentBlockNumber'
-
-const ethersProvider = getEthersProvider()
+import { getFarmingApr } from '../utils/getFarmingApr'
 
 export const useFarmingApr = () => {
   const [farmingApr, setFarmingApr] = useState<string | null>(null)
   const [loaded, setLoaded] = useState(false)
-  const blockNumber = useCurrentBlockNumber()
-
-  const liquidityContractAddress = UNISWAP_V2_WETH_FOX_POOL_ADDRESS
-  const uniswapLPContract = useMemo(
-    () => new Contract(liquidityContractAddress, IUniswapV2Pair.abi, ethersProvider),
-    [liquidityContractAddress],
-  )
-  const farmingRewardsContract = useMemo(
-    () => new Contract(UNISWAP_V2_WETH_FOX_FARMING_REWARDS_ADDRESS, farmingAbi, ethersProvider),
-    [],
-  )
 
   useEffect(() => {
-    if (
-      !ethersProvider ||
-      !Fetcher ||
-      !blockNumber ||
-      !uniswapLPContract ||
-      !farmingRewardsContract
-    )
-      return
     ;(async () => {
-      const foxRewardRatePerToken = await rewardRatePerToken(farmingRewardsContract)
-      const pair = await Fetcher.fetchPairData(
-        new Token(0, WETH_TOKEN_CONTRACT_ADDRESS, 18),
-        new Token(0, FOX_TOKEN_CONTRACT_ADDRESS, 18),
-        ethersProvider,
-      )
-
-      const totalSupply = await uniswapLPContract.totalSupply()
-
-      const token1PoolReservesEquivalent = bnOrZero(pair.reserve1.toFixed())
-        .times(2) // Double to get equivalent of both sides of pool
-        .times(`1e+${pair.token1.decimals}`) // convert to base unit value
-
-      const foxEquivalentPerLPToken = token1PoolReservesEquivalent
-        .div(bnOrZero(totalSupply.toString()))
-        .times(`1e+${pair.token1.decimals}`) // convert to base unit value
-
-      const apr = bnOrZero(foxRewardRatePerToken) // Fox Rewards per second for 1 staked LP token
-        .div(foxEquivalentPerLPToken) // Equivalent FOX value for 1 LP token
-        .times(100) // Decimal to percentage
-        .times(3600) // 3600 seconds per hour
-        .times(24) // 24 hours per day
-        .times(365.25) // 365.25 days per year
-        .decimalPlaces(4) // Arbitrary decimal cutoff
-        .toString()
-
-      setFarmingApr(bnOrZero(apr).div(100).toString())
+      const farmingApr = await getFarmingApr()
+      setFarmingApr(farmingApr)
       setLoaded(true)
     })()
-  }, [blockNumber, uniswapLPContract, farmingRewardsContract])
+  }, [])
 
   return { loaded, farmingApr }
 }

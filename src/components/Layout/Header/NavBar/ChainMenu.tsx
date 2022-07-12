165c752978cb75675d1e44a0b4422f6aa9333caa
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-07-12 23:52:58 +0200

    wip

--- src/components/Layout/Header/NavBar/ChainMenu.tsx
+++ src/components/Layout/Header/NavBar/ChainMenu.tsx
@@ -1,55 +1,93 @@
+import { ChevronDownIcon } from '@chakra-ui/icons'
 import { Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/menu'
 import { Button, Image } from '@chakra-ui/react'
-import { CHAIN_REFERENCE } from '@shapeshiftoss/caip'
-import { KnownChainIds } from '@shapeshiftoss/types'
-import { useEffect, useState } from 'react'
+import {
+  avalancheAssetId,
+  avalancheChainId,
+  CHAIN_REFERENCE,
+  chainIdToFeeAssetId,
+  ethAssetId,
+  ethChainId,
+  fromChainId,
+} from '@shapeshiftoss/caip'
+import { bnOrZero } from '@shapeshiftoss/investor-foxy'
+import { useEffect, useMemo, useState } from 'react'
 import { getChainAdapters } from 'context/PluginProvider/PluginProvider'
 import { useWallet } from 'hooks/useWallet/useWallet'
+import { selectAssetById } from 'state/slices/selectors'
+import { useAppSelector } from 'state/store'
 
-// TODO: Don't PR this, clean it and move it to lib
-
+// TODO: This should move to lib, we should be able to get `getName()` on each adapter
 const CHAIN_ID_TO_CHAIN_NAME = {
-  [CHAIN_REFERENCE.EthereumMainnet]: 'Ethereum',
-  [CHAIN_REFERENCE.AvalancheCChain]: 'Avalanche',
+  [ethChainId]: 'Ethereum',
+  [avalancheChainId]: 'Avalanche',
 }
 
-// feeAssetId is static, get this outside of component scope
-const feeAssetId = getChainAdapters().get(KnownChainIds.EthereumMainnet)?.getFeeAssetId()
+const ChainMenuItem: React.FC<{
+  chainId: string
+  chainName: string
+  handleEvmChainClick: any
+}> = ({ chainId, chainName, handleEvmChainClick }) => {
+  const { chainReference: evmChainId } = fromChainId(chainId)
+  console.log({ chainId })
+  const nativeAssetId = getChainAdapters().get(chainId)?.getFeeAssetId()
+  console.log({ chainAdapters: getChainAdapters() })
+  console.log({ nativeAssetId })
+  const nativeAsset = useAppSelector(state => selectAssetById(state, nativeAssetId ?? ''))
 
+  if (!nativeAsset) return null
+
+  return (
+    <MenuItem minH='48px' onClick={() => handleEvmChainClick(evmChainId)}>
+      <Image height='48px' src={nativeAsset.icon} />
+      <span>{chainName}</span>
+    </MenuItem>
+  )
+}
 export const ChainMenu = () => {
   const { state } = useWallet()
-  const [chainId, setChainId] = useState<string | null>(null)
+  const [evmChainId, setEvmChainId] = useState<string | null>(null)
 
   useEffect(() => {
     ;(async () => {
-      const chainId = await (state.wallet as any).ethGetChainId?.()
+      const chainId = await (state.wallet as any)?.ethGetChainId?.()
       // If this method is undefined or an error happens with the JSON-RPC call, meaning the wallet doesn't implement this method
       // Set chainId to the result of awaiting this expression (undefined/null), which will make this component not render anything
-      if (chainId) setChainId(chainId)
+      if (chainId) setEvmChainId(bnOrZero(chainId).toString())
     })()
   }, [state])
 
   const handleEvmChainClick = async (chainId: any) => {
     try {
       await (state.wallet as any).ethSwitchChain?.(Number(chainId))
-      setChainId(chainId)
+      setEvmChainId(chainId)
     } catch (e) {
       // TODO: handle me
     }
   }
 
   // TODO: check for supportsEthSwitchChain() here and don't make this clickable if wallet doesn't _supportsEthSwitchChain
-  if (!chainId) return null
+  if (!evmChainId) return null
 
   return (
     <Menu>
-      <MenuButton as={Button}>{CHAIN_ID_TO_CHAIN_NAME[chainId]}</MenuButton>
+      <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+        {(() => {
+          const currentChainIndex = Object.keys(CHAIN_ID_TO_CHAIN_NAME).find(chainId => {
+            const { chainReference } = fromChainId(chainId)
+            return chainReference === evmChainId
+          })
+
+          return CHAIN_ID_TO_CHAIN_NAME[currentChainIndex ?? '']
+        })()}
+      </MenuButton>
       <MenuList>
         {Object.entries(CHAIN_ID_TO_CHAIN_NAME).map(([chainId, chainName]) => (
-          <MenuItem minH='48px' onClick={() => handleEvmChainClick(chainId)}>
-            <Image src={feeAssetId?.icon} />
-            <span>{chainName}</span>
-          </MenuItem>
+          <ChainMenuItem
+            chainId={chainId}
+            chainName={chainName}
+            handleEvmChainClick={handleEvmChainClick}
+          />
         ))}
       </MenuList>
     </Menu>

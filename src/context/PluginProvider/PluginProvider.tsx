97cfd5ad4a21ab3da57952442e39e5ed020d91ee
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2022-07-14 05:36:37 +1000

    feat: add Avalanche plugin (#2105)
    * chore: bump packages for avax support
    
    * feat: add avalanche plugin wip
    
    * chore: add avax rpc url
    
    * feat: use new avalanche methods from unchained
    
    * feat: add avalancheChainId to walletSupportsChain
    
    * feat: bump hdwallet packages to latest
    
    * Update src/plugins/avalanche/index.tsx
    
    Co-authored-by: kevin <35275952+kaladinlight@users.noreply.github.com>
    
    * chore: update yarn.lock
    
    Co-authored-by: gomes <17035424+gomesalexandre@users.noreply.github.com>
    Co-authored-by: 0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
    Co-authored-by: kevin <35275952+kaladinlight@users.noreply.github.com>

--- src/context/PluginProvider/PluginProvider.tsx
+++ src/context/PluginProvider/PluginProvider.tsx
@@ -21,7 +21,7 @@ type PluginProviderContextProps = {
   routes: Route[]
 }
 
-const activePlugins = ['bitcoin', 'cosmos', 'ethereum', 'foxPage', 'osmosis']
+const activePlugins = ['bitcoin', 'cosmos', 'ethereum', 'foxPage', 'osmosis', 'avalanche']
 
 // don't export me, access me through the getter
 let _chainAdapterManager: ChainAdapterManager | undefined
@@ -129,19 +129,11 @@ export const PluginProvider = ({ children }: PluginProviderProps): JSX.Element =
 
     setRoutes(pluginRoutes)
 
-    const omittedChainIds: Array<string> = []
-
-    if (!featureFlags.Osmosis) {
-      omittedChainIds.push(KnownChainIds.OsmosisMainnet)
-    }
-
-    if (!featureFlags.Avalanche) {
-      omittedChainIds.push(KnownChainIds.AvalancheMainnet)
-    }
-
-    const _supportedChains = Object.values<ChainId>(KnownChainIds).filter(
-      chainId => !omittedChainIds.includes(chainId),
-    )
+    const _supportedChains = Object.values<ChainId>(KnownChainIds).filter(chainId => {
+      if (!featureFlags.Osmosis && chainId === KnownChainIds.OsmosisMainnet) return false
+      if (!featureFlags.Avalanche && chainId === KnownChainIds.AvalancheMainnet) return false
+      return true
+    })
 
     moduleLogger.trace({ supportedChains: _supportedChains }, 'Setting supportedChains')
     setSupportedChains(_supportedChains)

366ad11c8cc1a44ed8b1fbf9c830beebb20fc105
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-07-13 17:53:40 +0200

    chore: attempt 1 at fixing tests

--- src/state/slices/portfolioSlice/utils.test.ts
+++ src/state/slices/portfolioSlice/utils.test.ts
@@ -1,5 +1,6 @@
-import { AssetId, btcChainId, ethChainId } from '@shapeshiftoss/caip'
+import { AssetId, avalancheAssetId, btcChainId, ethAssetId, ethChainId } from '@shapeshiftoss/caip'
 import { Asset, KnownChainIds } from '@shapeshiftoss/types'
+import { getChainAdapters } from 'context/PluginProvider/PluginProvider'
 
 import {
   accountIdToChainId,
@@ -12,7 +13,30 @@ import {
   trimWithEndEllipsis,
 } from './utils'
 
+jest.mock('context/PluginProvider/PluginProvider')
+
+const mockChainAdapters = new Map([
+  [KnownChainIds.BitcoinMainnet, {}],
+  [KnownChainIds.CosmosMainnet, {}],
+  [
+    KnownChainIds.EthereumMainnet,
+    {
+      getFeeAssetId: () => ethAssetId,
+    },
+  ],
+  [
+    KnownChainIds.AvalancheMainnet,
+    {
+      getFeeAssetId: () => avalancheAssetId,
+    },
+  ],
+])
+
 describe('accountIdToChainId', () => {
+  beforeEach(() => {
+    ;(getChainAdapters as jest.Mock<unknown>).mockReturnValueOnce(mockChainAdapters)
+  })
+
   it('can get eth chainId from accountId', () => {
     const accountId = 'eip155:1:0xdef1cafe'
     const chainId = accountIdToChainId(accountId)

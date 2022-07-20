419fe2613c610ba4f6a50e58df573041f2a84678
Toshi <10103480+toshiSat@users.noreply.github.com>
2022-07-20 16:31:49 -0600

    Update react-app-rewired/headers/csps/chains/osmosis.ts
    Co-authored-by: Apotheosis <97164662+0xApotheosis@users.noreply.github.com>

--- react-app-rewired/headers/csps/chains/osmosis.ts
+++ react-app-rewired/headers/csps/chains/osmosis.ts
@@ -7,7 +7,7 @@ export const csp: Csp = {
     process.env.REACT_APP_UNCHAINED_OSMOSIS_HTTP_URL!,
     process.env.REACT_APP_UNCHAINED_OSMOSIS_WS_URL!,
     process.env.REACT_APP_OSMOSIS_NODE!, 
-    process.env.REACT_APP_COSMOS_NODE!
+    process.env.REACT_APP_COSMOS_NODE!,
   ],
   'img-src': ['https://raw.githubusercontent.com/osmosis-labs/'],
 }

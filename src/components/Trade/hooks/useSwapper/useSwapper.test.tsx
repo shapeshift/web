e48a4f241e1c3a75bd648733cc92f12b9f00df67
Sean Luther <elmutt@gmail.com>
2022-07-22 14:45:05 -0600

    fix: fix swapper feature flags (#2213)

--- src/components/Trade/hooks/useSwapper/useSwapper.test.tsx
+++ src/components/Trade/hooks/useSwapper/useSwapper.test.tsx
@@ -83,6 +83,9 @@ function setup({
     accountSpecifiers: {
       accountSpecifiers: [{ 'eip155:1': '0x8a65ac0e23f31979db06ec62af62b132a6df4741' }],
     },
+    preferences: {
+      featureFlags: {},
+    },
   }
   return {
     result,

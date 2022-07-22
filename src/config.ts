f7f4e955a4410e8d7065c3f9fa071bf00f97858e
Toshi <10103480+toshiSat@users.noreply.github.com>
2022-07-22 10:34:53 -0600

    feat: osmosis swapper (#2128)
    * add initial swapper
    
    * add nodes to headers
    
    * Fix csp stuff
    
    * update useSwapper
    
    * Add osmo to swapper
    
    * pushing to merge develop
    
    * lint
    
    * Fix names
    
    * osmo - atom working again
    
    * osmo swapping working
    
    * pass through urls
    
    * lint
    
    * Fix wrong buy asset chain
    
    * minor fixes
    
    * fix wrong fee asset
    
    * lint fixes
    
    * merge useSwapper with develop
    
    * use known chain ids
    
    * use correct explorerTx for osmo tx's
    
    * revert removing dependency + useChainId's instead of hardcode
    
    * Fix getting osmosis asset
    
    * Remove fix for changing buy asset on new chain
    
    * subtract tradeFee from buyAmount
    
    * Reset subtracting tradeFee from buy amount
    
    * Revert to tradFee row format from before
    
    * update swapper
    
    * merge with develop
    
    * fix confirm page
    
    * revert useTradeRoutes
    
    * jump through hoops
    
    * lint
    
    * remove feeAsset changes
    
    * use useMemo
    
    * pass receiveAddress + fix reverted code
    
    * Update src/components/Trade/hooks/useSwapper/useSwapper.ts
    
    Co-authored-by: gomes <17035424+gomesalexandre@users.noreply.github.com>
    
    * Update src/components/Trade/hooks/useSwapper/useSwapper.ts
    
    Co-authored-by: gomes <17035424+gomesalexandre@users.noreply.github.com>
    
    * Update src/components/Trade/TradeConfirm/TradeConfirm.tsx
    
    Co-authored-by: gomes <17035424+gomesalexandre@users.noreply.github.com>
    
    * use proxy nodes
    
    * add type guard
    
    * revert feeAsset changes
    
    * Update sample.env
    
    Co-authored-by: Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
    
    * Update react-app-rewired/headers/csps/chains/osmosis.ts
    
    Co-authored-by: Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
    
    * use url postfix
    
    * duplicate market-service in package.json
    
    * update yml files
    
    * chore: trigger CI
    
    * chore: fix whitespace lint
    
    * chore: move cosmos env var to cosmos csp file
    
    * chore: update yarn.lock
    
    * update swapper
    
    * fix: abstract getReceiveAddress logic in swapper
    
    * update typo
    
    * update typo
    
    * Use actual feeAsset in debounce function
    
    * update swapper
    
    Co-authored-by: gomes <17035424+gomesalexandre@users.noreply.github.com>
    Co-authored-by: Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
    Co-authored-by: 0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>

--- src/config.ts
+++ src/config.ts
@@ -62,6 +62,12 @@ const validators = {
   REACT_APP_MIDGARD_URL: url({
     default: 'https://midgard.thorchain.info/v2',
   }),
+  REACT_APP_COSMOS_NODE_URL: url({
+    default: 'https://rest.cosmos.directory/cosmoshub/',
+  }),
+  REACT_APP_OSMOSIS_NODE_URL: url({
+    default: 'https://rest.cosmos.directory/osmosis/',
+  }),
   REACT_APP_FEATURE_PENDO: bool({ default: false }),
   REACT_APP_PENDO_API_KEY: envalid.str({ default: '67c2f326-a6c2-4aa2-4559-08a53b679e93' }),
   REACT_APP_PENDO_CONSENT_VERSION: envalid.str({ default: 'v1' }),

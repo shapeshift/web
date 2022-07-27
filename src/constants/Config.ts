14d67afd8da4b9e7b1d5ff2485efa98a3f418c10
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2022-07-28 08:42:54 +1000

    chore: fix typo

--- src/constants/Config.ts
+++ src/constants/Config.ts
@@ -1,6 +1,6 @@
 import { HistoryTimeframe } from '@shapeshiftoss/types'
 
 export const constants = {
-  // used by AssetChart, Portfolio, and this config.ts to prefetch price history
+  // used by AssetChart, Portfolio, and config.ts to prefetch price history
   DEFAULT_HISTORY_TIMEFRAME: HistoryTimeframe.MONTH,
 } as const

ad70492094e58082ed1aca6662843786ee6b3019
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2022-07-27 18:33:41 +1000

    chore: fix useEvm hook

--- src/hooks/useEvm/useEvm.ts
+++ src/hooks/useEvm/useEvm.ts
@@ -1,6 +1,6 @@
 import { CHAIN_NAMESPACE, fromChainId } from '@shapeshiftoss/caip'
 import { useMemo } from 'react'
-import { getChainAdapters } from 'context/PluginProvider/PluginProvider'
+import { getChainAdapterManager } from 'context/PluginProvider/chainAdapterSingleton'
 import { selectFeatureFlags } from 'state/slices/preferencesSlice/selectors'
 import { useAppSelector } from 'state/store'
 
@@ -8,7 +8,7 @@ export const useEvm = () => {
   const featureFlags = useAppSelector(selectFeatureFlags)
   const supportedEvmChainIds = useMemo(
     () =>
-      Array.from(getChainAdapters().keys()).filter(
+      Array.from(getChainAdapterManager().keys()).filter(
         chainId => fromChainId(chainId).chainNamespace === CHAIN_NAMESPACE.Ethereum,
       ),
     // We want to explicitly react on featureFlags to get a new reference here

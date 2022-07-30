6eefa5d7285520364b9ba85b2943a2ce9729248a
jmurphy5613 <jmurphy5613@gmail.com>
2022-07-30 03:25:22 -0400

    set up working example without styles

--- src/components/Modals/FiatRamps/FiatRamps.tsx
+++ src/components/Modals/FiatRamps/FiatRamps.tsx
@@ -5,20 +5,47 @@ import { useModal } from 'hooks/useModal/useModal'
 import { FiatRampsRoutes } from './FiatRampsCommon'
 import { FiatRampsRouter } from './FiatRampsRouter'
 
+import { useHistory } from 'react-router-dom'
+import { IconButton } from '@chakra-ui/react'
+import { ArrowBackIcon } from '@chakra-ui/icons'
+
+
 const entries = [FiatRampsRoutes.Select, FiatRampsRoutes.Manager]
+
+
+const FiatRampsContent = () => {
+  const history = useHistory()
+  return (
+    <>
+      <ModalCloseButton />
+      <IconButton 
+        variant="ghost"
+        icon={<ArrowBackIcon />}
+        onClick={() => {
+          history.goBack()
+        }}
+        size="sm"
+        aria-label='Back'
+      />
+      <FiatRampsRouter />
+    </>
+  )
+}
 
 export const FiatRampsModal = () => {
   const { fiatRamps } = useModal()
   const { close, isOpen } = fiatRamps
+
+
   return (
-    <Modal isOpen={isOpen} onClose={close} isCentered variant='fluid'>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalCloseButton />
-        <MemoryRouter initialEntries={entries}>
-          <FiatRampsRouter />
-        </MemoryRouter>
-      </ModalContent>
-    </Modal>
+    <MemoryRouter initialEntries={entries}>
+      <Modal isOpen={isOpen} onClose={close} isCentered variant='fluid'>
+        <ModalOverlay />
+        <ModalContent>
+          <FiatRampsContent />
+        </ModalContent>
+      </Modal>
+    </MemoryRouter>
+
   )
 }

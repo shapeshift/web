007fb4335be460269a567d2078c1ac18c6e87769
stackedQ <stackedQ@gmail.com>
2022-08-02 17:00:55 +0430

    fix: trying to use signandbroadcasttx

--- src/plugins/foxPage/hooks/useFarmingApr.ts
+++ src/plugins/foxPage/hooks/useFarmingApr.ts
@@ -1,13 +1,22 @@
-import { TransactionRequest } from '@ethersproject/abstract-provider'
-import { BigNumber, BigNumberish } from '@ethersproject/bignumber'
 import { Contract } from '@ethersproject/contracts'
+import {
+  ChainAdapter,
+  ethereum,
+  EvmBaseAdapter,
+  EvmChainId,
+  FeeData,
+} from '@shapeshiftoss/chain-adapters'
+import { supportsETH } from '@shapeshiftoss/hdwallet-core'
+import { KnownChainIds } from '@shapeshiftoss/types'
 import { Fetcher, Token } from '@uniswap/sdk'
 import IUniswapV2Pair from '@uniswap/v2-core/build/IUniswapV2Pair.json'
 import IUniswapV2Router02ABI from '@uniswap/v2-periphery/build/IUniswapV2Router02.json'
 import { useCallback, useEffect, useMemo, useState } from 'react'
+import { getChainAdapterManager } from 'context/PluginProvider/chainAdapterSingleton'
+import { useEvm } from 'hooks/useEvm/useEvm'
+import { useWallet } from 'hooks/useWallet/useWallet'
 import { bnOrZero } from 'lib/bignumber/bignumber'
-import { toBaseUnit } from 'lib/math'
-import { selectAccountIdsByAssetId } from 'state/slices/selectors'
+import { selectAssetById, selectFirstAccountSpecifierByChainId } from 'state/slices/selectors'
 import { useAppSelector } from 'state/store'
 
 import {
@@ -23,43 +32,12 @@ import { getEthersProvider, makeTotalLpApr, rewardRatePerToken } from '../utils'
 import { useCurrentBlockNumber } from './useCurrentBlockNumber'
 
 const ethersProvider = getEthersProvider()
-
-function calculateSlippageMargin(percentage: number, amount: string) {
+function calculateSlippageMargin(amount: string | null) {
+  if (!amount) throw new Error('Amount not given for slippage')
+  const percentage = 3
   const remainingPercentage = (100 - percentage) / 100
-  return bnOrZero(toBaseUnit(amount, 18))
-    .times(bnOrZero(remainingPercentage))
-    .decimalPlaces(0)
-    .toFixed()
+  return bnOrZero(amount).times(bnOrZero(remainingPercentage)).decimalPlaces(0).toFixed()
 }
-
-function bufferGas(limitOrPrice: string) {
-  return bnOrZero(limitOrPrice).times(1.2).decimalPlaces(0).toFixed()
-}
-
-async function getBufferedGas(tx: TransactionRequest) {
-  let gasLimit = null
-  let gasPrice = null
-  try {
-    if (ethersProvider && tx) {
-      const gas = await ethersProvider?.estimateGas(tx)
-      const price = await ethersProvider?.getGasPrice()
-      if (gas && price) {
-        gasLimit = bufferGas(gas?.toString())
-        gasPrice = bufferGas(price?.toString())
-      }
-    }
-    return {
-      gasLimit,
-      gasPrice,
-    }
-  } catch {
-    throw Error('Problem estimating gas')
-  }
-}
-
-const toHex = (value: BigNumberish) => BigNumber.from(value).toHexString()
-
-const slippagePercentage = 3
 
 export const useFarmingApr = () => {
   const [farmingAprV2, setFarmingAprV2] = useState<string | null>(null)
@@ -67,11 +45,15 @@ export const useFarmingApr = () => {
   const [isFarmingAprV2Loaded, setIsFarmingAprV2Loaded] = useState(false)
   const [isFarmingAprV4Loaded, setIsFarmingAprV4Loaded] = useState(false)
   const blockNumber = useCurrentBlockNumber()
+  const { supportedEvmChainIds } = useEvm()
+  const ethAsset = useAppSelector(state => selectAssetById(state, 'eip155:1/slip44:60'))
+  const {
+    state: { wallet },
+  } = useWallet()
 
-  const accountIds = useAppSelector(state =>
-    selectAccountIdsByAssetId(state, { assetId: 'eip155:1/slip44:60' }),
+  const connectedWalletEthAddress = useAppSelector(state =>
+    selectFirstAccountSpecifierByChainId(state, ethAsset.chainId),
   )
-  const connectWalletEthAddress = accountIds[0]
 
   const uniV2LiquidityContractAddress = UNISWAP_V2_WETH_FOX_POOL_ADDRESS
 
@@ -138,56 +120,104 @@ export const useFarmingApr = () => {
   const addLiquidity = useCallback(
     async (foxAmount: string, ethAmount: string) => {
       try {
-        if (!connectWalletEthAddress || !uniswapRouterContract) return
-        const data = uniswapRouterContract.interface.encodeFunctionData('addLiquidityETH', [
-          FOX_TOKEN_CONTRACT_ADDRESS,
-          toBaseUnit(foxAmount, 18),
-          calculateSlippageMargin(slippagePercentage, foxAmount),
-          calculateSlippageMargin(slippagePercentage, ethAmount),
-          connectWalletEthAddress,
-          Date.now() + 1200000,
-        ])
+        if (!connectedWalletEthAddress || !uniswapRouterContract || !wallet) return
+        const chainAdapterManager = getChainAdapterManager()
+        try {
+          const adapter = chainAdapterManager.get(ethAsset.chainId) as ChainAdapter<KnownChainIds>
+          if (!adapter)
+            throw new Error(`addLiquidityEth: no adapter available for ${ethAsset.chainId}`)
+          const value = bnOrZero(ethAmount)
+            .times(bnOrZero(10).exponentiatedBy(ethAsset.precision))
+            .toFixed(0)
+          const data = uniswapRouterContract?.interface.encodeFunctionData('addLiquidityETH', [
+            FOX_TOKEN_CONTRACT_ADDRESS,
+            foxAmount,
+            calculateSlippageMargin(foxAmount),
+            calculateSlippageMargin(ethAmount),
+            connectedWalletEthAddress,
+            Date.now() + 1200000,
+          ])
+          const adapterType = adapter.getChainId()
+          const estimatedFees = await (adapter as unknown as EvmBaseAdapter<EvmChainId>).getFeeData(
+            {
+              to: UNISWAP_V2_ROUTER_ADDRESS,
+              value,
+              chainSpecific: {
+                contractData: data,
+                from: connectedWalletEthAddress,
+                contractAddress: FOX_TOKEN_CONTRACT_ADDRESS,
+              },
+            },
+          )
+          const result = await (async () => {
+            if (supportedEvmChainIds.includes(adapterType)) {
+              if (!supportsETH(wallet))
+                throw new Error(`addLiquidityEthFox: wallet does not support ethereum`)
+              const fees = estimatedFees.average as FeeData<EvmChainId>
+              const {
+                chainSpecific: { gasPrice, gasLimit, maxFeePerGas, maxPriorityFeePerGas },
+              } = fees
+              const shouldUseEIP1559Fees =
+                (await wallet.ethSupportsEIP1559()) &&
+                maxFeePerGas !== undefined &&
+                maxPriorityFeePerGas !== undefined
+              if (!shouldUseEIP1559Fees && gasPrice === undefined) {
+                throw new Error(`addLiquidityEthFox: missing gasPrice for non-EIP-1559 tx`)
+              }
+              return await (adapter as unknown as ethereum.ChainAdapter).buildSendTransaction({
+                to: UNISWAP_V2_ROUTER_ADDRESS,
+                value,
+                wallet,
+                chainSpecific: {
+                  contractData: data,
+                  erc20ContractAddress: FOX_TOKEN_CONTRACT_ADDRESS,
+                  gasLimit,
+                  ...(shouldUseEIP1559Fees ? { maxFeePerGas, maxPriorityFeePerGas } : { gasPrice }),
+                },
+              })
+            } else {
+              throw new Error(`addLiquidityEthFox: wallet does not support ethereum`)
+            }
+          })()
+          const txToSign = result.txToSign
 
-        const tx = {
-          from: connectWalletEthAddress,
-          to: UNISWAP_V2_ROUTER_ADDRESS,
-          data,
-          value: toHex(toBaseUnit(ethAmount, 18)),
-        }
+          const broadcastTXID = await (async () => {
+            if (wallet.supportsOfflineSigning()) {
+              const signedTx = await adapter.signTransaction({
+                txToSign,
+                wallet,
+              })
+              return adapter.broadcastTransaction(signedTx)
+            } else if (wallet.supportsBroadcast()) {
+              /**
+               * signAndBroadcastTransaction is an optional method on the HDWallet interface.
+               * Check and see if it exists; if so, call and make sure a txhash is returned
+               */
+              if (!adapter.signAndBroadcastTransaction) {
+                throw new Error('signAndBroadcastTransaction undefined for wallet')
+              }
+              return adapter.signAndBroadcastTransaction?.({ txToSign, wallet })
+            } else {
+              throw new Error('Bad hdwallet config')
+            }
+          })()
 
-        const { gasLimit, gasPrice } = await getBufferedGas(tx)
-        if (gasLimit && gasPrice) {
-          // dispatch({
-          //   type: LpActions.SET_TX_FEE,
-          //   payload: bn(gasLimit).times(gasPrice).toFixed()
-          // })
-          const ethBalance = await ethersProvider.getBalance(connectWalletEthAddress)
-          if (
-            bnOrZero(ethBalance.toString())
-              .minus(bnOrZero(toBaseUnit(ethAmount as string, 18)))
-              .lt(bnOrZero(gasLimit).times(gasPrice).toFixed())
-          ) {
-            throw new Error('Not enough ETH for gas')
+          if (!broadcastTXID) {
+            throw new Error('Broadcast failed')
           }
-
-          const nonce = await ethersProvider.getSigner().getTransactionCount()
-          const lpTx = await ethersProvider.getSigner().sendTransaction({
-            from: connectWalletEthAddress,
-            to: tx.to,
-            data: tx.data,
-            value: tx.value,
-            gasLimit: toHex(gasLimit),
-            gasPrice: toHex(gasPrice),
-            nonce,
-            chainId: 1,
-          })
-          return lpTx
-        }
+        } catch (e) {}
       } catch (error) {
         console.warn(error)
       }
     },
-    [connectWalletEthAddress, uniswapRouterContract],
+    [
+      connectedWalletEthAddress,
+      ethAsset.chainId,
+      ethAsset.precision,
+      supportedEvmChainIds,
+      uniswapRouterContract,
+      wallet,
+    ],
   )
 
   return { isFarmingAprV2Loaded, isFarmingAprV4Loaded, farmingAprV2, farmingAprV4, addLiquidity }

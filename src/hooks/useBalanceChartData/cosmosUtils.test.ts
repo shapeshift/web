993462c1a9ff9e83690b97381acdcc91319c0228
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-08-04 17:31:16 +0200

    fix: tests unborken

--- src/hooks/useBalanceChartData/cosmosUtils.test.ts
+++ src/hooks/useBalanceChartData/cosmosUtils.test.ts
@@ -1,7 +1,7 @@
 import { merge } from 'lodash'
 import { Tx } from 'state/slices/txHistorySlice/txHistorySlice'
 
-import { includeTransaction } from './cosmosUtils'
+import { excludeTransaction } from './cosmosUtils'
 
 const mockTx = (obj?: { parser?: string; method?: string }) =>
   ({
@@ -15,15 +15,15 @@ const mockTx = (obj?: { parser?: string; method?: string }) =>
   } as Tx)
 
 describe('cosmosUtils', () => {
-  describe('includeTransaction', () => {
+  describe('excludeTransaction', () => {
     it.each([[undefined], [{ method: 'begin_unbonding' }]])('should return false for %O', args => {
-      expect(includeTransaction(mockTx(args))).toBe(false)
+      expect(excludeTransaction(mockTx(args))).toBe(true)
     })
 
     it.each([[{ parser: 'bitcoin' }], [{ method: 'normal_tx' }]])(
       'should return true for %O',
       args => {
-        expect(includeTransaction(mockTx(args))).toBe(true)
+        expect(excludeTransaction(mockTx(args))).toBe(false)
       },
     )
   })

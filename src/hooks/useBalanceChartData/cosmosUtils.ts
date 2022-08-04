cce5f1df78a75f3b8603404a3f088092ae8b267d
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-08-04 17:22:36 +0200

    chore: cleaner exclude

--- src/hooks/useBalanceChartData/cosmosUtils.ts
+++ src/hooks/useBalanceChartData/cosmosUtils.ts
@@ -1,14 +1,13 @@
 import { Tx } from 'state/slices/txHistorySlice/txHistorySlice'
 
 export const excludeTransaction = (tx: Tx): boolean =>
-  !Boolean(
-    tx.data?.parser !== 'cosmos' ||
-      !(
-        tx?.data.method === 'delegate' ||
-        tx?.data.method === 'begin_unbonding' ||
-        tx?.data.method === 'withdraw_delegator_reward' ||
-        tx?.data.method === 'begin_redelegate' ||
-        tx?.data.method === 'cancel_unbond' ||
-        tx?.data.method === 'set_withdraw_address'
-      ),
+  Boolean(
+    tx.data &&
+      tx.data.parser === 'cosmos' &&
+      (tx.data.method === 'delegate' ||
+        tx.data.method === 'begin_unbonding' ||
+        tx.data.method === 'withdraw_delegator_reward' ||
+        tx.data.method === 'begin_redelegate' ||
+        tx.data.method === 'cancel_unbond' ||
+        tx.data.method === 'set_withdraw_address'),
   )

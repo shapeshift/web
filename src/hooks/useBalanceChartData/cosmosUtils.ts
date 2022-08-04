3dbe7a758b9d8ad33bd9ac5c77ff4501195fb5da
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-08-04 15:46:54 +0200

    fix: unrug cosmos negative balances

--- src/hooks/useBalanceChartData/cosmosUtils.ts
+++ src/hooks/useBalanceChartData/cosmosUtils.ts
@@ -2,4 +2,8 @@ import { Tx } from 'state/slices/txHistorySlice/txHistorySlice'
 
 export const includeTransaction = (tx: Tx): boolean =>
   tx.data?.parser !== 'cosmos' ||
-  !(tx?.data.method === 'delegate' || tx?.data.method === 'begin_unbonding')
+  !(
+    tx?.data.method === 'delegate' ||
+    tx?.data.method === 'begin_unbonding' ||
+    tx?.data.method === 'withdraw_delegator_reward'
+  )

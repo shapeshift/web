3056536a030acfc1d287e0352258239d1b1ae560
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-08-04 15:58:05 +0200

    fix: add cosmos-sdk begin_redelegate messages to excluded balance chart Txs

--- src/hooks/useBalanceChartData/cosmosUtils.ts
+++ src/hooks/useBalanceChartData/cosmosUtils.ts
@@ -5,5 +5,6 @@ export const includeTransaction = (tx: Tx): boolean =>
   !(
     tx?.data.method === 'delegate' ||
     tx?.data.method === 'begin_unbonding' ||
-    tx?.data.method === 'withdraw_delegator_reward'
+    tx?.data.method === 'withdraw_delegator_reward' ||
+    tx?.data.method === 'begin_redelegate'
   )

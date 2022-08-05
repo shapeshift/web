fefbfa502c4cab6cd0e3a3db598ea369f0315ae3
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2022-08-05 14:02:55 -0600

    fix: css theming of zendesk iframes to support our dark mode

--- src/components/Zendesk/Zendesk.tsx
+++ src/components/Zendesk/Zendesk.tsx
@@ -1,27 +1,40 @@
+import './zendesk.css'
+
 import { getConfig } from 'config'
 import { useEffect } from 'react'
 
 // https://support.zendesk.com/hc/en-us/articles/4500748175258
 export const Zendesk = () => {
   const key = getConfig().REACT_APP_ZENDESK_KEY
+
   useEffect(() => {
-    if (!key) return
+    if (!key) return // key is not set for private, is set for app
+
+    const scriptId = 'ze-snippet'
+    const maybeScript = document.getElementById(scriptId)
+    if (maybeScript) return // don't add multiple copies
+
+    // const style = document.createElement('style')
+    // style.id = 'launcher'
+    // style.
     const script = document.createElement('script')
+    script.id = scriptId
     script.src = `https://static.zdassets.com/ekr/snippet.js?key=${key}`
-    script.id = 'ze-snippet'
-    document.body.appendChild(script)
-    if ((window as any).zE) {
-      ;(window as any).zE('webWidget', 'updateSettings', {
-        webWidget: {
-          launcher: {
-            label: {
-              '*': '\u200B', // zero-width space replacing 'Support' label
-            },
-          },
-        },
-      })
-    }
-    return () => void document.body.removeChild(script)
+    document.head.appendChild(script)
+
+    // setTimeout(() => {
+    //   if ((window as any).zE) {
+    //     ;(window as any).zE('webWidget', 'updateSettings', {
+    //       webWidget: {
+    //         launcher: {
+    //           label: {
+    //             '*': '\u200B', // zero-width space replacing 'Support' label
+    //           },
+    //         },
+    //       },
+    //     })
+    //   }
+    // }, 0)
   }, [key])
 
   return null

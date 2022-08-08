a389a3e2f80177cbca643a20a8525017fad2faa5
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2022-08-09 06:16:45 +1000

    chore(unchained-client): use evm tx type (#958)
    * chore: use evm tx type
    
    * chore: use type imports in touched file

--- packages/unchained-client/src/evm/parser/zrx.ts
+++ packages/unchained-client/src/evm/parser/zrx.ts
@@ -1,10 +1,7 @@
-import { Tx as AvalancheTx } from '../../generated/avalanche'
-import { Tx as EthTx } from '../../generated/ethereum'
 import { Dex, TradeType } from '../../types'
-import { SubParser, txInteractsWithContract, TxSpecific } from '.'
+import { type Tx } from '../parser/types'
+import { type SubParser, type TxSpecific, txInteractsWithContract } from '.'
 import { ZRX_PROXY_CONTRACT } from './constants'
-
-type Tx = EthTx | AvalancheTx
 
 export class Parser implements SubParser<Tx> {
   async parse(tx: Tx): Promise<TxSpecific | undefined> {

f4674782d0ecb723733031f393033935ad8cf225
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2022-08-10 09:10:49 -0600

    chore: more specific query of zendesk iframe to be styled

--- src/components/Zendesk/Zendesk.tsx
+++ src/components/Zendesk/Zendesk.tsx
@@ -80,23 +80,23 @@ export const Zendesk = () => {
      */
     const SIXTY_FPS = 1000 / 60 // run this at 60fps to avoid a flicker if possible
     const id = setInterval(() => {
-      document.querySelectorAll('iframe').forEach(iframe => {
-        if (iframe.id !== 'launcher') return
-        const icon = iframe.contentDocument?.querySelector<HTMLSpanElement>(
-          'span[data-testid="Icon"]',
-        )
-        const button = iframe.contentDocument?.querySelector<HTMLButtonElement>(
-          'button[data-testid="launcher"]',
-        )
-        if (icon && button) {
-          // monkey patch css of icon because of zero-width space to avoid lopsided icon
-          icon.style.paddingRight = '0'
-          button.style.paddingRight = '0.92857rem'
-          button.style.paddingLeft = '0.92857rem'
-          moduleLogger.trace('Zendesk icon monkey styled')
-          clearInterval(id)
-        }
-      })
+      const maybeIframe = document.getElementById('launcher')
+      if (!maybeIframe) return
+      const iframe: HTMLIFrameElement = maybeIframe as HTMLIFrameElement
+      const icon = iframe.contentDocument?.querySelector<HTMLSpanElement>(
+        'span[data-testid="Icon"]',
+      )
+      const button = iframe.contentDocument?.querySelector<HTMLButtonElement>(
+        'button[data-testid="launcher"]',
+      )
+      if (icon && button) {
+        // monkey patch css of icon because of zero-width space to avoid lopsided icon
+        icon.style.paddingRight = '0'
+        button.style.paddingRight = '0.92857rem'
+        button.style.paddingLeft = '0.92857rem'
+        moduleLogger.trace('Zendesk icon monkey styled')
+        clearInterval(id)
+      }
     }, SIXTY_FPS)
 
     // everything is setup, add the script

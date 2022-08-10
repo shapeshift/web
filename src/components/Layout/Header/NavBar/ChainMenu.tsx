28c5a79839801010a45dc1d466883414fe5718a9
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-08-10 20:17:27 +0200

    Revert "fix: handle default swapper assets for non-evm chains (inc. Cosmos and Osmosis) (#2370)"
    This reverts commit 91ce7c1b80d956b6c7b695740b4d6834269c2f68.

--- src/components/Layout/Header/NavBar/ChainMenu.tsx
+++ src/components/Layout/Header/NavBar/ChainMenu.tsx
@@ -44,7 +44,7 @@ const ChainMenuItem: React.FC<{
 }
 export const ChainMenu = () => {
   const { state, load } = useWallet()
-  const { supportedEvmChainIds, connectedEvmChainId, setEthNetwork } = useEvm()
+  const { supportedEvmChainIds, connectedChainId, setEthNetwork } = useEvm()
   const chainAdapterManager = getChainAdapterManager()
 
   const handleChainClick = async (chainId: ChainId) => {
@@ -58,14 +58,14 @@ export const ChainMenu = () => {
   }
 
   const currentChainNativeAssetId = useMemo(
-    () => chainAdapterManager.get(connectedEvmChainId ?? '')?.getFeeAssetId(),
-    [chainAdapterManager, connectedEvmChainId],
+    () => chainAdapterManager.get(connectedChainId ?? '')?.getFeeAssetId(),
+    [chainAdapterManager, connectedChainId],
   )
   const currentChainNativeAsset = useAppSelector(state =>
     selectAssetById(state, currentChainNativeAssetId ?? ''),
   )
 
-  if (!state.wallet || !connectedEvmChainId || !currentChainNativeAsset) return null
+  if (!state.wallet || !connectedChainId || !currentChainNativeAsset) return null
   if (!supportsEthSwitchChain(state.wallet)) return null
 
   // don't show the menu if there is only one chain
@@ -81,7 +81,7 @@ export const ChainMenu = () => {
         <Flex alignItems='center'>
           <AssetIcon src={currentChainNativeAsset.icon} size='xs' mr='8px' />
           {chainAdapterManager
-            .get(supportedEvmChainIds.find(chainId => chainId === connectedEvmChainId) ?? '')
+            .get(supportedEvmChainIds.find(chainId => chainId === connectedChainId) ?? '')
             ?.getDisplayName() ?? ''}
         </Flex>
       </MenuButton>
@@ -89,7 +89,7 @@ export const ChainMenu = () => {
         <MenuGroup title={'Select a network'} ml={3} color='gray.500'>
           {supportedEvmChainIds.map(chainId => (
             <ChainMenuItem
-              isConnected={chainId === connectedEvmChainId}
+              isConnected={chainId === connectedChainId}
               key={chainId}
               chainId={chainId}
               onClick={handleChainClick}

a94fc678864a76a210677b7e4eb53b46b0d997bb
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2022-08-11 20:19:21 +1000

    fix: don't change assets when no input amounts (#2398)
    fix: do not change assets after trade asset selection

--- src/components/Trade/hooks/useTradeRoutes/useTradeRoutes.ts
+++ src/components/Trade/hooks/useTradeRoutes/useTradeRoutes.ts
@@ -101,11 +101,7 @@ export const useTradeRoutes = (
       const routeDefaultBuyAsset = assets[buyAssetId]
 
       // If we don't have a quote already, get one for the route's default assets
-      if (
-        routeDefaultSellAsset &&
-        routeDefaultBuyAsset &&
-        !(buyTradeAsset?.amount || sellTradeAsset?.amount)
-      ) {
+      if (routeDefaultSellAsset && routeDefaultBuyAsset && !(buyTradeAsset || sellTradeAsset)) {
         setValue('buyAsset.asset', routeDefaultBuyAsset)
         setValue('sellAsset.asset', routeDefaultSellAsset)
         await updateQuote({
@@ -123,13 +119,13 @@ export const useTradeRoutes = (
     }
   }, [
     assets,
-    buyTradeAsset?.amount,
+    buyTradeAsset,
     defaultBuyAssetId,
     defaultFeeAsset,
     defaultSellAssetId,
     routeBuyAssetId,
     selectedCurrencyToUsdRate,
-    sellTradeAsset?.amount,
+    sellTradeAsset,
     setValue,
     swapperManager,
     updateQuote,

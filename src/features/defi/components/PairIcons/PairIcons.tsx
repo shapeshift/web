2ed348eb5e7a5bd201383d82bfb84fe5d4658a75
Q <stackedq@gmail.com>
2022-08-16 03:02:43 +0430

    feat: fox farming opportunities (#2433)
    * feat: fox farming opportunities
    
    Co-authored-by: gomes <17035424+gomesalexandre@users.noreply.github.com>

--- src/features/defi/components/PairIcons/PairIcons.tsx
+++ src/features/defi/components/PairIcons/PairIcons.tsx
@@ -1,14 +1,27 @@
-import { Flex, useColorModeValue } from '@chakra-ui/react'
+import { Flex, FlexProps, ThemingProps, useColorModeValue } from '@chakra-ui/react'
 import { AssetIcon } from 'components/AssetIcon'
 
-export const PairIcons = ({ icons, isSmall }: { icons: Array<string>; isSmall?: boolean }) => {
-  const boxHeight = isSmall ? '38px' : '46px'
-  const assetBoxSize = isSmall ? '5' : '6'
+export const PairIcons = ({
+  icons,
+  iconSize,
+  iconBoxSize,
+  ...styleProps
+}: {
+  icons: Array<string>
+  iconBoxSize?: string
+  iconSize?: ThemingProps<'Avatar'>['size']
+} & FlexProps): JSX.Element => {
   const bg = useColorModeValue('gray.200', 'gray.700')
   return (
-    <Flex flexDirection='row' alignItems='center' bg={bg} p={1} borderRadius={8} h={boxHeight}>
+    <Flex flexDirection='row' alignItems='center' bg={bg} {...styleProps}>
       {icons.map((iconSrc, i) => (
-        <AssetIcon key={iconSrc} src={iconSrc} boxSize={assetBoxSize} ml={i === 0 ? '0' : '-2.5'} />
+        <AssetIcon
+          key={iconSrc}
+          src={iconSrc}
+          boxSize={iconBoxSize}
+          size={iconSize}
+          ml={i === 0 ? '0' : '-2.5'}
+        />
       ))}
     </Flex>
   )
---
+++ src/features/defi/providers/fox-farming/components/FoxFarmingManager/FoxFarmingManager.tsx
@@ -0,0 +1,43 @@
+import {
+  DefiAction,
+  DefiParams,
+  DefiQueryParams,
+} from 'features/defi/contexts/DefiManagerProvider/DefiCommon'
+import { AnimatePresence } from 'framer-motion'
+import { SlideTransition } from 'components/SlideTransition'
+import { useBrowserRouter } from 'hooks/useBrowserRouter/useBrowserRouter'
+
+import { FoxFarmingDeposit } from './Deposit/FoxFarmingDeposit'
+import { Claim } from './Overview/Claim/Claim'
+import { FoxFarmingOverview } from './Overview/FoxFarmingOverview'
+import { FoxFarmingWithdraw } from './Withdraw/FoxFarmingWithdraw'
+
+export const FoxFarmingManager = () => {
+  const { query } = useBrowserRouter<DefiQueryParams, DefiParams>()
+  const { modal } = query
+
+  return (
+    <AnimatePresence exitBeforeEnter initial={false}>
+      {modal === DefiAction.Overview && (
+        <SlideTransition key={DefiAction.Overview}>
+          <FoxFarmingOverview />
+        </SlideTransition>
+      )}
+      {modal === DefiAction.Deposit && (
+        <SlideTransition key={DefiAction.Deposit}>
+          <FoxFarmingDeposit />
+        </SlideTransition>
+      )}
+      {modal === DefiAction.Withdraw && (
+        <SlideTransition key={DefiAction.Withdraw}>
+          <FoxFarmingWithdraw />
+        </SlideTransition>
+      )}
+      {modal === DefiAction.Claim && (
+        <SlideTransition key={DefiAction.Claim}>
+          <Claim />
+        </SlideTransition>
+      )}
+    </AnimatePresence>
+  )
+}

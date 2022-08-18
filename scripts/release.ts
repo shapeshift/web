e37d1f821fd3969ca834511e79e92a3e039e7e3c
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2022-08-17 20:37:21 -0600

    feat: prompts in release script

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,6 +1,7 @@
 // CLI dev tool, we need dat console
 /* eslint-disable no-console */
-import chalk from 'chalk'
+import chalk from 'chalk' // do not upgrade to v5, not compatible with ts-node
+import inquirer from 'inquirer' // do not upgrade to v9, not compatible with ts-node
 import { simpleGit } from 'simple-git'
 
 const exit = () => process.exit(0)
@@ -18,9 +19,57 @@ export const assertIsCleanRepo = async () => {
   }
 }
 
+const releaseType = ['Regular', 'Hotfix'] as const
+type ReleaseType = keyof typeof releaseType
+
+const inquireReleaseType = async (): Promise<ReleaseType> => {
+  const questions: inquirer.QuestionCollection<{ releaseType: ReleaseType }> = [
+    {
+      type: 'list',
+      name: 'releaseType',
+      message: 'What type of release is this?',
+      choices: releaseType,
+    },
+  ]
+  return (await inquirer.prompt(questions)).releaseType
+}
+
+const doRegularRelease = async () => {
+  // git log --oneline --first-parent "origin/main".."origin/releases/$2"
+  const mergedPRs = await simpleGit().log([
+    '--oneline',
+    '--first-parent',
+    'origin/main..origin/develop',
+  ])
+  console.log(chalk.white(mergedPRs))
+  // console.log(chalk.green('Checking out develop...'))
+  // await simpleGit().checkout('develop')
+  // await simpleGit().checkout(['-B', 'release']) // reset release to develop
+  exit()
+}
+
+const doHotfixRelease = async () => {
+  console.log(chalk.yellow('Unimplemented. PRs welcome!'))
+  exit()
+}
+
 const main = async () => {
   // await assertIsCleanRepo()
+  const releaseType = await inquireReleaseType()
   await fetch()
+  switch (releaseType) {
+    case 'Regular': {
+      await doRegularRelease()
+      break
+    }
+    case 'Hotfix': {
+      await doHotfixRelease()
+      break
+    }
+    default: {
+      exit()
+    }
+  }
 }
 
 main()

3ab10cb722f183812bd2237ee4a5eb4432412dee
reallybeard <reallybeard@gmail.com>
2022-08-19 09:19:01 -0500

    Group nav items by category

--- src/components/Layout/Header/NavBar/NavBar.tsx
+++ src/components/Layout/Header/NavBar/NavBar.tsx
@@ -2,7 +2,9 @@ import { Stack, StackProps } from '@chakra-ui/react'
 import { union } from 'lodash'
 import { useTranslate } from 'react-polyglot'
 import { Link as ReactRouterLink } from 'react-router-dom'
+import { Route } from 'Routes/helpers'
 import { routes } from 'Routes/RoutesCommon'
+import { Text } from 'components/Text'
 import { usePlugins } from 'context/PluginProvider/PluginProvider'
 
 import { MainNavLink } from './MainNavLink'
@@ -15,25 +17,49 @@ export const NavBar = ({ isCompact, ...rest }: NavBarProps) => {
   const translate = useTranslate()
   const { routes: pluginRoutes } = usePlugins()
 
+  const allRoutes = union(routes, pluginRoutes).filter(route => !route.disable && !route.hide)
+  const groups = allRoutes.reduce(
+    (entryMap, e) => entryMap.set(e.category, [...(entryMap.get(e.category) || []), e]),
+    new Map(),
+  )
+  const groupArray = Array.from(groups.entries())
+  console.info(groupArray)
+
   return (
-    <Stack width='full' flex='1 1 0%' {...rest}>
-      {union(routes, pluginRoutes)
-        .filter(route => !route.disable && !route.hide)
-        .map((item, idx) => {
-          return (
-            <MainNavLink
-              isCompact={isCompact}
-              as={ReactRouterLink}
-              key={idx}
-              leftIcon={item.icon}
-              href={item.path}
-              to={item.path}
-              label={translate(item.label)}
-              aria-label={translate(item.label)}
-              data-test={`navigation-${item.label.split('.')[1]}-button`}
-            />
-          )
-        })}
+    <Stack width='full' flex='1 1 0%' spacing={6} {...rest}>
+      {groupArray.map((group, id) => {
+        const name = group[0]
+        const values = group[1]
+        return (
+          <Stack key={id}>
+            {name && (
+              <Text
+                px={4}
+                color='gray.500'
+                fontSize='xs'
+                textTransform='uppercase'
+                fontWeight='bold'
+                letterSpacing='wider'
+                display={{ base: 'none', xl: 'block' }}
+                translation={`navBar.${name}`}
+              />
+            )}
+            {values.map((item: Route, idx: number) => (
+              <MainNavLink
+                isCompact={isCompact}
+                as={ReactRouterLink}
+                key={idx}
+                leftIcon={item.icon}
+                href={item.path}
+                to={item.path}
+                label={translate(item.label)}
+                aria-label={translate(item.label)}
+                data-test={`navigation-${item.label.split('.')[1]}-button`}
+              />
+            ))}
+          </Stack>
+        )
+      })}
     </Stack>
   )
 }

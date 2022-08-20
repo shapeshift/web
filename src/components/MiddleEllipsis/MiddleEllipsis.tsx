f4b4a52dc7e2cfff22112ccf75c6a2ad676f8db9
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-08-20 20:14:30 +0200

    fix: hanging native wallet label (#2498)
    * fix: native wallet label
    
    * fix: hanging native wallet label
    
    Co-authored-by: 0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>

--- src/components/MiddleEllipsis/MiddleEllipsis.tsx
+++ src/components/MiddleEllipsis/MiddleEllipsis.tsx
@@ -2,14 +2,14 @@ import { Flex, FlexProps } from '@chakra-ui/react'
 import { firstFourLastFour } from 'state/slices/portfolioSlice/utils'
 
 type MiddleEllipsisProps = {
-  address: string
+  value: string
   shouldShorten?: boolean
 } & FlexProps
 
-export const MiddleEllipsis = ({ address, shouldShorten = true, ...rest }: MiddleEllipsisProps) => {
+export const MiddleEllipsis = ({ value, shouldShorten = true, ...rest }: MiddleEllipsisProps) => {
   return (
     <Flex alignItems='center' whiteSpace='nowrap' {...rest}>
-      <span style={{ lineHeight: 1 }}>{shouldShorten ? firstFourLastFour(address) : address}</span>
+      <span style={{ lineHeight: 1 }}>{shouldShorten ? firstFourLastFour(value) : value}</span>
     </Flex>
   )
 }

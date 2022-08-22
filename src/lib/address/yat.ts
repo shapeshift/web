fdcae72ac29369f68d5a30424a235981d05feddf
a-hirsch <a.hirsch@digitalcolliers.com>
2022-08-22 16:13:10 +0200

    chore: move yat resolver to abstraction

--- src/lib/address/yat.ts
+++ src/lib/address/yat.ts
@@ -3,17 +3,26 @@ import { toChecksumAddress } from 'web3-utils'
 
 import { ResolveYat, ValidateYat } from './address'
 
+type YatResponse = {
+  result: {
+    data: string
+    hash: string
+    tag: string
+  }[]
+  error: string | null
+}
+
 export const validateYat: ValidateYat = async ({ value }) =>
   /^\p{Extended_Pictographic}{1,5}$/u.test(value)
 
 export const resolveYat: ResolveYat = async args => {
-  const { data } = await axios.get(
+  const { data } = await axios.get<YatResponse>(
     `https://octopus-app-mkjlj.ondigitalocean.app/emoji_id/${args.value}`,
   )
   if (data.error) return ''
 
   const found = data.result.find(
-    (emoji: { data: string; tag: string; hash: string }) => emoji.tag === '0x1004', // 0x1004 is eth address
+    emoji => emoji.tag === '0x1004', // 0x1004 is eth address
   )
 
   if (!found) return ''

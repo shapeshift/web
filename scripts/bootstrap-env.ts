8f545545bd6bd788153407aa88a6a34b9f48c780
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2022-08-22 19:26:12 -0600

    feat: proof of concept composable configuration as code

---
+++ scripts/bootstrap-env.ts
@@ -0,0 +1,37 @@
+// build tool
+/* eslint-disable no-console */
+
+// use the same source of truth as runtime config management in the app
+import assert from 'assert'
+import dotenv from 'dotenv'
+
+/**
+ * these are injected by the hosted environment, i.e. fleek and should not be modified
+ * the existence of an injected environment variable indicates it has been set manually in the UI
+ * and should take precedence over configuration as code
+ */
+import { reactAppEnvVars } from '../src/env'
+
+const VALID_ENVIRONMENTS = ['develop', 'release', 'app', 'private'] as const
+type ValidEnvironment = typeof VALID_ENVIRONMENTS[number]
+
+const args = process.argv.slice(2)
+
+assert(args.length === 1, 'yarn env must be called with exactly one environment argument')
+const specifiedEnvironment = args[0]
+assert(
+  VALID_ENVIRONMENTS.includes(specifiedEnvironment as ValidEnvironment),
+  `yarn env must be called with a valid environment. expected one of ${JSON.stringify(
+    VALID_ENVIRONMENTS,
+    null,
+    2,
+  )} received ${specifiedEnvironment}`,
+)
+
+// always load the base config first
+dotenv.config({ path: `.base.env` }) // relative to root of repo
+
+// load the environment specific .env file
+dotenv.config({ path: `.${specifiedEnvironment}.env` })
+
+console.log(reactAppEnvVars(process.env))
--- src/env/index.ts
+++ src/env/index.ts
@@ -1,7 +1,10 @@
 // This is a Jest-friendly stub for use while testing. It will be swapped out for webpack.ts during build. See
 // react-app-rewired.config.js for details.
 
+export const reactAppEnvVars = (
+  processEnv: Record<string, string | undefined>,
+): Record<string, string | undefined> =>
+  Object.fromEntries(Object.entries(processEnv).filter(([k]) => k.startsWith('REACT_APP_')))
+
 // eslint-disable-next-line import/no-default-export
-export default Object.fromEntries(
-  Object.entries(process.env).filter(([k]) => k.startsWith('REACT_APP_')),
-)
+export default reactAppEnvVars(process.env)

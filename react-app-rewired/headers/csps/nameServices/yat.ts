5603d9247d6870da4fa0949784faa69004d8c470
a-hirsch <a.hirsch@digitalcolliers.com>
2022-08-23 13:18:48 +0200

    feat: add yat proxy context comment

--- react-app-rewired/headers/csps/nameServices/yat.ts
+++ react-app-rewired/headers/csps/nameServices/yat.ts
@@ -1,5 +1,6 @@
 import type { Csp } from '../../types'
 
+// Simple proxy server to avoid cors and security error thrown by yat when calling the api directly
 export const csp: Csp = {
   'connect-src': ['https://octopus-app-mkjlj.ondigitalocean.app'],
 }

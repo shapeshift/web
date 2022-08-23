ec72665a368a799c12ecdfb3804589f97ee61770
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-08-23 20:14:23 +0200

    chore: add RTK docs comment

--- src/state/apis/const.ts
+++ src/state/apis/const.ts
@@ -1,5 +1,6 @@
 import { fakeBaseQuery } from '@reduxjs/toolkit/dist/query/react'
 
+// https://redux-toolkit.js.org/rtk-query/api/createApi#parameters
 export const BASE_RTK_CREATE_API_CONFIG = {
   // not actually used, only used to satisfy createApi, we use a custom queryFn
   baseQuery: fakeBaseQuery(),

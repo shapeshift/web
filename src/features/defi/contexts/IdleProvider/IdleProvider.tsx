f8722f089f6ea13f5013ae1e1df3fecbf840cf5e
Samster91 <samster91>
2022-08-25 19:18:55 +0200

    idle investor singleton

--- src/features/defi/contexts/IdleProvider/IdleProvider.tsx
+++ src/features/defi/contexts/IdleProvider/IdleProvider.tsx
@@ -1,16 +1,14 @@
-import type { ChainAdapter } from '@shapeshiftoss/chain-adapters'
 import { IdleInvestor } from '@shapeshiftoss/investor-idle'
-import { KnownChainIds } from '@shapeshiftoss/types'
-import { getConfig } from 'config'
 import React, { PropsWithChildren, useContext, useEffect, useState } from 'react'
-import { getChainAdapterManager } from 'context/PluginProvider/chainAdapterSingleton'
 import { selectFeatureFlags } from 'state/slices/selectors'
 import { useAppSelector } from 'state/store'
+
+import { getIdleInvestor } from './idleInvestorSingleton'
 
 type IdleContextProps = {
   loading: boolean
   enabled: boolean
-  idle: IdleInvestor | null
+  idleInvestor: IdleInvestor | null
 }
 
 const IdleContext = React.createContext<IdleContextProps | null>(null)
@@ -22,33 +20,33 @@ export const useIdle = () => {
 }
 
 export const IdleProvider: React.FC<PropsWithChildren> = ({ children }) => {
-  const [idle, setIdle] = useState<IdleInvestor | null>(null)
+  const [idleInvestor, setIdle] = useState<IdleInvestor | null>(null)
   const [loading, setLoading] = useState<boolean>(false)
-  const chainAdapterManager = getChainAdapterManager()
   const featureFlags = useAppSelector(selectFeatureFlags)
-  const enabled = featureFlags.IdleFinance
+  const isIdleEnabled = featureFlags.IdleFinance
 
   useEffect(() => {
     ;(async () => {
       try {
-        if (!chainAdapterManager.has(KnownChainIds.EthereumMainnet) || !enabled) return
+        if (!isIdleEnabled) return
         setLoading(true)
-        const chainAdapter = chainAdapterManager.get(
-          KnownChainIds.EthereumMainnet,
-        ) as ChainAdapter<KnownChainIds.EthereumMainnet>
-        const idleInvestor = new IdleInvestor({
-          chainAdapter,
-          providerUrl: getConfig().REACT_APP_ETHEREUM_NODE_URL,
-        })
-        await idleInvestor.initialize()
-        setIdle(idleInvestor)
+
+        const investor = getIdleInvestor()
+        if (!investor) throw new Error('No Investor')
+
+        await investor.initialize()
+        setIdle(investor)
       } catch (error) {
         console.error('IdleManager: error', error)
       } finally {
         setLoading(false)
       }
     })()
-  }, [chainAdapterManager, enabled])
+  }, [isIdleEnabled])
 
-  return <IdleContext.Provider value={{ idle, loading, enabled }}>{children}</IdleContext.Provider>
+  return (
+    <IdleContext.Provider value={{ idleInvestor, loading, enabled: isIdleEnabled }}>
+      {children}
+    </IdleContext.Provider>
+  )
 }

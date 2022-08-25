1290c845c0c1229d81d974ebd4e3a1083d899484
reallybeard <89934888+reallybeard@users.noreply.github.com>
2022-08-25 16:36:27 -0500

    Update Debugging.tsx

--- src/pages/Flags/Debugging.tsx
+++ src/pages/Flags/Debugging.tsx
@@ -72,7 +72,7 @@ export const Debugging = () => {
           </Row>
           <Row alignItems='center'>
             <Row.Label>Pendo visitor ID</Row.Label>
-            <Row.Value display='flex' gap={4}>
+            <Row.Value display='flex' gap={4} alignItems='center'>
               <RawText>{visitorId}</RawText>
               <IconButton
                 aria-label='Copy'

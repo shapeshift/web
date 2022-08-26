38a5382f76b2e909c73a8ae9cb0fea3ef91c220d
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2022-08-26 15:29:05 -0600

    feat: dynamic receive address account number

--- src/state/slices/portfolioSlice/selectors.ts
+++ src/state/slices/portfolioSlice/selectors.ts
@@ -143,13 +143,12 @@ export const selectPortfolioAccountMetadata = (state: ReduxState): AccountMetada
 export const selectBIP44ParamsByAccountId = createSelector(
   selectPortfolioAccountMetadata,
   selectAccountIdParamFromFilter,
-  (accountMetadata, accountId): BIP44Params | undefined =>
-    accountMetadata?.[accountId]?.bip44Params,
+  (accountMetadata, accountId): BIP44Params => accountMetadata[accountId]?.bip44Params,
 )
 
 export const selectAccountNumberByAccountId = createSelector(
   selectBIP44ParamsByAccountId,
-  (bip44Params): number | undefined => bip44Params?.accountNumber,
+  (bip44Params): number => bip44Params.accountNumber,
 )
 
 export const selectAccountTypeByAccountId = createSelector(

6295f5545ec5b61044513b1f6f4251915c81b593
reallybeard <89934888+reallybeard@users.noreply.github.com>
2022-08-26 11:47:21 -0500

    Update src/plugins/pendo/components/OptInModal/OptInModalBody.tsx
    Co-authored-by: gomes <17035424+gomesalexandre@users.noreply.github.com>

--- src/plugins/pendo/components/OptInModal/OptInModalBody.tsx
+++ src/plugins/pendo/components/OptInModal/OptInModalBody.tsx
@@ -58,7 +58,7 @@ export const OptInModalBody: React.FC<OptInModalProps> = ({ onContinue }) => {
   }
 
   return enabled ? (
-    <LoadingBody isLoaded={typeof consent !== 'boolean'}>
+    <LoadingBody isLoaded={consent}>
       <ModalBody py={8}>
         <OptInIcon mb={4} />
         <Text

c63f82b1b2767f13a6b58c1e469bf65174b7c065
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2022-08-31 20:50:26 +1000

    refactor: make `getDefaultPair` a pure function & rename (#2618)
    ## Description
    
    In paving the way for a `tradeRoutes` refactor, this PR makes the `getDefaultPair` callback a pure function.
    
    ## Notice
    
    - [x] Have you followed the guidelines in our [Contributing]('https://github.com/shapeshift/web/CONTRIBUTING.md) guide?
    - [x] Have you checked to ensure there aren't other open [Pull Requests](https://github.com/shapeshift/web/pulls) for the same update/change?
    
    ## Pull Request Type
    
    - [ ] :bug: Bug fix (Non-breaking Change: Fixes an issue)
    - [x] :hammer_and_wrench: Chore (Non-breaking Change: Doc updates, pkg upgrades, typos, etc..)
    - [ ] :nail_care: New Feature (Breaking/Non-breaking Change)
    
    ## Issue (if applicable)
    
    Contributes to https://github.com/shapeshift/web/issues/2556
    
    ## Risk
    
    Small, no logic changes.
    
    ## Testing
    
    ### Engineering
    
    If default assets still load when connecting to a wallet, loading a new page etc we are 🛍️ 
    
    ### Operations
    
    If default assets still load when connecting to a wallet, loading a new page etc we are 🛍️ 
    
    ## Screenshots (if applicable)
    
    N/A

--- src/components/Trade/hooks/useSwapper/useSwapperV2.ts
+++ src/components/Trade/hooks/useSwapper/useSwapperV2.ts
@@ -1,11 +1,4 @@
 import { type Asset } from '@shapeshiftoss/asset-service'
-import {
-  type ChainId,
-  avalancheAssetId,
-  cosmosAssetId,
-  ethAssetId,
-  osmosisAssetId,
-} from '@shapeshiftoss/caip'
 import { SwapperManager } from '@shapeshiftoss/swapper'
 import { KnownChainIds } from '@shapeshiftoss/types'
 import { useCallback, useEffect, useState } from 'react'
@@ -14,7 +7,6 @@ import { useSelector } from 'react-redux'
 import { getSwapperManager } from 'components/Trade/hooks/useSwapper/swapperManager'
 import { filterAssetsByIds } from 'components/Trade/hooks/useSwapper/utils'
 import { type TradeState } from 'components/Trade/types'
-import { useFeatureFlag } from 'hooks/useFeatureFlag/useFeatureFlag'
 import { selectAssetIds } from 'state/slices/assetsSlice/selectors'
 import { selectFeatureFlags } from 'state/slices/preferencesSlice/selectors'
 
@@ -67,29 +59,9 @@ export const useSwapper = () => {
     [swapperManager, sellTradeAsset],
   )
 
-  const osmosisEnabled = useFeatureFlag('Osmosis')
-  const getDefaultPair = useCallback(
-    (buyAssetChainId: ChainId | undefined) => {
-      const ethFoxPair = [ethAssetId, 'eip155:1/erc20:0xc770eefad204b5180df6a14ee197d99d808ee52d']
-      switch (buyAssetChainId) {
-        case KnownChainIds.AvalancheMainnet:
-          return [avalancheAssetId, 'eip155:43114/erc20:0x49d5c2bdffac6ce2bfdb6640f4f80f226bc10bab']
-        case KnownChainIds.CosmosMainnet:
-          return osmosisEnabled ? [cosmosAssetId, osmosisAssetId] : ethFoxPair
-        case KnownChainIds.OsmosisMainnet:
-          return osmosisEnabled ? [osmosisAssetId, cosmosAssetId] : ethFoxPair
-        case KnownChainIds.EthereumMainnet:
-        default:
-          return ethFoxPair
-      }
-    },
-    [osmosisEnabled],
-  )
-
   return {
     getSupportedSellableAssets,
     getSupportedBuyAssetsFromSellAsset,
     swapperManager,
-    getDefaultPair,
   }
 }

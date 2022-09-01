3de1218b666403f753987f74d9179ab8d493fdd6
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2022-09-01 14:18:26 +1000

    fix: default asset pair issues

--- src/components/Trade/TradeRoutes/TradeRoutes.tsx
+++ src/components/Trade/TradeRoutes/TradeRoutes.tsx
@@ -4,6 +4,7 @@ import { AnimatePresence } from 'framer-motion'
 import { Redirect, Route, RouteComponentProps, Switch, useLocation } from 'react-router-dom'
 import { Approval } from 'components/Approval/Approval'
 import { SelectAccount } from 'components/Trade/SelectAccount'
+import { useDefaultAssetsService } from 'components/Trade/services/useDefaultAssetsService'
 import { useFeatureFlag } from 'hooks/useFeatureFlag/useFeatureFlag'
 
 import { useSwapper } from '../hooks/useSwapper/useSwapperV2'
@@ -16,14 +17,11 @@ import { TradeRoutePaths } from '../types'
 
 export const entries = ['/send/details', '/send/confirm']
 
-type TradeRoutesProps = {
-  defaultBuyAssetId?: AssetId
-}
-
-export const TradeRoutes = ({ defaultBuyAssetId }: TradeRoutesProps) => {
-  const location = useLocation()
-  const { handleAssetClick } = useTradeRoutes(defaultBuyAssetId)
+export const TradeRoutes = (defaultBuyAssetId?: AssetId) => {
+  useDefaultAssetsService(defaultBuyAssetId)
   const { getSupportedSellableAssets, getSupportedBuyAssetsFromSellAsset } = useSwapper()
+  const location = useLocation()
+  const { handleAssetClick } = useTradeRoutes()
 
   const isSwapperV2 = useFeatureFlag('SwapperV2')
   const TradeInputComponent = isSwapperV2 ? TradeInputV2 : TradeInputV1
---
+++ src/components/Trade/services/useDefaultAssetsService.ts
@@ -0,0 +1,93 @@
+/*
+The Default Asset Service is responsible for populating the trade widget with initial assets.
+It mutates the buyTradeAsset, sellTradeAsset, amount, and action properties of TradeState.
+*/
+import { skipToken } from '@reduxjs/toolkit/query'
+import {
+  AssetId,
+  cosmosChainId,
+  ethChainId,
+  fromAssetId,
+  osmosisChainId,
+} from '@shapeshiftoss/caip'
+import { supportsCosmos, supportsETH, supportsOsmosis } from '@shapeshiftoss/hdwallet-core'
+import { KnownChainIds } from '@shapeshiftoss/types'
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useFormContext } from 'react-hook-form'
+import { useSelector } from 'react-redux'
+import { getDefaultAssetIdPairByChainId } from 'components/Trade/hooks/useSwapper/utils'
+import { TradeAmountInputField, TradeState } from 'components/Trade/types'
+import { useEvm } from 'hooks/useEvm/useEvm'
+import { useWallet } from 'hooks/useWallet/useWallet'
+import { useGetUsdRateQuery } from 'state/apis/swapper/swapperApi'
+import { selectAssets } from 'state/slices/assetsSlice/selectors'
+import { selectFeatureFlags } from 'state/slices/preferencesSlice/selectors'
+import { useAppSelector } from 'state/store'
+
+export const useDefaultAssetsService = (routeBuyAssetId?: AssetId) => {
+  type UsdRateQueryInput = Parameters<typeof useGetUsdRateQuery>
+  type UsdRateInputArg = UsdRateQueryInput[0]
+
+  const {
+    state: { wallet },
+  } = useWallet()
+  const { connectedEvmChainId } = useEvm()
+  const { setValue } = useFormContext<TradeState<KnownChainIds>>()
+  const [buyAssetFiatRateArgs, setBuyAssetFiatRateArgs] = useState<UsdRateInputArg>(skipToken)
+  const [defaultAssetIdPair, setDefaultAssetIdPair] = useState<[AssetId, AssetId]>()
+
+  const featureFlags = useAppSelector(selectFeatureFlags)
+  const assets = useSelector(selectAssets)
+
+  // If the wallet is connected to a chain, use that ChainId
+  // Else, return a prioritized ChainId based on the wallet's supported chains
+  const maybeWalletChainId = useMemo(() => {
+    if (connectedEvmChainId) return connectedEvmChainId
+    if (!wallet) return
+    if (supportsETH(wallet)) return ethChainId
+    if (supportsCosmos(wallet)) return cosmosChainId
+    if (supportsOsmosis(wallet)) return osmosisChainId
+  }, [connectedEvmChainId, wallet])
+
+  useEffect(() => {
+    // Use the ChainId of the route's AssetId if we have one, else use the wallet's fallback ChainId
+    const maybeBuyAssetChainId = routeBuyAssetId
+      ? fromAssetId(routeBuyAssetId).chainId
+      : maybeWalletChainId
+    const [defaultSellAssetId, defaultBuyAssetId] = getDefaultAssetIdPairByChainId(
+      maybeBuyAssetChainId,
+      featureFlags,
+    )
+    setDefaultAssetIdPair([defaultSellAssetId, defaultBuyAssetId])
+  }, [featureFlags, maybeWalletChainId, routeBuyAssetId])
+
+  const { data: buyAssetFiatRateData } = useGetUsdRateQuery(buyAssetFiatRateArgs, {
+    pollingInterval: 30000,
+    selectFromResult: ({ data }) => ({
+      data: data?.usdRate,
+    }),
+  })
+
+  useEffect(
+    () =>
+      defaultAssetIdPair &&
+      setBuyAssetFiatRateArgs({
+        buyAssetId: defaultAssetIdPair[1],
+        sellAssetId: defaultAssetIdPair[0],
+        rateAssetId: defaultAssetIdPair[1],
+      }),
+    [defaultAssetIdPair],
+  )
+
+  const setInitialAssets = useCallback(() => {
+    // TODO: update Swapper to have an proper way to validate a pair is supported.
+    if (buyAssetFiatRateData && defaultAssetIdPair) {
+      setValue('buyTradeAsset.asset', assets[defaultAssetIdPair[1]])
+      setValue('sellTradeAsset.asset', assets[defaultAssetIdPair[0]])
+      setValue('action', TradeAmountInputField.SELL_CRYPTO)
+      setValue('amount', '0')
+    }
+  }, [assets, buyAssetFiatRateData, defaultAssetIdPair, setValue])
+
+  useEffect(() => setInitialAssets(), [setInitialAssets])
+}

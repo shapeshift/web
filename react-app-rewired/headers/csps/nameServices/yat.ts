146ce0e56652eab68ff1cd42f305c7efa155c4c9
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-09-07 18:34:10 +0200

    feat: rm comment about reverse server

--- react-app-rewired/headers/csps/nameServices/yat.ts
+++ react-app-rewired/headers/csps/nameServices/yat.ts
@@ -1,6 +1,5 @@
 import type { Csp } from '../../types'
 
-// Simple proxy server to avoid cors and security error thrown by yat when calling the api directly
 export const csp: Csp = {
   'connect-src': [process.env.REACT_APP_YAT_NODE_URL!],
 }

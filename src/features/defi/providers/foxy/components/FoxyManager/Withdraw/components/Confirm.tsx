f6e7c33e7a4222e074ec32f74aa186200fdcc032
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-09-08 22:25:20 +0200

    feat: use selectBIP44ParamsByAccountId

--- src/features/defi/providers/foxy/components/FoxyManager/Withdraw/components/Confirm.tsx
+++ src/features/defi/providers/foxy/components/FoxyManager/Withdraw/components/Confirm.tsx
@@ -28,7 +28,6 @@ import {
   selectAssetById,
   selectBIP44ParamsByAccountId,
   selectMarketDataById,
-  selectPortfolioAccountMetadataByAccountId,
   selectPortfolioCryptoHumanBalanceByAssetId,
 } from 'state/slices/selectors'
 import { useAppSelector } from 'state/store'

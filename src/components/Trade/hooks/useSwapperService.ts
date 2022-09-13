5246b748fa9a6d171208694b38bfd1716ce455da
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2022-09-13 17:51:10 +1000

    chore: remove console log debugging

--- src/components/Trade/hooks/useSwapperService.ts
+++ src/components/Trade/hooks/useSwapperService.ts
@@ -1,17 +1,12 @@
-import { useFormContext } from 'react-hook-form'
 import { useAccountsService } from 'components/Trade/hooks/useAccountsService'
 import { useFeesService } from 'components/Trade/hooks/useFeesService'
 import { useFiatRateService } from 'components/Trade/hooks/useFiatRateService'
 import { useTradeQuoteService } from 'components/Trade/hooks/useTradeQuoteService'
-import type { TS } from 'components/Trade/types'
 
 /*
 The Swapper Service is responsible for reacting to changes to the Trade form and updating state accordingly.
 */
 export const useSwapperService = () => {
-  const { getValues } = useFormContext<TS>()
-  console.log('xxx useSwapperService getValues', getValues())
-
   // Initialize child services
   useFiatRateService()
   useTradeQuoteService()

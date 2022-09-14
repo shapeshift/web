70fa8697550a537dac5d1d93d7b029eded7c36e6
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2022-09-14 11:46:59 -0600

    chore: use hdwallet multi account utility function

--- src/pages/Accounts/Accounts.tsx
+++ src/pages/Accounts/Accounts.tsx
@@ -1,36 +1,16 @@
 import { AddIcon } from '@chakra-ui/icons'
 import { Button, Heading, List, Stack } from '@chakra-ui/react'
-import type { HDWallet } from '@shapeshiftoss/hdwallet-core'
 import { useEffect, useMemo, useState } from 'react'
 import { useTranslate } from 'react-polyglot'
 import { useSelector } from 'react-redux'
 import { Main } from 'components/Layout/Main'
 import { Text } from 'components/Text'
-import { KeyManager } from 'context/WalletProvider/KeyManager'
-import { getLocalWalletType } from 'context/WalletProvider/local-wallet'
 import { useFeatureFlag } from 'hooks/useFeatureFlag/useFeatureFlag'
 import { useModal } from 'hooks/useModal/useModal'
 import { useWallet } from 'hooks/useWallet/useWallet'
 import { selectIsTxHistoryLoading, selectPortfolioChainIdsSortedFiat } from 'state/slices/selectors'
 
 import { ChainRow } from './components/ChainRow'
-
-// TODO(0xdef1cafe): delete me once hdwallet supports this concept
-const isMultiAccountSupportedWallet = (wallet: HDWallet | null): boolean => {
-  if (!wallet) return false
-  switch (getLocalWalletType()) {
-    /**
-     * currently - these are the only wallets that support the concept of multi account.
-     * there may be some WalletConnect wallets that do, but we can't interrogate the underlying wallet
-     * that WalletConnect is proxying to us.
-     */
-    case KeyManager.Native:
-    case KeyManager.KeepKey:
-      return true
-    default:
-      return false
-  }
-}
 
 const AccountHeader = () => {
   const translate = useTranslate()
@@ -43,7 +23,7 @@ const AccountHeader = () => {
 
   useEffect(() => {
     if (!wallet) return
-    setIsMultiChainWallet(isMultiAccountSupportedWallet(wallet))
+    setIsMultiChainWallet(wallet.supportsBip44Accounts())
   }, [wallet])
 
   const { addAccount } = useModal()

79b493449f37bc5d7cdfd980609c257cf748a788
southbear <108498243+southbears@users.noreply.github.com>
2022-09-16 00:21:06 +0200

    fix: translate pageInput text

--- src/plugins/walletConnectToDapps/components/PageInput.tsx
+++ src/plugins/walletConnectToDapps/components/PageInput.tsx
@@ -2,14 +2,16 @@ import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons'
 import { HStack, IconButton, Text, useColorModeValue } from '@chakra-ui/react'
 import type { FC } from 'react'
 import { useCallback } from 'react'
+import { useTranslate } from 'react-polyglot'
 
-interface Props {
+type Props = {
   value: number
   max: number
   onChange(value: number): void
 }
 
 export const PageInput: FC<Props> = ({ value, max, onChange }) => {
+  const translate = useTranslate()
   const nextPage = useCallback(() => onChange(value + 1), [onChange, value])
   const prevPage = useCallback(() => onChange(value - 1), [onChange, value])
 
@@ -22,7 +24,7 @@ export const PageInput: FC<Props> = ({ value, max, onChange }) => {
     >
       <IconButton
         colorScheme='gray'
-        aria-label='Previous Page'
+        aria-label={translate('plugins.walletConnectToDapps.pageInput.prevPage')}
         variant='ghost'
         icon={<ArrowBackIcon />}
         disabled={value <= 0}
@@ -30,10 +32,13 @@ export const PageInput: FC<Props> = ({ value, max, onChange }) => {
         onClick={prevPage}
       />
       <Text textAlign='center' minWidth={16}>
-        {value + 1} of {max + 1}
+        {translate('plugins.walletConnectToDapps.pageInput.label', {
+          current: value + 1,
+          total: max + 1,
+        })}
       </Text>
       <IconButton
-        aria-label='Next Page'
+        aria-label={translate('plugins.walletConnectToDapps.pageInput.nextPage')}
         variant='ghost'
         icon={<ArrowForwardIcon />}
         disabled={value >= max}

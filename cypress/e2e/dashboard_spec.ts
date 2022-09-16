323e0b5c7e8f5960628e2f10299c8662c35b0719
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2022-09-16 19:03:38 +1000

    chore: remove trade test for now

--- cypress/e2e/dashboard_spec.ts
+++ cypress/e2e/dashboard_spec.ts
@@ -11,15 +11,4 @@ describe('The Dashboard', () => {
     cy.getBySel('connect-wallet-button').should('be.visible').should('be.enabled')
     cy.getBySel('connect-demo-wallet-button').click()
   })
-
-  it('Trade widget works as expected', function () {
-    cy.getBySel('token-row-sell-token-button').should('have.text', 'ETH')
-    cy.getBySel('token-row-buy-token-button').should('have.text', 'FOX')
-    cy.getBySel('swap-assets-button').click()
-    cy.getBySel('token-row-sell-token-button').should('have.text', 'FOX')
-    cy.getBySel('token-row-buy-token-button').should('have.text', 'ETH')
-    cy.getBySel('trade-form-token-input-row-sell').find('input').type('300')
-    cy.getBySel('trade-form-preview-button').should('be.disabled')
-    cy.getBySel('trade-form-token-input-row-sell').find('input').type('0')
-  })
 })

3616fb629bea3dd4257270bdeaa79f491be7c848
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-09-22 00:42:19 +0200

    fix(foxy): add missing withdraw accountId filter (#2816)

--- src/features/defi/providers/foxy/components/FoxyManager/Withdraw/components/Withdraw.tsx
+++ src/features/defi/providers/foxy/components/FoxyManager/Withdraw/components/Withdraw.tsx
@@ -74,7 +74,9 @@ export const Withdraw: React.FC<
   const stakingAsset = useAppSelector(state => selectAssetById(state, stakingAssetId))
 
   // user info
-  const balance = useAppSelector(state => selectPortfolioCryptoBalanceByFilter(state, { assetId }))
+
+  const filter = useMemo(() => ({ assetId, accountId: accountId ?? '' }), [assetId, accountId])
+  const balance = useAppSelector(state => selectPortfolioCryptoBalanceByFilter(state, filter))
 
   const cryptoAmountAvailable = bnOrZero(bn(balance).div(bn(10).pow(asset?.precision)))
   const fiatAmountAvailable = bnOrZero(bn(cryptoAmountAvailable).times(bnOrZero(marketData?.price)))

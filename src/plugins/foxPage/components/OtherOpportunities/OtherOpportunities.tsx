e0574efc10ac665b7365291e410f8b8df1738060
reallybeard <89934888+reallybeard@users.noreply.github.com>
2022-09-23 12:13:25 -0500

    Update OtherOpportunities.tsx

--- src/plugins/foxPage/components/OtherOpportunities/OtherOpportunities.tsx
+++ src/plugins/foxPage/components/OtherOpportunities/OtherOpportunities.tsx
@@ -1,6 +1,7 @@
 import { Flex } from '@chakra-ui/layout'
 import { Accordion } from '@chakra-ui/react'
 import type { OpportunitiesBucket } from 'plugins/foxPage/FoxCommon'
+import { useMemo } from 'react'
 import { Card } from 'components/Card/Card'
 import { Text } from 'components/Text/Text'
 
@@ -17,6 +18,23 @@ export const OtherOpportunities: React.FC<OtherOpportunitiesProps> = ({
   description,
   opportunities,
 }) => {
+  const renderRows = useMemo(() => {
+    return opportunities.map(opportunitiesBucket => {
+      const { opportunities } = opportunitiesBucket
+      if (!opportunities.length || opportunities.every(opportunity => opportunity.isDisabled))
+        return null
+
+      return (
+        <FoxOtherOpportunityPanel
+          key={opportunitiesBucket.type}
+          opportunities={opportunities}
+          title={opportunitiesBucket.title}
+          type={opportunitiesBucket.type}
+        />
+      )
+    })
+  }, [opportunities])
+
   return (
     <Card display='block' width='full' borderRadius={8}>
       <Card.Header pb={0} mb={4}>
@@ -26,20 +44,7 @@ export const OtherOpportunities: React.FC<OtherOpportunitiesProps> = ({
         <Text translation={description} color='gray.500' />
       </Card.Header>
       <Accordion defaultIndex={[0]} allowToggle allowMultiple>
-        {opportunities.map(opportunitiesBucket => {
-          const { opportunities } = opportunitiesBucket
-          if (!opportunities.length || opportunities.every(opportunity => opportunity.isDisabled))
-            return null
-
-          return (
-            <FoxOtherOpportunityPanel
-              key={opportunitiesBucket.type}
-              opportunities={opportunities}
-              title={opportunitiesBucket.title}
-              type={opportunitiesBucket.type}
-            />
-          )
-        })}
+        {renderRows}
       </Accordion>
     </Card>
   )

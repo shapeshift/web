72ac1fd327e576fcfaf8e24228b46f0e89c44ab3
reallybeard <89934888+reallybeard@users.noreply.github.com>
2022-09-26 23:56:49 -0500

    Update FiatRamps.tsx

--- src/components/Layout/Header/NavBar/FiatRamps.tsx
+++ src/components/Layout/Header/NavBar/FiatRamps.tsx
@@ -1,4 +1,6 @@
 import type { BoxProps } from '@chakra-ui/react'
+import { useColorModeValue } from '@chakra-ui/react'
+import { keyframes } from '@chakra-ui/react'
 import { Box, IconButton, Tooltip } from '@chakra-ui/react'
 import { FaCreditCard } from 'react-icons/fa'
 import { useTranslate } from 'react-polyglot'
@@ -18,12 +20,30 @@ export const FiatRamps = (props: FiatRampsProps) => {
   const handleWalletModalOpen = () =>
     dispatch({ type: WalletActions.SET_WALLET_MODAL, payload: true })
 
+  const bg = useColorModeValue(
+    'linear-gradient(126deg, var(--chakra-colors-gray-100) 0%, var(--chakra-colors-gray-100) 40%, var(--chakra-colors-white) 50%, var(--chakra-colors-gray-100) 60%)',
+    'linear-gradient(126deg, var(--chakra-colors-gray-700) 0%, var(--chakra-colors-gray-700) 40%, var(--chakra-colors-gray-500) 50%, var(--chakra-colors-gray-700) 60%)',
+  )
+
+  const rainbow = keyframes`
+    0% {
+      background-position: 10% 0%;
+    }
+
+    100% {
+      background-position: 91% 100%;
+    }
+  `
+
   return (
     <Box {...props}>
       <Tooltip label={translate('fiatRamps.headerLabel')}>
         <IconButton
           icon={<FaCreditCard />}
           data-test='fiat-ramps-button'
+          bg={bg}
+          backgroundSize='300% 300%'
+          animation={`${rainbow} 3s ease infinite`}
           width='full'
           aria-label={translate('fiatRamps.headerLabel')}
           onClick={() => (isConnected ? fiatRamps.open({}) : handleWalletModalOpen())}

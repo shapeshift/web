6e90bd63c31e0fbb2ad09ca966ec3019f2eeb076
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-09-27 21:57:02 +0200

    feat(swapper): sellAssetAccountNumber -> bip44Params (#2877)
    * feat: sellAssetAccountNumber -> bip44Params
    
    * fix: ci
    
    * feat: bump swapper to 11.3.0
    
    * feat: use tilde range for swapper package.json entry
    
    * fix: tests
    
    * feat: cleanup
    
    * feat: consistently pass accountType in swapperv2 getAddress() calls
    
    * feat: swapper v1 metadata bip44Params / accountType and skip v1 tests
    
    * feat: buildTrade with accountType
    
    * feat: explicit accountType and bip44Params

--- src/components/Trade/hooks/useTradeQuoteService.tsx
+++ src/components/Trade/hooks/useTradeQuoteService.tsx
@@ -119,6 +119,16 @@ export const useTradeQuoteService = () => {
     selectPortfolioAccountMetadataByAccountId(state, sellAccountFilter),
   )
 
+  const sellAssetAccountIds = useAppSelector(state =>
+    selectPortfolioAccountIdsByAssetId(state, {
+      assetId: sellAsset?.assetId ?? '',
+    }),
+  )
+  const sellAccountFilter = { accountId: sellAssetAccountId ?? sellAssetAccountIds[0] }
+  const sellAccountMetadata = useAppSelector(state =>
+    selectPortfolioAccountMetadataByAccountId(state, sellAccountFilter),
+  )
+
   // API
   const { data: tradeQuote, isLoading: isLoadingTradeQuote } = useGetTradeQuoteQuery(
     tradeQuoteArgs,

4bbe135abbfd103460edadd21629aeec7a149e3c
southbears <108498243+southbears@users.noreply.github.com>
2022-09-30 22:30:01 +0200

    feat: dapp explorer (#2653)
    * add feature flag WalletConnectToDapps
    
    * updated link to CONTRIBUTING.md in PR tempalte
    
    * registered route for dapps
    
    * dapp banner component
    
    * script to scrape wallet connect's registry of dapps
    
    * dapp registry grid
    
    * search dapps in registry grid
    
    * reorganized file structure and added layout container
    
    * feat: feature flag wallet connect dapps plugin
    
    * feat: PageInput component
    
    * feat: add todos when clicking dapp exploration banner ctas
    
    * Update src/plugins/walletConnectToDapps/components/DappRegistryGrid.tsx
    
    Co-authored-by: gomes <17035424+gomesalexandre@users.noreply.github.com>
    
    * chore: pull request feedback
    
    * chore: lint
    
    * chore: pr comments
    
    * fix: lint
    
    * fix: translate pageInput text
    
    * feat: dapp explorer empty state
    
    Co-authored-by: gomes <17035424+gomesalexandre@users.noreply.github.com>
    Co-authored-by: 0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>

---
+++ src/plugins/walletConnectToDapps/components/DappRegistryGrid.tsx
@@ -0,0 +1,125 @@
+import { SearchIcon } from '@chakra-ui/icons'
+import {
+  Box,
+  Heading,
+  Image,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Link,
+  SimpleGrid,
+  Stack,
+  Text as PlainText,
+  VStack,
+} from '@chakra-ui/react'
+import type { FC } from 'react'
+import { useEffect, useMemo } from 'react'
+import { useForm, useWatch } from 'react-hook-form'
+import { Card } from 'components/Card/Card'
+import { Text } from 'components/Text'
+
+import type { RegistryItem } from '../types'
+import { PageInput } from './PageInput'
+
+const registryItems: RegistryItem[] = require('../registry.json')
+
+const PAGE_SIZE = 20
+
+export const DappRegistryGrid: FC = () => {
+  const { register, setValue, control } = useForm<{ search: string; page: number }>({
+    mode: 'onChange',
+    defaultValues: { search: '', page: 0 },
+  })
+
+  const search = useWatch({ control, name: 'search' })
+  const page = useWatch({ control, name: 'page' })
+  useEffect(() => setValue('page', 0), [search, setValue])
+
+  const filteredListings = useMemo(
+    () =>
+      registryItems.filter(
+        registryItem => !search || registryItem.name.toLowerCase().includes(search.toLowerCase()),
+      ),
+    [search],
+  )
+
+  const maxPage = Math.floor(filteredListings.length / PAGE_SIZE)
+
+  return (
+    <Box>
+      <Stack direction='row' alignItems='center' mb={4}>
+        <Heading flex={1} fontSize='2xl'>
+          <Text translation='plugins.walletConnectToDapps.registry.availableDapps' />
+        </Heading>
+        <Box>
+          <InputGroup>
+            <InputLeftElement pointerEvents='none'>
+              <SearchIcon color='gray.700' />
+            </InputLeftElement>
+            <Input
+              {...register('search')}
+              autoComplete='off'
+              type='text'
+              placeholder='Search'
+              pl={10}
+              variant='filled'
+            />
+          </InputGroup>
+        </Box>
+        <PageInput value={page} max={maxPage} onChange={value => setValue('page', value)} />
+      </Stack>
+      {!!filteredListings.length ? (
+        <SimpleGrid columns={{ lg: 4, sm: 2, base: 1 }} spacing={4}>
+          {filteredListings.slice(page * PAGE_SIZE, (page + 1) * PAGE_SIZE).map(listing => (
+            <Link key={listing.id} href={listing.homepage} isExternal>
+              <Box
+                borderRadius='lg'
+                p={2}
+                position='relative'
+                overflow='hidden'
+                _hover={{ opacity: 0.8, transition: 'opacity 0.2s ease-in-out' }}
+              >
+                <Image
+                  src={listing.image}
+                  style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                    filter: 'blur(20px)',
+                    opacity: 0.3,
+                    zIndex: -1,
+                  }}
+                />
+                <Stack direction='row' alignItems='center'>
+                  <Image borderRadius='full' boxSize='48px' m={2} src={listing.image} />
+                  <PlainText fontWeight='semibold'>{listing.name}</PlainText>
+                </Stack>
+              </Box>
+            </Link>
+          ))}
+        </SimpleGrid>
+      ) : (
+        <VStack alignItems='center' p={8} spacing={0}>
+          <Card
+            display='grid'
+            width={14}
+            height={14}
+            placeItems='center'
+            borderRadius='2xl'
+            borderWidth={0}
+            mb={4}
+          >
+            <SearchIcon color='gray.500' fontSize='xl' />{' '}
+          </Card>
+          <Text translation='common.noResultsFound' fontWeight='medium' fontSize='lg' />
+          <Text
+            translation='plugins.walletConnectToDapps.registry.emptyStateDescription'
+            color='gray.500'
+          />
+        </VStack>
+      )}
+    </Box>
+  )
+}

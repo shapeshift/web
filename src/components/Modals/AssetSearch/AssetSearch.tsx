42b516e2d70e91dc5daffe863489da46bb338c22
reallybeard <89934888+reallybeard@users.noreply.github.com>
2022-10-10 10:26:55 -0500

    Update AssetSearch.tsx

--- src/components/Modals/AssetSearch/AssetSearch.tsx
+++ src/components/Modals/AssetSearch/AssetSearch.tsx
@@ -5,12 +5,14 @@ import {
   ModalContent,
   ModalHeader,
   ModalOverlay,
+  useMediaQuery,
 } from '@chakra-ui/react'
 import type { Asset } from '@shapeshiftoss/asset-service'
 import { useTranslate } from 'react-polyglot'
 import { AssetSearch } from 'components/AssetSearch/AssetSearch'
 import { useModal } from 'hooks/useModal/useModal'
 import { useWindowSize } from 'hooks/useWindowSize/useWindowSize'
+import { breakpoints } from 'theme/theme'
 
 type AssetSearchModalProps = {
   onClick: (asset: Asset) => void
@@ -19,6 +21,7 @@ type AssetSearchModalProps = {
 
 export const AssetSearchModal = ({ onClick, filterBy }: AssetSearchModalProps) => {
   const translate = useTranslate()
+  const [isLargerThanMd] = useMediaQuery(`(min-width: ${breakpoints['md']})`, { ssr: false })
   const { height: windowHeight } = useWindowSize()
   const { assetSearch } = useModal()
   const { close, isOpen } = assetSearch
@@ -32,7 +35,7 @@ export const AssetSearchModal = ({ onClick, filterBy }: AssetSearchModalProps) =
     modalHeight = Math.min(Math.round((680 / windowHeight) * 100), 80)
   }
   return (
-    <Modal isOpen={isOpen} onClose={close} isCentered trapFocus={false}>
+    <Modal isOpen={isOpen} onClose={close} isCentered={isLargerThanMd} trapFocus={false}>
       <ModalOverlay />
       <ModalContent height={`${modalHeight}vh`}>
         <ModalHeader>{translate('common.selectAsset')}</ModalHeader>

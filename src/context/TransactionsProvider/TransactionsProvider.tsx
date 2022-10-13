703b21720edec76283c0f873551d93b1a298e886
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2022-10-12 21:01:00 -0600

    feat: subscribe txs by accountid, multi account support (#3048)
    * feat: subscribe txs by accountid, multi account support
    
    * fix: i wish typescript was nominally typed

--- src/context/TransactionsProvider/TransactionsProvider.tsx
+++ src/context/TransactionsProvider/TransactionsProvider.tsx
@@ -1,26 +1,20 @@
 import { cosmosChainId, fromAccountId, osmosisChainId } from '@shapeshiftoss/caip'
-import { TxStatus } from '@shapeshiftoss/unchained-client'
-import isEmpty from 'lodash/isEmpty'
 import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { getChainAdapterManager } from 'context/PluginProvider/chainAdapterSingleton'
-import { usePlugins } from 'context/PluginProvider/PluginProvider'
 import { useWallet } from 'hooks/useWallet/useWallet'
-import { walletSupportsChain } from 'hooks/useWalletSupportsChain/useWalletSupportsChain'
 import { logger } from 'lib/logger'
 import type { AccountSpecifierMap } from 'state/slices/accountSpecifiersSlice/accountSpecifiersSlice'
 import { portfolioApi } from 'state/slices/portfolioSlice/portfolioSlice'
 import {
-  selectAccountIdByAddress,
-  selectAccountSpecifiers,
-  selectAssets,
-  selectPortfolioAssetIds,
+  selectPortfolioAccountMetadata,
   selectPortfolioLoadingStatus,
-  selectTxHistoryStatus,
 } from 'state/slices/selectors'
 import { txHistory } from 'state/slices/txHistorySlice/txHistorySlice'
 import { validatorDataApi } from 'state/slices/validatorDataSlice/validatorDataSlice'
-import { store, useAppSelector } from 'state/store'
+import { useAppDispatch } from 'state/store'
+
+import { usePlugins } from '../PluginProvider/PluginProvider'
 
 const moduleLogger = logger.child({ namespace: ['TransactionsProvider'] })
 
@@ -28,138 +22,84 @@ type TransactionsProviderProps = {
   children: React.ReactNode
 }
 
-export const TransactionsProvider = ({ children }: TransactionsProviderProps): JSX.Element => {
-  const dispatch = useDispatch()
-  const {
-    state: { wallet, walletInfo },
-  } = useWallet()
-  const { supportedChains } = usePlugins()
+export const TransactionsProvider: React.FC<TransactionsProviderProps> = ({ children }) => {
+  const dispatch = useAppDispatch()
   const [isSubscribed, setIsSubscribed] = useState<boolean>(false)
-  const assets = useSelector(selectAssets)
-  const portfolioAssetIds = useSelector(selectPortfolioAssetIds)
-  const accountSpecifiers = useSelector(selectAccountSpecifiers)
+  const {
+    state: { wallet },
+  } = useWallet()
+  const portfolioAccountMetadata = useSelector(selectPortfolioAccountMetadata)
   const portfolioLoadingStatus = useSelector(selectPortfolioLoadingStatus)
-  const txHistoryStatus = useAppSelector(selectTxHistoryStatus)
+  const { supportedChains } = usePlugins()
 
-  const chainAdapterManager = getChainAdapterManager()
   /**
    * unsubscribe and cleanup logic
    */
   useEffect(() => {
-    // account specifiers changing will trigger this effect
     // we've disconnected/switched a wallet, unsubscribe from tx history and clear tx history
     if (!isSubscribed) return
-    moduleLogger.debug('unsubscribing txs')
-    supportedChains.forEach(chainId => chainAdapterManager.get(chainId)?.unsubscribeTxs())
+    moduleLogger.debug({ supportedChains }, 'unsubscribing txs')
+    // this is heavy handed but will ensure we're unsubscribed from everything
+    supportedChains.forEach(chainId => getChainAdapterManager().get(chainId)?.unsubscribeTxs())
     setIsSubscribed(false)
-    // setting isSubscribed to false will trigger this effect
+    // isSubscribed causes spurious unsubscriptions - this will react correctly when portfolioAccountMetadata changes
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [accountSpecifiers, dispatch, chainAdapterManager, supportedChains])
+  }, [supportedChains, wallet, portfolioAccountMetadata])
 
   /**
    * tx history subscription logic
    */
   useEffect(() => {
+    if (isSubscribed) return
     if (!wallet) return
-    if (isEmpty(assets)) return
+    const accountIds = Object.keys(portfolioAccountMetadata)
+    if (!accountIds.length) return
+    // this looks useless, but prevents attempting to subscribe multiple times
+    // something further up the tree from this provider is causing renders when the portfolio status changes,
+    // even though it shouldn't
     if (portfolioLoadingStatus === 'loading') return
-    if (isSubscribed) return // don't resubscribe
-    if (txHistoryStatus !== 'loaded') return
-    ;(async () =>
-      Promise.all(
-        supportedChains
-          .filter(chainId => {
-            return walletSupportsChain({ chainId, wallet })
-          })
-          .map(async chainId => {
-            const adapter = chainAdapterManager.get(chainId)
+    ;(async () => {
+      moduleLogger.debug({ accountIds }, 'subscribing txs')
+      await Promise.all(
+        accountIds.map(async accountId => {
+          const accountSpecifier = accountId // backwards compatibility, remove me 🔜
+          const { chainId, account } = fromAccountId(accountId)
+          const adapter = getChainAdapterManager().get(chainId)
 
-            // subscribe to new transactions for all supported accounts
-            try {
-              // account types are only supported for utxo chains, default to undefined if no accountTypes are supported
-              const supportedAccountTypes = adapter?.getSupportedAccountTypes?.() ?? [undefined]
+          const accountMetadata = portfolioAccountMetadata[accountId]
+          if (!accountMetadata) throw new Error('subscribe txs no accountMetadata?')
+          const { accountType, bip44Params } = accountMetadata
 
-              await Promise.all(
-                supportedAccountTypes.map(async accountType => {
-                  moduleLogger.debug({ chainId, accountType }, 'subscribing txs')
+          // subscribe to new transactions for all supported accounts
+          try {
+            return adapter?.subscribeTxs(
+              { wallet, accountType, bip44Params },
+              msg => {
+                const { getAccount } = portfolioApi.endpoints
+                const { getValidatorData } = validatorDataApi.endpoints
+                const { onMessage } = txHistory.actions
 
-                  const bip44Params = adapter?.getBIP44Params({ accountNumber: 0, accountType })
-                  if (!bip44Params) {
-                    moduleLogger.error(
-                      { chainId, accountType },
-                      'no bip44 params found for chain/accountType',
-                    )
-                    return
-                  }
+                // refetch validator data on new txs in case TVL or APR has changed
+                if ([cosmosChainId, osmosisChainId].includes(msg.chainId))
+                  dispatch(getValidatorData.initiate({ accountSpecifier, chainId }))
 
-                  return adapter?.subscribeTxs(
-                    {
-                      wallet,
-                      accountType,
-                      bip44Params,
-                    },
-                    msg => {
-                      const state = store.getState()
-                      const accountId = selectAccountIdByAddress(state, {
-                        accountSpecifier: `${msg.chainId}:${msg.address}`,
-                      })
+                const accountSpecifierMap: AccountSpecifierMap = { [msg.chainId]: account }
+                // refetch account on new tx
+                dispatch(getAccount.initiate({ accountSpecifierMap }, { forceRefetch: true }))
+                // deal with incoming message
+                dispatch(onMessage({ message: { ...msg, accountType }, accountSpecifier }))
+              },
+              (err: any) => moduleLogger.error(err),
+            )
+          } catch (e: unknown) {
+            moduleLogger.error(e, { accountId }, 'error subscribing to txs')
+          }
+        }),
+      )
 
-                      const { getAccount } = portfolioApi.endpoints
-                      const { getValidatorData } = validatorDataApi.endpoints
-
-                      // refetch validator data on new txs in case TVL or APR has changed
-                      if (msg.chainId === cosmosChainId || msg.chainId === osmosisChainId) {
-                        dispatch(
-                          getValidatorData.initiate({ accountSpecifier: accountId, chainId }),
-                        )
-                      }
-
-                      // refetch account on non pending txs
-                      // this catches Failed and Unknown status as well in case they caused account changes
-                      if (msg.status !== TxStatus.Pending) {
-                        const { account } = fromAccountId(accountId)
-                        const accountSpecifierMap: AccountSpecifierMap = {
-                          [msg.chainId]: account,
-                        }
-                        dispatch(
-                          getAccount.initiate({ accountSpecifierMap }, { forceRefetch: true }),
-                        )
-                      }
-
-                      dispatch(
-                        txHistory.actions.onMessage({
-                          message: { ...msg, accountType },
-                          accountSpecifier: accountId,
-                        }),
-                      )
-                    },
-                    (err: any) => moduleLogger.error(err),
-                  )
-                }),
-              )
-
-              // manage subscription state - we can't request this from chain adapters,
-              // and need this to prevent resubscribing when switching wallets
-              setIsSubscribed(true)
-            } catch (e: unknown) {
-              moduleLogger.error(e, { chainId }, 'error subscribing to transactions')
-            }
-          }),
-      ))()
-    // assets causes unnecessary renders, but doesn't actually change
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [
-    portfolioLoadingStatus,
-    isSubscribed,
-    dispatch,
-    walletInfo?.deviceId,
-    wallet,
-    chainAdapterManager,
-    supportedChains,
-    accountSpecifiers,
-    portfolioAssetIds,
-    txHistoryStatus,
-  ])
+      setIsSubscribed(true)
+    })()
+  }, [dispatch, isSubscribed, portfolioLoadingStatus, portfolioAccountMetadata, wallet])
 
   return <>{children}</>
 }

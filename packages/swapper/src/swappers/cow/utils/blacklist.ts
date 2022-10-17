f52cc881327eed9bbf118a6e7f08f11b65d3ca09
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2022-10-17 15:27:34 -0600

    chore: blacklist erc20 rune from swapping (#1065)
    chore: blacklist erc20 rune from swapped into

--- packages/swapper/src/swappers/cow/utils/blacklist.ts
+++ packages/swapper/src/swappers/cow/utils/blacklist.ts
@@ -3,4 +3,9 @@
 export const COWSWAP_UNSUPPORTED_ASSETS = Object.freeze([
   // Foxy token unsupported by cowswap
   'eip155:1/erc20:0xdc49108ce5c57bc3408c3a5e95f3d864ec386ed3',
+  /**
+   * ERC20 RUNE - we don't want people buying this instead of native RUNE
+   * as it's exchangeable value for native RUNE is currently decaying from 1 towards 0
+   */
+  'eip155:1/erc20:0x3155ba85d5f96b2d030a4966af206230e46849cb',
 ])

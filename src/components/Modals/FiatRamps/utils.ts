fe7a5faf5e8379d6992b0ca38e6fde3871ec7f92
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2022-10-21 09:59:10 -0600

    fix: sort buy and sell assets in fiat ramp by balance

--- src/components/Modals/FiatRamps/utils.ts
+++ src/components/Modals/FiatRamps/utils.ts
@@ -1,12 +1,19 @@
 import type { AssetId } from '@shapeshiftoss/caip'
-import { matchSorter } from 'match-sorter'
+import intersection from 'lodash/intersection'
+import { selectPortfolioAssetIdsSortedFiat } from 'state/slices/selectors'
 import { store } from 'state/store'
 
 export const middleEllipsis = (address: string, cut: number) =>
   `${address.slice(0, cut)}...${address.slice(-1 * cut)}`
 
 export const filterAssetsBySearchTerm = (search: string, assetIds: AssetId[]) => {
-  const assetsById = store.getState().assets.byId
-  const assets = assetIds.map(assetId => assetsById[assetId])
-  return matchSorter(assets, search, { keys: ['symbol', 'name'] }).map(({ assetId }) => assetId)
+  const state = store.getState()
+  const assetsById = state.assets.byId
+  const portfolioAssetIdsSortedFiat = selectPortfolioAssetIdsSortedFiat(state)
+  const actionableAssetIdsSortedFiat = intersection(portfolioAssetIdsSortedFiat, assetIds)
+  const uniqueSortedAssetIds = Array.from(new Set([...actionableAssetIdsSortedFiat, ...assetIds]))
+  const assets = uniqueSortedAssetIds.map(assetId => assetsById[assetId]).filter(Boolean)
+  return assets
+    .filter(asset => `${asset?.name}${asset?.symbol}`.toLowerCase().includes(search.toLowerCase()))
+    .map(({ assetId }) => assetId)
 }

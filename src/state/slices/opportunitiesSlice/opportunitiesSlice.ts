fcb3564a936ea41697cb1233012792778e794842
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-10-24 23:21:48 +0200

    fix: derp multiple Staking/Lp Ids

--- src/state/slices/opportunitiesSlice/opportunitiesSlice.ts
+++ src/state/slices/opportunitiesSlice/opportunitiesSlice.ts
@@ -48,9 +48,7 @@ type UserStakingId = `${AccountId}*${StakingId}`
 
 export type OpportunitiesState = {
   lp: {
-    byAccountId: {
-      // a 1:n foreign key of which user AccountIds hold this LpId  [id: AccountId]: LpId[]
-    }
+    byAccountId: Record<AccountId, LpId[]> // a 1:n foreign key of which user AccountIds hold this LpId  [id: AccountId]: LpId[]
     byId: Record<LpId, OpportunityMetadata>
     ids: LpId[]
   }
@@ -63,7 +61,7 @@ export type OpportunitiesState = {
 
   staking: {
     // a 1:n foreign key of which user AccountIds hold this StakingId
-    byAccountId: Record<AccountId, StakingId>
+    byAccountId: Record<AccountId, StakingId[]>
     byId: Record<StakingId, OpportunityMetadata>
     ids: StakingId[]
   }

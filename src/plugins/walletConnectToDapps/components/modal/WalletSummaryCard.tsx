76e39cf5ac13141b7dd9d47a1fd4a65033a2869f
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2022-10-24 13:48:46 -0600

    chore: usecallback click handler

--- src/plugins/walletConnectToDapps/components/modal/WalletSummaryCard.tsx
+++ src/plugins/walletConnectToDapps/components/modal/WalletSummaryCard.tsx
@@ -1,6 +1,7 @@
 import { CopyIcon, ExternalLinkIcon } from '@chakra-ui/icons'
 import { Box, HStack, IconButton, Link, useColorModeValue } from '@chakra-ui/react'
 import type { FC } from 'react'
+import { useCallback } from 'react'
 import { Card } from 'components/Card/Card'
 import { FoxIcon } from 'components/Icons/FoxIcon'
 import { MiddleEllipsis } from 'components/MiddleEllipsis/MiddleEllipsis'
@@ -13,33 +14,31 @@ type Props = {
   balance: number
 }
 
-export const WalletSummaryCard: FC<Props> = ({ address, name, url, balance }) => (
-  <Card bg={useColorModeValue('white', 'gray.850')} pt={4} pb={2} pl={4} borderRadius='md'>
-    <HStack spacing={0} mb={4}>
-      <FoxIcon color='gray.500' boxSize={6} />
-      <Box flex={1} pl={4}>
-        <MiddleEllipsis value={address} fontSize='lg' fontWeight='medium' mb={1} />
-        <RawText color='gray.500' fontWeight='medium'>
-          {name}
-        </RawText>
-      </Box>
-      <IconButton
-        variant='ghost'
-        aria-label='Copy'
-        icon={<CopyIcon />}
-        onClick={() => navigator.clipboard.writeText(address)}
-      />
-      <Link href={url} isExternal>
-        <IconButton
-          icon={<ExternalLinkIcon />}
-          variant='ghost'
-          aria-label={address}
-          colorScheme='gray'
-        />
-      </Link>
-    </HStack>
-    <RawText color='gray.500' fontWeight='medium'>
-      Balance: {balance} ETH
-    </RawText>
-  </Card>
-)
+export const WalletSummaryCard: FC<Props> = ({ address, name, url, balance }) => {
+  const handleClick = useCallback(() => navigator.clipboard.writeText(address), [address])
+  return (
+    <Card bg={useColorModeValue('white', 'gray.850')} pt={4} pb={2} pl={4} borderRadius='md'>
+      <HStack spacing={0} mb={4}>
+        <FoxIcon color='gray.500' boxSize={6} />
+        <Box flex={1} pl={4}>
+          <MiddleEllipsis value={address} fontSize='lg' fontWeight='medium' mb={1} />
+          <RawText color='gray.500' fontWeight='medium'>
+            {name}
+          </RawText>
+        </Box>
+        <IconButton variant='ghost' aria-label='Copy' icon={<CopyIcon />} onClick={handleClick} />
+        <Link href={url} isExternal>
+          <IconButton
+            icon={<ExternalLinkIcon />}
+            variant='ghost'
+            aria-label={address}
+            colorScheme='gray'
+          />
+        </Link>
+      </HStack>
+      <RawText color='gray.500' fontWeight='medium'>
+        Balance: {balance} ETH
+      </RawText>
+    </Card>
+  )
+}

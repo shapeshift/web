f23be08f4208c8612004778bb356e4bda468bae7
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-10-24 23:05:36 +0200

    feat: tackle review comments

--- src/state/slices/opportunitiesSlice/opportunitiesSlice.ts
+++ src/state/slices/opportunitiesSlice/opportunitiesSlice.ts
@@ -3,6 +3,7 @@ import { createApi } from '@reduxjs/toolkit/query/react'
 import type { AccountId, AssetId } from '@shapeshiftoss/caip'
 import type { DefiType } from 'features/defi/contexts/DefiManagerProvider/DefiCommon'
 import { BASE_RTK_CREATE_API_CONFIG } from 'state/apis/const'
+import type { Nominal } from 'types/common'
 
 export const initialState: OpportunitiesState = {
   lp: {
@@ -10,91 +11,61 @@ export const initialState: OpportunitiesState = {
     byId: {},
     ids: [],
   },
-  farming: {
+  staking: {
     byAccountId: {},
     byId: {},
     ids: [],
   },
-  userLp: {
-    byId: {},
-    ids: [],
-  },
-  userFarming: {
+  userStaking: {
     byId: {},
     ids: [],
   },
 }
 
-// The contract info, also referred to as "metadata" - doesn't contain any user-specific data
-type Opportunity = {
-  // The LP token AssetId
+type OpportunityMetadata = {
+  apy: string
   assetId: AssetId
   provider: string
   tvl: string
-  apy: string
   type: DefiType
-}
-
-type LpOpportunity = Opportunity & {
-  // A tuple of the underlying assets this LP represents
   underlyingAssetIds: [AssetId, AssetId]
 }
 
-type FarmingOpportunity = Opportunity
-
 // User-specific values for this opportunity
-type UserLpOpportunity = LpOpportunity & {
-  // The amount of held LP tokens
-  cryptoAmount: string
-  underlyingFoxCryptoBaseUnitAmount: string
-  underlyingEthCryptoBaseUnitAmount: string
-}
-
-// User-specific values for this opportunity
-type UserFarmingOpportunity = FarmingOpportunity & {
+type UserStakingOpportunity = {
   // The amount of farmed LP tokens
-  cryptoAmount: string
+  stakedAmountCryptoPrecision: string
+  // The amount of rewards available to claim for the farmed LP position
+  rewardsAmountCryptoPrecision: string
 }
 
-// The AccountId of the farming contract in the form of chainId:accountAddress
-type FarmingId = AccountId
+// The AccountId of the staking contract in the form of chainId:accountAddress
+type StakingId = Nominal<string, 'StakingId'>
 // The AccountId of the LP contract in the form of chainId:accountAddress
-type LpId = AccountId
-// The unique identifier of an lp opportunity in the form of UserAccountId:FarmingId
-type UserFarmingId = AccountId
-// The unique identifier of a farming opportunity in the form of UserAccountId:FarmingId
-type UserLpId = AccountId
+type LpId = Nominal<string, 'LpId'>
+// The unique identifier of an lp opportunity in the form of UserAccountId:StakingId
+type UserStakingId = Nominal<string, 'UserStakingId'>
 
 export type OpportunitiesState = {
-  userLp: {
-    byId: { [id: UserLpId]: UserLpOpportunity }
-    ids: UserLpId[]
-  }
   lp: {
     byAccountId: {
-      [id: AccountId]: LpId[]
+      // a 1:n foreign key of which user AccountIds hold this LpId  [id: AccountId]: LpId[]
     }
-    byId: {
-      [id: LpId]: LpOpportunity
-    }
+    byId: Record<LpId, OpportunityMetadata>
     ids: LpId[]
   }
-  // The user-specific farming data
-  userFarming: {
-    byId: {
-      [id: UserFarmingId]: UserFarmingOpportunity
-    }
-    ids: UserFarmingId[]
+  // Staking is the odd one here - it isn't a portfolio holding, but rather a synthetic value living on a smart contract
+  // Which means we can't just derive the data from portfolio, marketData and other slices but have to actually store the amount in the slice
+  userStaking: {
+    byId: Record<UserStakingId, UserStakingOpportunity>
+    ids: UserStakingId[]
   }
 
-  farming: {
-    byAccountId: {
-      [k: AccountId]: FarmingId[]
-    }
-    byId: {
-      [k: FarmingId]: FarmingOpportunity
-    }
-    ids: FarmingId[]
+  staking: {
+    // a 1:n foreign key of which user AccountIds hold this StakingId
+    byAccountId: Record<AccountId, StakingId>
+    byId: Record<StakingId, OpportunityMetadata>
+    ids: StakingId[]
   }
 }
 

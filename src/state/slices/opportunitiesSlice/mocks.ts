aec72d9dd117c28c3667c30e3d9fa05039690ab9
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-10-28 17:22:53 +0200

    feat: opportunity slice AccountId selectors (#3139)

---
+++ src/state/slices/opportunitiesSlice/mocks.ts
@@ -0,0 +1,9 @@
+export const gomesAccountId = 'eip155:1:0xgomes'
+export const fauxmesAccountId = 'eip155:1:0xfauxmes'
+export const catpuccinoAccountId = 'eip155:1:0xcatpuccino'
+
+export const mockLpContractOne = 'eip155:1:0xLpContractOne'
+export const mockLpContractTwo = 'eip155:1:0xLpContractTwo'
+
+export const mockStakingContractOne = 'eip155:1:0xStakingContractOne'
+export const mockStakingContractTwo = 'eip155:1:0xStakingContractTwo'

02e975e7f92071845fb7bf432401f21410643174
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-10-29 19:29:55 +0200

    feat: add opportunitiesSlice utils tests

---
+++ src/state/slices/opportunitiesSlice/utils.test.ts
@@ -0,0 +1,16 @@
+import { gomesAccountId, mockStakingContractOne } from './mocks'
+import { deserializeUserStakingId, serializeUserStakingId } from './utils'
+
+describe('opportunitiesSlice utils', () => {
+  const mockParts = [gomesAccountId, mockStakingContractOne]
+  describe('serializeUserStakingId', () => {
+    it('serializes a UserStakingId from AccountId and StakingId parts', () => {
+      const result = serializeUserStakingId(...mockParts)
+      expect(result).toEqual('eip155:1:0xgomes*eip155:1:0xStakingContractOne')
+    })
+  })
+  describe('deserializeUserStakingId', () => {
+    const result = deserializeUserStakingId('eip155:1:0xgomes*eip155:1:0xStakingContractOne')
+    expect(result).toEqual(mockParts)
+  })
+})

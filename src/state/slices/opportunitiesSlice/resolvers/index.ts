827f50960d929ebd876e488ab4ca1ced74faf561
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-10-31 11:08:11 +0100

    feat: narrow down resolver nested mapping type with string interpolation

--- src/state/slices/opportunitiesSlice/resolvers/index.ts
+++ src/state/slices/opportunitiesSlice/resolvers/index.ts
@@ -1,19 +1,15 @@
 import { DefiProvider, DefiType } from 'features/defi/contexts/DefiManagerProvider/DefiCommon'
 
 import { foxFarmingLpMetadataResolver, foxFarmingLpUserDataResolver } from './foxfarming'
-import type {
-  IDefiProviderToMetadataResolverByDeFiType,
-  IDefiProviderToUserDataResolverByDeFiType,
-} from './types'
 
 export const DefiProviderToMetadataResolverByDeFiType = {
-  [DefiProvider.FoxFarming]: {
-    [DefiType.LiquidityPool]: foxFarmingLpMetadataResolver,
+  [`${DefiProvider.FoxFarming}`]: {
+    [`${DefiType.LiquidityPool}`]: foxFarmingLpMetadataResolver,
   },
-} as IDefiProviderToMetadataResolverByDeFiType
+}
 
 export const DefiProviderToUserDataResolverByDeFiType = {
-  [DefiProvider.FoxFarming]: {
-    [DefiType.LiquidityPool]: foxFarmingLpUserDataResolver,
+  [`${DefiProvider.FoxFarming}`]: {
+    [`${DefiType.LiquidityPool}`]: foxFarmingLpUserDataResolver,
   },
-} as IDefiProviderToUserDataResolverByDeFiType
+}
--- src/state/slices/opportunitiesSlice/resolvers/types.ts
+++ src/state/slices/opportunitiesSlice/resolvers/types.ts
@@ -1,20 +1,3 @@
 import type { BaseQueryApi } from '@reduxjs/toolkit/dist/query/baseQueryTypes'
-import type { DefiProvider, DefiType } from 'features/defi/contexts/DefiManagerProvider/DefiCommon'
-
-import type { foxFarmingLpMetadataResolver, foxFarmingLpUserDataResolver } from './foxfarming'
 
 export type ReduxApi = Pick<BaseQueryApi, 'dispatch' | 'getState'>
-
-export type IDefiProviderToMetadataResolverByDeFiType = {
-  [key in DefiProvider]?: {
-    // TODO: Discriminated union, only FOX ETH LP/Farming is implemented so far
-    [key in DefiType]: typeof foxFarmingLpMetadataResolver
-  }
-}
-
-export type IDefiProviderToUserDataResolverByDeFiType = {
-  [key in DefiProvider]?: {
-    // TODO: Discriminated union, only FOX ETH LP/Farming is implemented so far
-    [key in DefiType]: typeof foxFarmingLpUserDataResolver
-  }
-}

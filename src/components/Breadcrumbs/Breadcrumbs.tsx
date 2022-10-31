32a217b7c9c71a63591d50bfce19d9835378fd6d
Q <stackedq@gmail.com>
2022-10-31 12:02:36 +0330

    fix: breadcrumb improvements, leftover hardcoded string

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -2,19 +2,20 @@ import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from '@chakra-ui/react'
 import { useMemo } from 'react'
 import type { BreadcrumbsRoute } from 'react-router-breadcrumbs-hoc'
 import withBreadcrumbs from 'react-router-breadcrumbs-hoc'
+import type { RouteComponentProps } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import { AccountLabel } from 'components/AssetHeader/AccountLabel'
 import { Text } from 'components/Text/Text'
 import { selectAssetById } from 'state/slices/selectors'
 import { useAppSelector } from 'state/store'
 
-const GetTranslatedPathPart = (props: any) => {
+const GetTranslatedPathPart = ({ match }: { match: RouteComponentProps['match'] }) => {
   // a hack to gain the ability to translate the breadcrumbs
   // takes out the path part from the url
   // and uses navbar.[pathPart] for the translation
-  const splittedPath = useMemo(() => props.match.params[0].split('/'), [props.match.params])
+  const pathFragments = useMemo(() => match.url.split('/'), [match.url])
   // getting the last part from the url, since this works recursively
-  const pathPart = splittedPath[splittedPath.length - 1]
+  const pathPart = pathFragments[pathFragments.length - 1]
   // translates `/` to home
   return <Text translation={`navBar.${pathPart ? pathPart : 'home'}`} />
 }

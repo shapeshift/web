d4078c19cd937c40e9fa216f4436f118c7af76b7
reallybeard <89934888+reallybeard@users.noreply.github.com>
2022-11-02 15:40:02 -0500

    feat: add react helmet support (#3203)
    * add react helment for SEO goodness
    
    * Update Seo.tsx
    
    * add
    
    * remove config stuff and add other assets to the sitemap
    
    * all caps the SEO component

---
+++ src/components/Layout/Seo.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Helmet } from 'react-helmet-async'
+import Thumb from 'assets/ss-thumb.jpg'
+
+type SeoProps = {
+  title?: string
+  description?: string
+  type?: string
+  name?: string
+}
+
+export const SEO: React.FC<SeoProps> = ({
+  title,
+  description = 'ShapeShift DAO | Your Web3 & DeFi Portal',
+  type = 'website',
+  name = 'ShapeShift',
+}) => {
+  const publicUrl = window.location.origin
+  return (
+    <Helmet>
+      {/* Standard metadata tags */}
+      <title>{title ? `${title} | ShapeShift` : 'ShapeShift'}</title>
+      <meta name='description' content={description} />
+      {/* End standard metadata tags */}
+      {/* Facebook tags */}
+      <meta property='og:image' content={`${publicUrl}${Thumb}`} />
+      <meta property='og:type' content={type} />
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={description} />
+      {/* End Facebook tags */}
+      {/* Twitter tags */}
+      <meta name='twitter:creator' content={name} />
+      <meta name='twitter:card' content={type} />
+      <meta name='twitter:title' content={title} />
+      <meta name='twitter:description' content={description} />
+      {/* End Twitter tags */}
+    </Helmet>
+  )
+}

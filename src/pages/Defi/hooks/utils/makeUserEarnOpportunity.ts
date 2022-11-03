5b902ab015b63efc664bdb11b1826ccff397891b
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-11-03 23:58:27 +0100

    feat: revert makeUserEarnOpportunity

--- src/pages/Defi/hooks/utils/makeUserEarnOpportunity.ts
+++
@@ -1,6 +0,0 @@
-import type { UserEarnOpportunityType } from 'context/FoxEthProvider/FoxEthProvider'
-import type { OpportunityMetadata } from 'state/slices/opportunitiesSlice/types'
-
-export const makeUserEarnOpportunity = (
-  opportunity: OpportunityMetadata,
-): UserEarnOpportunityType => {}

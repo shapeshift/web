d8ed99c9d682fac2320310b4c8a3c142458930fe
Sean Luther <elmutt@gmail.com>
2022-11-03 10:35:35 -0600

    cleanup vote and approval

--- src/components/Modals/kkVote/KKVote.tsx
+++ src/components/Modals/kkVote/KKVote.tsx
@@ -14,23 +14,20 @@ import type { ChainAdapter } from '@shapeshiftoss/chain-adapters'
 import type { ETHSignTx } from '@shapeshiftoss/hdwallet-core'
 import { bip32ToAddressNList } from '@shapeshiftoss/hdwallet-core'
 import type { KeepKeyHDWallet } from '@shapeshiftoss/hdwallet-keepkey'
-import { bnOrZero } from '@shapeshiftoss/investor-foxy'
 import type { KnownChainIds } from '@shapeshiftoss/types'
-import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import Web3 from 'web3'
 import { Text } from 'components/Text'
 import { getChainAdapterManager } from 'context/PluginProvider/chainAdapterSingleton'
 import { useKeepKey } from 'context/WalletProvider/KeepKeyProvider'
 import { useModal } from 'hooks/useModal/useModal'
 import { useWallet } from 'hooks/useWallet/useWallet'
-import { erc20Abi } from 'pages/Leaderboard/helpers/erc20Abi'
-import { nftAbi } from 'pages/Leaderboard/helpers/nftAbi'
 import { selectAssetById } from 'state/slices/selectors'
 import { useAppSelector } from 'state/store'
 
 export const KKVote = ({ geckoId }: { geckoId: any }) => {
   const ethAsset = useAppSelector(state => selectAssetById(state, ethAssetId))
-  const { getKeepkeyAsset } = useKeepKey()
+  const { getKeepkeyAsset, kkErc20Contract, kkNftContract, kkWeb3 } = useKeepKey()
   const {
     state: { wallet },
   } = useWallet()
@@ -41,73 +38,34 @@ export const KKVote = ({ geckoId }: { geckoId: any }) => {
   const { kkVote } = useModal()
   const { close, isOpen } = kkVote
 
-  const [approvedAmount, setApprovedAmount] = useState('0')
+  // has clicked button but not necessarily broadcast or mined yet
+  const [approveClicked, setApproveClicked] = useState(false)
+  const [voteClicked, setVoteClicked] = useState(false)
 
-  const [isApproving, setIsApproving] = useState(false)
+  // have a txid from but not necessarily mined yet
   const [approveTxid, setApproveTxid] = useState('')
-
-  const [isVoting, setIsVoting] = useState(false)
   const [voteTxid, setVoteTxid] = useState('')
 
-  const loadApprovedAmount = useCallback(async () => {
-    const network = 'goerli'
-    const web3 = new Web3(
-      new Web3.providers.HttpProvider(
-        `https://${network}.infura.io/v3/fb05c87983c4431baafd4600fd33de7e`,
-      ),
-    )
-    const erc20Contract = new web3.eth.Contract(
-      erc20Abi as any,
-      '0xcc5a5975E8f6dF4dDD9Ff4Eb57471a3Ff32526a3',
-    )
-    const addressNList = bip32ToAddressNList("m/44'/60'/0'/0/0")
-
-    const address = await (wallet as KeepKeyHDWallet).ethGetAddress({
-      addressNList,
-      showDisplay: false,
-    })
-
-    const approved = await erc20Contract.methods
-      .allowance(address, '0xa869a28a7185df50e4abdba376284c44497c4753')
-      .call()
-    setApprovedAmount(approved)
-  }, [wallet])
-
-  useEffect(() => {
-    loadApprovedAmount()
-  }, [loadApprovedAmount, geckoId])
+  // 1 or more confirmations
+  const [voteConfirmed, setVoteConfirmed] = useState(false)
+  const [approveConfirmed, setApproveConfirmed] = useState(false)
 
   const onVoteClick = useCallback(async () => {
-    setIsVoting(true)
-    const network = 'goerli'
-    const web3 = new Web3(
-      new Web3.providers.HttpProvider(
-        `https://${network}.infura.io/v3/fb05c87983c4431baafd4600fd33de7e`,
-      ),
-    )
-    const nftContract = new web3.eth.Contract(
-      nftAbi as any,
-      '0xa869a28a7185df50e4abdba376284c44497c4753',
-    )
-
-    const voteData = nftContract.methods.mintNFT(burnAmount, geckoId).encodeABI()
-
+    setVoteClicked(true)
+    const voteData = kkNftContract.methods.mintNFT(burnAmount, geckoId).encodeABI()
     const chainAdapterManager = getChainAdapterManager()
     const adapter = chainAdapterManager.get(ethAsset.chainId) as ChainAdapter<KnownChainIds>
-
     const addressNList = bip32ToAddressNList("m/44'/60'/0'/0/0")
-
-    const gasPrice = await web3.eth.getGasPrice()
-
+    const gasPrice = await kkWeb3.eth.getGasPrice()
     const address = await (wallet as KeepKeyHDWallet).ethGetAddress({
       addressNList,
       showDisplay: false,
     })
 
-    const nonce = await web3.eth.getTransactionCount(address)
+    const nonce = await kkWeb3.eth.getTransactionCount(address)
 
     const txToSign: ETHSignTx = {
-      to: '0xa869a28a7185df50e4abdba376284c44497c4753',
+      to: kkNftContract.options.Address,
       nonce: Web3.utils.toHex(nonce),
       data: voteData,
       value: '0x0',
@@ -123,31 +81,26 @@ export const KKVote = ({ geckoId }: { geckoId: any }) => {
       wallet,
     })
 
-    web3.eth.sendSignedTransaction(signedTx).then(() => {
-      setIsVoting(false)
-    })
-    const txHash = await web3.utils.sha3(signedTx)
+    const txHash = await kkWeb3.utils.sha3(signedTx)
     setVoteTxid(txHash ?? '')
-  }, [burnAmount, ethAsset?.chainId, geckoId, wallet])
+    kkWeb3.eth.sendSignedTransaction(signedTx).then(() => {
+      setVoteConfirmed(true)
+    })
+  }, [
+    burnAmount,
+    ethAsset.chainId,
+    geckoId,
+    kkNftContract.methods,
+    kkNftContract.options.Address,
+    kkWeb3.eth,
+    kkWeb3.utils,
+    wallet,
+  ])
 
   const onApproveClick = useCallback(async () => {
-    setIsApproving(true)
-    const network = 'goerli'
-    const web3 = new Web3(
-      new Web3.providers.HttpProvider(
-        `https://${network}.infura.io/v3/fb05c87983c4431baafd4600fd33de7e`,
-      ),
-    )
-    const erc20Contract = new web3.eth.Contract(
-      erc20Abi as any,
-      '0xcc5a5975E8f6dF4dDD9Ff4Eb57471a3Ff32526a3',
-    )
-
-    const approveData = erc20Contract.methods
-      .approve(
-        '0xa869a28a7185df50e4abdba376284c44497c4753',
-        '0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF',
-      )
+    setApproveClicked(true)
+    const approveData = kkErc20Contract.methods
+      .approve(kkNftContract.options.address, '0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF')
       .encodeABI()
 
     const chainAdapterManager = getChainAdapterManager()
@@ -155,17 +108,17 @@ export const KKVote = ({ geckoId }: { geckoId: any }) => {
 
     const addressNList = bip32ToAddressNList("m/44'/60'/0'/0/0")
 
-    const gasPrice = await web3.eth.getGasPrice()
+    const gasPrice = await kkWeb3.eth.getGasPrice()
 
     const address = await (wallet as KeepKeyHDWallet).ethGetAddress({
       addressNList,
       showDisplay: false,
     })
 
-    const nonce = await web3.eth.getTransactionCount(address)
+    const nonce = await kkWeb3.eth.getTransactionCount(address)
 
     const txToSign: ETHSignTx = {
-      to: '0xcc5a5975E8f6dF4dDD9Ff4Eb57471a3Ff32526a3',
+      to: kkErc20Contract.options.address,
       nonce: Web3.utils.toHex(nonce),
       data: approveData,
       value: '0x0',
@@ -181,12 +134,20 @@ export const KKVote = ({ geckoId }: { geckoId: any }) => {
       wallet,
     })
 
-    web3.eth.sendSignedTransaction(signedTx).then(() => {
-      setIsApproving(false)
-    })
-    const txHash = await web3.utils.sha3(signedTx)
+    const txHash = await kkWeb3.utils.sha3(signedTx)
     setApproveTxid(txHash ?? '')
-  }, [ethAsset, wallet])
+    kkWeb3.eth.sendSignedTransaction(signedTx).then(() => {
+      setApproveConfirmed(true)
+    })
+  }, [
+    ethAsset.chainId,
+    kkErc20Contract.methods,
+    kkErc20Contract.options.address,
+    kkNftContract.options.address,
+    kkWeb3.eth,
+    kkWeb3.utils,
+    wallet,
+  ])
   return (
     <Modal
       isOpen={isOpen}
@@ -215,8 +176,8 @@ export const KKVote = ({ geckoId }: { geckoId: any }) => {
             />
           </div>
           <div>
-            {bnOrZero(approvedAmount).eq(0) && !approveTxid && (
-              <Button isLoading={isApproving} onClick={onApproveClick}>
+            {!approveConfirmed && (
+              <Button isLoading={approveClicked} onClick={onApproveClick}>
                 Approve
               </Button>
             )}
@@ -225,8 +186,8 @@ export const KKVote = ({ geckoId }: { geckoId: any }) => {
               isExternal
               href={`https://goerli.etherscan.io/tx/${approveTxid}`}
             >{`${approveTxid}`}</Link>
-            {bnOrZero(approvedAmount).gt(0) && !voteTxid && (
-              <Button isLoading={isVoting} onClick={onVoteClick}>
+            {!voteConfirmed && (
+              <Button isLoading={voteClicked} onClick={onVoteClick}>
                 Vote
               </Button>
             )}

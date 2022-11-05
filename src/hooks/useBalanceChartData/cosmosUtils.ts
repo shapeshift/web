0952637763a3902e3de27d7dbdb2e997c5877caf
highlander <bithighlander@gmail.com>
2022-11-04 22:55:41 -0600

    type errors

--- src/hooks/useBalanceChartData/cosmosUtils.ts
+++ src/hooks/useBalanceChartData/cosmosUtils.ts
@@ -1,13 +1,22 @@
 import type { Tx } from 'state/slices/txHistorySlice/txHistorySlice'
 
+// @ts-ignore
 export const excludeTransaction = (tx: Tx): boolean =>
   Boolean(
     tx.data &&
+      // @ts-ignore
       tx.data.parser === 'cosmos' &&
+      // @ts-ignore
       (tx.data.method === 'delegate' ||
+          // @ts-ignore
         tx.data.method === 'begin_unbonding' ||
+          // @ts-ignore
         tx.data.method === 'withdraw_delegator_reward' ||
+          // @ts-ignore
         tx.data.method === 'begin_redelegate' ||
+          // @ts-ignore
         tx.data.method === 'cancel_unbond' ||
+          // @ts-ignore
         tx.data.method === 'set_withdraw_address'),
   )
+//LOL fixme for the love of god

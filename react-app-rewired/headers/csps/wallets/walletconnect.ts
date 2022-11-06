92fd11e9f30c6a5fc5f4e8c45027805e3c8fd6b1
highlander <bithighlander@gmail.com>
2022-11-06 13:21:39 -0500

    bump

--- react-app-rewired/headers/csps/wallets/walletconnect.ts
+++ react-app-rewired/headers/csps/wallets/walletconnect.ts
@@ -2,6 +2,7 @@ import type { Csp } from '../../types'
 
 export const csp: Csp = {
   'connect-src': [
+    '*',
     'wss://*.bridge.walletconnect.org/',
     'https://registry.walletconnect.com/api/v2/wallets',
     "https://api.etherscan.io"

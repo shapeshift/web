4c04fb7f64c66d72fdf45b9296c3577dbe3a736a
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2022-11-07 10:19:16 -0700

    chore: memoize notification widget

--- src/components/Layout/Header/NavBar/Notifications.tsx
+++ src/components/Layout/Header/NavBar/Notifications.tsx
@@ -3,6 +3,8 @@ import { IconButton } from '@chakra-ui/react'
 import type { CustomTheme } from '@wherever/react-notification-feed'
 import { NotificationBell, NotificationFeed } from '@wherever/react-notification-feed'
 import { NotificationFeedProvider } from '@wherever/react-notification-feed'
+import { getConfig } from 'config'
+import { useMemo } from 'react'
 import { useFeatureFlag } from 'hooks/useFeatureFlag/useFeatureFlag'
 import { breakpoints } from 'theme/theme'
 import { theme } from 'theme/theme'
@@ -11,33 +13,35 @@ export const Notifications = () => {
   const isWhereverEnabled = useFeatureFlag('Wherever')
   const { colorMode } = useColorMode()
 
+  const mobileBreakpoint = Number(breakpoints.md.replace('px', ''))
+  const themeObj: CustomTheme = useMemo(() => {
+    const baseTheme =
+      colorMode === 'light'
+        ? {
+            primaryColor: theme.colors.primary,
+            backgroundColor: theme.colors.gray[100],
+            textColor: theme.colors.gray[800],
+            bellColor: theme.colors.gray[700],
+          }
+        : {
+            textColor: theme.colors.gray[50],
+          }
+    return {
+      ...baseTheme,
+      borderRadius: 'md',
+      mobileBreakpoint,
+    }
+  }, [colorMode, mobileBreakpoint])
+
   if (!isWhereverEnabled) return null
 
   const disableAnalytics = window.location.hostname.includes('private.shapeshift.com')
-
-  const mobileBreakpoint = Number(breakpoints.md.replace('px', ''))
-
-  const themeOptions: CustomTheme =
-    colorMode === 'light'
-      ? {
-          primaryColor: theme.colors.primary,
-          backgroundColor: theme.colors.gray[100],
-          textColor: theme.colors.gray[800],
-          bellColor: theme.colors.gray[700],
-        }
-      : {
-          textColor: theme.colors.gray[50],
-        }
+  const partnerKey = getConfig().REACT_APP_WHEREVER_PARTNER_KEY
 
   return (
     <NotificationFeedProvider
-      env={'staging'} // TODO(nivo33): Remove before PR is merged
-      partnerKey={process.env.REACT_APP_WHEREVER_PARTNER_KEY as string}
-      theme={{
-        borderRadius: 'md',
-        mobileBreakpoint,
-        ...themeOptions,
-      }}
+      partnerKey={partnerKey}
+      theme={themeObj}
       disableAnalytics={disableAnalytics}
     >
       <NotificationFeed>

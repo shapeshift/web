e4d1df62cdd3091ad25f8e103b4637efa1a86e6b
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2022-11-07 10:07:29 -0700

    chore: remove wrong comment from wherever csp

--- react-app-rewired/headers/csps/plugins/wherever.ts
+++ react-app-rewired/headers/csps/plugins/wherever.ts
@@ -1,9 +1,5 @@
 import type { Csp } from '../../types'
 
-// These directives come from the "Full agent functionality and displaying guides
-// in production" section of https://support.pendo.io/hc/en-us/articles/360032209131,
-// with the addition of https://cdn.pendo.io to the connect-src list so we can apply
-// agent fixups.
 const agentCsp: Csp = {
   'connect-src': [
     'https://staging-api.wherever.to',

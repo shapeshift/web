5909f4db786e5da1b530d8db354aa69c27dcdb99
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-11-07 17:40:19 +0100

    wip toEarnOpportunity

--- src/state/slices/opportunitiesSlice/utils.ts
+++ src/state/slices/opportunitiesSlice/utils.ts
@@ -1,7 +1,7 @@
 import type { AccountId } from '@shapeshiftoss/caip'
 
 import { STAKING_ID_DELIMITER } from './constants'
-import type { StakingId, UserStakingId } from './types'
+import type { OpportunityMetadata, StakingId, UserStakingId } from './types'
 
 export type UserStakingIdParts = [accountId: AccountId, stakingId: StakingId]
 
@@ -28,3 +28,5 @@ export const filterUserStakingIdByStakingIdCompareFn = (
 
   return deserializedStakingId === stakingId
 }
+
+export const toEarnOpportunity = (opportunityMeta: OpportunityMetadata) => {}

e0709361b705b889e13acf905f54aada479527b7
Amitoj Singh <amitojsingh366@gmail.com>
2022-11-09 05:50:48 +0400

    ::

--- electron/src/helpers/utils.ts
+++ electron/src/helpers/utils.ts
@@ -111,7 +111,7 @@ export const createMainWindow = async () => {
 
     windows.mainWindow.loadURL(isDev
         ? 'http://localhost:3000'
-        : `file://${path.join(__dirname, '../../../build/index.html')}`)
+        : `file://${path.join(__dirname, '../../build/index.html')}`)
 
     startWindowListeners()
 

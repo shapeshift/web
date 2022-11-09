04133c851cc9addd91189d4a1197dc3fb59c2eb5
Amitoj Singh <amitojsingh366@gmail.com>
2022-11-10 01:12:54 +0400

    fix build even more

--- electron/src/helpers/globalState.ts
+++ electron/src/helpers/globalState.ts
@@ -12,7 +12,7 @@ import { createAndUpdateTray } from "../tray";
 import { queueIpcEvent } from "./utils";
 import { BridgeLogger } from "./bridgeLogger";
 
-export const assetsDirectory = path.join(__dirname, '../../assets')
+export const assetsDirectory = path.join(__dirname, '../../../assets')
 export const isMac = process.platform === "darwin"
 export const isWin = process.platform === "win32"
 export const isLinux = process.platform !== "darwin" && process.platform !== "win32"

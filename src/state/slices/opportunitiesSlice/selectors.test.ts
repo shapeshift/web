12dcacded56ca35fd68b8669eee9768085a058ef
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-11-09 23:52:26 +0100

    chore: cleanup diff

--- src/state/slices/opportunitiesSlice/selectors.test.ts
+++ src/state/slices/opportunitiesSlice/selectors.test.ts
@@ -1,5 +1,8 @@
+import type { AssetId } from '@shapeshiftoss/caip'
+import { ethAssetId, foxAssetId } from '@shapeshiftoss/caip'
 import { DefiProvider, DefiType } from 'features/defi/contexts/DefiManagerProvider/DefiCommon'
 import { mockStore } from 'test/mocks/store'
+import type { OpportunityMetadata } from 'state/slices/opportunitiesSlice/types'
 
 import { foxEthLpAssetId, foxEthPair } from './constants'
 import {
@@ -135,25 +138,31 @@ describe('opportunitiesSlice selectors', () => {
     })
   })
   describe('selectUserStakingOpportunityByUserStakingId', () => {
-    const mockMetadata = {
-      apy: '1000',
-      assetId: mockStakingContractTwo,
+    const mockOpportunityMetadata: OpportunityMetadata = {
+      // The LP token AssetId
+      assetId: foxEthLpAssetId,
       provider: DefiProvider.FoxEthLP,
-      tvl: '91283233211',
+      tvl: '424242',
+      apy: '0.42',
       type: DefiType.LiquidityPool,
-      underlyingAssetIds: foxEthPair,
       underlyingAssetId: foxEthLpAssetId,
+      underlyingAssetIds: [foxAssetId, ethAssetId] as [AssetId, AssetId],
       underlyingAssetRatios: ['5000000000000000', '202200000000000000000'] as [string, string],
     }
-
     const staking = {
+      ...initialState.staking,
+      ids: [
+        serializeUserStakingId(gomesAccountId, mockStakingContractTwo),
+        serializeUserStakingId(gomesAccountId, mockStakingContractOne),
+        serializeUserStakingId(fauxmesAccountId, mockStakingContractOne),
+      ],
       byId: {
-        [mockStakingContractOne]: mockMetadata,
-        [mockStakingContractTwo]: mockMetadata,
+        [mockStakingContractOne]: mockOpportunityMetadata,
+        [mockStakingContractTwo]: mockOpportunityMetadata,
       },
     }
     const userStaking = {
-      ...initialState.staking,
+      ...initialState.userStaking,
       ids: [
         serializeUserStakingId(gomesAccountId, mockStakingContractTwo),
         serializeUserStakingId(gomesAccountId, mockStakingContractOne),
@@ -186,18 +195,38 @@ describe('opportunitiesSlice selectors', () => {
           userStakingId: serializeUserStakingId(gomesAccountId, mockStakingContractTwo),
         }),
       ).toEqual({
-        stakedAmountCryptoPrecision: '1337',
+        apy: '0.42',
+        assetId: 'eip155:1/erc20:0x470e8de2ebaef52014a47cb5e6af86884947f08c',
+        provider: 'UNI V2',
         rewardsAmountCryptoPrecision: '420',
-        ...mockMetadata,
+        stakedAmountCryptoPrecision: '1337',
+        tvl: '424242',
+        type: 'lp',
+        underlyingAssetId: 'eip155:1/erc20:0x470e8de2ebaef52014a47cb5e6af86884947f08c',
+        underlyingAssetIds: [
+          'eip155:1/erc20:0xc770eefad204b5180df6a14ee197d99d808ee52d',
+          'eip155:1/slip44:60',
+        ],
+        underlyingAssetRatios: ['5000000000000000', '202200000000000000000'],
       })
       expect(
         selectUserStakingOpportunityByUserStakingId(mockState, {
           userStakingId: serializeUserStakingId(gomesAccountId, mockStakingContractOne),
         }),
       ).toEqual({
+        apy: '0.42',
+        assetId: 'eip155:1/erc20:0x470e8de2ebaef52014a47cb5e6af86884947f08c',
+        provider: 'UNI V2',
         stakedAmountCryptoPrecision: '4',
         rewardsAmountCryptoPrecision: '3',
-        ...mockMetadata,
+        tvl: '424242',
+        type: 'lp',
+        underlyingAssetId: 'eip155:1/erc20:0x470e8de2ebaef52014a47cb5e6af86884947f08c',
+        underlyingAssetIds: [
+          'eip155:1/erc20:0xc770eefad204b5180df6a14ee197d99d808ee52d',
+          'eip155:1/slip44:60',
+        ],
+        underlyingAssetRatios: ['5000000000000000', '202200000000000000000'],
       })
     })
   })

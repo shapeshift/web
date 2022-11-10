1b0034a70ae2651f4f3b8ca484af9e9ae16403f8
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2022-11-10 10:08:40 -0700

    chore: comment magic number

--- src/state/slices/marketDataSlice/marketServiceManagerSingleton.ts
+++ src/state/slices/marketDataSlice/marketServiceManagerSingleton.ts
@@ -11,7 +11,7 @@ export const getMarketServiceManager: GetMarketServiceManager = () => {
   if (!_marketServiceManager) {
     _marketServiceManager = new MarketServiceManager({
       coinGeckoAPIKey: config.REACT_APP_COINGECKO_API_KEY,
-      yearnChainReference: 1,
+      yearnChainReference: 1, // CHAIN_REFERENCE.EthereumMainnet is '1', yearn requires strict number union
       providerUrls: {
         jsonRpcProviderUrl: config.REACT_APP_ETHEREUM_NODE_URL,
         unchainedEthereumHttpUrl: config.REACT_APP_UNCHAINED_ETHEREUM_HTTP_URL,

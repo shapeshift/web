1464941866459cbd7cdadf4cfef30cd15adfa96c
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2022-11-10 13:34:40 -0700

    fix: we can't use this in a meta tag

--- react-app-rewired/headers/csps/fiatRamps/onRamper.ts
+++ react-app-rewired/headers/csps/fiatRamps/onRamper.ts
@@ -2,6 +2,5 @@ import type { Csp } from '../../types'
 
 export const csp: Csp = {
   'frame-src': ['https://widget.onramper.com', 'https://swap.onramper.com', 'https://buy.moonpay.com'],
-  'frame-ancestors': ['*'],
   'connect-src': [process.env.REACT_APP_ONRAMPER_API_URL!, process.env.REACT_APP_ONRAMPER_WIDGET_URL!],
 }

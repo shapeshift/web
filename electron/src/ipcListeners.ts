bd5d7a58c181e82b1cc70fea2fdffe959e9b83c9
Amitoj Singh <amitojsingh366@gmail.com>
2022-11-10 04:30:22 +0400

    pls fix build :(

--- electron/src/ipcListeners.ts
+++ electron/src/ipcListeners.ts
@@ -8,6 +8,7 @@ import {
 } from './helpers/kk-state-controller/firmwareUtils'
 import * as path from 'path'
 import { queueIpcEvent } from './helpers/utils'
+import log from 'electron-log'
 
 export const startIpcListeners = () => {
     ipcMain.on('@app/get-asset-url', (event, data) => {
@@ -83,8 +84,10 @@ export const startIpcListeners = () => {
     // web render thread has indicated it is ready to receive ipc messages
     // send any that have queued since then
     ipcMain.on('renderListenersReady', async () => {
+        log.info('renderListenersReady')
         setRenderListenersReady(true)
         ipcQueue.forEach((item, idx) => {
+            log.info('ipc event called from queue', item)
             if (windows.mainWindow && !windows.mainWindow.isDestroyed()) windows.mainWindow.webContents.send(item.eventName, item.args)
             ipcQueue.splice(idx, 1);
         })

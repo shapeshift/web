f2a705d73e4fd56b70de6afd8841175eaeab8fb2
reallybeard <89934888+reallybeard@users.noreply.github.com>
2022-11-10 13:51:24 -0600

    remove duplicate csp

--- react-app-rewired/headers/csps/defi/onramper.ts
+++
@@ -1,5 +0,0 @@
-import type { Csp } from '../../types'
-
-export const csp: Csp = {
-  'connect-src': [process.env.REACT_APP_ONRAMPER_API_URL!, process.env.REACT_APP_ONRAMPER_WIDGET_URL!],
-}
--- react-app-rewired/headers/csps/fiatRamps/onRamper.ts
+++ react-app-rewired/headers/csps/fiatRamps/onRamper.ts
@@ -2,5 +2,6 @@ import type { Csp } from '../../types'
 
 export const csp: Csp = {
   'frame-src': ['https://widget.onramper.com', 'https://swap.onramper.com', 'https://buy.moonpay.com'],
-  'frame-ancestors': ['*']
+  'frame-ancestors': ['*'],
+  'connect-src': [process.env.REACT_APP_ONRAMPER_API_URL!, process.env.REACT_APP_ONRAMPER_WIDGET_URL!],
 }

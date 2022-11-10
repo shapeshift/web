610532e0ab550849ea011f6535adad42f3ff8b45
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-11-10 12:18:08 +0100

    fix: buyPage.body consistent dark/light whiteAlpha.900 color

--- src/pages/Buy/Buy.tsx
+++ src/pages/Buy/Buy.tsx
@@ -105,7 +105,7 @@ export const Buy = () => {
                   translation='buyPage.title.second'
                 />
               </Heading>
-              <Text fontSize='lg' translation='buyPage.body' />
+              <Text fontSize='lg' translation='buyPage.body' color='whiteAlpha.900' />
               <Text fontSize='sm' color='gray.500' translation='buyPage.disclaimer' />
             </Flex>
             <Box flexBasis='400px'>

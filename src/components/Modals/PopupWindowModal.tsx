f25e80f8db127d92b15babb6576b441106a97a8e
reallybeard <89934888+reallybeard@users.noreply.github.com>
2022-11-14 10:57:35 -0600

    copy

--- src/components/Modals/PopupWindowModal.tsx
+++ src/components/Modals/PopupWindowModal.tsx
@@ -1,7 +1,8 @@
 import { Button, Center, Modal, ModalContent, ModalOverlay } from '@chakra-ui/react'
 import { useCallback, useEffect, useRef } from 'react'
+import { useTranslate } from 'react-polyglot'
 import { CircularProgress } from 'components/CircularProgress/CircularProgress'
-import { RawText } from 'components/Text'
+import { Text } from 'components/Text'
 import { useModal } from 'hooks/useModal/useModal'
 
 function popupCenterWindow(url: string, windowName: string, w: number, h: number) {
@@ -18,17 +19,18 @@ function popupCenterWindow(url: string, windowName: string, w: number, h: number
 type PopupWindowModalProps = {
   url: string
   title: string
-  width?: string
-  height?: string
+  width?: number
+  height?: number
 }
 
 export const PopupWindowModal: React.FC<PopupWindowModalProps> = ({
   title,
   url,
-  width = '482px',
-  height = '660px',
+  width = 482,
+  height = 660,
 }) => {
   const { popup } = useModal()
+  const translate = useTranslate()
   const { close: onClose, isOpen } = popup
   const popupRef = useRef<Window | null | void>(null)
 
@@ -38,34 +40,55 @@ export const PopupWindowModal: React.FC<PopupWindowModalProps> = ({
     }
   }, [])
 
+  const handleCloseWindow = useCallback(() => {
+    if (popupRef.current) {
+      popupRef.current.close()
+    }
+  }, [])
+
+  const handleContinue = useCallback(() => {
+    window.open(url, '_blank')?.focus()
+  }, [url])
+
   useEffect(() => {
     if (!isOpen) return
-    popupRef.current = popupCenterWindow(url, title, 450, 600)
+    popupRef.current = popupCenterWindow(url, title, width, height)
     var loop = setInterval(function () {
       if (popupRef.current && popupRef.current.closed) {
         clearInterval(loop)
         popup.close()
       }
     }, 1000)
-  }, [popup, isOpen, title, url])
+  }, [popup, isOpen, title, url, width, height])
   return (
     <Modal isOpen={isOpen} onClose={onClose} size='full'>
       <ModalOverlay
         backdropFilter='blur(10px)'
         bgColor='blackAlpha.100'
-        bgImage='radial-gradient(ellipse at center, rgba(0,0,0,0) 0%,rgba(0,0,0,0) 3%,rgba(0,0,0,1) 77%,rgba(0,0,0,1) 100%)'
+        bgImage='radial-gradient(ellipse at center, rgba(0,0,0,1) 0%,rgba(0,0,0,1) 1%,rgba(0,0,0,0) 100%);'
       />
       <ModalContent alignItems='center' justifyContent='center' bg='transparent'>
         <Center
-          width={{ base: '100%', md: width }}
+          width={{ base: '100%', md: '350px' }}
           height={height}
-          position='relative'
-          flexDir='column'
+          flexDirection='column'
+          textAlign='center'
           gap={6}
+          position='relative'
         >
           <CircularProgress />
-          <RawText fontSize='xl'>We loaded the window for you</RawText>
-          <Button onClick={handleFocusWindow}>Show Window</Button>
+          <Text
+            translation={popupRef.current ? 'modals.popup.body' : 'modals.popup.body2'}
+            fontSize='xl'
+          />
+          {popupRef?.current ? (
+            <Button onClick={handleFocusWindow}>{translate('modals.popup.showWindow')}</Button>
+          ) : (
+            <Button onClick={handleContinue}>{translate('modals.popup.continue')}</Button>
+          )}
+          <Button onClick={handleCloseWindow} variant='ghost'>
+            {translate('modals.popup.cancel')}
+          </Button>
         </Center>
       </ModalContent>
     </Modal>

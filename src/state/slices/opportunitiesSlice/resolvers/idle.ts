bc21056d7bc299fa1c1353b26dd102c073dae07e
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-11-16 13:45:57 +0100

    refactor: async reduce -> for of

--- src/state/slices/opportunitiesSlice/resolvers/idle.ts
+++ src/state/slices/opportunitiesSlice/resolvers/idle.ts
@@ -29,48 +29,43 @@ export const idleStakingOpportunitiesMetadataResolver = async ({
   const { getState } = reduxApi
   const state: any = getState() // ReduxState causes circular dependency
 
-  const stakingOpportunitiesById = await opportunities.reduce(
-    async (acc: Promise<OpportunitiesState[DefiType.Staking]['byId']>, opportunity) => {
-      const assetId = toAssetId({
-        assetNamespace: 'erc20',
-        assetReference: opportunity.id,
-        chainId: fromAssetId(opportunity.feeAsset.assetId).chainId,
-      })
+  const stakingOpportunitiesById: OpportunitiesState[DefiType.Staking]['byId'] = {}
 
-      const accResolved = await acc
+  for (const opportunity of opportunities) {
+    const assetId = toAssetId({
+      assetNamespace: 'erc20',
+      assetReference: opportunity.id,
+      chainId: fromAssetId(opportunity.feeAsset.assetId).chainId,
+    })
 
-      const asset = selectAssetById(state, assetId)
+    const asset = selectAssetById(state, assetId)
 
-      // Asset doesn't exist in portfolio, meaning this asset is bogus, e.g these two
-      // https://etherscan.io/address/0xa0154a44c1c45bd007743fa622fd0da4f6d67d57
-      // https://etherscan.io/address/0x5f45a578491a23ac5aee218e2d405347a0fafa8e
-      if (!asset) return accResolved
+    // Asset doesn't exist in portfolio, meaning this asset is bogus, e.g these two
+    // https://etherscan.io/address/0xa0154a44c1c45bd007743fa622fd0da4f6d67d57
+    // https://etherscan.io/address/0x5f45a578491a23ac5aee218e2d405347a0fafa8e
+    if (!asset) continue
 
-      const rewardAssetIds = (await opportunity.getRewardAssetIds().catch(error => {
-        moduleLogger.debug(
-          { fn: 'idleStakingOpportunitiesMetadataResolver', error },
-          'Error fetching Idle opportunities metadata',
-        )
-      })) as [AssetId] | [AssetId, AssetId] | [AssetId, AssetId, AssetId] | undefined
+    const rewardAssetIds = (await opportunity.getRewardAssetIds().catch(error => {
+      moduleLogger.debug(
+        { fn: 'idleStakingOpportunitiesMetadataResolver', error },
+        'Error fetching Idle opportunities metadata',
+      )
+    })) as [AssetId] | [AssetId, AssetId] | [AssetId, AssetId, AssetId] | undefined
 
-      accResolved[assetId as StakingId] = {
-        apy: opportunity.apy.toString(),
-        assetId,
-        provider: DefiProvider.Idle,
-        tvl: opportunity.tvl.balance.toString(),
-        type: DefiType.Staking,
-        underlyingAssetId: assetId,
-        underlyingAssetIds: [opportunity.underlyingAsset.assetId],
-        ...{ rewardAssetIds },
-        // Idle opportunities wrap a single yield-bearing asset, so the ratio will always be 1
-        underlyingAssetRatios: ['1'],
-        name: asset.name,
-      }
-
-      return accResolved
-    },
-    Promise.resolve({}),
-  )
+    stakingOpportunitiesById[assetId as StakingId] = {
+      apy: opportunity.apy.toString(),
+      assetId,
+      provider: DefiProvider.Idle,
+      tvl: opportunity.tvl.balance.toString(),
+      type: DefiType.Staking,
+      underlyingAssetId: assetId,
+      underlyingAssetIds: [opportunity.underlyingAsset.assetId],
+      ...{ rewardAssetIds },
+      // Idle opportunities wrap a single yield-bearing asset, so the ratio will always be 1
+      underlyingAssetRatios: ['1'],
+      name: asset.name,
+    }
+  }
 
   const data = {
     byId: stakingOpportunitiesById,

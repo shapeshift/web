5e9ba9660fbd935a330f3e0e05ed92a17744fd2e
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-11-21 15:38:45 +0100

    feat: don't whitelist opportunities

--- src/state/store.ts
+++ src/state/store.ts
@@ -20,7 +20,7 @@ import { validatorDataApi } from './slices/validatorDataSlice/validatorDataSlice
 
 const persistConfig = {
   key: 'root',
-  whitelist: ['txHistory', 'portfolio', 'opportunities'],
+  whitelist: ['txHistory', 'portfolio'],
   storage: localforage,
 }
 

355cc764d31902add6285c57ae4d8404a3ed3d67
reallybeard <89934888+reallybeard@users.noreply.github.com>
2022-11-28 10:45:10 -0600

    add logic for demo wallet or disconnected wallet

--- src/components/Promo/PromoCard.tsx
+++ src/components/Promo/PromoCard.tsx
@@ -1,12 +1,14 @@
 import { Button } from '@chakra-ui/react'
 import dayjs from 'dayjs'
 import isBetween from 'dayjs/plugin/isBetween'
-import { useMemo } from 'react'
-import { NavLink } from 'react-router-dom'
+import { useCallback, useMemo } from 'react'
+import { useHistory } from 'react-router-dom'
 import FoxyPromo from 'assets/foxy-promo.jpg'
 import { Card } from 'components/Card/Card'
 import { Carousel } from 'components/Carousel/Carousel'
 import { RawText } from 'components/Text'
+import { WalletActions } from 'context/WalletProvider/actions'
+import { useWallet } from 'hooks/useWallet/useWallet'
 dayjs.extend(isBetween)
 
 const promoData = [
@@ -19,49 +21,77 @@ const promoData = [
     startDate: '2022-11-28 8:00 AM',
     endDate: '2022-12-4 8:00 AM',
     id: 'foxy-promo',
+    walletRequired: true,
     href: '?chainId=eip155%3A1&contractAddress=0xee77aa3Fd23BbeBaf94386dD44b548e9a785ea4b&assetReference=0xc770eefad204b5180df6a14ee197d99d808ee52d&rewardId=0xDc49108ce5C57bc3408c3A5E95F3d864eC386Ed3&provider=ShapeShift&modal=deposit',
   },
 ]
 
 export const PromoCard = () => {
+  const {
+    dispatch,
+    state: { isDemoWallet, isConnected },
+  } = useWallet()
+  const history = useHistory()
+
+  const handleWalletModalOpen = useCallback(
+    () => dispatch({ type: WalletActions.SET_WALLET_MODAL, payload: true }),
+    [dispatch],
+  )
+
+  const handleClick = useCallback(
+    ({ href, walletRequired }: { href: string; walletRequired: boolean }) => {
+      if (walletRequired) {
+        if (isConnected && !isDemoWallet) {
+          history.push(href)
+        } else {
+          handleWalletModalOpen()
+        }
+      } else {
+        history.push(href)
+      }
+    },
+    [handleWalletModalOpen, history, isConnected, isDemoWallet],
+  )
+
   const renderPromos = useMemo(() => {
     const filteredPromoCards = promoData.filter(e =>
       dayjs().isBetween(dayjs(e.startDate), dayjs(e.endDate)),
     )
-    return filteredPromoCards.map(({ image, title, body, colorScheme = 'blue', href, cta, id }) => {
-      return (
-        <Card
-          backgroundImage={image}
-          backgroundSize='cover'
-          backgroundPosition='center center'
-          key={id}
-        >
-          <Card.Body
-            display='flex'
-            flexDir='column'
-            gap={2}
-            pb={filteredPromoCards.length > 1 ? 8 : 6}
+    return filteredPromoCards.map(
+      ({ image, title, body, colorScheme = 'blue', href, cta, id, walletRequired }) => {
+        return (
+          <Card
+            backgroundImage={image}
+            backgroundSize='cover'
+            backgroundPosition='center center'
+            key={id}
           >
-            <RawText fontWeight='bold'>{title}</RawText>
-            <RawText fontSize='sm' mr={24}>
-              {body}
-            </RawText>
-            <Button
-              variant='outline'
-              width='full'
-              colorScheme={colorScheme}
-              mt={2}
-              as={NavLink}
-              data-test={`${id}-button`}
-              to={href}
+            <Card.Body
+              display='flex'
+              flexDir='column'
+              gap={2}
+              pb={filteredPromoCards.length > 1 ? 8 : 6}
             >
-              {cta}
-            </Button>
-          </Card.Body>
-        </Card>
-      )
-    })
-  }, [])
+              <RawText fontWeight='bold'>{title}</RawText>
+              <RawText fontSize='sm' mr={24}>
+                {body}
+              </RawText>
+              <Button
+                variant='outline'
+                width='full'
+                colorScheme={colorScheme}
+                mt={2}
+                onClick={() => handleClick({ href, walletRequired })}
+                data-test={`${id}-button`}
+              >
+                {cta}
+              </Button>
+            </Card.Body>
+          </Card>
+        )
+      },
+    )
+  }, [handleClick])
 
   return (
     <Carousel autoPlay interval={10000} loop showArrows={false}>

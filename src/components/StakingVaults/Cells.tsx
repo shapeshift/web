3f247e5a1e4dfbb1a60a3d526b0bb9f4b8a290cb
reallybeard <89934888+reallybeard@users.noreply.github.com>
2022-12-02 13:45:51 -0600

    Update Cells.tsx

--- src/components/StakingVaults/Cells.tsx
+++ src/components/StakingVaults/Cells.tsx
@@ -11,6 +11,7 @@ import {
 } from '@chakra-ui/react'
 import type { Asset } from '@shapeshiftoss/asset-service'
 import type { AssetId } from '@shapeshiftoss/caip'
+import { PairIcons } from 'features/defi/components/PairIcons/PairIcons'
 import { debounce } from 'lodash'
 import { useState } from 'react'
 import { FaInfoCircle } from 'react-icons/fa'
@@ -81,21 +82,11 @@ export const AssetCell = ({
         </Popover>
       )}
       <HStack flex={1}>
-        <SkeletonCircle isLoaded={!!asset} mr={2}>
+        <SkeletonCircle isLoaded={!!asset} mr={2} width='auto'>
           {icons ? (
-            <Flex flexDirection='row' alignItems='center' width={{ base: 'auto', md: '40%' }}>
-              {icons.map((iconSrc, i) => (
-                <AssetIcon
-                  key={iconSrc}
-                  src={iconSrc}
-                  boxSize='8'
-                  mr={i === icons.length - 1 ? 2 : 0}
-                  ml={i === 0 ? '-3.5' : '-4'}
-                />
-              ))}
-            </Flex>
+            <PairIcons icons={icons} iconSize='sm' bg='none' />
           ) : (
-            <AssetIcon assetId={asset.assetId} boxSize='8' />
+            <AssetIcon assetId={asset.assetId} size='sm' />
           )}
         </SkeletonCircle>
         <SkeletonText noOfLines={2} isLoaded={!!asset} flex={1}>

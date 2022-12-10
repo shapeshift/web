c1907be0641c162120bf178f0295558b7d98219a
reallybeard <89934888+reallybeard@users.noreply.github.com>
2022-12-10 12:12:48 -0600

    Update YearnOverview.tsx

--- src/features/defi/providers/yearn/components/YearnManager/Overview/YearnOverview.tsx
+++ src/features/defi/providers/yearn/components/YearnManager/Overview/YearnOverview.tsx
@@ -1,56 +1,51 @@
 import { ArrowDownIcon, ArrowUpIcon } from '@chakra-ui/icons'
-import { Center, useToast } from '@chakra-ui/react'
+import { Center } from '@chakra-ui/react'
+import type { Asset } from '@shapeshiftoss/asset-service'
 import type { AccountId } from '@shapeshiftoss/caip'
-import { toAssetId } from '@shapeshiftoss/caip'
-import type { YearnOpportunity } from '@shapeshiftoss/investor-yearn'
-import { USDC_PRECISION } from 'constants/constants'
+import { ethChainId, toAssetId } from '@shapeshiftoss/caip'
 import { Overview } from 'features/defi/components/Overview/Overview'
 import type {
   DefiParams,
   DefiQueryParams,
 } from 'features/defi/contexts/DefiManagerProvider/DefiCommon'
 import { DefiAction } from 'features/defi/contexts/DefiManagerProvider/DefiCommon'
-import { useYearn } from 'features/defi/contexts/YearnProvider/YearnProvider'
-import { useEffect, useMemo, useState } from 'react'
-import { useTranslate } from 'react-polyglot'
+import { useMemo } from 'react'
 import type { AccountDropdownProps } from 'components/AccountDropdown/AccountDropdown'
 import { CircularProgress } from 'components/CircularProgress/CircularProgress'
 import { useBrowserRouter } from 'hooks/useBrowserRouter/useBrowserRouter'
-import { bnOrZero } from 'lib/bignumber/bignumber'
-import { logger } from 'lib/logger'
+import { bn, bnOrZero } from 'lib/bignumber/bignumber'
 import { useGetAssetDescriptionQuery } from 'state/slices/assetsSlice/assetsSlice'
+import { serializeUserStakingId, toOpportunityId } from 'state/slices/opportunitiesSlice/utils'
 import {
   selectAssetById,
+  selectAssets,
+  selectEarnUserStakingOpportunityByUserStakingId,
+  selectFirstAccountIdByChainId,
+  selectHighestBalanceAccountIdByStakingId,
   selectMarketDataById,
   selectPortfolioCryptoBalanceByFilter,
   selectSelectedLocale,
 } from 'state/slices/selectors'
 import { useAppSelector } from 'state/store'
 
-const moduleLogger = logger.child({
-  namespace: ['DeFi', 'Providers', 'Yearn', 'YearnOverview'],
-})
-
 export const YearnOverview: React.FC<{
   accountId?: AccountId | undefined
   onAccountIdChange: AccountDropdownProps['onChange']
 }> = ({ accountId, onAccountIdChange: handleAccountIdChange }) => {
-  const { yearn: api } = useYearn()
-  const translate = useTranslate()
-  const toast = useToast()
   const { query } = useBrowserRouter<DefiQueryParams, DefiParams>()
-  const [opportunity, setOpportunity] = useState<YearnOpportunity | null>(null)
-  const { chainId, contractAddress: vaultAddress, assetReference } = query
+  const { chainId, contractAddress, assetReference } = query
+  console.info(chainId, contractAddress, assetReference)
 
   const assetNamespace = 'erc20'
   const assetId = toAssetId({ chainId, assetNamespace, assetReference })
+
   const vaultTokenId = toAssetId({
     chainId,
     assetNamespace,
-    assetReference: vaultAddress,
+    assetReference: contractAddress,
   })
-  const asset = useAppSelector(state => selectAssetById(state, vaultTokenId))
-  const underlyingToken = useAppSelector(state => selectAssetById(state, assetId))
+  const assets = useAppSelector(selectAssets)
+  const vaultAsset = useAppSelector(state => selectAssetById(state, vaultTokenId))
   const marketData = useAppSelector(state => selectMarketDataById(state, assetId))
   // user info
   const filter = useMemo(
@@ -59,55 +54,66 @@ export const YearnOverview: React.FC<{
   )
   const balance = useAppSelector(state => selectPortfolioCryptoBalanceByFilter(state, filter))
 
-  const amountAvailableCryptoPrecision = bnOrZero(balance).div(`1e${asset.precision}`)
-  const fiatAmountAvailable = bnOrZero(amountAvailableCryptoPrecision).times(marketData.price)
+  const cryptoAmountAvailable = useMemo(
+    () => bnOrZero(balance).div(bn(10).pow(vaultAsset?.precision)),
+    [balance, vaultAsset?.precision],
+  )
+  const fiatAmountAvailable = useMemo(
+    () => bnOrZero(cryptoAmountAvailable).times(marketData.price),
+    [cryptoAmountAvailable, marketData.price],
+  )
 
   const selectedLocale = useAppSelector(selectSelectedLocale)
   const descriptionQuery = useGetAssetDescriptionQuery({ assetId, selectedLocale })
 
-  useEffect(() => {
-    ;(async () => {
-      try {
-        if (!(vaultAddress && api)) return
-        const [opportunity] = await Promise.all([
-          api.findByOpportunityId(
-            toAssetId({ chainId, assetNamespace, assetReference: vaultAddress }),
-          ),
-        ])
-        if (!opportunity) {
-          return toast({
-            position: 'top-right',
-            description: translate('common.somethingWentWrongBody'),
-            title: translate('common.somethingWentWrong'),
-            status: 'error',
-          })
-        }
-        setOpportunity(opportunity)
-      } catch (error) {
-        // TODO: handle client side errors
-        moduleLogger.error(error, 'YearnDeposit error')
-      }
-    })()
-  }, [api, vaultAddress, chainId, toast, translate])
+  const opportunityId = useMemo(
+    () => toOpportunityId({ chainId, assetNamespace: 'erc20', assetReference: contractAddress }),
+    [chainId, contractAddress],
+  )
+  const highestBalanceAccountIdFilter = useMemo(
+    () => ({ stakingId: opportunityId }),
+    [opportunityId],
+  )
+  const highestBalanceAccountId = useAppSelector(state =>
+    selectHighestBalanceAccountIdByStakingId(state, highestBalanceAccountIdFilter),
+  )
+  const defaultAccountId = useAppSelector(state => selectFirstAccountIdByChainId(state, ethChainId))
+  const opportunityDataFilter = useMemo(
+    () => ({
+      userStakingId: serializeUserStakingId(
+        (accountId ?? highestBalanceAccountId ?? defaultAccountId)!,
+        toOpportunityId({
+          chainId,
+          assetNamespace: 'erc20',
+          assetReference: contractAddress,
+        }),
+      ),
+    }),
+    [accountId, chainId, contractAddress, defaultAccountId, highestBalanceAccountId],
+  )
+
+  const opportunityData = useAppSelector(state =>
+    selectEarnUserStakingOpportunityByUserStakingId(state, opportunityDataFilter),
+  )
+
+  const underlyingAssetId = useMemo(
+    () => opportunityData?.underlyingAssetIds?.[0],
+    [opportunityData?.underlyingAssetIds],
+  )
+  const underlyingAsset: Asset | undefined = useMemo(
+    () => assets[underlyingAssetId ?? ''],
+    [assets, underlyingAssetId],
+  )
 
   const underlyingAssets = useMemo(
     () => [
       {
-        ...underlyingToken,
-        cryptoBalancePrecision: amountAvailableCryptoPrecision.toPrecision(),
+        ...underlyingAsset,
+        cryptoBalancePrecision: cryptoAmountAvailable.toPrecision(),
         allocationPercentage: '1',
       },
     ],
-    [amountAvailableCryptoPrecision, underlyingToken],
-  )
-
-  const description = useMemo(
-    () => ({
-      description: underlyingToken.description,
-      isLoaded: !descriptionQuery.isLoading,
-      isTrustedDescription: underlyingToken.isTrustedDescription,
-    }),
-    [descriptionQuery.isLoading, underlyingToken.description, underlyingToken.isTrustedDescription],
+    [cryptoAmountAvailable, underlyingAsset],
   )
 
   const menu = useMemo(
@@ -126,7 +132,7 @@ export const YearnOverview: React.FC<{
     [],
   )
 
-  if (!opportunity) {
+  if (!opportunityData) {
     return (
       <Center minW='500px' minH='350px'>
         <CircularProgress />
@@ -138,14 +144,18 @@ export const YearnOverview: React.FC<{
     <Overview
       accountId={accountId}
       onAccountIdChange={handleAccountIdChange}
-      asset={asset}
-      name={`${underlyingToken.name} Vault (${opportunity.version})`}
+      asset={vaultAsset}
+      name={opportunityData.name ?? ''}
       opportunityFiatBalance={fiatAmountAvailable.toFixed(2)}
       underlyingAssetsCryptoPrecision={underlyingAssets}
       provider='Yearn Finance'
-      description={description}
-      tvl={opportunity.tvl.balanceUsdc.div(`1e+${USDC_PRECISION}`).toString()}
-      apy={opportunity.apy.toString()}
+      description={{
+        description: underlyingAsset.description,
+        isLoaded: !descriptionQuery.isLoading,
+        isTrustedDescription: underlyingAsset.isTrustedDescription,
+      }}
+      tvl={bnOrZero(opportunityData.tvl).toFixed(2)}
+      apy={opportunityData.apy}
       menu={menu}
     />
   )

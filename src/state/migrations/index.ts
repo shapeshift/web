fecf056b669eab7b7afcda8d22539410cb87cb32
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-01-03 23:59:50 +0100

    fix: filter out expired yearn opportunities

--- src/state/migrations/index.ts
+++ src/state/migrations/index.ts
@@ -1,5 +1,7 @@
 import { clearOpportunitiesSlice } from './000_clear_opportunities_slice'
+import { filterExpiredYearnOpportunities } from './001_filter_expired_yearn_opportunities'
 
 export const migrations = {
   0: clearOpportunitiesSlice,
+  1: filterExpiredYearnOpportunities,
 }

1e2a3a217e54730c72b80dd7f3bc50396ded5443
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-01-07 00:27:09 +0100

    feat: thorchain savers opportunities IDs (#3573)

---
+++ src/state/slices/opportunitiesSlice/resolvers/thorchainsavers/index.ts
@@ -0,0 +1,35 @@
+import { adapters } from '@shapeshiftoss/caip'
+import type { ThornodePoolResponse } from '@shapeshiftoss/swapper'
+import axios from 'axios'
+import { getConfig } from 'config'
+import { bnOrZero } from 'lib/bignumber/bignumber'
+
+import type { GetOpportunityIdsOutput, OpportunityId, StakingId } from '../../types'
+
+export const thorchainSaversOpportunityIdsResolver = async (): Promise<{
+  data: GetOpportunityIdsOutput
+}> => {
+  const { data: opportunitiesData } = await axios.get<ThornodePoolResponse[]>(
+    `${getConfig().REACT_APP_THORCHAIN_NODE_URL}/lcd/thorchain/pools`,
+  )
+
+  if (!opportunitiesData) return { data: [] }
+
+  const opportunityIds = opportunitiesData.reduce<OpportunityId[]>((acc, currentOpportunity) => {
+    const maybeOpportunityId = adapters.poolAssetIdToAssetId(currentOpportunity.asset)
+
+    if (
+      bnOrZero(currentOpportunity.savers_depth).gt(0) &&
+      maybeOpportunityId &&
+      currentOpportunity.status === 'Available'
+    ) {
+      acc.push(maybeOpportunityId as StakingId)
+    }
+
+    return acc
+  }, [])
+
+  return {
+    data: opportunityIds,
+  }
+}

a0ba0c3991a2c76626a68ea65886fa1d9e00ea30
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2023-01-09 17:09:20 +1100

    chore: handle ErrorWithDetails code

--- src/state/apis/utils.ts
+++ src/state/apis/utils.ts
@@ -1,7 +1,11 @@
+import { ErrorWithDetails } from '@shapeshiftoss/errors'
+
 export const handleApiError = (
   e: unknown,
   defaultErrorMessage: string,
 ): { error: Record<string, unknown> } => {
+  // 'code' is a getter of ErrorWithDetails, so we need to access it specifically
+  if (e instanceof ErrorWithDetails) return { error: { ...e, code: e.code } }
   if (e instanceof Error) return { error: { ...e } }
   else
     return {

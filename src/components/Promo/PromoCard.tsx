14052d24353f1bf44aac7d788031d82e47a71e44
reallybeard <89934888+reallybeard@users.noreply.github.com>
2023-01-10 11:32:39 -0600

    add on ramper apple pay cta

--- src/components/Promo/PromoCard.tsx
+++ src/components/Promo/PromoCard.tsx
@@ -1,13 +1,16 @@
-import { Button } from '@chakra-ui/react'
+import { ArrowForwardIcon } from '@chakra-ui/icons'
+import { Button, Flex, Image } from '@chakra-ui/react'
 import { supportsETH } from '@shapeshiftoss/hdwallet-core'
 import dayjs from 'dayjs'
 import customParseFormat from 'dayjs/plugin/customParseFormat'
 import isBetween from 'dayjs/plugin/isBetween'
 import { useCallback, useMemo } from 'react'
+import { useTranslate } from 'react-polyglot'
 import { useHistory } from 'react-router-dom'
+import OnRamperLogo from 'assets/on-ramper.png'
 import { Card } from 'components/Card/Card'
 import { Carousel } from 'components/Carousel/Carousel'
-import { RawText } from 'components/Text'
+import { Text } from 'components/Text'
 import { WalletActions } from 'context/WalletProvider/actions'
 import { useWallet } from 'hooks/useWallet/useWallet'
 
@@ -15,7 +18,22 @@ dayjs.extend(isBetween)
 dayjs.extend(customParseFormat)
 const promoDateFormat = 'YYYY-MM-DD hh:mm A'
 
-const promoData = [
+type PromoItem = {
+  id: string
+  title: string
+  body: string
+  cta: string
+  href: string
+  startDate: string
+  endDate: string
+  image?: string
+  colorScheme?: string
+  walletRequired: boolean
+  rightElement?: JSX.Element
+  isExternal?: boolean
+}
+
+const promoData: PromoItem[] = [
   {
     title: 'Earn 3.15% APY on FOX',
     body: 'Your FOX is put to work across different DeFi protocols to earn the best yield possible.',
@@ -28,6 +46,19 @@ const promoData = [
     walletRequired: true,
     href: '?chainId=eip155%3A1&contractAddress=0xee77aa3Fd23BbeBaf94386dD44b548e9a785ea4b&assetReference=0xc770eefad204b5180df6a14ee197d99d808ee52d&rewardId=0xDc49108ce5C57bc3408c3A5E95F3d864eC386Ed3&provider=ShapeShift&modal=deposit',
   },
+  {
+    title: 'promo.onRamper.title',
+    body: 'promo.onRamper.body',
+    cta: 'promo.onRamper.cta',
+    image: `url(https://uploads-ssl.webflow.com/5cec55545d0f47cfe2a39a8e/637d3eab8977b9c820ecb3fc_foxy-promo-1.jpg)`,
+    startDate: '2023-01-01 08:00 AM',
+    endDate: '2023-02-01 08:00 AM',
+    id: 'apple-pay',
+    href: 'https://widget.onramper.com/?apiKey=pk_prod_ViOib9FcqKQeqqBsLF6ZPYis8X0Wdl9ma16rBhTxXmw0&defaultCrypto=ETH&supportSell=false&isAddressEditable=false&language=en&darkMode=true&redirectURL=https%3A%2F%2Fapp.shapeshift.com%2F%23%2Fbuy-crypto&onlyGateways=Mercuryo',
+    walletRequired: false,
+    rightElement: <Image width='80px' overflow='hidden' borderRadius='lg' src={OnRamperLogo} />,
+    isExternal: true,
+  },
 ]
 
 export const PromoCard = () => {
@@ -36,6 +67,7 @@ export const PromoCard = () => {
     state: { wallet, isDemoWallet },
   } = useWallet()
   const history = useHistory()
+  const translate = useTranslate()
 
   const handleWalletModalOpen = useCallback(
     () => dispatch({ type: WalletActions.SET_WALLET_MODAL, payload: true }),
@@ -43,15 +75,23 @@ export const PromoCard = () => {
   )
 
   const handleClick = useCallback(
-    ({ href, walletRequired }: { href: string; walletRequired: boolean }) => {
+    ({
+      href,
+      walletRequired,
+      isExternal,
+    }: {
+      href: string
+      walletRequired: boolean
+      isExternal?: boolean
+    }) => {
       if (walletRequired) {
         if (wallet && !isDemoWallet && supportsETH(wallet)) {
-          history.push(href)
+          isExternal ? window.open(href) : history.push(href)
         } else {
           handleWalletModalOpen()
         }
       } else {
-        history.push(href)
+        isExternal ? window.open(href) : history.push(href)
       }
     },
     [handleWalletModalOpen, history, isDemoWallet, wallet],
@@ -62,41 +102,40 @@ export const PromoCard = () => {
       dayjs().isBetween(dayjs(e.startDate, promoDateFormat), dayjs(e.endDate, promoDateFormat)),
     )
     return filteredPromoCards.map(
-      ({ image, title, body, colorScheme = 'blue', href, cta, id, walletRequired }) => {
+      ({
+        rightElement,
+        title,
+        body,
+        colorScheme = 'blue',
+        href,
+        cta,
+        id,
+        walletRequired,
+        isExternal,
+      }) => {
         return (
-          <Card
-            backgroundImage={image}
-            backgroundSize='cover'
-            backgroundPosition='center center'
-            key={id}
-          >
-            <Card.Body
-              display='flex'
-              flexDir='column'
-              gap={2}
-              pb={filteredPromoCards.length > 1 ? 8 : 6}
-            >
-              <RawText fontWeight='bold' color={'whiteAlpha.900'}>
-                {title}
-              </RawText>
-              <RawText fontSize='sm' mr={24} color={'whiteAlpha.900'}>
-                {body}
-              </RawText>
-              <Button
-                variant='outline'
-                width='full'
-                colorScheme={colorScheme}
-                mt={2}
-                onClick={() => handleClick({ href, walletRequired })}
-                data-test={`${id}-button`}
-              >
-                {cta}
-              </Button>
+          <Card key={id}>
+            <Card.Body display='flex' pb={filteredPromoCards.length > 1 ? 8 : 6} gap={6}>
+              <Flex direction='column' alignItems='flex-start' gap={2}>
+                <Text fontWeight='bold' color={'whiteAlpha.900'} translation={title} />
+                <Text color={'gray.500'} translation={body} />
+                <Button
+                  variant='link'
+                  colorScheme={colorScheme}
+                  mt={4}
+                  onClick={() => handleClick({ href, walletRequired, isExternal })}
+                  rightIcon={<ArrowForwardIcon />}
+                  data-test={`${id}-button`}
+                >
+                  {translate(cta)}
+                </Button>
+              </Flex>
+              {rightElement && <Flex alignItems='center'>{rightElement}</Flex>}
             </Card.Body>
           </Card>
         )
       },
     )
-  }, [handleClick])
+  }, [handleClick, translate])
   return renderPromos.length ? <Carousel>{renderPromos}</Carousel> : null
 }

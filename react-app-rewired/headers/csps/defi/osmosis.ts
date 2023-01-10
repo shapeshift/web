ce211da77bd6787bbf09dfa5f0c53dfbe7667a9e
pastaghost <pastaghost@2520.io>
2023-01-09 20:01:06 -0700

    feat: add osmosis defi csp entries

---
+++ react-app-rewired/headers/csps/defi/osmosis.ts
@@ -0,0 +1,8 @@
+import type { Csp } from '../../types'
+
+export const csp: Csp = {
+  'connect-src': [
+   'https://lcd-osmosis.keplr.app/osmosis/',
+    'https://api-osmosis.imperator.co/',
+  ]
+}

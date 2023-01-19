036ff4dfeb52aea9414df73adc799816b87465a6
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-01-19 21:42:09 +0400

    feat: tests refactor

--- src/state/slices/opportunitiesSlice/resolvers/thorchainsavers/utils.test.ts
+++ src/state/slices/opportunitiesSlice/resolvers/thorchainsavers/utils.test.ts
@@ -1,4 +1,5 @@
 import type { Asset } from '@shapeshiftoss/asset-service'
+import { AssetService } from '@shapeshiftoss/asset-service'
 import { btcAssetId, osmosisAssetId } from '@shapeshiftoss/caip'
 import axios from 'axios'
 
@@ -24,6 +25,17 @@ const thorchainErrorResponse = {
   error: 'Invalid pool',
 }
 
+let service: AssetService | undefined = undefined
+// do not export this, views get data from selectors
+// or directly from the store outside react components
+const getAssetService = () => {
+  if (!service) {
+    service = new AssetService()
+  }
+
+  return service
+}
+
 describe('resolvers/thorchainSavers/utils', () => {
   describe('getThorchainSaversQuote', () => {
     it('gets a quote for a valid pool AssetId', async () => {
@@ -33,12 +45,12 @@ describe('resolvers/thorchainSavers/utils', () => {
         }),
       )
 
-      const btcAssetMock = { assetId: btcAssetId, precision: 8 } as unknown as Asset
+      const btcAssetMock = getAssetService().getAll()[btcAssetId]
       const saversQuote = await getThorchainSaversQuote(btcAssetMock, '10000000')
 
       expect(saversQuote).toMatchObject(btcQuote)
     })
-    it('thros for an invalid pool AssetId', async () => {
+    it('throws for an invalid pool AssetId', async () => {
       mockAxios.get.mockImplementationOnce(() =>
         Promise.resolve({
           data: thorchainErrorResponse,
@@ -55,7 +67,7 @@ describe('resolvers/thorchainSavers/utils', () => {
         }),
       )
 
-      const btcAssetMock = { assetId: btcAssetId, precision: 8 } as unknown as Asset
+      const btcAssetMock = getAssetService().getAll()[btcAssetId]
       await expect(getThorchainSaversQuote(btcAssetMock, '10000000000000')).rejects.toThrow()
     })
   })

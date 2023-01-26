8a6c40351a79031a3b764bd6422a90b1e2dd68fd
pastaghost <pastaghost@2520.io>
2023-01-26 15:03:13 -0700

    refactor: remove osmosisprovider

--- src/features/defi/contexts/OsmosisProvider/useOsmosis.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react'
-
-import { OsmosisContext } from './OsmosisProvider'
-
-export const useOsmosis = () => {
-  const context = useContext(OsmosisContext)
-  if (!context) throw new Error("useOsmosis can't be used outside of the OsmosisProvider")
-  return context
-}

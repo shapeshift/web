7d23515e96ab5f0b50ae78c6d6115ee1858c3011
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-01-31 20:20:01 +0400

    feat(savers): feeless tracking memo

--- src/state/slices/opportunitiesSlice/resolvers/thorchainsavers/utils.ts
+++ src/state/slices/opportunitiesSlice/resolvers/thorchainsavers/utils.ts
@@ -35,13 +35,8 @@ const THOR_PRECISION = '8'
 const BASE_BPS_POINTS = '10000'
 
 export const THORCHAIN_AFFILIATE_NAME = 'ss'
-// TODO: We will need an affiliate address on all L1s, remove these two and get the programmatic address
-// for the current PoolId when that's done
-// For now, we use the affiliate name in place of an address, which won't result in affiliate fees
-// See: https://discord.com/channels/838986635756044328/1060678683486072903/1060821122423201843
-// const AFFILIATE_ADDRESS = THORCHAIN_AFFILIATE_NAME
-// Affiliate bps must be >0, so we set to 1 for the time being until we get L1 addresses rolling
-// const AFFILIATE_BPS = 1
+// BPS are needed as part of the memo, but 0bps won't incur any fees, only used for tracking purposes for now
+const AFFILIATE_BPS = 0
 
 // The minimum amount to be sent both for deposit and withdraws
 // else it will be considered a dust attack and gifted to the network
@@ -169,7 +164,10 @@ export const getThorchainSaversDepositQuote = async ({
   if (!quoteData || 'error' in quoteData)
     throw new Error(`Error fetching THORChain savers quote: ${quoteData?.error}`)
 
-  return quoteData
+  return {
+    ...quoteData,
+    memo: `${quoteData.memo}::${THORCHAIN_AFFILIATE_NAME}:${AFFILIATE_BPS}`,
+  }
 }
 
 export const getThorchainSaversWithdrawQuote = async ({

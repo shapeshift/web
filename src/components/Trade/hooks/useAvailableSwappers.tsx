3d645dd5fe86012f03b3c63d49b987e6c6e77356
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2023-02-01 09:56:01 +0900

    fix: race condition between form & custom hook state

--- src/components/Trade/hooks/useAvailableSwappers.tsx
+++ src/components/Trade/hooks/useAvailableSwappers.tsx
@@ -1,8 +1,10 @@
 import type { Asset } from '@shapeshiftoss/asset-service'
 import type { GetSwappersWithQuoteMetadataReturn } from '@shapeshiftoss/swapper'
 import { useEffect, useState } from 'react'
+import { useFormContext, useWatch } from 'react-hook-form'
 import { getSwapperManager } from 'components/Trade/hooks/useSwapper/swapperManager'
 import { useTradeQuoteService } from 'components/Trade/hooks/useTradeQuoteService'
+import type { TS } from 'components/Trade/types'
 import { isSome } from 'lib/utils'
 import { getSwappersApi } from 'state/apis/swapper/getSwappersApi'
 import { selectFeatureFlags } from 'state/slices/preferencesSlice/selectors'
@@ -12,6 +14,17 @@ type AvailableSwapperArgs = { feeAsset: Asset | undefined }
 
 // A helper hook to get the available swappers from the RTK API, mapping the SwapperTypes to swappers
 export const useAvailableSwappers = ({ feeAsset }: AvailableSwapperArgs) => {
+  // Form hooks
+  const { control } = useFormContext<TS>()
+  const sellTradeAsset = useWatch({ control, name: 'sellTradeAsset' })
+  const buyTradeAsset = useWatch({ control, name: 'buyTradeAsset' })
+
+  // Constants
+  const sellAsset = sellTradeAsset?.asset
+  const buyAsset = buyTradeAsset?.asset
+  const buyAssetId = buyAsset?.assetId
+  const sellAssetId = sellAsset?.assetId
+
   const [swappersWithQuoteMetadata, setSwappersWithQuoteMetadata] =
     useState<GetSwappersWithQuoteMetadataReturn>()
   const dispatch = useAppDispatch()
@@ -48,9 +61,23 @@ export const useAvailableSwappers = ({ feeAsset }: AvailableSwapperArgs) => {
             : undefined
         })
         .filter(isSome)
-      setSwappersWithQuoteMetadata(availableSwappersWithQuoteMetadata)
+      // Handle a race condition between form state and useTradeQuoteService
+      if (
+        tradeQuoteArgs?.buyAsset.assetId === buyAssetId &&
+        tradeQuoteArgs?.sellAsset.assetId === sellAssetId
+      ) {
+        setSwappersWithQuoteMetadata(availableSwappersWithQuoteMetadata)
+      }
     })()
-  }, [dispatch, featureFlags, feeAsset, getAvailableSwappers, tradeQuoteArgs])
+  }, [
+    buyAssetId,
+    dispatch,
+    featureFlags,
+    feeAsset,
+    getAvailableSwappers,
+    sellAssetId,
+    tradeQuoteArgs,
+  ])
 
   const bestSwapperWithQuoteMetadata = swappersWithQuoteMetadata?.[0]
 

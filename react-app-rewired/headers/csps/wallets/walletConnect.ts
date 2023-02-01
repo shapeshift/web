8b81437080657b91d796b04e6078c5f56c18158a
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2023-02-01 15:00:50 +0900

    chore: use REACT_APP_WALLET_CONNECT_RELAY_URL env var

--- react-app-rewired/headers/csps/wallets/walletConnect.ts
+++ react-app-rewired/headers/csps/wallets/walletConnect.ts
@@ -2,10 +2,10 @@ import type { Csp } from '../../types'
 
 export const csp: Csp = {
   'connect-src': [
+    process.env.REACT_APP_WALLET_CONNECT_RELAY_URL!,
     'wss://*.bridge.walletconnect.org/',
     'https://registry.walletconnect.com/api/v2/wallets',
     'https://api.etherscan.io',
-    'wss://relay.walletconnect.com',
   ],
   'img-src': ['https://imagedelivery.net/', 'https://registry.walletconnect.com/api/v2/logo/', '*'],
 }

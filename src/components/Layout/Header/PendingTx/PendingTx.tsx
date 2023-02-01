2bea1350c5d80e2834e7cca50667ae268b371c75
reallybeard <89934888+reallybeard@users.noreply.github.com>
2023-02-01 12:13:48 -0600

    Update PendingTx.tsx

--- src/components/Layout/Header/PendingTx/PendingTx.tsx
+++ src/components/Layout/Header/PendingTx/PendingTx.tsx
@@ -3,12 +3,19 @@ import {
   Box,
   Center,
   Circle,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
   IconButton,
   Popover,
   PopoverBody,
   PopoverContent,
   PopoverTrigger,
 } from '@chakra-ui/react'
+import { useState } from 'react'
 import { MdPending, MdPendingActions } from 'react-icons/md'
 import { useSelector } from 'react-redux'
 import { TransactionsGroupByDate } from 'components/TransactionHistory/TransactionsGroupByDate'
@@ -17,30 +24,38 @@ import { selectLastNTxIds } from 'state/slices/selectors'
 
 export const PendingTxWindow = () => {
   const recentTxIds = useSelector((state: ReduxState) => selectLastNTxIds(state, 10))
+  const [isOpen, setIsOpen] = useState(false)
   return (
-    <Popover placement='bottom-start'>
-      <PopoverTrigger>
-        <Box position='relative'>
-          <IconButton aria-label='Pending Transactions' icon={<MdPending />} />
-          <Circle
-            position='absolute'
-            size='18px'
-            fontSize='12px'
-            fontWeight='bold'
-            bg='blue.500'
-            color='white'
-            top='-0.2em'
-            right='-0.2em'
-          >
-            2
-          </Circle>
-        </Box>
-      </PopoverTrigger>
-      <PopoverContent minHeight='60vh' maxHeight='60vh' overflow='auto'>
-        <PopoverBody px={0}>
-          <TransactionsGroupByDate txIds={recentTxIds} useCompactMode />
-        </PopoverBody>
-      </PopoverContent>
-    </Popover>
+    <>
+      <Box position='relative'>
+        <IconButton
+          aria-label='Pending Transactions'
+          icon={<MdPending />}
+          onClick={() => setIsOpen(!isOpen)}
+        />
+        <Circle
+          position='absolute'
+          size='18px'
+          fontSize='12px'
+          fontWeight='bold'
+          bg='blue.500'
+          color='white'
+          top='-0.2em'
+          right='-0.2em'
+        >
+          2
+        </Circle>
+      </Box>
+      <Drawer isOpen={isOpen} onClose={() => setIsOpen(false)} size='sm'>
+        <DrawerOverlay backdropBlur='10px' />
+        <DrawerContent minHeight='100vh' maxHeight='100vh' overflow='auto'>
+          <DrawerCloseButton />
+          <DrawerHeader>Pending Transactions</DrawerHeader>
+          <DrawerBody px={0}>
+            <TransactionsGroupByDate txIds={recentTxIds} useCompactMode />
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
   )
 }

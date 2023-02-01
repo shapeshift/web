56230c9359b571abb638aa633124d8232b3536c9
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2023-02-01 12:51:37 +0900

    chore: whitelist wss://relay.walletconnect.com

--- react-app-rewired/headers/csps/wallets/walletConnect.ts
+++ react-app-rewired/headers/csps/wallets/walletConnect.ts
@@ -5,6 +5,7 @@ export const csp: Csp = {
     'wss://*.bridge.walletconnect.org/',
     'https://registry.walletconnect.com/api/v2/wallets',
     'https://api.etherscan.io',
+    'wss://relay.walletconnect.com',
   ],
   'img-src': ['https://imagedelivery.net/', 'https://registry.walletconnect.com/api/v2/logo/', '*'],
 }

0eec9a69eafc66ba9cf9816594cddb79355f618a
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2023-02-04 15:32:27 +0900

    fix: do not halt run sell asset

--- src/components/Trade/utils.ts
+++ src/components/Trade/utils.ts
@@ -1,5 +1,5 @@
 import type { AssetId } from '@shapeshiftoss/caip'
-import { getInboundAddressDataForChain, SwapperName } from '@shapeshiftoss/swapper'
+import { getInboundAddressDataForChain, isRune, SwapperName } from '@shapeshiftoss/swapper'
 import { getConfig } from 'config'
 
 export const isTradingActive = async (
@@ -10,9 +10,23 @@ export const isTradingActive = async (
     case SwapperName.Thorchain: {
       const daemonUrl = getConfig().REACT_APP_THORCHAIN_NODE_URL
       const inboundAddressData = await getInboundAddressDataForChain(daemonUrl, assetId, false)
-      // If we can't get inbound address data, we MUST assume trading is halted for safety
-      return inboundAddressData ? !inboundAddressData.halted : false
+      /*
+      Unless we are trading from RUNE, which has no inbound address data, we MUST
+      get confirmation that trading is not halted. We fail-closed for safety.
+       */
+      switch (true) {
+        // The sell asset is RUNE, there is no inbound address data to check against
+        case assetId && isRune(assetId):
+          return true
+        // We have inboundAddressData for the sell asset, check if it is halted
+        case !!inboundAddressData:
+          return !inboundAddressData!.halted
+        // We have no inboundAddressData for the sell asset, fail-closed
+        default:
+          return false
+      }
     }
+    // The swapper does not require any additional checks, we assume trading is active
     default:
       return true
   }

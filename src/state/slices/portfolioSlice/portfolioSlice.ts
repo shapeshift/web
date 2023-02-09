4987cb9fd93ea4f3fd8cd126dc8d872a1f1b6b61
reallybeard <89934888+reallybeard@users.noreply.github.com>
2023-02-09 09:45:15 -0600

    Update portfolioSlice.ts

--- src/state/slices/portfolioSlice/portfolioSlice.ts
+++ src/state/slices/portfolioSlice/portfolioSlice.ts
@@ -30,7 +30,9 @@ export const portfolio = createSlice({
       state.walletId = payload
       if (!payload) return
       state.wallet.ids = Array.from(new Set([...state.wallet.ids, payload]))
-      state.walletId && getMixPanel().alias(state.walletId)
+      if (state.walletId) {
+        getMixPanel().identify(state.walletId)
+      }
     },
     upsertAccountMetadata: (state, { payload }: { payload: AccountMetadataById }) => {
       moduleLogger.debug('upserting account metadata')

fd8a4052cda22e718106fd798c3c5345f3b298e0
highlander <bithighlander@gmail.com>
2023-02-11 13:46:19 -0500

    linting

--- src/context/WalletProvider/KeepKey/setupKeepKeySdk.ts
+++ src/context/WalletProvider/KeepKey/setupKeepKeySdk.ts
@@ -1,20 +1,20 @@
 import { KeepKeySdk } from '@keepkey/keepkey-sdk'
 
 export const setupKeepKeySDK = async () => {
-    let serviceKey = window.localStorage.getItem('@app/serviceKey')
-    let config: any = {
-        apiKey: serviceKey,
-        pairingInfo: {
-            name: 'ShapeShift',
-            imageUrl: 'https://assets.coincap.io/assets/icons/fox@2x.png',
-            basePath: 'http://localhost:1646/spec/swagger.json',
-            url: 'https://app.shapeshift.com',
-        },
-    }
-    let sdk = await KeepKeySdk.create(config)
+  let serviceKey = window.localStorage.getItem('@app/serviceKey')
+  let config: any = {
+    apiKey: serviceKey,
+    pairingInfo: {
+      name: 'ShapeShift',
+      imageUrl: 'https://assets.coincap.io/assets/icons/fox@2x.png',
+      basePath: 'http://localhost:1646/spec/swagger.json',
+      url: 'https://app.shapeshift.com',
+    },
+  }
+  let sdk = await KeepKeySdk.create(config)
 
-    if (!serviceKey) {
-        window.localStorage.setItem('@app/serviceKey', config.apiKey)
-    }
-    return sdk
+  if (!serviceKey) {
+    window.localStorage.setItem('@app/serviceKey', config.apiKey)
+  }
+  return sdk
 }

cd10fb07820ecc70355af8cc43d571e2da6292ee
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-02-13 08:14:44 +0400

    feat: add comment

--- src/state/slices/opportunitiesSlice/resolvers/foxy/types.ts
+++ src/state/slices/opportunitiesSlice/resolvers/foxy/types.ts
@@ -6,5 +6,8 @@ export type UserUndelegation = {
 }
 
 export type FoxySpecificUserStakingOpportunity = UserStakingOpportunityBase & {
+  // Undelegations is a Cosmos SDK specific terminology https://docs.cosmos.network/main/modules/staking
+  // The terminology has been reused here for FOXy to keep things abstracted, but Cosmos SDK undelegations
+  // and FOXy delayed withdraws are two very different implementations, on two different chains
   undelegations: UserUndelegation[]
 }

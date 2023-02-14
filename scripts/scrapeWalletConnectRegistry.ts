872a5539e2e68210658769adbda869bb94d69eeb
Fireb0mb1 <firebomb1@gmail.com>
2023-02-14 22:38:09 +0100

    fix: use walletconnect cloud api to scrape dapps
    Mapping the data structure of their new API to match our
    existing registry structure.

--- scripts/scrapeWalletConnectRegistry.ts
+++ scripts/scrapeWalletConnectRegistry.ts
@@ -1,26 +1,36 @@
 import * as fs from 'fs'
 import fetch from 'node-fetch'
 import * as path from 'path'
-import { RegistryItem } from '../src/plugins/walletConnectToDapps/types'
+import { RegistryItem, APIRegistryItem } from '../src/plugins/walletConnectToDapps/types'
 
-const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
-
-const registryJsonUrl =
-  'https://explorer.walletconnect.com/_next/data/rBOMJOAm1Nrct0fjtOOlA/index.json'
+const registryJsonUrl = 'https://explorer-api.walletconnect.com/v3/all?projectId=2f05ae7f1116030fde2d36508f472bfb'
 
 async function run() {
   const listings: RegistryItem[] = []
-  for (const searchQuery of ['', ...alphabet]) {
-    // eslint-disable-next-line no-console
-    console.log('Scraping listings for query: ', { searchQuery })
+  // eslint-disable-next-line no-console
+  console.log('Scraping dApps listings from WalletConnect Cloud API...')
 
-    const newListings = await fetch(`${registryJsonUrl}?search=${searchQuery}`)
-      .then(res => res.json())
-      .then(res => res.pageProps.listings.filter((l: RegistryItem) => !listings.some(ll => l.id === ll.id) && l.category !== 'wallet'))
-    listings.push(...newListings)
-  }
-
+  const newListings = await fetch(registryJsonUrl)
+      .then(response => response.json())
+      .then(data => {
+      const entries: APIRegistryItem[] = Object.values(data.listings)
+      const filteredEntries = entries.filter(entry => entry.app_type !== 'wallet')
+      // Mapping the WalledConnect API registry structure to our app registry structure.
+      return filteredEntries.map(
+        (entry: APIRegistryItem) => {
+          return {
+            category: entry.app_type,
+            id: entry.id,
+            homepage: entry.homepage,
+            name: entry.name,
+            image: entry.image_url.md
+          }
+        }
+      )
+    })
+  listings.push(...newListings)
   fs.writeFileSync(path.join(__dirname, '../src/plugins/walletConnectToDapps', 'registry.json'), JSON.stringify(listings, null, 2))
+  console.log(`${listings.length} entries scrapped.`)
 }
 
 run()

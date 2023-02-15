2996229ee99b0bbfa1e2d1166584aea73bec5c38
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2023-02-15 11:39:43 +1100

    feat: walletconnect registry scrape overrides

--- scripts/scrapeWalletConnectRegistry.ts
+++ scripts/scrapeWalletConnectRegistry.ts
@@ -6,11 +6,10 @@ import { RegistryItem, APIRegistryItem } from '../src/plugins/walletConnectToDap
 const registryJsonUrl = 'https://explorer-api.walletconnect.com/v3/all?projectId=2f05ae7f1116030fde2d36508f472bfb'
 
 async function run() {
-  const listings: RegistryItem[] = []
   // eslint-disable-next-line no-console
   console.log('Scraping dApps listings from WalletConnect Cloud API...')
 
-  const newListings = await fetch(registryJsonUrl)
+  const newData: RegistryItem[] = await fetch(registryJsonUrl)
       .then(response => response.json())
       .then(data => {
       const entries: APIRegistryItem[] = Object.values(data.listings)
@@ -28,9 +27,16 @@ async function run() {
         }
       )
     })
-  listings.push(...newListings)
-  fs.writeFileSync(path.join(__dirname, '../src/plugins/walletConnectToDapps', 'registry.json'), JSON.stringify(listings, null, 2))
-  console.log(`${listings.length} entries scrapped.`)
+
+  const overrideData = JSON.parse(fs.readFileSync(path.join(__dirname, '../src/plugins/walletConnectToDapps', 'registry-overrides.json'), 'utf8'))
+
+  overrideData.forEach((newObject: any) => {
+    const index = newData.findIndex((existingObject: any) => existingObject.id === newObject.id)
+    if (index !== -1) newData.splice(index, 1, newObject)
+  })
+
+  fs.writeFileSync(path.join(__dirname, '../src/plugins/walletConnectToDapps', 'registry.json'), JSON.stringify(newData, null, 2))
+  console.log(`${newData.length} entries scrapped.`)
 }
 
 run()

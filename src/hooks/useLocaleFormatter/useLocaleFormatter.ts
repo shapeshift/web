4147dc701c92ed75234a65d4e4a4bb7c5d034c7e
Fireb0mb1 <firebomb1@gmail.com>
2023-02-17 11:46:34 +0100

    fix: handle undefined options in number abbrev

--- src/hooks/useLocaleFormatter/useLocaleFormatter.ts
+++ src/hooks/useLocaleFormatter/useLocaleFormatter.ts
@@ -187,14 +187,18 @@ export const useLocaleFormatter = (args?: useLocaleFormatterArgs): NumberFormatt
         minimumFractionDigits,
         lessThanMin ? 6 : getFiatNumberFractionDigits(number),
       )
+      // Filter out undefined options caused by optional component props so they do not override the defaults
+      const filteredOptions = options
+        ? Object.fromEntries(Object.entries(options).filter(([_, value]) => value !== undefined))
+        : undefined
       const formatter = new Intl.NumberFormat(deviceLocale, {
         notation: number < bounds.min || noDecimals ? 'standard' : 'compact',
         compactDisplay: fiatType || number < longCompactDisplayLowerBound ? 'short' : 'long',
         style: fiatType ? 'currency' : 'decimal',
         currency: fiatType,
         minimumFractionDigits,
-        maximumFractionDigits: 10,
-        ...options,
+        maximumFractionDigits,
+        ...filteredOptions,
       })
 
       const parts = formatter.formatToParts(formatNumber)

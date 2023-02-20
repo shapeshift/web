57e9931c4507ac19981005acc89cb1ad5296d069
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2023-02-20 12:47:12 +1100

    chore: single source of truth image url

--- src/context/WalletProvider/KeepKey/setupKeepKeySdk.ts
+++ src/context/WalletProvider/KeepKey/setupKeepKeySdk.ts
@@ -1,26 +1,37 @@
 import type { PairingInfo } from '@keepkey/keepkey-sdk'
 import { KeepKeySdk } from '@keepkey/keepkey-sdk'
+import { foxAssetId } from '@shapeshiftoss/caip'
+import { store } from 'state/store'
 
-interface Config {
+type Config = {
   apiKey: string
   pairingInfo: PairingInfo
 }
 
 export const setupKeepKeySDK = async () => {
+  // is `notSet` some magic value?
   const serviceKey = window.localStorage.getItem('@app/serviceKey') || 'notSet'
-  let config: Config = {
+  const imageUrl = store.getState().assets.byId[foxAssetId]?.icon || ''
+
+  /**
+   * NOTE - the KeepKeySdk.create() call mutates the config it's passed in...
+   */
+  const config: Config = {
     apiKey: serviceKey,
     pairingInfo: {
       name: 'ShapeShift',
-      imageUrl: 'https://assets.coincap.io/assets/icons/fox@2x.png',
+      imageUrl,
       url: 'https://app.shapeshift.com',
     },
   }
+
   const sdk = await KeepKeySdk.create(config)
-  //If apiKey is revoked by wallet, or 'notSet' a user will be prompted to pair and new apiKey issued by wallet.
+
+  // If apiKey is revoked by wallet, or 'notSet' a user will be prompted to pair and new apiKey issued by wallet.
   if (serviceKey !== config.apiKey) {
-    //store apiKey to avoid needing to pair again
+    // store apiKey to avoid needing to pair again
     window.localStorage.setItem('@app/serviceKey', config.apiKey)
   }
+
   return sdk
 }

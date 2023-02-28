de9733dad87c0cdc4b1be4b371e88d2284b7ae23
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2023-02-28 09:20:15 -0800

    ci: cloudflare deploy (#3919)

--- scripts/bootstrap-env.ts
+++ scripts/bootstrap-env.ts
@@ -50,7 +50,7 @@ const getSerializedEnvVars = (environment: Environment) => {
  */
 const getSpecifiedEnvironment = (): Environment => {
   const args = process.argv.slice(2)
-  const branch = process.env.CF_PAGES_BRANCH
+  const branch = process.env.CF_PAGES_BRANCH || process.env.GITHUB_BRANCH_NAME
 
   // we're in a CI environment - we called the script as `yarn env` and hope the branch is set
   if (branch) {

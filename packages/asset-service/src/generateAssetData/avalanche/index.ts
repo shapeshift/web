b0b78e4202499ebaa84f11678981d3bdfce1e389
kevin <35275952+kaladinlight@users.noreply.github.com>
2023-03-01 10:30:31 -0700

    chore: add identicon generation to avalanche and optimism (#1216)

--- packages/asset-service/src/generateAssetData/avalanche/index.ts
+++ packages/asset-service/src/generateAssetData/avalanche/index.ts
@@ -1,10 +1,19 @@
 import { avalancheChainId } from '@shapeshiftoss/caip'
 
+import { Asset } from '../../service/AssetService'
+import { getRenderedIdenticonBase64 } from '../../service/GenerateAssetIcon'
 import { avax } from '../baseAssets'
 import * as coingecko from '../coingecko'
 
-export const getAssets = async () => {
+export const getAssets = async (): Promise<Asset[]> => {
   const assets = await coingecko.getAssets(avalancheChainId)
-  assets.push(avax)
-  return assets
+  return [...assets, avax].map((asset) => ({
+    ...asset,
+    icon:
+      asset.icon ||
+      getRenderedIdenticonBase64(asset.assetId, asset.symbol, {
+        identiconImage: { size: 128, background: [45, 55, 72, 255] },
+        identiconText: { symbolScale: 7, enableShadow: true },
+      }),
+  }))
 }
--- packages/asset-service/src/generateAssetData/optimism/index.ts
+++ packages/asset-service/src/generateAssetData/optimism/index.ts
@@ -1,10 +1,18 @@
 import { optimismChainId } from '@shapeshiftoss/caip'
 
+import { getRenderedIdenticonBase64 } from '../../service/GenerateAssetIcon'
 import { optimism } from '../baseAssets'
 import * as coingecko from '../coingecko'
 
 export const getAssets = async () => {
   const assets = await coingecko.getAssets(optimismChainId)
-  assets.push(optimism)
-  return assets
+  return [...assets, optimism].map((asset) => ({
+    ...asset,
+    icon:
+      asset.icon ||
+      getRenderedIdenticonBase64(asset.assetId, asset.symbol, {
+        identiconImage: { size: 128, background: [45, 55, 72, 255] },
+        identiconText: { symbolScale: 7, enableShadow: true },
+      }),
+  }))
 }

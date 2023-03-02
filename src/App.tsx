043b003c6ba7f05743216d0c8ed4e183d577a088
reallybeard <89934888+reallybeard@users.noreply.github.com>
2023-03-02 00:08:33 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ export const App = () => {
 
   useEffect(() => {
     logger.debug({ shouldUpdate, updateId }, 'Update Check')
-    if (!shouldUpdate && !toast.isActive(updateId)) {
+    if (shouldUpdate && !toast.isActive(updateId)) {
       const toastId = toast({
         render: ({ onClose }) => {
           return (

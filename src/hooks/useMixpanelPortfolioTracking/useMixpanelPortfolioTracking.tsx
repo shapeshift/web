2e0a87b9eaf3a99c0bd567bb97528523897659bb
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2023-03-14 16:30:28 -0600

    fix: better reactions for portfolio tracking

--- src/hooks/useMixpanelPortfolioTracking/useMixpanelPortfolioTracking.tsx
+++ src/hooks/useMixpanelPortfolioTracking/useMixpanelPortfolioTracking.tsx
@@ -1,14 +1,14 @@
 import { QueryStatus } from '@reduxjs/toolkit/dist/query'
 import { useEffect, useState } from 'react'
 import { createSelector } from 'reselect'
+import { useWallet } from 'hooks/useWallet/useWallet'
 import { getMixPanel } from 'lib/mixpanel/mixPanelSingleton'
 import { MixPanelEvents } from 'lib/mixpanel/types'
 import type { ReduxState } from 'state/reducer'
+import { selectWalletId } from 'state/slices/common-selectors'
 import { marketApi } from 'state/slices/marketDataSlice/marketDataSlice'
 import { selectPortfolioAnonymized, selectPortfolioAssetIds } from 'state/slices/selectors'
 import { useAppSelector } from 'state/store'
-
-import { useWallet } from '../useWallet/useWallet'
 
 // define selector here to avoid circular imports, specific to this hook below
 const selectMarketDataLoaded = createSelector(
@@ -26,18 +26,20 @@ const selectMarketDataLoaded = createSelector(
 export const useMixpanelPortfolioTracking = () => {
   const anonymizedPortfolio = useAppSelector(selectPortfolioAnonymized)
   const [isTracked, setIsTracked] = useState(false)
-  const { wallet, isDemoWallet } = useWallet().state
+  const { isDemoWallet } = useWallet().state
   const isMarketDataLoaded = useAppSelector(selectMarketDataLoaded)
+  const walletId = useAppSelector(selectWalletId)
 
   useEffect(() => {
     // only track anonymized portfolio for real wallets
     if (isDemoWallet) return
-    // we've changed wallets, reset tracking
-    if (!wallet) return setIsTracked(false)
     // only track once per wallet connection
     if (isTracked) return
     // only track if market data is loaded
     if (!isMarketDataLoaded) return
+
+    // only track if we have a wallet id
+    if (!walletId) return
 
     const mp = getMixPanel()
     if (!mp) return
@@ -47,5 +49,10 @@ export const useMixpanelPortfolioTracking = () => {
     mp.people.set(anonymizedPortfolio) // TODO(0xdef1cafe): restructure multiple wallets per user
     // don't track again for this wallet connection session
     setIsTracked(true)
-  }, [anonymizedPortfolio, isDemoWallet, isMarketDataLoaded, isTracked, wallet])
+  }, [anonymizedPortfolio, isDemoWallet, isMarketDataLoaded, isTracked, walletId])
+
+  useEffect(() => {
+    // we've changed wallets, reset tracking
+    setIsTracked(false)
+  }, [walletId])
 }

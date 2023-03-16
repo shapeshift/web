20fbc84aa2023d8c86c19a1a847106faf4d1dcd5
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2023-03-16 13:12:56 -0600

    feat: mixpanel empty wallet tracking (#4059)

--- src/lib/mixpanel/types.ts
+++ src/lib/mixpanel/types.ts
@@ -46,6 +46,7 @@ export type TrackOpportunityProps = {
 }
 
 export type AnonymizedPortfolio = {
+  'Has Crypto Balance': boolean
   'Is Mobile': boolean
   'Wallet ID': string // e.g. 2398734895
   'Wallet Name': string // e.g. 'Native' | 'Metamask' | 'WalletConnect'

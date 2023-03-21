ce1a0c441e97956740a4ac09e9d9e8b413fae3f7
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2023-03-21 13:31:39 -0600

    test: better test

--- src/state/slices/assetsSlice/assetsSlice.test.ts
+++ src/state/slices/assetsSlice/assetsSlice.test.ts
@@ -1,5 +1,6 @@
 import type { AssetId } from '@shapeshiftoss/caip'
 import { ASSET_NAMESPACE, ethChainId, toAssetId } from '@shapeshiftoss/caip'
+import { ethereum } from 'test/mocks/assets'
 import { mockChainAdapters } from 'test/mocks/portfolio'
 
 import type { MinimalAsset } from './assetsSlice'
@@ -20,12 +21,24 @@ describe('makeAsset', () => {
     const assetId: AssetId = toAssetId({ chainId, assetNamespace, assetReference })
     const minimalAsset: MinimalAsset = { assetId, symbol, precision, name }
     const asset = makeAsset(minimalAsset)
+    const asset2 = makeAsset(minimalAsset)
 
+    // doesn't molest asset id, precision, symbol, name
     expect(asset.assetId).toEqual(assetId)
+    expect(asset.name).toEqual(name)
+    expect(asset.symbol).toEqual(symbol)
+    expect(asset.precision).toEqual(precision)
+
+    // belongs to correct chain
     expect(asset.chainId).toEqual(chainId)
 
-    expect(asset.explorer).toBeDefined()
-    expect(asset.explorerTxLink).toBeDefined()
-    expect(asset.explorerAddressLink).toBeDefined()
+    // inherits fee asset explorer links
+    expect(asset.explorer).toEqual(ethereum.explorer)
+    expect(asset.explorerTxLink).toEqual(ethereum.explorerTxLink)
+    expect(asset.explorerAddressLink).toEqual(ethereum.explorerAddressLink)
+
+    // is deterministic on color and icon
+    expect(asset.color).toEqual(asset2.color)
+    expect(asset.icon).toEqual(asset2.icon)
   })
 })

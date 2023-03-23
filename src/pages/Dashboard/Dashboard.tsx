7468d1ae500085905fa17e6b181040795928f4dc
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-03-23 18:36:41 +0100

    feat: consume flags

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,17 +1,43 @@
+import { Stack } from '@chakra-ui/react'
 import { useTranslate } from 'react-polyglot'
 import { Main } from 'components/Layout/Main'
 import { SEO } from 'components/Layout/Seo'
+import { useFeatureFlag } from 'hooks/useFeatureFlag/useFeatureFlag'
 
 import { DashboardHeader } from './components/DashboardHeader'
+import { DashboardSidebar } from './DashboardSidebar'
+import { Portfolio } from './Portfolio'
 import { WalletDashboard } from './WalletDashboard'
 
 export const Dashboard = () => {
   const translate = useTranslate()
-  return (
-    <Main headerComponent={<DashboardHeader />}>
-      <SEO title={translate('navBar.dashboard')} />
+  const isDashboardTabsEnabled = useFeatureFlag('DashboardTabs')
 
-      <WalletDashboard />
+  if (isDashboardTabsEnabled)
+    return (
+      <Main headerComponent={<DashboardHeader />}>
+        <SEO title={translate('navBar.dashboard')} />
+
+        <WalletDashboard />
+      </Main>
+    )
+
+  return (
+    <Main>
+      <SEO title={translate('navBar.dashboard')} />
+      <Stack
+        alignItems='flex-start'
+        spacing={4}
+        mx='auto'
+        direction={{ base: 'column', xl: 'row' }}
+      >
+        <Stack spacing={4} flex='1 1 0%' width='full'>
+          <Portfolio />
+        </Stack>
+        <Stack flex='1 1 0%' width='full' maxWidth={{ base: 'full', xl: 'sm' }} spacing={4}>
+          <DashboardSidebar />
+        </Stack>
+      </Stack>
     </Main>
   )
 }

fbeda2a8af2d89d556cf65f1ada820d95750a5f8
reallybeard <89934888+reallybeard@users.noreply.github.com>
2023-03-27 11:29:53 -0500

    Update ResultsEmpty.tsx

--- src/components/StakingVaults/ResultsEmpty.tsx
+++ src/components/StakingVaults/ResultsEmpty.tsx
@@ -1,3 +1,4 @@
+import { ArrowForwardIcon } from '@chakra-ui/icons'
 import { Button, Circle, Flex } from '@chakra-ui/react'
 import { useColorModeValue } from '@chakra-ui/system'
 import { useTranslate } from 'react-polyglot'
@@ -12,7 +13,7 @@ export const ResultsEmpty = () => {
     <Flex p={6} textAlign='center' alignItems='center' width='full' flexDir='column' gap={4}>
       <Flex>
         <Circle bg={bgColor} size='40px'>
-          <DefiIcon />
+          <DefiIcon boxSize='24px' color='purple.500' />
         </Circle>
       </Flex>
       <Flex alignItems='center' textAlign='center' flexDir='column' gap={2}>
@@ -27,7 +28,13 @@ export const ResultsEmpty = () => {
           letterSpacing='0.012em'
           translation='assets.assetCards.stakingBody'
         />
-        <Button as={Link} to='/defi/earn' mt={4}>
+        <Button
+          colorScheme='purple'
+          as={Link}
+          to='/defi/earn'
+          mt={4}
+          rightIcon={<ArrowForwardIcon />}
+        >
           {translate('defi.startEarning')}
         </Button>
       </Flex>

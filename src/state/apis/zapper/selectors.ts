3529d5c8b9b807085128b2c479a3d9f8c2108911
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-03-28 23:33:19 +0200

    feat: git rm src/state/apis/zapper/selectors.ts

--- src/state/apis/zapper/selectors.ts
+++
@@ -1,5 +0,0 @@
-import { QueryStatus } from '@reduxjs/toolkit/query'
-import type { ReduxState } from 'state/reducer'
-
-export const selectZapperApiFullfilled = (state: ReduxState) =>
-  Object.values(state.zapperApi.queries).filter(query => query?.status === QueryStatus.fulfilled)

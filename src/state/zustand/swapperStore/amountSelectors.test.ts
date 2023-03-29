09f94cac4436246a761fda0e1a157312ed24e06b
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2023-03-29 19:12:39 +1100

    fix: tests

--- src/state/zustand/swapperStore/amountSelectors.test.ts
+++ src/state/zustand/swapperStore/amountSelectors.test.ts
@@ -10,11 +10,12 @@ describe('calculateAmounts', () => {
     const state: SwapperState = { ...baseSwapperState, action, amount }
     const tradeAmountsByActionAndAmount = selectTradeAmountsByActionAndAmount(state)
 
+    // Negative values are expected as the swapper fees are higher than the sell amount
     expect(tradeAmountsByActionAndAmount).toEqual({
       sellAmountSellAssetBaseUnit: '1000000000000000',
-      buyAmountBuyAssetBaseUnit: '53540633027089177171.70247933884298653524',
+      buyAmountBuyAssetBaseUnit: '-204624938472859992504',
       fiatSellAmount: '1.767',
-      fiatBuyAmount: '1.766840889',
+      fiatBuyAmount: '-6.752622954',
     })
   })
 
@@ -24,9 +25,9 @@ describe('calculateAmounts', () => {
     const tradeAmountsByActionAndAmount = selectTradeAmountsByActionAndAmount(state)
 
     expect(tradeAmountsByActionAndAmount).toEqual({
-      sellAmountSellAssetBaseUnit: '616298811544992',
+      sellAmountSellAssetBaseUnit: '5437298811544992',
       buyAmountBuyAssetBaseUnit: '33000000000000000000',
-      fiatSellAmount: '1.088472',
+      fiatSellAmount: '9.607179',
       fiatBuyAmount: '1.089',
     })
   })
@@ -38,9 +39,9 @@ describe('calculateAmounts', () => {
 
     expect(tradeAmountsByActionAndAmount).toEqual({
       sellAmountSellAssetBaseUnit: '18675721561969440',
-      buyAmountBuyAssetBaseUnit: '999995178481634632496',
+      buyAmountBuyAssetBaseUnit: '741829607527140007496',
       fiatSellAmount: '33',
-      fiatBuyAmount: '32.999840874',
+      fiatBuyAmount: '24.480377031',
     })
   })
 
@@ -50,9 +51,9 @@ describe('calculateAmounts', () => {
     const tradeAmountsByActionAndAmount = selectTradeAmountsByActionAndAmount(state)
 
     expect(tradeAmountsByActionAndAmount).toEqual({
-      sellAmountSellAssetBaseUnit: '18675721561969440',
+      sellAmountSellAssetBaseUnit: '23496721561969440',
       buyAmountBuyAssetBaseUnit: '1000000000000000000000',
-      fiatSellAmount: '32.998725',
+      fiatSellAmount: '41.517432',
       fiatBuyAmount: '33',
     })
   })

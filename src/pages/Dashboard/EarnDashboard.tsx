6727b993b25fb3aec9ef3d518b71d51343cad526
reallybeard <89934888+reallybeard@users.noreply.github.com>
2023-03-30 14:31:30 -0500

    add text for the earn/rewards tabs

--- src/pages/Dashboard/EarnDashboard.tsx
+++ src/pages/Dashboard/EarnDashboard.tsx
@@ -1,12 +1,44 @@
+import { ArrowForwardIcon } from '@chakra-ui/icons'
+import { Button, Flex, Heading } from '@chakra-ui/react'
+import { useTranslate } from 'react-polyglot'
+import { Link as NavLink } from 'react-router-dom'
 import { Main } from 'components/Layout/Main'
 import { DeFiEarn } from 'components/StakingVaults/DeFiEarn'
+import { RawText } from 'components/Text'
 
 import { DashboardHeader } from './components/DashboardHeader'
+
+const EarnHeader = () => {
+  const translate = useTranslate()
+
+  return (
+    <Flex
+      alignItems={{ base: 'flex-start', md: 'center' }}
+      gap={4}
+      px={4}
+      flexDir={{ base: 'column', md: 'row' }}
+    >
+      <Heading fontSize='xl'>{translate('defi.myPositions')}</Heading>
+      <RawText color='gray.500'>{translate('defi.myPositionsBody')}</RawText>
+      <Button
+        colorScheme='purple'
+        variant='ghost'
+        as={NavLink}
+        to='/earn'
+        size='sm'
+        ml='auto'
+        rightIcon={<ArrowForwardIcon />}
+      >
+        {translate('defi.viewAllPositions')}
+      </Button>
+    </Flex>
+  )
+}
 
 export const EarnDashboard = () => {
   return (
     <Main headerComponent={<DashboardHeader />}>
-      <DeFiEarn includeEarnBalances />
+      <DeFiEarn includeEarnBalances header={<EarnHeader />} />
     </Main>
   )
 }
--- src/pages/Dashboard/RewardsDashboard.tsx
+++ src/pages/Dashboard/RewardsDashboard.tsx
@@ -1,12 +1,43 @@
+import { ArrowForwardIcon } from '@chakra-ui/icons'
+import { Button, Flex, Heading } from '@chakra-ui/react'
+import { useTranslate } from 'react-polyglot'
+import { Link as NavLink } from 'react-router-dom'
 import { Main } from 'components/Layout/Main'
 import { DeFiEarn } from 'components/StakingVaults/DeFiEarn'
+import { RawText } from 'components/Text'
 
 import { DashboardHeader } from './components/DashboardHeader'
+
+const RewardsHeader = () => {
+  const translate = useTranslate()
+  return (
+    <Flex
+      alignItems={{ base: 'flex-start', md: 'center' }}
+      gap={4}
+      px={4}
+      flexDir={{ base: 'column', md: 'row' }}
+    >
+      <Heading fontSize='xl'>{translate('defi.myRewards')}</Heading>
+      <RawText color='gray.500'>{translate('defi.myRewardsBody')}</RawText>
+      <Button
+        colorScheme='purple'
+        as={NavLink}
+        to='/earn'
+        size='sm'
+        variant='ghost'
+        ml='auto'
+        rightIcon={<ArrowForwardIcon />}
+      >
+        {translate('defi.viewAllPositions')}
+      </Button>
+    </Flex>
+  )
+}
 
 export const RewardsDashboard = () => {
   return (
     <Main headerComponent={<DashboardHeader />}>
-      <DeFiEarn includeRewardsBalances />
+      <DeFiEarn includeRewardsBalances header={<RewardsHeader />} />
     </Main>
   )
 }

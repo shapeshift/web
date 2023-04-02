1fe5afa3a8bda46f19f5775c2aee3265f3119aa6
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-04-02 12:06:31 +0200

    feat: use 'Approve' copy instead of 'Approve on wallet

--- src/features/defi/providers/idle/components/IdleManager/Deposit/components/Approve.tsx
+++ src/features/defi/providers/idle/components/IdleManager/Deposit/components/Approve.tsx
@@ -240,7 +240,7 @@ export const Approve: React.FC<IdleApproveProps> = ({ accountId, onNext }) => {
         .times(feeMarketData.price)
         .toFixed(2)}
       loading={state.loading}
-      loadingText={translate('common.approveOnWallet')}
+      loadingText={translate('common.approve')}
       preFooter={preFooter}
       providerIcon={underlyingAsset?.icon}
       learnMoreLink='https://shapeshift.zendesk.com/hc/en-us/articles/360018501700'

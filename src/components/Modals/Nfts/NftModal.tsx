ba02fdd9d2660de9725b020c679ac79be6cc2866
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2023-04-04 13:17:31 -0600

    feat: hide collection tab if collection data missing

--- src/components/Modals/Nfts/NftModal.tsx
+++ src/components/Modals/Nfts/NftModal.tsx
@@ -67,6 +67,7 @@ export const NftModal: React.FC<NftModalProps> = ({ zapperNft }) => {
   const name = zapperNft?.name
   const collectionName = zapperNft?.collection?.name
   const collectionAddress = zapperNft?.collection?.address
+  const hasCollectionData = Boolean(zapperNft?.collection)
 
   // const collectionLink = (() => {
   //   const zapperNetwork = zapperNft?.collection?.network
@@ -170,7 +171,7 @@ export const NftModal: React.FC<NftModalProps> = ({ zapperNft }) => {
           <TabList gap={4} px={8} bg={modalHeaderBg}>
             <NftTab>{translate('nft.overview')}</NftTab>
             <NftTab>{translate('nft.properties')}</NftTab>
-            <NftTab>{translate('nft.collection')}</NftTab>
+            {hasCollectionData && <NftTab>{translate('nft.collection')}</NftTab>}
           </TabList>
           <TabIndicator mt='-1.5px' height='2px' bg='blue.200' borderRadius='1px' />
         </Box>
@@ -189,7 +190,7 @@ export const NftModal: React.FC<NftModalProps> = ({ zapperNft }) => {
         </TabPanels>
       </Tabs>
     )
-  }, [modalHeaderBg, translate])
+  }, [hasCollectionData, modalHeaderBg, translate])
 
   const nftModalContent = useMemo(() => {
     return (

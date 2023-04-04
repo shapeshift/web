cbfffae25b9759465ff80bad65b3ccb9bd7363e7
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2023-04-04 14:37:12 -0600

    add investor and logger

---
+++ packages/logger/src/format.test.ts
@@ -0,0 +1,64 @@
+import format from './format'
+
+class RuntimeError extends Error {}
+
+class ErrorWithDetails extends Error {
+  cause: unknown
+  code?: string
+  details?: Record<string, unknown>
+}
+
+describe('format(x: any)', () => {
+  it('should format a string to a message', () => {
+    expect(format('asdf')).toStrictEqual({ message: 'asdf' })
+  })
+
+  it('should pass through an object', () => {
+    expect(format({ name: 'name' })).toStrictEqual({ name: 'name' })
+  })
+
+  it('should format a normal error', () => {
+    const err = new RuntimeError('Some error')
+    expect(format(err)).toStrictEqual({
+      error: {
+        message: err.message,
+        stack: expect.stringMatching(/_callCircusTest(?!_runTestsForDescribeBlock)/m),
+        kind: 'RuntimeError',
+      },
+    })
+  })
+
+  it('should format an error with cause and details', () => {
+    const err = new ErrorWithDetails('details')
+    err.code = 'ERR_TEST'
+    err.details = { foo: 'bar' }
+    err.cause = new Error('cause')
+    expect(format(err)).toStrictEqual({
+      error: {
+        code: 'ERR_TEST',
+        cause: {
+          error: {
+            message: 'cause',
+            stack: expect.stringMatching(/_callCircusTest(?!_runTestsForDescribeBlock)/m),
+            kind: 'Error',
+          },
+        },
+        details: { foo: 'bar' },
+        message: err.message,
+        stack: expect.stringMatching(/_callCircusTest(?!_runTestsForDescribeBlock)/m),
+        kind: 'ErrorWithDetails',
+      },
+    })
+  })
+
+  it('should throw if provided an array', () => {
+    expect(() => format([])).toThrowError(Error)
+  })
+
+  it.each([[undefined], [null], [0], [true], [new Date()], [/abc/]])(
+    'should return undefined for any other value',
+    (x) => {
+      expect(format(x)).toBeUndefined()
+    },
+  )
+})

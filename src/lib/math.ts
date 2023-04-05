ffac532f2cbe5a07d8d0b77bad5ec9c5586dff19
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-04-05 15:14:52 +0200

    feat: stakingByFilterIncludeEmpty return precision amounts

--- src/lib/math.ts
+++ src/lib/math.ts
@@ -3,15 +3,18 @@ import BigNumber from 'bignumber.js'
 import type { BN } from './bignumber/bignumber'
 import { bn, bnOrZero } from './bignumber/bignumber'
 
+// Converts from base unit to a precision/ish number
+// If no displayDecimals are provided, it will use the full precision of the number
+// If displayDecimals are provided, it will use that precision, to return e.g a human, precision, or number stripped to any arbitrary decimal places
 export const fromBaseUnit = (
   value: BigNumber.Value,
-  decimals: number,
-  displayDecimals = 6,
+  precision: number,
+  displayDecimals = 18,
 ): string => {
   return bnOrZero(value)
-    .div(`1e+${decimals}`)
+    .div(bn(10).pow(precision))
     .decimalPlaces(displayDecimals, BigNumber.ROUND_DOWN)
-    .toString()
+    .toFixed()
 }
 
 export const toBaseUnit = (amount: BigNumber.Value | undefined, precision: number): string => {

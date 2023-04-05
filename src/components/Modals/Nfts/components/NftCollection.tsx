f5f896081f4c07a9e86650b619356117532ca8f1
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2023-04-05 15:44:38 -0600

    feat: collection tab with description

--- src/components/Modals/Nfts/components/NftCollection.tsx
+++ src/components/Modals/Nfts/components/NftCollection.tsx
@@ -1,3 +1,8 @@
+import { Flex, Text } from '@chakra-ui/react'
+import { useTranslate } from 'react-polyglot'
+import { ParsedHtml } from 'components/ParsedHtml/ParsedHtml'
+import { RawText } from 'components/Text'
+import { markdownLinkToHTML } from 'lib/utils'
 import type { V2NftCollectionType } from 'state/apis/zapper/client'
 
 type NftCollectionProps = {
@@ -5,5 +10,19 @@ type NftCollectionProps = {
 }
 
 export const NftCollection: React.FC<NftCollectionProps> = ({ zapperCollection }) => {
-  return <div>collection tab content</div>
+  const translate = useTranslate()
+  const collection = zapperCollection?.[0]?.collection
+
+  if (!collection) return null
+
+  const { description, name: collectionName } = collection
+
+  return (
+    <Flex gap={4} flexDir='column' px={8} py={6}>
+      <Text fontWeight='medium'>{translate('nft.aboutCollection', { collectionName })}</Text>
+      <RawText color='gray.500'>
+        <ParsedHtml innerHtml={markdownLinkToHTML(description)} />
+      </RawText>
+    </Flex>
+  )
 }

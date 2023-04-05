f0f195dbf40272c05783b23e0728013c22687734
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2023-04-04 18:56:29 -0600

    chore: rename network to chain, add chain icon

--- src/components/Modals/Nfts/components/NftOverview.tsx
+++ src/components/Modals/Nfts/components/NftOverview.tsx
@@ -1,12 +1,15 @@
 import { Flex, Tag } from '@chakra-ui/react'
 import type { ChainId } from '@shapeshiftoss/caip'
 import { useTranslate } from 'react-polyglot'
+import { AssetIcon } from 'components/AssetIcon'
 import { Row } from 'components/Row/Row'
 import { Text } from 'components/Text'
 import { getChainAdapterManager } from 'context/PluginProvider/chainAdapterSingleton'
 import type { SupportedZapperNetwork } from 'state/apis/zapper/client'
 import { ZAPPER_NETWORKS_TO_CHAIN_ID_MAP } from 'state/apis/zapper/client'
+import { selectAssetById } from 'state/slices/assetsSlice/selectors'
 import { firstFourLastFour } from 'state/slices/portfolioSlice/utils'
+import { useAppSelector } from 'state/store'
 
 import type { NftModalProps } from '../NftModal'
 
@@ -19,9 +22,12 @@ export const NftOverview: React.FC<NftModalProps> = ({ zapperNft }) => {
   const maybeNetwork = collection?.network
   const nftStandard = collection?.nftStandard
 
-  const networkDisplayName = getChainAdapterManager()
-    .get(ZAPPER_NETWORKS_TO_CHAIN_ID_MAP[maybeNetwork as SupportedZapperNetwork] as ChainId)
-    ?.getDisplayName()
+  const maybeChainAdapter = getChainAdapterManager().get(
+    ZAPPER_NETWORKS_TO_CHAIN_ID_MAP[maybeNetwork as SupportedZapperNetwork] as ChainId,
+  )
+  const maybeFeeAssetId = maybeChainAdapter?.getFeeAssetId()
+  const chainDisplayName = maybeChainAdapter?.getDisplayName()
+  const maybeFeeAsset = useAppSelector(s => selectAssetById(s, maybeFeeAssetId ?? ''))
 
   return (
     <Flex flexDir='column'>
@@ -38,10 +44,14 @@ export const NftOverview: React.FC<NftModalProps> = ({ zapperNft }) => {
               <Row.Value>{firstFourLastFour(address)}</Row.Value>
             </Row>
           )}
-          {networkDisplayName && (
+          {chainDisplayName && (
             <Row>
-              <Row.Label>{translate('nft.network')}</Row.Label>
-              <Row.Value>{networkDisplayName}</Row.Value>
+              <Row.Label>{translate('nft.chain')}</Row.Label>
+              <Row.Value>
+                <>
+                  <AssetIcon src={maybeFeeAsset?.icon} size='xs' /> {chainDisplayName}
+                </>
+              </Row.Value>
             </Row>
           )}
           <Row>

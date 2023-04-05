659ab919440967ec9bb9ebcfbea85768f2e54377
reallybeard <89934888+reallybeard@users.noreply.github.com>
2023-04-05 15:30:34 -0500

    Update DashboardTab.tsx

--- src/pages/Dashboard/components/DashboardTab.tsx
+++ src/pages/Dashboard/components/DashboardTab.tsx
@@ -1,7 +1,6 @@
-import { Box, Button, Flex, useColorModeValue } from '@chakra-ui/react'
+import { Box, Flex, useColorModeValue } from '@chakra-ui/react'
 import type { ReactNode } from 'react'
 import { useCallback, useMemo } from 'react'
-import { useTranslate } from 'react-polyglot'
 import { useHistory, useLocation } from 'react-router'
 import { Amount } from 'components/Amount/Amount'
 import { Text } from 'components/Text'
@@ -23,7 +22,6 @@ export const DashboardTab: React.FC<DashboardTabProps> = ({
 }) => {
   const history = useHistory()
   const location = useLocation()
-  const translate = useTranslate()
   const handleClick = useCallback(() => {
     history.push(path)
   }, [history, path])
@@ -33,49 +31,38 @@ export const DashboardTab: React.FC<DashboardTabProps> = ({
 
   const isActive = useMemo(() => path === location.pathname, [location.pathname, path])
 
-  // return (
-  //   <Flex
-  //     as='a'
-  //     gap={4}
-  //     alignItems='center'
-  //     py={6}
-  //     px={8}
-  //     mb='-1px'
-  //     flex={1}
-  //     bg={isActive ? isActiveBg : 'transparent'}
-  //     onClick={handleClick}
-  //     _hover={{ bg: isActive ? isActiveBg : hoverBg }}
-  //     cursor='pointer'
-  //     borderLeftWidth={{ base: 0, md: 1 }}
-  //     borderRightWidth={{ base: 0, md: 1 }}
-  //     borderColor={isActive ? borderColor : 'transparent'}
-  //     position='relative'
-  //     _first={{ borderLeftWidth: 0 }}
-  //   >
-  //     <Box color={color} fontSize='3xl'>
-  //       {icon}
-  //     </Box>
-  //     <Flex flexDir='column'>
-  //       <Text
-  //         translation={label}
-  //         color='gray.500'
-  //         fontSize='sm'
-  //         fontWeight='medium'
-  //         lineHeight='shorter'
-  //       />
-  //       <Amount.Fiat value={fiatValue} fontSize='2xl' lineHeight='shorter' />
-  //     </Flex>
-  //   </Flex>
-  // )
   return (
-    <Button
-      variant='tab'
+    <Flex
+      as='a'
+      gap={4}
+      alignItems='center'
+      py={6}
+      px={8}
+      mb='-1px'
+      flex={1}
+      bg={isActive ? isActiveBg : 'transparent'}
       onClick={handleClick}
-      isActive={isActive}
-      _active={{ color, borderColor: color }}
-      _hover={{ color, borderColor: color }}
+      _hover={{ bg: isActive ? isActiveBg : hoverBg }}
+      cursor='pointer'
+      borderLeftWidth={{ base: 0, md: 1 }}
+      borderRightWidth={{ base: 0, md: 1 }}
+      borderColor={isActive ? borderColor : 'transparent'}
+      position='relative'
+      _first={{ borderLeftWidth: 0 }}
     >
-      {translate(label)}
-    </Button>
+      <Box color={color} fontSize='3xl'>
+        {icon}
+      </Box>
+      <Flex flexDir='column'>
+        <Text
+          translation={label}
+          color='gray.500'
+          fontSize='sm'
+          fontWeight='medium'
+          lineHeight='shorter'
+        />
+        <Amount.Fiat value={fiatValue} fontSize='2xl' lineHeight='shorter' />
+      </Flex>
+    </Flex>
   )
 }

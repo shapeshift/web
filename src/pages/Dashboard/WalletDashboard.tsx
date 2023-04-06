112e9cf22c1206a26d43c81694fe993db3c76192
reallybeard <89934888+reallybeard@users.noreply.github.com>
2023-04-06 11:16:53 -0500

    Update WalletDashboard.tsx

--- src/pages/Dashboard/WalletDashboard.tsx
+++ src/pages/Dashboard/WalletDashboard.tsx
@@ -1,24 +1,17 @@
-import { Flex, Stack } from '@chakra-ui/react'
+import { Stack } from '@chakra-ui/react'
 
 import { DashboardSidebar } from './DashboardSidebar'
 import { Portfolio } from './Portfolio'
 
 export const WalletDashboard = () => {
   return (
-    <Flex gap={4} flexDir='column'>
-      <Stack
-        alignItems='flex-start'
-        spacing={4}
-        mx='auto'
-        direction={{ base: 'column', xl: 'row' }}
-      >
-        <Stack spacing={4} flex='1 1 0%' width='full'>
-          <Portfolio />
-        </Stack>
-        <Stack flex='1 1 0%' width='full' maxWidth={{ base: 'full', xl: 'sm' }} spacing={4}>
-          <DashboardSidebar />
-        </Stack>
+    <Stack alignItems='flex-start' spacing={4} mx='auto' direction={{ base: 'column', xl: 'row' }}>
+      <Stack spacing={4} flex='1 1 0%' width='full'>
+        <Portfolio />
       </Stack>
-    </Flex>
+      <Stack flex='1 1 0%' width='full' maxWidth={{ base: 'full', xl: 'sm' }} spacing={4}>
+        <DashboardSidebar />
+      </Stack>
+    </Stack>
   )
 }

072cb82667cb5e3cb20ae55f4e92174ab3ca6086
woody <125113430+woodenfurniture@users.noreply.github.com>
2023-04-12 08:57:47 +1000

    feat: lifi swapper (#4009)
    * fixed invalid token symbol for lifi getUsdRate
    
    * draft implementation of lifi trade execution
    
    * working implementation of trade execution
    
    * handle gas for multi-hop swaps
    
    * implemented single-hop recommendation from lifi peeps
    
    * allow trades without use of ethers.js signer
    
    * fixed issue handling gas fees denominated in eth
    
    * tidy up gas processing
    
    * actioned code review feedback
    
    * added type guard for EvmChainAdapter
    
    * tidy up hard-coded approval gas
    
    * added test coverage for lifi asset and chain map utils
    
    * added more test coverage
    
    * added unit tests for minimum amount parsing
    
    * enable lifi swapper in develop
    
    * fixed missing case for bnb
    
    * fixed 'no receive address' error for lifi swapper
    
    * removed broken stack trace capturing in ErrorWithCause
    
    * fixed broken lifi trades due to lifi sdk mutating frozen input params
    
    * linting

--- react-app-rewired/headers/csps/defi/swappers/Lifi.ts
+++ react-app-rewired/headers/csps/defi/swappers/Lifi.ts
@@ -4,5 +4,7 @@ export const csp: Csp = {
   'connect-src': [
     // src/lib/swapper/LifiSwapper/LifiSwapper.ts
     'https://li.quest',
+    'https://mainnet.infura.io',
+    'https://api.avax.network',
   ],
 }

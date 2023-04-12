bf1af1b671e192fde9d4ae77f9e902baa275141c
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2023-04-12 17:09:17 +1000

    fix: straighten out lifi gas fees

--- src/lib/swapper/LifiSwapper/utils/transformLifiFeeData/transformLifiFeeData.ts
+++ src/lib/swapper/LifiSwapper/utils/transformLifiFeeData/transformLifiFeeData.ts
@@ -4,7 +4,6 @@ import type { EvmChainId } from '@shapeshiftoss/chain-adapters'
 import type { QuoteFeeData } from '@shapeshiftoss/swapper'
 import { APPROVAL_GAS_LIMIT } from '@shapeshiftoss/swapper/dist/swappers/utils/constants'
 import { bn, bnOrZero, toHuman } from 'lib/bignumber/bignumber'
-import { LIFI_GAS_FEE_BASE } from 'lib/swapper/LifiSwapper/utils/constants'
 import { getFeeAssets } from 'lib/swapper/LifiSwapper/utils/getFeeAssets/getFeeAssets'
 import { processGasCosts } from 'lib/swapper/LifiSwapper/utils/processGasCosts/processGasCosts'
 
@@ -62,24 +61,13 @@ export const transformLifiFeeData = ({
     initialSellAssetTradeFeeUsd,
   })
 
-  // the sum of all 'APPROVE' gas fees
-  // TODO: validate this with lifi
-  const approvalFeeCryptoBaseUnit =
-    allRouteGasCosts
-      .filter(gasCost => gasCost.type === 'APPROVE')
-      .reduce((a, v) => (v.estimate ? a.plus(bn(v.estimate, LIFI_GAS_FEE_BASE)) : a), bn(0)) ??
-    bn(0)
-
   return {
     networkFeeCryptoBaseUnit: networkFeeCryptoBaseUnit.toString(), // UI shows this next to the gas icon
     chainSpecific: {
-      // TODO: add gasPriceCryptoBaseUnit so approvals are not displayed as 0
-      // gasPriceCryptoBaseUnit: gas price for approval
-
       // lifi handles approval gas internally but need to set a gas limit so the
       // approval limit isnt exceeded when the trade is executed.
-      estimatedGas: APPROVAL_GAS_LIMIT,
-      approvalFeeCryptoBaseUnit: approvalFeeCryptoBaseUnit.toString(),
+      estimatedGas: APPROVAL_GAS_LIMIT, // the gas for the send tx, aka estimatedGasCryptoBaseUnit
+      approvalFeeCryptoBaseUnit: APPROVAL_GAS_LIMIT, // the gas for the erc20 approval tx
     },
     // UI shows the sum of these as "protocol fee"
     buyAssetTradeFeeUsd: buyAssetTradeFeeUsd.toString(),

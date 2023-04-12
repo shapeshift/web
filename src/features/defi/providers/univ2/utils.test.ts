bbe52cce56bb77a5ddb4d1d861ce6838ee176706
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-04-12 22:37:52 +0200

    feat: bad bot

--- src/features/defi/providers/univ2/utils.test.ts
+++ src/features/defi/providers/univ2/utils.test.ts
@@ -18,7 +18,7 @@ describe('calculateSlippageMargin', () => {
   it('should return the correct slippage margin for different amount and precision values', () => {
     const amount = '500'
     const precision = 6
-    const expectedResult = '497500'
+    const expectedResult = '497500000'
 
     expect(calculateSlippageMargin(amount, precision)).toBe(expectedResult)
   })
@@ -34,7 +34,7 @@ describe('calculateSlippageMargin', () => {
   it('should return the correct slippage margin for decimal values', () => {
     const amount = '123.456'
     const precision = 18
-    const expectedResult = '122857470000000000000'
+    const expectedResult = '122838720000000000000'
 
     expect(calculateSlippageMargin(amount, precision)).toBe(expectedResult)
   })
@@ -42,7 +42,7 @@ describe('calculateSlippageMargin', () => {
   it('should return the correct slippage margin for decimal values and precision 6', () => {
     const amount = '123.456'
     const precision = 6
-    const expectedResult = '122857'
+    const expectedResult = '122838720'
 
     expect(calculateSlippageMargin(amount, precision)).toBe(expectedResult)
   })
@@ -50,7 +50,7 @@ describe('calculateSlippageMargin', () => {
   it('should return the correct slippage margin for large amount and precision values', () => {
     const amount = '1000000000000000'
     const precision = 18
-    const expectedResult = '995000000000000000000000000000000000'
+    const expectedResult = '995000000000000000000000000000000'
 
     expect(calculateSlippageMargin(amount, precision)).toBe(expectedResult)
   })
@@ -58,7 +58,7 @@ describe('calculateSlippageMargin', () => {
   it('should return the correct slippage margin for large amount and precision 6', () => {
     const amount = '1000000000000000'
     const precision = 6
-    const expectedResult = '995000000000000000'
+    const expectedResult = '995000000000000000000'
 
     expect(calculateSlippageMargin(amount, precision)).toBe(expectedResult)
   })

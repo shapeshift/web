f9c7a2fdf7e5075d059b9d0e5e4ffc31ee27ae98
Łukasz Myśliński <lukasz@mlnative.com>
2023-04-19 17:12:40 +0200

    Test fix

--- packages/caip/src/adapters/coingecko/utils.test.ts
+++ packages/caip/src/adapters/coingecko/utils.test.ts
@@ -159,7 +159,7 @@ describe('adapters:coingecko:utils', () => {
         },
         'eip155:137': {
           'eip155:137/slip44:60': 'matic-network',
-          'eip155:137/erc20:0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2': 'weth',
+          'eip155:137/erc20:0x7ceb23fd6bc0add59e62ac25578270cff1b9f619': 'weth',
         },
       }
       expect(result).toEqual(expected)

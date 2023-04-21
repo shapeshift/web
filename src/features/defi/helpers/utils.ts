fc835f7a028565a6f66a099806e6b4857099466c
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2023-04-21 11:32:18 -0600

    add fee and build/broadcast helpers

--- src/features/defi/helpers/utils.ts
+++ src/features/defi/helpers/utils.ts
@@ -1,6 +1,9 @@
 import type { Asset } from '@shapeshiftoss/asset-service'
 import type { AccountId, ChainId } from '@shapeshiftoss/caip'
 import { cosmosChainId, osmosisChainId } from '@shapeshiftoss/caip'
+import type { evm, EvmChainAdapter } from '@shapeshiftoss/chain-adapters'
+import type { HDWallet } from '@shapeshiftoss/hdwallet-core'
+import { supportsETH } from '@shapeshiftoss/hdwallet-core'
 import { bnOrZero } from 'lib/bignumber/bignumber'
 import { selectPortfolioCryptoPrecisionBalanceByFilter } from 'state/slices/selectors'
 import { store } from 'state/store'
@@ -33,4 +36,69 @@ export const canCoverTxFees = ({
   })
 
   return bnOrZero(feeAssetBalanceCryptoHuman).minus(bnOrZero(estimatedGasCryptoPrecision)).gte(0)
+}
+
+type GetFeesArgs = {
+  wallet: HDWallet
+  feeData: evm.FeeData
+}
+
+export const getFees = async ({
+  wallet,
+  feeData: { gasLimit, gasPrice, maxFeePerGas, maxPriorityFeePerGas },
+}: GetFeesArgs): Promise<evm.Fees & { gasLimit: string }> => {
+  if (!supportsETH(wallet)) throw new Error('wallet has no evm support')
+  if (!gasLimit) throw new Error('gasLimit is required')
+
+  const eip1559Support = await wallet.ethSupportsEIP1559()
+
+  // use eip1559 fees if able
+  if (eip1559Support && maxFeePerGas && maxPriorityFeePerGas) {
+    return { gasLimit, maxFeePerGas, maxPriorityFeePerGas }
+  }
+
+  // fallback to legacy fees if unable to use eip1559
+  if (gasPrice) return { gasLimit, gasPrice }
+
+  throw new Error('legacy gas or eip1559 gas required')
+}
+
+type BuildAndBroadcastArgs = GetFeesArgs & {
+  accountNumber: number
+  adapter: EvmChainAdapter
+  data?: string
+  to: string
+  value: string
+}
+
+export const buildAndBroadcast = async ({
+  accountNumber,
+  adapter,
+  data,
+  feeData,
+  to,
+  value,
+  wallet,
+}: BuildAndBroadcastArgs) => {
+  const { txToSign } = await adapter.buildCustomTx({
+    wallet,
+    to,
+    accountNumber,
+    value,
+    data: data ?? '0x',
+    ...(await getFees({ wallet, feeData })),
+  })
+
+  if (wallet.supportsOfflineSigning()) {
+    const signedTx = await adapter.signTransaction({ txToSign, wallet })
+    const txid = await adapter.broadcastTransaction(signedTx)
+    return txid
+  }
+
+  if (wallet.supportsBroadcast() && adapter.signAndBroadcastTransaction) {
+    const txid = await adapter.signAndBroadcastTransaction({ txToSign, wallet })
+    return txid
+  }
+
+  throw new Error('buildAndBroadcast: no broadcast support')
 }

f4dc6ee7c29533dc594f396a7758b40a74150cb0
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-04-27 22:09:31 +0200

    feat: more throws removed

--- src/lib/swapper/swappers/OneInchSwapper/getTradeQuote/getTradeQuote.ts
+++ src/lib/swapper/swappers/OneInchSwapper/getTradeQuote/getTradeQuote.ts
@@ -3,12 +3,13 @@ import type { EvmBaseAdapter, EvmChainId } from '@shapeshiftoss/chain-adapters'
 import { isEvmChainId } from '@shapeshiftoss/chain-adapters'
 import type { GasFeeDataEstimate } from '@shapeshiftoss/chain-adapters/src/evm/types'
 import type { Result } from '@sniptt/monads'
+import { Err } from '@sniptt/monads'
 import type { AxiosResponse } from 'axios'
 import axios from 'axios'
 import { getChainAdapterManager } from 'context/PluginProvider/chainAdapterSingleton'
 import { bn, bnOrZero } from 'lib/bignumber/bignumber'
 import type { GetEvmTradeQuoteInput, SwapErrorRight, TradeQuote } from 'lib/swapper/api'
-import { SwapError, SwapErrorType } from 'lib/swapper/api'
+import { makeSwapErrorRight, SwapErrorType } from 'lib/swapper/api'
 
 import { isNativeEvmAsset } from '../../utils/helpers/helpers'
 import { getApprovalAddress } from '../getApprovalAddress/getApprovalAddress'
@@ -24,9 +25,12 @@ export async function getTradeQuote(
   const { chainId, sellAsset, buyAsset, sellAmountBeforeFeesCryptoBaseUnit, accountNumber } = input
 
   if (sellAsset.chainId !== buyAsset.chainId) {
-    throw new SwapError('[getTradeQuote] cross chain swaps not supported', {
-      code: SwapErrorType.UNSUPPORTED_PAIR,
-    })
+    return Err(
+      makeSwapErrorRight({
+        message: '[getTradeQuote] cross chain swaps not supported',
+        code: SwapErrorType.UNSUPPORTED_PAIR,
+      }),
+    )
   }
 
   if (
@@ -34,23 +38,29 @@ export async function getTradeQuote(
     !isEvmChainId(sellAsset.chainId) ||
     !isEvmChainId(buyAsset.chainId)
   ) {
-    throw new SwapError('[getTradeQuote] invalid chainId', {
-      code: SwapErrorType.UNSUPPORTED_CHAIN,
-    })
+    return Err(
+      makeSwapErrorRight({
+        message: '[getTradeQuote] invalid chainId',
+        code: SwapErrorType.UNSUPPORTED_CHAIN,
+      }),
+    )
   }
 
   if (isNativeEvmAsset(sellAsset.assetId) || isNativeEvmAsset(buyAsset.assetId)) {
-    throw new SwapError('[getTradeQuote] 1inch swapper only supports ERC20s', {
-      code: SwapErrorType.UNSUPPORTED_CHAIN,
-    })
+    return Err(
+      makeSwapErrorRight({
+        message: '[getTradeQuote] 1inch swapper only supports ERC20s',
+        code: SwapErrorType.UNSUPPORTED_CHAIN,
+      }),
+    )
   }
 
   if (sellAmountBeforeFeesCryptoBaseUnit === '0') {
-    throw new SwapError(
-      '[getTradeQuote] sellAmountBeforeFeesCryptoBaseUnit must be greater than 0',
-      {
+    return Err(
+      makeSwapErrorRight({
+        message: '[getTradeQuote] sellAmountBeforeFeesCryptoBaseUnit must be greater than 0',
         code: SwapErrorType.TRADE_BELOW_MINIMUM,
-      },
+      }),
     )
   }
 
@@ -77,10 +87,13 @@ export async function getTradeQuote(
   // We guard against !isEvmChainId(chainId) above, so this cast is safe
   const adapter = chainAdapterManager.get(chainId) as unknown as EvmBaseAdapter<EvmChainId>
   if (adapter === undefined) {
-    throw new SwapError('[getTradeQuote] - getChainAdapterManager returned undefined', {
-      code: SwapErrorType.UNSUPPORTED_NAMESPACE,
-      details: { chainId: sellAsset.chainId },
-    })
+    return Err(
+      makeSwapErrorRight({
+        message: '[getTradeQuote] - getChainAdapterManager returned undefined',
+        code: SwapErrorType.UNSUPPORTED_NAMESPACE,
+        details: { chainId: sellAsset.chainId },
+      }),
+    )
   }
   const gasFeeData: GasFeeDataEstimate = await adapter.getGasFeeData()
 

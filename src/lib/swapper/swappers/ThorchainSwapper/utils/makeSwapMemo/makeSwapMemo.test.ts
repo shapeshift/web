563f9a4cec0058403f07a5e6b867f8f011485527
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2023-04-28 16:07:48 +1000

    chore: add long memo test

--- src/lib/swapper/swappers/ThorchainSwapper/utils/makeSwapMemo/makeSwapMemo.test.ts
+++ src/lib/swapper/swappers/ThorchainSwapper/utils/makeSwapMemo/makeSwapMemo.test.ts
@@ -33,9 +33,22 @@ describe('makeSwapMemo', () => {
       buyAssetId: 'bip122:000000000019d6689c085ae165831e93/slip44:0',
       destinationAddress: 'bc1qkw9g3tgv6m2gwc4x4hvdefcwt0uxeedfgag27h',
       limit: '420',
+      affiliateBps: '333',
+    })
+    expect(memo).toEqual('s:BTC.BTC:bc1qkw9g3tgv6m2gwc4x4hvdefcwt0uxeedfgag27h:420:ss:333')
+  })
+
+  it('should make a trade with a long memo', () => {
+    const memo = makeSwapMemo({
+      buyAssetId: 'eip155:1/erc20:0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+      destinationAddress: '0x8a65ac0E23F31979db06Ec62Af62b132a6dF4741',
+      limit: '10000000000000',
       affiliateBps: '1000',
     })
-    expect(memo).toEqual('s:BTC.BTC:bc1qkw9g3tgv6m2gwc4x4hvdefcwt0uxeedfgag27h:420:ss:1000')
+    expect(memo).toEqual(
+      // This is the longers memo we can make (80 chars)
+      's:ETH.USDC-B48:0x8a65ac0E23F31979db06Ec62Af62b132a6dF4741:10000000000000:ss:1000',
+    )
   })
 
   it('should make a trade with passing a 0 affiliate fee', () => {

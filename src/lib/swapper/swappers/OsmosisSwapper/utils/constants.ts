0d2a66d22e42c5a59bc604ec9b8d8e7b40db3547
pastaghost <pastaghost@2520.io>
2023-04-28 09:13:56 -0600

    fix: use assetid literal

--- src/lib/swapper/swappers/OsmosisSwapper/utils/constants.ts
+++ src/lib/swapper/swappers/OsmosisSwapper/utils/constants.ts
@@ -1,4 +1,3 @@
-import { adapters } from '@shapeshiftoss/caip'
 import { SwapperName } from 'lib/swapper/api'
 
 export const DEFAULT_SOURCE = [{ name: SwapperName.Osmosis, proportion: '1' }]
@@ -7,4 +6,5 @@ export const OSMOSIS_PRECISION = 6
 export const COSMO_OSMO_CHANNEL = 'channel-141'
 export const OSMO_COSMO_CHANNEL = 'channel-0'
 
-export const atomOnOsmosisAssetId = adapters.osmosisToAssetId('ATOM') ?? ''
+export const atomOnOsmosisAssetId =
+  'cosmos:osmosis-1/ibc:27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2'

b8025dda21dc2a4da0a6d9f7d001f6189cfaf78b
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-05-01 23:12:59 +0200

    feat: clear portfolio migration

--- src/state/migrations/index.ts
+++ src/state/migrations/index.ts
@@ -1,37 +1,26 @@
-import { clearOpportunitiesSlice } from './000_clear_opportunities_slice'
-import { filterExpiredYearnOpportunities } from './001_filter_expired_yearn_opportunities'
-import { filterRetiredYearnOpportunities } from './002_filter_retired_yearn_opportunities'
-import { purgeOldIdleOpportunities } from './003_purge_old_idle_opportunities'
-import { renameUnderlyingAssetRatios } from './004_opportunities_underlyingAssetRatios_underlyingAssetRatiosBaseUnit'
-import { disableYearnOpportunities } from './005_disable_yearn_opportunities'
-import { cosmosSdkOpportunitiesSliceAbstraction } from './006_opportunities_cosmos_abstraction'
-import { clearWithdrawnSaversOpportunities } from './007_clear_withdrawn_savers_opportunities'
-import { clearCosmosSdkOpportunities } from './008_clear_cosmos_sdk_opportunities'
-import { splitUniV2EthFoxFarming } from './009_split_univ2_eth_fox_farming'
-import { clearFoxFarming } from './010_clear_fox_farming'
-import { clearIdleOpportunities } from './011_clear_idle_opportunities'
-import { clearOpportunities } from './012_clear_opportunities'
-import { clearOpportunitiesAgain } from './013_clear_opportunities'
+import { clearOpportunities } from './clearOpportunities'
+import { clearPortfolio } from './clearPortfolio'
 
 export const migrations = {
-  0: clearOpportunitiesSlice,
-  1: filterExpiredYearnOpportunities,
-  2: filterRetiredYearnOpportunities,
-  3: purgeOldIdleOpportunities,
-  4: renameUnderlyingAssetRatios,
-  5: disableYearnOpportunities,
-  6: cosmosSdkOpportunitiesSliceAbstraction,
-  7: clearWithdrawnSaversOpportunities,
-  8: clearCosmosSdkOpportunities,
-  9: splitUniV2EthFoxFarming,
-  10: clearFoxFarming,
-  11: clearIdleOpportunities,
+  0: clearOpportunities,
+  1: clearOpportunities,
+  2: clearOpportunities,
+  3: clearOpportunities,
+  4: clearOpportunities,
+  5: clearOpportunities,
+  6: clearOpportunities,
+  7: clearOpportunities,
+  8: clearOpportunities,
+  9: clearOpportunities,
+  10: clearOpportunities,
+  11: clearOpportunities,
   12: clearOpportunities,
-  13: clearOpportunitiesAgain,
-  14: clearOpportunitiesAgain,
-  15: clearOpportunitiesAgain,
-  16: clearOpportunitiesAgain,
-  17: clearOpportunitiesAgain,
-  18: clearOpportunitiesAgain,
-  19: clearOpportunitiesAgain,
+  13: clearOpportunities,
+  14: clearOpportunities,
+  15: clearOpportunities,
+  16: clearOpportunities,
+  17: clearOpportunities,
+  18: clearOpportunities,
+  19: clearOpportunities,
+  20: clearPortfolio,
 }

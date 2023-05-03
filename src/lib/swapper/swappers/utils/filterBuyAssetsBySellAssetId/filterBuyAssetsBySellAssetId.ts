b3d610dcae6f92602c1c44d98372ec9fb3032335
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2023-05-03 11:26:40 +1000

    chore: linting

--- src/lib/swapper/swappers/utils/filterBuyAssetsBySellAssetId/filterBuyAssetsBySellAssetId.ts
+++ src/lib/swapper/swappers/utils/filterBuyAssetsBySellAssetId/filterBuyAssetsBySellAssetId.ts
@@ -1,29 +1,39 @@
-import { Asset } from '@shapeshiftoss/asset-service'
+import type { Asset } from '@shapeshiftoss/asset-service'
 import type { AssetId, ChainId } from '@shapeshiftoss/caip'
 import { isEvmChainId } from '@shapeshiftoss/chain-adapters'
 import type { BuyAssetBySellIdInput } from 'lib/swapper/api'
 import { selectAssets } from 'state/slices/selectors'
 import { store } from 'state/store'
 
-const getSameChainEvmAssetsPredicate = (sellAssetChainId: ChainId, assets: Partial<Record<AssetId, Asset>>) => (assetId: AssetId): boolean => {
-  const buyAsset = assets[assetId]
+const getSameChainEvmAssetsPredicate =
+  (sellAssetChainId: ChainId, assets: Partial<Record<AssetId, Asset>>) =>
+  (assetId: AssetId): boolean => {
+    const buyAsset = assets[assetId]
 
-  if (buyAsset === undefined) return false
+    if (buyAsset === undefined) return false
 
-  // same-chain swaps and evm only
-  return buyAsset.chainId === sellAssetChainId && isEvmChainId(buyAsset.chainId)
-}
+    // same-chain swaps and evm only
+    return buyAsset.chainId === sellAssetChainId && isEvmChainId(buyAsset.chainId)
+  }
 
-const getCrossChainEvmAssetsPredicate = (sellAssetChainId: ChainId, assets: Partial<Record<AssetId, Asset>>) => (assetId: AssetId): boolean => {
-  const buyAsset = assets[assetId]
+const getCrossChainEvmAssetsPredicate =
+  (sellAssetChainId: ChainId, assets: Partial<Record<AssetId, Asset>>) =>
+  (assetId: AssetId): boolean => {
+    const buyAsset = assets[assetId]
 
-  if (buyAsset === undefined) return false
+    if (buyAsset === undefined) return false
 
-  // cross-chain swaps and evm only
-  return buyAsset.chainId !== sellAssetChainId && isEvmChainId(buyAsset.chainId)
-}
+    // cross-chain swaps and evm only
+    return buyAsset.chainId !== sellAssetChainId && isEvmChainId(buyAsset.chainId)
+  }
 
-const _filterEvmBuyAssetsBySellAssetId = (input: BuyAssetBySellIdInput, getPredicate: (chainId: ChainId, assets: Partial<Record<AssetId, Asset>>) => (assetId: AssetId) => boolean): AssetId[] => {
+const _filterEvmBuyAssetsBySellAssetId = (
+  input: BuyAssetBySellIdInput,
+  getPredicate: (
+    chainId: ChainId,
+    assets: Partial<Record<AssetId, Asset>>,
+  ) => (assetId: AssetId) => boolean,
+): AssetId[] => {
   const { assetIds = [], sellAssetId } = input
 
   const assets = selectAssets(store.getState())
@@ -35,10 +45,14 @@ const _filterEvmBuyAssetsBySellAssetId = (input: BuyAssetBySellIdInput, getPredi
   return assetIds.filter(predicate)
 }
 
-export const filterSameChainEvmBuyAssetsBySellAssetId = (input: BuyAssetBySellIdInput): AssetId[] => {
+export const filterSameChainEvmBuyAssetsBySellAssetId = (
+  input: BuyAssetBySellIdInput,
+): AssetId[] => {
   return _filterEvmBuyAssetsBySellAssetId(input, getSameChainEvmAssetsPredicate)
 }
 
-export const filterCrossChainEvmBuyAssetsBySellAssetId = (input: BuyAssetBySellIdInput): AssetId[] => {
+export const filterCrossChainEvmBuyAssetsBySellAssetId = (
+  input: BuyAssetBySellIdInput,
+): AssetId[] => {
   return _filterEvmBuyAssetsBySellAssetId(input, getCrossChainEvmAssetsPredicate)
 }

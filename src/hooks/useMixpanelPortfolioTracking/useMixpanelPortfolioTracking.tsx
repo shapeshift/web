74a4992c10630face2f5d286ef5d05b00028b7cc
Fireb0mb1 <firebomb1@gmail.com>
2023-05-09 00:38:33 +0200

    fix: merge tracked data correctly

--- src/hooks/useMixpanelPortfolioTracking/useMixpanelPortfolioTracking.tsx
+++ src/hooks/useMixpanelPortfolioTracking/useMixpanelPortfolioTracking.tsx
@@ -51,14 +51,18 @@ export const useMixpanelPortfolioTracking = () => {
 
     const mp = getMixPanel()
     if (!mp) return
-    mp.people.set({
-      // set this against the user
-      anonymizedPortfolio,
-      // track user preferences
-      'Selected Locale': selectedLocale,
-      'Selected Currency': selectedCurrency,
-      'Selected Currency Format': selectedCurrencyFormat,
-    }) // TODO(0xdef1cafe): restructure multiple wallets per user
+    mp.people.set(
+      Object.assign(
+        // set this against the user
+        anonymizedPortfolio,
+        {
+          // track user preferences
+          'Selected Locale': selectedLocale,
+          'Selected Currency': selectedCurrency,
+          'Selected Currency Format': selectedCurrencyFormat,
+        },
+      ),
+    ) // TODO(0xdef1cafe): restructure multiple wallets per user
     // don't track again for this wallet connection session
     setIsTracked(true)
   }, [

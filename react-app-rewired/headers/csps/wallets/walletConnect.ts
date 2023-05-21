3e9f585f8183d0c4415ec8301104bc4e9d820d02
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2023-05-22 09:34:44 +1000

    chore: add wc verify url as frame-src

--- react-app-rewired/headers/csps/wallets/walletConnect.ts
+++ react-app-rewired/headers/csps/wallets/walletConnect.ts
@@ -4,9 +4,9 @@ export const csp: Csp = {
   'connect-src': [
     process.env.REACT_APP_WALLET_CONNECT_RELAY_URL!,
     'wss://*.bridge.walletconnect.org/',
-    'https://verify.walletconnect.com/',
     'https://registry.walletconnect.com/api/v2/wallets',
     'https://api.etherscan.io',
   ],
   'img-src': ['https://imagedelivery.net/', 'https://registry.walletconnect.com/api/v2/logo/', '*'],
+  'frame-src': ['https://verify.walletconnect.com/'],
 }

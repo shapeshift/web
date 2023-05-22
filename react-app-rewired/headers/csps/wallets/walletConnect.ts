3ac26f7c3d1e9850ca9bef5315d6def5ab2e9276
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2023-05-22 11:07:08 +1000

    chore: add src url to csp

--- react-app-rewired/headers/csps/wallets/walletConnect.ts
+++ react-app-rewired/headers/csps/wallets/walletConnect.ts
@@ -6,6 +6,7 @@ export const csp: Csp = {
     'wss://*.bridge.walletconnect.org/',
     'https://registry.walletconnect.com/api/v2/wallets',
     'https://api.etherscan.io',
+    'https://verify.walletconnect.com/',
   ],
   'img-src': ['https://imagedelivery.net/', 'https://registry.walletconnect.com/api/v2/logo/', '*'],
   'frame-src': ['https://verify.walletconnect.com/'],

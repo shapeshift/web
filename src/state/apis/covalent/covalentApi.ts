a0c35abf8d2baba46797069a3015d3a90828882a
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-05-22 23:03:41 +0200

    feat: add x-allow-incomplete=true queryparam

--- src/state/apis/covalent/covalentApi.ts
+++ src/state/apis/covalent/covalentApi.ts
@@ -50,7 +50,7 @@ export const covalentApi = createApi({
           try {
             const url = `/${network}/address/${address}/balances_v2/?key=${
               getConfig().REACT_APP_COVALENT_API_KEY
-            }&nft=true`
+            }&nft=true&x-allow-incomplete=true`
 
             const payload = { ...options, url }
             const response = await axios.request<CovalentNftUserTokensResponseType>(payload)

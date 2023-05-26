d94c3d601717552449b7e1cda950954371bcc180
woody <125113430+woodenfurniture@users.noreply.github.com>
2023-05-26 16:26:44 +1000

    chore: change lifi default slippage to 0.5% (#4581)

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -6,6 +6,7 @@ export const USDC_PRECISION = 6
 // Slippage defaults. Don't export these to ensure the getDefaultSlippagePercentageForSwapper helper function is used.
 const DEFAULT_SLIPPAGE_DECIMAL_PERCENTAGE = '0.002' // .2%
 const DEFAULT_COWSWAP_SLIPPAGE_DECIMAL_PERCENTAGE = '0.005' // .5%
+const DEFAULT_LIFI_SLIPPAGE_DECIMAL_PERCENTAGE = '0.005' // .5%
 
 export const getDefaultSlippagePercentageForSwapper = (swapperName?: SwapperName): string => {
   if (swapperName === undefined) return DEFAULT_SLIPPAGE_DECIMAL_PERCENTAGE
@@ -14,9 +15,10 @@ export const getDefaultSlippagePercentageForSwapper = (swapperName?: SwapperName
     case SwapperName.Zrx:
     case SwapperName.OneInch:
     case SwapperName.Osmosis:
-    case SwapperName.LIFI:
     case SwapperName.Test:
       return DEFAULT_SLIPPAGE_DECIMAL_PERCENTAGE
+    case SwapperName.LIFI:
+      return DEFAULT_LIFI_SLIPPAGE_DECIMAL_PERCENTAGE
     case SwapperName.CowSwap:
       return DEFAULT_COWSWAP_SLIPPAGE_DECIMAL_PERCENTAGE
     default:

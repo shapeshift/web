9459632397270be80be0bc5d3c55a12440b8c980
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-06-01 22:09:39 +0200

    feat: you shall not opt-chain

--- src/components/Modals/Nfts/NftModal.tsx
+++ src/components/Modals/Nfts/NftModal.tsx
@@ -270,7 +270,7 @@ export const NftModal: React.FC<NftModalProps> = ({ nftItem }) => {
     if (!nftCollection) return null
 
     const hasUsefulCollectionData = Boolean(
-      nftCollection?.description || nftCollection?.socialLinks?.length,
+      nftCollection.description || nftCollection.socialLinks?.length,
     )
     return (
       <Tabs display='flex' flexDir='column' position='relative' variant='unstyled' flex={1}>

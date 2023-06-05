77dfa591b26e4687a8434c502f85b045232c7b07
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2023-06-05 13:32:26 +1000

    Revert "feat: nft refresh button (#4639)"
    This reverts commit 8871403a207e00bdb9fcde18f873374ef3318fd2.

--- src/plugins/bnbsmartchain/index.tsx
+++ src/plugins/bnbsmartchain/index.tsx
@@ -6,14 +6,6 @@ import * as unchained from '@shapeshiftoss/unchained-client'
 import { getConfig } from 'config'
 import { type Plugins } from 'plugins/types'
 
-// I'm an unchained-client wrapper around `/api/v1/` endpoints
-// If you ever need to consume endpoints which are not abstracted by chain-adapters, consume me
-export const http = new unchained.bnbsmartchain.V1Api(
-  new unchained.bnbsmartchain.Configuration({
-    basePath: getConfig().REACT_APP_UNCHAINED_BNBSMARTCHAIN_HTTP_URL,
-  }),
-)
-
 // eslint-disable-next-line import/no-default-export
 export default function register(): Plugins {
   return [
@@ -27,6 +19,12 @@ export default function register(): Plugins {
             [
               KnownChainIds.BnbSmartChainMainnet,
               () => {
+                const http = new unchained.bnbsmartchain.V1Api(
+                  new unchained.bnbsmartchain.Configuration({
+                    basePath: getConfig().REACT_APP_UNCHAINED_BNBSMARTCHAIN_HTTP_URL,
+                  }),
+                )
+
                 const ws = new unchained.ws.Client<unchained.bnbsmartchain.Tx>(
                   getConfig().REACT_APP_UNCHAINED_BNBSMARTCHAIN_WS_URL,
                 )
--- src/plugins/polygon/index.ts
+++ src/plugins/polygon/index.ts
@@ -6,14 +6,6 @@ import * as unchained from '@shapeshiftoss/unchained-client'
 import { getConfig } from 'config'
 import { type Plugins } from 'plugins/types'
 
-// I'm an unchained-client wrapper around `/api/v1/` endpoints
-// If you ever need to consume endpoints which are not abstracted by chain-adapters, consume me
-export const http = new unchained.polygon.V1Api(
-  new unchained.polygon.Configuration({
-    basePath: getConfig().REACT_APP_UNCHAINED_POLYGON_HTTP_URL,
-  }),
-)
-
 // eslint-disable-next-line import/no-default-export
 export default function register(): Plugins {
   return [
@@ -27,6 +19,12 @@ export default function register(): Plugins {
             [
               KnownChainIds.PolygonMainnet,
               () => {
+                const http = new unchained.polygon.V1Api(
+                  new unchained.polygon.Configuration({
+                    basePath: getConfig().REACT_APP_UNCHAINED_POLYGON_HTTP_URL,
+                  }),
+                )
+
                 const ws = new unchained.ws.Client<unchained.polygon.Tx>(
                   getConfig().REACT_APP_UNCHAINED_POLYGON_WS_URL,
                 )

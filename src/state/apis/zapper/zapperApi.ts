7efcb21d548a03fdc3de1709c831a8b462777fa4
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-06-12 19:35:48 +0200

    fix: derp

--- src/state/apis/zapper/zapperApi.ts
+++ src/state/apis/zapper/zapperApi.ts
@@ -567,23 +567,19 @@ export const zapper = createApi({
                   }
 
                   const underlyingAssetRatiosBaseUnit = (() =>
-                    (asset.dataProps?.reserves ?? [])
-                      .map(reserve => {
-                        const reserveBaseUnit = toBaseUnit(reserve, asset.decimals ?? 18)
-                        const totalSupplyBaseUnit =
-                          typeof asset.supply === 'number'
-                            ? toBaseUnit(asset.supply, asset.decimals ?? 18)
-                            : undefined
-                        const tokenPoolRatio = totalSupplyBaseUnit
-                          ? bn(reserveBaseUnit).div(totalSupplyBaseUnit).toString()
+                    (asset.dataProps?.reserves ?? []).map(reserve => {
+                      const reserveBaseUnit = toBaseUnit(reserve, asset.decimals ?? 18)
+                      const totalSupplyBaseUnit =
+                        typeof asset.supply === 'number'
+                          ? toBaseUnit(asset.supply, asset.decimals ?? 18)
                           : undefined
-                        if (!tokenPoolRatio) return '0'
-                        const ratio = toBaseUnit(tokenPoolRatio, asset.tokens[0].decimals)
-                        return ratio
-                      })
-                      .filter(
-                        isSome,
-                      ) as unknown as OpportunityMetadataBase['underlyingAssetRatiosBaseUnit'])()
+                      const tokenPoolRatio = totalSupplyBaseUnit
+                        ? bn(reserveBaseUnit).div(totalSupplyBaseUnit).toString()
+                        : undefined
+                      if (!tokenPoolRatio) return '0'
+                      const ratio = toBaseUnit(tokenPoolRatio, asset.tokens[0].decimals)
+                      return ratio
+                    }) as unknown as OpportunityMetadataBase['underlyingAssetRatiosBaseUnit'])()
 
                   if (!acc.opportunities[opportunityId]) {
                     acc.opportunities[opportunityId] = {

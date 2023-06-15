7b1316c470367cd257caa1bfee50586f5fcc5fb7
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2023-06-15 16:57:14 -0600

    set receive amount to 0 if below min

--- src/lib/swapper/swappers/ZrxSwapper/getZrxTradeQuote/getZrxTradeQuote.ts
+++ src/lib/swapper/swappers/ZrxSwapper/getZrxTradeQuote/getZrxTradeQuote.ts
@@ -76,6 +76,10 @@ export async function getZrxTradeQuote<T extends ZrxSupportedChainId>(
   const useSellAmount = !!sellAmount
   const rate = useSellAmount ? data.price : bn(1).div(data.price).toString()
 
+  // don't show buy amount if less than min sell amount
+  const isSellAmountBelowMinimum = bnOrZero(normalizedSellAmount).lt(minimumCryptoBaseUnit)
+  const buyAmountCryptoBaseUnit = isSellAmountBelowMinimum ? '0' : data.buyAmount
+
   // 0x approvals are cheaper than trades, but we don't have dynamic quote data for them.
   // Instead, we use a hardcoded gasLimit estimate in place of the estimatedGas in the 0x quote response.
   try {
@@ -102,8 +106,8 @@ export async function getZrxTradeQuote<T extends ZrxSupportedChainId>(
             networkFeeCryptoBaseUnit,
             protocolFees: {},
           },
-          buyAmountBeforeFeesCryptoBaseUnit: data.buyAmount,
-          sellAmountBeforeFeesCryptoBaseUnit: data.sellAmount,
+          buyAmountBeforeFeesCryptoBaseUnit: buyAmountCryptoBaseUnit,
+          sellAmountBeforeFeesCryptoBaseUnit: normalizedSellAmount,
           sources: data.sources?.filter(s => parseFloat(s.proportion) > 0) || DEFAULT_SOURCE,
         },
       ],

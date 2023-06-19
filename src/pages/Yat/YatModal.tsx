00b2da4b8b816197e0da11268503b27d22ed1b0d
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2023-06-19 12:09:45 +1000

    feat: can resolve yat eid from url to emojis, eth and usdc addresses in modal

--- src/pages/Yat/YatModal.tsx
+++ src/pages/Yat/YatModal.tsx
@@ -6,11 +6,17 @@ import {
   ModalHeader,
   ModalOverlay,
 } from '@chakra-ui/react'
-import { useCallback } from 'react'
+import type { AssetId } from '@shapeshiftoss/caip'
+import { ethAssetId } from '@shapeshiftoss/caip'
+import { useCallback, useEffect, useState } from 'react'
+import { usdcAssetId } from 'components/Modals/FiatRamps/config'
 import { useBrowserRouter } from 'hooks/useBrowserRouter/useBrowserRouter'
 import { useModal } from 'hooks/useModal/useModal'
+import { resolveYat, validateYat } from 'lib/address/yat'
 
 type YatModalProps = {
+  // https://a.y.at/emoji_id/%F0%9F%A6%8A%F0%9F%9A%80%F0%9F%8C%88 for example
+  // which is 🦊🚀🌈 without URL encoding
   eid: string
 }
 
@@ -19,8 +25,11 @@ type YatModalProps = {
  * this modal is used to handle when a user successfully purchases a yat
  *
  * the app (including mobile app) will redirect to /yat?eid=<idOfPurchasedYat>
+ * full example url http://localhost:3000/#/dashboard/yat?eid=%F0%9F%A6%8A%F0%9F%9A%80%F0%9F%8C%88
  */
 export const YatModal: React.FC<YatModalProps> = ({ eid }) => {
+  const [maybeYatEthAddress, setMaybeYatEthAddress] = useState<string | null>(null)
+  const [maybeYatUsdcAddress, setMaybeYatUsdcAddress] = useState<string | null>(null)
   const { history, location } = useBrowserRouter()
   const { yat: yatModal } = useModal()
   const { close } = yatModal
@@ -28,13 +37,48 @@ export const YatModal: React.FC<YatModalProps> = ({ eid }) => {
   console.log('YatModal', { eid, isOpen })
 
   const handleClose = useCallback(() => {
-    // remove the ?eid=<idOfPurchasedYat> query param from the route
+    // remove the query param from the route so we don't immediately re-open the modal
     history.replace({ pathname: location.pathname })
     // close the modal
     close()
   }, [close, history, location.pathname])
 
-  // TODO(0xdef1cafe): parse eid into emojis and display them
+  /**
+   * this logic is here in the modal rather than the context
+   * so it's not spamming yat api on every render cycle
+   */
+  useEffect(() => {
+    if (!eid) return
+    /**
+     * unlike ENS names, yat's can be associated with multiple addresses by asset
+     * namely, eth address and the usdc address
+     */
+    type YatResolution = {
+      assetId: AssetId
+      setter: React.Dispatch<React.SetStateAction<string | null>>
+    }
+
+    const resolutionPaths: YatResolution[] = [
+      {
+        assetId: ethAssetId,
+        setter: setMaybeYatEthAddress,
+      },
+      {
+        assetId: usdcAssetId,
+        setter: setMaybeYatUsdcAddress,
+      },
+    ]
+    resolutionPaths.forEach(async ({ assetId, setter }) => {
+      // this isn't *really* async and doesn't make network requests
+      // so ok to validate the same eid twice here
+      const isValidYat = await validateYat({ maybeAddress: eid })
+      if (!isValidYat) return
+      const maybeResolvedAddress = await resolveYat({ assetId, maybeAddress: eid })
+      // can return empty string
+      if (!maybeResolvedAddress) return
+      setter(maybeResolvedAddress)
+    })
+  }, [eid])
 
   return (
     <Modal isOpen={isOpen} onClose={handleClose} isCentered size='sm'>
@@ -43,7 +87,9 @@ export const YatModal: React.FC<YatModalProps> = ({ eid }) => {
         <ModalCloseButton />
         <ModalHeader>YatModal</ModalHeader>
         <ModalBody>
-          <pre>{JSON.stringify({ eid }, null, 2)}</pre>
+          yat emojis: {eid}
+          yat usdc address: {maybeYatUsdcAddress}
+          yat eth address: {maybeYatEthAddress}
         </ModalBody>
       </ModalContent>
     </Modal>

f1817baaba97816abf19b04dc7ea7fd5a21f266c
Fireb0mb1 <firebomb1@gmail.com>
2023-06-22 14:22:08 +0200

    feat: default fiat currency

--- src/lib/browserLocale.ts
+++ src/lib/browserLocale.ts
@@ -1,4 +1,8 @@
+import { DEFAULT_FIAT_CURRENCY } from 'constants/Config'
+import { getCurrency } from 'locale-currency'
 import { translations } from 'assets/translations'
+import type { SupportedFiatCurrencies } from 'lib/market-service'
+import { SupportedFiatCurrenciesList } from 'lib/market-service'
 
 function assumeLocale() {
   if (typeof window === 'undefined' || typeof window.navigator === 'undefined') {
@@ -17,4 +21,10 @@ export function simpleLocale() {
     locale = 'en'
   }
   return locale
+}
+
+export function defaultBrowserCurrency(): SupportedFiatCurrencies {
+  const userLocale = window?.navigator?.languages?.[0] ?? navigator?.language
+  const userCurrency = getCurrency(userLocale) as SupportedFiatCurrencies
+  return SupportedFiatCurrenciesList.includes(userCurrency) ? userCurrency : DEFAULT_FIAT_CURRENCY
 }

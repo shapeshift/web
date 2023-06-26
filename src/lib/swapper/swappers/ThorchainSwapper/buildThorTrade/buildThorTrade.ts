d218a375c757f0cd36b862dfb3ef2c0f9264b25e
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-06-26 18:57:39 +0200

    feat: plumb from through UTXOs `buildThorTrade` (#4827)

--- src/lib/swapper/swappers/ThorchainSwapper/buildThorTrade/buildThorTrade.ts
+++ src/lib/swapper/swappers/ThorchainSwapper/buildThorTrade/buildThorTrade.ts
@@ -75,7 +75,8 @@ export const buildTrade = async (
   if (chainNamespace === CHAIN_NAMESPACE.Evm) {
     const evmQuote = quote as ThorEvmTradeQuote
 
-    // TODO(gomes): update to use from instead of wallet
+    // TODO(gomes): it is not - plumb from through
+    if (!wallet) throw new Error('wallet is required to make a trade Tx')
     const maybeEthTradeTx = await makeTradeTx({
       accountNumber,
       adapter: sellAdapter as unknown as ThorEvmSupportedChainAdapter,
@@ -112,12 +113,16 @@ export const buildTrade = async (
     if (maybeThorTxInfo.isErr()) return Err(maybeThorTxInfo.unwrapErr())
     const { vault, opReturnData } = maybeThorTxInfo.unwrap()
 
-    // TODO(gomes): update to use from instead of wallet
-    const buildTxResponse = await (
-      sellAdapter as unknown as UtxoBaseAdapter<ThorUtxoSupportedChainId>
-    ).buildSendTransaction({
-      value: sellAmountCryptoBaseUnit,
+    const adapter = sellAdapter as unknown as UtxoBaseAdapter<ThorUtxoSupportedChainId>
+    const { xpub } = await adapter.getPublicKey(
       wallet,
+      accountNumber,
+      (input as GetUtxoTradeQuoteInput).accountType,
+    )
+
+    const buildTxResponse = await adapter.buildSendApiTransaction({
+      xpub,
+      value: sellAmountCryptoBaseUnit,
       to: vault,
       accountNumber,
       chainSpecific: {
@@ -132,7 +137,7 @@ export const buildTrade = async (
       chainId: sellAsset.chainId as ThorUtxoSupportedChainId,
       ...quote.steps[0],
       receiveAddress,
-      txData: buildTxResponse.txToSign,
+      txData: buildTxResponse,
     })
   } else if (chainNamespace === CHAIN_NAMESPACE.CosmosSdk) {
     const from = await sellAdapter.getAddress({ accountNumber, wallet })

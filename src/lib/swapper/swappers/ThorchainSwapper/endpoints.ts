cad2e23dbca817ecdc0796831148d11eacc69971
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2023-06-28 10:05:51 +1000

    chore: comment out circular dep causing code

--- src/lib/swapper/swappers/ThorchainSwapper/endpoints.ts
+++ src/lib/swapper/swappers/ThorchainSwapper/endpoints.ts
@@ -12,7 +12,6 @@ import type {
 
 import { getThorTradeQuote } from './getThorTradeQuote/getTradeQuote'
 import type { Rates, ThorUtxoSupportedChainId } from './ThorchainSwapper'
-import { ThorchainSwapper } from './ThorchainSwapper'
 import { getSignTxFromQuote } from './utils/getSignTxFromQuote'
 
 export const thorchainApi: Swapper2Api = {
@@ -66,30 +65,32 @@ export const thorchainApi: Swapper2Api = {
     })
   },
 
+  // FIXME: implement in a way that doesn't cause circular dependencies
+  // eslint-disable-next-line require-await
   checkTradeStatus: async ({
     txId,
   }): Promise<{ status: TxStatus; buyTxId: string | undefined; message: string | undefined }> => {
-    const thorchainSwapper = new ThorchainSwapper()
+    // const thorchainSwapper = new ThorchainSwapper()
     // thorchain swapper uses txId to get tx status (not trade ID)
-    const txsResult = await thorchainSwapper.getTradeTxs({ tradeId: txId })
-
-    const status = (() => {
-      switch (true) {
-        case txsResult.isOk() && !!txsResult.unwrap().buyTxid:
-          return TxStatus.Confirmed
-        case txsResult.isOk() && !txsResult.unwrap().buyTxid:
-          return TxStatus.Pending
-        case txsResult.isErr():
-          return TxStatus.Failed
-        default:
-          return TxStatus.Unknown
-      }
-    })()
-
-    return {
-      buyTxId: txsResult.isOk() ? txsResult.unwrap().buyTxid : undefined,
-      status,
-      message: undefined,
-    }
+    // const txsResult = await thorchainSwapper.getTradeTxs({ tradeId: txId })
+    // const status = (() => {
+    //   switch (true) {
+    //     case txsResult.isOk() && !!txsResult.unwrap().buyTxid:
+    //       return TxStatus.Confirmed
+    //     case txsResult.isOk() && !txsResult.unwrap().buyTxid:
+    //       return TxStatus.Pending
+    //     case txsResult.isErr():
+    //       return TxStatus.Failed
+    //     default:
+    //       return TxStatus.Unknown
+    //   }
+    // })()
+    //
+    // return {
+    //   buyTxId: txsResult.isOk() ? txsResult.unwrap().buyTxid : undefined,
+    //   status,
+    //   message: undefined,
+    // }
+    return Promise.resolve({ buyTxId: txId, message: txId, status: TxStatus.Unknown })
   },
 }

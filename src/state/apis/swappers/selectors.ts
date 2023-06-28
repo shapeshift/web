9d8313f195b36bb8ef111447869963dcf0cf5c28
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-06-28 10:18:23 +0200

    fix: _selectMostRecentTradeQuoteQuery on no getTradeQuote RTK queries stored

--- src/state/apis/swappers/selectors.ts
+++ src/state/apis/swappers/selectors.ts
@@ -2,16 +2,21 @@ import { createSelector } from '@reduxjs/toolkit'
 import { QueryStatus } from '@reduxjs/toolkit/query'
 import type { ReduxState } from 'state/reducer'
 
-const _selectMostRecentTradeQuoteQuery = (state: ReduxState) =>
-  Object.values(state.swappersApi.queries)
-    .filter(query => query?.endpointName === 'getTradeQuote')
-    .reduce((mostRecentQuery, query) =>
-      query?.startedTimeStamp &&
-      mostRecentQuery?.startedTimeStamp &&
-      query?.startedTimeStamp > mostRecentQuery?.startedTimeStamp
-        ? query
-        : mostRecentQuery,
-    )
+const _selectMostRecentTradeQuoteQuery = (state: ReduxState) => {
+  const getTradeQuoteQueries = Object.values(state.swappersApi.queries).filter(
+    query => query?.endpointName === 'getTradeQuote',
+  )
+
+  if (!getTradeQuoteQueries.length) return undefined
+
+  return getTradeQuoteQueries.reduce((mostRecentQuery, query) =>
+    query?.startedTimeStamp &&
+    mostRecentQuery?.startedTimeStamp &&
+    query?.startedTimeStamp > mostRecentQuery?.startedTimeStamp
+      ? query
+      : mostRecentQuery,
+  )
+}
 
 export const selectSwappersApiTradeQuotePending = createSelector(
   _selectMostRecentTradeQuoteQuery,

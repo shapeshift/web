8bed9187bf71816a3235ead4c65ac16358ffd206
Willy Ogorzaly <willy@ogorzaly.com>
2023-06-30 10:46:26 -0500

    Add card to Missions for Optimism Season 2
    The next season of OP rewards will begin as soon as season 1 ends. On ShapeShift's end, everything for season 2 is the same except for the URL, start date, and end date

--- src/pages/Missions/Missions.tsx
+++ src/pages/Missions/Missions.tsx
@@ -45,6 +45,15 @@ export const Missions = () => {
         endDate: '2023-07-04 05:59 PM',
       },
       {
+        title: translate('missions.optimism.title'),
+        subtitle: translate('missions.optimism.subtitle'),
+        buttonText: translate('missions.optimism.cta'),
+        coverImage: OptimismBg,
+        onClick: () => window.open('https://rewards.shapeshift.com/optimistic-fox-2'),
+        startDate: '2023-07-04 05:59 PM',
+        endDate: '2023-07-31 05:59 PM',
+      },
+      {
         title: translate('missions.foxArmy.title'),
         subtitle: translate('missions.foxArmy.subtitle'),
         buttonText: translate('missions.foxArmy.cta'),

cbd0f5bdddfded6aecf59a01f14503f122222f14
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-07-12 21:09:11 +0200

    fix: use estimated gasPrice or 3 gwei minimum for BSC getFeeData()

--- packages/chain-adapters/src/evm/bnbsmartchain/BscChainAdapter.ts
+++ packages/chain-adapters/src/evm/bnbsmartchain/BscChainAdapter.ts
@@ -3,8 +3,11 @@ import { ASSET_REFERENCE, bscAssetId } from '@shapeshiftoss/caip'
 import type { BIP44Params } from '@shapeshiftoss/types'
 import { KnownChainIds } from '@shapeshiftoss/types'
 import * as unchained from '@shapeshiftoss/unchained-client'
+import BigNumber from 'bignumber.js'
 
+import type { FeeDataEstimate, GetFeeDataInput } from '../../types'
 import { ChainAdapterDisplayName } from '../../types'
+import { bnOrZero } from '../../utils/bignumber'
 import type { ChainAdapterArgs } from '../EvmBaseAdapter'
 import { EvmBaseAdapter } from '../EvmBaseAdapter'
 
@@ -51,5 +54,42 @@ export class ChainAdapter extends EvmBaseAdapter<KnownChainIds.BnbSmartChainMain
 
   getFeeAssetId(): AssetId {
     return this.assetId
+  }
+
+  async getFeeData(
+    input: GetFeeDataInput<KnownChainIds.BnbSmartChainMainnet>,
+  ): Promise<FeeDataEstimate<KnownChainIds.BnbSmartChainMainnet>> {
+    const req = await this.buildEstimateGasRequest(input)
+
+    const { gasLimit } = await this.providers.http.estimateGas(req)
+    const { fast, average, slow } = await this.getGasFeeData()
+
+    // Binance official JSON-RPC endpoint has a minimum enforced gas price of 3 Gwei
+    const MIN_GAS_PRICE = '3000000000'
+
+    const fastGasPriceOrMinimum = BigNumber.max(fast.gasPrice, MIN_GAS_PRICE)
+    const averageGasPriceOrMinimum = BigNumber.max(average.gasPrice, MIN_GAS_PRICE)
+    const slowGasPriceOrMinimum = BigNumber.max(slow.gasPrice, MIN_GAS_PRICE)
+
+    return {
+      fast: {
+        txFee: bnOrZero(
+          BigNumber.max(fastGasPriceOrMinimum, fast.maxFeePerGas ?? 0).times(gasLimit),
+        ).toFixed(0),
+        chainSpecific: { gasLimit, ...fast },
+      },
+      average: {
+        txFee: bnOrZero(
+          BigNumber.max(averageGasPriceOrMinimum, average.maxFeePerGas ?? 0).times(gasLimit),
+        ).toFixed(0),
+        chainSpecific: { gasLimit, ...average },
+      },
+      slow: {
+        txFee: bnOrZero(
+          BigNumber.max(slowGasPriceOrMinimum, slow.maxFeePerGas ?? 0).times(gasLimit),
+        ).toFixed(0),
+        chainSpecific: { gasLimit, ...slow },
+      },
+    } as FeeDataEstimate<KnownChainIds.BnbSmartChainMainnet>
   }
 }

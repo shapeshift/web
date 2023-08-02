91ee899e0745b01d05e940e754c4c567f7718bc2
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2023-08-02 11:15:39 +1000

    chore: inpcremental swapper cleanup

--- src/components/Trade/hooks/useSwapper/utils.ts
+++ src/components/Trade/hooks/useSwapper/utils.ts
@@ -4,45 +4,9 @@ import type { KnownChainIds } from '@shapeshiftoss/types'
 import type { DisplayFeeData, GetFormFeesArgs, GetReceiveAddressArgs } from 'components/Trade/types'
 import { getChainAdapterManager } from 'context/PluginProvider/chainAdapterSingleton'
 import type { Asset } from 'lib/asset-service'
-import { bn, bnOrZero, positiveOrZero } from 'lib/bignumber/bignumber'
+import { bn, bnOrZero } from 'lib/bignumber/bignumber'
 import { fromBaseUnit } from 'lib/math'
-import type { ProtocolFee, SwapperName, TradeBase, TradeQuote } from 'lib/swapper/api'
-
-// used to handle market fluctations and variations in gas estimates (e.g shapeshift vs metamask) to prevent failed trades
-const FEE_FLUCTUATION_DECIMAL_PERCENTAGE = 0.25
-
-export const getSendMaxAmountCryptoPrecision = (
-  sellAsset: Asset,
-  feeAsset: Asset,
-  quote: TradeQuote<KnownChainIds>,
-  sellAssetBalanceCryptoBaseUnit: string,
-  networkFeeRequiresBalance: boolean,
-) => {
-  // Only subtract fee if sell asset is the fee asset
-  const isFeeAsset = feeAsset.assetId === sellAsset.assetId
-  const protocolFee: ProtocolFee | undefined =
-    quote.steps[0].feeData.protocolFees[sellAsset.assetId]
-
-  const protocolFeeCryptoBaseUnit = protocolFee?.requiresBalance
-    ? bn(protocolFee.amountCryptoBaseUnit)
-    : bn(0)
-
-  const networkFeeCryptoBaseUnit =
-    networkFeeRequiresBalance && isFeeAsset
-      ? bnOrZero(quote.steps[0].feeData.networkFeeCryptoBaseUnit)
-      : bn(0)
-
-  const totalFeesCryptoBaseUnit = networkFeeCryptoBaseUnit.plus(protocolFeeCryptoBaseUnit)
-  // sell asset balance minus expected fees = maxTradeAmount
-  return positiveOrZero(
-    fromBaseUnit(
-      bnOrZero(sellAssetBalanceCryptoBaseUnit).minus(
-        totalFeesCryptoBaseUnit.times(1 + FEE_FLUCTUATION_DECIMAL_PERCENTAGE),
-      ),
-      sellAsset.precision,
-    ),
-  ).toFixed()
-}
+import type { SwapperName, TradeBase } from 'lib/swapper/api'
 
 const getEvmFees = <T extends EvmChainId>(
   trade: TradeBase<T>,

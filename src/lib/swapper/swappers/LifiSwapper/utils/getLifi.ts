b70bcb86b8501618b1253d997b3e76b3da54f565
Apotheosis <0xapotheosis@gmail.com>
2023-08-07 16:47:20 +1000

    fix: bump LiFi SDK (#5041)

--- src/lib/swapper/swappers/LifiSwapper/utils/getLifi.ts
+++ src/lib/swapper/swappers/LifiSwapper/utils/getLifi.ts
@@ -1,15 +1,20 @@
-import LIFI from '@lifi/sdk'
+import type { ConfigUpdate } from '@lifi/sdk'
+import { LiFi } from '@lifi/sdk'
 
 // don't export me, access me through the getter
-let _lifi: LIFI | null = null
+let _lifi: LiFi | null = null
 
-export const getLifi = (): LIFI => {
+// Mandatory to pass the configuration object
+const lifiConfig: ConfigUpdate = {
+  disableVersionCheck: true, // prevent console fetching and notifying client about updates
+  integrator: 'your-integrator-name',
+}
+
+export const getLifi = (): LiFi => {
   if (_lifi) return _lifi
 
   // instantiate if it doesn't already exist
-  _lifi = new LIFI({
-    disableVersionCheck: true, // prevent console fetching and notifying client about updates
-  })
+  _lifi = new LiFi(lifiConfig)
 
   return _lifi
 }

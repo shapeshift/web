1c2775ee5e2177350290f341bdf0490051e557b7
Apotheosis <0xapotheosis@gmail.com>
2023-08-22 19:14:33 +1000

    feat: custom send nonce (#5137)

--- src/components/Modals/Send/utils.ts
+++ src/components/Modals/Send/utils.ts
@@ -172,6 +172,7 @@ export const handleSend = async ({
             ...(shouldUseEIP1559Fees ? { maxFeePerGas, maxPriorityFeePerGas } : { gasPrice }),
           },
           sendMax: sendInput.sendMax,
+          customNonce: sendInput.customNonce,
         })
       }
 

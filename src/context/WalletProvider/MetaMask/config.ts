a96197c4142f4973f0fb713fb741b405db649838
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-08-31 19:01:41 +0200

    feat: flaggy things

--- src/context/WalletProvider/MetaMask/config.ts
+++ src/context/WalletProvider/MetaMask/config.ts
@@ -1,9 +1,13 @@
-import { MetaMaskAdapter } from '@shapeshiftoss/hdwallet-shapeshift-multichain'
+import { MetaMaskAdapter } from '@shapeshiftoss/hdwallet-metamask'
+import { MetaMaskAdapter as MetaMaskSnapAdapter } from '@shapeshiftoss/hdwallet-shapeshift-multichain'
+import { getConfig } from 'config'
 import { MetaMaskIcon } from 'components/Icons/MetaMaskIcon'
 import type { SupportedWalletInfo } from 'context/WalletProvider/config'
 
 export const MetaMaskConfig: Omit<SupportedWalletInfo, 'routes'> = {
-  adapters: [MetaMaskAdapter],
+  adapters: [
+    getConfig().REACT_APP_EXPERIMENTAL_MM_SNAPPY_FINGERS ? MetaMaskSnapAdapter : MetaMaskAdapter,
+  ],
   supportsMobile: 'browser',
   icon: MetaMaskIcon,
   name: 'MetaMask',

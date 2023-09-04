bcb08cab3f9e35be91303ea0b51d87dbef928982
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-09-04 12:06:09 +0200

    fix: derp tests

--- src/state/slices/opportunitiesSlice/utils.test.ts
+++ src/state/slices/opportunitiesSlice/utils.test.ts
@@ -1,4 +1,4 @@
-import { gomesAccountId, mockStakingContractOne, mockStakingContractTwo } from './mocks'
+import { gomesAccountId, mockStakingContractOne } from './mocks'
 import type { UserStakingId } from './types'
 import type { UserStakingIdParts } from './utils'
 import { deserializeUserStakingId, serializeUserStakingId } from './utils'
@@ -6,7 +6,6 @@ import { deserializeUserStakingId, serializeUserStakingId } from './utils'
 describe('opportunitiesSlice utils', () => {
   const mockParts: UserStakingIdParts = [gomesAccountId, mockStakingContractOne]
   const mockStakingIdOne: UserStakingId = `${gomesAccountId}*${mockStakingContractOne}`
-  const mockStakingIdTwo: UserStakingId = `${gomesAccountId}*${mockStakingContractTwo}`
   describe('serializeUserStakingId', () => {
     it('serializes a UserStakingId from AccountId and StakingId parts', () => {
       const result = serializeUserStakingId(...mockParts)
@@ -14,7 +13,7 @@ describe('opportunitiesSlice utils', () => {
     })
   })
   describe('deserializeUserStakingId', () => {
-    const result = deserializeUserStakingId(mockStakingIdTwo)
+    const result = deserializeUserStakingId(mockStakingIdOne)
     expect(result).toEqual(mockParts)
   })
 })

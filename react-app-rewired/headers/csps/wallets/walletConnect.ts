b685c79b85b49fbbdfeff78a696382b4195fc658
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2023-09-06 11:41:13 +1000

    chore: remove wagmi dependency

--- react-app-rewired/headers/csps/wallets/walletConnect.ts
+++ react-app-rewired/headers/csps/wallets/walletConnect.ts
@@ -10,6 +10,7 @@ export const csp: Csp = {
     'https://verify.walletconnect.com/',
     'https://verify.walletconnect.org/',
     'https://explorer-api.walletconnect.com/',
+    'https://rpc.walletconnect.com/v1/',
   ],
   'img-src': ['https://imagedelivery.net/', 'https://registry.walletconnect.com/api/v2/logo/', '*'],
   'frame-src': ['https://verify.walletconnect.com/', 'https://verify.walletconnect.org/'],
--- src/plugins/walletConnectToDapps/Web3Modal.tsx
+++ src/plugins/walletConnectToDapps/Web3Modal.tsx
@@ -1,23 +1,18 @@
 import { createConfig } from '@wagmi/core'
-import { mainnet } from '@wagmi/core/chains'
-import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { EthereumClient, w3mConnectors } from '@web3modal/ethereum'
 import { Web3Modal } from '@web3modal/react'
 import type { FC } from 'react'
-import { configureChains } from 'wagmi'
+import { mainnet } from 'viem/chains'
+import { viemClient } from 'lib/viem-client'
 
 export const Web3ModalService: FC = () => {
   const chains = [mainnet]
   const walletConnectProjectId = '2f05ae7f1116030fde2d36508f472bfb'
 
-  // FIXME: use viemClient instead if possible
-  const { publicClient } = configureChains(chains, [
-    w3mProvider({ projectId: walletConnectProjectId }),
-  ])
-
   const wagmiConfig = createConfig({
     autoConnect: true,
     connectors: w3mConnectors({ projectId: walletConnectProjectId, chains }),
-    publicClient,
+    publicClient: viemClient,
   })
   const ethereumClient = new EthereumClient(wagmiConfig, chains)
 

2bb4af0f35fac84bcea8387b80a109a05a99ee46
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-09-08 20:43:03 +0200

    fix: savers "you need <amount> <asset> more to complete transaction" calculation

--- src/features/defi/providers/thorchain-savers/components/ThorchainSaversManager/Withdraw/components/Confirm.tsx
+++ src/features/defi/providers/thorchain-savers/components/ThorchainSaversManager/Withdraw/components/Confirm.tsx
@@ -31,7 +31,7 @@ import { getChainAdapterManager } from 'context/PluginProvider/chainAdapterSingl
 import { useBrowserRouter } from 'hooks/useBrowserRouter/useBrowserRouter'
 import { useWallet } from 'hooks/useWallet/useWallet'
 import { BigNumber, bn, bnOrZero } from 'lib/bignumber/bignumber'
-import { toBaseUnit } from 'lib/math'
+import { fromBaseUnit, toBaseUnit } from 'lib/math'
 import { trackOpportunityEvent } from 'lib/mixpanel/helpers'
 import { getMixPanel } from 'lib/mixpanel/mixPanelSingleton'
 import { MixPanelEvents } from 'lib/mixpanel/types'
@@ -418,22 +418,28 @@ export const Confirm: React.FC<ConfirmProps> = ({ accountId, onNext }) => {
   useEffect(() => {
     ;(async () => {
       if (!contextDispatch) return
-      const estimatedFees = await (async () => {
-        if (isTokenWithdraw) return getCustomTxFees()
-        const estimateFeeArgs = await getEstimateFeesArgs()
-        return estimateFees(estimateFeeArgs!)
-      })()
 
-      if (!estimatedFees) return
+      try {
+        const estimatedFees = await (async () => {
+          if (isTokenWithdraw) return getCustomTxFees()
+          const estimateFeeArgs = await getEstimateFeesArgs()
+          return estimateFees(estimateFeeArgs!)
+        })()
 
-      setNetworkFeeCryptoBaseUnit(estimatedFees.fast.txFee)
+        if (!estimatedFees) return
 
-      contextDispatch({
-        type: ThorchainSaversWithdrawActionType.SET_WITHDRAW,
-        payload: {
-          networkFeeCryptoBaseUnit: estimatedFees.fast.txFee,
-        },
-      })
+        setNetworkFeeCryptoBaseUnit(estimatedFees.fast.txFee)
+
+        contextDispatch({
+          type: ThorchainSaversWithdrawActionType.SET_WITHDRAW,
+          payload: {
+            networkFeeCryptoBaseUnit: estimatedFees.fast.txFee,
+          },
+        })
+      } catch (e) {
+        // We don't want to throw in case there's an error in estimating fees - this would put users in a bad state
+        console.error(e)
+      }
     })()
   }, [contextDispatch, getCustomTxFees, getEstimateFeesArgs, isTokenWithdraw])
 
@@ -761,27 +767,37 @@ export const Confirm: React.FC<ConfirmProps> = ({ accountId, onNext }) => {
     onNext(DefiStep.Info)
   }, [onNext])
 
-  const missingBalanceForGas = useMemo(() => {
+  const missingBalanceForGasCryptoPrecision = useMemo(() => {
     // Token withdraws aren't dust sends, they're actual contract calls
     // Hence, the balance required for them is denominated in the native fee asset
     if (isTokenWithdraw) {
-      return bnOrZero(feeAssetBalanceCryptoBaseUnit)
-        .minus(bnOrZero(state?.withdraw.estimatedGasCryptoBaseUnit))
-        .times(-1)
+      return fromBaseUnit(
+        bnOrZero(feeAssetBalanceCryptoBaseUnit)
+          .minus(bnOrZero(state?.withdraw.estimatedGasCryptoBaseUnit))
+          .times(-1),
+        feeAsset.precision,
+      )
     }
-    return bnOrZero(assetBalanceBaseUnit)
-      .minus(bnOrZero(state?.withdraw.estimatedGasCryptoBaseUnit))
-      .minus(bnOrZero(dustAmountCryptoBaseUnit))
-      .times(-1)
+    return fromBaseUnit(
+      bnOrZero(assetBalanceBaseUnit)
+        .minus(bnOrZero(state?.withdraw.estimatedGasCryptoBaseUnit))
+        .minus(bnOrZero(dustAmountCryptoBaseUnit))
+        .times(-1),
+      feeAsset.precision,
+    )
   }, [
     isTokenWithdraw,
     assetBalanceBaseUnit,
     state?.withdraw.estimatedGasCryptoBaseUnit,
     dustAmountCryptoBaseUnit,
     feeAssetBalanceCryptoBaseUnit,
+    feeAsset.precision,
   ])
 
-  const hasEnoughBalanceForGas = useMemo(() => missingBalanceForGas.lte(0), [missingBalanceForGas])
+  const hasEnoughBalanceForGas = useMemo(
+    () => bn(missingBalanceForGasCryptoPrecision).lte(0),
+    [missingBalanceForGasCryptoPrecision],
+  )
 
   useEffect(() => {
     if (!hasEnoughBalanceForGas) {
@@ -911,8 +927,11 @@ export const Confirm: React.FC<ConfirmProps> = ({ accountId, onNext }) => {
               translation={[
                 'modals.confirm.missingFundsForGas',
                 {
-                  cryptoAmountHuman: missingBalanceForGas.toFixed(6, BigNumber.ROUND_UP),
-                  assetSymbol: asset.symbol,
+                  cryptoAmountHuman: bn(missingBalanceForGasCryptoPrecision).toFixed(
+                    6,
+                    BigNumber.ROUND_UP,
+                  ),
+                  assetSymbol: feeAsset.symbol,
                 },
               ]}
             />

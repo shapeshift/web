33e6986972f38f433322e6ab3e2956e47065b653
woody <125113430+woodenfurniture@users.noreply.github.com>
2023-09-11 17:32:52 +1000

    feat: upgrade walletconnect ui to align to multiple connection design (#5237)
    * chore: bump @walletconnect/web3wallet version
    
    * feat: ui skeleton for new walletconnect client ui
    
    * tidy up wallet connect dropdown
    
    * fix: linting
    
    * fix: typo
    
    ---------
    
    Co-authored-by: reallybeard <89934888+reallybeard@users.noreply.github.com>

---
+++ src/plugins/walletConnectToDapps/components/header/AddressLinks.tsx
@@ -0,0 +1,61 @@
+import { HStack, Link, Stack } from '@chakra-ui/react'
+import type { AssetId } from '@shapeshiftoss/caip'
+import { fromAccountId } from '@shapeshiftoss/caip'
+import { useMemo } from 'react'
+import { MiddleEllipsis } from 'components/MiddleEllipsis/MiddleEllipsis'
+import { Text } from 'components/Text'
+import { isSome } from 'lib/utils'
+import { accountIdToFeeAssetId } from 'state/slices/portfolioSlice/utils'
+import { selectAssetById } from 'state/slices/selectors'
+import { useAppSelector } from 'state/store'
+
+type AddressAndChainProps = {
+  accountIds: string[]
+}
+
+const AddressLink = ({ feeAssetId, accountId }: { feeAssetId: AssetId; accountId: string }) => {
+  const feeAsset = useAppSelector(state => selectAssetById(state, feeAssetId ?? ''))
+  const { account: address } = fromAccountId(accountId)
+  if (!feeAsset) return null
+  return (
+    <Link href={`${feeAsset.explorerAddressLink}${address}`} isExternal color='text.info'>
+      <MiddleEllipsis value={address} />
+    </Link>
+  )
+}
+
+// renders all unique links for a given set of account ids - actual addresses may not be unique though
+export const AddressLinks: React.FC<AddressAndChainProps> = ({ accountIds }) => {
+  const feeAssetIds = useMemo(
+    () =>
+      Array.from(
+        new Set(
+          accountIds
+            .map(accountId => {
+              const feeAssetId = accountIdToFeeAssetId(accountId)
+              if (!feeAssetId) return undefined
+              return { feeAssetId, accountId }
+            })
+            .filter(isSome),
+        ),
+      ),
+    [accountIds],
+  )
+
+  const addressLabel = useMemo(() => {
+    return feeAssetIds.length > 1
+      ? 'plugins.walletConnectToDapps.header.menu.addresses'
+      : 'plugins.walletConnectToDapps.header.menu.address'
+  }, [feeAssetIds.length])
+
+  return (
+    <HStack justifyContent='space-between' spacing={4} alignItems='flex-start'>
+      <Text translation={addressLabel} color='text.subtle' />
+      <Stack alignItems='center' justifyContent='flex-end' flexWrap='wrap'>
+        {feeAssetIds.map((props, i) => (
+          <AddressLink key={i} {...props} />
+        ))}
+      </Stack>
+    </HStack>
+  )
+}

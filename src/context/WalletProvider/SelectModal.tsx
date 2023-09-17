a128744a3623df67c0665c63fa0e17b613baa438
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2023-09-18 08:28:19 +1000

    fix: optimise rerendering in wallet provider

--- src/context/WalletProvider/SelectModal.tsx
+++ src/context/WalletProvider/SelectModal.tsx
@@ -1,4 +1,5 @@
 import { CheckCircleIcon } from '@chakra-ui/icons'
+import type { ResponsiveArray } from '@chakra-ui/react'
 import {
   Button,
   Center,
@@ -9,6 +10,8 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react'
 import { getConfig } from 'config'
+import type { Property } from 'csstype'
+import { useCallback, useMemo } from 'react'
 import { isMobile } from 'react-device-detect'
 import { useTranslate } from 'react-polyglot'
 import { RawText, Text } from 'components/Text'
@@ -17,6 +20,83 @@ import { isMobile as isMobileApp } from 'lib/globals'
 
 import { SUPPORTED_WALLETS } from './config'
 import { KeyManager } from './KeyManager'
+import type { WalletInfo } from './WalletProvider'
+
+const gridTemplateColumnsProp = { base: '1fr', md: '1fr 1fr' }
+const flexDirProp: ResponsiveArray<Property.FlexDirection> = ['column', 'row']
+const mbProp = [3]
+const mlProp = [0, 1.5]
+
+const WalletSelectItem = ({
+  walletType,
+  walletInfo,
+  connect,
+}: {
+  walletType: KeyManager
+  walletInfo: WalletInfo | null
+  connect: (adapter: KeyManager) => void
+}) => {
+  const greenColor = useColorModeValue('green.500', 'green.200')
+
+  const option = SUPPORTED_WALLETS[walletType]
+  // some wallets (e.g. keepkey) do not exist on mobile
+
+  const isSupported = useMemo(() => {
+    if (isMobileApp) {
+      return ['both', 'app'].includes(String(option.supportsMobile))
+    }
+    if (isMobile) {
+      return ['both', 'browser'].includes(String(option.supportsMobile))
+    }
+    // Don't display mobile supported wallets on desktop
+    return option.supportsMobile !== 'app'
+  }, [option.supportsMobile])
+
+  const handleConnect = useCallback(() => connect(walletType), [connect, walletType])
+
+  if (!isSupported) return null
+
+  const isCoinbaseEnabled = getConfig().REACT_APP_FEATURE_COINBASE_WALLET
+  if (walletType === KeyManager.Coinbase && !isCoinbaseEnabled) return null
+
+  const isWalletConnectV2Enabled = getConfig().REACT_APP_FEATURE_WALLET_CONNECT_V2
+  if (walletType === KeyManager.WalletConnectV2 && !isWalletConnectV2Enabled) return null
+
+  const Icon = option.icon
+  const activeWallet = walletInfo?.name === option.name
+  // TODO: We can probably do better than a hardcoded ETH-only option for Walletconnect here.
+  const supportsETHOnly = option.name.toLowerCase() === KeyManager.WalletConnect
+  const walletSubText = activeWallet
+    ? 'common.connected'
+    : supportsETHOnly
+    ? 'common.walletSupportsETHOnly'
+    : null
+
+  return (
+    <Button
+      key={walletType}
+      w='full'
+      size='md'
+      py={8}
+      isActive={activeWallet}
+      justifyContent='space-between'
+      onClick={handleConnect}
+      data-test={`connect-wallet-${walletType}-button`}
+    >
+      <Flex alignItems='flex-start' flexDir='column'>
+        <RawText fontWeight='semibold'>{option.name}</RawText>
+        <Text fontSize='xs' color='text.subtle' translation={walletSubText} />
+      </Flex>
+      <Center width='25%'>
+        {activeWallet ? (
+          <CheckCircleIcon color={greenColor} />
+        ) : (
+          <Icon width='24px' height='auto' />
+        )}
+      </Center>
+    </Button>
+  )
+}
 
 export const SelectModal = () => {
   const {
@@ -26,8 +106,15 @@ export const SelectModal = () => {
   } = useWallet()
   const translate = useTranslate()
 
-  const wallets = Object.values(KeyManager).filter(key => key !== KeyManager.Demo)
-  const greenColor = useColorModeValue('green.500', 'green.200')
+  const wallets = useMemo(
+    () => Object.values(KeyManager).filter(key => key !== KeyManager.Demo),
+    [],
+  )
+
+  const handleCreate = useCallback(
+    () => create(isMobileApp ? KeyManager.Mobile : KeyManager.Native),
+    [create],
+  )
 
   return (
     <>
@@ -36,83 +123,32 @@ export const SelectModal = () => {
       </ModalHeader>
       <ModalBody>
         <Text mb={6} color='text.subtle' translation={'walletProvider.selectModal.body'} />
-        <Grid mb={6} gridTemplateColumns={{ base: '1fr', md: '1fr 1fr' }} gridGap={4}>
+        <Grid mb={6} gridTemplateColumns={gridTemplateColumnsProp} gridGap={4}>
           {adapters &&
             // TODO: KeepKey adapter may fail due to the USB interface being in use by another tab
             // So not all of the supported wallets will have an initialized adapter
-            wallets.map(walletType => {
-              const option = SUPPORTED_WALLETS[walletType]
-              // some wallets (e.g. keepkey) do not exist on mobile
-
-              const isCoinbaseEnabled = getConfig().REACT_APP_FEATURE_COINBASE_WALLET
-              if (walletType === KeyManager.Coinbase && !isCoinbaseEnabled) return null
-
-              const isWalletConnectV2Enabled = getConfig().REACT_APP_FEATURE_WALLET_CONNECT_V2
-              if (walletType === KeyManager.WalletConnectV2 && !isWalletConnectV2Enabled)
-                return null
-
-              const isSupported = (() => {
-                if (isMobileApp) {
-                  return ['both', 'app'].includes(String(option.supportsMobile))
-                }
-                if (isMobile) {
-                  return ['both', 'browser'].includes(String(option.supportsMobile))
-                }
-                // Don't display mobile supported wallets on desktop
-                return option.supportsMobile !== 'app'
-              })()
-
-              if (!isSupported) return null
-
-              const Icon = option.icon
-              const activeWallet = walletInfo?.name === option.name
-              // TODO: We can probably do better than a hardcoded ETH-only option for Walletconnect here.
-              const supportsETHOnly = option.name.toLowerCase() === KeyManager.WalletConnect
-              const walletSubText = activeWallet
-                ? 'common.connected'
-                : supportsETHOnly
-                ? 'common.walletSupportsETHOnly'
-                : null
-
-              return (
-                <Button
-                  key={walletType}
-                  w='full'
-                  size='md'
-                  py={8}
-                  isActive={activeWallet}
-                  justifyContent='space-between'
-                  onClick={() => connect(walletType)}
-                  data-test={`connect-wallet-${walletType}-button`}
-                >
-                  <Flex alignItems='flex-start' flexDir='column'>
-                    <RawText fontWeight='semibold'>{option.name}</RawText>
-                    {<Text fontSize='xs' color='text.subtle' translation={walletSubText} />}
-                  </Flex>
-                  <Center width='25%'>
-                    {activeWallet ? (
-                      <CheckCircleIcon color={greenColor} />
-                    ) : (
-                      <Icon width='24px' height='auto' />
-                    )}
-                  </Center>
-                </Button>
-              )
-            })}
+            wallets.map(walletType => (
+              <WalletSelectItem
+                key={walletType}
+                walletType={walletType}
+                walletInfo={walletInfo}
+                connect={connect}
+              />
+            ))}
         </Grid>
-        <Flex direction={['column', 'row']} mt={2} justifyContent='center' alignItems='center'>
+        <Flex direction={flexDirProp} mt={2} justifyContent='center' alignItems='center'>
           <Text
-            mb={[3]}
+            mb={mbProp}
             color='text.subtle'
             translation={walletInfo?.name ? 'common.or' : 'walletProvider.selectModal.footer'}
           />
           <Button
             variant='link'
-            mb={[3]}
-            ml={[0, 1.5]}
+            mb={mbProp}
+            ml={mlProp}
             borderTopRadius='none'
             colorScheme='blue'
-            onClick={() => create(isMobileApp ? KeyManager.Mobile : KeyManager.Native)}
+            onClick={handleCreate}
             data-test='connect-wallet-create-one-button'
           >
             {translate('walletProvider.selectModal.create')}

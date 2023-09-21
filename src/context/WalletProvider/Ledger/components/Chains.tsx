57c361369f830689975f8668c9f582037eb93763
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-09-21 22:40:29 +0200

    feat: loading/loaded states

--- src/context/WalletProvider/Ledger/components/Chains.tsx
+++ src/context/WalletProvider/Ledger/components/Chains.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, ModalBody, Text as CText } from '@chakra-ui/react'
+import { Box, Button, Flex, ModalBody, Spinner, Text as CText } from '@chakra-ui/react'
 import type { ChainId } from '@shapeshiftoss/caip'
 import {
   bchAssetId,
@@ -15,7 +15,7 @@ import {
   ltcChainId,
 } from '@shapeshiftoss/caip'
 import pull from 'lodash/pull'
-import { useCallback, useMemo } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import { AssetIcon } from 'components/AssetIcon'
 import { Text } from 'components/Text'
 import { useWallet } from 'hooks/useWallet/useWallet'
@@ -37,69 +37,80 @@ export const LedgerChains = () => {
     () => [btcAssetId, dogeAssetId, bchAssetId, ltcAssetId, ethAssetId],
     [],
   )
+
   const availableAssets = useMemo(
     () => availableAssetIds.map(id => assets[id]).filter(isSome),
     [assets, availableAssetIds],
   )
+
   const availableChainIds = useMemo(
     () => [btcChainId, dogeChainId, bchChainId, ltcChainId, ethChainId],
     [],
   )
 
+  const [loadingChains, setLoadingChains] = useState<Record<ChainId, boolean>>({})
+  const [loadedChains, setLoadedChains] = useState<Record<ChainId, boolean>>({})
+
   const handleConnectClick = useCallback(
     async (chainId: ChainId) => {
       if (!wallet) return
 
-      const accountNumber = 0
+      setLoadingChains(prevLoading => ({ ...prevLoading, [chainId]: true }))
 
-      // TODO(gomes): this should be programmatic
-      const { chainNamespace } = fromChainId(chainId)
-      const accountMetadataByAccountId = await deriveAccountIdsAndMetadataForChainNamespace[
-        chainNamespace
-      ]({
-        accountNumber,
-        chainIds: [chainId],
-        wallet,
-      })
+      try {
+        // TODO(gomes): this should be programmatic
+        const { chainNamespace } = fromChainId(chainId)
+        const accountMetadataByAccountId = await deriveAccountIdsAndMetadataForChainNamespace[
+          chainNamespace
+        ]({
+          accountNumber: 0,
+          chainIds: [chainId],
+          wallet,
+        })
 
-      const accountIds = Object.keys(accountMetadataByAccountId)
-      const { getAccount } = portfolioApi.endpoints
-      const opts = { forceRefetch: true }
-
-      const accountPromises = accountIds.map(accountId =>
-        dispatch(getAccount.initiate({ accountId }, opts)),
-      )
-
-      const accountResults = await Promise.allSettled(accountPromises)
-
-      const balanceByChainId = accountResults.reduce<Record<ChainId, BN>>((acc, res, idx) => {
-        if (res.status === 'rejected') return acc
-        const { data: account } = res.value
-        if (!account) return acc
-
-        const accountId = accountIds[idx]
-        const { chainId } = fromAccountId(accountId)
-        const accountBalance = Object.values(account.accountBalances.byId).reduce(
-          (acc, byAssetId) => {
-            Object.values(byAssetId).forEach(balance => (acc = acc.plus(bnOrZero(balance))))
-            return acc
-          },
-          bnOrZero(0),
+        const accountIds = Object.keys(accountMetadataByAccountId)
+        const { getAccount } = portfolioApi.endpoints
+        const opts = { forceRefetch: true }
+        const accountPromises = accountIds.map(accountId =>
+          dispatch(getAccount.initiate({ accountId }, opts)),
         )
+        const accountResults = await Promise.allSettled(accountPromises)
 
-        acc[chainId] = bnOrZero(acc[chainId]).plus(accountBalance)
-        if (accountNumber > 0 && accountBalance.eq(0)) return acc
+        const balanceByChainId = accountResults.reduce<Record<ChainId, BN>>((acc, res, idx) => {
+          if (res.status === 'rejected') return acc
+          const { data: account } = res.value
+          if (!account) return acc
 
-        const accountMetadata = accountMetadataByAccountId[accountId]
-        const payload = { [accountId]: accountMetadata }
-        dispatch(portfolio.actions.upsertAccountMetadata(payload))
-        dispatch(portfolio.actions.upsertPortfolio(account))
-        return acc
-      }, {})
+          const accountId = accountIds[idx]
+          const { chainId } = fromAccountId(accountId)
+          const accountBalance = Object.values(account.accountBalances.byId).reduce(
+            (acc, byAssetId) => {
+              Object.values(byAssetId).forEach(balance => (acc = acc.plus(bnOrZero(balance))))
+              return acc
+            },
+            bnOrZero(0),
+          )
 
-      Object.entries(balanceByChainId).forEach(([chainId, balance]) => {
-        if (balance.eq(0)) pull(availableChainIds, chainId)
-      })
+          acc[chainId] = bnOrZero(acc[chainId]).plus(accountBalance)
+
+          const accountMetadata = accountMetadataByAccountId[accountId]
+          const payload = { [accountId]: accountMetadata }
+          dispatch(portfolio.actions.upsertAccountMetadata(payload))
+          dispatch(portfolio.actions.upsertPortfolio(account))
+          return acc
+        }, {})
+
+        Object.entries(balanceByChainId).forEach(([chainId, balance]) => {
+          if (balance.eq(0)) pull(availableChainIds, chainId)
+        })
+
+        // Mark the chain as loaded
+        setLoadedChains(prevLoaded => ({ ...prevLoaded, [chainId]: true }))
+      } catch (e) {
+        console.error(e)
+      } finally {
+        setLoadingChains(prevLoading => ({ ...prevLoading, [chainId]: false }))
+      }
     },
     [availableChainIds, dispatch, wallet],
   )
@@ -124,17 +135,25 @@ export const LedgerChains = () => {
             mb={2}
           />
           <Box>
-            {availableAssets.map(asset => (
-              <Flex alignItems='center' justifyContent='space-between' mb={4} key={asset.assetId}>
-                <Flex alignItems='center'>
-                  <AssetIcon assetId={asset.assetId} mr={2} />
-                  <CText>{asset.name}</CText>
+            {availableAssets
+              .sort(a => (loadedChains[a.chainId] ? 1 : -1))
+              .map(asset => (
+                <Flex alignItems='center' justifyContent='space-between' mb={4} key={asset.assetId}>
+                  <Flex alignItems='center'>
+                    <AssetIcon assetId={asset.assetId} mr={2} />
+                    <CText>{asset.name}</CText>
+                  </Flex>
+                  {loadedChains[asset.chainId] ? (
+                    <CText>Added</CText>
+                  ) : loadingChains[asset.chainId] ? (
+                    <Spinner />
+                  ) : (
+                    <Button colorScheme='blue' onClick={() => handleConnectClick(asset.chainId)}>
+                      Connect
+                    </Button>
+                  )}
                 </Flex>
-                <Button colorScheme='blue' onClick={() => handleConnectClick(asset.chainId)}>
-                  Connect
-                </Button>
-              </Flex>
-            ))}
+              ))}
           </Box>
         </Box>
       </ModalBody>

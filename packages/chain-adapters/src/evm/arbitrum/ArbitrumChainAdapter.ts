3472328bfb176e0417260a36f1982be33c9f29e5
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2023-09-21 09:51:24 -0600

    add to chain adapters

---
+++ packages/chain-adapters/src/evm/arbitrum/ArbitrumChainAdapter.ts
@@ -0,0 +1,55 @@
+import type { AssetId } from '@shapeshiftoss/caip'
+import { arbitrumAssetId, ASSET_REFERENCE } from '@shapeshiftoss/caip'
+import type { BIP44Params } from '@shapeshiftoss/types'
+import { KnownChainIds } from '@shapeshiftoss/types'
+import * as unchained from '@shapeshiftoss/unchained-client'
+
+import { ChainAdapterDisplayName } from '../../types'
+import type { ChainAdapterArgs } from '../EvmBaseAdapter'
+import { EvmBaseAdapter } from '../EvmBaseAdapter'
+
+const SUPPORTED_CHAIN_IDS = [KnownChainIds.ArbitrumMainnet]
+const DEFAULT_CHAIN_ID = KnownChainIds.ArbitrumMainnet
+
+export class ChainAdapter extends EvmBaseAdapter<KnownChainIds.ArbitrumMainnet> {
+  public static readonly defaultBIP44Params: BIP44Params = {
+    purpose: 44,
+    coinType: Number(ASSET_REFERENCE.Arbitrum),
+    accountNumber: 0,
+  }
+
+  constructor(args: ChainAdapterArgs<unchained.polygon.V1Api>) {
+    super({
+      assetId: arbitrumAssetId,
+      chainId: DEFAULT_CHAIN_ID,
+      supportedChainIds: SUPPORTED_CHAIN_IDS,
+      defaultBIP44Params: ChainAdapter.defaultBIP44Params,
+      parser: new unchained.polygon.TransactionParser({
+        assetId: arbitrumAssetId,
+        chainId: args.chainId ?? DEFAULT_CHAIN_ID,
+        rpcUrl: args.rpcUrl,
+        api: args.providers.http,
+      }),
+      ...args,
+    })
+  }
+
+  getDisplayName() {
+    return ChainAdapterDisplayName.Arbitrum
+  }
+
+  getName() {
+    const enumIndex = Object.values(ChainAdapterDisplayName).indexOf(
+      ChainAdapterDisplayName.Arbitrum,
+    )
+    return Object.keys(ChainAdapterDisplayName)[enumIndex]
+  }
+
+  getType(): KnownChainIds.ArbitrumMainnet {
+    return KnownChainIds.ArbitrumMainnet
+  }
+
+  getFeeAssetId(): AssetId {
+    return this.assetId
+  }
+}

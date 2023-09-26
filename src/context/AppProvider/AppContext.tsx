1345a1723ddae20183d1f67e684fd557345c5325
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2023-09-26 10:34:06 +1000

    revert: fetch all staking opportunities metadata in a single rtk query request (#5340)
    This reverts commit 4161c0b9013446a2b602883cc79369d463c48a06.

--- src/context/AppProvider/AppContext.tsx
+++ src/context/AppProvider/AppContext.tsx
@@ -37,7 +37,6 @@ import {
   marketData,
   useFindAllQuery,
 } from 'state/slices/marketDataSlice/marketDataSlice'
-import { foxEthLpAssetId } from 'state/slices/opportunitiesSlice/constants'
 import { opportunitiesApi } from 'state/slices/opportunitiesSlice/opportunitiesApiSlice'
 import {
   fetchAllOpportunitiesIdsByChainId,
@@ -49,8 +48,6 @@ import { portfolio, portfolioApi } from 'state/slices/portfolioSlice/portfolioSl
 import { preferences } from 'state/slices/preferencesSlice/preferencesSlice'
 import {
   selectAssetIds,
-  selectIsMarketDataLoaded,
-  selectMarketDataById,
   selectPortfolioAccounts,
   selectPortfolioAssetIds,
   selectPortfolioLoadingStatus,
@@ -85,7 +82,6 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   const routeAssetId = useRouteAssetId()
   const DynamicLpAssets = useFeatureFlag('DynamicLpAssets')
   const isSnapInstalled = useIsSnapInstalled()
-  const isMarketDataLoaded = useAppSelector(selectIsMarketDataLoaded)
 
   // track anonymous portfolio
   useMixpanelPortfolioTracking()
@@ -192,13 +188,10 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
     })()
   }, [dispatch, requestedAccountIds, portfolioLoadingStatus])
 
-  const lpTokenMarketData = useAppSelector(state => selectMarketDataById(state, foxEthLpAssetId))
-  console.log(lpTokenMarketData)
-
   // once portfolio is loaded, fetch remaining chain specific data
   useEffect(() => {
     ;(async () => {
-      if (portfolioLoadingStatus === 'loading' || !isMarketDataLoaded) return
+      if (portfolioLoadingStatus === 'loading') return
 
       const { getFoxyRebaseHistoryByAccountId } = txHistoryApi.endpoints
 
@@ -258,7 +251,6 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
     dispatch,
     requestedAccountIds,
     portfolioAssetIds,
-    isMarketDataLoaded,
   ])
 
   const uniV2LpIdsData = useAppSelector(

bc84be307c3d873f997c52bbcc1fdbad8e948e20
reallybeard <89934888+reallybeard@users.noreply.github.com>
2023-09-29 12:55:41 -0500

    adjust it

--- src/pages/Lending/Pool/components/Borrow/BorrowConfirm.tsx
+++ src/pages/Lending/Pool/components/Borrow/BorrowConfirm.tsx
@@ -6,11 +6,12 @@ import { useTranslate } from 'react-polyglot'
 import { useHistory } from 'react-router'
 import { usdcAssetId } from 'test/mocks/accounts'
 import { Amount } from 'components/Amount/Amount'
+import { HelperTooltip } from 'components/HelperTooltip/HelperTooltip'
 import { AssetToAsset } from 'components/MultiHopTrade/components/TradeConfirm/AssetToAsset'
 import { WithBackButton } from 'components/MultiHopTrade/components/WithBackButton'
 import { Row } from 'components/Row/Row'
 import { SlideTransition } from 'components/SlideTransition'
-import { Text } from 'components/Text'
+import { RawText, Text } from 'components/Text'
 import { selectAssetById } from 'state/slices/selectors'
 import { useAppSelector } from 'state/store'
 
@@ -45,12 +46,32 @@ export const BorrowConfirm = () => {
             px={6}
             mb={4}
           />
-          <LoanSummary borderTopWidth={0} mt={0} />
           <Stack py={4} spacing={4} px={6} fontSize='sm' fontWeight='medium'>
+            <RawText fontWeight='bold'>{translate('lending.transactionInfo')}</RawText>
+            <Row>
+              <Row.Label>Send</Row.Label>
+              <Row.Value textAlign='right'>
+                <Stack spacing={1} flexDir='row' flexWrap='wrap'>
+                  <Amount.Crypto value='1' symbol={collateralAsset?.symbol ?? ''} />
+                  <Amount.Fiat color='text.subtle' value='29850' prefix='≈' />
+                </Stack>
+              </Row.Value>
+            </Row>
+            <Row>
+              <Row.Label>Recieve</Row.Label>
+              <Row.Value textAlign='right'>
+                <Stack spacing={1} flexDir='row' flexWrap='wrap'>
+                  <Amount.Crypto value='14820' symbol={debtAsset?.symbol ?? ''} />
+                  <Amount.Fiat color='text.subtle' value='14820' prefix='≈' />
+                </Stack>
+              </Row.Value>
+            </Row>
             <Row fontSize='sm' fontWeight='medium'>
-              <Row.Label>{translate('common.slippage')}</Row.Label>
+              <HelperTooltip label='tbd'>
+                <Row.Label>{translate('common.feesPlusSlippage')}</Row.Label>
+              </HelperTooltip>
               <Row.Value>
-                <Amount.Crypto value='20' symbol='BTC' />
+                <Amount.Fiat value='10' />
               </Row.Value>
             </Row>
             <Row fontSize='sm' fontWeight='medium'>
@@ -59,13 +80,8 @@ export const BorrowConfirm = () => {
                 <Amount.Fiat value='10' />
               </Row.Value>
             </Row>
-            <Row fontSize='sm' fontWeight='medium'>
-              <Row.Label>{translate('common.fees')}</Row.Label>
-              <Row.Value>
-                <Amount.Fiat value='0' />
-              </Row.Value>
-            </Row>
           </Stack>
+          <LoanSummary borderTopWidth={0} mt={0} />
           <CardFooter px={4} py={4}>
             <Button colorScheme='blue' size='lg' width='full'>
               {translate('lending.confirmAndBorrow')}

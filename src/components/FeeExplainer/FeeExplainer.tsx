ea3960926051a1d753416f82061d1719664ed443
reallybeard <89934888+reallybeard@users.noreply.github.com>
2023-10-03 16:02:58 -0500

    update styles

--- src/components/FeeExplainer/FeeExplainer.tsx
+++ src/components/FeeExplainer/FeeExplainer.tsx
@@ -2,6 +2,7 @@ import {
   Box,
   Card,
   CardBody,
+  CardHeader,
   Flex,
   Heading,
   Slider,
@@ -15,18 +16,23 @@ import {
   VStack,
 } from '@chakra-ui/react'
 import { LinearGradient } from '@visx/gradient'
-import { ScaleSVG } from '@visx/responsive'
+import { GridColumns, GridRows } from '@visx/grid'
+import { ParentSize, ScaleSVG } from '@visx/responsive'
+import type { ParentSizeProvidedProps } from '@visx/responsive/lib/components/ParentSize'
 import type { GlyphProps } from '@visx/xychart'
 import {
   AnimatedAreaSeries,
   AnimatedAxis,
   AnimatedGlyphSeries,
+  AnimatedGrid,
+  DataContext,
+  Grid,
   Tooltip,
   XYChart,
 } from '@visx/xychart'
 import type { RenderTooltipParams } from '@visx/xychart/lib/components/Tooltip'
 import debounce from 'lodash/debounce'
-import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useCallback, useContext, useEffect, useMemo, useState } from 'react'
 import { Amount } from 'components/Amount/Amount'
 import { RawText } from 'components/Text'
 import { bn } from 'lib/bignumber/bignumber'
@@ -69,6 +75,33 @@ const renderTooltip = ({ tooltipData }: RenderTooltipParams<ChartData>) => {
       <strong>Trade Size: </strong> {tooltipData?.nearestDatum?.datum?.x} <br />
       <strong>Fee (bps): </strong> {tooltipData?.nearestDatum?.datum?.y}
     </div>
+  )
+}
+
+const CustomGrid = ({ xNumTicks, yNumTicks }: { xNumTicks: number; yNumTicks: number }) => {
+  const { xScale, yScale, innerHeight, innerWidth, margin } = useContext(DataContext)
+  const borderColor = useToken('colors', 'border.base')
+  if (!xScale || !yScale) {
+    return null
+  }
+  return (
+    <>
+      <GridRows
+        left={margin?.left ?? 0}
+        width={innerWidth ?? 0}
+        scale={yScale}
+        numTicks={xNumTicks}
+        stroke={borderColor}
+      />
+      <GridColumns
+        top={margin?.top ?? 0}
+        height={innerHeight ?? 0}
+        scale={xScale}
+        numTicks={yNumTicks}
+        stroke={borderColor}
+        strokeDasharray='4'
+      />
+    </>
   )
 }
 
@@ -155,49 +188,66 @@ const FeeChart: React.FC<FeeChartProps> = ({ foxHolding, tradeSize }) => {
   )
 
   return (
-    <ScaleSVG width={width} height={height}>
-      <XYChart xScale={xScale} yScale={yScale} width={width} height={height}>
-        <LinearGradient id='area-gradient' from={foxBlue} to={foxBlue} toOpacity={0} />
+    <Box height={height} width='full'>
+      <ParentSize>
+        {({ height, width }: ParentSizeProvidedProps) => (
+          <ScaleSVG width={width} height={height}>
+            <XYChart
+              xScale={xScale}
+              yScale={yScale}
+              width={width}
+              height={height}
+              margin={{ left: 30, right: 30, top: 0, bottom: 30 }}
+            >
+              <LinearGradient id='area-gradient' from={foxBlue} to={foxBlue} toOpacity={0} />
 
-        <AnimatedAxis
-          orientation='bottom'
-          numTicks={4}
-          tickLabelProps={tickLabelProps}
-          tickFormat={tickFormat}
-          labelProps={labelProps(textColor)}
-          stroke={borderColor}
-          tickStroke={borderColor}
-        />
-        <AnimatedAxis
-          orientation='left'
-          labelProps={labelProps(textColor)}
-          labelOffset={30}
-          numTicks={FEE_CURVE_MAX_FEE_BPS / 7}
-          tickLabelProps={tickLabelProps}
-          stroke={borderColor}
-          tickStroke={borderColor}
-        />
+              <AnimatedAxis
+                orientation='bottom'
+                numTicks={3}
+                tickLabelProps={tickLabelProps}
+                tickFormat={tickFormat}
+                labelProps={labelProps(textColor)}
+                stroke={borderColor}
+                strokeWidth={0}
+                hideTicks={true}
+                tickStroke={borderColor}
+              />
+              <AnimatedAxis
+                orientation='left'
+                labelProps={labelProps(textColor)}
+                numTicks={FEE_CURVE_MAX_FEE_BPS / 7}
+                tickLabelProps={tickLabelProps}
+                stroke={borderColor}
+                hideTicks={true}
+                tickStroke={borderColor}
+                hideZero={true}
+              />
 
-        <AnimatedAreaSeries
-          {...accessors}
-          dataKey='Line 1'
-          data={data}
-          lineProps={lineProps}
-          fill='url(#area-gradient)'
-          fillOpacity={0.1}
-          strokeWidth={2}
-        />
-        <AnimatedGlyphSeries
-          {...accessors}
-          dataKey='Current Point'
-          data={currentPoint}
-          renderGlyph={renderGlyph}
-          /* additional styling here */
-        />
+              <CustomGrid xNumTicks={FEE_CURVE_MAX_FEE_BPS / 7} yNumTicks={3} />
 
-        <Tooltip renderTooltip={renderTooltip} />
-      </XYChart>
-    </ScaleSVG>
+              <AnimatedAreaSeries
+                {...accessors}
+                dataKey='Line 1'
+                data={data}
+                lineProps={lineProps}
+                fill='url(#area-gradient)'
+                fillOpacity={0.1}
+                strokeWidth={2}
+              />
+              <AnimatedGlyphSeries
+                {...accessors}
+                dataKey='Current Point'
+                data={currentPoint}
+                renderGlyph={renderGlyph}
+                /* additional styling here */
+              />
+
+              <Tooltip renderTooltip={renderTooltip} />
+            </XYChart>
+          </ScaleSVG>
+        )}
+      </ParentSize>
+    </Box>
   )
 }
 
@@ -224,29 +274,6 @@ const FeeSliders: React.FC<FeeSlidersProps> = ({
 }) => {
   return (
     <VStack height='100%' spacing={12} mb={8}>
-      <Stack width='full'>
-        <Flex width='full' justifyContent='space-between'>
-          <RawText>Trade Size</RawText>
-          <Amount.Fiat value={tradeSize} />
-        </Flex>
-        <Box width='100%'>
-          <Slider min={0} max={CHART_TRADE_SIZE_MAX_USD} value={tradeSize} onChange={setTradeSize}>
-            <SliderMark value={CHART_TRADE_SIZE_MAX_USD * 0.2} {...labelStyles}>
-              <Amount.Fiat value={CHART_TRADE_SIZE_MAX_USD * 0.2} abbreviated />
-            </SliderMark>
-            <SliderMark value={CHART_TRADE_SIZE_MAX_USD * 0.5} {...labelStyles}>
-              <Amount.Fiat value={CHART_TRADE_SIZE_MAX_USD * 0.5} abbreviated />
-            </SliderMark>
-            <SliderMark value={CHART_TRADE_SIZE_MAX_USD * 0.8} {...labelStyles}>
-              <Amount.Fiat value={CHART_TRADE_SIZE_MAX_USD * 0.8} abbreviated={true} />
-            </SliderMark>
-            <SliderTrack>
-              <SliderFilledTrack />
-            </SliderTrack>
-            <SliderThumb />
-          </Slider>
-        </Box>
-      </Stack>
       <Stack width='full'>
         <Flex width='full' justifyContent='space-between'>
           <Text>FOX Holding</Text>
@@ -278,6 +305,29 @@ const FeeSliders: React.FC<FeeSlidersProps> = ({
           </Slider>
         </Box>
       </Stack>
+      <Stack width='full'>
+        <Flex width='full' justifyContent='space-between'>
+          <RawText>Trade Size</RawText>
+          <Amount.Fiat value={tradeSize} />
+        </Flex>
+        <Box width='100%'>
+          <Slider min={0} max={CHART_TRADE_SIZE_MAX_USD} value={tradeSize} onChange={setTradeSize}>
+            <SliderMark value={CHART_TRADE_SIZE_MAX_USD * 0.2} {...labelStyles}>
+              <Amount.Fiat value={CHART_TRADE_SIZE_MAX_USD * 0.2} abbreviated />
+            </SliderMark>
+            <SliderMark value={CHART_TRADE_SIZE_MAX_USD * 0.5} {...labelStyles}>
+              <Amount.Fiat value={CHART_TRADE_SIZE_MAX_USD * 0.5} abbreviated />
+            </SliderMark>
+            <SliderMark value={CHART_TRADE_SIZE_MAX_USD * 0.8} {...labelStyles}>
+              <Amount.Fiat value={CHART_TRADE_SIZE_MAX_USD * 0.8} abbreviated={true} />
+            </SliderMark>
+            <SliderTrack>
+              <SliderFilledTrack />
+            </SliderTrack>
+            <SliderThumb />
+          </Slider>
+        </Box>
+      </Stack>
     </VStack>
   )
 }
@@ -288,44 +338,45 @@ type FeeOutputProps = {
 }
 
 export const FeeOutput: React.FC<FeeOutputProps> = ({ tradeSize, foxHolding }) => {
-  const { feeBps, feeUsd, foxDiscountPercent } = calculateFees({
+  const { feeUsd, feeBps, feeUsdDiscount, foxDiscountPercent } = calculateFees({
     tradeAmountUsd: bn(tradeSize),
     foxHeld: bn(foxHolding),
   })
+  const feeWithDiscount = useMemo(() => {
+    const discountAmount = bn(feeUsdDiscount).gt(feeUsd) ? feeUsd : feeUsdDiscount
+    return bn(feeUsd).minus(discountAmount)
+  }, [feeUsd, feeUsdDiscount])
   return (
-    <Stack>
-      <Flex gap={4}>
-        <Card flex={1}>
-          <CardBody>
-            {feeUsd.lte(0) ? (
+    <CardHeader fontWeight='medium' pb={0}>
+      <Stack width='full'>
+        <Flex gap={4}>
+          <Box flex={1} textAlign='center'>
+            <RawText color='text.subtle'>Total Fees</RawText>
+            {feeUsdDiscount.lte(0) ? (
               <RawText fontSize='3xl' color={'green.500'}>
                 Free
               </RawText>
             ) : (
-              <Amount.Fiat
-                fontSize='3xl'
-                value={feeUsd.lte(0) ? '0' : feeUsd.toFixed(2)}
-                color={'green.500'}
-              />
+              <Amount.Fiat fontSize='3xl' value={feeWithDiscount.toFixed(2)} color={'green.500'} />
             )}
-            <RawText color='text.subtle'>Total Fees</RawText>
-          </CardBody>
-        </Card>
-        <Card flex={1}>
-          <CardBody>
+          </Box>
+          <Box flex={1} textAlign='center'>
+            <RawText color='text.subtle'>FOX Holder Discount</RawText>
             <Amount.Percent
               fontSize='3xl'
               value={foxDiscountPercent.div(100).toNumber()}
               color={'green.500'}
             />
-            <RawText color='text.subtle'>FOX Holder Discount</RawText>
-          </CardBody>
-        </Card>
-      </Flex>
-      <RawText color='text.subtle' fontSize='sm' textAlign='center'>
-        Fee before discount: ${feeUsd.toFixed(2)} ({feeBps.toFixed(2)} bps)
-      </RawText>
-    </Stack>
+          </Box>
+        </Flex>
+        <RawText color='text.subtle' fontSize='sm' textAlign='center'>
+          Based on a fee of: ${feeUsd.toFixed(2)} ({feeBps.toFixed(2)} bps)
+        </RawText>
+        <RawText>
+          <Amount.Fiat value={feeUsdDiscount.toFixed(2)} />
+        </RawText>
+      </Stack>
+    </CardHeader>
   )
 }
 
@@ -343,11 +394,13 @@ export const FeeExplainer = () => {
   }
 
   return (
-    <Card flexDir='column-reverse' maxWidth='600px' width='full' mx='auto'>
+    <Card flexDir='column' maxWidth='600px' width='full' mx='auto'>
       <CardBody flex='1' p={feeExplainerCardBody}>
-        <Heading as='h5'>Calculate your FOX Savings</Heading>
+        <Heading as='h5'>Simulate your FOX Savings</Heading>
         <RawText color='text.subtle'>
-          Something about savings, put good copy in here that doesn't suck.
+          Use this handy calculator to determine your total fees and the discount you'll receive on
+          your trade. Simply adjust the sliders below to indicate your FOX holder amount and the
+          trade size in fiat, and watch your potential savings unfold!
         </RawText>
         <RawText color='text.subtle'>FOX voting power {currentFoxHoldings} FOX</RawText>
         <Stack spacing={4} mt={6}>
@@ -358,12 +411,14 @@ export const FeeExplainer = () => {
             setFoxHolding={setFoxHolding}
             currentFoxHoldings={currentFoxHoldings ?? '0'}
           />
-          <FeeOutput tradeSize={tradeSize} foxHolding={foxHolding} />
         </Stack>
       </CardBody>
-      <Flex flex='1' justifyContent='center' alignItems='center'>
-        <FeeChart tradeSize={tradeSize} foxHolding={foxHolding} onHover={onHover} />
-      </Flex>
+      <CardBody flex='1' flexDir='column' justifyContent='center' alignItems='center' pt={0}>
+        <Card>
+          <FeeOutput tradeSize={tradeSize} foxHolding={foxHolding} />
+          <FeeChart tradeSize={tradeSize} foxHolding={foxHolding} onHover={onHover} />
+        </Card>
+      </CardBody>
     </Card>
   )
 }

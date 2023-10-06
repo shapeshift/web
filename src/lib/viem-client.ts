fe610575b098bf1580c5b6387b13ff45eaba5223
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2023-10-06 15:01:06 +1100

    chore: undo unused changes

--- src/lib/viem-client.ts
+++ src/lib/viem-client.ts
@@ -1,50 +1,8 @@
-import { type ChainReference, fromChainId } from '@shapeshiftoss/caip'
-import { type EvmChainId, evmChainIds } from '@shapeshiftoss/chain-adapters'
-import { KnownChainIds } from '@shapeshiftoss/types'
-import type { HttpTransport, PublicClient } from 'viem'
+import { getConfig } from 'config'
 import { createPublicClient, http } from 'viem'
-import type { Chain } from 'viem/chains'
 import { mainnet } from 'viem/chains'
 
-import { rpcUrlByChainId } from './ethersProviderSingleton'
-
-const viemClientsByChain = evmChainIds.reduce<
-  Record<EvmChainId, PublicClient<HttpTransport, Chain>>
->(
-  (acc, chainId) => {
-    const nodeUrl = rpcUrlByChainId(chainId)
-    acc[chainId] = createPublicClient({
-      chain: mainnet,
-      transport: http(nodeUrl),
-    })
-
-    return acc
-  },
-  {} as Record<EvmChainId, PublicClient<HttpTransport, Chain>>,
-)
-
-export const viemEthMainnetClient = viemClientsByChain[KnownChainIds.EthereumMainnet]
-
-const viemChainToChainId = (chain: ChainReference | undefined): EvmChainId => {
-  switch (chain) {
-    case fromChainId(KnownChainIds.AvalancheMainnet).chainReference:
-      return KnownChainIds.AvalancheMainnet
-    case fromChainId(KnownChainIds.OptimismMainnet).chainReference:
-      return KnownChainIds.OptimismMainnet
-    case fromChainId(KnownChainIds.BnbSmartChainMainnet).chainReference:
-      return KnownChainIds.BnbSmartChainMainnet
-    case fromChainId(KnownChainIds.PolygonMainnet).chainReference:
-      return KnownChainIds.PolygonMainnet
-    case fromChainId(KnownChainIds.GnosisMainnet).chainReference:
-      return KnownChainIds.GnosisMainnet
-    case fromChainId(KnownChainIds.EthereumMainnet).chainReference:
-    default:
-      return KnownChainIds.EthereumMainnet
-  }
-}
-
-export const getViemClientByChain = ({ chainId }: { chainId?: number }) => {
-  const chainReference: ChainReference | undefined =
-    chainId !== undefined ? (String(chainId) as ChainReference) : undefined
-  return viemClientsByChain[viemChainToChainId(chainReference)]
-}
+export const viemEthMainnetClient = createPublicClient({
+  chain: mainnet,
+  transport: http(getConfig().REACT_APP_ETHEREUM_NODE_URL),
+})

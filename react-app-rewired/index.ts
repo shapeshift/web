fa0a0dec6ecb84beda75255de846a266d0d08354
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2023-10-13 12:39:00 +1100

    fix: resolve cowswap package also for dev, not just prod

--- react-app-rewired/index.ts
+++ react-app-rewired/index.ts
@@ -327,7 +327,6 @@ const reactAppRewireConfig = {
             // we want to happen in the browser during the build of the webpack bundle.
             resolve: {
               alias: {
-                'ethers/lib/utils': 'ethers/lib/utils.js',
                 [path.join(buildPath, 'src/env/index.ts')]: path.join(
                   buildPath,
                   'src/env/webpack.ts',
@@ -339,6 +338,18 @@ const reactAppRewireConfig = {
             },
           }
         : {},
+
+      // resolve bundling issue with @cowprotocol/app-data where it produces the compilation error:
+      // Module not found: Error: Can't resolve 'ethers/lib/utils' in 'web/node_modules/@cowprotocol/app-data/dist'
+      // BREAKING CHANGE: The request 'ethers/lib/utils' failed to resolve only because it was resolved as fully specified
+      // (probably because the origin is strict EcmaScript Module, e. g. a module with javascript mimetype, a '*.mjs' file, or a '*.js' file where the package.json contains '"type": "module"').
+      _.merge(config, {
+        resolve: {
+          alias: {
+            'ethers/lib/utils': 'ethers/lib/utils.js',
+          },
+        },
+      }),
     )
 
     const MAXIMUM_FILE_SIZE_TO_CACHE_IN_BYTES = 50 * 1024 * 1024 // 50MB

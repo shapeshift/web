0288aa4ed938d29e7d67b7ee60b83188375a1ea0
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-10-19 22:32:09 +0200

    feat: doge day doge icon

--- src/components/Layout/Header/AppLoadingIcon.tsx
+++ src/components/Layout/Header/AppLoadingIcon.tsx
@@ -1,25 +1,52 @@
-import { Center } from '@chakra-ui/react'
-import { AnimatePresence } from 'framer-motion'
-import { memo } from 'react'
+import { Center, Image } from '@chakra-ui/react'
+import { AnimatePresence, motion } from 'framer-motion'
+import { memo, useCallback, useState } from 'react'
 import { Link } from 'react-router-dom'
+import Doge from 'assets/doge.png'
 import { CircularProgress } from 'components/CircularProgress/CircularProgress'
 import { FoxIcon } from 'components/Icons/FoxIcon'
 import { SlideTransitionY } from 'components/SlideTransitionY'
 import { useIsAnyApiFetching } from 'hooks/useIsAnyApiFetching/useIsAnyApiFetching'
 
+const dogeVariants = {
+  hidden: { rotate: 0, opacity: 0 },
+  visible: { rotate: 360, opacity: 1 },
+  exit: { rotate: -360, opacity: 0 },
+}
+
 export const AppLoadingIcon: React.FC = memo(() => {
+  const [isHovered, setIsHovered] = useState(false)
   const isLoading = useIsAnyApiFetching()
+
+  const handleMouseEnter = useCallback(() => {
+    setIsHovered(true)
+  }, [])
+
+  const handleMouseLeave = useCallback(() => {
+    setIsHovered(false)
+  }, [])
+
   return (
-    <Link to='/'>
+    <Link to='/' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       <AnimatePresence exitBeforeEnter initial>
-        {isLoading ? (
+        {isHovered ? (
+          <motion.div
+            key='doge-flip'
+            initial='hidden'
+            animate='visible'
+            exit='exit'
+            variants={dogeVariants}
+          >
+            <Image src={Doge} alt={'Doge'} boxSize='7' />
+          </motion.div>
+        ) : isLoading ? (
           <SlideTransitionY key='loader'>
             <Center boxSize='7'>
               <CircularProgress size={7} />
             </Center>
           </SlideTransitionY>
         ) : (
-          <SlideTransitionY key='logo'>
+          <SlideTransitionY key='fox'>
             <FoxIcon boxSize='7' />
           </SlideTransitionY>
         )}

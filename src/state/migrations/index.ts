b627e535f8e1619852fd8bbe8bd8124571765d1d
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-10-23 20:20:16 +0200

    feat: rm useless comment

--- src/state/migrations/index.ts
+++ src/state/migrations/index.ts
@@ -3,7 +3,6 @@ import { clearNfts } from './clearNfts'
 import { clearOpportunities } from './clearOpportunities'
 import { clearPortfolio } from './clearPortfolio'
 
-// when adding a migration, dont forget to increment `persistConfig.version` in `src/state/store.js`
 export const migrations = {
   0: clearOpportunities,
   1: clearOpportunities,

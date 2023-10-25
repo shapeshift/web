1a29b080892b67846b276c6f10fd4fafeb12f84c
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-10-25 10:40:20 +0200

    feat: ledger instructions popover

--- src/context/WalletProvider/Ledger/components/Chains.tsx
+++ src/context/WalletProvider/Ledger/components/Chains.tsx
@@ -1,5 +1,20 @@
-import { CheckCircleIcon } from '@chakra-ui/icons'
-import { Alert, AlertDescription, AlertIcon, Box, Button, Flex, ModalBody } from '@chakra-ui/react'
+import { CheckCircleIcon, InfoOutlineIcon } from '@chakra-ui/icons'
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  Box,
+  Button,
+  Flex,
+  Icon,
+  Link,
+  ModalBody,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger,
+} from '@chakra-ui/react'
 import type { ChainId } from '@shapeshiftoss/caip'
 import {
   bchAssetId,
@@ -123,27 +138,53 @@ export const LedgerChains = () => {
 
   const chainsRows = useMemo(
     () =>
-      availableAssets.map(asset => {
-        return (
-          <Flex alignItems='center' justifyContent='space-between' mb={4} key={asset.assetId}>
-            <Flex alignItems='center' gap={2}>
-              <AssetIcon assetId={asset.assetId} size='sm' />
-              <RawText fontWeight='bold'>{asset.name}</RawText>
-            </Flex>
-            <Button
-              isLoading={loadingChains[asset.chainId]}
-              // we need to pass an arg here, so we need an anonymous function wrapper
-              // eslint-disable-next-line react-memo/require-usememo
-              onClick={() => handleConnectClick(asset.chainId)}
-              colorScheme={walletChainIds.includes(asset.chainId) ? 'green' : 'gray'}
-              isDisabled={walletChainIds.includes(asset.chainId)}
-              leftIcon={walletChainIds.includes(asset.chainId) ? <CheckCircleIcon /> : undefined}
-            >
-              {walletChainIds.includes(asset.chainId) ? 'Added' : 'Connect'}
-            </Button>
+      availableAssets.map(asset => (
+        <Flex alignItems='center' justifyContent='space-between' mb={4} key={asset.assetId}>
+          <Flex alignItems='center' gap={2}>
+            <AssetIcon assetId={asset.assetId} size='sm' />
+            <RawText fontWeight='bold'>{asset.name}</RawText>
+            {asset.assetId === thorchainAssetId && (
+              <Popover trigger='hover' placement='right-start'>
+                <PopoverTrigger>
+                  <Box>
+                    <Icon
+                      as={InfoOutlineIcon}
+                      boxSize={4}
+                      ml={1}
+                      color='gray.500'
+                      cursor='pointer'
+                    />
+                  </Box>
+                </PopoverTrigger>
+                <PopoverContent>
+                  <PopoverArrow />
+                  <PopoverBody>
+                    {'Follow the instructions '}
+                    <Link
+                      color='blue.200'
+                      href='https://support.ledger.com/hc/en-us/articles/4402987997841-THORChain-RUNE-?docs=true'
+                    >
+                      here
+                    </Link>
+                    {' to get the RUNE app on your Ledger'}
+                  </PopoverBody>
+                </PopoverContent>
+              </Popover>
+            )}
           </Flex>
-        )
-      }),
+          <Button
+            isLoading={loadingChains[asset.chainId]}
+            // we need to pass an arg here, so we need an anonymous function wrapper
+            // eslint-disable-next-line react-memo/require-usememo
+            onClick={() => handleConnectClick(asset.chainId)}
+            colorScheme={walletChainIds.includes(asset.chainId) ? 'green' : 'gray'}
+            isDisabled={walletChainIds.includes(asset.chainId)}
+            leftIcon={walletChainIds.includes(asset.chainId) ? <CheckCircleIcon /> : undefined}
+          >
+            {walletChainIds.includes(asset.chainId) ? 'Added' : 'Connect'}
+          </Button>
+        </Flex>
+      )),
     [availableAssets, handleConnectClick, loadingChains, walletChainIds],
   )
 

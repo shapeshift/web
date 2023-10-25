8e9f45681f223d4a08d8d32645b1153f8b6c3747
reallybeard <89934888+reallybeard@users.noreply.github.com>
2023-10-25 11:42:17 -0500

    fix: change default route (#5534)

--- src/pages/Dashboard/DashboardSidebar.tsx
+++ src/pages/Dashboard/DashboardSidebar.tsx
@@ -4,11 +4,9 @@ import { memo } from 'react'
 import OnRamperLogo from 'assets/on-ramper.png'
 import SaversVaultTop from 'assets/savers-vault-top.png'
 import { AssetIcon } from 'components/AssetIcon'
-import { MultiHopTrade } from 'components/MultiHopTrade/MultiHopTrade'
 import { PromoCard } from 'components/Promo/PromoCard'
 import type { PromoItem } from 'components/Promo/types'
 import { useWallet } from 'hooks/useWallet/useWallet'
-import { isMobile } from 'lib/globals'
 import { EligibleCarousel } from 'pages/Defi/components/EligibleCarousel'
 import { MissionSidebar } from 'pages/Missions/Missions'
 
@@ -55,8 +53,6 @@ const promoData: PromoItem[] = [
   },
 ]
 
-const display = { base: 'none', xl: 'block' }
-
 export const DashboardSidebar = memo(() => {
   const {
     state: { isConnected, isDemoWallet },
@@ -66,7 +62,6 @@ export const DashboardSidebar = memo(() => {
   return (
     <Flex width='full' flexDir='column' gap={6}>
       <PromoCard data={promoData} />
-      {!isMobile && <MultiHopTrade display={display} />}
       <MissionSidebar />
       <EligibleCarousel />
       <RecentTransactions limit={8} viewMoreLink />

aeab1065ee1792501e966d73941d32a6ba998901
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-10-26 23:33:35 +0200

    wip: wip

--- src/pages/Lending/Pool/components/PoolInfo.tsx
+++ src/pages/Lending/Pool/components/PoolInfo.tsx
@@ -1,20 +1,56 @@
 import { CheckCircleIcon } from '@chakra-ui/icons'
 import { Flex } from '@chakra-ui/react'
 import { Tag, TagLeftIcon } from '@chakra-ui/tag'
+import type { AssetId } from '@shapeshiftoss/caip'
+import { useQuery } from '@tanstack/react-query'
 import { useMemo } from 'react'
 import { useTranslate } from 'react-polyglot'
 import { Amount } from 'components/Amount/Amount'
 import { RawText, Text } from 'components/Text'
+import { selectAssetById } from 'state/slices/assetsSlice/selectors'
+import { getAllThorchainLendingPositions } from 'state/slices/opportunitiesSlice/resolvers/thorchainLending/utils'
+import { useAppSelector } from 'state/store'
 
 import { DynamicComponent } from './PoolStat'
 
 const labelProps = { fontSize: 'sm ' }
 
-export const PoolInfo = () => {
+type PoolInfoProps = {
+  poolAssetId: AssetId
+}
+
+export const PoolInfo = ({ poolAssetId }: PoolInfoProps) => {
   const translate = useTranslate()
+  const asset = useAppSelector(state => selectAssetById(state, poolAssetId))
+
+  const poolDataQueryKey: [string, { assetId: AssetId }] = useMemo(
+    () => ['thorchainLendingPoolData', { assetId: poolAssetId }],
+    [poolAssetId],
+  )
+
+  const { data: poolData, isLoading: isPoolDataLoading } = useQuery({
+    queryKey: poolDataQueryKey,
+    queryFn: async ({ queryKey }) => {
+      const [, { assetId }] = queryKey
+      const positions = await getAllThorchainLendingPositions(assetId)
+      return positions
+    },
+    select: data => {
+      // returns actual derived data, or zero's out fields in case there is no active position
+      const totalBorrowers = data?.length ?? 0
+
+      return {
+        totalBorrowers,
+      }
+    },
+    enabled: true,
+  })
+
   const totalCollateralComponent = useMemo(
-    () => <Amount.Crypto fontSize='2xl' value='25' symbol='BTC' fontWeight='medium' />,
-    [],
+    () => (
+      <Amount.Crypto fontSize='2xl' value='25' symbol={asset?.symbol ?? ''} fontWeight='medium' />
+    ),
+    [asset?.symbol],
   )
   const totalDebtBalance = useMemo(
     () => <Amount.Fiat fontSize='2xl' value={25} fontWeight='medium' />,
@@ -24,7 +60,13 @@ export const PoolInfo = () => {
     () => <Amount.Percent value={2.93} color='text.success' fontSize='lg' />,
     [],
   )
-  const totalBorrowersComponent = useMemo(() => <RawText fontSize='lg'>123</RawText>, [])
+  const totalBorrowersComponent = useMemo(
+    () => <RawText fontSize='lg'>{poolData?.totalBorrowers}</RawText>,
+    [poolData?.totalBorrowers],
+  )
+
+  if (!asset) return null
+
   return (
     <>
       <Flex gap={4} alignItems='center'>
@@ -36,12 +78,14 @@ export const PoolInfo = () => {
       </Flex>
       <Flex>
         <DynamicComponent
+          isLoading={isPoolDataLoading}
           label='lending.totalCollateral'
           component={totalCollateralComponent}
           flex={1}
           labelProps={labelProps}
         />
         <DynamicComponent
+          isLoading={isPoolDataLoading}
           label='lending.totalDebtBalance'
           component={totalDebtBalance}
           flex={1}
@@ -50,12 +94,14 @@ export const PoolInfo = () => {
       </Flex>
       <Flex>
         <DynamicComponent
+          isLoading={isPoolDataLoading}
           label='lending.estCollateralizationRatio'
           component={estCollateralizationRatioComponent}
           flex={1}
           labelProps={labelProps}
         />
         <DynamicComponent
+          isLoading={isPoolDataLoading}
           label='lending.totalBorrowers'
           component={totalBorrowersComponent}
           flex={1}

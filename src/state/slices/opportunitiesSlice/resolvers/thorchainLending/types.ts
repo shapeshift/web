a1a46d9d9ceeee40b39891bf1bca4b25d0c52653
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-10-26 20:57:53 +0200

    feat: consolidate types

--- src/state/slices/opportunitiesSlice/resolvers/thorchainLending/types.ts
+++ src/state/slices/opportunitiesSlice/resolvers/thorchainLending/types.ts
@@ -1,31 +1,36 @@
-export type LendingDepositQuoteResponseSuccess = {
+type QuoteFees = {
+  asset: string
+  affiliate?: string
+  outbound?: string
+  liquidity: string
+  total: string
+  slippage_bps: number
+  total_bps: number
+}
+
+type BaseQuoteResponse = {
+  dust_threshold: string
+  expected_amount_out: string
+  expiry: number
+  fees: QuoteFees
   inbound_address: string
   inbound_confirmation_blocks: number
   inbound_confirmation_seconds: number
+  memo: string
+  notes: string
   outbound_delay_blocks: number
   outbound_delay_seconds: number
-  fees: {
-    asset: string
-    affiliate: string
-    outbound: string
-    liquidity: string
-    total: string
-    slippage_bps: number
-    total_bps: number
-  }
+  recommended_min_amount_in: string
+  router: string
   slippage_bps: number
   streaming_slippage_bps: number
-  router: string
-  expiry: number
   warning: string
-  notes: string
-  dust_threshold: string
-  recommended_min_amount_in: string
-  memo: string
-  expected_amount_out: string
-  expected_collateralization_ratio: string
-  expected_collateral_deposited: string
+}
+
+export type LendingDepositQuoteResponseSuccess = BaseQuoteResponse & {
   expected_debt_issued: string
+  expected_collateral_deposited: string
+  expected_collateralization_ratio: string
 }
 
 export type LendingDepositQuoteResponseError = {
@@ -36,31 +41,7 @@ export type LendingDepositQuoteResponse =
   | LendingDepositQuoteResponseSuccess
   | LendingDepositQuoteResponseError
 
-export type LendingWithdrawQuoteResponseSuccess = {
-  inbound_address: string
-  inbound_confirmation_blocks: number
-  inbound_confirmation_seconds: number
-  outbound_delay_blocks: number
-  outbound_delay_seconds: number
-  fees: {
-    asset: string
-    affiliate: string
-    outbound: string
-    liquidity: string
-    total: string
-    slippage_bps: number
-    total_bps: number
-  }
-  slippage_bps: number
-  streaming_slippage_bps: number
-  router: string
-  expiry: number
-  warning: string
-  notes: string
-  dust_threshold: string
-  recommended_min_amount_in: string
-  memo: string
-  expected_amount_out: string
+export type LendingWithdrawQuoteResponseSuccess = BaseQuoteResponse & {
   expected_collateral_withdrawn: string
   expected_debt_repaid: string
 }

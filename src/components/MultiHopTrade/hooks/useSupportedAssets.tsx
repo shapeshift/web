7e5fc4624dbfdf492c6c8d7855b19bae8008c9f5
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2023-10-31 10:48:38 +1100

    perf: move supported asset fetch into rtk query endpoint

--- src/components/MultiHopTrade/hooks/useSupportedAssets.tsx
+++ src/components/MultiHopTrade/hooks/useSupportedAssets.tsx
@@ -1,59 +1,34 @@
-import type { AssetId } from '@shapeshiftoss/caip'
-import { fromAssetId } from '@shapeshiftoss/caip'
-import { useEffect, useMemo, useState } from 'react'
+import { KnownChainIds } from '@shapeshiftoss/types'
+import { useMemo } from 'react'
 import { useIsSnapInstalled } from 'hooks/useIsSnapInstalled/useIsSnapInstalled'
 import { useWallet } from 'hooks/useWallet/useWallet'
 import { walletSupportsChain } from 'hooks/useWalletSupportsChain/useWalletSupportsChain'
-import type { Asset } from 'lib/asset-service'
-import { getSupportedBuyAssetIds, getSupportedSellAssetIds } from 'lib/swapper/swapper'
-import { getEnabledSwappers } from 'lib/swapper/utils'
+import { useGetSupportedAssetsQuery } from 'state/apis/swappers/swappersApi'
 import { selectAssetsSortedByMarketCapUserCurrencyBalanceAndName } from 'state/slices/common-selectors'
-import { selectAssets, selectFeatureFlags, selectSellAsset } from 'state/slices/selectors'
 import { useAppSelector } from 'state/store'
 
 export const useSupportedAssets = () => {
-  const sellAsset = useAppSelector(selectSellAsset)
-  const assets = useAppSelector(selectAssets)
   const sortedAssets = useAppSelector(selectAssetsSortedByMarketCapUserCurrencyBalanceAndName)
-  const featureFlags = useAppSelector(selectFeatureFlags)
   const wallet = useWallet().state.wallet
-
-  const enabledSwappers = useMemo(() => getEnabledSwappers(featureFlags, false), [featureFlags])
-
-  const [supportedSellAssets, setSupportedSellAssets] = useState<Asset[]>([])
-  const [supportedSellAssetIds, setSupportedSellAssetIds] = useState<Set<AssetId>>(new Set())
-  const [supportedBuyAssets, setSupportedBuyAssets] = useState<Asset[]>([])
-  const [supportedBuyAssetsIds, setSupportedBuyAssetsIds] = useState<Set<AssetId>>(new Set())
-
   const isSnapInstalled = useIsSnapInstalled()
-  useEffect(() => {
-    ;(async () => {
-      const assetIds = await getSupportedSellAssetIds(enabledSwappers, assets)
-      const filteredAssetIds = new Set<AssetId>()
-      assetIds.forEach(assetId => {
-        const chainId = fromAssetId(assetId).chainId
-        if (walletSupportsChain({ chainId, wallet, isSnapInstalled })) {
-          filteredAssetIds.add(assetId)
-        }
-      })
-      setSupportedSellAssetIds(filteredAssetIds)
-    })()
-  }, [assets, enabledSwappers, isSnapInstalled, sortedAssets, wallet])
 
-  useEffect(() => {
-    ;(async () => {
-      const assetIds = await getSupportedBuyAssetIds(enabledSwappers, sellAsset, assets)
-      setSupportedBuyAssetsIds(assetIds)
-    })()
-  }, [assets, enabledSwappers, sellAsset, sortedAssets])
+  const walletSupportedChains = useMemo(() => {
+    return Object.values(KnownChainIds).filter(chainId =>
+      walletSupportsChain({ chainId, wallet, isSnapInstalled }),
+    )
+  }, [isSnapInstalled, wallet])
 
-  useEffect(() => {
-    setSupportedSellAssets(sortedAssets.filter(asset => supportedSellAssetIds.has(asset.assetId)))
-  }, [supportedSellAssetIds, enabledSwappers, sortedAssets])
+  const { data } = useGetSupportedAssetsQuery(walletSupportedChains)
 
-  useEffect(() => {
-    setSupportedBuyAssets(sortedAssets.filter(asset => supportedBuyAssetsIds.has(asset.assetId)))
-  }, [supportedBuyAssetsIds, enabledSwappers, sortedAssets])
+  const supportedSellAssets = useMemo(() => {
+    if (data === undefined) return []
+    return sortedAssets.filter(asset => data.supportedSellAssetIds.includes(asset.assetId))
+  }, [data, sortedAssets])
+
+  const supportedBuyAssets = useMemo(() => {
+    if (data === undefined) return []
+    return sortedAssets.filter(asset => data.supportedBuyAssetsIds.includes(asset.assetId))
+  }, [data, sortedAssets])
 
   return {
     supportedSellAssets,

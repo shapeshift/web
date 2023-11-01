dbea5d10daf8c0e521f35a046b4e6120704abf95
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-11-02 08:21:04 +0900

    fix: derp

--- src/pages/Lending/hooks/useLendingPositionData.tsx
+++ src/pages/Lending/hooks/useLendingPositionData.tsx
@@ -20,7 +20,7 @@ export const useLendingPositionData = ({ accountId, assetId }: UseLendingPositio
 
   const lendingPositionData = useQuery({
     // 2 minutes before the data is considered stale, meaning firing this query will trigger queryFn
-    staleTime: 120,
+    staleTime: 120_000,
     queryKey: lendingPositionQueryKey,
     queryFn: async ({ queryKey }) => {
       const [, { accountId, assetId }] = queryKey

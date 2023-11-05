011213f9e1b8c8baabd12f4a78dfa14768a50319
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-11-06 05:22:12 +0900

    feat: wire-up thorchain lending Your Loans (#5569)
    * feat: thorchain lending improvements (#5562)
    
    * feat: tackle TODOs
    
    * feat: move AccountId state management up top in Borrow.tsx
    
    * feat: lending improvements
    
    * feat: 2mn staleTime for thorchainLendingPosition
    
    * feat: remove Infinity staleTime comment
    
    * feat: programmatic thor lending assets
    
    * feat: more skeletons
    
    * feat: more more skeletons
    
    * feat: error-handling
    
    * feat: cleanup
    
    * feat: improve loading state
    
    * fix: flash of content
    
    * feat: more skeletony
    
    * feat: utxo deposits
    
    * feat: rm console.log
    
    * feat: improve error handling
    
    * fix: derp
    
    * feat: no default
    
    * feat: add unification comment
    
    * feat: wire-up thorchain lending Available Pools
    
    * feat: oh noes
    
    * feat: extract props
    
    * feat: wire-up thorchain lending Your Loans
    
    * fix: path

--- src/pages/Lending/YourLoans.tsx
+++ src/pages/Lending/YourLoans.tsx
@@ -1,23 +1,125 @@
-import { Button, type GridProps, SimpleGrid, Stack } from '@chakra-ui/react'
-import { btcAssetId } from '@shapeshiftoss/caip'
-import { useMemo } from 'react'
+import { Button, type GridProps, SimpleGrid, Skeleton, Stack } from '@chakra-ui/react'
+import type { AssetId } from '@shapeshiftoss/caip'
+import { fromAssetId } from '@shapeshiftoss/caip'
+import { useCallback, useMemo } from 'react'
 import { useTranslate } from 'react-polyglot'
+import { useHistory } from 'react-router'
 import { Amount } from 'components/Amount/Amount'
 import { HelperTooltip } from 'components/HelperTooltip/HelperTooltip'
 import { Main } from 'components/Layout/Main'
 import { AssetCell } from 'components/StakingVaults/Cells'
 import { RawText, Text } from 'components/Text'
+import type { Asset } from 'lib/asset-service'
+import { bnOrZero } from 'lib/bignumber/bignumber'
+import { selectFirstAccountIdByChainId } from 'state/slices/selectors'
+import { useAppSelector } from 'state/store'
 
 import { LendingHeader } from './components/LendingHeader'
+import { useLendingPositionData } from './hooks/useLendingPositionData'
+import { useLendingSupportedAssets } from './hooks/useLendingSupportedAssets'
 
 export const lendingRowGrid: GridProps['gridTemplateColumns'] = {
   base: 'minmax(150px, 1fr) repeat(1, minmax(40px, max-content))',
   md: 'repeat(4, 1fr)',
 }
 
+type LendingRowGridProps = {
+  asset: Asset
+  onPoolClick: (assetId: AssetId) => void
+}
+
+const LendingRowGrid = ({ asset, onPoolClick }: LendingRowGridProps) => {
+  // TODO(gomes): this only handles displaying positions of account 0 for now - we may want to make this component accomodate positions over all accounts
+  // however, this would rug the "Repayment Lock" data, so we need to figure out a UX way around this
+
+  const accountId =
+    useAppSelector(state =>
+      selectFirstAccountIdByChainId(state, fromAssetId(asset.assetId).chainId),
+    ) ?? ''
+
+  const { data: lendingPositionData, isLoading: isLendingPositionDataLoading } =
+    useLendingPositionData({
+      assetId: asset.assetId,
+      accountId,
+    })
+
+  const handlePoolClick = useCallback(() => {
+    onPoolClick(asset.assetId)
+  }, [asset.assetId, onPoolClick])
+
+  if (
+    lendingPositionData &&
+    bnOrZero(lendingPositionData.collateralBalanceCryptoPrecision)
+      .plus(lendingPositionData.debtBalanceFiatUSD)
+      .isZero()
+  )
+    return null
+
+  return (
+    <Stack mx={-4}>
+      <Button
+        variant='ghost'
+        display='grid'
+        gridTemplateColumns={lendingRowGrid}
+        columnGap={4}
+        alignItems='center'
+        textAlign='left'
+        py={4}
+        width='full'
+        height='auto'
+        color='text.base'
+        onClick={handlePoolClick}
+      >
+        <AssetCell assetId={asset.assetId} />
+        <Skeleton isLoaded={!isLendingPositionDataLoading}>
+          <Stack spacing={0}>
+            <Amount.Fiat value={lendingPositionData?.debtBalanceFiatUSD ?? '0'} />
+          </Stack>
+        </Skeleton>
+        <Skeleton isLoaded={!isLendingPositionDataLoading}>
+          <Stack spacing={0}>
+            <Amount.Crypto
+              value={lendingPositionData?.collateralBalanceCryptoPrecision ?? '0'}
+              symbol={asset.symbol}
+            />
+            <Amount.Fiat
+              value={lendingPositionData?.collateralBalanceFiatUserCurrency ?? '0'}
+              fontSize='sm'
+              color='text.subtle'
+            />
+          </Stack>
+        </Skeleton>
+        <Skeleton isLoaded={!isLendingPositionDataLoading}>
+          {/* TODO(gomes): programmatic */}
+          <RawText>30 days</RawText>
+        </Skeleton>
+      </Button>
+    </Stack>
+  )
+}
+
 export const YourLoans = () => {
   const translate = useTranslate()
   const lendingHeader = useMemo(() => <LendingHeader />, [])
+
+  const { data: lendingSupportedAssets } = useLendingSupportedAssets()
+
+  const history = useHistory()
+
+  const handlePoolClick = useCallback(
+    (assetId: AssetId) => {
+      history.push(`/lending/pool/${assetId}`)
+    },
+    [history],
+  )
+
+  const lendingRowGrids = useMemo(() => {
+    if (!lendingSupportedAssets) return new Array(2).fill(null).map(() => <Skeleton height={16} />)
+    return lendingSupportedAssets.map(asset => (
+      <LendingRowGrid asset={asset} onPoolClick={handlePoolClick} />
+    ))
+  }, [handlePoolClick, lendingSupportedAssets])
+
   return (
     <Main headerComponent={lendingHeader}>
       <Stack>
@@ -39,31 +141,7 @@ export const YourLoans = () => {
             <Text translation='lending.repaymentLock' textAlign='right' />
           </HelperTooltip>
         </SimpleGrid>
-        <Stack mx={-4}>
-          <Button
-            variant='ghost'
-            display='grid'
-            gridTemplateColumns={lendingRowGrid}
-            columnGap={4}
-            alignItems='center'
-            textAlign='left'
-            py={4}
-            width='full'
-            height='auto'
-            color='text.base'
-          >
-            <AssetCell assetId={btcAssetId} />
-            <Stack spacing={0}>
-              <Amount.Crypto value='14820' symbol='TOR' />
-              <Amount.Fiat value='14820' fontSize='sm' color='text.subtle' />
-            </Stack>
-            <Stack spacing={0}>
-              <Amount.Crypto value='1.0' symbol='BTC' />
-              <Amount.Fiat value='29640' fontSize='sm' color='text.subtle' />
-            </Stack>
-            <RawText>30 days</RawText>
-          </Button>
-        </Stack>
+        {lendingRowGrids}
       </Stack>
     </Main>
   )

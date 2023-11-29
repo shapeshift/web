5cad5ee6807c7d96418c8652f5aaa1ebf04646da
woody <125113430+woodenfurniture@users.noreply.github.com>
2023-11-29 16:02:23 +1100

    fix: passing invalid address to isSmartContractAddress should return false instead of throw

--- src/lib/address/utils.ts
+++ src/lib/address/utils.ts
@@ -4,7 +4,7 @@ import { getEthersProvider } from 'lib/ethersProviderSingleton'
 export const isEthAddress = (address: string): boolean => /^0x[0-9A-Fa-f]{40}$/.test(address)
 
 export const isSmartContractAddress = async (address: string): Promise<boolean> => {
-  if (!isAddress(address)) throw new Error('Invalid address')
+  if (!isAddress(address)) return false
   const bytecode = await getEthersProvider().getCode(address)
   return bytecode !== '0x'
 }

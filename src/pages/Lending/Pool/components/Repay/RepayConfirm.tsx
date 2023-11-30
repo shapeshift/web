98f6711af839537ab1cc18eab9993be0706bf0de
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-11-30 21:45:46 +0100

    fix: merge

--- src/pages/Lending/Pool/components/Repay/RepayConfirm.tsx
+++ src/pages/Lending/Pool/components/Repay/RepayConfirm.tsx
@@ -212,36 +212,6 @@ export const RepayConfirm = ({
       contractAddress: undefined,
     })
 
-    if (repaymentAsset.assetId === thorchainAssetId) {
-      const { account } = fromAccountId(repaymentAccountId)
-
-      const adapter = chainAdapter as unknown as thorchain.ChainAdapter
-
-      // repayment using THOR is a MsgDeposit tx
-      const { txToSign } = await adapter.buildDepositTransaction({
-        from: account,
-        accountNumber: repaymentAccountNumber,
-        value: bnOrZero(repaymentAmountCryptoPrecision)
-          .times(bn(10).pow(repaymentAsset.precision))
-          .toFixed(0),
-        memo: lendingQuoteCloseData.quoteMemo,
-        chainSpecific: {
-          gas: (estimatedFees as FeeDataEstimate<KnownChainIds.ThorchainMainnet>).fast.chainSpecific
-            .gasLimit,
-          fee: (estimatedFees as FeeDataEstimate<KnownChainIds.ThorchainMainnet>).fast.txFee,
-        },
-      })
-      const signedTx = await adapter.signTransaction({
-        txToSign,
-        wallet,
-      })
-      return adapter.broadcastTransaction({
-        senderAddress: account,
-        receiverAddress: lendingQuoteCloseData.quoteInboundAddress,
-        hex: signedTx,
-      })
-    }
-
     const maybeTxId = await (() => {
       if (repaymentAsset.assetId === thorchainAssetId) {
         return (async () => {

ea2fb6a1c3c2398405190ccf3eb46f84d7d93dcc
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2023-12-01 12:16:20 -0700

    more explicit dependency

--- src/components/MultiHopTrade/components/TradeInput/TradeInput.tsx
+++ src/components/MultiHopTrade/components/TradeInput/TradeInput.tsx
@@ -456,7 +456,7 @@ export const TradeInput = memo(() => {
       slippageDecimal,
       totalNetworkFeeFiatPrecision,
       totalProtocolFees,
-      tradeQuoteStep,
+      tradeQuoteStep?.source,
     ],
   )
 

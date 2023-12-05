da1c35ea56fe5b88e8e83ccc9466aa1f36b00ef7
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-12-05 01:07:30 +0100

    feat: lending supported assets narrow by wallet supported chain (#5760)
    feat: lending supported assets narrow on wallet supported chain

--- src/pages/Lending/hooks/useLendingSupportedAssets/index.ts
+++ src/pages/Lending/hooks/useLendingSupportedAssets/index.ts
@@ -1,6 +1,11 @@
-import { thorchainAssetId } from '@shapeshiftoss/caip'
+import { fromAssetId, thorchainAssetId } from '@shapeshiftoss/caip'
+import { supportsThorchain } from '@shapeshiftoss/hdwallet-core'
+import { KnownChainIds } from '@shapeshiftoss/types'
 import { useQuery } from '@tanstack/react-query'
-import { useCallback } from 'react'
+import { useCallback, useMemo } from 'react'
+import { useIsSnapInstalled } from 'hooks/useIsSnapInstalled/useIsSnapInstalled'
+import { useWallet } from 'hooks/useWallet/useWallet'
+import { walletSupportsChain } from 'hooks/useWalletSupportsChain/useWalletSupportsChain'
 import { bnOrZero } from 'lib/bignumber/bignumber'
 import type { ThornodePoolResponse } from 'lib/swapper/swappers/ThorchainSwapper/types'
 import { poolAssetIdToAssetId } from 'lib/swapper/swappers/ThorchainSwapper/utils/poolAssetHelpers/poolAssetHelpers'
@@ -12,12 +17,23 @@ import { store } from 'state/store'
 const queryKey = ['lendingSupportedAssets']
 
 export const useLendingSupportedAssets = ({ type }: { type: 'collateral' | 'borrow' }) => {
+  const wallet = useWallet().state.wallet
+  const isSnapInstalled = useIsSnapInstalled()
+
   const { data: availablePools } = useQuery({
     // Mark pools data as stale after 60 seconds to handle the case of going from halted to available and vice versa
     staleTime: 60_000,
     queryKey: ['thorchainAvailablePools'],
     queryFn: getThorchainAvailablePools,
   })
+
+  const walletSupportChains = useMemo(
+    () =>
+      Object.values(KnownChainIds).filter(chainId =>
+        walletSupportsChain({ chainId, wallet, isSnapInstalled }),
+      ),
+    [isSnapInstalled, wallet],
+  )
 
   const selectSupportedAssets = useCallback(
     (data: ThornodePoolResponse[] | undefined) => {
@@ -29,19 +45,22 @@ export const useLendingSupportedAssets = ({ type }: { type: 'collateral' | 'borr
       const supportedAssets = pools
         .map(pool => {
           const assetId = poolAssetIdToAssetId(pool.asset)
+          const chainId = assetId ? (fromAssetId(assetId).chainId as KnownChainIds) : undefined
+          if (!chainId || !walletSupportChains.includes(chainId)) return undefined
+
           const asset = selectAssetById(store.getState(), assetId ?? '')
           return asset
         })
         .filter(isSome)
 
-      if (type === 'borrow') {
+      if (type === 'borrow' && wallet && supportsThorchain(wallet)) {
         const runeAsset = selectAssetById(store.getState(), thorchainAssetId)
         if (!runeAsset) return
         supportedAssets.push(runeAsset)
       }
       return supportedAssets
     },
-    [availablePools, type],
+    [availablePools, type, wallet, walletSupportChains],
   )
 
   const lendingSupportedAssetsQuery = useQuery({

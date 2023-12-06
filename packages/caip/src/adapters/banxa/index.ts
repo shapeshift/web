803eb5a1c86793d253f61740143c32199f1ab4ad
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-12-06 20:14:44 +0100

    feat: rm TODO

--- packages/caip/src/adapters/banxa/index.ts
+++ packages/caip/src/adapters/banxa/index.ts
@@ -6,7 +6,6 @@ import type { ChainId } from '../../chainId/chainId'
 import {
   avalancheAssetId,
   avalancheChainId,
-  bscAssetId,
   bscChainId,
   btcAssetId,
   btcChainId,
@@ -37,7 +36,6 @@ const AssetIdToBanxaTickerMap = {
   [optimismAssetId]: 'eth',
   [polygonAssetId]: 'matic',
   [thorchainAssetId]: 'rune',
-  [bscAssetId]: 'bnb', // TODO(gomes): double check dis correct
   'eip155:1/erc20:0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9': 'aave',
   'eip155:1/erc20:0xbb0e17ef65f82ab018d8edd776e8dd940327b28b': 'axs',
   'eip155:1/erc20:0x4d224452801aced8b2f0aebe155379bb5d594381': 'ape',

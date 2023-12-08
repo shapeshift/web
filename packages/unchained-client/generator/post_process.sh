144eedc38729b1bd6bf4d190a0fae7662d3e2039
0xean <0xean.eth@gmail.com>
2023-12-08 17:07:52 -0400

    revert post_process changes

--- packages/unchained-client/generator/post_process.sh
+++ packages/unchained-client/generator/post_process.sh
@@ -8,6 +8,6 @@ if [ "$(uname)" != "Darwin" ]; then
   sed -i "$SED_COMMAND_1" $1
   sed -i "$SED_COMMAND_2" $1
 else
-  /usr/bin/sed -i '' "$SED_COMMAND_1" $1
-  /usr/bin/sed -i '' "$SED_COMMAND_2" $1
+  sed -i '' "$SED_COMMAND_1" $1
+  sed -i '' "$SED_COMMAND_2" $1
 fi

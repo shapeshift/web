5ec2111516f81f133dfb38c2e04005ce760ce492
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2023-12-14 16:46:57 +1100

    chore: update comment

--- src/lib/swapper/swappers/ThorchainSwapper/utils/parseThorBuyTxHash.ts
+++ src/lib/swapper/swappers/ThorchainSwapper/utils/parseThorBuyTxHash.ts
@@ -8,7 +8,7 @@ export const parseThorBuyTxHash = (
   const outCoinAsset: string | undefined = thorActionsData.actions[0]?.out[0]?.coins[0]?.asset
   const isDoubleSwap = outCoinAsset !== 'THOR.RUNE' && inCoinAsset !== 'THOR.RUNE'
 
-  // rune swaps aren't double swaps so don't have a second tx
+  // swaps into rune aren't double swaps so don't have a second tx (buy tx)
   if (!isDoubleSwap) return sellTxId
 
   const isEvmCoinAsset = outCoinAsset?.startsWith('ETH.') || outCoinAsset?.startsWith('AVAX.')

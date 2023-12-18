927a8c8cda1ef4f59460acb14bbe9dc9326be118
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2023-12-18 15:49:35 +1100

    chore: cleanup

--- src/state/slices/txHistorySlice/utils.ts
+++ src/state/slices/txHistorySlice/utils.ts
@@ -3,7 +3,7 @@ import type { AccountId, AssetId } from '@shapeshiftoss/caip'
 import type { Tx } from './txHistorySlice'
 
 type TxIndex = string
-export type TxDescriptor = {
+type TxDescriptor = {
   accountId: AccountId
   txid: Tx['txid']
   address: Tx['address']

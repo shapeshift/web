cbf6dc89d14d13482f3da8799db0e97fbb4aa9a6
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-01-05 22:08:55 +0100

    feat: one more to go

--- src/setupVitest.ts
+++ src/setupVitest.ts
@@ -1,4 +1,7 @@
-import { vi } from 'vitest'
+import { beforeAll, vi } from 'vitest'
 
+beforeAll(() => {
+  ;(global as any).IS_REACT_ACT_ENVIRONMENT = true
+})
 vi.resetModules()
 vi.mock('ethers')

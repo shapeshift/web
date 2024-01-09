46e4f02db6ba5a3156da00e7a3cc7883b61ab55f
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-01-09 16:49:01 +0100

    fix: fox donations max. fee curve bps

--- src/lib/fees/parameters.ts
+++ src/lib/fees/parameters.ts
@@ -1,4 +1,6 @@
-export const FEE_CURVE_MAX_FEE_BPS = 29 // basis points
+import { DEFAULT_SWAPPER_AFFILIATE_BPS } from 'components/MultiHopTrade/constants'
+
+export const FEE_CURVE_MAX_FEE_BPS = Number(DEFAULT_SWAPPER_AFFILIATE_BPS) // basis points
 export const FEE_CURVE_MIN_FEE_BPS = 10 // basis points
 export const FEE_CURVE_NO_FEE_THRESHOLD_USD = 1_000 // usd
 export const FEE_CURVE_FOX_MAX_DISCOUNT_THRESHOLD = 1_000_000 // fox

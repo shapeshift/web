85b35b282aa288e70eefb6700aaa64355ad0a504
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-01-09 01:21:53 +0100

    feat: bump axios and migrate  to axios-cache-interceptor (#5943)
    * feat: f this, migrating to vitest is just quicker at this point
    
    * feat: add config, fix some types and bump vitest to latest
    
    * fix: disregard me, pinning 1.1.1
    
    see https://github.com/shapeshift/web/pull/5949
    
    * feat: add TODO
    
    * feat: migrate unchained-client parser tests to vitest (but still broken
    :()
    
    * feat: oh boi I friggin did it hnnng
    
    * feat: bring back ethers mocking
    
    * feat: one more to go
    
    * fix: oh boy I did it
    
    * feat: update snapshots
    
    * feat: remove remaining jest reference
    
    * feat: migrate test scripts and rm packages
    
    * feat: cleanup
    
    * feat: rm sindresorhus package old CJS version resolution
    
    * Revert "feat: rm sindresorhus package old CJS version resolution"
    
    This reverts commit bbd153a20389fe677c032a88151683cac2752cf4.
    
    * feat: skipping for now, TODO bring back as separate suite
    
    * feat: split configs and make shit work
    
    * feat: remove jest comment reference
    
    * feat: errego fixed types
    
    * chore: rm console.log in test
    
    * feat: bump axios
    
    * feat: revert origin/develop -- getCowSwapTradeQuote.ts
    
    * fix: types
    
    ---------
    
    Co-authored-by: Apotheosis <97164662+0xApotheosis@users.noreply.github.com>

--- src/lib/swapper/swappers/ZrxSwapper/utils/zrxService.ts
+++ src/lib/swapper/swappers/ZrxSwapper/utils/zrxService.ts
@@ -1,6 +1,5 @@
 import { SwapperName } from '@shapeshiftoss/swapper'
 import type { AxiosInstance, AxiosRequestConfig } from 'axios'
-import axios from 'axios'
 import identity from 'lodash/identity'
 import type { RetryConfig } from 'retry-axios'
 import type { MonadicSwapperAxiosService } from 'lib/swapper/utils'
@@ -8,7 +7,6 @@ import { createCache, makeSwapperAxiosServiceMonadic } from 'lib/swapper/utils'
 
 const maxAge = 5 * 1000 // 5 seconds
 const cachedUrls = ['/swap/v1/price']
-const cache = createCache(maxAge, cachedUrls)
 
 // A higher-order function to be applied to the proxied axios instance
 type AxiosInstanceHoF = (
@@ -18,7 +16,6 @@ type AxiosInstanceHoF = (
 
 const axiosConfig: AxiosRequestConfig = {
   timeout: 10000,
-  adapter: cache.adapter,
 }
 
 export const zrxServiceFactory = ({
@@ -28,6 +25,7 @@ export const zrxServiceFactory = ({
   baseUrl: string
   wrapper?: AxiosInstanceHoF
 }): MonadicSwapperAxiosService => {
-  const axiosInstance = wrapper(axios.create({ ...axiosConfig, baseURL: baseUrl }))
+  const cache = createCache(maxAge, cachedUrls, { ...axiosConfig, baseURL: baseUrl })
+  const axiosInstance = wrapper(cache)
   return makeSwapperAxiosServiceMonadic(axiosInstance, SwapperName.Zrx)
 }

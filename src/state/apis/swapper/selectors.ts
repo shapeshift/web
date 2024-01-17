afdfa91a7113f079f0b1a5c7aab96dc04faf1cbf
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2024-01-18 07:42:20 +1100

    chore: remove unused selectors in swapper slice

--- src/state/apis/swapper/selectors.ts
+++ src/state/apis/swapper/selectors.ts
@@ -1,18 +1,7 @@
 import { QueryStatus } from '@reduxjs/toolkit/query'
 import type { ReduxState } from 'state/reducer'
 
-export const selectSwapperApiPending = (state: ReduxState) =>
-  Object.values(state.swapperApi.queries).some(query => query?.status === QueryStatus.pending)
-
-export const selectSwapperApiTradeQuotePending = (state: ReduxState) =>
-  Object.values(state.swapperApi.queries).some(
-    query => query?.endpointName === 'getTradeQuote' && query?.status === QueryStatus.pending,
-  )
-
 export const selectSwapperApiTradingActivePending = (state: ReduxState) =>
   Object.values(state.swapperApi.queries).some(
     query => query?.endpointName === 'getIsTradingActive' && query?.status === QueryStatus.pending,
   )
-
-export const selectSwapperQueriesInitiated = (state: ReduxState) =>
-  Object.keys(state.swapperApi.queries).length

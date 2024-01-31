7cb55d2e46ab0d502df1c84c3dff5c51f9a6e83a
reallybeard <89934888+reallybeard@users.noreply.github.com>
2024-01-31 15:55:14 -0600

    Update RateGasRow.tsx

--- src/components/MultiHopTrade/components/RateGasRow.tsx
+++ src/components/MultiHopTrade/components/RateGasRow.tsx
@@ -1,8 +1,12 @@
+import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons'
 import type { FlexProps } from '@chakra-ui/react'
-import { Stack } from '@chakra-ui/react'
-import { type FC, memo } from 'react'
+import { Center, Collapse, Divider, Flex, Stack, useDisclosure } from '@chakra-ui/react'
+import type { SwapperName } from '@shapeshiftoss/swapper'
+import type { PropsWithChildren } from 'react'
+import { type FC, memo, useCallback } from 'react'
 import { FaGasPump } from 'react-icons/fa'
 import { useTranslate } from 'react-polyglot'
+import { useHistory } from 'react-router'
 import { Amount } from 'components/Amount/Amount'
 import { CircularProgress } from 'components/CircularProgress/CircularProgress'
 import { HelperTooltip } from 'components/HelperTooltip/HelperTooltip'
@@ -11,6 +15,9 @@ import { Text } from 'components/Text'
 import { bnOrZero } from 'lib/bignumber/bignumber'
 import { firstNonZeroDecimal } from 'lib/math'
 
+import { TradeRoutePaths } from '../types'
+import { SwapperIcon } from './TradeInput/components/SwapperIcon/SwapperIcon'
+
 type RateGasRowProps = {
   sellSymbol?: string
   buySymbol?: string
@@ -18,24 +25,37 @@ type RateGasRowProps = {
   gasFee: string
   isLoading?: boolean
   isError?: boolean
-}
+  swapperName?: SwapperName
+} & PropsWithChildren
 
 const helpersTooltipFlexProps: FlexProps = { flexDirection: 'row-reverse' }
+const rowHover = { bg: 'background.surface.raised.base' }
+const rateHover = {
+  cursor: 'pointer',
+  '.rate': { borderColor: 'text.link' },
+}
 
 export const RateGasRow: FC<RateGasRowProps> = memo(
-  ({ sellSymbol, buySymbol, rate, gasFee, isLoading, isError }) => {
+  ({ sellSymbol, buySymbol, rate, gasFee, isLoading, isError, swapperName, children }) => {
     const translate = useTranslate()
+    const history = useHistory()
+    const { isOpen, onToggle } = useDisclosure()
+
+    const handleRateClick = useCallback(() => {
+      history.push(TradeRoutePaths.Quotes)
+    }, [history])
+
     switch (true) {
       case isLoading:
         return (
-          <Stack direction='row' alignItems='center' fontSize='sm'>
+          <Stack direction='row' alignItems='center' fontSize='sm' px={6} py={4}>
             <CircularProgress size='16px' />
             <Text translation={'trade.searchingRate'} />
           </Stack>
         )
       case !rate || isError:
         return (
-          <Stack direction='row' alignItems='center' fontSize='sm'>
+          <Stack direction='row' alignItems='center' fontSize='sm' px={6} py={4}>
             <HelperTooltip
               label={translate('trade.tooltip.noRateAvailable')}
               flexProps={helpersTooltipFlexProps}
@@ -46,14 +66,52 @@ export const RateGasRow: FC<RateGasRowProps> = memo(
         )
       default:
         return (
-          <Stack direction='row' fontWeight='medium'>
-            <Row fontSize='sm' flex={1}>
-              <Row.Value fontSize='sm'>
-                <HelperTooltip
-                  label={translate('trade.tooltip.rate')}
-                  flexProps={helpersTooltipFlexProps}
+          <Stack
+            fontWeight='medium'
+            spacing={0}
+            _hover={rowHover}
+            bg={isOpen ? 'background.surface.raised.base' : 'transparent'}
+            transitionProperty='common'
+            transitionDuration='normal'
+          >
+            <Flex
+              alignItems='center'
+              justifyContent='space-between'
+              onClick={onToggle}
+              cursor='pointer'
+              px={6}
+              py={4}
+            >
+              <Row fontSize='sm' flex={1}>
+                <Row.Value
+                  fontSize='sm'
+                  display='flex'
+                  alignItems='center'
+                  gap={2}
+                  _hover={rateHover}
+                  onClick={handleRateClick}
+                  zIndex={2}
                 >
-                  <Stack width='full' direction='row' spacing={1}>
+                  <Center
+                    className='quote-icon'
+                    bg='background.surface.raised.base'
+                    borderRadius='md'
+                    borderWidth={1}
+                    p='2px'
+                    borderColor='border.base'
+                    boxShadow='0 1px 2px rgba(0,0,0,.2)'
+                  >
+                    {swapperName && <SwapperIcon size='2xs' swapperName={swapperName} />}
+                  </Center>
+                  <Stack
+                    width='full'
+                    direction='row'
+                    spacing={1}
+                    color='text.link'
+                    className='rate'
+                    borderBottomWidth={1}
+                    borderColor='transparent'
+                  >
                     <Amount.Crypto
                       fontSize='sm'
                       value='1'
@@ -66,17 +124,30 @@ export const RateGasRow: FC<RateGasRowProps> = memo(
                       symbol={buySymbol ?? ''}
                     />
                   </Stack>
-                </HelperTooltip>
-              </Row.Value>
-            </Row>
-            <Row justifyContent='flex-end' alignItems='center' width='auto' columnGap={2}>
-              <Row.Label fontSize='sm'>
-                <FaGasPump />
-              </Row.Label>
-              <Row.Value>
-                <Amount.Fiat fontSize='sm' value={gasFee} />
-              </Row.Value>
-            </Row>
+                </Row.Value>
+              </Row>
+              <Flex gap={2} alignItems='center'>
+                <Row justifyContent='flex-end' alignItems='center' width='auto' columnGap={2}>
+                  <Row.Label fontSize='sm'>
+                    <FaGasPump />
+                  </Row.Label>
+                  <Row.Value>
+                    <Amount.Fiat fontSize='sm' value={gasFee} />
+                  </Row.Value>
+                </Row>
+                {isOpen ? (
+                  <ChevronUpIcon color='text.subtle' />
+                ) : (
+                  <ChevronDownIcon color='text.subtle' />
+                )}
+              </Flex>
+            </Flex>
+            <Collapse in={isOpen}>
+              <>
+                <Divider borderColor='border.base' />
+                {children}
+              </>
+            </Collapse>
           </Stack>
         )
     }

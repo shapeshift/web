23c5d6152d0b24baf25a66fc019b7f5e509ff861
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-02-07 01:37:24 +0100

    feat: thorchain lp fees (#6127)
    * feat: improve pool asset / fee asset estimation/balance checks and
    display as error
    
    * feat: improve venacular
    
    * fix: thorchain LP accountId selection
    
    * fix: thorchain lp AccountId selection
    
    * feat: cleanup diff
    
    * feat: wire up gas fees display for pool asset
    
    * wip: wip
    
    * fix: use pool asset fee asset market data instead of pool asset
    
    * wip: wip
    
    * feat: cherry-pick #6126 bits
    
    * feat: cleanup flow
    
    * feat: error copy colorScheme
    
    * fix isZero checks
    
    * fix: pool asset fee asset balance checks
    
    * fix: approval isn't an error state
    
    * [skip ci] feat: confirm step programmatic display of one/both fees
    
    * [skip ci] feat: re-estimate at status step
    
    * feat: cleanup diff
    
    * fix: consistent 2 dp fiat fee
    
    * feat: optional asset address

--- src/lib/utils/thorchain/lp/types.ts
+++ src/lib/utils/thorchain/lp/types.ts
@@ -198,8 +198,12 @@ export type LpConfirmedDepositQuote = {
   accountIdsByChainId: Record<ChainId, AccountId>
   feeBps: string
   feeAmountFiat: string
-  assetAddress: string
+  assetAddress?: string
   quoteInboundAddress: string
+  // For informative purposes only at confirm step - to be recalculated before signing
+  totalGasFeeFiat: string
+  runeGasFeeFiat: string
+  poolAssetGasFeeFiat: string
 }
 export enum AsymSide {
   Asset = 'asset',

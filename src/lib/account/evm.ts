befdffece2f5b34a757a0a10ed047e7713c93de6
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2024-02-14 09:42:19 -0700

    simplify and use same single derivation for all wallets

--- src/lib/account/evm.ts
+++ src/lib/account/evm.ts
@@ -1,5 +1,14 @@
-import { CHAIN_REFERENCE, fromChainId, toAccountId } from '@shapeshiftoss/caip'
-import { isEvmChainId } from '@shapeshiftoss/chain-adapters'
+import {
+  arbitrumChainId,
+  arbitrumNovaChainId,
+  avalancheChainId,
+  bscChainId,
+  ethChainId,
+  gnosisChainId,
+  optimismChainId,
+  polygonChainId,
+  toAccountId,
+} from '@shapeshiftoss/caip'
 import {
   supportsArbitrum,
   supportsArbitrumNova,
@@ -10,7 +19,6 @@ import {
   supportsOptimism,
   supportsPolygon,
 } from '@shapeshiftoss/hdwallet-core'
-import { isLedger } from '@shapeshiftoss/hdwallet-ledger'
 import type { AccountMetadataById } from '@shapeshiftoss/types'
 import { assertGetEvmChainAdapter } from 'lib/utils/evm'
 
@@ -18,73 +26,29 @@ import type { DeriveAccountIdsAndMetadata } from './account'
 
 export const deriveEvmAccountIdsAndMetadata: DeriveAccountIdsAndMetadata = async args => {
   const { accountNumber, chainIds, wallet } = args
-  // Initializes a cached EVM address, since all EVM ChainIds for a given accountNumber will yield the same address
-  // Unless we do this, Ledger will be spammed, which will make things slow at worst, and possibly break the connect button
-  const ledgerEvmAddress: string | null = await (async () => {
-    if (isLedger(wallet)) {
-      for (const chainId of chainIds) {
-        if (isEvmChainId(chainId)) {
-          const adapter = assertGetEvmChainAdapter(chainId)
-          return await adapter.getAddress({ accountNumber, wallet })
-        }
-      }
-    }
-    return null
-  })()
-    // if this throws for any reason, we don't want to make this whole async function reject
-    .catch(() => null)
 
-  const result = await (async () => {
-    let acc: AccountMetadataById = {}
-    for (const chainId of chainIds) {
-      const { chainReference } = fromChainId(chainId)
-      const adapter = assertGetEvmChainAdapter(chainId)
+  let address = ''
+  const result: AccountMetadataById = {}
+  for (const chainId of chainIds) {
+    if (chainId === ethChainId && !supportsETH(wallet)) continue
+    if (chainId === avalancheChainId && !supportsAvalanche(wallet)) continue
+    if (chainId === optimismChainId && !supportsOptimism(wallet)) continue
+    if (chainId === bscChainId && !supportsBSC(wallet)) continue
+    if (chainId === polygonChainId && !supportsPolygon(wallet)) continue
+    if (chainId === gnosisChainId && !supportsGnosis(wallet)) continue
+    if (chainId === arbitrumChainId && !supportsArbitrum(wallet)) continue
+    if (chainId === arbitrumNovaChainId && !supportsArbitrumNova(wallet)) continue
 
-      if (chainReference === CHAIN_REFERENCE.EthereumMainnet) {
-        if (!supportsETH(wallet)) continue
-      }
+    const adapter = assertGetEvmChainAdapter(chainId)
+    const bip44Params = adapter.getBIP44Params({ accountNumber })
 
-      if (chainReference === CHAIN_REFERENCE.AvalancheCChain) {
-        if (!supportsAvalanche(wallet)) continue
-      }
+    // use address if we have it, there is no need to re-derive an address for every chainId since they all use the same derivation path
+    address = address || (await adapter.getAddress({ accountNumber, wallet }))
+    if (!address) continue
 
-      if (chainReference === CHAIN_REFERENCE.OptimismMainnet) {
-        if (!supportsOptimism(wallet)) continue
-      }
-
-      if (chainReference === CHAIN_REFERENCE.BnbSmartChainMainnet) {
-        if (!supportsBSC(wallet)) continue
-      }
-
-      if (chainReference === CHAIN_REFERENCE.PolygonMainnet) {
-        if (!supportsPolygon(wallet)) continue
-      }
-
-      if (chainReference === CHAIN_REFERENCE.GnosisMainnet) {
-        if (!supportsGnosis(wallet)) continue
-      }
-
-      if (chainReference === CHAIN_REFERENCE.ArbitrumMainnet) {
-        if (!supportsArbitrum(wallet)) continue
-      }
-
-      if (chainReference === CHAIN_REFERENCE.ArbitrumNovaMainnet) {
-        if (!supportsArbitrumNova(wallet)) continue
-      }
-
-      const bip44Params = adapter.getBIP44Params({ accountNumber })
-      const pubkey =
-        isLedger(wallet) && ledgerEvmAddress
-          ? ledgerEvmAddress
-          : await adapter.getAddress({ accountNumber, wallet })
-
-      if (!pubkey) continue
-
-      const accountId = toAccountId({ chainId, account: pubkey })
-      acc[accountId] = { bip44Params }
-    }
-    return acc
-  })()
+    const accountId = toAccountId({ chainId, account: address })
+    result[accountId] = { bip44Params }
+  }
 
   return result
 }

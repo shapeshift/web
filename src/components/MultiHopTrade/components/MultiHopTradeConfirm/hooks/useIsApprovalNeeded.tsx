f887d0becf2efcf6f262c2a62ef1a8f4f5e3032c
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-02-14 15:13:27 +0100

    feat: refactor useIsApprovalNeeded to use a selector on allowanceCryptobaseUnit

--- src/components/MultiHopTrade/components/MultiHopTradeConfirm/hooks/useIsApprovalNeeded.tsx
+++ src/components/MultiHopTrade/components/MultiHopTradeConfirm/hooks/useIsApprovalNeeded.tsx
@@ -1,52 +1,60 @@
-import type { AccountId } from '@shapeshiftoss/caip'
-import { type EvmChainId, evmChainIds } from '@shapeshiftoss/chain-adapters'
+import { type AccountId, fromAccountId } from '@shapeshiftoss/caip'
 import type { TradeQuoteStep } from '@shapeshiftoss/swapper'
-import { useMemo } from 'react'
+import type { Result } from '@sniptt/monads'
+import { useQuery } from '@tanstack/react-query'
+import { useCallback } from 'react'
+import { reactQueries } from 'react-queries'
 import { bn } from 'lib/bignumber/bignumber'
 import { assertUnreachable } from 'lib/utils'
 
 import { GetAllowanceErr } from './helpers'
-import { useMultiHopTradeStepAllowance } from './useMultiHopTradeStepAllowance'
 
 export const useIsApprovalNeeded = (
   tradeQuoteStep: TradeQuoteStep | undefined,
   sellAssetAccountId: AccountId | undefined,
 ) => {
-  const { isLoading, data } = useMultiHopTradeStepAllowance(tradeQuoteStep, sellAssetAccountId)
+  const selectIsApprovalNeeded = useCallback(
+    (data: Result<string, GetAllowanceErr>) => {
+      if (tradeQuoteStep === undefined) return undefined
 
-  // TODO(gomes): this already works as-is thanks to the 15s refetch interval, but surely we could do one better?
-  // Can we introspect there's an approval Txid, and fire `invalidateQueries`?
-  const isApprovalNeeded = useMemo(() => {
-    if (tradeQuoteStep === undefined) return undefined
-
-    const isEvmChainId = evmChainIds.includes(tradeQuoteStep.sellAsset.chainId as EvmChainId)
-
-    if (!isEvmChainId) return false
-
-    if (data?.isErr()) {
-      const error = data.unwrapErr()
-      // the error type is a GetAllowanceErr enum so we can handle all cases with exhaustiveness
-      // checking to prevent returning the wrong value if we add more error cases
-      switch (error) {
-        case GetAllowanceErr.IsFeeAsset:
-        case GetAllowanceErr.NotEVMChain:
-          // approval not required
-          return false
-        default:
-          assertUnreachable(error)
+      if (data?.isErr()) {
+        const error = data.unwrapErr()
+        // the error type is a GetAllowanceErr enum so we can handle all cases with exhaustiveness
+        // checking to prevent returning the wrong value if we add more error cases
+        switch (error) {
+          case GetAllowanceErr.IsFeeAsset:
+          case GetAllowanceErr.NotEVMChain:
+            // approval not required
+            return false
+          default:
+            assertUnreachable(error)
+        }
       }
-    }
 
-    const allowanceOnChainCryptoBaseUnit = data?.unwrap()
-    return allowanceOnChainCryptoBaseUnit !== undefined
-      ? bn(allowanceOnChainCryptoBaseUnit).lt(
-          tradeQuoteStep.sellAmountIncludingProtocolFeesCryptoBaseUnit,
-        )
-      : undefined
-  }, [data, tradeQuoteStep])
+      const allowanceOnChainCryptoBaseUnit = data?.unwrap()
+      return allowanceOnChainCryptoBaseUnit !== undefined
+        ? bn(allowanceOnChainCryptoBaseUnit).lt(
+            tradeQuoteStep.sellAmountIncludingProtocolFeesCryptoBaseUnit,
+          )
+        : undefined
+    },
+    [tradeQuoteStep],
+  )
+
+  const { data: isApprovalNeeded, isLoading: isApprovalNeededLoading } = useQuery({
+    ...reactQueries.common.allowanceCryptoBaseUnit(
+      tradeQuoteStep?.sellAsset.assetId,
+      tradeQuoteStep?.allowanceContract,
+      sellAssetAccountId ? fromAccountId(sellAssetAccountId)?.account : undefined,
+    ),
+    refetchInterval: 15_000,
+    enabled: Boolean(true),
+    select: selectIsApprovalNeeded,
+  })
 
   return {
-    isLoading: isApprovalNeeded === undefined || tradeQuoteStep === undefined || isLoading,
+    isLoading:
+      isApprovalNeeded === undefined || tradeQuoteStep === undefined || isApprovalNeededLoading,
     isApprovalNeeded,
   }
 }

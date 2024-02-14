445cdde269d2023728b8435b25291fe409ffd883
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-02-14 23:57:58 +0100

    feat: consume selectAllowanceCryptoBaseUnit in useIsApprovalNeeded

--- src/components/MultiHopTrade/components/MultiHopTradeConfirm/hooks/useIsApprovalNeeded.tsx
+++ src/components/MultiHopTrade/components/MultiHopTradeConfirm/hooks/useIsApprovalNeeded.tsx
@@ -4,10 +4,10 @@ import type { Result } from '@sniptt/monads'
 import { useQuery } from '@tanstack/react-query'
 import { useCallback } from 'react'
 import { reactQueries } from 'react-queries'
+import { selectAllowanceCryptoBaseUnit } from 'react-queries/hooks/selectors'
 import { bn } from 'lib/bignumber/bignumber'
-import { assertUnreachable } from 'lib/utils'
 
-import { GetAllowanceErr } from './helpers'
+import type { GetAllowanceErr } from './helpers'
 
 export const useIsApprovalNeeded = (
   tradeQuoteStep: TradeQuoteStep | undefined,
@@ -17,23 +17,9 @@ export const useIsApprovalNeeded = (
     (data: Result<string, GetAllowanceErr>) => {
       if (tradeQuoteStep === undefined) return undefined
 
-      if (data?.isErr()) {
-        const error = data.unwrapErr()
-        // the error type is a GetAllowanceErr enum so we can handle all cases with exhaustiveness
-        // checking to prevent returning the wrong value if we add more error cases
-        switch (error) {
-          case GetAllowanceErr.IsFeeAsset:
-          case GetAllowanceErr.NotEVMChain:
-            // approval not required
-            return false
-          default:
-            assertUnreachable(error)
-        }
-      }
-
-      const allowanceOnChainCryptoBaseUnit = data?.unwrap()
-      return allowanceOnChainCryptoBaseUnit !== undefined
-        ? bn(allowanceOnChainCryptoBaseUnit).lt(
+      const allowanceCryptoBaseUnit = selectAllowanceCryptoBaseUnit(data)
+      return allowanceCryptoBaseUnit !== undefined
+        ? bn(allowanceCryptoBaseUnit).lt(
             tradeQuoteStep.sellAmountIncludingProtocolFeesCryptoBaseUnit,
           )
         : undefined

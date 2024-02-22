7c786ef385c4e7cbb08f2105562f09b2209fa1af
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2024-02-23 08:00:32 +1100

    fix: remove rounding of cowswap sell amounts, dont pass-thru sell amount

--- src/lib/swapper/swappers/CowSwapper/getCowSwapTradeQuote/getCowSwapTradeQuote.ts
+++ src/lib/swapper/swappers/CowSwapper/getCowSwapTradeQuote/getCowSwapTradeQuote.ts
@@ -22,22 +22,25 @@ import {
   getSupportedChainIds,
   getValuesFromQuoteResponse,
 } from 'lib/swapper/swappers/CowSwapper/utils/helpers/helpers'
-import {
-  isNativeEvmAsset,
-  normalizeIntegerAmount,
-} from 'lib/swapper/swappers/utils/helpers/helpers'
+import { isNativeEvmAsset } from 'lib/swapper/swappers/utils/helpers/helpers'
 
 export async function getCowSwapTradeQuote(
   input: GetTradeQuoteInput,
 ): Promise<Result<TradeQuote, SwapErrorRight>> {
-  const { sellAsset, buyAsset, accountNumber, chainId, receiveAddress } = input
+  const {
+    sellAsset,
+    buyAsset,
+    accountNumber,
+    chainId,
+    receiveAddress,
+    sellAmountIncludingProtocolFeesCryptoBaseUnit,
+  } = input
 
   const slippageTolerancePercentageDecimal =
     input.slippageTolerancePercentageDecimal ??
     getDefaultSlippageDecimalPercentageForSwapper(SwapperName.CowSwap)
 
   const supportedChainIds = getSupportedChainIds()
-  const sellAmount = input.sellAmountIncludingProtocolFeesCryptoBaseUnit
 
   const assertion = assertValidTrade({ buyAsset, sellAsset, supportedChainIds })
   if (assertion.isErr()) return Err(assertion.unwrapErr())
@@ -45,9 +48,6 @@ export async function getCowSwapTradeQuote(
   const buyToken = !isNativeEvmAsset(buyAsset.assetId)
     ? fromAssetId(buyAsset.assetId).assetReference
     : COW_SWAP_NATIVE_ASSET_MARKER_ADDRESS
-
-  // making sure we do not have decimals for cowswap api (can happen at least from minQuoteSellAmount)
-  const normalizedSellAmountCryptoBaseUnit = normalizeIntegerAmount(sellAmount)
 
   const maybeNetwork = getCowswapNetwork(chainId)
   if (maybeNetwork.isErr()) return Err(maybeNetwork.unwrapErr())
@@ -70,7 +70,7 @@ export async function getCowSwapTradeQuote(
       partiallyFillable: false,
       from: receiveAddress,
       kind: ORDER_KIND_SELL,
-      sellAmountBeforeFee: normalizedSellAmountCryptoBaseUnit,
+      sellAmountBeforeFee: sellAmountIncludingProtocolFeesCryptoBaseUnit,
     },
   )
 
@@ -96,6 +96,7 @@ export async function getCowSwapTradeQuote(
   const {
     feeAmount: feeAmountInSellTokenCryptoBaseUnit,
     buyAmount: buyAmountAfterFeesCryptoBaseUnit,
+    sellAmount,
   } = data.quote
 
   const { rate, buyAmountBeforeFeesCryptoBaseUnit } = getValuesFromQuoteResponse({
@@ -128,7 +129,7 @@ export async function getCowSwapTradeQuote(
             },
           },
         },
-        sellAmountIncludingProtocolFeesCryptoBaseUnit: normalizedSellAmountCryptoBaseUnit,
+        sellAmountIncludingProtocolFeesCryptoBaseUnit: sellAmount,
         buyAmountBeforeFeesCryptoBaseUnit,
         buyAmountAfterFeesCryptoBaseUnit,
         source: SwapperName.CowSwap,

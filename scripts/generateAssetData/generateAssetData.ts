c9c90630105da4a000d7c3bb44635161cf481986
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-02-22 13:00:28 -0700

    feat: eth manual related assets data (#6273)

--- scripts/generateAssetData/generateAssetData.ts
+++ scripts/generateAssetData/generateAssetData.ts
@@ -9,7 +9,6 @@ import {
 } from '@shapeshiftoss/caip'
 import type { Asset, AssetsById } from '@shapeshiftoss/types'
 import { KnownChainIds } from '@shapeshiftoss/types'
-import assert from 'assert'
 import fs from 'fs'
 import merge from 'lodash/merge'
 import orderBy from 'lodash/orderBy'
@@ -165,15 +164,8 @@ const generateAssetData = async () => {
   )
 }
 
-;(async () => {
+const main = async () => {
   try {
-    // check zerion api key is set before starting, prevents getting through generateAssetData()
-    // and then failing
-    assert(
-      process.env.ZERION_API_KEY !== undefined,
-      'Missing Zerion API key - see readme for instructions',
-    )
-
     await generateAssetData()
     await generateRelatedAssetIndex()
 
@@ -183,4 +175,6 @@ const generateAssetData = async () => {
   } catch (err) {
     console.info(err)
   }
-})()
+}
+
+main()

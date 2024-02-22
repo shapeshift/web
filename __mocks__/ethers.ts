043cbbfc7f388a359509c35aaeeaa1ad4f24c0c9
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2024-02-22 12:27:23 -0700

    remove remaining copy pasta mocks

--- __mocks__/ethers.ts
+++ __mocks__/ethers.ts
@@ -1,49 +1,54 @@
-import ethers from 'ethers'
+import { ethers } from 'ethers'
 import { vi } from 'vitest'
+
+const Contract = vi.fn().mockImplementation(address => ({
+  decimals: () => {
+    switch (address as string) {
+      case '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48':
+        return 6
+      case '0xc770EEfAd204B5180dF6a14Ee197D99d808ee52d':
+        return 18
+      case '0x470e8de2eBaef52014A47Cb5E6aF86884947F08c':
+        return 18
+      case '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2':
+        return 18
+      default:
+        throw new Error(`no decimals mock for address: ${address}`)
+    }
+  },
+  name: () => {
+    switch (address as string) {
+      case '0xc770EEfAd204B5180dF6a14Ee197D99d808ee52d':
+        return 'FOX'
+      case '0x470e8de2eBaef52014A47Cb5E6aF86884947F08c':
+        return 'Uniswap V2'
+      case '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2':
+        return 'Wrapped Ether'
+      default:
+        throw new Error(`no decimals mock for address: ${address}`)
+    }
+  },
+  symbol: () => {
+    switch (address as string) {
+      case '0xc770EEfAd204B5180dF6a14Ee197D99d808ee52d':
+        return 'FOX'
+      case '0x470e8de2eBaef52014A47Cb5E6aF86884947F08c':
+        return 'UNI-V2'
+      case '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2':
+        return 'WETH'
+      default:
+        throw new Error(`no decimals mock for address: ${address}`)
+    }
+  },
+}))
+
+const JsonRpcProvider = vi.fn()
 
 const ethersMock = {
   ...ethers,
-  JsonRpcProvider: vi.fn(),
-  Contract: vi.fn().mockImplementation(address => ({
-    decimals: () => {
-      switch (address as string) {
-        case '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48':
-          return 6
-        case '0xc770EEfAd204B5180dF6a14Ee197D99d808ee52d':
-          return 18
-        case '0x470e8de2eBaef52014A47Cb5E6aF86884947F08c':
-          return 18
-        case '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2':
-          return 18
-        default:
-          throw new Error(`no decimals mock for address: ${address}`)
-      }
-    },
-    name: () => {
-      switch (address as string) {
-        case '0xc770EEfAd204B5180dF6a14Ee197D99d808ee52d':
-          return 'FOX'
-        case '0x470e8de2eBaef52014A47Cb5E6aF86884947F08c':
-          return 'Uniswap V2'
-        case '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2':
-          return 'Wrapped Ether'
-        default:
-          throw new Error(`no decimals mock for address: ${address}`)
-      }
-    },
-    symbol: () => {
-      switch (address as string) {
-        case '0xc770EEfAd204B5180dF6a14Ee197D99d808ee52d':
-          return 'FOX'
-        case '0x470e8de2eBaef52014A47Cb5E6aF86884947F08c':
-          return 'UNI-V2'
-        case '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2':
-          return 'WETH'
-        default:
-          throw new Error(`no decimals mock for address: ${address}`)
-      }
-    },
-  })),
+  JsonRpcProvider,
+  Contract,
 }
 
-export { ethersMock as ethers }
+export * from 'ethers'
+export { ethersMock as ethers, Contract, JsonRpcProvider }
--- packages/unchained-client/src/evm/optimism/parser/__tests__/optimism.test.ts
+++ packages/unchained-client/src/evm/optimism/parser/__tests__/optimism.test.ts
@@ -1,6 +1,5 @@
 import { optimismAssetId, optimismChainId } from '@shapeshiftoss/caip'
 import type { evm } from '@shapeshiftoss/common-api'
-import type { ethers } from 'ethers'
 import { describe, expect, it, vi } from 'vitest'
 
 import type { Trade, Transfer } from '../../../../types'
@@ -21,98 +20,6 @@ import zrxTradeOpToEth from './mockData/zrxTradeOpToEth'
 import zrxTradeUsdcToOp from './mockData/zrxTradeUsdcToOp'
 
 const mockedApi = vi.mocked(new V1Api())
-
-vi.mock('ethers', async importActual => {
-  const actual: typeof ethers = await importActual()
-  return {
-    ...actual,
-    Contract: vi.fn().mockImplementation(address => ({
-      decimals: () => {
-        switch (address as string) {
-          case '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48':
-            return 6
-          case '0xc770EEfAd204B5180dF6a14Ee197D99d808ee52d':
-            return 18
-          case '0x470e8de2eBaef52014A47Cb5E6aF86884947F08c':
-            return 18
-          case '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2':
-            return 18
-          default:
-            throw new Error(`no decimals mock for address: ${address}`)
-        }
-      },
-      name: () => {
-        switch (address as string) {
-          case '0xc770EEfAd204B5180dF6a14Ee197D99d808ee52d':
-            return 'FOX'
-          case '0x470e8de2eBaef52014A47Cb5E6aF86884947F08c':
-            return 'Uniswap V2'
-          case '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2':
-            return 'Wrapped Ether'
-          default:
-            throw new Error(`no decimals mock for address: ${address}`)
-        }
-      },
-      symbol: () => {
-        switch (address as string) {
-          case '0xc770EEfAd204B5180dF6a14Ee197D99d808ee52d':
-            return 'FOX'
-          case '0x470e8de2eBaef52014A47Cb5E6aF86884947F08c':
-            return 'UNI-V2'
-          case '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2':
-            return 'WETH'
-          default:
-            throw new Error(`no decimals mock for address: ${address}`)
-        }
-      },
-    })),
-
-    JsonRpcProvider: vi.fn(),
-    default: {
-      ...actual,
-      Contract: vi.fn().mockImplementation(address => ({
-        decimals: () => {
-          switch (address as string) {
-            case '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48':
-              return 6
-            case '0xc770EEfAd204B5180dF6a14Ee197D99d808ee52d':
-              return 18
-            case '0x470e8de2eBaef52014A47Cb5E6aF86884947F08c':
-              return 18
-            case '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2':
-              return 18
-            default:
-              throw new Error(`no decimals mock for address: ${address}`)
-          }
-        },
-        name: () => {
-          switch (address as string) {
-            case '0xc770EEfAd204B5180dF6a14Ee197D99d808ee52d':
-              return 'FOX'
-            case '0x470e8de2eBaef52014A47Cb5E6aF86884947F08c':
-              return 'Uniswap V2'
-            case '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2':
-              return 'Wrapped Ether'
-            default:
-              throw new Error(`no decimals mock for address: ${address}`)
-          }
-        },
-        symbol: () => {
-          switch (address as string) {
-            case '0xc770EEfAd204B5180dF6a14Ee197D99d808ee52d':
-              return 'FOX'
-            case '0x470e8de2eBaef52014A47Cb5E6aF86884947F08c':
-              return 'UNI-V2'
-            case '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2':
-              return 'WETH'
-            default:
-              throw new Error(`no decimals mock for address: ${address}`)
-          }
-        },
-      })),
-    },
-  }
-})
 
 const tokenMetadata: evm.TokenMetadata = {
   name: 'Foxy',

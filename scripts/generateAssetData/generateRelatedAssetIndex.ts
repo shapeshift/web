faaa2406d837c149747ca1510a649c31074d4ff7
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-02-22 10:40:11 -0700

    feat: cleanup

--- scripts/generateAssetData/generateRelatedAssetIndex.ts
+++ scripts/generateAssetData/generateRelatedAssetIndex.ts
@@ -1,18 +1,9 @@
 import 'dotenv/config'
 
-import assert from 'assert'
-
 import { generateRelatedAssetIndex } from './generateRelatedAssetIndex/generateRelatedAssetIndex'
 
 const main = async () => {
   try {
-    // check zerion api key is set before starting, prevents getting through generateAssetIndex()
-    // and then failing
-    assert(
-      process.env.ZERION_API_KEY !== undefined,
-      'Missing Zerion API key - see readme for instructions',
-    )
-
     await generateRelatedAssetIndex()
 
     console.info(

4fa2c1048fd1d4dc6cb3e035611121f635b12481
Apotheosis <0xapotheosis@gmail.com>
2024-02-22 17:11:17 +1100

    chore: wire remove liquidity confirm page (#6266)

--- src/pages/ThorChainLP/components/RemoveLiquidity/RemoveLiquidityConfirm.tsx
+++ src/pages/ThorChainLP/components/RemoveLiquidity/RemoveLiquidityConfirm.tsx
@@ -1,3 +1,32 @@
-export const RemoveLiquidityConfirm = () => {
-  return <p>Remove Confirm</p>
+import { thorchainAssetId } from '@shapeshiftoss/caip'
+import { useCallback } from 'react'
+import { useHistory } from 'react-router'
+import type { LpConfirmedWithdrawalQuote } from 'lib/utils/thorchain/lp/types'
+
+import { ReusableLpConfirm } from '../ReusableLpConfirm'
+import { RemoveLiquidityRoutePaths } from './types'
+
+type RemoveLiquidityConfirmProps = {
+  confirmedQuote: LpConfirmedWithdrawalQuote
+}
+
+export const RemoveLiquidityConfirm = ({ confirmedQuote }: RemoveLiquidityConfirmProps) => {
+  const history = useHistory()
+
+  const handleBack = useCallback(() => {
+    history.push(RemoveLiquidityRoutePaths.Input)
+  }, [history])
+
+  const handleConfirm = useCallback(() => {
+    history.push(RemoveLiquidityRoutePaths.Status)
+  }, [history])
+
+  return (
+    <ReusableLpConfirm
+      baseAssetId={thorchainAssetId}
+      confirmedQuote={confirmedQuote}
+      handleBack={handleBack}
+      handleConfirm={handleConfirm}
+    />
+  )
 }

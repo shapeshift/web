828eb9c2e3527101a4329f6a4754cbee2306b576
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2024-02-26 20:24:41 +1100

    feat: add withdraw utxo tx

--- src/pages/ThorChainLP/components/ReusableLpStatus/TransactionRow.tsx
+++ src/pages/ThorChainLP/components/ReusableLpStatus/TransactionRow.tsx
@@ -333,12 +333,12 @@ export const TransactionRow: React.FC<TransactionRowProps> = ({
       }
       case 'Send': {
         if (!inboundAddressData || !accountAssetAddress) return undefined
-        const memo = `+:${thorchainNotationAssetId}:${otherAssetAddress ?? ''}:ss:${
-          confirmedQuote.feeBps
-        }`
+        const memo = isDeposit
+          ? `+:${thorchainNotationAssetId}:${otherAssetAddress ?? ''}:ss:${confirmedQuote.feeBps}`
+          : `-:${thorchainNotationAssetId}:${confirmedQuote.withdrawalBps}`
 
         return {
-          cryptoAmount: amountCryptoPrecision,
+          cryptoAmount: isDeposit ? toBaseUnit(amountCryptoPrecision, asset.precision) : '0',
           assetId,
           to: inboundAddressData.address,
           from: accountAssetAddress,
@@ -526,12 +526,14 @@ export const TransactionRow: React.FC<TransactionRowProps> = ({
             // ATOM/RUNE/ UTXOs- obviously make me a switch case for things to be cleaner
             if (!accountAssetAddress) throw new Error('No accountAddress found')
 
-            const memo = `+:${thorchainNotationAssetId}:${otherAssetAddress ?? ''}:ss:${
-              confirmedQuote.feeBps
-            }`
+            const memo = isDeposit
+              ? `+:${thorchainNotationAssetId}:${otherAssetAddress ?? ''}:ss:${
+                  confirmedQuote.feeBps
+                }`
+              : `-:${thorchainNotationAssetId}:${confirmedQuote.withdrawalBps}`
 
             const estimateFeesArgs = {
-              cryptoAmount: amountCryptoPrecision,
+              cryptoAmount: isDeposit ? amountCryptoPrecision : '0',
               assetId,
               to: inboundAddressData?.address,
               from: accountAssetAddress,
@@ -542,7 +544,7 @@ export const TransactionRow: React.FC<TransactionRowProps> = ({
             }
             const estimatedFees = await estimateFees(estimateFeesArgs)
             const sendInput: SendInput = {
-              cryptoAmount: amountCryptoPrecision,
+              cryptoAmount: isDeposit ? toBaseUnit(amountCryptoPrecision, asset.precision) : '0',
               assetId,
               to: inboundAddressData?.address,
               from: accountAssetAddress,

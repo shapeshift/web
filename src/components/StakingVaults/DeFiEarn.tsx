cb46015daf8c37d43afe09130f40530b44574919
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2024-02-29 14:40:22 +1100

    perf: dont fetch opportunities twice on dashbaord

--- src/components/StakingVaults/DeFiEarn.tsx
+++ src/components/StakingVaults/DeFiEarn.tsx
@@ -10,7 +10,6 @@ import { selectPortfolioChainIdsSortedUserCurrency } from 'state/slices/selector
 import { useAppSelector } from 'state/store'
 
 import { GlobalFilter } from './GlobalFilter'
-import { useFetchOpportunities } from './hooks/useFetchOpporunities'
 import type { PositionTableProps } from './PositionTable'
 import { PositionTable } from './PositionTable'
 import type { ProviderTableProps } from './ProviderTable'
@@ -45,8 +44,6 @@ export const DeFiEarn: React.FC<DefiEarnProps> = ({
   const translate = useTranslate()
   const [selectedChainId, setSelectedChainId] = useState<ChainId | undefined>()
   const portfolioChainIds = useAppSelector(selectPortfolioChainIdsSortedUserCurrency)
-
-  useFetchOpportunities()
 
   return (
     <Flex width='full' flexDir='column' gap={6}>

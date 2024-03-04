7b192becc8adbe837c11d9afd977e34f53527a36
reallybeard <89934888+reallybeard@users.noreply.github.com>
2024-03-04 10:35:14 -0600

    Update DialogCloseButton.tsx

--- src/components/Modal/components/DialogCloseButton.tsx
+++ src/components/Modal/components/DialogCloseButton.tsx
@@ -1,4 +1,4 @@
-import { CloseIcon } from '@chakra-ui/icons'
+import { SmallCloseIcon } from '@chakra-ui/icons'
 import type { ModalCloseButtonProps } from '@chakra-ui/react'
 import { IconButton, ModalCloseButton, useMediaQuery } from '@chakra-ui/react'
 import { isMobile } from 'react-device-detect'
@@ -6,12 +6,21 @@ import { breakpoints } from 'theme/theme'
 
 type DialogCloseButtonProps = ModalCloseButtonProps
 
-const closeIcon = <CloseIcon />
+const closeIcon = <SmallCloseIcon />
 
 export const DialogCloseButton: React.FC<DialogCloseButtonProps> = ({ children, ...rest }) => {
   const [isLargerThanMd] = useMediaQuery(`(min-width: ${breakpoints['md']})`, { ssr: false })
   if (isMobile || !isLargerThanMd) {
-    return <IconButton size='sm' fontSize='xs' isRound icon={closeIcon} aria-label='close dialog' />
+    return (
+      <IconButton
+        size='sm'
+        fontSize='xs'
+        variant='ghost'
+        isRound
+        icon={closeIcon}
+        aria-label='close dialog'
+      />
+    )
   }
   return <ModalCloseButton position='static' {...rest} />
 }

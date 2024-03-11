99f25bce36acc2a80973135cea14851cfa31ed00
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2024-03-12 09:37:21 +1100

    perf: use react-query for fetchAllOpportunities requests

--- src/components/StakingVaults/hooks/useFetchOpportunities.tsx
+++ src/components/StakingVaults/hooks/useFetchOpportunities.tsx
@@ -1,3 +1,4 @@
+import type { AccountId, AssetId } from '@shapeshiftoss/caip'
 import {
   avalancheChainId,
   bchChainId,
@@ -5,11 +6,11 @@ import {
   btcChainId,
   cosmosChainId,
   dogeChainId,
-  ethChainId,
   fromAccountId,
   ltcChainId,
 } from '@shapeshiftoss/caip'
-import { useEffect, useMemo, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { useMemo } from 'react'
 import { useSelector } from 'react-redux'
 import { useFeatureFlag } from 'hooks/useFeatureFlag/useFeatureFlag'
 import {
@@ -21,23 +22,20 @@ import {
   fetchAllOpportunitiesMetadataByChainId,
   fetchAllOpportunitiesUserDataByAccountId,
 } from 'state/slices/opportunitiesSlice/thunks'
+import type { PortfolioAccount } from 'state/slices/portfolioSlice/portfolioSliceCommon'
 import {
   selectPortfolioAccounts,
   selectPortfolioAssetIds,
   selectPortfolioLoadingStatus,
   selectWalletAccountIds,
 } from 'state/slices/selectors'
-import { useAppDispatch } from 'state/store'
 
 export const useFetchOpportunities = () => {
-  const dispatch = useAppDispatch()
   const portfolioLoadingStatus = useSelector(selectPortfolioLoadingStatus)
   const requestedAccountIds = useSelector(selectWalletAccountIds)
   const portfolioAssetIds = useSelector(selectPortfolioAssetIds)
   const portfolioAccounts = useSelector(selectPortfolioAccounts)
   const DynamicLpAssets = useFeatureFlag('DynamicLpAssets')
-
-  const [isLoading, setIsLoading] = useState(true)
 
   const { isLoading: isZapperAppsBalancesOutputLoading } = useGetZapperAppsBalancesOutputQuery()
   const { isLoading: isZapperUniV2PoolAssetIdsLoading } = useGetZapperUniV2PoolAssetIdsQuery(
@@ -45,16 +43,24 @@ export const useFetchOpportunities = () => {
     { skip: !DynamicLpAssets },
   )
 
-  // TODO: this needs to be split up into separate RTK queries so we aren't refetching when moving around the app
-  useEffect(() => {
-    ;(async () => {
-      if (!requestedAccountIds.length) return
-      if (portfolioLoadingStatus === 'loading') return
+  const queryKey: [
+    string,
+    {
+      requestedAccountIds: AccountId[]
+      portfolioAssetIds: AssetId[]
+      portfolioAccounts: Record<AccountId, PortfolioAccount>
+    },
+  ] = useMemo(() => {
+    return ['fetchAllOpportunities', { requestedAccountIds, portfolioAssetIds, portfolioAccounts }]
+  }, [portfolioAccounts, portfolioAssetIds, requestedAccountIds])
 
-      setIsLoading(true)
+  const { isLoading } = useQuery({
+    queryKey,
+    queryFn: async ({ queryKey: [_, { requestedAccountIds }] }) => {
+      if (!requestedAccountIds.length) return
 
       await Promise.all(
-        requestedAccountIds.map(accountId => {
+        requestedAccountIds.map(async accountId => {
           const { chainId } = fromAccountId(accountId)
           switch (chainId) {
             case btcChainId:
@@ -64,28 +70,24 @@ export const useFetchOpportunities = () => {
             case cosmosChainId:
             case bscChainId:
             case avalancheChainId:
-            case ethChainId:
-              return (async () => {
-                await fetchAllOpportunitiesIdsByChainId(chainId)
-                await fetchAllOpportunitiesMetadataByChainId(chainId)
-                await fetchAllOpportunitiesUserDataByAccountId(accountId)
-              })()
+              await fetchAllOpportunitiesIdsByChainId(chainId)
+              await fetchAllOpportunitiesMetadataByChainId(chainId)
+              await fetchAllOpportunitiesUserDataByAccountId(accountId)
+              break
             default:
-              return Promise.resolve()
+              break
           }
         }),
       )
 
-      setIsLoading(false)
-    })()
-  }, [
-    portfolioLoadingStatus,
-    portfolioAccounts,
-    DynamicLpAssets,
-    dispatch,
-    requestedAccountIds,
-    portfolioAssetIds,
-  ])
+      // We *have* to return a value other than undefined from react-query queries, see
+      // https://tanstack.com/query/v4/docs/react/guides/migrating-to-react-query-4#undefined-is-an-illegal-cache-value-for-successful-queries
+      return null
+    },
+    // once the portfolio is loaded, fetch opportunities data
+    enabled: portfolioLoadingStatus !== 'loading',
+    refetchInterval: false,
+  })
 
   return useMemo(
     () => ({

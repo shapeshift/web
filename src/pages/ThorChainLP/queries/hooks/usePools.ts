320e1a09bdfb85c12d5461e7480f00dfa133e39b
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-03-15 18:31:44 +0100

    feat: expose volumeStats union in pool query-wrapper

--- src/pages/ThorChainLP/queries/hooks/usePools.ts
+++ src/pages/ThorChainLP/queries/hooks/usePools.ts
@@ -1,14 +1,15 @@
 import { thorchainAssetId } from '@shapeshiftoss/caip'
-import { useQuery } from '@tanstack/react-query'
-import { useCallback } from 'react'
+import { useQueries, useQuery } from '@tanstack/react-query'
+import { useCallback, useMemo } from 'react'
 import { reactQueries } from 'react-queries'
 import { bn } from 'lib/bignumber/bignumber'
 import type { MidgardPoolResponse } from 'lib/swapper/swappers/ThorchainSwapper/types'
+import type { MidgardSwapHistoryResponse } from 'lib/utils/thorchain/lp/types'
 import { selectAssets, selectMarketDataByAssetIdUserCurrency } from 'state/slices/selectors'
 import { useAppSelector } from 'state/store'
 
-import type { Pool } from './usePool'
-import { getPool } from './usePool'
+import type { Pool, VolumeStats } from './usePool'
+import { getPool, getVolumeStats, selectSwapsData } from './usePool'
 
 export type { Pool } from './usePool'
 
@@ -42,5 +43,44 @@ export const usePools = () => {
     select: selectPools,
   })
 
-  return pools
+  const swapsDataQueries = useMemo(
+    () => ({
+      queries: (pools.data ?? []).map(pool => ({
+        ...reactQueries.midgard.swapsData(pool.asset, 'hour', 7 * 24),
+        select: (swapsData: MidgardSwapHistoryResponse) => selectSwapsData(swapsData, pool.asset),
+      })),
+    }),
+    [pools.data],
+  )
+
+  const swapsData = useQueries(swapsDataQueries)
+
+  const volumeStatsByThorchainNotationPoolAssetId = useMemo(
+    () =>
+      swapsData.reduce<Record<string, VolumeStats>>((acc, query) => {
+        const { data } = query
+        if (!data) return acc
+        const { thorchainNotationPoolAssetId } = data
+
+        acc[thorchainNotationPoolAssetId] = getVolumeStats(data, runeMarketData.price)
+
+        return acc
+      }, {}),
+    [runeMarketData.price, swapsData],
+  )
+
+  const poolsWithVolumeStats = useMemo(() => {
+    const data = (pools.data ?? []).reduce<Pool[]>((acc, pool) => {
+      if (!pool) return acc
+      const volumeStats = volumeStatsByThorchainNotationPoolAssetId[pool.asset]
+      if (!volumeStats) return acc
+
+      acc.push({ ...pool, ...volumeStats })
+      return acc
+    }, [])
+
+    return { ...pools, data }
+  }, [pools, volumeStatsByThorchainNotationPoolAssetId])
+
+  return poolsWithVolumeStats
 }

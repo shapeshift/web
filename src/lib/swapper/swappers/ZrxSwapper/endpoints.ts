0e48942c55ec12c62ec7010e216066bd500169fb
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2024-03-21 10:09:49 +1100

    chore: add comment

--- src/lib/swapper/swappers/ZrxSwapper/endpoints.ts
+++ src/lib/swapper/swappers/ZrxSwapper/endpoints.ts
@@ -68,6 +68,8 @@ export const zrxApi: SwapperApi = {
       value,
       data,
       chainId: Number(fromChainId(chainId).chainReference),
+      // Use the higher amount of the node or the API, as the node doesn't always provide enought gas padding for
+      // total gas used.
       gasLimit: BigNumber.max(gasLimit, estimatedGas).toFixed(),
       ...feeData,
     }

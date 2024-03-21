78e5cdaf7ccccdc61856796a163c168a3bbe0832
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2024-03-21 11:25:36 +1100

    fix: assets page using incorrect component

--- src/pages/Assets/Assets.tsx
+++ src/pages/Assets/Assets.tsx
@@ -1,14 +1,19 @@
+import { useMemo } from 'react'
 import { useTranslate } from 'react-polyglot'
+import { AssetSearch } from 'components/AssetSearch/AssetSearch'
 import { Main } from 'components/Layout/Main'
 import { SEO } from 'components/Layout/Seo'
-import { TradeAssetSearch } from 'components/TradeAssetSearch/TradeAssetSearch'
+import { selectAssetsSortedByMarketCapUserCurrencyBalanceAndName } from 'state/slices/common-selectors'
+import { useAppSelector } from 'state/store'
 
 export const Assets = () => {
   const translate = useTranslate()
+  const maybeAssets = useAppSelector(selectAssetsSortedByMarketCapUserCurrencyBalanceAndName)
+  const assets = useMemo(() => maybeAssets ?? [], [maybeAssets])
   return (
     <Main display='flex' flexDir='column' height='calc(100vh - 72px)'>
       <SEO title={translate('navBar.assets')} />
-      <TradeAssetSearch />
+      <AssetSearch assets={assets} allowWalletUnsupportedAssets />
     </Main>
   )
 }

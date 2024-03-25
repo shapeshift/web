34b046c59c588a5001b2983e9b922f51c454a6f8
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2024-03-25 13:02:09 +1100

    chore: use condensed form

--- src/pages/Dashboard/RecentTransactions.tsx
+++ src/pages/Dashboard/RecentTransactions.tsx
@@ -6,14 +6,13 @@ import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import { Text } from 'components/Text'
 import { TransactionHistoryList } from 'components/TransactionHistory/TransactionHistoryList'
-import type { ReduxState } from 'state/reducer'
 import { selectTxIds } from 'state/slices/selectors'
 
 type RecentTransactionProps = { limit?: number; viewMoreLink?: boolean } & CardProps
 
 export const RecentTransactions: React.FC<RecentTransactionProps> = memo(
   ({ limit = 10, viewMoreLink, ...rest }) => {
-    const txIds = useSelector((state: ReduxState) => selectTxIds(state))
+    const txIds = useSelector(selectTxIds)
     const translate = useTranslate()
     return (
       <Card variant='dashboard' {...rest}>

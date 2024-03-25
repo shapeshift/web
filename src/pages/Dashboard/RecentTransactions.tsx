83955732fac904f46e60af8c0b85a8f591f5cfbc
Apotheosis <0xapotheosis@gmail.com>
2024-03-25 13:03:01 +1100

    fix: allow load more recent txs on wallet overview (#6547)
    * fix: allow load more recent txs on wallet overview
    
    * chore: use initialTxsCount
    
    * chore: use condensed form

--- src/pages/Dashboard/RecentTransactions.tsx
+++ src/pages/Dashboard/RecentTransactions.tsx
@@ -6,14 +6,13 @@ import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import { Text } from 'components/Text'
 import { TransactionHistoryList } from 'components/TransactionHistory/TransactionHistoryList'
-import type { ReduxState } from 'state/reducer'
-import { selectLastNTxIds } from 'state/slices/selectors'
+import { selectTxIds } from 'state/slices/selectors'
 
 type RecentTransactionProps = { limit?: number; viewMoreLink?: boolean } & CardProps
 
 export const RecentTransactions: React.FC<RecentTransactionProps> = memo(
   ({ limit = 10, viewMoreLink, ...rest }) => {
-    const recentTxIds = useSelector((state: ReduxState) => selectLastNTxIds(state, limit))
+    const txIds = useSelector(selectTxIds)
     const translate = useTranslate()
     return (
       <Card variant='dashboard' {...rest}>
@@ -27,7 +26,7 @@ export const RecentTransactions: React.FC<RecentTransactionProps> = memo(
             </Button>
           )}
         </CardHeader>
-        <TransactionHistoryList txIds={recentTxIds} useCompactMode={true} />
+        <TransactionHistoryList txIds={txIds} useCompactMode={true} initialTxsCount={limit} />
       </Card>
     )
   },

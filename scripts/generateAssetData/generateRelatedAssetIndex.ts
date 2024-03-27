4fcb921b70c40c50f9080557538c2e7fb3646178
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-03-27 17:23:04 +0100

    feat: regenerate asset data workflow (#6574)

--- scripts/generateAssetData/generateRelatedAssetIndex.ts
+++ scripts/generateAssetData/generateRelatedAssetIndex.ts
@@ -7,10 +7,12 @@ const main = async () => {
     await generateRelatedAssetIndex()
 
     console.info(
-      "Done. Don't forget to add a migration to clear assets state so the new assets are loaded.",
+      "Related assets data generated. Don't forget to add a migration to clear assets state so the new assets are loaded.",
     )
+    process.exit(0)
   } catch (err) {
     console.info(err)
+    process.exit(1)
   }
 }
 

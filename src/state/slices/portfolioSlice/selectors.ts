3527622b70f03bef1126dd248390c2a7ba5f5d07
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-04-01 20:34:50 +0200

    feat: more cleanup

--- src/state/slices/portfolioSlice/selectors.ts
+++ src/state/slices/portfolioSlice/selectors.ts
@@ -707,10 +707,9 @@ export const selectAccountIdsByAssetId = createCachedSelector(
 export const selectAccountIdsByChainId = createDeepEqualOutputSelector(
   selectPortfolioAccounts,
   (accounts): Record<ChainId, AccountId[] | undefined> =>
-    Object.keys(accounts).reduce<Record<ChainId, AccountId[] | undefined>>((acc, accountId) => {
+    Object.keys(accounts).reduce<Record<ChainId, AccountId[]>>((acc, accountId) => {
       const chainId = fromAccountId(accountId).chainId
       if (!acc[chainId]) acc[chainId] = []
-      // TS is drunk, we just pushed an empty array to this in case acc[chainId] was undefined
       acc[chainId]!.push(accountId)
       return acc
     }, {}),

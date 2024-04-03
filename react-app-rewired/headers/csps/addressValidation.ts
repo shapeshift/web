cca0b8ee94642dad292a6f5b2848cad47615aabc
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2024-04-04 08:44:01 +1100

    fix: csp

--- react-app-rewired/headers/csps/addressValidation.ts
+++ react-app-rewired/headers/csps/addressValidation.ts
@@ -1,5 +1,5 @@
 import type { Csp } from '../types'
 
 export const csp: Csp = {
-  'connect-src': ['https://proxy.shapeshift.com/validate/'],
+  'connect-src': ['https://api.proxy.shapeshift.com/api/v1/validate/'],
 }

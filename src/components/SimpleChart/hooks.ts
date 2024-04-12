d169ae26058910553d1ff5a4446a847ea4e8ed19
reallybeard <89934888+reallybeard@users.noreply.github.com>
2024-04-12 07:51:36 -0500

    feat: chart styles and loading state (#6655)

---
+++ src/components/SimpleChart/hooks.ts
@@ -0,0 +1,22 @@
+import type { UTCTimestamp } from 'lightweight-charts'
+import { useCallback } from 'react'
+import { selectSelectedLocale } from 'state/slices/selectors'
+import { useAppSelector } from 'state/store'
+
+export function useHeaderDateFormatter() {
+  const locale = useAppSelector(selectSelectedLocale)
+  return useCallback(
+    (time?: UTCTimestamp) => {
+      if (!time) return '-'
+      const headerTimeFormatOptions: Intl.DateTimeFormatOptions = {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+      }
+      return new Date(time * 1000).toLocaleString(locale, headerTimeFormatOptions)
+    },
+    [locale],
+  )
+}

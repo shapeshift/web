a80e3298a7be7cadeb5b5b00768e1b219ada3cf6
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2024-04-17 08:33:22 +1000

    chore: extract motion style to constant

--- src/components/WarningAcknowledgement/WarningAcknowledgement.tsx
+++ src/components/WarningAcknowledgement/WarningAcknowledgement.tsx
@@ -11,20 +11,21 @@ const initialProps = { opacity: 0 }
 const animateProps = { opacity: 1 }
 const exitProps = { opacity: 0, transition: { duration: 0.5 } }
 const transitionProps = { delay: 0.2, duration: 0.1 }
+const motionStyle: MotionStyle = {
+  backgroundColor: 'var(--chakra-colors-blanket)',
+  position: 'absolute',
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+  zIndex: 4,
+}
 
 const WarningOverlay: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <motion.div
       key='overlay'
-      style={{
-        backgroundColor: 'var(--chakra-colors-blanket)',
-        position: 'absolute',
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-        zIndex: 4,
-      }}
+      style={motionStyle}
       initial={initialProps}
       animate={animateProps}
       exit={exitProps}

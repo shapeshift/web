de0d01373e0b114b48d77f2511eef2c85f5735ab
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-04-16 16:51:59 +0400

    chore: update src/constants/chains.ts

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -2,7 +2,7 @@ import { arbitrumNovaChainId, gnosisChainId } from '@shapeshiftoss/caip'
 import { KnownChainIds } from '@shapeshiftoss/types'
 import { selectFeatureFlag } from 'state/slices/selectors'
 import { store } from 'state/store'
-// returns known ChainIds as an array, excludingg the ones that are currently flagged off
+// returns known ChainIds as an array, excluding the ones that are currently flagged off
 export const knownChainIds = Object.values(KnownChainIds).filter(chainId => {
   const isArbitrumEnabled = selectFeatureFlag(store.getState(), 'Arbitrum')
   const isArbitrumNovaEnabled = selectFeatureFlag(store.getState(), 'ArbitrumNova')

f131df4afb70c378397159a13ab2e9329fb1addb
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-04-16 19:46:53 +0400

    feat: use KnownChainIds for all

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -1,4 +1,3 @@
-import { arbitrumNovaChainId, gnosisChainId } from '@shapeshiftoss/caip'
 import { KnownChainIds } from '@shapeshiftoss/types'
 import { selectFeatureFlag } from 'state/slices/selectors'
 import { store } from 'state/store'
@@ -9,9 +8,9 @@ export const knownChainIds = Object.values(KnownChainIds).filter(chainId => {
   const isGnosisEnabled = selectFeatureFlag(store.getState(), 'Gnosis')
   const isPolygonEnabled = selectFeatureFlag(store.getState(), 'Polygon')
   const isOptimismEnabled = selectFeatureFlag(store.getState(), 'Optimism')
-  if (chainId === arbitrumNovaChainId && !isArbitrumNovaEnabled) return false
+  if (chainId === KnownChainIds.ArbitrumNovaMainnet && !isArbitrumNovaEnabled) return false
   if (chainId === KnownChainIds.ArbitrumMainnet && !isArbitrumEnabled) return false
-  if (chainId === gnosisChainId && !isGnosisEnabled) return false
+  if (chainId === KnownChainIds.GnosisMainnet && !isGnosisEnabled) return false
   if (chainId === KnownChainIds.PolygonMainnet && !isPolygonEnabled) return false
   if (chainId === KnownChainIds.OptimismMainnet && !isOptimismEnabled) return false
 

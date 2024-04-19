cf72775be8ae6efeabc08fa298ea988acf03061f
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2024-04-19 11:01:02 +1000

    feat: load accounts without upserting into redux

--- src/components/ImportAccountsDrawer/ImportAccountsDrawer.tsx
+++ src/components/ImportAccountsDrawer/ImportAccountsDrawer.tsx
@@ -7,6 +7,7 @@ import {
   DrawerFooter,
   DrawerHeader,
   DrawerOverlay,
+  Skeleton,
   Switch,
   Table,
   TableContainer,
@@ -18,15 +19,16 @@ import {
 import type { ChainId } from '@shapeshiftoss/caip'
 import { type AccountId } from '@shapeshiftoss/caip'
 import type { Asset } from '@shapeshiftoss/types'
-import { useCallback, useMemo } from 'react'
+import { useIsFetching, useQueryClient } from '@tanstack/react-query'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import { useTranslate } from 'react-polyglot'
+import { reactQueries } from 'react-queries'
 import { Amount } from 'components/Amount/Amount'
 import { MiddleEllipsis } from 'components/MiddleEllipsis/MiddleEllipsis'
 import { RawText, Text } from 'components/Text'
+import { useWallet } from 'hooks/useWallet/useWallet'
 import { accountIdToLabel } from 'state/slices/portfolioSlice/utils'
 import {
-  selectAccountIdsByChainId,
-  selectAccountNumberByAccountId,
   selectFeeAssetByChainId,
   selectPortfolioCryptoPrecisionBalanceByFilter,
 } from 'state/slices/selectors'
@@ -36,22 +38,18 @@ export type ImportAccountsDrawerProps = {
   chainId: ChainId
   isOpen: boolean
   onClose: () => void
-  toggleAccountId: (accountId: AccountId) => void
-  finalFocusRef?: React.RefObject<HTMLElement>
 }
 
 type TableRowProps = {
   accountId: AccountId
+  accountNumber: number
   feeAsset: Asset
   toggleAccountId: (accountId: AccountId) => void
 }
 
-const TableRow = ({ feeAsset, accountId, toggleAccountId }: TableRowProps) => {
-  const translate = useTranslate()
-  const accountNumberFilter = useMemo(() => ({ accountId }), [accountId])
-  const accountNumber = useAppSelector(state =>
-    selectAccountNumberByAccountId(state, accountNumberFilter),
-  )
+const disabledProp = { opacity: 0.5, cursor: 'not-allowed', userSelect: 'none' }
+
+const TableRow = ({ feeAsset, accountId, accountNumber, toggleAccountId }: TableRowProps) => {
   const handleChange = useCallback(() => toggleAccountId(accountId), [accountId, toggleAccountId])
   const accountLabel = useMemo(() => accountIdToLabel(accountId), [accountId])
   const balanceFilter = useMemo(
@@ -64,7 +62,7 @@ const TableRow = ({ feeAsset, accountId, toggleAccountId }: TableRowProps) => {
   return (
     <Tr>
       <Td>
-        <RawText>{`${translate('common.account')} #${accountNumber}`}</RawText>
+        <RawText>{accountNumber}</RawText>
       </Td>
       <Td>
         <Switch onChange={handleChange} />
@@ -81,21 +79,94 @@ const TableRow = ({ feeAsset, accountId, toggleAccountId }: TableRowProps) => {
   )
 }
 
-export const ImportAccountsDrawer = ({
-  chainId,
-  isOpen,
-  onClose,
-  toggleAccountId,
-  finalFocusRef,
-}: ImportAccountsDrawerProps) => {
-  const translate = useTranslate()
-  const accountIdsByChainId = useAppSelector(selectAccountIdsByChainId)
-  const accountIds = useMemo(
-    () => accountIdsByChainId[chainId] ?? [],
-    [accountIdsByChainId, chainId],
+const LoadingRow = () => {
+  return (
+    <Tr>
+      <Td>
+        <Skeleton height='24px' width='100%' />
+      </Td>
+      <Td>
+        <Skeleton height='24px' width='100%' />
+      </Td>
+      <Td>
+        <Skeleton height='24px' width='100%' />
+      </Td>
+      <Td>
+        <Skeleton height='24px' width='100%' />
+      </Td>
+    </Tr>
   )
+}
+
+export const ImportAccountsDrawer = ({ chainId, isOpen, onClose }: ImportAccountsDrawerProps) => {
+  const translate = useTranslate()
+  const wallet = useWallet().state.wallet
   const feeAsset = useAppSelector(state => selectFeeAssetByChainId(state, chainId))
   const chainNamespaceDisplayName = feeAsset?.networkName ?? ''
+  const [accounts, setAccounts] = useState<{ accountNumber: number; accountId: AccountId }[]>([])
+  const queryClient = useQueryClient()
+  const isLoading = useIsFetching({ queryKey: ['accountManagement', 'loadAccount'] }) > 0
+
+  // TODO:
+  // when "done" is clicked, all enabled accounts for this chain will be upserted into the portfolio
+  // all disabled ones will be removed.
+  // need to call dispatch(portfolioApi.getAccount({ accountId, upsertOnFetch: true }))
+  // which internally calls dispatch(portfolio.actions.upsertPortfolio(account)) and upserts assets
+  // But also need to remove accounts that were disabled.
+
+  // initial fetch to detect the number of accounts based on the "first empty account" heuristic
+  useEffect(() => {
+    ;(async () => {
+      if (!wallet) return
+
+      let accountNumber = 0
+      const accounts = []
+
+      while (true) {
+        try {
+          const accountResult = await queryClient.fetchQuery(
+            reactQueries.accountManagement.loadAccount(accountNumber, chainId, wallet),
+          )
+
+          if (!accountResult) break
+
+          const { accountId, hasActivity } = accountResult
+
+          // Add the account before checking if it has activity so user has ability to toggle it if needed.
+          accounts.push({ accountNumber, accountId })
+
+          // If the account has no activity, it's the first empty account.
+          if (!hasActivity) {
+            break
+          }
+        } catch (error) {
+          console.error(error)
+          break
+        }
+
+        accountNumber++
+      }
+
+      setAccounts(accounts)
+    })()
+  }, [chainId, queryClient, wallet])
+
+  const handleLoadMore = useCallback(async () => {
+    if (!wallet) return
+    const accountNumber = accounts.length
+    const accountResult = await queryClient.fetchQuery(
+      reactQueries.accountManagement.loadAccount(accountNumber, chainId, wallet),
+    )
+    if (!accountResult) return
+    setAccounts(previousAccounts => {
+      const { accountId } = accountResult
+      return [...previousAccounts, { accountNumber, accountId }]
+    })
+  }, [accounts, chainId, queryClient, wallet])
+
+  const handleToggleAccountId = useCallback((accountId: AccountId) => {
+    console.log('handleToggleAccountId', accountId)
+  }, [])
 
   if (!feeAsset) {
     console.error(`No fee asset found for chainId: ${chainId}`)
@@ -103,13 +174,7 @@ export const ImportAccountsDrawer = ({
   }
 
   return (
-    <Drawer
-      isOpen={isOpen}
-      size='lg'
-      placement='right'
-      onClose={onClose}
-      finalFocusRef={finalFocusRef}
-    >
+    <Drawer isOpen={isOpen} size='lg' placement='right' onClose={onClose}>
       <DrawerOverlay />
       <DrawerContent>
         <DrawerCloseButton />
@@ -128,27 +193,42 @@ export const ImportAccountsDrawer = ({
           <TableContainer>
             <Table variant='simple'>
               <Tbody>
-                {accountIds.map(accountId => (
+                {accounts.map(({ accountId, accountNumber }) => (
                   <TableRow
                     key={accountId}
                     accountId={accountId}
+                    accountNumber={accountNumber}
                     feeAsset={feeAsset}
-                    toggleAccountId={toggleAccountId}
+                    toggleAccountId={handleToggleAccountId}
                   />
                 ))}
+                {isLoading && <LoadingRow />}
               </Tbody>
             </Table>
           </TableContainer>
-          <Button colorScheme='gray' onClick={onClose}>
+          <Button
+            colorScheme='gray'
+            onClick={handleLoadMore}
+            isDisabled={isLoading}
+            _disabled={disabledProp}
+          >
             {translate('common.loadMore')}
           </Button>
         </DrawerBody>
 
         <DrawerFooter>
-          <Button colorScheme='gray' mr={3} onClick={onClose}>
+          <Button
+            colorScheme='gray'
+            mr={3}
+            onClick={onClose}
+            isDisabled={isLoading}
+            _disabled={disabledProp}
+          >
             {translate('common.cancel')}
           </Button>
-          <Button colorScheme='blue'>{translate('common.done')}</Button>
+          <Button colorScheme='blue' isDisabled={isLoading} _disabled={disabledProp}>
+            {translate('common.done')}
+          </Button>
         </DrawerFooter>
       </DrawerContent>
     </Drawer>

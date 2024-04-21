d7eab609492a5483d71a0f3fe6fd111f62c31527
Apotheosis <0xapotheosis@gmail.com>
2024-04-22 09:52:18 +1000

    feat: account management menu (#6724)
    * chore: add manage accounts menu item
    
    * chore: enable account management for dev
    
    * chore: make account management a connectedMenuComponent concern
    
    * chore: add ledger menu & refactor
    
    * chore: add demo wallet menu
    
    * chore: add kk connected menu
    
    * chore: add ledger comment
    
    * fix: hide MenuDivider when !isAccountManagementEnabled

---
+++ src/context/WalletProvider/KeepKey/components/KeepKeyMenu.tsx
@@ -0,0 +1,33 @@
+import { EditIcon } from '@chakra-ui/icons'
+import { MenuDivider, MenuItem } from '@chakra-ui/react'
+import { useCallback } from 'react'
+import { useTranslate } from 'react-polyglot'
+import { useFeatureFlag } from 'hooks/useFeatureFlag/useFeatureFlag'
+import { useModal } from 'hooks/useModal/useModal'
+
+const editIcon = <EditIcon />
+
+export const KeepKeyConnectedMenuItems = () => {
+  const translate = useTranslate()
+  const isAccountManagementEnabled = useFeatureFlag('AccountManagement')
+
+  const accountManagementPopover = useModal('backupNativePassphrase') // FIXME: use accountManagementPopover once ready
+
+  const handleManageAccountsMenuItemClick = useCallback(
+    () => accountManagementPopover.open({}),
+    [accountManagementPopover],
+  )
+
+  return (
+    <>
+      {isAccountManagementEnabled && (
+        <>
+          <MenuDivider />
+          <MenuItem icon={editIcon} onClick={handleManageAccountsMenuItemClick}>
+            {translate('manageAccounts.menuTitle')}
+          </MenuItem>
+        </>
+      )}
+    </>
+  )
+}

bdd79254f07f9c93449760ce623de9b8bc5cabfb
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2024-04-24 09:51:43 +1000

    feat: wire up select chain UI

--- src/components/ManageAccountsDrawer/ManageAccountsDrawer.tsx
+++ src/components/ManageAccountsDrawer/ManageAccountsDrawer.tsx
@@ -1,42 +1,65 @@
-import { ethChainId } from '@shapeshiftoss/caip'
-import { useEffect, useMemo, useState } from 'react'
+import type { ChainId } from '@shapeshiftoss/caip'
+import { useCallback, useMemo, useState } from 'react'
 import { assertUnreachable } from 'lib/utils'
 
 import { DrawerWrapper } from './components/DrawerWrapper'
 import { ImportAccounts } from './components/ImportAccounts'
+import { SelectChain } from './components/SelectChain'
 
 export type ManageAccountsDrawerProps = {
   isOpen: boolean
   onClose: () => void
 }
 
-type ManageAccountsStep = 'manageAccounts' | 'selectChain' | 'ledgerOpenApp' | 'importAccounts'
+type ManageAccountsStep = 'selectChain' | 'ledgerOpenApp' | 'importAccounts'
 
 export const ManageAccountsDrawer = ({ isOpen, onClose }: ManageAccountsDrawerProps) => {
-  const [step, setStep] = useState<ManageAccountsStep>('manageAccounts')
+  const [step, setStep] = useState<ManageAccountsStep>('selectChain')
+  const [selectedChainId, setSelectedChainId] = useState<ChainId | null>(null)
 
-  // TEMP: Set the initial step to 'importAccounts' until we have the other steps implemented
-  useEffect(() => {
-    setStep('importAccounts')
-  }, [])
+  const isLedger = false
+
+  const handleNext = useCallback(() => {
+    switch (step) {
+      case 'selectChain':
+        if (isLedger) {
+          setStep('ledgerOpenApp')
+        }
+        setStep('importAccounts')
+        break
+      case 'ledgerOpenApp':
+        setStep('importAccounts')
+        break
+      case 'importAccounts':
+        onClose()
+        break
+      default:
+        assertUnreachable(step)
+    }
+  }, [isLedger, onClose, step])
+
+  const handleSelectChainId = useCallback(
+    (chainId: ChainId) => {
+      setSelectedChainId(chainId)
+      handleNext()
+    },
+    [handleNext],
+  )
 
   const drawerContent = useMemo(() => {
     switch (step) {
-      case 'manageAccounts':
-        // TODO: Implement ManageAccounts component
-        return null
       case 'selectChain':
-        // TODO: Implement SelectChain component
-        return null
+        return <SelectChain onSelectChainId={handleSelectChainId} />
       case 'ledgerOpenApp':
         // TODO: Implement LedgerOpenApp component
         return null
       case 'importAccounts':
-        return <ImportAccounts chainId={ethChainId} onClose={onClose} />
+        if (!selectedChainId) return null
+        return <ImportAccounts chainId={selectedChainId} onClose={onClose} />
       default:
         assertUnreachable(step)
     }
-  }, [onClose, step])
+  }, [handleSelectChainId, onClose, selectedChainId, step])
 
   return (
     <DrawerWrapper isOpen={isOpen} onClose={onClose}>

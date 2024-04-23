6b109770780de61277802e0730f70832f60f900a
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2024-04-24 09:51:43 +1000

    feat: wire up select chain UI

--- src/components/ManageAccountsDrawer/components/SelectChain.tsx
+++ src/components/ManageAccountsDrawer/components/SelectChain.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { SearchIcon } from '@chakra-ui/icons'
 import {
   Box,
@@ -21,32 +20,13 @@ import { selectAssetById, selectWalletSupportedChainIds } from 'state/slices/sel
 import { useAppSelector } from 'state/store'
 
 import { filterChainIdsBySearchTerm } from '../helpers'
-=======
-import { Button, SimpleGrid, VStack } from '@chakra-ui/react'
-import type { ChainId } from '@shapeshiftoss/caip'
-import { useCallback, useMemo, useState } from 'react'
-import { useTranslate } from 'react-polyglot'
-import { LazyLoadAvatar } from 'components/LazyLoadAvatar'
-import { RawText } from 'components/Text'
-import { chainIdToFeeAssetId } from 'lib/utils'
-import { selectAssetById, selectWalletSupportedChainIds } from 'state/slices/selectors'
-import { useAppSelector } from 'state/store'
-
->>>>>>> d3d710611c (chore: move previous manage accounts UI into select chain UI)
 import { DrawerContentWrapper } from './DrawerContent'
 
 export type SelectChainProps = {
   onSelectChainId: (chainId: ChainId) => void
-<<<<<<< HEAD
   onClose: () => void
 }
 
-=======
-}
-
-const disabledProp = { opacity: 0.5, cursor: 'not-allowed', userSelect: 'none' }
-
->>>>>>> d3d710611c (chore: move previous manage accounts UI into select chain UI)
 const ChainButton = ({
   chainId,
   onClick,
@@ -58,7 +38,6 @@ const ChainButton = ({
   const feeAsset = useAppSelector(state => selectAssetById(state, feeAssetId ?? ''))
   const handleClick = useCallback(() => onClick(chainId), [chainId, onClick])
 
-<<<<<<< HEAD
   const chainAdapter = useMemo(() => {
     return assertGetChainAdapter(chainId)
   }, [chainId])
@@ -70,21 +49,11 @@ const ChainButton = ({
       <VStack direction='column'>
         <LazyLoadAvatar src={feeAsset.networkIcon ?? feeAsset.icon} size='sm' />
         <RawText>{chainAdapter.getDisplayName()}</RawText>
-=======
-  if (!feeAsset) return null
-
-  return (
-    <Button height='100px' width='100px' onClick={handleClick}>
-      <VStack direction='column'>
-        <LazyLoadAvatar src={feeAsset.networkIcon ?? feeAsset.icon} size='sm' />
-        <RawText>{feeAsset.symbol}</RawText>
->>>>>>> d3d710611c (chore: move previous manage accounts UI into select chain UI)
       </VStack>
     </Button>
   )
 }
 
-<<<<<<< HEAD
 export const SelectChain = ({ onSelectChainId, onClose }: SelectChainProps) => {
   const translate = useTranslate()
   const [searchTermChainIds, setSearchTermChainIds] = useState<ChainId[]>([])
@@ -153,48 +122,6 @@ export const SelectChain = ({ onSelectChainId, onClose }: SelectChainProps) => {
       </>
     )
   }, [chainButtons, handleSubmit, register, translate])
-=======
-export const SelectChain = ({ onSelectChainId }: SelectChainProps) => {
-  const translate = useTranslate()
-  const [selectedChainId, setSelectedChainId] = useState<ChainId | null>(null)
-
-  const walletSupportedChainIds = useAppSelector(selectWalletSupportedChainIds)
-
-  const handleClickDone = useCallback(() => {
-    // This should never happen, but just in case.
-    if (!selectedChainId) return
-
-    onSelectChainId(selectedChainId)
-  }, [onSelectChainId, selectedChainId])
-
-  const chainButtons = useMemo(() => {
-    return walletSupportedChainIds.map(chainId => {
-      return <ChainButton key={chainId} chainId={chainId} onClick={setSelectedChainId} />
-    })
-  }, [walletSupportedChainIds])
-
-  const footer = useMemo(() => {
-    return (
-      <Button
-        disabled={selectedChainId === null}
-        colorScheme='blue'
-        onClick={handleClickDone}
-        width='full'
-        _disabled={disabledProp}
-      >
-        {translate('common.done')}
-      </Button>
-    )
-  }, [handleClickDone, selectedChainId, translate])
-
-  const body = useMemo(() => {
-    return (
-      <SimpleGrid columns={3} spacing={4} placeItems='center' height='100%'>
-        {chainButtons}
-      </SimpleGrid>
-    )
-  }, [chainButtons])
->>>>>>> d3d710611c (chore: move previous manage accounts UI into select chain UI)
 
   return (
     <DrawerContentWrapper

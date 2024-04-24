f64a97cdd067365fa8c91096917956108c2af503
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2024-04-24 11:30:57 +1000

    feat: reimplemented manage accounts modal UI

--- src/components/Modals/ManageAccounts/ManageAccountsModal.tsx
+++ src/components/Modals/ManageAccounts/ManageAccountsModal.tsx
@@ -1,6 +1,9 @@
 import { InfoIcon } from '@chakra-ui/icons'
 import {
+  Box,
   Button,
+  Flex,
+  HStack,
   IconButton,
   Modal,
   ModalBody,
@@ -11,16 +14,54 @@ import {
   ModalOverlay,
   VStack,
 } from '@chakra-ui/react'
-import { useCallback } from 'react'
+import type { ChainId } from '@shapeshiftoss/caip'
+import { useCallback, useMemo } from 'react'
 import { useTranslate } from 'react-polyglot'
+import { LazyLoadAvatar } from 'components/LazyLoadAvatar'
 import { RawText } from 'components/Text'
 import { useModal } from 'hooks/useModal/useModal'
+import { assertGetChainAdapter, chainIdToFeeAssetId } from 'lib/utils'
+import { selectWalletSupportedChainIds } from 'state/slices/common-selectors'
+import { selectAccountIdsByChainId, selectAssetById } from 'state/slices/selectors'
+import { useAppSelector } from 'state/store'
 
 export type ManageAccountsModalProps = {
   title?: string
 }
 
 const infoIcon = <InfoIcon />
+
+const ConnectedChain = ({
+  chainId,
+  onClick,
+}: {
+  chainId: ChainId
+  onClick: (chainId: ChainId) => void
+}) => {
+  const accountIdsByChainId = useAppSelector(selectAccountIdsByChainId)
+  const numAccounts = accountIdsByChainId[chainId]?.length ?? 0
+
+  const feeAssetId = chainIdToFeeAssetId(chainId)
+  const feeAsset = useAppSelector(state => selectAssetById(state, feeAssetId ?? ''))
+  const handleClick = useCallback(() => onClick(chainId), [chainId, onClick])
+
+  const chainAdapter = useMemo(() => {
+    return assertGetChainAdapter(chainId)
+  }, [chainId])
+
+  if (numAccounts === 0 || !feeAsset) return null
+  return (
+    <Button width='full' onClick={handleClick} p={2}>
+      <Flex justifyContent='space-between' width='full' alignItems='center'>
+        <HStack>
+          <LazyLoadAvatar src={feeAsset.networkIcon ?? feeAsset.icon} size='sm' />
+          <RawText>{chainAdapter.getDisplayName()}</RawText>
+        </HStack>
+        <Box>{numAccounts}</Box>
+      </Flex>
+    </Button>
+  )
+}
 
 export const ManageAccountsModal = ({
   title = 'accountManagement.manageAccounts.title',
@@ -31,6 +72,23 @@ export const ManageAccountsModal = ({
   const handleInfoClick = useCallback(() => {
     console.log('info clicked')
   }, [])
+
+  // TODO: redux will have to be updated to use the selected chains from this flow
+  const walletSupportedChainIds = useAppSelector(selectWalletSupportedChainIds)
+
+  const handleClickChain = useCallback((chainId: ChainId) => {
+    console.log('chain clicked', chainId)
+  }, [])
+
+  const handleClickAddChain = useCallback(() => {
+    console.log('add chain clicked')
+  }, [])
+
+  const connectedChains = useMemo(() => {
+    return walletSupportedChainIds.map(chainId => {
+      return <ConnectedChain key={chainId} chainId={chainId} onClick={handleClickChain} />
+    })
+  }, [handleClickChain, walletSupportedChainIds])
 
   return (
     <Modal isOpen={isOpen} onClose={close} isCentered size='sm'>
@@ -53,13 +111,14 @@ export const ManageAccountsModal = ({
           onClick={handleInfoClick}
         />
         <ModalCloseButton position='absolute' top={3} right={3} />
-        <ModalBody>
-          {/* TODO: Implement me */}
-          Placeholder content
+        <ModalBody maxH='400px' overflow='scroll'>
+          <VStack spacing={2} width='full'>
+            {connectedChains}
+          </VStack>
         </ModalBody>
         <ModalFooter justifyContent='center' pb={6}>
           <VStack spacing={2} width='full'>
-            <Button colorScheme='blue' onClick={close} width='full'>
+            <Button colorScheme='blue' onClick={handleClickAddChain} width='full'>
               {translate('accountManagement.manageAccounts.addChain')}
             </Button>
             <Button colorScheme='gray' onClick={close} width='full'>

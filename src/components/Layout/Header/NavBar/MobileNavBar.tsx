e8dc4bb814d65778703ea4782abd0a4a91988456
reallybeard <89934888+reallybeard@users.noreply.github.com>
2024-04-24 14:39:52 -0500

    feat: add new mobile nav bar (#6747)
    * add bottom sheet dialog
    
    * try out vaul
    
    * hook up send
    
    * Create SubPage.tsx
    
    * adjusting dialog header
    
    * Update DialogCloseButton.tsx
    
    * add new action menu
    
    * updates
    
    * update more height stuff
    
    * use 100dvh instead of vh
    
    * use 100vh
    
    * anchor footer at the bottom of the screen
    
    * update new asset search
    
    * Update SelectAssets.tsx
    
    * add min height for regular modals
    
    * fix not connected button
    
    * Delete SubPage.tsx
    
    * remove mobile order
    
    * Update main.json
    
    * fix the qr code modal
    
    * Update MobileNavBar.tsx
    
    * Update MobileNavBar.tsx

--- src/components/Layout/Header/NavBar/MobileNavBar.tsx
+++ src/components/Layout/Header/NavBar/MobileNavBar.tsx
@@ -1,15 +1,97 @@
-import { Flex, useColorModeValue } from '@chakra-ui/react'
+import { ArrowDownIcon } from '@chakra-ui/icons'
+import type { CenterProps } from '@chakra-ui/react'
+import {
+  Button,
+  Center,
+  Flex,
+  IconButton,
+  SimpleGrid,
+  Stack,
+  useColorModeValue,
+  useDisclosure,
+} from '@chakra-ui/react'
 import { union } from 'lodash'
-import { memo, useLayoutEffect, useMemo } from 'react'
+import React, { memo, useCallback, useLayoutEffect, useMemo } from 'react'
+import { FaRegCreditCard } from 'react-icons/fa'
+import { useTranslate } from 'react-polyglot'
+import { useHistory } from 'react-router'
 import { routes } from 'Routes/RoutesCommon'
+import { QRCodeIcon } from 'components/Icons/QRCode'
+import { SendIcon } from 'components/Icons/SendIcon'
+import { SwapIcon } from 'components/Icons/SwapIcon'
+import { Dialog } from 'components/Modal/components/Dialog'
+import { DialogBody } from 'components/Modal/components/DialogBody'
+import { DialogHeader } from 'components/Modal/components/DialogHeader'
+import { RawText } from 'components/Text'
 import { usePlugins } from 'context/PluginProvider/PluginProvider'
+import { useModal } from 'hooks/useModal/useModal'
+import { useWallet } from 'hooks/useWallet/useWallet'
 import { bnOrZero } from 'lib/bignumber/bignumber'
 
 import { MobileNavLink } from './MobileNavLink'
 
-const displayProp = { base: 'flex', md: 'none' }
+const displayProp = { base: 'grid', md: 'none' }
+
+const swapIcon = <SwapIcon />
+const sendIcon = <SendIcon />
+const receiveIcon = <ArrowDownIcon />
+const buyIcon = <FaRegCreditCard />
+const qrCodeIcon = <QRCodeIcon />
+
+type ActionMenuButtonProps = {
+  icon?: JSX.Element
+  title: string
+  body: string
+  onClick?: () => void
+
+  iconColor?: CenterProps['bg']
+  isDisabled?: boolean
+}
+
+const actionMenuButtonHover = { bg: 'transparent' }
+const actionButtonActive = { opacity: 0.5, bg: 'transparent' }
+
+const ActionMenuButton: React.FC<ActionMenuButtonProps> = ({
+  icon,
+  title,
+  body,
+  iconColor = 'blue.500',
+  onClick,
+
+  isDisabled,
+}) => {
+  return (
+    <Button
+      height='auto'
+      onClick={onClick}
+      variant='ghost'
+      display='flex'
+      alignItems='center'
+      width='full'
+      color='text.base'
+      justifyContent='flex-start'
+      textAlign='left'
+      gap={4}
+      px={0}
+      isDisabled={isDisabled}
+      _hover={actionMenuButtonHover}
+      _active={actionButtonActive}
+    >
+      <Center flexShrink={0} bg={iconColor} boxSize='48px' borderRadius='full' fontSize='xl'>
+        {icon}
+      </Center>
+      <Stack spacing={1} flex={1}>
+        <RawText fontWeight='semibold'>{title}</RawText>
+        <RawText color='text.subtle' fontWeight='normal'>
+          {body}
+        </RawText>
+      </Stack>
+    </Button>
+  )
+}
 
 export const MobileNavBar = memo(() => {
+  const translate = useTranslate()
   const bg = useColorModeValue(
     `linear-gradient(
       to top,
@@ -50,7 +132,15 @@ export const MobileNavBar = memo(() => {
       hsla(211, 11%, 7%, 0) 100%
     );`,
   )
+  const { isOpen, onClose, onOpen } = useDisclosure()
+  const {
+    state: { isConnected },
+  } = useWallet()
+  const send = useModal('send')
+  const receive = useModal('receive')
+  const qrCode = useModal('qrCode')
   const { routes: pluginRoutes } = usePlugins()
+  const history = useHistory()
   const allRoutes = useMemo(
     () =>
       union(routes, pluginRoutes)
@@ -77,22 +167,107 @@ export const MobileNavBar = memo(() => {
     }
   }, [])
 
+  const handleQrCodeClick = useCallback(() => {
+    onClose()
+    qrCode.open({})
+  }, [onClose, qrCode])
+
+  const handleSendClick = useCallback(() => {
+    onClose()
+    send.open({})
+  }, [onClose, send])
+
+  const handleReceiveClick = useCallback(() => {
+    onClose()
+    receive.open({})
+  }, [onClose, receive])
+
+  const handleTradeClick = useCallback(() => {
+    onClose()
+    history.push('/trade')
+  }, [history, onClose])
+
+  const handleBuyClick = useCallback(() => {
+    onClose()
+    history.push('/buy-crypto')
+  }, [history, onClose])
+
   return (
-    <Flex
-      position='fixed'
-      bottom={0}
-      left={0}
-      width='100%'
-      bgImage={bg}
-      pt={6}
-      zIndex='banner'
-      paddingBottom='calc(env(safe-area-inset-bottom, 16px) - 16px)'
-      display={displayProp}
-      className='mobile-nav'
-    >
-      {allRoutes.map(route => (
-        <MobileNavLink key={route.path} {...route} />
-      ))}
-    </Flex>
+    <>
+      <SimpleGrid
+        position='fixed'
+        bottom={0}
+        left={0}
+        width='100%'
+        gridTemplateColumns='1fr 1fr 1fr 1fr 1fr'
+        bgImage={bg}
+        pt={6}
+        zIndex='banner'
+        alignItems='center'
+        paddingBottom='calc(env(safe-area-inset-bottom, 16px) - 16px)'
+        display={displayProp}
+        className='mobile-nav'
+      >
+        {allRoutes.map(route => (
+          <MobileNavLink key={route.path} {...route} />
+        ))}
+        <Flex alignItems='center' justifyContent='center' order={3}>
+          <IconButton
+            size='lg'
+            icon={swapIcon}
+            isRound
+            colorScheme='blue'
+            aria-label='action menu'
+            onClick={onOpen}
+          />
+        </Flex>
+      </SimpleGrid>
+      <Dialog isOpen={isOpen} onClose={onClose} height='auto'>
+        <DialogHeader />
+        <DialogBody
+          pb='calc(env(safe-area-inset-bottom) + 2rem)'
+          display='flex'
+          flexDir='column'
+          gap={8}
+          px={6}
+        >
+          <ActionMenuButton
+            onClick={handleSendClick}
+            icon={sendIcon}
+            title={translate('navBar.actionMenu.send.title')}
+            body={translate('navBar.actionMenu.send.body')}
+            isDisabled={!isConnected}
+          />
+          <ActionMenuButton
+            onClick={handleReceiveClick}
+            iconColor='green.600'
+            icon={receiveIcon}
+            title={translate('navBar.actionMenu.receive.title')}
+            body={translate('navBar.actionMenu.receive.body')}
+            isDisabled={!isConnected}
+          />
+          <ActionMenuButton
+            onClick={handleTradeClick}
+            iconColor='purple.500'
+            icon={swapIcon}
+            title={translate('navBar.actionMenu.trade.title')}
+            body={translate('navBar.actionMenu.trade.body')}
+          />
+          <ActionMenuButton
+            onClick={handleBuyClick}
+            icon={buyIcon}
+            title={translate('navBar.actionMenu.buy.title')}
+            body={translate('navBar.actionMenu.buy.body')}
+          />
+          <ActionMenuButton
+            onClick={handleQrCodeClick}
+            icon={qrCodeIcon}
+            title={translate('navBar.actionMenu.qrcode.title')}
+            body={translate('navBar.actionMenu.qrcode.body')}
+            isDisabled={!isConnected}
+          />
+        </DialogBody>
+      </Dialog>
+    </>
   )
 })

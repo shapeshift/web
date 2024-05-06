70ef4cc006f6beb05d477a28cbb6107c87943aef
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-05-06 10:04:58 +0300

    feat: rm monkey patch

--- src/index.tsx
+++ src/index.tsx
@@ -13,57 +13,57 @@ import { reportWebVitals } from 'lib/reportWebVitals'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 // Remove this condition to test sentry locally
-// if (window.location.hostname !== 'localhost') {
-const VALID_ENVS = [
-  'localhost',
-  'develop',
-  'release',
-  'app',
-  'private',
-  'yeet',
-  'beard',
-  'juice',
-  'wood',
-  'gome',
-] as const
+if (window.location.hostname !== 'localhost') {
+  const VALID_ENVS = [
+    'localhost',
+    'develop',
+    'release',
+    'app',
+    'private',
+    'yeet',
+    'beard',
+    'juice',
+    'wood',
+    'gome',
+  ] as const
 
-const environment = (() => {
-  if (window.location.hostname.includes('app')) return 'production'
+  const environment = (() => {
+    if (window.location.hostname.includes('app')) return 'production'
 
-  if (VALID_ENVS.some(env => window.location.hostname.includes(env)))
-    return window.location.hostname.split('.')[0]
-})()
-Sentry.init({
-  environment,
-  dsn: getConfig().REACT_APP_SENTRY_DSN_URL,
-  attachStacktrace: true,
-  integrations: [
-    // Sentry.browserTracingIntegration(),
-    // Sentry.replayIntegration(),
-    Sentry.httpClientIntegration({
-      failedRequestStatusCodes: [
-        [400, 428],
-        // i.e no 429s
-        [430, 599],
-      ],
+    if (VALID_ENVS.some(env => window.location.hostname.includes(env)))
+      return window.location.hostname.split('.')[0]
+  })()
+  Sentry.init({
+    environment,
+    dsn: getConfig().REACT_APP_SENTRY_DSN_URL,
+    attachStacktrace: true,
+    integrations: [
+      // Sentry.browserTracingIntegration(),
+      // Sentry.replayIntegration(),
+      Sentry.httpClientIntegration({
+        failedRequestStatusCodes: [
+          [400, 428],
+          // i.e no 429s
+          [430, 599],
+        ],
 
-      failedRequestTargets: [/^(?!.*\.?alchemy\.com).*$/],
-    }),
-    Sentry.browserApiErrorsIntegration(),
-    Sentry.breadcrumbsIntegration(),
-    Sentry.globalHandlersIntegration(),
-    Sentry.httpContextIntegration(),
-  ],
-  beforeSend(event) {
-    // https://github.com/getsentry/sentry-javascript/issues/8353 / https://forum.sentry.io/t/turn-off-event-grouping/10916/3
-    event.fingerprint = [(Math.random() * 1000000).toString()]
-    return event
-  },
-  enableTracing: true,
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ['localhost'],
-})
-// }
+        failedRequestTargets: [/^(?!.*\.?alchemy\.com).*$/],
+      }),
+      Sentry.browserApiErrorsIntegration(),
+      Sentry.breadcrumbsIntegration(),
+      Sentry.globalHandlersIntegration(),
+      Sentry.httpContextIntegration(),
+    ],
+    beforeSend(event) {
+      // https://github.com/getsentry/sentry-javascript/issues/8353 / https://forum.sentry.io/t/turn-off-event-grouping/10916/3
+      event.fingerprint = [(Math.random() * 1000000).toString()]
+      return event
+    },
+    enableTracing: true,
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: ['localhost'],
+  })
+}
 
 const rootElement = document.getElementById('root')!
 const root = createRoot(rootElement)

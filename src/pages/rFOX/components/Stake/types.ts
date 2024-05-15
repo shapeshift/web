8a47fd3a9f25c3febe2460a7b46834c63fb45d3e
reallybeard <89934888+reallybeard@users.noreply.github.com>
2024-05-15 10:50:23 -0500

    revert

--- src/pages/rFOX/components/Stake/types.ts
+++
@@ -1,9 +0,0 @@
-export enum StakeRoutePaths {
-  Input = '/add/input',
-  Confirm = '/add/confirm',
-  Status = '/add/status',
-}
-
-export type StakeRouteProps = {
-  headerComponent?: JSX.Element
-}

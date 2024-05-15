47647f5660868c7644ce01552f049d724b016d96
woody <125113430+woodenfurniture@users.noreply.github.com>
2024-05-15 10:48:08 +1000

    fix: only display wallet connected chains in related asset selection (#6910)
    * fix: only display wallet connected chains in related asset selection
    
    * fix: show all chains including those which are not connected in add liquidity input
    
    * fix: allow users to buy assets on chains the wallet isnt connected to

--- src/components/RelatedAssets/RelatedAssets.tsx
+++ src/components/RelatedAssets/RelatedAssets.tsx
@@ -6,7 +6,8 @@ import type { Column, Row } from 'react-table'
 import { ReactTable } from 'components/ReactTable/ReactTable'
 import { AssetCell } from 'components/StakingVaults/Cells'
 import { Text } from 'components/Text'
-import { selectAssets, selectRelatedAssetIds } from 'state/slices/selectors'
+import { selectRelatedAssetIds } from 'state/slices/related-assets-selectors'
+import { selectAssets } from 'state/slices/selectors'
 import { useAppSelector } from 'state/store'
 
 type RelatedAssetsProps = {
@@ -14,7 +15,10 @@ type RelatedAssetsProps = {
 }
 
 export const RelatedAssets: React.FC<RelatedAssetsProps> = ({ assetId }) => {
-  const relatedAssetIds = useAppSelector(state => selectRelatedAssetIds(state, assetId))
+  const relatedAssetIdsFilter = useMemo(() => ({ assetId }), [assetId])
+  const relatedAssetIds = useAppSelector(state =>
+    selectRelatedAssetIds(state, relatedAssetIdsFilter),
+  )
   const assets = useAppSelector(selectAssets)
   const history = useHistory()
 

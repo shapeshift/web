78237fa00fa4f7914e3f859a57b1a16137bff4b9
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-05-16 01:07:08 +0200

    [skip ci] fix: forgotten mergefix

--- src/pages/RFOX/components/Stake/components/StakeSummary.tsx
+++ src/pages/RFOX/components/Stake/components/StakeSummary.tsx
@@ -1,6 +1,5 @@
 import { Skeleton, Stack } from '@chakra-ui/react'
 import type { AssetId } from '@shapeshiftoss/caip'
-<<<<<<< HEAD
 import { foxStakingV1Abi } from 'contracts/abis/FoxStakingV1'
 import { useCallback } from 'react'
 import { useTranslate } from 'react-polyglot'
@@ -10,20 +9,12 @@ import { Amount } from 'components/Amount/Amount'
 import { Row } from 'components/Row/Row'
 import { Text } from 'components/Text'
 import { formatDuration } from 'lib/utils/time'
-=======
-import { useCallback } from 'react'
-import { useTranslate } from 'react-polyglot'
-import { Amount } from 'components/Amount/Amount'
-import { Row } from 'components/Row/Row'
-import { Text } from 'components/Text'
->>>>>>> origin/develop
 import { selectAssetById } from 'state/slices/selectors'
 import { useAppSelector } from 'state/store'
 
 type StakeSummaryProps = {
   isLoading?: boolean
   assetId: AssetId
-<<<<<<< HEAD
   stakingAmountCryptoPrecision: string
 }
 
@@ -44,14 +35,6 @@ export const StakeSummary: React.FC<StakeSummaryProps> = ({
     select: data => formatDuration(Number(data as BigInt)),
   })
 
-=======
-}
-
-export const StakeSummary: React.FC<StakeSummaryProps> = ({ isLoading, assetId }) => {
-  const asset = useAppSelector(state => selectAssetById(state, assetId))
-  const translate = useTranslate()
-
->>>>>>> origin/develop
   const stakeAmountToolTip = useCallback(() => {
     return <Text color='text.subtle' translation='RFOX.tooltips.stakeAmount' />
   }, [])
@@ -81,33 +64,21 @@ export const StakeSummary: React.FC<StakeSummaryProps> = ({ isLoading, assetId }
         <Row.Label>{translate('RFOX.stakeAmount')}</Row.Label>
         <Row.Value>
           <Skeleton isLoaded={!isLoading}>
-<<<<<<< HEAD
             <Amount.Crypto value={stakingAmountCryptoPrecision} symbol={asset.symbol} />
-=======
-            <Amount.Crypto value='0' symbol={asset.symbol} />
->>>>>>> origin/develop
           </Skeleton>
         </Row.Value>
       </Row>
       <Row Tooltipbody={lockupPeriodToolTip}>
         <Row.Label>{translate('RFOX.lockupPeriod')}</Row.Label>
         <Row.Value>
-<<<<<<< HEAD
           <Skeleton isLoaded={isCooldownPeriodSuccess}>{cooldownPeriod}</Skeleton>
-=======
-          <Skeleton isLoaded={!isLoading}>28 days</Skeleton>
->>>>>>> origin/develop
         </Row.Value>
       </Row>
       <Row Tooltipbody={shareOfPoolToolTip}>
         <Row.Label>{translate('RFOX.shareOfPool')}</Row.Label>
         <Row.Value>
           <Skeleton isLoaded={!isLoading}>
-<<<<<<< HEAD
             <Amount.Percent value='TODO' />
-=======
-            <Amount.Percent value='0.02' />
->>>>>>> origin/develop
           </Skeleton>
         </Row.Value>
       </Row>

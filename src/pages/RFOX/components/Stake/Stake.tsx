8b733f4e0e3802b8b3f4e71fe927b52a463142ef
reallybeard <89934888+reallybeard@users.noreply.github.com>
2024-05-15 10:54:17 -0500

    rename folders

---
+++ src/pages/RFOX/components/Stake/Stake.tsx
@@ -0,0 +1,83 @@
+import { AnimatePresence } from 'framer-motion'
+import React, { lazy, Suspense, useCallback } from 'react'
+import { MemoryRouter, Route, Switch, useLocation } from 'react-router'
+import { makeSuspenseful } from 'utils/makeSuspenseful'
+
+import type { StakeRouteProps } from './types'
+import { StakeRoutePaths } from './types'
+
+const suspenseFallback = <div>Loading...</div>
+
+const StakeInput = makeSuspenseful(
+  lazy(() =>
+    import('./StakeInput').then(({ StakeInput }) => ({
+      default: StakeInput,
+    })),
+  ),
+)
+
+const StakeConfirm = makeSuspenseful(
+  lazy(() =>
+    import('./StakeConfirm').then(({ StakeConfirm }) => ({
+      default: StakeConfirm,
+    })),
+  ),
+)
+
+const StakeStatus = makeSuspenseful(
+  lazy(() =>
+    import('./StakeStatus').then(({ StakeStatus }) => ({
+      default: StakeStatus,
+    })),
+  ),
+)
+
+const StakeEntries = [StakeRoutePaths.Input, StakeRoutePaths.Confirm, StakeRoutePaths.Status]
+
+export const Stake: React.FC<StakeRouteProps> = ({ headerComponent }) => {
+  return (
+    <MemoryRouter initialEntries={StakeEntries} initialIndex={0}>
+      <StakeRoutes headerComponent={headerComponent} />
+    </MemoryRouter>
+  )
+}
+
+export const StakeRoutes: React.FC<StakeRouteProps> = ({ headerComponent }) => {
+  const location = useLocation()
+
+  const renderStakeInput = useCallback(() => {
+    return <StakeInput headerComponent={headerComponent} />
+  }, [headerComponent])
+
+  const renderStakeConfirm = useCallback(() => {
+    return <StakeConfirm headerComponent={headerComponent} />
+  }, [headerComponent])
+
+  const renderStakeStatus = useCallback(() => {
+    return <StakeStatus headerComponent={headerComponent} />
+  }, [headerComponent])
+
+  return (
+    <AnimatePresence mode='wait' initial={false}>
+      <Switch location={location}>
+        <Suspense fallback={suspenseFallback}>
+          <Route
+            key={StakeRoutePaths.Input}
+            path={StakeRoutePaths.Input}
+            render={renderStakeInput}
+          />
+          <Route
+            key={StakeRoutePaths.Confirm}
+            path={StakeRoutePaths.Confirm}
+            render={renderStakeConfirm}
+          />
+          <Route
+            key={StakeRoutePaths.Status}
+            path={StakeRoutePaths.Status}
+            render={renderStakeStatus}
+          />
+        </Suspense>
+      </Switch>
+    </AnimatePresence>
+  )
+}

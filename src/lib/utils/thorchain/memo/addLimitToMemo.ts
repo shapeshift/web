57645ea765acdc3a6794023e263d8da8e653ce47
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-05-16 09:07:18 +0200

    feat: multiline jsdoc rug

--- src/lib/utils/thorchain/memo/addLimitToMemo.ts
+++ src/lib/utils/thorchain/memo/addLimitToMemo.ts
@@ -2,15 +2,21 @@ import { assertAndProcessMemo } from './assertAndProcessMemo'
 import { MEMO_PART_DELIMITER } from './constants'
 
 type AddLimitToMemoArgs = {
-  // A THORChain memo, with or without a limit/MinOut
+  /**
+   * A THORChain memo, with or without a limit/MinOut.
+   */
   memo: string
-  // The limit/minout to be added to be added to the memo. Must be in THOR base unit, or problems.
+  /**
+   * The limit/minout to be added to the memo. Must be in THOR base unit, or problems.
+   */
   limit: string
 }
 
-// A simple util that adds a limit to a THORChain memo, if the memo doesn't already have one.
-// This is made to be composable, do one thing, and do it well.
-// If you need to handle slippage concerns, this should be done beforehand, with addLimitToMemo/assertAndProcessMemo being the final steps.
+/**
+ * A simple util that adds a limit to a THORChain memo, if the memo doesn't already have one.
+ * This is made to be composable, do one thing, and do it well.
+ * If you need to handle slippage concerns, this should be done beforehand, with addLimitToMemo/assertAndProcessMemo being the final steps.
+ */
 export const addLimitToMemo = ({ memo, limit }: AddLimitToMemoArgs) => {
   const memoParts = memo.split(MEMO_PART_DELIMITER)
   const [action] = memoParts

8f3d830fd63d28d82c34aa35e68c6b783d9a66ac
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-05-17 14:35:49 +0200

    feat: cleanup useless mutation

--- src/pages/RFOX/components/Stake/StakeConfirm.tsx
+++ src/pages/RFOX/components/Stake/StakeConfirm.tsx
@@ -330,47 +330,31 @@ export const StakeConfirm: React.FC<StakeConfirmProps & StakeRouteProps> = ({
     )
   }, [confirmedQuote.stakingAssetAccountId, stakeTxid, stakingAssetAccountAddress])
 
-  const {
-    mutateAsync: sendStakeTx,
-    isPending: isStakeMutationPending,
-    isSuccess: isStakeMutationSuccess,
-  } = useMutation({
-    mutationFn: async () => {
-      if (!wallet || stakingAssetAccountNumber === undefined || !stakingAsset) return
+  const handleStake = useCallback(async () => {
+    if (!wallet || stakingAssetAccountNumber === undefined || !stakingAsset) return
 
-      const adapter = assertGetEvmChainAdapter(stakingAsset.chainId)
+    const adapter = assertGetEvmChainAdapter(stakingAsset.chainId)
 
-      const buildCustomTxInput = await createBuildCustomTxInput({
-        accountNumber: stakingAssetAccountNumber,
-        adapter,
-        data: stakeCallData,
-        value: '0',
-        to: RFOX_PROXY_CONTRACT_ADDRESS,
-        wallet,
-      })
+    const buildCustomTxInput = await createBuildCustomTxInput({
+      accountNumber: stakingAssetAccountNumber,
+      adapter,
+      data: stakeCallData,
+      value: '0',
+      to: RFOX_PROXY_CONTRACT_ADDRESS,
+      wallet,
+    })
 
-      const txId = await buildAndBroadcast({
-        adapter,
-        buildCustomTxInput,
-        receiverAddress: CONTRACT_INTERACTION, // no receiver for this contract call
-      })
+    const txId = await buildAndBroadcast({
+      adapter,
+      buildCustomTxInput,
+      receiverAddress: CONTRACT_INTERACTION, // no receiver for this contract call
+    })
 
-      return txId
-    },
-    onSuccess: (txId: string | undefined) => {
-      if (!txId) return
-
-      setStakeTxid(txId)
-    },
-  })
-
-  const handleStake = useCallback(() => sendStakeTx(undefined), [sendStakeTx])
+    setStakeTxid(txId)
+  }, [setStakeTxid, stakeCallData, stakingAsset, stakingAssetAccountNumber, wallet])
 
   const stakeTx = useAppSelector(gs => selectTxById(gs, serializedStakeTxIndex))
-  const isStakeTxPending = useMemo(
-    () => isStakeMutationPending || (isStakeMutationSuccess && !stakeTx),
-    [isStakeMutationPending, isStakeMutationSuccess, stakeTx],
-  )
+  const isStakeTxPending = useMemo(() => Boolean(stakeTxid && !stakeTx), [stakeTx, stakeTxid])
 
   const handleGoBack = useCallback(() => {
     history.push(StakeRoutePaths.Input)

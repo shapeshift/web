17d5751437d0588c93c6531688992d94f2fb5c74
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2024-05-20 11:31:29 +1000

    chore: cleanup

--- src/components/ManageAccountsDrawer/components/ImportAccounts.tsx
+++ src/components/ManageAccountsDrawer/components/ImportAccounts.tsx
@@ -266,7 +266,6 @@ export const ImportAccounts = ({ chainId, onClose }: ImportAccountsProps) => {
       Array.from(toggledAccountIds).map(async accountId => {
         const isEnabled = selectIsAccountIdEnabled(store.getState(), { accountId })
         if (isEnabled) {
-          console.log(`Account ${accountId} is already enabled`)
           return
         }
         await dispatch(

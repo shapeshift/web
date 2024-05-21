6a876b39c395d03b4beab577f39245fbdce5d745
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-05-22 00:02:34 +0200

    feat: invalidateQueries

--- src/pages/RFOX/components/Unstake/UnstakeConfirm.tsx
+++ src/pages/RFOX/components/Unstake/UnstakeConfirm.tsx
@@ -12,7 +12,7 @@ import {
 } from '@chakra-ui/react'
 import { fromAccountId, fromAssetId } from '@shapeshiftoss/caip'
 import { CONTRACT_INTERACTION } from '@shapeshiftoss/chain-adapters'
-import { useMutation, useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { erc20ABI } from 'contracts/abis/ERC20ABI'
 import { foxStakingV1Abi } from 'contracts/abis/FoxStakingV1'
 import { RFOX_PROXY_CONTRACT_ADDRESS } from 'contracts/constants'
@@ -64,6 +64,7 @@ export const UnstakeConfirm: React.FC<UnstakeRouteProps & UnstakeConfirmProps> =
   unstakeTxid,
   setUnstakeTxid,
 }) => {
+  const queryClient = useQueryClient()
   const wallet = useWallet().state.wallet
   const history = useHistory()
   const translate = useTranslate()
@@ -215,21 +216,10 @@ export const UnstakeConfirm: React.FC<UnstakeRouteProps & UnstakeConfirmProps> =
     },
   })
 
-  const handleSubmit = useCallback(async () => {
-    await sendUnstakeTx(undefined)
-
-    history.push(UnstakeRoutePaths.Status)
-  }, [history, sendUnstakeTx])
-
-  const unstakeTx = useAppSelector(gs => selectTxById(gs, serializedUnstakeTxIndex))
-  const isUnstakeTxPending = useMemo(
-    () => isUnstakeMutationPending || (isUnstakeMutationSuccess && !unstakeTx),
-    [isUnstakeMutationPending, isUnstakeMutationSuccess, unstakeTx],
-  )
-
   const {
     data: userStakingBalanceOfCryptoBaseUnit,
     isSuccess: isUserStakingBalanceOfCryptoBaseUnitSuccess,
+    queryKey: userStakingBalanceOfCryptoBaseUnitQueryKey,
   } = useReadContract({
     abi: foxStakingV1Abi,
     address: RFOX_PROXY_CONTRACT_ADDRESS,
@@ -245,6 +235,7 @@ export const UnstakeConfirm: React.FC<UnstakeRouteProps & UnstakeConfirmProps> =
   const {
     data: newContractBalanceOfCryptoBaseUnit,
     isSuccess: isNewContractBalanceOfCryptoBaseUnitSuccess,
+    queryKey: newContractBalanceOfCryptoBaseUnitQueryKey,
   } = useReadContract({
     abi: erc20ABI,
     address: getAddress(fromAssetId(confirmedQuote.stakingAssetId).assetReference),
@@ -267,6 +258,27 @@ export const UnstakeConfirm: React.FC<UnstakeRouteProps & UnstakeConfirmProps> =
       newContractBalanceOfCryptoBaseUnit,
       userStakingBalanceOfCryptoBaseUnit,
     ],
+  )
+
+  const handleSubmit = useCallback(async () => {
+    await sendUnstakeTx(undefined)
+
+    await queryClient.invalidateQueries({ queryKey: userStakingBalanceOfCryptoBaseUnitQueryKey })
+    await queryClient.invalidateQueries({ queryKey: newContractBalanceOfCryptoBaseUnitQueryKey })
+
+    history.push(UnstakeRoutePaths.Status)
+  }, [
+    history,
+    newContractBalanceOfCryptoBaseUnitQueryKey,
+    queryClient,
+    sendUnstakeTx,
+    userStakingBalanceOfCryptoBaseUnitQueryKey,
+  ])
+
+  const unstakeTx = useAppSelector(gs => selectTxById(gs, serializedUnstakeTxIndex))
+  const isUnstakeTxPending = useMemo(
+    () => isUnstakeMutationPending || (isUnstakeMutationSuccess && !unstakeTx),
+    [isUnstakeMutationPending, isUnstakeMutationSuccess, unstakeTx],
   )
 
   return (

b84d97dc1192e90152e5b537b04b47588e0c58d9
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2024-05-22 08:17:02 +1000

    chore: translations & address progress

--- src/pages/RFOX/components/ChangeAddress/ChangeAddressInput.tsx
+++ src/pages/RFOX/components/ChangeAddress/ChangeAddressInput.tsx
@@ -1,13 +1,8 @@
-import { Button, CardFooter, Collapse, Stack } from '@chakra-ui/react'
+import { Button, CardFooter, Stack } from '@chakra-ui/react'
 import { foxAssetId } from '@shapeshiftoss/caip'
 import { useCallback, useMemo, useState } from 'react'
 import { useTranslate } from 'react-polyglot'
 import { useHistory } from 'react-router'
-import { Amount } from 'components/Amount/Amount'
-import { TradeAssetSelect } from 'components/AssetSelection/AssetSelection'
-import { FormDivider } from 'components/FormDivider'
-import { TradeAssetInput } from 'components/MultiHopTrade/components/TradeAssetInput'
-import { Row } from 'components/Row/Row'
 import { SlideTransition } from 'components/SlideTransition'
 import { WarningAcknowledgement } from 'components/WarningAcknowledgement/WarningAcknowledgement'
 import { selectAssetById } from 'state/slices/selectors'
@@ -16,35 +11,14 @@ import { useAppSelector } from 'state/store'
 import { AddressSelection } from '../AddressSelection'
 import { ChangeAddressRoutePaths, type ChangeAddressRouteProps } from './types'
 
-const formControlProps = {
-  borderRadius: 0,
-  background: 'transparent',
-  borderWidth: 0,
-  paddingBottom: 0,
-  paddingTop: 0,
-}
-
 export const ChangeAddressInput: React.FC<ChangeAddressRouteProps> = ({ headerComponent }) => {
   const translate = useTranslate()
   const history = useHistory()
   const asset = useAppSelector(state => selectAssetById(state, foxAssetId))
   const [showWarning, setShowWarning] = useState(false)
-  const percentOptions = useMemo(() => [1], [])
-  const [cryptoAmount, setCryptoAmount] = useState('')
-  const [fiatAmount, setFiatAmount] = useState('')
+  const [newAddress, setNewAddress] = useState<string | null>()
 
-  const handleAccountIdChange = useCallback(() => {}, [])
-
-  // @TODO: Need to add a fiat toggle for the input field
-  const hasEnteredValue = useMemo(() => !!fiatAmount || !!cryptoAmount, [cryptoAmount, fiatAmount])
-
-  const handleChange = useCallback((value: string, isFiat?: boolean) => {
-    if (isFiat) {
-      setFiatAmount(value)
-    } else {
-      setCryptoAmount(value)
-    }
-  }, [])
+  const hasEnteredAddress = useMemo(() => !!newAddress, [newAddress])
 
   const handleWarning = useCallback(() => {
     setShowWarning(true)
@@ -53,10 +27,6 @@ export const ChangeAddressInput: React.FC<ChangeAddressRouteProps> = ({ headerCo
   const handleSubmit = useCallback(() => {
     history.push(ChangeAddressRoutePaths.Confirm)
   }, [history])
-
-  const assetSelectComponent = useMemo(() => {
-    return <TradeAssetSelect assetId={asset?.assetId} isReadOnly onlyConnectedChains={true} />
-  }, [asset?.assetId])
 
   if (!asset) return null
 
@@ -72,47 +42,7 @@ export const ChangeAddressInput: React.FC<ChangeAddressRouteProps> = ({ headerCo
       >
         <Stack>
           {headerComponent}
-          <TradeAssetInput
-            assetId={asset?.assetId}
-            assetSymbol={asset?.symbol ?? ''}
-            assetIcon={asset?.icon ?? ''}
-            percentOptions={percentOptions}
-            onAccountIdChange={handleAccountIdChange}
-            formControlProps={formControlProps}
-            layout='inline'
-            label={translate('transactionRow.amount')}
-            labelPostFix={assetSelectComponent}
-            isSendMaxDisabled={false}
-            onChange={handleChange}
-            cryptoAmount={cryptoAmount}
-            fiatAmount={fiatAmount}
-          />
-          <FormDivider />
           <AddressSelection />
-          <Collapse in={hasEnteredValue}>
-            <CardFooter
-              borderTopWidth={1}
-              borderColor='border.subtle'
-              flexDir='column'
-              gap={4}
-              px={6}
-              py={4}
-              bg='background.surface.raised.accent'
-            >
-              <Row fontSize='sm' fontWeight='medium'>
-                <Row.Label>{translate('common.gasFee')}</Row.Label>
-                <Row.Value>
-                  <Amount.Fiat value='10' />
-                </Row.Value>
-              </Row>
-              <Row fontSize='sm' fontWeight='medium'>
-                <Row.Label>{translate('common.fees')}</Row.Label>
-                <Row.Value>
-                  <Amount.Fiat value='0.0' />
-                </Row.Value>
-              </Row>
-            </CardFooter>
-          </Collapse>
         </Stack>
         <CardFooter
           borderTopWidth={1}
@@ -128,7 +58,7 @@ export const ChangeAddressInput: React.FC<ChangeAddressRouteProps> = ({ headerCo
             mx={-2}
             onClick={handleWarning}
             colorScheme='blue'
-            isDisabled={!hasEnteredValue}
+            isDisabled={!hasEnteredAddress}
           >
             {translate('RFOX.stake')}
           </Button>

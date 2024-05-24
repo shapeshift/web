41c073a5cc4ad800ee415b1c63f97385e5981949
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-05-24 15:59:06 +0200

    feat: add bridge icon

--- src/components/MultiHopTrade/components/TradeInput/components/SwapperIcon/SwapperIcon.tsx
+++ src/components/MultiHopTrade/components/TradeInput/components/SwapperIcon/SwapperIcon.tsx
@@ -6,6 +6,7 @@ import { assertUnreachable } from 'lib/utils'
 
 import ZrxIcon from './0x-icon.png'
 import OneInchIcon from './1inch-icon.png'
+import ArbitrumBridgeIcon from './arbitrum-bridge-icon.png'
 import CowIcon from './cow-icon.png'
 import LiFiIcon from './lifi-icon.png'
 import THORChainIcon from './thorchain-icon.png'
@@ -30,6 +31,7 @@ export const SwapperIcon = ({
       case SwapperName.OneInch:
         return OneInchIcon
       case SwapperName.ArbitrumBridge:
+        return ArbitrumBridgeIcon
       case SwapperName.Test:
         return ''
       default:

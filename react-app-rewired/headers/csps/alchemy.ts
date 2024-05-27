173a3ef6fe1b92ca9352cf9a9e7f18733ae26add
kevin <35275952+kaladinlight@users.noreply.github.com>
2024-05-26 19:58:51 -0600

    feat: add nft support for base network (#6993)

--- react-app-rewired/headers/csps/alchemy.ts
+++ react-app-rewired/headers/csps/alchemy.ts
@@ -3,13 +3,15 @@ import type { Csp } from '../types'
 export const csp: Csp = {
   'connect-src': [
     // Alchemy SDK NFT resolution on Polygon mainnet
-    'https://polygon-mainnet.g.alchemy.com/nft/v2/',
+    'https://polygon-mainnet.g.alchemy.com/nft/v3/',
     // Alchemy SDK NFT resolution on Ethereum mainnet
-    'https://eth-mainnet.g.alchemy.com/nft/v2/',
+    'https://eth-mainnet.g.alchemy.com/nft/v3/',
     // Alchemy SDK NFT resolution on Optimism mainnet
-    'https://opt-mainnet.g.alchemy.com/nft/v2/',
+    'https://opt-mainnet.g.alchemy.com/nft/v3/',
     // Alchemy SDK NFT resolution on Arbitrum mainnet
-    'https://arb-mainnet.g.alchemy.com/nft/v2/',
+    'https://arb-mainnet.g.alchemy.com/nft/v3/',
+    // Alchemy SDK NFT resolution on Base mainnet
+    'https://base-mainnet.g.alchemy.com/nft/v3/',
     // Mercle IPNS gateway for NFT resolution
     'https://backend.mercle.xyz/ipns/',
   ],

542310e1a196dbc8d7818243266f4dcf42712164
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-05-29 15:48:36 +0200

    feat: formatSecondsToDuration handle sub-hour durations

---
+++ src/lib/utils/time.test.ts
@@ -0,0 +1,38 @@
+import { describe, expect, it } from 'vitest'
+
+import { formatSecondsToDuration } from './time'
+
+describe('formatSecondsToDuration', () => {
+  it('should format seconds correctly when less than a minute', () => {
+    expect(formatSecondsToDuration(0)).toBe('0 seconds')
+    expect(formatSecondsToDuration(1)).toBe('1 second')
+    expect(formatSecondsToDuration(59)).toBe('59 seconds')
+  })
+
+  it('should format seconds into minutes when less than an hour', () => {
+    expect(formatSecondsToDuration(60)).toBe('1 minute')
+    expect(formatSecondsToDuration(3599)).toBe('59 minutes')
+  })
+
+  it('should format seconds into hours when less than a day', () => {
+    expect(formatSecondsToDuration(3600)).toBe('1 hour')
+    expect(formatSecondsToDuration(7200)).toBe('2 hours')
+    expect(formatSecondsToDuration(86399)).toBe('23 hours')
+  })
+
+  it('should format seconds into days when less than a month', () => {
+    expect(formatSecondsToDuration(86400)).toBe('1 day')
+    expect(formatSecondsToDuration(172800)).toBe('2 days')
+    expect(formatSecondsToDuration(2591999)).toBe('29 days')
+    expect(formatSecondsToDuration(2592000)).toBe('30 days')
+  })
+
+  it('should format seconds into months when more than or equal to a month', () => {
+    expect(formatSecondsToDuration(5184000)).toBe('2 months')
+    expect(formatSecondsToDuration(7776000)).toBe('3 months')
+  })
+
+  it('should handle 0-value seconds correctly', () => {
+    expect(formatSecondsToDuration(0)).toBe('0 seconds')
+  })
+})
--- src/lib/utils/time.ts
+++ src/lib/utils/time.ts
@@ -7,12 +7,20 @@ export const formatSecondsToDuration = (seconds: number) => {
   const hours = duration.asHours()
   const days = duration.asDays()
   const months = duration.asMonths()
+  const minutes = duration.asMinutes()
 
+  if (seconds < 60) {
+    return `${Math.floor(seconds)} second${Math.floor(seconds) !== 1 ? 's' : ''}`
+  }
+
+  if (minutes < 60) {
+    return `${Math.floor(minutes)} minute${Math.floor(minutes) !== 1 ? 's' : ''}`
+  }
   if (hours < 24) {
     return `${Math.floor(hours)} hour${Math.floor(hours) !== 1 ? 's' : ''}`
-  } else if (days < 31) {
-    return `${Math.floor(days)} day${Math.floor(days) !== 1 ? 's' : ''}`
-  } else {
-    return `${Math.floor(months)} month${Math.floor(months) !== 1 ? 's' : ''}`
   }
+  if (days < 31) {
+    return `${Math.floor(days)} day${Math.floor(days) !== 1 ? 's' : ''}`
+  }
+  return `${Math.floor(months)} month${Math.floor(months) !== 1 ? 's' : ''}`
 }

70877f3dcbec29889df039a48ca2de35f86f9cf8
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-05-30 20:35:56 +0200

    feat: use dayjs

--- src/lib/utils/time.ts
+++ src/lib/utils/time.ts
@@ -1,26 +1,17 @@
+import { LanguageTypeEnum } from 'constants/LanguageTypeEnum'
 import dayjs from 'dayjs'
-import durationPlugin from 'dayjs/plugin/duration'
+import duration from 'dayjs/plugin/duration'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import { selectSelectedLocale } from 'state/slices/selectors'
+import { store } from 'state/store'
+
+dayjs.extend(duration)
+dayjs.extend(relativeTime)
 
 export const formatSecondsToDuration = (seconds: number) => {
-  dayjs.extend(durationPlugin)
-  const duration = dayjs.duration(seconds, 'seconds')
-  const hours = duration.asHours()
-  const days = duration.asDays()
-  const months = duration.asMonths()
-  const minutes = duration.asMinutes()
+  const selectedLocale = selectSelectedLocale(store.getState())
+  const locale = selectedLocale in LanguageTypeEnum ? selectedLocale : 'en'
+  require(`dayjs/locale/${locale}.js`)
 
-  if (seconds < 60) {
-    return `${Math.floor(seconds)} second${Math.floor(seconds) !== 1 ? 's' : ''}`
-  }
-
-  if (minutes < 60) {
-    return `${Math.floor(minutes)} minute${Math.floor(minutes) !== 1 ? 's' : ''}`
-  }
-  if (hours < 24) {
-    return `${Math.floor(hours)} hour${Math.floor(hours) !== 1 ? 's' : ''}`
-  }
-  if (days < 30) {
-    return `${Math.floor(days)} day${Math.floor(days) !== 1 ? 's' : ''}`
-  }
-  return `${Math.floor(months)} month${Math.floor(months) !== 1 ? 's' : ''}`
+  return dayjs.duration(seconds, 'seconds').locale(locale).humanize()
 }

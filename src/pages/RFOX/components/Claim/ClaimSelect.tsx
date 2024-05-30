f6e58bb948e801b9de63d11969490d5219d50db7
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2024-05-31 09:54:45 +1000

    feat: add cooldown logic

--- src/pages/RFOX/components/Claim/ClaimSelect.tsx
+++ src/pages/RFOX/components/Claim/ClaimSelect.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, CardBody, Center, Flex, Skeleton, Stack } from '@chakra-ui/react'
+import { Box, Button, CardBody, Center, Flex, Skeleton, Stack, Tooltip } from '@chakra-ui/react'
 import {
   type AssetId,
   foxAssetId,
@@ -9,6 +9,7 @@ import { bnOrZero } from '@shapeshiftoss/chain-adapters'
 import { TransferType } from '@shapeshiftoss/unchained-client'
 import { foxStakingV1Abi } from 'contracts/abis/FoxStakingV1'
 import { RFOX_PROXY_CONTRACT_ADDRESS } from 'contracts/constants'
+import { formatDistanceToNow } from 'date-fns'
 import { type FC, useCallback, useMemo } from 'react'
 import { useTranslate } from 'react-polyglot'
 import { useHistory } from 'react-router'
@@ -22,7 +23,7 @@ import { AssetIconWithBadge } from 'components/AssetIconWithBadge'
 import { SlideTransition } from 'components/SlideTransition'
 import { RawText, Text } from 'components/Text'
 import { TransactionTypeIcon } from 'components/TransactionHistory/TransactionTypeIcon'
-import { toBaseUnit } from 'lib/math'
+import { fromBaseUnit, toBaseUnit } from 'lib/math'
 import { TabIndex } from 'pages/RFOX/RFOX'
 import { selectAssetById, selectFirstAccountIdByChainId } from 'state/slices/selectors'
 import { useAppSelector } from 'state/store'
@@ -30,11 +31,17 @@ import { useAppSelector } from 'state/store'
 import type { RfoxClaimQuote } from './types'
 import { ClaimRoutePaths, type ClaimRouteProps } from './types'
 
+enum ClaimStatus {
+  Available = 'Available',
+  CoolingDown = 'Cooling down',
+}
+
 type ClaimRowProps = {
   stakingAssetId: AssetId
   amountCryptoPrecision: string
-  status: string
+  status: ClaimStatus
   setConfirmedQuote: (quote: RfoxClaimQuote) => void
+  cooldownPeriodHuman: string
 }
 
 const hoverProps = { bg: 'gray.700' }
@@ -44,6 +51,7 @@ const ClaimRow: FC<ClaimRowProps> = ({
   amountCryptoPrecision,
   status,
   setConfirmedQuote,
+  cooldownPeriodHuman,
 }) => {
   const translate = useTranslate()
   const history = useHistory()
@@ -75,38 +83,59 @@ const ClaimRow: FC<ClaimRowProps> = ({
   }, [claimQuote, history, setConfirmedQuote])
 
   return (
-    <Flex
-      as={Button}
-      align='center'
-      variant='unstyled'
-      p={8}
-      borderRadius='md'
-      width='100%'
-      onClick={handleClick}
-      _hover={hoverProps}
+    <Tooltip
+      label={translate(
+        status === ClaimStatus.Available
+          ? 'RFOX.tooltips.cooldownComplete'
+          : 'RFOX.tooltips.unstakePendingCooldown',
+        { cooldownPeriodHuman },
+      )}
     >
-      <Box mr={4}>
-        <AssetIconWithBadge assetId={foxAssetId}>
-          <TransactionTypeIcon type={TransferType.Receive} />
-        </AssetIconWithBadge>
-      </Box>
-      <Box mr={4}>
-        <RawText fontSize='sm' color='gray.400' align={'start'}>
-          {translate('RFOX.unstakeFrom', { assetSymbol: stakingAssetSymbol })}
-        </RawText>
-        <RawText fontSize='xl' fontWeight='bold' color='white' align={'start'}>
-          {stakingAssetSymbol}
-        </RawText>
-      </Box>
-      <Box flex='1' alignItems={'end'}>
-        <RawText fontSize='sm' fontWeight='bold' color='green.300' align={'end'}>
-          {status}
-        </RawText>
-        <RawText fontSize='xl' fontWeight='bold' color='white' align={'end'}>
-          <Amount.Crypto value={amountCryptoPrecision} symbol={stakingAssetSymbol ?? ''} />
-        </RawText>
-      </Box>
-    </Flex>
+      <Flex
+        as={Button}
+        justifyContent={'space-between'}
+        mt={2}
+        align='center'
+        variant='unstyled'
+        p={8}
+        borderRadius='md'
+        width='100%'
+        onClick={handleClick}
+        isDisabled={status !== ClaimStatus.Available}
+        _hover={hoverProps}
+      >
+        <Flex>
+          <Box mr={4}>
+            <AssetIconWithBadge assetId={foxAssetId}>
+              <TransactionTypeIcon type={TransferType.Receive} />
+            </AssetIconWithBadge>
+          </Box>
+          <Box mr={4}>
+            <RawText fontSize='sm' color='gray.400' align={'start'}>
+              {translate('RFOX.unstakeFrom', { assetSymbol: stakingAssetSymbol })}
+            </RawText>
+            <RawText fontSize='xl' fontWeight='bold' color='white' align={'start'}>
+              {stakingAssetSymbol}
+            </RawText>
+          </Box>
+        </Flex>
+        <Flex justifyContent={'flex-end'}>
+          <Box flexGrow={1} alignItems={'end'}>
+            <RawText
+              fontSize='sm'
+              fontWeight='bold'
+              color={status === ClaimStatus.Available ? 'green.300' : 'yellow.300'}
+              align={'end'}
+            >
+              {status}
+            </RawText>
+            <RawText fontSize='xl' fontWeight='bold' color='white' align={'end'}>
+              <Amount.Crypto value={amountCryptoPrecision} symbol={stakingAssetSymbol ?? ''} />
+            </RawText>
+          </Box>
+        </Flex>
+      </Flex>
+    </Tooltip>
   )
 }
 
@@ -189,6 +218,7 @@ export const ClaimSelect: FC<ClaimSelectProps & ClaimRouteProps> = ({
           stakingAssetAccountAddress
             ? getAddress(stakingAssetAccountAddress)
             : ('' as Address, BigInt(index)),
+          index,
         ],
         chainId: arbitrum.id,
       }) as const,
@@ -211,17 +241,28 @@ export const ClaimSelect: FC<ClaimSelectProps & ClaimRouteProps> = ({
       <CardBody py={12}>
         <Skeleton isLoaded={!isUnstakingRequestCountLoading && !isUnstakingRequestLoading}>
           <Flex flexDir='column' gap={4}>
-            {hasClaims ? (
+            {hasClaims && isUnstakingRequestSuccess ? (
               unstakingRequestResponse?.map(unstakingRequest => {
-                const amountCryptoPrecision = unstakingRequest.unstakingBalance
-                const status = 'Available'
+                const amountCryptoPrecision = fromBaseUnit(
+                  unstakingRequest.unstakingBalance.toString() ?? '',
+                  stakingAsset?.precision ?? 0,
+                )
+                const currentTimestampMs: number = Date.now()
+                const unstakingTimestampMs: number = Number(unstakingRequest.cooldownExpiry) * 1000
+                const isAvailable = currentTimestampMs >= unstakingTimestampMs
+                const cooldownDeltaMs = unstakingTimestampMs - currentTimestampMs
+                const cooldownPeriodHuman = formatDistanceToNow(Date.now() + cooldownDeltaMs, {
+                  addSuffix: true,
+                })
+                const status = isAvailable ? ClaimStatus.Available : ClaimStatus.CoolingDown
                 return (
                   <ClaimRow
                     key={unstakingRequest.cooldownExpiry.toString()}
                     stakingAssetId={foxAssetId}
-                    amountCryptoPrecision={amountCryptoPrecision.toString()}
+                    amountCryptoPrecision={amountCryptoPrecision?.toString() ?? ''}
                     status={status}
                     setConfirmedQuote={setConfirmedQuote}
+                    cooldownPeriodHuman={cooldownPeriodHuman}
                   />
                 )
               })

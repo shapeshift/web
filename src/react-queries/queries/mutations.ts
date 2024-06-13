ca498fdb980427df9ee14f452b7b074cec40ba5c
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-06-13 20:04:45 +0200

    feat: better home for approve

--- src/react-queries/queries/mutations.ts
+++ src/react-queries/queries/mutations.ts
@@ -1,14 +1,7 @@
 import { createMutationKeys } from '@lukemorales/query-key-factory'
-import { type AssetId, fromAssetId } from '@shapeshiftoss/caip'
-import { CONTRACT_INTERACTION } from '@shapeshiftoss/chain-adapters'
+import { type AssetId } from '@shapeshiftoss/caip'
 import type { HDWallet } from '@shapeshiftoss/hdwallet-core'
-import { supportsETH } from '@shapeshiftoss/hdwallet-core'
-import {
-  assertGetEvmChainAdapter,
-  buildAndBroadcast,
-  getApproveContractData,
-  getFees,
-} from 'lib/utils/evm'
+import { approve } from 'lib/utils/evm/approve'
 
 type ApproveInput = {
   assetId: AssetId
@@ -19,53 +12,8 @@ type ApproveInput = {
 }
 export type MaybeApproveInput = Partial<ApproveInput>
 
-type ApproveInputWithWallet = ApproveInput & { wallet: HDWallet }
+export type ApproveInputWithWallet = ApproveInput & { wallet: HDWallet }
 type MaybeApproveInputWithWallet = Partial<ApproveInputWithWallet>
-
-export const approve = async ({
-  assetId,
-  spender,
-  amountCryptoBaseUnit,
-  wallet,
-  from,
-  accountNumber,
-}: ApproveInputWithWallet) => {
-  const { assetReference, chainId } = fromAssetId(assetId)
-  const approvalCalldata = getApproveContractData({
-    approvalAmountCryptoBaseUnit: amountCryptoBaseUnit,
-    spender,
-    to: assetReference,
-    chainId,
-  })
-
-  const adapter = assertGetEvmChainAdapter(chainId)
-
-  const { networkFeeCryptoBaseUnit, ...fees } = await getFees({
-    adapter,
-    to: assetReference,
-    value: '0',
-    data: approvalCalldata,
-    from,
-    supportsEIP1559: supportsETH(wallet) && (await wallet.ethSupportsEIP1559()),
-  })
-
-  const buildCustomTxInput = {
-    accountNumber,
-    data: approvalCalldata,
-    to: assetReference,
-    value: '0',
-    wallet,
-    ...fees,
-  }
-
-  const txHash = await buildAndBroadcast({
-    adapter,
-    buildCustomTxInput,
-    receiverAddress: CONTRACT_INTERACTION,
-  })
-
-  return txHash
-}
 
 export const mutations = createMutationKeys('mutations', {
   approve: ({

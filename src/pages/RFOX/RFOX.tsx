c52322ae1ceac2c2a4d97f9d9f83548843af56c8
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-06-17 18:36:45 +0200

    feat: rfox dashboard wire-up staked balance (#7142)

--- src/pages/RFOX/RFOX.tsx
+++ src/pages/RFOX/RFOX.tsx
@@ -1,8 +1,11 @@
 import type { StackDirection } from '@chakra-ui/react'
 import { Heading, Stack } from '@chakra-ui/react'
+import { foxOnArbitrumOneAssetId } from '@shapeshiftoss/caip'
 import { useTranslate } from 'react-polyglot'
 import { Main } from 'components/Layout/Main'
 import { useFeatureFlag } from 'hooks/useFeatureFlag/useFeatureFlag'
+import { selectAssetById, selectFirstAccountIdByChainId } from 'state/slices/selectors'
+import { useAppSelector } from 'state/store'
 
 import { Faq } from './components/Faq/Faq'
 import { Overview } from './components/Overview/Overview'
@@ -13,9 +16,19 @@ const direction: StackDirection = { base: 'column-reverse', xl: 'row' }
 const maxWidth = { base: 'full', lg: 'full', xl: 'sm' }
 const paddingVerticalResponsiveProps = { base: 8, md: 16 }
 
+const stakingAssetId = foxOnArbitrumOneAssetId
+
 export const RFOX: React.FC = () => {
   const translate = useTranslate()
   const isRFOXDashboardEnabled = useFeatureFlag('RFOXDashboard')
+
+  const stakingAsset = useAppSelector(state => selectAssetById(state, stakingAssetId))
+  // TODO(gomes): make this programmatic when we implement multi-account
+  const stakingAssetAccountId = useAppSelector(state =>
+    selectFirstAccountIdByChainId(state, stakingAsset?.chainId ?? ''),
+  )
+
+  if (!stakingAsset) return null
 
   return (
     <Main py={paddingVerticalResponsiveProps} px={4}>
@@ -23,7 +36,12 @@ export const RFOX: React.FC = () => {
 
       <Stack alignItems='flex-start' spacing={4} mx='auto' direction={direction}>
         <Stack spacing={4} flex='1 1 0%' width='full'>
-          {isRFOXDashboardEnabled && <Overview />}
+          {isRFOXDashboardEnabled && (
+            <Overview
+              stakingAssetId={stakingAssetId}
+              stakingAssetAccountId={stakingAssetAccountId}
+            />
+          )}
           {isRFOXDashboardEnabled && <RewardsAndClaims />}
           <Faq />
         </Stack>

279fe11843368c946d74d070701586f9eb4dc85b
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-06-18 00:46:28 +0200

    feat: rfox use faster cooldown contracts in develop

--- packages/unchained-client/src/evm/arbitrum/parser/index.ts
+++ packages/unchained-client/src/evm/arbitrum/parser/index.ts
@@ -9,7 +9,6 @@ import * as rfox from '../../parser/rfox'
 import * as zrx from '../../parser/zrx'
 
 export const ZRX_ARBITRUM_PROXY_CONTRACT = '0xDef1C0ded9bec7F1a1670819833240f027b25EfF'
-export const RFOX_PROXY_CONTRACT_ADDRESS = '0xaC2a4fD70BCD8Bab0662960455c363735f0e2b56'
 
 export class TransactionParser extends BaseTransactionParser<Tx> {
   constructor(args: BaseTransactionParserArgs) {
@@ -24,7 +23,7 @@ export class TransactionParser extends BaseTransactionParser<Tx> {
       new erc20.Parser({ chainId: this.chainId, provider: this.provider }),
       new zrx.Parser({ proxyContract: ZRX_ARBITRUM_PROXY_CONTRACT }),
       new rfox.Parser({
-        proxyContract: RFOX_PROXY_CONTRACT_ADDRESS,
+        proxyContract: process.env.REACT_APP_RFOX_PROXY_CONTRACT_ADDRESS ?? '',
         stakingAssetId: foxOnArbitrumOneAssetId,
       }),
     ])

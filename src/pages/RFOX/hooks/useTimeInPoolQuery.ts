82fdd63c9e277662eb9944b45b39f30a4fae1c49
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-06-21 12:17:21 +0200

    feat: askchyually make it work

--- src/pages/RFOX/hooks/useTimeInPoolQuery.ts
+++ src/pages/RFOX/hooks/useTimeInPoolQuery.ts
@@ -3,6 +3,7 @@ import { foxStakingV1Abi } from 'contracts/abis/FoxStakingV1'
 import { RFOX_PROXY_CONTRACT_ADDRESS } from 'contracts/constants'
 import dayjs from 'dayjs'
 import { useMemo } from 'react'
+import type { GetFilterLogsReturnType } from 'viem'
 import { getAbiItem, getAddress } from 'viem'
 import { arbitrum } from 'viem/chains'
 import { viemClientByNetworkId } from 'lib/viem-client'
@@ -15,10 +16,62 @@ type UseTimeInPoolProps<SelectData = bigint> = {
 }
 
 export const RFOX_CONTRACT_CREATION_BLOCK_NUMBER = 222913582n
+
+export const getTimeInPoolSeconds = async (
+  sortedLogs: GetFilterLogsReturnType<typeof foxStakingV1Abi, 'Stake' | 'Unstake'>,
+) => {
+  // If we don't have stake or unstake events, we know the user was never active in the pool
+  if (!sortedLogs.length) return 0n
+
+  let stakingBalance = 0n
+  let earliestNonZeroStakingBalanceBlockNumber = undefined
+
+  for (const log of sortedLogs) {
+    switch (log.eventName) {
+      case 'Stake': {
+        stakingBalance += log.args.amount ?? 0n
+        // Set the earliest non-zero block number if it's not already set
+        if (earliestNonZeroStakingBalanceBlockNumber === undefined) {
+          earliestNonZeroStakingBalanceBlockNumber = log.blockNumber
+        }
+        break
+      }
+      case 'Unstake': {
+        stakingBalance -= log.args.amount ?? 0n
+        // Reset the earliest non-zero block number if balance hits zero
+        if (stakingBalance === 0n) {
+          earliestNonZeroStakingBalanceBlockNumber = undefined
+        }
+        break
+      }
+      default:
+        break
+    }
+  }
+
+  // If the balance never reached zero, set the block number to the earliest staking event
+  if (stakingBalance > 0n && earliestNonZeroStakingBalanceBlockNumber === undefined) {
+    earliestNonZeroStakingBalanceBlockNumber = sortedLogs[0].blockNumber
+  }
+
+  // If the staking balance never got set, the user has never staked
+  if (!earliestNonZeroStakingBalanceBlockNumber) return 0n
+
+  // Get the block timestamp of the earliest non-zero staking balance
+  const { timestamp: earliestNonZeroStakingBalanceTimestamp } = await client.getBlock({
+    blockNumber: earliestNonZeroStakingBalanceBlockNumber,
+  })
+
+  const now = dayjs().unix()
+
+  // Return the time the account has most recently had a non-zero staking balance to now
+  return BigInt(now) - BigInt(earliestNonZeroStakingBalanceTimestamp)
+}
+
 /**
  * Calculates the time the account has most recently had a non-zero staking balance to now, in seconds
  */
-const getTimeInPoolSeconds = async (stakingAssetAccountAddress: string): Promise<bigint> => {
+const fetchTimeInPoolSeconds = async (stakingAssetAccountAddress: string): Promise<bigint> => {
   const stakeEvent = getAbiItem({ abi: foxStakingV1Abi, name: 'Stake' })
   const unstakeEvent = getAbiItem({ abi: foxStakingV1Abi, name: 'Unstake' })
 
@@ -40,7 +93,6 @@ const getTimeInPoolSeconds = async (stakingAssetAccountAddress: string): Promise
       },
     }),
   ])
-
   // Fetch all stake and unstake logs. Assumes the user wont generate more than the payload limit of logs
   const [stakeLogs, unstakeLogs] = await Promise.all([
     client.getFilterLogs({ filter: stakeFilter }),
@@ -53,42 +105,7 @@ const getTimeInPoolSeconds = async (stakingAssetAccountAddress: string): Promise
     return Number(a.logIndex - b.logIndex)
   })
 
-  let stakingBalance = 0n
-  let earliestNonZeroStakingBalanceBlockNumber = undefined
-
-  for (const log of sortedLogs) {
-    switch (log.eventName) {
-      case 'Stake': {
-        stakingBalance += log.args.amount ?? 0n
-        break
-      }
-      case 'Unstake': {
-        stakingBalance -= log.args.amount ?? 0n
-        break
-      }
-      default:
-        break
-    }
-
-    // Every time the balance hits zero, reset the timestamp
-    if (stakingBalance === 0n) {
-      earliestNonZeroStakingBalanceBlockNumber = log.blockNumber
-      break
-    }
-  }
-
-  // If the staking balance never got set, the user has never staked
-  if (!earliestNonZeroStakingBalanceBlockNumber) return 0n
-
-  // Get the block timestamp of the earliest non-zero staking balance
-  const { timestamp: earliestNonZeroStakingBalanceTimestamp } = await client.getBlock({
-    blockNumber: earliestNonZeroStakingBalanceBlockNumber,
-  })
-
-  const now = dayjs().unix()
-
-  // Return the time the account has most recently had a non-zero staking balance to now
-  return BigInt(now) - BigInt(earliestNonZeroStakingBalanceTimestamp)
+  return getTimeInPoolSeconds(sortedLogs)
 }
 
 export const useTimeInPoolQuery = <SelectData = bigint>({
@@ -102,7 +119,7 @@ export const useTimeInPoolQuery = <SelectData = bigint>({
   const queryFn = useMemo(
     () =>
       stakingAssetAccountAddress
-        ? () => getTimeInPoolSeconds(stakingAssetAccountAddress)
+        ? () => fetchTimeInPoolSeconds(stakingAssetAccountAddress)
         : skipToken,
     [stakingAssetAccountAddress],
   )

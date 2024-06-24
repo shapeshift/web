b02b924e6b81a1b424c1161589f3fb24156941bb
woody <125113430+woodenfurniture@users.noreply.github.com>
2024-06-24 10:10:44 +1000

    chore: get earliest arbitrum block number of given unix timestamp, without etherscan api (#7193)
    * chore: remove etherscan api from useBlockNumberByTimestampQuery
    
    * chore: placeholder unit test for debugging
    
    * fix: always get the earliest block for a given unix timestamp
    
    * chore: unit testing various edge cases
    
    * chore: update file names
    
    * chore: renaming
    
    * chore: use Math.abs with floor to calculate number of blocks to move
    
    * chore: minimise test data

---
+++ src/pages/RFOX/hooks/useEarliestBlockNumberByTimestampQuery/test-data/blocks.ts
@@ -0,0 +1,404 @@
+import type { Block } from 'viem'
+
+export const blocks: Block[] = [
+  {
+    number: 223998109n,
+    timestamp: 1718926596n,
+  },
+  {
+    number: 223998108n,
+    timestamp: 1718926596n,
+  },
+  {
+    number: 223998107n,
+    timestamp: 1718926596n,
+  },
+  {
+    number: 223998106n,
+    timestamp: 1718926595n,
+  },
+  {
+    number: 223998105n,
+    timestamp: 1718926595n,
+  },
+  {
+    number: 223998104n,
+    timestamp: 1718926595n,
+  },
+  {
+    number: 223998103n,
+    timestamp: 1718926595n,
+  },
+  {
+    number: 223998102n,
+    timestamp: 1718926594n,
+  },
+  {
+    number: 223998101n,
+    timestamp: 1718926594n,
+  },
+  {
+    number: 223998100n,
+    timestamp: 1718926594n,
+  },
+  {
+    number: 223998099n,
+    timestamp: 1718926594n,
+  },
+  {
+    number: 223998098n,
+    timestamp: 1718926593n,
+  },
+  {
+    number: 223998097n,
+    timestamp: 1718926593n,
+  },
+  {
+    number: 223998096n,
+    timestamp: 1718926593n,
+  },
+  {
+    number: 223998095n,
+    timestamp: 1718926593n,
+  },
+  {
+    number: 223998094n,
+    timestamp: 1718926592n,
+  },
+  {
+    number: 223998093n,
+    timestamp: 1718926592n,
+  },
+  {
+    number: 223998092n,
+    timestamp: 1718926592n,
+  },
+  {
+    number: 223998091n,
+    timestamp: 1718926592n,
+  },
+  {
+    number: 223998090n,
+    timestamp: 1718926591n,
+  },
+  {
+    number: 223998089n,
+    timestamp: 1718926591n,
+  },
+  {
+    number: 223998088n,
+    timestamp: 1718926591n,
+  },
+  {
+    number: 223998087n,
+    timestamp: 1718926591n,
+  },
+  {
+    number: 223998086n,
+    timestamp: 1718926590n,
+  },
+  {
+    number: 223998085n,
+    timestamp: 1718926590n,
+  },
+  {
+    number: 223998084n,
+    timestamp: 1718926590n,
+  },
+  {
+    number: 223998083n,
+    timestamp: 1718926590n,
+  },
+  {
+    number: 223998082n,
+    timestamp: 1718926589n,
+  },
+  {
+    number: 223998081n,
+    timestamp: 1718926589n,
+  },
+  {
+    number: 223998080n,
+    timestamp: 1718926589n,
+  },
+  {
+    number: 223998079n,
+    timestamp: 1718926589n,
+  },
+  {
+    number: 223998078n,
+    timestamp: 1718926588n,
+  },
+  {
+    number: 223998077n,
+    timestamp: 1718926588n,
+  },
+  {
+    number: 223998076n,
+    timestamp: 1718926588n,
+  },
+  {
+    number: 223998075n,
+    timestamp: 1718926588n,
+  },
+  {
+    number: 223998074n,
+    timestamp: 1718926587n,
+  },
+  {
+    number: 223998073n,
+    timestamp: 1718926587n,
+  },
+  {
+    number: 223998072n,
+    timestamp: 1718926587n,
+  },
+  {
+    number: 223998071n,
+    timestamp: 1718926587n,
+  },
+  {
+    number: 223998070n,
+    timestamp: 1718926586n,
+  },
+  {
+    number: 223998069n,
+    timestamp: 1718926586n,
+  },
+  {
+    number: 223998068n,
+    timestamp: 1718926586n,
+  },
+  {
+    number: 223998067n,
+    timestamp: 1718926586n,
+  },
+  {
+    number: 223998066n,
+    timestamp: 1718926585n,
+  },
+  {
+    number: 223998065n,
+    timestamp: 1718926585n,
+  },
+  {
+    number: 223998064n,
+    timestamp: 1718926585n,
+  },
+  {
+    number: 223998063n,
+    timestamp: 1718926585n,
+  },
+  {
+    number: 223998062n,
+    timestamp: 1718926584n,
+  },
+  {
+    number: 223998061n,
+    timestamp: 1718926584n,
+  },
+  {
+    number: 223998060n,
+    timestamp: 1718926584n,
+  },
+  {
+    number: 223998059n,
+    timestamp: 1718926584n,
+  },
+  {
+    number: 223998058n,
+    timestamp: 1718926583n,
+  },
+  {
+    number: 223998057n,
+    timestamp: 1718926583n,
+  },
+  {
+    number: 223998056n,
+    timestamp: 1718926583n,
+  },
+  {
+    number: 223998055n,
+    timestamp: 1718926583n,
+  },
+  {
+    number: 223998054n,
+    timestamp: 1718926582n,
+  },
+  {
+    number: 223998053n,
+    timestamp: 1718926582n,
+  },
+  {
+    number: 223998052n,
+    timestamp: 1718926582n,
+  },
+  {
+    number: 223998051n,
+    timestamp: 1718926582n,
+  },
+  {
+    number: 223998050n,
+    timestamp: 1718926581n,
+  },
+  {
+    number: 223998049n,
+    timestamp: 1718926581n,
+  },
+  {
+    number: 223998048n,
+    timestamp: 1718926581n,
+  },
+  {
+    number: 223998047n,
+    timestamp: 1718926581n,
+  },
+  {
+    number: 223998046n,
+    timestamp: 1718926580n,
+  },
+  {
+    number: 223998045n,
+    timestamp: 1718926580n,
+  },
+  {
+    number: 223998044n,
+    timestamp: 1718926580n,
+  },
+  {
+    number: 223998043n,
+    timestamp: 1718926580n,
+  },
+  {
+    number: 223998042n,
+    timestamp: 1718926579n,
+  },
+  {
+    number: 223998041n,
+    timestamp: 1718926579n,
+  },
+  {
+    number: 223998040n,
+    timestamp: 1718926579n,
+  },
+  {
+    number: 223998039n,
+    timestamp: 1718926579n,
+  },
+  {
+    number: 223998038n,
+    timestamp: 1718926578n,
+  },
+  {
+    number: 223998037n,
+    timestamp: 1718926578n,
+  },
+  {
+    number: 223998036n,
+    timestamp: 1718926578n,
+  },
+  {
+    number: 223998035n,
+    timestamp: 1718926578n,
+  },
+  {
+    number: 223998034n,
+    timestamp: 1718926577n,
+  },
+  {
+    number: 223998033n,
+    timestamp: 1718926577n,
+  },
+  {
+    number: 223998032n,
+    timestamp: 1718926577n,
+  },
+  {
+    number: 223998031n,
+    timestamp: 1718926577n,
+  },
+  {
+    number: 223998030n,
+    timestamp: 1718926576n,
+  },
+  {
+    number: 223998029n,
+    timestamp: 1718926576n,
+  },
+  {
+    number: 223998028n,
+    timestamp: 1718926576n,
+  },
+  {
+    number: 223998027n,
+    timestamp: 1718926576n,
+  },
+  {
+    number: 223998026n,
+    timestamp: 1718926575n,
+  },
+  {
+    number: 223998025n,
+    timestamp: 1718926575n,
+  },
+  {
+    number: 223998024n,
+    timestamp: 1718926575n,
+  },
+  {
+    number: 223998023n,
+    timestamp: 1718926575n,
+  },
+  {
+    number: 223998022n,
+    timestamp: 1718926574n,
+  },
+  {
+    number: 223998021n,
+    timestamp: 1718926574n,
+  },
+  {
+    number: 223998020n,
+    timestamp: 1718926574n,
+  },
+  {
+    number: 223998019n,
+    timestamp: 1718926574n,
+  },
+  {
+    number: 223998018n,
+    timestamp: 1718926573n,
+  },
+  {
+    number: 223998017n,
+    timestamp: 1718926573n,
+  },
+  {
+    number: 223998016n,
+    timestamp: 1718926573n,
+  },
+  {
+    number: 223998015n,
+    timestamp: 1718926573n,
+  },
+  {
+    number: 223998014n,
+    timestamp: 1718926572n,
+  },
+  {
+    number: 223998013n,
+    timestamp: 1718926572n,
+  },
+  {
+    number: 223998012n,
+    timestamp: 1718926572n,
+  },
+  {
+    number: 223998011n,
+    timestamp: 1718926572n,
+  },
+  {
+    number: 223998010n,
+    timestamp: 1718926571n,
+  },
+] as Block[]

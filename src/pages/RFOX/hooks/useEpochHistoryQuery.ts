3de29894554631d58082a90ed535a4ef9490a4b3
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-06-24 10:55:31 +0200

    feat: wire-up lifetime rewards (ish)

--- src/pages/RFOX/hooks/useEpochHistoryQuery.ts
+++ src/pages/RFOX/hooks/useEpochHistoryQuery.ts
@@ -12,7 +12,9 @@ import {
 
 type EpochHistoryQueryKey = ['epochHistory']
 
-const RFOX_FIRST_EPOCH_START_TIMESTAMP = BigInt(dayjs('2024-07-01T00:00:00Z').unix())
+// TODO(gomes): revert me -  this obviously won't work until first rewards epoch start
+// const RFOX_FIRST_EPOCH_START_TIMESTAMP = BigInt(dayjs('2024-07-01T00:00:00Z').unix())
+const RFOX_FIRST_EPOCH_START_TIMESTAMP = BigInt(dayjs().subtract(1, 'month').unix())
 
 // The query key excludes the current timestamp so we don't inadvertently end up with stupid things like reactively fetching every second etc.
 // Instead we will rely on staleTime to refetch at a sensible interval.
@@ -32,6 +34,11 @@ export const epochHistoryQueryFn = async (): Promise<EpochMetadata[]> => {
 
   while (startTimestamp < now) {
     const nextStartTimestamp = BigInt(dayjs.unix(Number(startTimestamp)).add(1, 'month').unix())
+
+    if (nextStartTimestamp >= now) {
+      // We cannot fetch blocks in the future using Etherscan's API, or we'll get an "Error! Block timestamp too far in the future" error
+      break
+    }
 
     // using queryClient.fetchQuery here is ok because block timestamps do not change so reactivity is not needed
     const nextBlockNumber = await queryClient.fetchQuery({

c7fdb892f6c11737dec0677d2db602cd7f1ba927
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-06-25 12:27:22 +0200

    fix: 30 days != 1 month

--- src/pages/RFOX/hooks/useEpochHistoryQuery.ts
+++ src/pages/RFOX/hooks/useEpochHistoryQuery.ts
@@ -14,7 +14,7 @@ type EpochHistoryQueryKey = ['epochHistory']
 
 // TODO(gomes): revert me -  this obviously won't work until first rewards epoch start
 // const RFOX_FIRST_EPOCH_START_TIMESTAMP = BigInt(dayjs('2024-07-01T00:00:00Z').unix())
-const RFOX_FIRST_EPOCH_START_TIMESTAMP = BigInt(dayjs().subtract(1, 'month').unix())
+const RFOX_FIRST_EPOCH_START_TIMESTAMP = BigInt(dayjs().subtract(30, 'days').unix())
 
 // The query key excludes the current timestamp so we don't inadvertently end up with stupid things like reactively fetching every second etc.
 // Instead we will rely on staleTime to refetch at a sensible interval.
@@ -33,7 +33,7 @@ export const fetchEpochHistory = async (): Promise<EpochMetadata[]> => {
   const epochHistory = []
 
   while (startTimestamp < now) {
-    const nextStartTimestamp = BigInt(dayjs.unix(Number(startTimestamp)).add(1, 'month').unix())
+    const nextStartTimestamp = BigInt(dayjs.unix(Number(startTimestamp)).add(30, 'days').unix())
     startTimestamp = nextStartTimestamp
 
     if (nextStartTimestamp > now) {

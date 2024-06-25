0d388454d3dc328c2aaeebbefae6e979d260262f
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2024-06-25 17:04:16 +1000

    chore: combine queries in useCurrentEpochRewardsQuery

--- src/pages/RFOX/hooks/useCurrentEpochRewardsQuery.ts
+++ src/pages/RFOX/hooks/useCurrentEpochRewardsQuery.ts
@@ -1,5 +1,6 @@
-import { skipToken, useQuery, useQueryClient } from '@tanstack/react-query'
-import { useMemo } from 'react'
+import type { UseQueryResult } from '@tanstack/react-query'
+import { useQueries } from '@tanstack/react-query'
+import { useCallback } from 'react'
 
 import type { EpochMetadata } from '../types'
 import { calcEpochRewardForAccountRuneBaseUnit } from './helpers'
@@ -17,63 +18,72 @@ export const useCurrentEpochRewardsQuery = ({
   stakingAssetAccountAddress,
   epochMetadata,
 }: UseCurrentEpochRewardsQueryProps) => {
-  const queryClient = useQueryClient()
+  const combineResults = useCallback(
+    (results: [UseQueryResult<bigint, Error>, UseQueryResult<bigint, Error>]) => {
+      const isLoading = results.some(result => result.isLoading)
+      const isPending = results.some(result => result.isError || result.isLoading)
+      const isError = results.some(result => result.isError)
 
-  const queryKey = useMemo(
-    () => [
-      'lifetimeRewards',
+      if (isLoading || isPending) {
+        return {
+          data: undefined,
+          isLoading: true,
+          isError: false,
+        }
+      }
+
+      if (isError) {
+        return {
+          data: undefined,
+          isLoading: false,
+          isError: true,
+        }
+      }
+
+      const [previousEpochEarned, currentEpochEarned] = results.map(result => result.data)
+
+      const epochEarningsForAccount =
+        (currentEpochEarned as bigint) - (previousEpochEarned as bigint)
+
+      const epochRewardRuneBaseUnit = calcEpochRewardForAccountRuneBaseUnit(
+        epochEarningsForAccount,
+        epochMetadata,
+      )
+
+      return {
+        data: epochRewardRuneBaseUnit,
+        isLoading: false,
+        isError: false,
+      }
+    },
+    [epochMetadata],
+  )
+
+  const combinedQueries = useQueries({
+    queries: [
       {
-        stakingAssetAccountAddress,
+        queryKey: getEarnedQueryKey({
+          stakingAssetAccountAddress,
+          blockNumber: epochMetadata.startBlockNumber - 1n,
+        }),
+        queryFn: getEarnedQueryFn({
+          stakingAssetAccountAddress,
+          blockNumber: epochMetadata.startBlockNumber - 1n,
+        }),
+      },
+      {
+        queryKey: getEarnedQueryKey({
+          stakingAssetAccountAddress,
+          blockNumber: undefined,
+        }),
+        queryFn: getEarnedQueryFn({
+          stakingAssetAccountAddress,
+          blockNumber: undefined,
+        }),
       },
     ],
-    [stakingAssetAccountAddress],
-  )
-
-  const queryFn = useMemo(
-    () =>
-      stakingAssetAccountAddress
-        ? async () => {
-            const [previousEpochEarned, currentEpochEarned] = await Promise.all([
-              queryClient.fetchQuery({
-                queryKey: getEarnedQueryKey({
-                  stakingAssetAccountAddress,
-                  blockNumber: epochMetadata.startBlockNumber - 1n,
-                }),
-                queryFn: getEarnedQueryFn({
-                  stakingAssetAccountAddress,
-                  blockNumber: epochMetadata.startBlockNumber - 1n,
-                }),
-              }),
-              queryClient.fetchQuery({
-                queryKey: getEarnedQueryKey({
-                  stakingAssetAccountAddress,
-                  blockNumber: undefined,
-                }),
-                queryFn: getEarnedQueryFn({
-                  stakingAssetAccountAddress,
-                  blockNumber: undefined,
-                }),
-              }),
-            ])
-
-            const epochEarningsForAccount =
-              (currentEpochEarned as bigint) - (previousEpochEarned as bigint)
-
-            const epochRewardRuneBaseUnit = calcEpochRewardForAccountRuneBaseUnit(
-              epochEarningsForAccount,
-              epochMetadata,
-            )
-
-            return epochRewardRuneBaseUnit
-          }
-        : skipToken,
-    [epochMetadata, queryClient, stakingAssetAccountAddress],
-  )
-
-  const epochEarnedHistoryQuery = useQuery({
-    queryKey,
-    queryFn,
+    combine: combineResults,
   })
 
-  return epochEarnedHistoryQuery
+  return combinedQueries
 }

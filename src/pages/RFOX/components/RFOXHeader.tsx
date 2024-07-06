454a3eb66eaad21bf4a674f1eac08a605a598678
reallybeard <89934888+reallybeard@users.noreply.github.com>
2024-07-06 01:28:17 -0500

    add rfox to explore page
    Add rfox to explore page
    add mobile header nav for rfox
    update max width of the widget

---
+++ src/pages/RFOX/components/RFOXHeader.tsx
@@ -0,0 +1,41 @@
+import { Heading, Stack } from '@chakra-ui/react'
+import { useCallback } from 'react'
+import { useTranslate } from 'react-polyglot'
+import { useHistory } from 'react-router'
+import { Display } from 'components/Display'
+import { PageBackButton, PageHeader } from 'components/Layout/Header/PageHeader'
+import { Text } from 'components/Text'
+
+const containerPaddingTop = { base: 0, md: 8 }
+
+export const RFOXHeader = () => {
+  const translate = useTranslate()
+  const history = useHistory()
+
+  const handleBack = useCallback(() => {
+    history.push('/explore')
+  }, [history])
+
+  return (
+    <PageHeader>
+      <Display.Mobile>
+        <PageHeader.Left>
+          <PageBackButton onBack={handleBack} />
+        </PageHeader.Left>
+      </Display.Mobile>
+      <Display.Desktop>
+        <PageHeader.Left>
+          <Stack pb={4}>
+            <Heading>{translate('RFOX.staking')}</Heading>
+            <Text translation='explore.rfox.body' color='text.subtle' />
+          </Stack>
+        </PageHeader.Left>
+      </Display.Desktop>
+      <Display.Mobile>
+        <PageHeader.Middle>
+          <PageHeader.Title>{translate('RFOX.staking')}</PageHeader.Title>
+        </PageHeader.Middle>
+      </Display.Mobile>
+    </PageHeader>
+  )
+}

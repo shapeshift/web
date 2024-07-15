5886a7b1495d4fcaac79b797da910409e2cc7e79
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2024-07-16 07:34:34 +1000

    fix: use ackchyual ipfs hash for current epoch

--- src/pages/RFOX/constants.ts
+++ src/pages/RFOX/constants.ts
@@ -8,4 +8,4 @@ export const withdrawEvent = getAbiItem({ abi: foxStakingV1Abi, name: 'Withdraw'
 
 export const IPFS_GATEWAY = 'https://gateway.shapeshift.com/ipfs'
 
-export const CURRENT_EPOCH_IPFS_HASH = 'QmaPiFbJSVoUVb2TJdZ2NkwExJPMD172LGGjAvDuKrJayK'
+export const CURRENT_EPOCH_IPFS_HASH = 'QmPWBaWE7WqYmv88ayi2NTbmEfkuxwaogKGVDmQes9U6jd'

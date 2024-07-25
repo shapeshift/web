f45adf079ba02fc7ef84dc103dc8f9d57c321464
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-07-25 17:10:28 +0300

    feat: hasta la vista identicons generated icons (#7409)

--- scripts/generateAssetData/bnbsmartchain/index.ts
+++ scripts/generateAssetData/bnbsmartchain/index.ts
@@ -4,7 +4,6 @@ import uniqBy from 'lodash/uniqBy'
 
 import { bnbsmartchain } from '../baseAssets'
 import * as coingecko from '../coingecko'
-import { getRenderedIdenticonBase64 } from '../generateAssetIcon/generateAssetIcon'
 // import { getPortalTokens } from '../utils/portals'
 
 export const getAssets = async (): Promise<Asset[]> => {
@@ -22,13 +21,5 @@ export const getAssets = async (): Promise<Asset[]> => {
 
   const allAssets = uniqBy(assets.concat(portalsAssets).concat([bnbsmartchain]), 'assetId')
 
-  return allAssets.map(asset => ({
-    ...asset,
-    icon:
-      asset.icon ||
-      getRenderedIdenticonBase64(asset.assetId, asset.symbol, {
-        identiconImage: { size: 128, background: [45, 55, 72, 255] },
-        identiconText: { symbolScale: 7, enableShadow: true },
-      }),
-  }))
+  return allAssets
 }

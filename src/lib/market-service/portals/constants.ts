d896e30abf9f17f2f655f7520e06d0875adcc4e5
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-07-30 22:07:52 +0300

    feat: move constant somewhere sane

---
+++ src/lib/market-service/portals/constants.ts
@@ -0,0 +1,22 @@
+import type { ChainId } from '@shapeshiftoss/caip'
+import {
+  avalancheChainId,
+  baseChainId,
+  bscChainId,
+  ethChainId,
+  gnosisChainId,
+  optimismChainId,
+  polygonChainId,
+} from '@shapeshiftoss/caip'
+import { arbitrumChainId } from '@shapeshiftoss/caip/src/constants'
+
+export const CHAIN_ID_TO_PORTALS_NETWORK: Partial<Record<ChainId, string>> = {
+  [avalancheChainId]: 'avalanche',
+  [ethChainId]: 'ethereum',
+  [polygonChainId]: 'polygon',
+  [bscChainId]: 'bsc',
+  [optimismChainId]: 'optimism',
+  [arbitrumChainId]: 'arbitrum',
+  [gnosisChainId]: 'gnosis',
+  [baseChainId]: 'base',
+}

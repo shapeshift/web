36b48653307c200c5e05220bd255299d43f75846
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-08-01 17:53:11 +0300

    feat: easier fix

--- scripts/generateAssetData/setColors.ts
+++ scripts/generateAssetData/setColors.ts
@@ -9,20 +9,13 @@ export const setColors = async (assets: Asset[]): Promise<Asset[]> => {
 
   for await (const [index, asset] of filteredAssets.entries()) {
     try {
-      // No icon, no color :shrugs:
-      if (!asset.icon) break
-      // Don't generate colors for pool assets, or this will run for a very long time in CI and eventually fail
-      // Many pool assets also just happen to have webp/svg icons which colorThief can't handle because of inherent get-pixels limitations
-      if (asset.isPool) break
-      // Only generate colors for assets that have a default white background (i.e don't have a custom color generated yet)
-      if (asset.color !== '#FFFFFF') break
-
-      // colorThief.getColor returns the most dominant color in the icon.
-      const [r, g, b] = await colorThief.getColor(asset.icon)
-      const hexColor = `#${toHex(r)}${toHex(g)}${toHex(b)}`
-      asset.color = hexColor
+      if (asset.color === '#FFFFFF' && asset.icon) {
+        // colorThief.getColor returns the most dominant color in the icon.
+        const [r, g, b] = await colorThief.getColor(asset.icon)
+        const hexColor = `#${toHex(r)}${toHex(g)}${toHex(b)}`
+        asset.color = hexColor
+      }
     } catch (err) {
-      console.error(err)
       console.info(
         `${index + 1}/${filteredAssets.length} Could not get color for ${asset.assetId} iconUrl: ${
           asset.icon

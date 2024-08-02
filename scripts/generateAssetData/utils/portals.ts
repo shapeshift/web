a69391a3cf7a2f25ee1c7a5249f79c4a3d76fab7
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-08-02 23:32:30 +0300

    feat: upstream fallback and regen assets and icons

--- scripts/generateAssetData/utils/portals.ts
+++ scripts/generateAssetData/utils/portals.ts
@@ -1,7 +1,6 @@
 import type { ChainId } from '@shapeshiftoss/caip'
 import { ASSET_NAMESPACE, bscChainId, toAssetId } from '@shapeshiftoss/caip'
 import type { Asset, AssetsByIdPartial } from '@shapeshiftoss/types'
-import { isSome } from '@shapeshiftoss/utils'
 import axios from 'axios'
 import qs from 'qs'
 import { getAddress, isAddressEqual, zeroAddress } from 'viem'
@@ -12,6 +11,7 @@ import type {
   PlatformsById,
   TokenInfo,
 } from 'lib/market-service/portals/types'
+import { isSome } from 'lib/utils'
 
 import generatedAssetData from '../../../src/lib/asset-service/service/generatedAssetData.json'
 import { colorMap } from '../colorMap'
@@ -112,6 +112,11 @@ const fetchPortalsTokens = async (
   }
 }
 
+const maybeTokenImage = (image: string | undefined) => {
+  if (!image) return
+  if (image === 'missing_large.png') return
+  return image
+}
 export const getPortalTokens = async (nativeAsset: Asset): Promise<Asset[]> => {
   const portalsPlatforms = await fetchPortalsPlatforms()
   const chainId = nativeAsset.chainId
@@ -122,26 +127,28 @@ export const getPortalTokens = async (nativeAsset: Asset): Promise<Asset[]> => {
   }
 
   const portalsTokens = await fetchPortalsTokens(chainId)
-  return portalsTokens.map(token => {
-    const assetId = toAssetId({
-      chainId,
-      assetNamespace: chainId === bscChainId ? ASSET_NAMESPACE.bep20 : ASSET_NAMESPACE.erc20,
-      assetReference: token.address,
-    })
-    const asset = assets[assetId]
+  return portalsTokens
+    .map(token => {
+      const assetId = toAssetId({
+        chainId,
+        assetNamespace: chainId === bscChainId ? ASSET_NAMESPACE.bep20 : ASSET_NAMESPACE.erc20,
+        assetReference: token.address,
+      })
+      const asset = assets[assetId]
 
-    const platform = portalsPlatforms[token.platform]
-    const isPool = Boolean(platform && token.tokens?.length) || undefined
+      const platform = portalsPlatforms[token.platform]
+      const isPool = Boolean(platform && token.tokens?.length) || undefined
 
-    const iconOrIcons = (() => {
-      // There are no underlying tokens, return asset icon
-      if (!token.tokens?.length) return { icon: asset?.icon }
+      const iconOrIcons = (() => {
+        // There are no underlying tokens, return asset icon
+        // Note, images are effectively off-by-1 in the API, the first image is the platform image
+        if (!token.tokens?.length)
+          return { icon: asset?.icon || maybeTokenImage(token.images?.[1]) }
 
-      // This is a multiple assets pool, populate icons array
-      if (token.tokens.length > 1)
-        return {
-          icons: token.tokens
-            .map(underlyingToken => {
+        // This is a multiple assets pool, populate icons array
+        if (token.tokens.length > 1)
+          return {
+            icons: token.tokens.map((underlyingToken, i) => {
               const underlyingAssetId = toAssetId({
                 chainId,
                 assetNamespace:
@@ -149,68 +156,75 @@ export const getPortalTokens = async (nativeAsset: Asset): Promise<Asset[]> => {
                 assetReference: underlyingToken,
               })
               const underlyingAsset = assets[underlyingAssetId]
-              return underlyingAsset?.icon
-            })
-            .filter(isSome),
-          icon: undefined,
-        }
-
-      // Only a single asset pool, no need to populate the icons array
-      return { icon: asset?.icon }
-    })()
-
-    // New naming logic
-    const name = (() => {
-      // For single assets, just use the token name
-      if (!isPool) return token.name
-      // For pools, create a name in the format of "<platform> <assets> Pool"
-      // e.g "UniswapV2 ETH/FOX Pool"
-      const assetSymbols =
-        token.tokens?.map(underlyingToken => {
-          const assetId = toAssetId({
-            chainId,
-            assetNamespace: chainId === bscChainId ? ASSET_NAMESPACE.bep20 : ASSET_NAMESPACE.erc20,
-            assetReference: underlyingToken,
-          })
-          const underlyingAsset = assets[assetId]
-          if (!underlyingAsset) return undefined
-
-          // This doesn't generalize, but this'll do, this is only a visual hack to display native asset instead of wrapped
-          // We could potentially use related assets for this and use primary implementation, though we'd have to remove BTC from there as WBTC and BTC are very
-          // much different assets on diff networks, i.e can't deposit BTC instead of WBTC automagically like you would with ETH instead of WETH
-          switch (underlyingAsset.symbol) {
-            case 'WETH':
-              return 'ETH'
-            case 'WBNB':
-              return 'BNB'
-            case 'WMATIC':
-              return 'MATIC'
-            case 'WAVAX':
-              return 'AVAX'
-            default:
-              return underlyingAsset.symbol
+              return underlyingAsset?.icon || maybeTokenImage(token.images?.[i + 1])
+            }),
+            icon: undefined,
           }
-        }) ?? []
 
-      // Our best effort to contruct sane name using the native asset -> asset naming hack failed, but thankfully, upstream name is very close e.g
-      // for "UniswapV2 LP TRUST/WETH", we just have to append "Pool" to that and we're gucci
-      if (assetSymbols.some(symbol => !symbol)) return `${token.name} Pool`
-      return `${platform.name} ${assetSymbols.join('/')} Pool`
-    })()
+        // There *should* be only a single asset in pool, though that's not necessarily the case
+        // e.g `0xdf666a5370fb4b21672f9145b29a56d589cc91fd` has token.length === 1, but actually has two underlying tokens
+        // Assume the above is an edge case and return the icon if present in generatedAssetData.json, else assume sad and return the protocol icon
+        return { icon: asset?.icon || maybeTokenImage(token.images?.[0]) }
+      })()
 
-    return {
-      ...explorerData,
-      color: colorMap[assetId] ?? '#FFFFFF',
-      ...iconOrIcons,
-      name,
-      precision: Number(token.decimals),
-      symbol: token.symbol,
-      chainId: nativeAsset.chainId,
-      assetId,
-      relatedAssetKey: undefined,
-      // undefined short-circuit isn't a mistake - JSON doesn't support undefined, so this will avoid adding an additional line to the JSON
-      // for non-pool assets
-      isPool,
-    }
-  })
+      // No icons in assets, nor upstream
+      if (!iconOrIcons.icon && !iconOrIcons.icons?.some(isSome)) return undefined
+
+      // New naming logic
+      const name = (() => {
+        // For single assets, just use the token name
+        if (!isPool) return token.name
+        // For pools, create a name in the format of "<platform> <assets> Pool"
+        // e.g "UniswapV2 ETH/FOX Pool"
+        const assetSymbols =
+          token.tokens?.map(underlyingToken => {
+            const assetId = toAssetId({
+              chainId,
+              assetNamespace:
+                chainId === bscChainId ? ASSET_NAMESPACE.bep20 : ASSET_NAMESPACE.erc20,
+              assetReference: underlyingToken,
+            })
+            const underlyingAsset = assets[assetId]
+            if (!underlyingAsset) return undefined
+
+            // This doesn't generalize, but this'll do, this is only a visual hack to display native asset instead of wrapped
+            // We could potentially use related assets for this and use primary implementation, though we'd have to remove BTC from there as WBTC and BTC are very
+            // much different assets on diff networks, i.e can't deposit BTC instead of WBTC automagically like you would with ETH instead of WETH
+            switch (underlyingAsset.symbol) {
+              case 'WETH':
+                return 'ETH'
+              case 'WBNB':
+                return 'BNB'
+              case 'WMATIC':
+                return 'MATIC'
+              case 'WAVAX':
+                return 'AVAX'
+              default:
+                return underlyingAsset.symbol
+            }
+          }) ?? []
+
+        // Our best effort to contruct sane name using the native asset -> asset naming hack failed, but thankfully, upstream name is very close e.g
+        // for "UniswapV2 LP TRUST/WETH", we just have to append "Pool" to that and we're gucci
+        if (assetSymbols.some(symbol => !symbol)) return `${token.name} Pool`
+        return `${platform.name} ${assetSymbols.join('/')} Pool`
+      })()
+
+      return {
+        ...explorerData,
+        color: colorMap[assetId] ?? '#FFFFFF',
+        // This looks weird but we need this - l.165 check above nulls the type safety of this object, so we cast it back
+        ...(iconOrIcons as { icon: string } | { icons: string[]; icon: undefined }),
+        name,
+        precision: Number(token.decimals),
+        symbol: token.symbol,
+        chainId: nativeAsset.chainId,
+        assetId,
+        relatedAssetKey: undefined,
+        // undefined short-circuit isn't a mistake - JSON doesn't support undefined, so this will avoid adding an additional line to the JSON
+        // for non-pool assets
+        isPool,
+      }
+    })
+    .filter(isSome)
 }

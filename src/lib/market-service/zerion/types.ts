f5f012c79bf83ad8adad054e5cef596ec90910be
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2024-08-07 06:38:08 +1000

    fix: zerion types

--- src/lib/market-service/zerion/types.ts
+++ src/lib/market-service/zerion/types.ts
@@ -1,14 +1,14 @@
 export type ZerionMarketData = {
-  price: number
-  total_supply: number
-  circulating_supply: number
-  fully_diluted_valuation: number
-  market_cap: number
+  price: number | null
+  total_supply: number | null
+  circulating_supply: number | null
+  fully_diluted_valuation: number | null
+  market_cap: number | null
   changes: {
-    percent_1d: number
-    percent_30d: number
-    percent_90d: number
-    pecent_365d: number
+    percent_1d: number | null
+    percent_30d: number | null
+    percent_90d: number | null
+    pecent_365d: number | null
   }
 }
 

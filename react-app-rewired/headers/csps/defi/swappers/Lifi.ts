c20fabd4ef01f6a3b72aabd8f5b378f4bb24d35e
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-08-08 21:07:28 +0300

    feat: csp

--- react-app-rewired/headers/csps/defi/swappers/Lifi.ts
+++ react-app-rewired/headers/csps/defi/swappers/Lifi.ts
@@ -4,8 +4,6 @@ export const csp: Csp = {
   'connect-src': [
     // src/lib/swapper/LifiSwapper/LifiSwapper.ts
     'https://li.quest',
-    // Move me to a better place if we ever use this PoC and support all EVM chains
-    'https://safe-transaction-avalanche.safe.global/api/v1/',
     'https://mainnet.infura.io',
     'https://api.avax.network',
   ],

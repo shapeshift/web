ac7f3c2898a2f002a228baf798a839df4068076b
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2024-08-09 09:34:34 +1000

    chore: rename const

--- src/components/Modals/LedgerOpenApp/hooks/useLedgerAppDetails.tsx
+++ src/components/Modals/LedgerOpenApp/hooks/useLedgerAppDetails.tsx
@@ -6,16 +6,16 @@ import { selectAssetById, selectFeeAssetByChainId } from 'state/slices/selectors
 import { useAppSelector } from 'state/store'
 
 export const useLedgerAppDetails = (chainId: ChainId) => {
-  const asset = useAppSelector(state => selectFeeAssetByChainId(state, chainId))
+  const feeAsset = useAppSelector(state => selectFeeAssetByChainId(state, chainId))
   const ethAsset = useAppSelector(state => selectAssetById(state, ethAssetId))
   const appName = useMemo(() => {
     if (isEvmChainId(chainId)) return ethAsset?.networkName
-    return asset?.networkName
-  }, [asset?.networkName, chainId, ethAsset?.networkName])
+    return feeAsset?.networkName
+  }, [feeAsset?.networkName, chainId, ethAsset?.networkName])
   const appAsset = useMemo(() => {
     if (isEvmChainId(chainId)) return ethAsset
-    return asset
-  }, [asset, chainId, ethAsset])
+    return feeAsset
+  }, [feeAsset, chainId, ethAsset])
 
   return { appName, appAsset }
 }

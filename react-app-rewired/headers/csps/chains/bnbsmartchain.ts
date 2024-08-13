ec72fac4747d4d20b29f9c85aa25ccaed407a02d
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-08-13 14:18:13 -0700

    fix: hotfix graceful evm accounts derivation failure tolerance (#7549)

--- react-app-rewired/headers/csps/chains/bnbsmartchain.ts
+++ react-app-rewired/headers/csps/chains/bnbsmartchain.ts
@@ -5,5 +5,6 @@ export const csp: Csp = {
     process.env.REACT_APP_BNBSMARTCHAIN_NODE_URL!,
     process.env.REACT_APP_UNCHAINED_BNBSMARTCHAIN_HTTP_URL!,
     process.env.REACT_APP_UNCHAINED_BNBSMARTCHAIN_WS_URL!,
+    'https://binance.llamarpc.com',
   ],
 }

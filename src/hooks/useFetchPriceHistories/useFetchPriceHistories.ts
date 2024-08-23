bf4027ece7107e320368077dfe68a1bf30aee61a
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-08-23 15:24:18 -0600

    feat: useFetchPriceHistories leverage useQueries

--- src/hooks/useFetchPriceHistories/useFetchPriceHistories.ts
+++ src/hooks/useFetchPriceHistories/useFetchPriceHistories.ts
@@ -1,6 +1,6 @@
 import type { AssetId } from '@shapeshiftoss/caip'
 import type { HistoryTimeframe } from '@shapeshiftoss/types'
-import { useQuery } from '@tanstack/react-query'
+import { useQueries } from '@tanstack/react-query'
 import { DEFAULT_HISTORY_TIMEFRAME } from 'constants/Config'
 import { useEffect } from 'react'
 import { marketApi, marketData } from 'state/slices/marketDataSlice/marketDataSlice'
@@ -23,44 +23,46 @@ export const useFetchPriceHistories = (assetIds: AssetId[], timeframe: HistoryTi
 
   const portfolioLoadingStatus = useAppSelector(selectPortfolioLoadingStatus)
 
-  useQuery({
-    queryKey: ['marketData', assetIds],
-    queryFn: async () => {
-      // Only commented out to make it clear we do NOT want to use RTK options here
-      // We use react-query as a wrapper because it allows us to disable refetch for background tabs
-      // const opts = { subscriptionOptions: { pollingInterval: marketDataPollingInterval } }
-      const timeframe = DEFAULT_HISTORY_TIMEFRAME
+  useQueries({
+    queries: assetIds.map(assetId => ({
+      queryKey: ['marketData', assetId],
+      queryFn: async () => {
+        // Only commented out to make it clear we do NOT want to use RTK options here
+        // We use react-query as a wrapper because it allows us to disable refetch for background tabs
+        // const opts = { subscriptionOptions: { pollingInterval: marketDataPollingInterval } }
+        const timeframe = DEFAULT_HISTORY_TIMEFRAME
 
-      await Promise.all(
-        assetIds.map(assetId =>
-          dispatch(
-            marketApi.endpoints.findPriceHistoryByAssetId.initiate(
-              {
-                timeframe,
-                assetId,
-              },
-              // Since we use react-query as a polling wrapper, every initiate call *is* a force refetch here
-              { forceRefetch: true },
+        await Promise.all(
+          assetIds.map(assetId =>
+            dispatch(
+              marketApi.endpoints.findPriceHistoryByAssetId.initiate(
+                {
+                  timeframe,
+                  assetId,
+                },
+                // Since we use react-query as a polling wrapper, every initiate call *is* a force refetch here
+                { forceRefetch: true },
+              ),
             ),
           ),
-        ),
-      )
+        )
 
-      // used to trigger mixpanel init after load of market data
-      dispatch(marketData.actions.setMarketDataLoaded())
+        // used to trigger mixpanel init after load of market data
+        dispatch(marketData.actions.setMarketDataLoaded())
 
-      // We *have* to return a value other than undefined from react-query queries, see
-      // https://tanstack.com/query/v4/docs/react/guides/migrating-to-react-query-4#undefined-is-an-illegal-cache-value-for-successful-queries
-      return null
-    },
-    // once the portfolio is loaded, fetch market data for all portfolio assets
-    // and start refetch timer to keep market data up to date
-    enabled: portfolioLoadingStatus !== 'loading',
-    refetchInterval: marketDataPollingInterval,
-    // Do NOT refetch market data in background to avoid spamming coingecko
-    refetchIntervalInBackground: false,
-    // Do NOT refetch market data on window focus to avoid spamming coingecko
-    refetchOnWindowFocus: false,
+        // We *have* to return a value other than undefined from react-query queries, see
+        // https://tanstack.com/query/v4/docs/react/guides/migrating-to-react-query-4#undefined-is-an-illegal-cache-value-for-successful-queries
+        return null
+      },
+      // once the portfolio is loaded, fetch market data for all portfolio assets
+      // and start refetch timer to keep market data up to date
+      enabled: portfolioLoadingStatus !== 'loading',
+      refetchInterval: marketDataPollingInterval,
+      // Do NOT refetch market data in background to avoid spamming coingecko
+      refetchIntervalInBackground: false,
+      // Do NOT refetch market data on window focus to avoid spamming coingecko
+      refetchOnWindowFocus: false,
+    })),
   })
 
   useEffect(() => {

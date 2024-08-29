f51bc16bec2768747a2e731aca35ad3ced977910
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-08-29 17:27:21 +0200

    fix: derp

--- src/lib/investor/investor-foxy/api/api.ts
+++ src/lib/investor/investor-foxy/api/api.ts
@@ -140,13 +140,13 @@ export class FoxyApi {
       data: payload.data,
       accountNumber: payload.bip44Params.accountNumber,
       ...(shouldUseEIP1559Fees ? { maxFeePerGas, maxPriorityFeePerGas } : { gasPrice }),
-      checkLedgerAppOpenIfLedgerConnected: () => Promise.resolve(),
+      checkLedgerAppOpenIfLedgerConnected,
     })
 
     const senderAddress = await this.adapter.getAddress({
       accountNumber: payload.bip44Params.accountNumber,
       wallet,
-      checkLedgerAppOpenIfLedgerConnected: () => Promise.resolve(),
+      checkLedgerAppOpenIfLedgerConnected,
     })
 
     if (wallet.supportsOfflineSigning()) {

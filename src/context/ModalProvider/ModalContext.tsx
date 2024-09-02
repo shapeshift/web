010e1b73fc7652ff275cd3de413d413d9dbe7876
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-09-02 11:46:32 +0200

    feat: improve types

--- src/context/ModalProvider/ModalContext.tsx
+++ src/context/ModalProvider/ModalContext.tsx
@@ -1,14 +1,12 @@
 import { createContext } from 'react'
 
 import { MODAL_KEYS } from './constants'
-import type { BaseProps, ModalContextType, Modals, ModalState } from './types'
+import type { BaseProps, Modals, ModalState } from './types'
 
-// @ts-ignore blatant ignore as we inject the correct props during provider creation in ModalContainer
-export const modalContext: ModalState = MODAL_KEYS.reduce<ModalContextType>(
+export const modalContext: ModalState = MODAL_KEYS.reduce<ModalState>(
   (acc, key: keyof Modals) => ({
     ...acc,
-    // blatant cast as we inject the correct props during provider creation in ModalContainer
     [key]: createContext<BaseProps<keyof Modals>>({} as BaseProps<keyof Modals>),
   }),
-  {} as ModalContextType,
+  {} as ModalState,
 )

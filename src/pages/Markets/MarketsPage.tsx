b1f0c3f4ce1a4bf57672427a5d3c5ebf01c55eb5
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-09-06 17:17:48 +0200

    feat: recommended markets page boilerplate

--- src/pages/Markets/MarketsPage.tsx
+++ src/pages/Markets/MarketsPage.tsx
@@ -1,1 +1,20 @@
-export const MarketsPage = () => null
+import { Redirect, Route, Switch, useRouteMatch } from 'react-router'
+
+import { Recommended } from './Recommended'
+
+export const MarketsPage = () => {
+  const { path } = useRouteMatch()
+
+  return (
+    <Switch>
+      <Route exact path={`${path}`}>
+        <Redirect to={`${path}/recommended`} />
+      </Route>
+      <Route exact path={`${path}/recommended`}>
+        <Recommended />
+      </Route>
+      <Route path={`${path}/watchlist`}></Route>
+      <Route path={`${path}/categories`}></Route>
+    </Switch>
+  )
+}

d164248201c737b92e3d1a2c7ffa3a752bbf0261
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2024-09-06 09:43:59 -0600

    use chain adapters for getPublicKey

--- src/lib/account/utxo.ts
+++ src/lib/account/utxo.ts
@@ -1,11 +1,6 @@
 import { toAccountId } from '@shapeshiftoss/caip'
-import {
-  convertXpubVersion,
-  toRootDerivationPath,
-  utxoAccountParams,
-  utxoChainIds,
-} from '@shapeshiftoss/chain-adapters'
-import { bip32ToAddressNList, supportsBTC } from '@shapeshiftoss/hdwallet-core'
+import { utxoAccountParams, utxoChainIds } from '@shapeshiftoss/chain-adapters'
+import { supportsBTC } from '@shapeshiftoss/hdwallet-core'
 import { MetaMaskShapeShiftMultiChainHDWallet } from '@shapeshiftoss/hdwallet-shapeshift-multichain'
 import type { AccountMetadataById, UtxoChainId } from '@shapeshiftoss/types'
 import { UtxoAccountType } from '@shapeshiftoss/types'
@@ -29,24 +24,13 @@ export const deriveUtxoAccountIdsAndMetadata: DeriveAccountIdsAndMetadata = asyn
         supportedAccountTypes = [UtxoAccountType.P2pkh]
       }
       for (const accountType of supportedAccountTypes) {
-        const { bip44Params, scriptType } = utxoAccountParams(chainId, accountType, accountNumber)
-        const addressNList = bip32ToAddressNList(toRootDerivationPath(bip44Params))
-        const pubkeys = await wallet.getPublicKeys([
-          {
-            coin: adapter.getCoinName(),
-            addressNList,
-            curve: 'secp256k1',
-            scriptType,
-          },
-        ])
+        const { xpub: pubkey } = await adapter.getPublicKey(wallet, accountNumber, accountType)
 
-        // We do not want to throw for all ChainIds and script types, if one fails
-        if (!pubkeys?.[0]?.xpub || typeof pubkeys?.[0]?.xpub !== 'string') continue
-
-        const pubkey = convertXpubVersion(pubkeys[0].xpub, accountType)
         if (!pubkey) continue
 
+        const { bip44Params } = utxoAccountParams(chainId, accountType, accountNumber)
         const accountId = toAccountId({ chainId, account: pubkey })
+
         acc[accountId] = { accountType, bip44Params }
       }
     }

7126a3fefbf2e1cf566f7419d713e73863d792de
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-09-06 16:53:05 +0200

    feat: bootstrap markets page

--- src/config.ts
+++ src/config.ts
@@ -168,6 +168,7 @@ const validators = {
   REACT_APP_FEATURE_ARBITRUM_BRIDGE_CLAIMS: bool({ default: false }),
   REACT_APP_FEATURE_USDT_APPROVAL_RESET: bool({ default: false }),
   REACT_APP_FEATURE_RUNEPOOL: bool({ default: false }),
+  REACT_APP_FEATURE_MARKETS: bool({ default: false }),
   REACT_APP_PORTALS_BASE_URL: url(),
   REACT_APP_ZERION_BASE_URL: url(),
 }

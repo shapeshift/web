204a2954a07e1e02be7f06a05163c34789001977
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-09-09 22:41:41 +0200

    feat: add csp

--- react-app-rewired/headers/csps/chains/thorchain.ts
+++ react-app-rewired/headers/csps/chains/thorchain.ts
@@ -6,5 +6,6 @@ export const csp: Csp = {
     process.env.REACT_APP_UNCHAINED_THORCHAIN_WS_URL!,
     process.env.REACT_APP_UNCHAINED_THORCHAIN_V1_HTTP_URL!,
     process.env.REACT_APP_THORCHAIN_NODE_URL!,
+    process.env.REACT_APP_THORCHAIN_V1_NODE_URL!,
   ],
 }

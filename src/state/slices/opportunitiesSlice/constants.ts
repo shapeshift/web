0821b81f21547cd3be6fd619af10f5bbea760b9d
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2024-09-19 16:48:28 +1000

    chore: comment out evergreen contract for merge

--- src/state/slices/opportunitiesSlice/constants.ts
+++ src/state/slices/opportunitiesSlice/constants.ts
@@ -61,7 +61,7 @@ export const foxEthAssetIds = [
   foxEthStakingAssetIdV7,
   foxEthStakingAssetIdV8,
   foxEthStakingAssetIdV9,
-  foxEthStakingAssetIdEvergreen,
+  // foxEthStakingAssetIdEvergreen, // TODO: Uncomment when Evergreen staking is ready for go-live
 ] as const
 export const foxEthStakingIds = foxEthAssetIds as readonly StakingId[]
 

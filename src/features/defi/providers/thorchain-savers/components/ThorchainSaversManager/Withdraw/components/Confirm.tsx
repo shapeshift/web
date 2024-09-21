557b335bbae44597e8b95ab81e7a5bd2edfb9a5f
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-09-21 12:27:35 +0200

    feat: more cleanup

--- src/features/defi/providers/thorchain-savers/components/ThorchainSaversManager/Withdraw/components/Confirm.tsx
+++ src/features/defi/providers/thorchain-savers/components/ThorchainSaversManager/Withdraw/components/Confirm.tsx
@@ -220,7 +220,6 @@ export const Confirm: React.FC<ConfirmProps> = ({ accountId, onNext }) => {
         setProtocolFeeCryptoBaseUnit(
           toBaseUnit(fromThorBaseUnit(protocolFeeCryptoThorBaseUnit), asset.precision),
         )
-
         setDustAmountCryptoBaseUnit(
           bnOrZero(toBaseUnit(fromThorBaseUnit(dust_amount), asset.precision)).toFixed(
             asset.precision,
@@ -249,7 +248,6 @@ export const Confirm: React.FC<ConfirmProps> = ({ accountId, onNext }) => {
     protocolFeeCryptoBaseUnit,
     expiry,
     isRunePool,
-    feeAsset?.assetId,
   ])
 
   useEffect(() => {

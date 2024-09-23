3cded9843f0cd934964281530fd65d7f0d184889
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2024-09-23 10:39:08 +1000

    feat: upgrade zrx swapper quotes to support v2 api endpoints with permit2

--- packages/swapper/src/swappers/ZrxSwapper/utils/fetchFromZrx.ts
+++ packages/swapper/src/swappers/ZrxSwapper/utils/fetchFromZrx.ts
@@ -1,5 +1,9 @@
+import { viemNetworkIdByChainId } from '@shapeshiftoss/contracts'
 import type { Asset } from '@shapeshiftoss/types'
-import { convertBasisPointsToDecimalPercentage } from '@shapeshiftoss/utils'
+import {
+  convertBasisPointsToDecimalPercentage,
+  convertDecimalPercentageToBasisPoints,
+} from '@shapeshiftoss/utils'
 import type { Result } from '@sniptt/monads'
 import { Err, Ok } from '@sniptt/monads'
 
@@ -7,7 +11,13 @@ import { getDefaultSlippageDecimalPercentageForSwapper } from '../../../constant
 import type { SwapErrorRight } from '../../../types'
 import { SwapperName } from '../../../types'
 import { getTreasuryAddressFromChainId } from '../../utils/helpers/helpers'
-import type { ZrxPriceResponse, ZrxQuoteResponse, ZrxSupportedChainId } from '../types'
+import type {
+  ZrxPermit2PriceResponse,
+  ZrxPermit2QuoteResponse,
+  ZrxPriceResponse,
+  ZrxQuoteResponse,
+  ZrxSupportedChainId,
+} from '../types'
 import { AFFILIATE_ADDRESS } from './constants'
 import { assetToToken, baseUrlFromChainId } from './helpers/helpers'
 import { zrxServiceFactory } from './zrxService'
@@ -19,7 +29,7 @@ export type FetchFromZrxArgs<T extends 'price' | 'quote'> = {
   sellAmountIncludingProtocolFeesCryptoBaseUnit: string
   receiveAddress: string
   affiliateBps: string
-  slippageTolerancePercentageDecimal?: string
+  slippageTolerancePercentageDecimal: string
 }
 
 export const fetchFromZrx = async <T extends 'price' | 'quote'>({
@@ -62,6 +72,54 @@ export const fetchFromZrx = async <T extends 'price' | 'quote'>({
         feeRecipientTradeSurplus: maybeTreasuryAddress, // Where trade surplus is sent
         buyTokenPercentageFee: convertBasisPointsToDecimalPercentage(affiliateBps).toNumber(),
       }),
+    },
+  })
+
+  if (maybeZrxPriceResponse.isErr()) {
+    return Err(maybeZrxPriceResponse.unwrapErr())
+  }
+
+  return Ok(maybeZrxPriceResponse.unwrap().data)
+}
+
+export const fetchFromZrxPermit2 = async <T extends 'price' | 'quote'>({
+  priceOrQuote,
+  buyAsset,
+  sellAsset,
+  sellAmountIncludingProtocolFeesCryptoBaseUnit,
+  receiveAddress,
+  affiliateBps,
+  slippageTolerancePercentageDecimal,
+}: FetchFromZrxArgs<T>): Promise<
+  Result<T extends 'quote' ? ZrxPermit2QuoteResponse : ZrxPermit2PriceResponse, SwapErrorRight>
+> => {
+  const baseUrl = baseUrlFromChainId(buyAsset.chainId as ZrxSupportedChainId)
+  const zrxService = zrxServiceFactory({ baseUrl })
+
+  const maybeTreasuryAddress = (() => {
+    try {
+      return getTreasuryAddressFromChainId(buyAsset.chainId)
+    } catch (err) {}
+  })()
+
+  // https://0x.org/docs/api#tag/Swap/operation/swap::permit2::getPrice
+  // https://0x.org/docs/api#tag/Swap/operation/swap::permit2::getQuote
+  const maybeZrxPriceResponse = await zrxService.get<
+    T extends 'quote' ? ZrxPermit2QuoteResponse : ZrxPermit2PriceResponse
+  >(`/swap/permit2/${priceOrQuote}`, {
+    params: {
+      chainId: viemNetworkIdByChainId[sellAsset.chainId],
+      buyToken: assetToToken(buyAsset),
+      sellToken: assetToToken(sellAsset),
+      sellAmount: sellAmountIncludingProtocolFeesCryptoBaseUnit,
+      taker: receiveAddress,
+      swapFeeBps: parseInt(affiliateBps),
+      swapFeeToken: assetToToken(buyAsset), // must be set to the buy asset to simplify fee calcs
+      slippageBps: convertDecimalPercentageToBasisPoints(
+        slippageTolerancePercentageDecimal,
+      ).toNumber(),
+      feeRecipient: maybeTreasuryAddress, // Where affiliate fees are sent
+      feeRecipientTradeSurplus: maybeTreasuryAddress, // Where trade surplus is sent
     },
   })
 

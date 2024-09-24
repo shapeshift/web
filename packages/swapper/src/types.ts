3b3851b9a45d77006d1cb9cb9097c8c2671305e9
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2024-09-25 09:52:23 +1000

    feat: plumb in permit2 signature to trade execution

--- packages/swapper/src/types.ts
+++ packages/swapper/src/types.ts
@@ -286,7 +286,7 @@ export type CommonGetUnsignedTransactionArgs = {
 export type GetUnsignedEvmTransactionArgs = CommonGetUnsignedTransactionArgs &
   EvmAccountMetadata &
   Omit<EvmSwapperDeps, 'fetchIsSmartContractAddressQuery'> & {
-    permit2Signature?: string
+    permit2Signature: string | undefined
     supportsEIP1559: boolean
   }
 
@@ -402,7 +402,7 @@ export type CommonTradeExecutionInput = {
 
 export type EvmTransactionExecutionInput = CommonTradeExecutionInput &
   EvmTransactionExecutionProps &
-  EvmAccountMetadata & { supportsEIP1559: boolean }
+  EvmAccountMetadata & { supportsEIP1559: boolean; permit2Signature: string | undefined }
 
 export type EvmMessageExecutionInput = CommonTradeExecutionInput &
   EvmMessageExecutionProps &

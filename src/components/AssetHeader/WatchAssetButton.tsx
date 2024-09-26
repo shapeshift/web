aaceacacadac27cf9a1e572fc1046080727b95f8
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2024-09-26 12:03:21 -0600

    fix nested button html error

--- src/components/AssetHeader/WatchAssetButton.tsx
+++ src/components/AssetHeader/WatchAssetButton.tsx
@@ -1,5 +1,5 @@
-import type { IconButtonProps } from '@chakra-ui/react'
-import { IconButton } from '@chakra-ui/react'
+import type { BoxProps } from '@chakra-ui/react'
+import { Box, Icon } from '@chakra-ui/react'
 import type { AssetId } from '@shapeshiftoss/caip'
 import { useCallback } from 'react'
 import { FaRegStar, FaStar } from 'react-icons/fa'
@@ -7,16 +7,14 @@ import { preferences } from 'state/slices/preferencesSlice/preferencesSlice'
 import { selectIsAssetIdWatched } from 'state/slices/selectors'
 import { useAppDispatch, useAppSelector } from 'state/store'
 
-const starEmpty = <FaRegStar />
-const starFilled = <FaStar />
-
-type WatchAssetButtonProps = Partial<IconButtonProps> & {
+type WatchAssetButtonProps = Partial<BoxProps> & {
   assetId: AssetId
 }
 
 export const WatchAssetButton: React.FC<WatchAssetButtonProps> = ({ assetId, ...props }) => {
   const appDispatch = useAppDispatch()
   const isAssetIdWatched = useAppSelector(state => selectIsAssetIdWatched(state, assetId))
+
   const handleToggleWatchAsset: React.MouseEventHandler<HTMLButtonElement> = useCallback(
     e => {
       e.stopPropagation()
@@ -24,15 +22,25 @@ export const WatchAssetButton: React.FC<WatchAssetButtonProps> = ({ assetId, ...
     },
     [appDispatch, assetId],
   )
+
   return (
-    <IconButton
-      onClick={handleToggleWatchAsset}
-      icon={isAssetIdWatched ? starFilled : starEmpty}
-      isRound
-      size='sm'
+    <Box
+      as='span'
+      display='inline-flex'
+      justifyContent='center'
+      alignItems='center'
+      minWidth='auto'
+      borderRadius='full'
+      bg='var(--chakra-colors-background-button-secondary-base)'
+      // eslint-disable-next-line react-memo/require-usememo
+      _hover={{ bg: 'var(--chakra-colors-background-button-secondary-hover)' }}
+      p={2}
       ml={2}
       aria-label='favorite asset'
+      onClick={handleToggleWatchAsset}
       {...props}
-    />
+    >
+      <Icon as={isAssetIdWatched ? FaStar : FaRegStar} />
+    </Box>
   )
 }

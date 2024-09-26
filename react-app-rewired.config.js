33dc46f78f89c64b30117d336d3c5630fd4f50b5
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2024-09-26 15:46:35 +1000

    chore: add webpack-bundle-analyzer

--- react-app-rewired.config.js
+++ react-app-rewired.config.js
@@ -1,4 +1,6 @@
 const tsNode = require('ts-node')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+
 tsNode.register({
   extends: './tsconfig.web.json',
   include: ['src'],
@@ -8,4 +10,22 @@ tsNode.register({
   },
 })
 
-module.exports = require('./react-app-rewired').default
+const rewiredConfig = require('./react-app-rewired').default
+
+module.exports = function override(config, env) {
+  // Apply the existing rewired configuration
+  config = rewiredConfig(config, env)
+
+  // Add the BundleAnalyzerPlugin if ANALYZE is true
+  if (process.env.ANALYZE === 'true') {
+    config.plugins.push(
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'server',
+        analyzerPort: 8888,
+        openAnalyzer: true,
+      }),
+    )
+  }
+
+  return config
+}

b9b69b800380611372e3c2b92458952d9c5961b7
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2024-09-26 15:56:36 +1000

    chore: fix bundle analyze config

--- react-app-rewired.config.js
+++ react-app-rewired.config.js
@@ -1,5 +1,4 @@
 const tsNode = require('ts-node')
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 tsNode.register({
   extends: './tsconfig.web.json',
@@ -10,22 +9,4 @@ tsNode.register({
   },
 })
 
-const rewiredConfig = require('./react-app-rewired').default
-
-module.exports = function override(config, env) {
-  // Apply the existing rewired configuration
-  config = rewiredConfig(config, env)
-
-  // Add the BundleAnalyzerPlugin if ANALYZE is true
-  if (process.env.ANALYZE === 'true') {
-    config.plugins.push(
-      new BundleAnalyzerPlugin({
-        analyzerMode: 'server',
-        analyzerPort: 8888,
-        openAnalyzer: true,
-      }),
-    )
-  }
-
-  return config
-}
+module.exports = require('./react-app-rewired').default

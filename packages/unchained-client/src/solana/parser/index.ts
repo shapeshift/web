4ab1c60423c8a42b1a79db32cca9269a670e038d
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2024-10-14 08:56:14 -0600

    comment

--- packages/unchained-client/src/solana/parser/index.ts
+++ packages/unchained-client/src/solana/parser/index.ts
@@ -48,6 +48,7 @@ export class TransactionParser<T extends Tx> {
       blockHeight: tx.blockHeight,
       blockTime: tx.timestamp,
       chainId: this.chainId,
+      // all transactions from unchained are finalized with at least 1 confirmation (unused throughout web)
       confirmations: 1,
       status: this.getStatus(tx),
       trade: parserResult?.trade,

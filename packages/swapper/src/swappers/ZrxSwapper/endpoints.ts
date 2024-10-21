486607c07f6c97827d6a2f9ccffab918abc496b5
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-10-21 20:59:25 +0200

    [skip ci] feat: use isExecutableTradeQuote

--- packages/swapper/src/swappers/ZrxSwapper/endpoints.ts
+++ packages/swapper/src/swappers/ZrxSwapper/endpoints.ts
@@ -13,7 +13,7 @@ import type {
   SwapperDeps,
   TradeQuote,
 } from '../../types'
-import { checkEvmSwapStatus } from '../../utils'
+import { checkEvmSwapStatus, isExecutableTradeQuote } from '../../utils'
 import { getZrxTradeQuote } from './getZrxTradeQuote/getZrxTradeQuote'
 import { fetchFromZrx } from './utils/fetchFromZrx'
 
@@ -39,10 +39,10 @@ export const zrxApi: SwapperApi = {
     supportsEIP1559,
     assertGetEvmChainAdapter,
   }: GetUnsignedEvmTransactionArgs): Promise<EvmTransactionRequest> => {
+    if (!isExecutableTradeQuote(tradeQuote)) throw new Error('Cannot execute a trade rate')
+
     const { affiliateBps, receiveAddress, slippageTolerancePercentageDecimal, steps } = tradeQuote
     const { buyAsset, sellAsset, sellAmountIncludingProtocolFeesCryptoBaseUnit } = steps[0]
-
-    if (!receiveAddress) throw new Error('receiveAddress is required for ZrxSwapper quotes')
 
     // We need to re-fetch the quote from 0x here because actual quote fetches include validation of
     // approvals, which prevent quotes during trade input from succeeding if the user hasn't already

dfa6a3a1f8440927704a8246c45fad1360f13aa4
David Cumps <david@cumps.be>
2024-10-24 01:00:51 +0200

    refactor: delete unneeded generated openai models

--- packages/swapper/src/swappers/ChainflipSwapper/models/index.ts
+++ packages/swapper/src/swappers/ChainflipSwapper/models/index.ts
@@ -1,18 +1,10 @@
-// @ts-nocheck
 /* tslint:disable */
 /* eslint-disable */
-export * from './ChainflipBaasAssetsAssetInfo';
-export * from './ChainflipBaasAssetsAssetsResponse';
-export * from './ChainflipBaasBalanceBalanceResponse';
-export * from './ChainflipBaasFeesFeesResponse';
-export * from './ChainflipBaasNetworksNetworkInfo';
-export * from './ChainflipBaasNetworksNetworksResponse';
 export * from './ChainflipBaasQuoteBoostQuote';
 export * from './ChainflipBaasQuotePoolFee';
 export * from './ChainflipBaasQuotePoolInfo';
 export * from './ChainflipBaasQuoteQuote';
 export * from './ChainflipBaasQuoteQuoteFee';
-export * from './ChainflipBaasRpcRpcRequest';
 export * from './ChainflipBaasStatusAffiliateBroker';
 export * from './ChainflipBaasStatusBoost';
 export * from './ChainflipBaasStatusCcmParameters';
@@ -29,8 +21,4 @@ export * from './ChainflipBaasStatusReason';
 export * from './ChainflipBaasStatusStatusResponse';
 export * from './ChainflipBaasStatusSwap';
 export * from './ChainflipBaasStatusSwapStatus';
-export * from './ChainflipBaasSwapAdvancedSwapRequest';
-export * from './ChainflipBaasSwapCcmRequest';
 export * from './ChainflipBaasSwapDepositAddress';
-export * from './MicrosoftAspNetCoreMvcProblemDetails';
-export * from './MicrosoftAspNetCoreMvcValidationProblemDetails';

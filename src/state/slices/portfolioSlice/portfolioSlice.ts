b8462185f8f3b67a3c7dbc4357800f21b09457d2
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-10-25 19:01:19 +0200

    feat: revert of the revert of "fix: swapper transition states (#7917)" (#8002)

--- src/state/slices/portfolioSlice/portfolioSlice.ts
+++ src/state/slices/portfolioSlice/portfolioSlice.ts
@@ -32,9 +32,6 @@ export const portfolio = createSlice({
     clear: () => {
       return initialState
     },
-    setIsAccountsMetadataLoading: (state, { payload }: { payload: boolean }) => {
-      state.isAccountsMetadataLoading = payload
-    },
     setIsAccountMetadataLoading: (
       state,
       { payload }: { payload: { accountId: AccountId; isLoading: boolean } },

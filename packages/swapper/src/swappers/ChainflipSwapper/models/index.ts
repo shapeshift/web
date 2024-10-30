206a337e6b7dd20ddd7b2766ce976b7bd6e46dfc
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-10-31 08:52:47 +0900

    feat: cleanup

--- packages/swapper/src/swappers/ChainflipSwapper/models/index.ts
+++ packages/swapper/src/swappers/ChainflipSwapper/models/index.ts
@@ -1,24 +1,3 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './ChainflipBaasQuoteBoostQuote';
-export * from './ChainflipBaasQuotePoolFee';
-export * from './ChainflipBaasQuotePoolInfo';
-export * from './ChainflipBaasQuoteQuote';
-export * from './ChainflipBaasQuoteQuoteFee';
-export * from './ChainflipBaasStatusAffiliateBroker';
-export * from './ChainflipBaasStatusBoost';
-export * from './ChainflipBaasStatusCcmParameters';
-export * from './ChainflipBaasStatusChunkInfo';
-export * from './ChainflipBaasStatusDca';
-export * from './ChainflipBaasStatusDcaParameters';
-export * from './ChainflipBaasStatusDeposit';
-export * from './ChainflipBaasStatusDepositChannel';
-export * from './ChainflipBaasStatusEgress';
-export * from './ChainflipBaasStatusFailure';
-export * from './ChainflipBaasStatusFillOrKillParameters';
-export * from './ChainflipBaasStatusPaidFee';
-export * from './ChainflipBaasStatusReason';
-export * from './ChainflipBaasStatusStatusResponse';
-export * from './ChainflipBaasStatusSwap';
-export * from './ChainflipBaasStatusSwapStatus';
-export * from './ChainflipBaasSwapDepositAddress';
+export * from './ChainflipBaasQuoteQuote'
+export * from './ChainflipBaasQuoteQuoteFee'
+export * from './ChainflipBaasSwapDepositAddress'

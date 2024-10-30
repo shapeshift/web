0886d190934d75fae0b28cdcc3b7093db883c2d9
kevin <35275952+kaladinlight@users.noreply.github.com>
2024-10-30 16:14:57 -0600

    feat: remove solana from swapper asset search (#8039)

--- src/components/TradeAssetSearch/components/SearchTermAssetList.tsx
+++ src/components/TradeAssetSearch/components/SearchTermAssetList.tsx
@@ -1,6 +1,6 @@
 import { ASSET_NAMESPACE, bscChainId, type ChainId, toAssetId } from '@shapeshiftoss/caip'
 import { isEvmChainId } from '@shapeshiftoss/chain-adapters'
-import type { Asset } from '@shapeshiftoss/types'
+import { type Asset, KnownChainIds } from '@shapeshiftoss/types'
 import { bnOrZero, makeAsset, type MinimalAsset } from '@shapeshiftoss/utils'
 import { orderBy } from 'lodash'
 import { useMemo } from 'react'
@@ -23,6 +23,7 @@ export type SearchTermAssetListProps = {
   activeChainId: ChainId | 'All'
   searchString: string
   allowWalletUnsupportedAssets: boolean | undefined
+  isSwapper?: boolean
   onAssetClick: (asset: Asset) => void
   onImportClick: (asset: Asset) => void
 }
@@ -32,6 +33,7 @@ export const SearchTermAssetList = ({
   activeChainId,
   searchString,
   allowWalletUnsupportedAssets,
+  isSwapper,
   onAssetClick: handleAssetClick,
   onImportClick,
 }: SearchTermAssetListProps) => {
@@ -56,15 +58,18 @@ export const SearchTermAssetList = ({
 
   const assetsForChain = useMemo(() => {
     if (activeChainId === 'All') {
-      if (allowWalletUnsupportedAssets) return assets
-      return assets.filter(asset => walletConnectedChainIds.includes(asset.chainId))
+      const _assets = assets.filter(asset =>
+        isSwapper ? asset.chainId !== KnownChainIds.SolanaMainnet : true,
+      )
+      if (allowWalletUnsupportedAssets) return _assets
+      return _assets.filter(asset => walletConnectedChainIds.includes(asset.chainId))
     }
 
     // Should never happen, but paranoia.
     if (!allowWalletUnsupportedAssets && !walletConnectedChainIds.includes(activeChainId)) return []
 
     return assets.filter(asset => asset.chainId === activeChainId)
-  }, [activeChainId, allowWalletUnsupportedAssets, assets, walletConnectedChainIds])
+  }, [activeChainId, allowWalletUnsupportedAssets, assets, walletConnectedChainIds, isSwapper])
 
   const customAssets: Asset[] = useMemo(
     () =>

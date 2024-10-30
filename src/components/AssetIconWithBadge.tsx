a2a5596bc27e6ffb8c1c3c1c4defa54cd1d915a5
Apotheosis <0xapotheosis@gmail.com>
2024-10-30 14:11:47 +1100

    refactor: component AssetIconWithBadge (#8040)

--- src/components/AssetIconWithBadge.tsx
+++ src/components/AssetIconWithBadge.tsx
@@ -4,7 +4,7 @@ import type { AssetId } from '@shapeshiftoss/caip'
 import type { TransferType } from '@shapeshiftoss/unchained-client'
 import uniqBy from 'lodash/uniqBy'
 import type { PropsWithChildren } from 'react'
-import React, { useMemo } from 'react'
+import { useMemo } from 'react'
 import { LuGlobe } from 'react-icons/lu'
 import FlipShadow from 'assets/flip-shadow.svg'
 import { type Transfer } from 'hooks/useTxDetails/useTxDetails'
@@ -17,14 +17,81 @@ import {
   topClipPath,
 } from './AssetIcon'
 
-const StatusStyle = { svg: { transform: 'scale(0.8)' } }
-
 type AssetIconWithBadgeProps = {
   size?: AvatarProps['size']
   assetId?: AssetId
   transfersByType?: Record<TransferType, Transfer[]>
   type?: string
 } & PropsWithChildren
+
+const sxProps = { svg: { transform: 'scale(0.8)' } }
+
+const useTransferData = (transfersByType?: Record<TransferType, Transfer[]>) => {
+  const transfers = useMemo(
+    () => (transfersByType ? uniqBy(Object.values(transfersByType).flat(), 'assetId') : []),
+    [transfersByType],
+  )
+
+  const isNft = useMemo(
+    () =>
+      Boolean(
+        transfersByType &&
+          Object.values(transfersByType)
+            .flat()
+            .some(transfer => !!transfer.id),
+      ),
+    [transfersByType],
+  )
+
+  return { transfers, isNft }
+}
+
+const getClipPath = (index: number, totalTransfers: number, isNft: boolean) => {
+  if (isNft) return squareClipPath
+  if (totalTransfers === 1) return defaultClipPath
+  if (index === 1) return bottomClipPath
+  return topClipPath
+}
+
+const TransferIcon: React.FC<{
+  transfer: Transfer
+  index: number
+  totalTransfers: number
+  isNft: boolean
+  size?: AvatarProps['size']
+}> = ({ transfer, index, totalTransfers, isNft, size }) => {
+  const isSecondTransfer = index === 1
+  const shouldShowShadow = isSecondTransfer && totalTransfers > 1
+
+  if (totalTransfers > 2 && index === 2) return null // Skip third+ transfers
+
+  return (
+    <Box>
+      {shouldShowShadow && (
+        <Image
+          src={FlipShadow}
+          position='absolute'
+          width='100%'
+          height='100%'
+          left={0}
+          top={0}
+          zIndex={2}
+        />
+      )}
+      <AssetIcon
+        showNetworkIcon={false}
+        assetId={transfer.assetId}
+        clipPath={getClipPath(index, totalTransfers, isNft)}
+        name={transfer.asset.name}
+        size={size}
+        position={shouldShowShadow ? 'absolute' : 'static'}
+        borderRadius={isNft ? 0 : 'full'}
+        left={0}
+        top={0}
+      />
+    </Box>
+  )
+}
 
 export const AssetIconWithBadge: React.FC<AssetIconWithBadgeProps> = ({
   size,
@@ -33,67 +100,10 @@ export const AssetIconWithBadge: React.FC<AssetIconWithBadgeProps> = ({
   type,
   children,
 }) => {
-  const isNft = useMemo(() => {
-    return Boolean(
-      transfersByType &&
-        Object.values(transfersByType)
-          .flat()
-          .some(transfer => !!transfer.id),
-    )
-  }, [transfersByType])
-  const WebIcon = useMemo(() => <LuGlobe />, [])
-  const transfers = useMemo(
-    () => (transfersByType ? uniqBy(Object.values(transfersByType).flat(), 'assetId') : []),
-    [transfersByType],
-  )
+  const { transfers, isNft } = useTransferData(transfersByType)
+  const webIcon = <LuGlobe />
 
-  const renderIcons = useMemo(() => {
-    if (!transfers) return null
-
-    return transfers.map((transfer, index) => {
-      const compareIndex = index + 1
-      let overideClipPath
-      if (isNft) {
-        overideClipPath = squareClipPath
-      }
-      if (transfers.length > 1) {
-        overideClipPath = topClipPath
-      }
-      if (compareIndex === 2) {
-        overideClipPath = bottomClipPath
-      }
-      if (transfers.length > 2 && compareIndex === 3) return null
-      return (
-        <Box key={index}>
-          {compareIndex === 2 && (
-            <Image
-              src={FlipShadow}
-              position='absolute'
-              width='100%'
-              height='100%'
-              left={0}
-              top={0}
-              zIndex={2}
-            />
-          )}
-
-          <AssetIcon
-            showNetworkIcon={false}
-            key={transfer.assetId}
-            assetId={transfer.assetId}
-            clipPath={overideClipPath ? overideClipPath : defaultClipPath}
-            name={transfer.asset.name}
-            size={size}
-            position={compareIndex === 2 ? 'absolute' : 'static'}
-            borderRadius={isNft ? 0 : 'full'}
-            left={0}
-            top={0}
-          />
-        </Box>
-      )
-    })
-  }, [isNft, size, transfers])
-  const renderIcon = useMemo(() => {
+  const renderContent = () => {
     if (assetId) {
       return (
         <AssetIcon
@@ -104,28 +114,31 @@ export const AssetIconWithBadge: React.FC<AssetIconWithBadgeProps> = ({
         />
       )
     }
+
     if (type === '' && transfers.length === 0) {
       return (
-        <AssetIcon size={size} icon={WebIcon} clipPath={defaultClipPath} color='text.subtlest' />
+        <AssetIcon size={size} icon={webIcon} clipPath={defaultClipPath} color='text.subtlest' />
       )
     }
-    return renderIcons
-  }, [WebIcon, assetId, renderIcons, size, transfers.length, type])
+
+    return transfers.map((transfer, index) => (
+      <TransferIcon
+        key={transfer.assetId}
+        transfer={transfer}
+        index={index}
+        totalTransfers={transfers.length}
+        isNft={isNft}
+        size={size}
+      />
+    ))
+  }
+
   return (
-    <>
-      <Center position='relative'>
-        <Center
-          position='absolute'
-          left='-8%'
-          top='-8%'
-          boxSize='40%'
-          sx={StatusStyle}
-          color='white'
-        >
-          {children}
-        </Center>
-        {renderIcon}
+    <Center position='relative'>
+      <Center position='absolute' left='-8%' top='-8%' boxSize='40%' sx={sxProps} color='white'>
+        {children}
       </Center>
-    </>
+      {renderContent()}
+    </Center>
   )
 }

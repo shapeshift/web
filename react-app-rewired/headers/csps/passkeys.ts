f982b2733c24eddf77638915426d4de8138393eb
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-11-05 23:00:01 +0900

    fix: csp

--- react-app-rewired/headers/csps/passkeys.ts
+++ react-app-rewired/headers/csps/passkeys.ts
@@ -3,4 +3,5 @@ import type { Csp } from '../types'
 export const csp: Csp = {
   'connect-src': ['https://embedded.passkeys.foundation/'],
   'frame-src': ['https://embedded.passkeys.foundation/'],
+  'frame-ancestors': ['https://embedded.passkeys.foundation/'],
 }

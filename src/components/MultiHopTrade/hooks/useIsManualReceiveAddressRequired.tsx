2790f9fd59b7be5a3e61b3accea03ddc99320b3d
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2024-11-07 08:43:34 +1100

    fix: use runtime support check when deciding whether to show manual address entry

--- src/components/MultiHopTrade/hooks/useIsManualReceiveAddressRequired.tsx
+++ src/components/MultiHopTrade/hooks/useIsManualReceiveAddressRequired.tsx
@@ -1,10 +1,12 @@
 import type { AccountId } from '@shapeshiftoss/caip'
-import { isLedger } from '@shapeshiftoss/hdwallet-ledger'
 import type { Asset } from '@shapeshiftoss/types'
 import { useMemo } from 'react'
 import { useAccountsFetchQuery } from 'context/AppProvider/hooks/useAccountsFetchQuery'
 import { useWallet } from 'hooks/useWallet/useWallet'
-import { useWalletSupportsChain } from 'hooks/useWalletSupportsChain/useWalletSupportsChain'
+import {
+  useWalletSupportsChain,
+  useWalletSupportsChainAtRuntime,
+} from 'hooks/useWalletSupportsChain/useWalletSupportsChain'
 import { selectAccountIdsByAssetId } from 'state/slices/selectors'
 import { useAppSelector } from 'state/store'
 
@@ -35,6 +37,10 @@ export const useIsManualReceiveAddressRequired = ({
 
   const { isFetching: isAccountsMetadataLoading } = useAccountsFetchQuery()
   const walletSupportsBuyAssetChain = useWalletSupportsChain(buyAsset.chainId, wallet)
+  const walletSupportsBuyAssetChainAtRuntime = useWalletSupportsChainAtRuntime(
+    buyAsset.chainId,
+    wallet,
+  )
 
   const forceDisplayManualAddressEntry = useMemo(() => {
     if (isWalletReceiveAddressLoading) return false
@@ -43,23 +49,24 @@ export const useIsManualReceiveAddressRequired = ({
     if (manualReceiveAddress) return false
     if (!walletReceiveAddress) return true
     if (!walletSupportsBuyAssetChain) return true
-    // Ledger "supports" all chains, but may not have them connected
-    if (wallet && isLedger(wallet)) return !buyAssetAccountIds.length
-    // We want to display the manual address entry if the wallet doesn't support the buy asset chain
+    // If the wallet supports the chian at runtime, display the manual address entry if there are no
+    // accounts connected for the buy asset
+    if (walletSupportsBuyAssetChainAtRuntime) return !buyAssetAccountIds.length
+    // If the parent wants to force the manual address entry to be displayed, do it
     if (shouldForceManualAddressEntry) return true
 
     return false
   }, [
-    isConnected,
+    buyAssetAccountIds.length,
     isAccountsMetadataLoading,
-    sellAccountId,
+    isConnected,
+    isWalletReceiveAddressLoading,
     manualReceiveAddress,
+    sellAccountId,
+    shouldForceManualAddressEntry,
     walletReceiveAddress,
     walletSupportsBuyAssetChain,
-    wallet,
-    buyAssetAccountIds.length,
-    shouldForceManualAddressEntry,
-    isWalletReceiveAddressLoading,
+    walletSupportsBuyAssetChainAtRuntime,
   ])
 
   return forceDisplayManualAddressEntry

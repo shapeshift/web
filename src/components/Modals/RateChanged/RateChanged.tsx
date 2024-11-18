21502945042a372c202c5e715b8b824abf4bf353
reallybeard <89934888+reallybeard@users.noreply.github.com>
2024-11-18 01:56:04 -0600

    rate expired modal

--- src/components/Modals/RateChanged/RateChanged.tsx
+++ src/components/Modals/RateChanged/RateChanged.tsx
@@ -1,14 +1,40 @@
-import { Modal, ModalContent, ModalOverlay } from '@chakra-ui/react'
+import { WarningIcon } from '@chakra-ui/icons'
+import {
+  Button,
+  Heading,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalOverlay,
+  Stack,
+} from '@chakra-ui/react'
+import { useTranslate } from 'react-polyglot'
+import { Text } from 'components/Text'
 import { useModal } from 'hooks/useModal/useModal'
 
 export const RateChangedModal = () => {
   const rateChanged = useModal('rateChanged')
   const { close, isOpen } = rateChanged
+  const translate = useTranslate()
 
   return (
     <Modal isOpen={isOpen} onClose={close} isCentered size='sm'>
       <ModalOverlay />
-      <ModalContent>Rate changed - TODO @reallybeard oil</ModalContent>
+      <ModalContent>
+        <ModalBody display='flex' alignItems='center' py={16} flexDir='column' gap={6}>
+          <WarningIcon color='text.warning' boxSize={8} />
+          <Stack textAlign='center'>
+            <Heading size='sm'>{translate('trade.rates.rateExpired.title')}</Heading>
+            <Text translation='trade.rates.rateExpired.body' color='text.subtle' />
+          </Stack>
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={close} colorScheme='blue' width='full'>
+            {translate('trade.rates.rateExpired.cta')}
+          </Button>
+        </ModalFooter>
+      </ModalContent>
     </Modal>
   )
 }

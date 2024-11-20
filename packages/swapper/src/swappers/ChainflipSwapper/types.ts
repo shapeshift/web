9c179ff189174e31f2c2d060906b6f0c6a6ee16d
David Cumps <david@cumps.be>
2024-11-20 19:48:54 +0100

    feat: add swap status to chainflipStatus type

--- packages/swapper/src/swappers/ChainflipSwapper/types.ts
+++ packages/swapper/src/swappers/ChainflipSwapper/types.ts
@@ -1,11 +1,12 @@
 import type { ChainflipBaasStatusEgress } from './models/ChainflipBaasStatusEgress'
+import type { ChainflipBaasStatusSwap } from './models/ChainflipBaasStatusSwap'
 
 // Non-exhaustive
 export type ChainFlipStatus = {
   status: {
-    // TODO(gomes): Status polling a la THOR
     state: 'waiting' | 'receiving' | 'swapping' | 'sending' | 'sent' | 'completed' | 'failed'
-    swapEgress?: ChainflipBaasStatusEgress
+    swap?: ChainflipBaasStatusSwap,
+    swapEgress?: ChainflipBaasStatusEgress,
   }
 }
 

e356a3b526b148b6063f5a9918a819cf472da203
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2024-11-22 11:01:44 +0800

    chore: additional confirm details wiring

--- src/components/MultiHopTrade/components/TradeConfirm/TradeConfirm.tsx
+++ src/components/MultiHopTrade/components/TradeConfirm/TradeConfirm.tsx
@@ -1,67 +1,69 @@
 import { ExternalLinkIcon } from '@chakra-ui/icons'
-import {
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  Heading,
-  HStack,
-  Icon,
-  Stack,
-} from '@chakra-ui/react'
-import { useCallback, useEffect, useMemo } from 'react'
-import { useHistory } from 'react-router'
-import { TradeSlideTransition } from 'components/MultiHopTrade/TradeSlideTransition'
-import { TradeRoutePaths } from 'components/MultiHopTrade/types'
+import { HStack, Icon, Stack, useColorModeValue } from '@chakra-ui/react'
+import type { AmountDisplayMeta } from '@shapeshiftoss/swapper'
+import { bnOrZero, fromBaseUnit, isSome } from '@shapeshiftoss/utils'
+import { useCallback, useMemo } from 'react'
+import { Amount } from 'components/Amount/Amount'
+import { usePriceImpact } from 'components/MultiHopTrade/hooks/quoteValidation/usePriceImpact'
 import { Row } from 'components/Row/Row'
 import { RawText, Text } from 'components/Text'
+import { getChainAdapterManager } from 'context/PluginProvider/chainAdapterSingleton'
+import { selectInputBuyAsset } from 'state/slices/tradeInputSlice/selectors'
 import {
   selectActiveQuote,
   selectActiveSwapperName,
-  selectConfirmedTradeExecutionState,
+  selectBuyAmountAfterFeesCryptoPrecision,
   selectFirstHop,
+  selectTotalProtocolFeeByAsset,
+  selectTradeSlippagePercentageDecimal,
 } from 'state/slices/tradeQuoteSlice/selectors'
-import { tradeQuoteSlice } from 'state/slices/tradeQuoteSlice/tradeQuoteSlice'
-import { TradeExecutionState } from 'state/slices/tradeQuoteSlice/types'
-import { useAppDispatch, useAppSelector } from 'state/store'
+import { useAppSelector } from 'state/store'
 
-import { Footer as FooterButton } from '../MultiHopTradeConfirm/components/Footer'
+import { Footer as TradeFooterButton } from '../MultiHopTradeConfirm/components/Footer'
 import { useIsApprovalInitiallyNeeded } from '../MultiHopTradeConfirm/hooks/useIsApprovalInitiallyNeeded'
+import { PriceImpact } from '../PriceImpact'
+import { SharedConfirm } from '../SharedConfirm/SharedConfirm'
+import { MaxSlippage } from '../TradeInput/components/MaxSlippage'
 import { SwapperIcon } from '../TradeInput/components/SwapperIcon/SwapperIcon'
-import { WithBackButton } from '../WithBackButton'
 
-const cardBorderRadius = { base: 'xl' }
+const parseAmountDisplayMeta = (items: AmountDisplayMeta[]) => {
+  return items
+    .filter(({ amountCryptoBaseUnit }) => bnOrZero(amountCryptoBaseUnit).gt(0))
+    .map(({ amountCryptoBaseUnit, asset }: AmountDisplayMeta) => ({
+      symbol: asset.symbol,
+      chainName: getChainAdapterManager().get(asset.chainId)?.getDisplayName(),
+      amountCryptoPrecision: fromBaseUnit(amountCryptoBaseUnit, asset.precision),
+    }))
+}
+
+const ProtocolFeeToolTip = () => {
+  return <Text color='text.subtle' translation={'trade.tooltip.protocolFee'} />
+}
 
 export const TradeConfirm = () => {
-  const dispatch = useAppDispatch()
-  //   const translate = useTranslate()
-  const history = useHistory()
-  const { isLoading } = useIsApprovalInitiallyNeeded()
-
-  const confirmedTradeExecutionState = useAppSelector(selectConfirmedTradeExecutionState)
-  const activeQuote = useAppSelector(selectActiveQuote)
   const swapperName = useAppSelector(selectActiveSwapperName)
+  const activeQuote = useAppSelector(selectActiveQuote)
   const tradeQuoteStep = useAppSelector(selectFirstHop)
+  const buyAsset = useAppSelector(selectInputBuyAsset)
+  const buyAmountAfterFeesCryptoPrecision = useAppSelector(selectBuyAmountAfterFeesCryptoPrecision)
+  const slippagePercentageDecimal = useAppSelector(selectTradeSlippagePercentageDecimal)
+  const totalProtocolFees = useAppSelector(selectTotalProtocolFeeByAsset)
+
+  const { priceImpactPercentage } = usePriceImpact(activeQuote)
+  const { isLoading } = useIsApprovalInitiallyNeeded()
+  const redColor = useColorModeValue('red.500', 'red.300')
+
   const swapSource = tradeQuoteStep?.source
-
-  const isTradeComplete = useMemo(
-    () => confirmedTradeExecutionState === TradeExecutionState.TradeComplete,
-    [confirmedTradeExecutionState],
-  )
-
-  useEffect(() => {
-    if (isLoading || !activeQuote) return
-
-    dispatch(tradeQuoteSlice.actions.setTradeInitialized(activeQuote.id))
-  }, [dispatch, isLoading, activeQuote])
-
-  const handleBack = useCallback(() => {
-    if (isTradeComplete) {
-      dispatch(tradeQuoteSlice.actions.clear())
-    }
-
-    history.push(TradeRoutePaths.Input)
-  }, [dispatch, history, isTradeComplete])
+  const intermediaryTransactionOutputs = tradeQuoteStep?.intermediaryTransactionOutputs
+  const intermediaryTransactionOutputsParsed = intermediaryTransactionOutputs
+    ? parseAmountDisplayMeta(intermediaryTransactionOutputs)
+    : undefined
+  const hasIntermediaryTransactionOutputs =
+    intermediaryTransactionOutputsParsed && intermediaryTransactionOutputsParsed.length > 0
+  const protocolFeesParsed = totalProtocolFees
+    ? parseAmountDisplayMeta(Object.values(totalProtocolFees).filter(isSome))
+    : undefined
+  const hasProtocolFees = protocolFeesParsed && protocolFeesParsed.length > 0
 
   const handleSubmit = useCallback(() => {
     console.log('submit')
@@ -86,21 +88,30 @@ export const TradeConfirm = () => {
 
         <Row>
           <Row.Label>
-            <Text translation='trade.rate' />
-          </Row.Label>
-          <Row.Value>
-            <RawText>1 METH = 0.0 ETH</RawText>
-          </Row.Value>
-        </Row>
-
-        <Row>
-          <Row.Label>
             <Text translation='trade.transactionFee' />
           </Row.Label>
           <Row.Value>
             <RawText>$0.00258</RawText>
           </Row.Value>
         </Row>
+
+        {hasProtocolFees && (
+          <Row Tooltipbody={ProtocolFeeToolTip} isLoading={isLoading}>
+            <Row.Label>
+              <Text translation='trade.protocolFee' />
+            </Row.Label>
+            <Row.Value color='text.base'>
+              {protocolFeesParsed?.map(({ amountCryptoPrecision, symbol }) => (
+                <Amount.Crypto
+                  key={`${amountCryptoPrecision}`}
+                  color={redColor}
+                  value={amountCryptoPrecision}
+                  symbol={symbol}
+                />
+              ))}
+            </Row.Value>
+          </Row>
+        )}
 
         <Row>
           <Row.Label>
@@ -125,48 +136,57 @@ export const TradeConfirm = () => {
             </HStack>
           </Row.Value>
         </Row>
+
+        {/* TODO: Hide the below unless expanded */}
+
+        <Row>
+          <Row.Label>
+            <Text translation='trade.rate' />
+          </Row.Label>
+          <Row.Value>
+            <RawText>1 METH = 0.0 ETH</RawText>
+          </Row.Value>
+        </Row>
+
+        <MaxSlippage
+          swapSource={tradeQuoteStep?.source}
+          isLoading={isLoading}
+          symbol={buyAsset.symbol}
+          amountCryptoPrecision={buyAmountAfterFeesCryptoPrecision ?? '0'}
+          slippagePercentageDecimal={slippagePercentageDecimal}
+          hasIntermediaryTransactionOutputs={hasIntermediaryTransactionOutputs}
+          intermediaryTransactionOutputs={intermediaryTransactionOutputs}
+        />
+
+        {priceImpactPercentage && <PriceImpact priceImpactPercentage={priceImpactPercentage} />}
       </Stack>
     )
-  }, [activeQuote, swapSource, swapperName])
+  }, [
+    activeQuote,
+    buyAmountAfterFeesCryptoPrecision,
+    buyAsset.symbol,
+    hasIntermediaryTransactionOutputs,
+    hasProtocolFees,
+    intermediaryTransactionOutputs,
+    isLoading,
+    priceImpactPercentage,
+    protocolFeesParsed,
+    redColor,
+    slippagePercentageDecimal,
+    swapSource,
+    swapperName,
+    tradeQuoteStep?.source,
+  ])
 
-  if (!confirmedTradeExecutionState) return null
+  const FooterButton = useMemo(() => {
+    return <TradeFooterButton isLoading={isLoading} handleSubmit={handleSubmit} />
+  }, [isLoading, handleSubmit])
 
   return (
-    <TradeSlideTransition>
-      {/* TODO: Add WarningAcknowledgement */}
-      <Card
-        flex={1}
-        borderRadius={cardBorderRadius}
-        width='full'
-        variant='dashboard'
-        maxWidth='500px'
-      >
-        <CardHeader px={6} pt={4}>
-          <WithBackButton onBack={handleBack}>
-            <Heading textAlign='center' fontSize='md'>
-              <Text
-                translation={
-                  [TradeExecutionState.Initializing, TradeExecutionState.Previewing].includes(
-                    confirmedTradeExecutionState,
-                  )
-                    ? 'trade.confirmDetails'
-                    : 'trade.trade'
-                }
-              />
-            </Heading>
-          </WithBackButton>
-        </CardHeader>
-        <CardBody py={0} px={0}>
-          gm
-        </CardBody>
-        <CardFooter bg='background.surface.overlay.base' borderBottomRadius='xl'>
-          <Stack width='full'>
-            {TradeDetailTable}
-            <FooterButton isLoading={isLoading} handleSubmit={handleSubmit} />
-          </Stack>
-        </CardFooter>
-      </Card>
-    </TradeSlideTransition>
-    // TODO: Add FeeModal
+    <SharedConfirm
+      ConfirmDetailTable={TradeDetailTable}
+      FooterButton={FooterButton}
+      isLoading={isLoading}
+    />
   )
 }

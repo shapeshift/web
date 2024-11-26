ab8494aaddfd45dda5f4bdb6ee303a0df12a89d9
witty <131909329+0xwitty@users.noreply.github.com>
2024-11-26 21:54:29 +0300

    Fix sed Compatibility Issue for Extended Regular Expressions
    Replaced sed -r with sed -E to ensure compatibility with macOS and other systems where the -r flag is not supported. This change prevents potential errors when running the script on different platforms and improves its portability.

--- scripts/sha256sums.sh
+++ scripts/sha256sums.sh
@@ -2,5 +2,5 @@
 
 rm -f ./build/SHA256SUMS
 TEMP=$(mktemp)
-find -L ./build -type f | xargs shasum -a 256 | sed -r 's/^([0-9a-f]{64}\s+)\.\/build\//\1/' | LC_ALL=C sort -k 2 | tee "$TEMP"
+find -L ./build -type f | xargs shasum -a 256 | sed -E 's/^([0-9a-f]{64}\s+)\.\/build\//\1/' | LC_ALL=C sort -k 2 | tee "$TEMP"
 mv "$TEMP" ./build/SHA256SUMS

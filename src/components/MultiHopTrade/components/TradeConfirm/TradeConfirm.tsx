a26a2df876d37c7adb6b1bbc39dac27546ebaae2
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2024-11-26 15:58:25 +1100

    chore: begin body impl, tweak footer

--- src/components/MultiHopTrade/components/TradeConfirm/TradeConfirm.tsx
+++ src/components/MultiHopTrade/components/TradeConfirm/TradeConfirm.tsx
@@ -2,16 +2,14 @@ import { useMemo } from 'react'
 
 import { useIsApprovalInitiallyNeeded } from '../MultiHopTradeConfirm/hooks/useIsApprovalInitiallyNeeded'
 import { SharedConfirm } from '../SharedConfirm/SharedConfirm'
+import { TradeConfirmBody } from './TradeConfirmBody'
 import { TradeConfirmFooter } from './TradeConfirmFooter'
 
 export const TradeConfirm = () => {
   const { isLoading } = useIsApprovalInitiallyNeeded()
 
-  const Body = useMemo(() => {
-    return <>{'gm'}</>
-  }, [])
-
   const Footer = useMemo(() => <TradeConfirmFooter />, [])
+  const Body = useMemo(() => <TradeConfirmBody />, [])
 
   return <SharedConfirm Body={Body} Footer={Footer} isLoading={isLoading} />
 }

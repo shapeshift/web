0111088de1cb9d0cf46275e380b7bd5a5403fdc5
NeOMakinG <14963751+NeOMakinG@users.noreply.github.com>
2024-11-26 11:27:12 +0100

    fix: split rate and quote

--- packages/swapper/src/swappers/JupiterSwapper/swapperApi/getTradeQuote.ts
+++ packages/swapper/src/swappers/JupiterSwapper/swapperApi/getTradeQuote.ts
@@ -30,12 +30,15 @@ import type {
 import { SwapperName, TradeQuoteError } from '../../../types'
 import { getRate, makeSwapErrorRight } from '../../../utils'
 import type { Instruction } from '../models/Instruction'
+import type { QuoteResponse } from '../models/QuoteResponse'
 import { getJupiterQuote, getJupiterSwapInstructions, isSupportedChainId } from '../utils/helpers'
 
-const _getTradeQuote = async (
-  input: CommonTradeQuoteInput,
+const tradeQuoteMetadata: Map<string, QuoteResponse> = new Map()
+
+export const getTradeRate = async (
+  input: GetTradeRateInput,
   deps: SwapperDeps,
-): Promise<Result<TradeQuote[], SwapErrorRight>> => {
+): Promise<Result<TradeRate[], SwapErrorRight>> => {
   const {
     sellAsset,
     buyAsset,
@@ -104,6 +107,177 @@ const _getTradeQuote = async (
 
   const { data: quoteResponse } = maybeQuoteResponse.unwrap()
 
+  tradeQuoteMetadata.set('rate', quoteResponse)
+
+  const getFeeData = async () => {
+    const { chainNamespace } = fromAssetId(sellAsset.assetId)
+
+    switch (chainNamespace) {
+      case CHAIN_NAMESPACE.Solana: {
+        const sellAdapter = deps.assertGetSolanaChainAdapter(sellAsset.chainId)
+        const getFeeDataInput: GetFeeDataInput<KnownChainIds.SolanaMainnet> = {
+          to: receiveAddress ?? sendAddress,
+          value: sellAmount,
+          chainSpecific: {
+            from: input.sendAddress!,
+            tokenId:
+              sellAsset.assetId === solAssetId
+                ? undefined
+                : fromAssetId(sellAsset.assetId).assetReference,
+          },
+        }
+        const { fast } = await sellAdapter.getFeeData(getFeeDataInput)
+        return { networkFeeCryptoBaseUnit: fast.txFee }
+      }
+
+      default:
+        throw new Error('Unsupported chainNamespace')
+    }
+  }
+
+  const protocolFees: Record<AssetId, ProtocolFee> = quoteResponse.routePlan.reduce(
+    (acc, route) => {
+      const feeAssetId = toAssetId({
+        assetReference: route.swapInfo.feeMint,
+        assetNamespace: ASSET_NAMESPACE.splToken,
+        chainNamespace: CHAIN_NAMESPACE.Solana,
+        chainReference: CHAIN_REFERENCE.SolanaMainnet,
+      })
+      const feeAsset = assetsById[feeAssetId]
+
+      // If we can't find the feeAsset, we can't provide a protocol fee to display
+      // But these fees exists at protocol level, it's mostly to make TS happy as we should have the market data and assets
+      if (!feeAsset) return acc
+
+      acc[feeAssetId] = {
+        requiresBalance: false,
+        amountCryptoBaseUnit: bnOrZero(route.swapInfo.feeAmount).toFixed(0),
+        asset: feeAsset,
+      }
+
+      return acc
+    },
+    {} as Record<AssetId, ProtocolFee>,
+  )
+
+  const getQuoteRate = (sellAmountCryptoBaseUnit: string, buyAmountCryptoBaseUnit: string) => {
+    return getRate({
+      sellAmountCryptoBaseUnit,
+      buyAmountCryptoBaseUnit,
+      sellAsset,
+      buyAsset,
+    })
+  }
+
+  const rates: TradeRate[] = []
+
+  const feeData = await getFeeData()
+
+  const rate = getQuoteRate(quoteResponse.inAmount, quoteResponse.outAmount)
+
+  const tradeRate: TradeRate = {
+    id: uuid(),
+    rate,
+    receiveAddress,
+    potentialAffiliateBps: affiliateBps,
+    affiliateBps,
+    accountNumber,
+    slippageTolerancePercentageDecimal:
+      slippageTolerancePercentageDecimal ??
+      getDefaultSlippageDecimalPercentageForSwapper(SwapperName.Jupiter),
+    steps: [
+      {
+        buyAmountBeforeFeesCryptoBaseUnit: quoteResponse.outAmount,
+        buyAmountAfterFeesCryptoBaseUnit: quoteResponse.outAmount,
+        sellAmountIncludingProtocolFeesCryptoBaseUnit: quoteResponse.inAmount,
+        jupiterQuoteResponse: quoteResponse,
+        feeData: {
+          protocolFees,
+          ...feeData,
+        },
+        rate,
+        source: SwapperName.Jupiter,
+        buyAsset,
+        sellAsset,
+        accountNumber,
+        allowanceContract: '0x0',
+        estimatedExecutionTimeMs: quoteResponse.timeTaken! * 1000,
+      },
+    ],
+  }
+
+  rates.push(tradeRate)
+
+  return Ok(rates)
+}
+
+export const getTradeQuote = async (
+  input: CommonTradeQuoteInput,
+  deps: SwapperDeps,
+): Promise<Result<TradeQuote[], SwapErrorRight>> => {
+  const {
+    sellAsset,
+    buyAsset,
+    affiliateBps,
+    receiveAddress,
+    accountNumber,
+    sendAddress,
+    slippageTolerancePercentageDecimal,
+  } = input
+
+  const { assetsById } = deps
+
+  const jupiterUrl = deps.config.REACT_APP_JUPITER_API_URL
+
+  if (accountNumber === undefined) {
+    return Err(
+      makeSwapErrorRight({
+        message: `accountNumber is required`,
+        code: TradeQuoteError.UnknownError,
+      }),
+    )
+  }
+
+  if (!isSupportedChainId(sellAsset.chainId)) {
+    return Err(
+      makeSwapErrorRight({
+        message: `unsupported chainId`,
+        code: TradeQuoteError.UnsupportedChain,
+        details: { chainId: sellAsset.chainId },
+      }),
+    )
+  }
+
+  if (!isSupportedChainId(buyAsset.chainId)) {
+    return Err(
+      makeSwapErrorRight({
+        message: `unsupported chainId`,
+        code: TradeQuoteError.UnsupportedChain,
+        details: { chainId: sellAsset.chainId },
+      }),
+    )
+  }
+
+  if (!sendAddress) {
+    return Err(
+      makeSwapErrorRight({
+        message: `sendAddress is required`,
+        code: TradeQuoteError.UnknownError,
+      }),
+    )
+  }
+
+  const quoteResponse = tradeQuoteMetadata.get('rate')
+
+  if (!quoteResponse) {
+    return Err(
+      makeSwapErrorRight({
+        message: `quote not found`,
+        code: TradeQuoteError.UnknownError,
+      }),
+    )
+  }
+
   const contractAddress =
     buyAsset.assetId === solAssetId ? undefined : fromAssetId(buyAsset.assetId).assetReference
 
@@ -115,7 +289,7 @@ const _getTradeQuote = async (
     contractAddress && isCrossAccountTrade
       ? await adapter.createAssociatedTokenAccountInstruction({
           from: sendAddress,
-          to: receiveAddress,
+          to: receiveAddress!,
           tokenId: contractAddress,
         })
       : { instruction: undefined, destinationTokenAccount: undefined }
@@ -227,14 +401,15 @@ const _getTradeQuote = async (
   const tradeQuote: TradeQuote = {
     id: uuid(),
     rate,
-    receiveAddress,
     potentialAffiliateBps: affiliateBps,
     affiliateBps,
+    receiveAddress,
     slippageTolerancePercentageDecimal:
       slippageTolerancePercentageDecimal ??
       getDefaultSlippageDecimalPercentageForSwapper(SwapperName.Jupiter),
     steps: [
       {
+        accountNumber,
         buyAmountBeforeFeesCryptoBaseUnit: quoteResponse.outAmount,
         buyAmountAfterFeesCryptoBaseUnit: quoteResponse.outAmount,
         sellAmountIncludingProtocolFeesCryptoBaseUnit: quoteResponse.inAmount,
@@ -252,7 +427,6 @@ const _getTradeQuote = async (
         source: SwapperName.Jupiter,
         buyAsset,
         sellAsset,
-        accountNumber,
         allowanceContract: '0x0',
         estimatedExecutionTimeMs: quoteResponse.timeTaken! * 1000,
       },
@@ -262,31 +436,4 @@ const _getTradeQuote = async (
   quotes.push(tradeQuote)
 
   return Ok(quotes)
-}
-
-export const getTradeRate = async (
-  input: GetTradeRateInput,
-  deps: SwapperDeps,
-): Promise<Result<TradeRate[], SwapErrorRight>> => {
-  const rates = await _getTradeQuote(input as unknown as CommonTradeQuoteInput, deps)
-  return rates as Result<TradeRate[], SwapErrorRight>
-}
-
-export const getTradeQuote = async (
-  input: CommonTradeQuoteInput,
-  deps: SwapperDeps,
-): Promise<Result<TradeQuote[], SwapErrorRight>> => {
-  const { accountNumber } = input
-
-  if (accountNumber === undefined) {
-    return Err(
-      makeSwapErrorRight({
-        message: `accountNumber is required`,
-        code: TradeQuoteError.UnknownError,
-      }),
-    )
-  }
-
-  const quotes = await _getTradeQuote(input, deps)
-  return quotes
 }

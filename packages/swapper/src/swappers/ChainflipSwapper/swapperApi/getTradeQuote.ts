11048614fac786752daaab3854cfe3ca5be78698
cumpsd <david@cumps.be>
2024-12-05 16:15:16 +0100

    chore: linter

--- packages/swapper/src/swappers/ChainflipSwapper/swapperApi/getTradeQuote.ts
+++ packages/swapper/src/swappers/ChainflipSwapper/swapperApi/getTradeQuote.ts
@@ -18,7 +18,11 @@ import type {
   TradeQuote,
 } from '../../../types'
 import { SwapperName, TradeQuoteError } from '../../../types'
-import { getInputOutputRate, createTradeAmountTooSmallErr, makeSwapErrorRight } from '../../../utils'
+import {
+  createTradeAmountTooSmallErr,
+  getInputOutputRate,
+  makeSwapErrorRight,
+} from '../../../utils'
 import {
   CHAINFLIP_BAAS_COMMISSION,
   CHAINFLIP_BOOST_SWAP_SOURCE,

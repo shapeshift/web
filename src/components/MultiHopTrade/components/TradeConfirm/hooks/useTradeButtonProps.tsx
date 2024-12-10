93a35bf59f5702c0e15ded90ce906045fde0a4c7
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2024-12-10 11:26:03 +1100

    chore: approval sign progress

--- src/components/MultiHopTrade/components/TradeConfirm/hooks/useTradeButtonProps.tsx
+++ src/components/MultiHopTrade/components/TradeConfirm/hooks/useTradeButtonProps.tsx
@@ -1,3 +1,4 @@
+import type { TradeQuoteStep } from '@shapeshiftoss/swapper'
 import { useCallback, useMemo } from 'react'
 import { assertUnreachable } from 'lib/utils'
 import {
@@ -9,7 +10,13 @@ import { HopExecutionState } from 'state/slices/tradeQuoteSlice/types'
 import { useAppDispatch, useAppSelector, useSelectorWithArgs } from 'state/store'
 
 import { getHopExecutionStateButtonTranslation } from '../helpers'
-import { useCurrentHopIndex } from './useCurrentHopIndex'
+import { useSignAllowanceApproval } from './useSignAllowanceApproval'
+
+type UseTradeButtonPropsProps = {
+  tradeQuoteStep: TradeQuoteStep
+  currentHopIndex: number
+  activeTradeId: string
+}
 
 type TradeButtonProps = {
   handleSubmit: () => void
@@ -18,12 +25,20 @@ type TradeButtonProps = {
   isDisabled: boolean
 }
 
-// TODO: This should return an object with button action, text, loading, and disabled
-export const useTradeButtonProps = (): TradeButtonProps | undefined => {
+export const useTradeButtonProps = ({
+  tradeQuoteStep,
+  currentHopIndex,
+  activeTradeId,
+}: UseTradeButtonPropsProps): TradeButtonProps | undefined => {
   const dispatch = useAppDispatch()
   const activeQuote = useAppSelector(selectActiveQuote)
-  const activeTradeId = useAppSelector(selectActiveQuote)?.id
-  const currentHopIndex = useCurrentHopIndex()
+  const { handleSignAllowanceApproval, isLoading: isAllowanceApprovalLoading } =
+    useSignAllowanceApproval({
+      tradeQuoteStep,
+      isExactAllowance: true,
+      currentHopIndex,
+      activeTradeId: activeTradeId ?? '', // FIXME: handle undefined
+    }) // FIXME: handle allowance selection
   const handleTradeConfirm = useCallback(() => {
     if (!activeQuote) return
     dispatch(tradeQuoteSlice.actions.confirmTrade(activeQuote.id))
@@ -69,9 +84,9 @@ export const useTradeButtonProps = (): TradeButtonProps | undefined => {
         }
       case HopExecutionState.AwaitingAllowanceApproval:
         return {
-          handleSubmit: () => {},
+          handleSubmit: handleSignAllowanceApproval,
           buttonText,
-          isLoading: false,
+          isLoading: isAllowanceApprovalLoading,
           isDisabled: false,
         }
       case HopExecutionState.AwaitingPermit2:

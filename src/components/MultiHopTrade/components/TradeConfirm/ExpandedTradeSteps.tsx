9f5a899682e32532edfdf198b9df28631b01d593
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2024-12-12 10:06:25 +1100

    feat: aw yeah, tx links for all steps

--- src/components/MultiHopTrade/components/TradeConfirm/ExpandedTradeSteps.tsx
+++ src/components/MultiHopTrade/components/TradeConfirm/ExpandedTradeSteps.tsx
@@ -1,14 +1,14 @@
 import { CheckCircleIcon } from '@chakra-ui/icons'
-import { CircularProgress, Flex, Link, Stepper, StepStatus } from '@chakra-ui/react'
-import type { AccountId } from '@shapeshiftoss/caip'
+import { CircularProgress, Flex, Stepper, StepStatus, VStack } from '@chakra-ui/react'
 import { useMemo } from 'react'
 import { useTranslate } from 'react-polyglot'
-import { MiddleEllipsis } from 'components/MiddleEllipsis/MiddleEllipsis'
 import { RawText, Text } from 'components/Text'
 import { getChainAdapterManager } from 'context/PluginProvider/chainAdapterSingleton'
-import { useSafeTxQuery } from 'hooks/queries/useSafeTx'
-import { getTxLink } from 'lib/getTxLink'
-import { selectIsActiveQuoteMultiHop } from 'state/slices/tradeInputSlice/selectors'
+import {
+  selectFirstHopSellAccountId,
+  selectIsActiveQuoteMultiHop,
+  selectSecondHopSellAccountId,
+} from 'state/slices/tradeInputSlice/selectors'
 import {
   selectActiveQuote,
   selectActiveSwapperName,
@@ -20,38 +20,10 @@ import { useAppSelector, useSelectorWithArgs } from 'state/store'
 
 import { StepperStep } from '../MultiHopTradeConfirm/components/StepperStep'
 import { useTradeSteps } from './hooks/useTradeSteps'
+import { TxLabel } from './TxLabel'
 
 const pendingStepIndicator = <CircularProgress size={5} trackColor='blue.500' />
 const completedStepIndicator = <CheckCircleIcon color='text.success' />
-
-// TODO: Extract me to another file
-const TxElement = ({
-  txHash,
-  explorerTxLink,
-  accountId,
-}: {
-  txHash: string
-  explorerTxLink: string
-  accountId: AccountId
-}) => {
-  const { data: maybeSafeTx } = useSafeTxQuery({
-    maybeSafeTxHash: txHash,
-    accountId,
-  })
-
-  const txLink = getTxLink({
-    defaultExplorerBaseUrl: explorerTxLink,
-    maybeSafeTx,
-    tradeId: txHash,
-    accountId,
-  })
-
-  return txLink ? (
-    <Link isExternal href={txLink} color='text.link'>
-      <MiddleEllipsis value={maybeSafeTx?.transaction?.transactionHash ?? txHash} />
-    </Link>
-  ) : null
-}
 
 export const ExpandedTradeSteps = () => {
   const translate = useTranslate()
@@ -60,6 +32,9 @@ export const ExpandedTradeSteps = () => {
   const swapperName = useAppSelector(selectActiveSwapperName)
   const tradeQuoteFirstHop = useAppSelector(selectFirstHop)
   const tradeQuoteLastHop = useAppSelector(selectLastHop)
+  // this is the account we're selling from - assume this is the AccountId of the approval Tx
+  const firstHopSellAccountId = useAppSelector(selectFirstHopSellAccountId)
+  const lastHopSellAccountId = useAppSelector(selectSecondHopSellAccountId)
   const isMultiHopTrade = useAppSelector(selectIsActiveQuoteMultiHop)
   const isFirstHopBridge = useMemo(
     () => tradeQuoteFirstHop?.buyAsset.chainId !== tradeQuoteFirstHop?.sellAsset.chainId,
@@ -70,7 +45,7 @@ export const ExpandedTradeSteps = () => {
     [tradeQuoteLastHop?.buyAsset.chainId, tradeQuoteLastHop?.sellAsset.chainId],
   )
   const chainAdapterManager = getChainAdapterManager()
-  const firstHopActionTitle = useMemo(() => {
+  const firstHopActionTitleText = useMemo(() => {
     const sellAssetChainId = tradeQuoteFirstHop?.sellAsset.chainId
     const buyAssetChainId = tradeQuoteFirstHop?.buyAsset.chainId
     if (!sellAssetChainId || !buyAssetChainId) return undefined
@@ -88,7 +63,7 @@ export const ExpandedTradeSteps = () => {
     tradeQuoteFirstHop?.sellAsset.chainId,
     translate,
   ])
-  const lastHopActionTitle = useMemo(() => {
+  const lastHopActionTitleText = useMemo(() => {
     const sellAssetChainId = tradeQuoteLastHop?.sellAsset.chainId
     const buyAssetChainId = tradeQuoteLastHop?.buyAsset.chainId
     if (!sellAssetChainId || !buyAssetChainId) return undefined
@@ -117,7 +92,7 @@ export const ExpandedTradeSteps = () => {
     allowanceApproval: firstHopAllowanceApproval,
     permit2: firstHopPermit2,
     allowanceReset: firstHopAllowanceReset,
-    // swap,
+    swap: firstHopSwap,
   } = useSelectorWithArgs(selectHopExecutionMetadata, firstHopExecutionMetadataFilter)
 
   const lastHopExecutionMetadataFilter = useMemo(() => {
@@ -132,7 +107,7 @@ export const ExpandedTradeSteps = () => {
     allowanceApproval: lastHopAllowanceApproval,
     permit2: lastHopPermit2,
     allowanceReset: lastHopAllowanceReset,
-    // swap,
+    swap: lastHopSwap,
   } = useSelectorWithArgs(selectHopExecutionMetadata, lastHopExecutionMetadataFilter)
 
   const { currentStep } = useTradeSteps()
@@ -149,19 +124,152 @@ export const ExpandedTradeSteps = () => {
   )
 
   const firstHopAllowanceResetTitle = useMemo(() => {
-    const txLink = getTxLink({
-      defaultExplorerBaseUrl: tradeQuoteFirstHop?.sellAsset.explorerTxLink,
-      maybeSafeTx: firstHopAllowanceReset.txHash,
-      tradeId: firstHopAllowanceReset.txHash ?? '',
-      accountId: firstHopSellAccountId,
-    })
     return (
       <Flex alignItems='center' justifyContent='space-between' flex={1}>
         <Text translation='trade.awaitingAllowanceReset' />
-        {firstHopAllowanceReset.txHash && <RawText>{firstHopAllowanceReset.txHash}</RawText>}
+        {firstHopAllowanceReset.txHash && tradeQuoteFirstHop && firstHopSellAccountId && (
+          <TxLabel
+            txHash={firstHopAllowanceReset.txHash}
+            explorerTxLink={tradeQuoteFirstHop.sellAsset.explorerTxLink}
+            accountId={firstHopSellAccountId}
+          />
+        )}
       </Flex>
     )
-  }, [firstHopAllowanceReset.txHash])
+  }, [firstHopAllowanceReset.txHash, firstHopSellAccountId, tradeQuoteFirstHop])
+
+  const firstHopAllowanceApprovalTitle = useMemo(() => {
+    return (
+      <Flex alignItems='center' justifyContent='space-between' flex={1}>
+        {firstHopPermit2.isRequired === true ? (
+          // TODO: Add permit2 tooltip
+          <Text translation='trade.awaitingPermit2Approval' />
+        ) : (
+          <>
+            <Text translation='trade.awaitingApproval' />
+            {firstHopAllowanceApproval.txHash && tradeQuoteFirstHop && firstHopSellAccountId && (
+              <TxLabel
+                txHash={firstHopAllowanceApproval.txHash}
+                explorerTxLink={tradeQuoteFirstHop.sellAsset.explorerTxLink}
+                accountId={firstHopSellAccountId}
+              />
+            )}
+          </>
+        )}
+      </Flex>
+    )
+  }, [
+    firstHopAllowanceApproval.txHash,
+    firstHopPermit2.isRequired,
+    firstHopSellAccountId,
+    tradeQuoteFirstHop,
+  ])
+
+  const firstHopActionTitle = useMemo(() => {
+    return (
+      <Flex alignItems='center' justifyContent='space-between' flex={1}>
+        <RawText>{firstHopActionTitleText}</RawText>
+        {tradeQuoteFirstHop && firstHopSellAccountId && (
+          <VStack>
+            {firstHopSwap.buyTxHash && (
+              <TxLabel
+                txHash={firstHopSwap.buyTxHash}
+                explorerTxLink={tradeQuoteFirstHop.buyAsset.explorerTxLink}
+                accountId={firstHopSellAccountId}
+              />
+            )}
+            {firstHopSwap.sellTxHash && (
+              <TxLabel
+                txHash={firstHopSwap.sellTxHash}
+                explorerTxLink={tradeQuoteFirstHop.sellAsset.explorerTxLink}
+                accountId={firstHopSellAccountId}
+              />
+            )}
+          </VStack>
+        )}
+      </Flex>
+    )
+  }, [
+    firstHopActionTitleText,
+    firstHopSellAccountId,
+    firstHopSwap.buyTxHash,
+    firstHopSwap.sellTxHash,
+    tradeQuoteFirstHop,
+  ])
+
+  const lastHopAllowanceResetTitle = useMemo(() => {
+    return (
+      <Flex alignItems='center' justifyContent='space-between' flex={1}>
+        <Text translation='trade.awaitingAllowanceReset' />
+        {lastHopAllowanceReset.txHash && tradeQuoteLastHop && lastHopSellAccountId && (
+          <TxLabel
+            txHash={lastHopAllowanceReset.txHash}
+            explorerTxLink={tradeQuoteLastHop.sellAsset.explorerTxLink}
+            accountId={lastHopSellAccountId}
+          />
+        )}
+      </Flex>
+    )
+  }, [lastHopAllowanceReset.txHash, lastHopSellAccountId, tradeQuoteLastHop])
+
+  const lastHopAllowanceApprovalTitle = useMemo(() => {
+    return (
+      <Flex alignItems='center' justifyContent='space-between' flex={1}>
+        {lastHopPermit2.isRequired === true ? (
+          // TODO: Add permit2 tooltip
+          <Text translation='trade.awaitingPermit2Approval' />
+        ) : (
+          <>
+            <Text translation='trade.awaitingApproval' />
+            {lastHopAllowanceApproval.txHash && tradeQuoteLastHop && lastHopSellAccountId && (
+              <TxLabel
+                txHash={lastHopAllowanceApproval.txHash}
+                explorerTxLink={tradeQuoteLastHop.sellAsset.explorerTxLink}
+                accountId={lastHopSellAccountId}
+              />
+            )}
+          </>
+        )}
+      </Flex>
+    )
+  }, [
+    lastHopAllowanceApproval.txHash,
+    lastHopPermit2.isRequired,
+    lastHopSellAccountId,
+    tradeQuoteLastHop,
+  ])
+
+  const lastHopActionTitle = useMemo(() => {
+    return (
+      <Flex alignItems='center' justifyContent='space-between' flex={1}>
+        <RawText>{lastHopActionTitleText}</RawText>
+        {tradeQuoteLastHop && lastHopSellAccountId && (
+          <VStack>
+            {lastHopSwap.buyTxHash && (
+              <TxLabel
+                txHash={lastHopSwap.buyTxHash}
+                explorerTxLink={tradeQuoteLastHop.buyAsset.explorerTxLink}
+                accountId={lastHopSellAccountId}
+              />
+            )}
+            {lastHopSwap.sellTxHash && (
+              <TxLabel
+                txHash={lastHopSwap.sellTxHash}
+                explorerTxLink={tradeQuoteLastHop.sellAsset.explorerTxLink}
+                accountId={lastHopSellAccountId}
+              />
+            )}
+          </VStack>
+        )}
+      </Flex>
+    )
+  }, [
+    lastHopActionTitleText,
+    lastHopSellAccountId,
+    lastHopSwap.buyTxHash,
+    lastHopSwap.sellTxHash,
+    tradeQuoteLastHop,
+  ])
 
   return (
     <Stepper orientation='vertical' index={currentStep} gap='0'>
@@ -176,11 +284,7 @@ export const ExpandedTradeSteps = () => {
       {firstHopAllowanceApproval.isInitiallyRequired === true ||
       firstHopPermit2.isRequired === true ? (
         <StepperStep
-          title={
-            firstHopPermit2.isRequired
-              ? translate('trade.awaitingPermit2Approval')
-              : translate('trade.awaitingApproval')
-          }
+          title={firstHopAllowanceApprovalTitle}
           stepIndicator={stepIndicator}
           stepProps={stepProps}
           useSpacer={false}
@@ -196,7 +300,7 @@ export const ExpandedTradeSteps = () => {
         <>
           {lastHopAllowanceReset.isRequired === true ? (
             <StepperStep
-              title={translate('trade.awaitingAllowanceReset')}
+              title={lastHopAllowanceResetTitle}
               stepIndicator={stepIndicator}
               stepProps={stepProps}
               useSpacer={false}
@@ -205,11 +309,7 @@ export const ExpandedTradeSteps = () => {
           {lastHopAllowanceApproval.isInitiallyRequired === true ||
           lastHopPermit2.isRequired === true ? (
             <StepperStep
-              title={
-                lastHopPermit2.isRequired
-                  ? translate('trade.awaitingPermit2Approval')
-                  : translate('trade.awaitingApproval')
-              }
+              title={lastHopAllowanceApprovalTitle}
               stepIndicator={stepIndicator}
               stepProps={stepProps}
               useSpacer={false}

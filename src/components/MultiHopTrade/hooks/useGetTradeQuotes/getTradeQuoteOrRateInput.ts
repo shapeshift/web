011139cadbc54da333878b47fd0c692ef55a4e84
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-12-12 16:39:30 +0800

    fix: absolute derp

--- src/components/MultiHopTrade/hooks/useGetTradeQuotes/getTradeQuoteOrRateInput.ts
+++ src/components/MultiHopTrade/hooks/useGetTradeQuotes/getTradeQuoteOrRateInput.ts
@@ -64,6 +64,7 @@ export const getTradeQuoteOrRateInput = async ({
           allowMultiHop,
           slippageTolerancePercentageDecimal,
           quoteOrRate: 'quote',
+          originalRate,
         }
       : {
           sellAmountIncludingProtocolFeesCryptoBaseUnit: toBaseUnit(

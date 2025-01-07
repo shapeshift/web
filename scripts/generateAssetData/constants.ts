dd7b97991a776523ac88ea72ad2dd5098d2e1392
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2025-01-07 12:06:17 +1100

    chore: delete migration script

--- scripts/generateAssetData/constants.ts
+++ scripts/generateAssetData/constants.ts
@@ -1,17 +1,5 @@
 import path from 'path'
 
-// TODO: Remove me when migration to encoded asset data complete
-export const LEGACY_ASSET_DATA_PATH = path.join(
-  __dirname,
-  '../../src/lib/asset-service/service/generatedAssetData.json',
-)
-
-// TODO: Remove me when migration to encoded asset data complete
-export const LEGACY_RELATED_ASSET_INDEX_PATH = path.join(
-  __dirname,
-  '../../src/lib/asset-service/service/relatedAssetIndex.json',
-)
-
 export const ASSET_DATA_PATH = path.join(
   __dirname,
   '../../src/lib/asset-service/service/encodedAssetData.json',

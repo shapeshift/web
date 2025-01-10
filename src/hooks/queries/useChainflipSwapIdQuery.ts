d69056070a4effa940f26296821aff0e685427f4
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-01-10 12:34:30 +0400

    feat: chainflip explorer link (#8529)

---
+++ src/hooks/queries/useChainflipSwapIdQuery.ts
@@ -0,0 +1,59 @@
+import type { SwapSource } from '@shapeshiftoss/swapper'
+import { SwapperName } from '@shapeshiftoss/swapper'
+import type { UseQueryResult } from '@tanstack/react-query'
+import { skipToken, useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+
+type UseChainflipSwapIdArgs = {
+  txHash: string | undefined
+  swapperName: SwapSource | undefined
+}
+
+type SwapIdResponse = {
+  data: {
+    txRefs: {
+      nodes: {
+        swap: {
+          nativeId: string | undefined
+        }
+      }[]
+    }
+  }
+}
+
+const nativeSwapIdQuery = `
+  query GetStringSearchResults($searchString: String!) {
+    txRefs: allTransactionRefs(filter: {ref: {equalTo: $searchString}}) {
+      nodes {
+        swap: swapRequestBySwapRequestId {
+          nativeId
+        }
+      }
+    }
+  }
+`
+
+export const useChainflipSwapIdQuery = ({
+  txHash,
+  swapperName,
+}: UseChainflipSwapIdArgs): UseQueryResult<string | undefined, Error> => {
+  return useQuery({
+    queryKey: ['chainflipSwapId', { txHash }],
+    queryFn:
+      txHash && swapperName?.includes(SwapperName.Chainflip)
+        ? () =>
+            // Yes, this is ugly, just another day in "we're doing inline raw GraphQL queries with http"
+            // Note, this is undocumented but is actually the exact same query fragment CF UI uses to go from Tx hash to swap ID (though only a fragment of it,
+            // actually stripped out to the bare minimum here vs. cf UI)
+            axios.post<SwapIdResponse>('https://explorer-service-processor.chainflip.io/graphql', {
+              query: nativeSwapIdQuery,
+              variables: {
+                searchString: txHash,
+              },
+            })
+        : skipToken,
+    select: ({ data: { data } }) => data.txRefs.nodes[0].swap.nativeId,
+    // Long-poll until swap by initiating Txid is indexed
+    refetchInterval: 10_000,
+  })
+}

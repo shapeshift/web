fc081266104ef5b58177951e3a0d5f9d8e6ba6d1
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2025-01-23 12:43:48 +1100

    fix: dir casing

---
+++ src/components/MultiHopTrade/components/LimitOrderShared/useAllowanceReset.tsx
@@ -0,0 +1,109 @@
+import { assertGetViemClient } from '@shapeshiftoss/contracts'
+import { COW_SWAP_VAULT_RELAYER_ADDRESS } from '@shapeshiftoss/swapper'
+import { useMutation } from '@tanstack/react-query'
+import { useEffect, useMemo } from 'react'
+import { reactQueries } from 'react-queries'
+import type { Hash } from 'viem'
+import { AllowanceType, useApprovalFees } from 'hooks/queries/useApprovalFees'
+import { useIsAllowanceResetRequired } from 'hooks/queries/useIsAllowanceResetRequired'
+import { useErrorToast } from 'hooks/useErrorToast/useErrorToast'
+import { useWallet } from 'hooks/useWallet/useWallet'
+import { limitOrderSlice } from 'state/slices/limitOrderSlice/limitOrderSlice'
+import type { LimitOrderActiveQuote } from 'state/slices/limitOrderSlice/types'
+import { selectAccountNumberByAccountId } from 'state/slices/selectors'
+import { useAppDispatch, useAppSelector } from 'state/store'
+
+type UseAllowanceResetProps = {
+  activeQuote: LimitOrderActiveQuote
+  feeQueryEnabled: boolean
+  isInitiallyRequired: boolean
+}
+
+// handles allowance reset tx execution, fees, and state orchestration
+export const useAllowanceReset = ({
+  activeQuote,
+  feeQueryEnabled,
+  isInitiallyRequired,
+}: UseAllowanceResetProps) => {
+  const dispatch = useAppDispatch()
+  const { showErrorToast } = useErrorToast()
+  const wallet = useWallet().state.wallet ?? undefined
+
+  const accountNumberFilter = useMemo(
+    () => ({ accountId: activeQuote.params.accountId }),
+    [activeQuote.params.accountId],
+  )
+  const accountNumber = useAppSelector(state =>
+    selectAccountNumberByAccountId(state, accountNumberFilter),
+  )
+
+  const { isAllowanceResetRequired, isLoading: isAllowanceResetRequirementsLoading } =
+    useIsAllowanceResetRequired({
+      amountCryptoBaseUnit: activeQuote.params.sellAmountCryptoBaseUnit,
+      assetId: activeQuote.params.sellAssetId,
+      from: activeQuote.params.sellAccountAddress,
+      spender: COW_SWAP_VAULT_RELAYER_ADDRESS,
+      isDisabled: !(isInitiallyRequired && feeQueryEnabled),
+    })
+
+  const { evmFeesResult } = useApprovalFees({
+    amountCryptoBaseUnit: activeQuote.params.sellAmountCryptoBaseUnit,
+    assetId: activeQuote.params.sellAssetId,
+    from: activeQuote.params.sellAccountAddress,
+    allowanceType: AllowanceType.Reset,
+    spender: COW_SWAP_VAULT_RELAYER_ADDRESS,
+    enabled: isInitiallyRequired && feeQueryEnabled,
+  })
+  useEffect(() => {
+    if (!feeQueryEnabled || !isInitiallyRequired || isAllowanceResetRequired !== false) return
+
+    // Mark the whole allowance approval step complete if adequate allowance was found.
+    // This is deliberately disjoint to the approval transaction orchestration to allow users to
+    // complete an approval externally and have the app respond to the updated allowance on chain.
+    dispatch(limitOrderSlice.actions.setAllowanceApprovalStepComplete(activeQuote.response.id ?? 0))
+  }, [
+    activeQuote.response.id,
+    dispatch,
+    feeQueryEnabled,
+    isAllowanceResetRequired,
+    isInitiallyRequired,
+  ])
+
+  const allowanceResetMutation = useMutation({
+    ...reactQueries.mutations.approve({
+      accountNumber,
+      amountCryptoBaseUnit: '0',
+      assetId: activeQuote.params.sellAssetId,
+      spender: COW_SWAP_VAULT_RELAYER_ADDRESS,
+      from: activeQuote.params.sellAccountAddress,
+      wallet,
+    }),
+    onMutate() {
+      dispatch(limitOrderSlice.actions.setAllowanceResetTxPending(activeQuote.response.id ?? 0))
+    },
+    async onSuccess(txHash) {
+      dispatch(
+        limitOrderSlice.actions.setAllowanceApprovalTxHash({
+          txHash,
+          id: activeQuote.response.id ?? 0,
+        }),
+      )
+
+      const publicClient = assertGetViemClient(activeQuote.params.chainId)
+      await publicClient.waitForTransactionReceipt({ hash: txHash as Hash })
+
+      dispatch(limitOrderSlice.actions.setAllowanceResetTxComplete(activeQuote.response.id ?? 0))
+    },
+    onError(err) {
+      dispatch(limitOrderSlice.actions.setAllowanceResetTxFailed(activeQuote.response.id ?? 0))
+      showErrorToast(err)
+    },
+  })
+
+  return {
+    isLoading: isAllowanceResetRequirementsLoading || evmFeesResult.isLoading,
+    allowanceResetMutation,
+    allowanceResetNetworkFeeCryptoBaseUnit: evmFeesResult.data?.networkFeeCryptoBaseUnit,
+    isAllowanceResetRequired,
+  }
+}

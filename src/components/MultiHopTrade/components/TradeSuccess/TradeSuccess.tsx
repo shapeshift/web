b38aaf1ef26f36cbe6f158659264adc2c782701e
woody <125113430+woodenfurniture@users.noreply.github.com>
2025-01-28 04:59:00 +1100

    feat: fox discount summary on trade completion (#8660)

--- src/components/MultiHopTrade/components/TradeSuccess/TradeSuccess.tsx
+++ src/components/MultiHopTrade/components/TradeSuccess/TradeSuccess.tsx
@@ -5,23 +5,35 @@ import {
   CardBody,
   CardFooter,
   Collapse,
+  Divider,
   Flex,
   HStack,
   Icon,
   Stack,
   useDisclosure,
 } from '@chakra-ui/react'
+import { foxAssetId, foxOnArbitrumOneAssetId, foxOnGnosisAssetId } from '@shapeshiftoss/caip'
 import type { Asset } from '@shapeshiftoss/types'
 import type { InterpolationOptions } from 'node-polyglot'
-import { useCallback } from 'react'
+import { useCallback, useMemo } from 'react'
 import { useTranslate } from 'react-polyglot'
 import { Amount } from 'components/Amount/Amount'
 import { AnimatedCheck } from 'components/AnimatedCheck'
 import { AssetIcon } from 'components/AssetIcon'
 import { SlideTransition } from 'components/SlideTransition'
 import { Text } from 'components/Text'
+import { bnOrZero } from 'lib/bignumber/bignumber'
+import {
+  selectFirstHop,
+  selectLastHop,
+  selectTradeQuoteAffiliateFeeAfterDiscountUserCurrency,
+  selectTradeQuoteAffiliateFeeDiscountUserCurrency,
+} from 'state/slices/tradeQuoteSlice/selectors'
+import { useAppSelector } from 'state/store'
 
 import { TwirlyToggle } from '../TwirlyToggle'
+import { YouCouldHaveSaved } from './components/YouCouldHaveSaved'
+import { YouSaved } from './components/YouSaved'
 
 export type TradeSuccessProps = {
   handleBack: () => void
@@ -52,6 +64,18 @@ export const TradeSuccess = ({
     defaultIsOpen: false,
   })
 
+  const firstHop = useAppSelector(selectFirstHop)
+  const lastHop = useAppSelector(selectLastHop)
+
+  const feeSavingUserCurrency = useAppSelector(selectTradeQuoteAffiliateFeeDiscountUserCurrency)
+
+  const affiliateFeeUserCurrency = useAppSelector(
+    selectTradeQuoteAffiliateFeeAfterDiscountUserCurrency,
+  )
+
+  const hasFeeSaving = !bnOrZero(feeSavingUserCurrency).isZero()
+  const couldHaveReducedFee = !hasFeeSaving && !bnOrZero(affiliateFeeUserCurrency).isZero()
+
   const AmountsLine = useCallback(() => {
     if (!(sellAsset && buyAsset)) return null
     if (!(sellAmountCryptoPrecision && buyAmountCryptoPrecision)) return null
@@ -79,6 +103,25 @@ export const TradeSuccess = ({
     )
   }, [sellAsset, buyAsset, sellAmountCryptoPrecision, buyAmountCryptoPrecision])
 
+  // NOTE: This is a temporary solution to enable the Fox discount summary only if the user did NOT
+
+  // trade FOX. If a user trades FOX, the discount calculations will have changed from the correct
+  // values because the amount of FOX held in the wallet will have changed.
+  // See https://github.com/shapeshift/web/issues/8028 for more details.
+  const enableFoxDiscountSummary = useMemo(() => {
+    const foxAssetIds = [foxAssetId, foxOnGnosisAssetId, foxOnArbitrumOneAssetId]
+    const didTradeFox = foxAssetIds.some(assetId => {
+      return (
+        firstHop?.buyAsset.assetId === assetId ||
+        firstHop?.sellAsset.assetId === assetId ||
+        lastHop?.buyAsset.assetId === assetId ||
+        lastHop?.sellAsset.assetId === assetId
+      )
+    })
+
+    return !didTradeFox
+  }, [firstHop, lastHop])
+
   return (
     <>
       <CardBody pb={0} px={0}>
@@ -95,22 +138,31 @@ export const TradeSuccess = ({
           <Button mt={4} size='lg' width='full' onClick={handleBack} colorScheme='blue'>
             {translate(buttonTranslation)}
           </Button>
+          {enableFoxDiscountSummary && hasFeeSaving && (
+            <YouSaved feeSavingUserCurrency={enableFoxDiscountSummary && feeSavingUserCurrency!} />
+          )}
+          {couldHaveReducedFee && (
+            <YouCouldHaveSaved affiliateFeeUserCurrency={affiliateFeeUserCurrency!} />
+          )}
         </Stack>
       </CardBody>
       {summaryTranslation && children && (
-        <CardFooter flexDir='column' gap={2} px={8}>
-          <SlideTransition>
-            <HStack width='full' justifyContent='space-between' mt={4}>
-              <Button variant='link' onClick={handleToggle} px={2}>
-                {translate(summaryTranslation)}
-              </Button>
-              <TwirlyToggle isOpen={isOpen} onToggle={handleToggle} />
-            </HStack>
-            <Box>
-              <Collapse in={isOpen}>{children}</Collapse>
-            </Box>
-          </SlideTransition>
-        </CardFooter>
+        <>
+          <Divider />
+          <CardFooter flexDir='column' gap={2} px={8}>
+            <SlideTransition>
+              <HStack width='full' justifyContent='space-between' mt={4}>
+                <Button variant='link' onClick={handleToggle} px={2}>
+                  {translate(summaryTranslation)}
+                </Button>
+                <TwirlyToggle isOpen={isOpen} onToggle={handleToggle} />
+              </HStack>
+              <Box>
+                <Collapse in={isOpen}>{children}</Collapse>
+              </Box>
+            </SlideTransition>
+          </CardFooter>
+        </>
       )}
     </>
   )

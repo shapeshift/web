c03e3b5c1cf06846513d9881c3a3824af438156c
Apotheosis <0xapotheosis@gmail.com>
2025-01-28 19:31:44 +1100

    chore: implement getAssetNamespaceFromChainId helper (#8677)

--- src/components/TradeAssetSearch/helpers/customAssetSearch.ts
+++ src/components/TradeAssetSearch/helpers/customAssetSearch.ts
@@ -1,5 +1,3 @@
-import type { AssetNamespace, ChainId } from '@shapeshiftoss/caip'
-import { ASSET_NAMESPACE, bscChainId, solanaChainId } from '@shapeshiftoss/caip'
 import { PublicKey } from '@solana/web3.js'
 
 export const isSolanaAddress = (contractAddress: string) => {
@@ -9,16 +7,5 @@ export const isSolanaAddress = (contractAddress: string) => {
   } catch (error) {
     // If instantiation fails, it's not a valid Solana address
     return false
-  }
-}
-
-export const getAssetNamespaceFromChainId = (chainId: ChainId): AssetNamespace => {
-  switch (chainId) {
-    case bscChainId:
-      return ASSET_NAMESPACE.bep20
-    case solanaChainId:
-      return ASSET_NAMESPACE.splToken
-    default:
-      return ASSET_NAMESPACE.erc20
   }
 }

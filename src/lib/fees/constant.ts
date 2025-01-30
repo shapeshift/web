b7713cd89b04a6a996d616e06b4bd9e2f2869ff8
NeOMakinG <14963751+NeOMakinG@users.noreply.github.com>
2025-01-30 23:15:50 +0100

    fix: review feedbacks

--- src/lib/fees/constant.ts
+++ src/lib/fees/constant.ts
@@ -2,8 +2,8 @@ import { toBaseUnit } from 'lib/math'
 
 // This timestamp is the 03 of February 2025 10:30 PST
 export const FOX_WIF_HAT_CAMPAIGN_STARTING_TIME_MS = 1738575000000
-// 90 days from the start
-export const FOX_WIF_HAT_CAMPAIGN_ENDING_TIME_MS = 1000 * 60 * 60 * 24 * 90
+// 120 days from the start
+export const FOX_WIF_HAT_CAMPAIGN_ENDING_TIME_MS = 1000 * 60 * 60 * 24 * 120
 
 // Assuming foxwifhat is 18 decimals
 export const FOX_WIF_HAT_MINIMUM_AMOUNT_BASE_UNIT = toBaseUnit(10000, 18)

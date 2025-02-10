0c25c68047f04673e42c38d8a0d387887eb1a566
NeOMakinG <14963751+NeOMakinG@users.noreply.github.com>
2025-02-11 07:57:24 +0800

    feat: activate foxwifhat on wednesday 1030 pst (#8772)
    * feat: activate foxwifhat on wednesday 1030 pst
    
    * fix: update comment

--- src/lib/fees/constant.ts
+++ src/lib/fees/constant.ts
@@ -1,7 +1,8 @@
 import { toBaseUnit } from 'lib/math'
 
-// This timestamp is the 03 of February 2025 10:30 PST
-export const FOX_WIF_HAT_CAMPAIGN_STARTING_TIME_MS = 1738575000000
+// This timestamp is the 12 of February 2025 10:30 PST
+export const FOX_WIF_HAT_CAMPAIGN_STARTING_TIME_MS = 1739385000000
+
 // 120 days from the start
 export const FOX_WIF_HAT_CAMPAIGN_ENDING_TIME_MS =
   FOX_WIF_HAT_CAMPAIGN_STARTING_TIME_MS + 1000 * 60 * 60 * 24 * 120

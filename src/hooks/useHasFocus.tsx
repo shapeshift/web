23f2f85fb465683393facc223177a4d1a2f33be0
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2025-02-14 07:36:05 +1100

    fix: useHasFocus

--- src/hooks/useHasFocus.tsx
+++ src/hooks/useHasFocus.tsx
@@ -1,16 +1,22 @@
 import { useEffect, useState } from 'react'
 
 export const useHasFocus = () => {
-  const [hasFocus, setHasFocus] = useState(document.hasFocus())
+  const [hasFocus, setHasFocus] = useState(true)
 
   useEffect(() => {
-    const handleFocus = () => {
-      setHasFocus(document.hasFocus())
-    }
+    const handleFocus = () => setHasFocus(true)
+    const handleBlur = () => setHasFocus(false)
+    const handleResume = () => setHasFocus(true)
 
     window.addEventListener('focus', handleFocus)
+    window.addEventListener('blur', handleBlur)
+    document.addEventListener('resume', handleResume) // Handle mobile app lifecycle events
 
-    return () => window.removeEventListener('focus', handleFocus)
+    return () => {
+      window.removeEventListener('focus', handleFocus)
+      window.removeEventListener('blur', handleBlur)
+      document.removeEventListener('resume', handleResume)
+    }
   }, [])
 
   return hasFocus

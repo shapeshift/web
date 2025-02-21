fc557b31fc3518a83397e1c3ac007af7a3aae308
NeOMakinG <14963751+NeOMakinG@users.noreply.github.com>
2025-02-21 22:09:39 +0100

    fix: finish

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react-swc'
-import { fileURLToPath } from 'url'
 import { dirname, resolve } from 'path'
 import { visualizer } from 'rollup-plugin-visualizer'
+import { fileURLToPath } from 'url'
 import { defineConfig, loadEnv } from 'vite'
 // import circularDependency from 'vite-plugin-circular-dependency'
 import tsconfigPaths from 'vite-tsconfig-paths'
@@ -20,7 +20,7 @@ const externalShapeshiftPackages = [
   '@shapeshiftoss/hdwallet-metamask',
 ]
 
-export default defineConfig(async ({ mode }) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
@@ -129,7 +129,7 @@ export default defineConfig(async ({ mode }) => {
     define: {
       'process.env': JSON.stringify(env),
       global: 'globalThis',
-      "process.browser": true
+      'process.browser': true,
     },
   }
 })

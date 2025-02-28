ad5f0beadad6714f5156cd6ada0d1d292ef483c6
NeOMakinG <14963751+NeOMakinG@users.noreply.github.com>
2025-02-28 08:50:05 +0100

    feat: use vite mode

--- headers/csps/defi/swappers/Thor.ts
+++ headers/csps/defi/swappers/Thor.ts
@@ -2,7 +2,7 @@ import { loadEnv } from 'vite'
 
 import type { Csp } from '../../../types'
 
-const mode = process.env.NODE_ENV || 'development'
+const mode = process.env.MODE ?? process.env.NODE_ENV ?? 'development'
 const env = loadEnv(mode, process.cwd(), '')
 
 export const csp: Csp = {

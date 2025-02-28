bc94cf4902bed0dc35b2f709b4865bf07c111c9f
NeOMakinG <14963751+NeOMakinG@users.noreply.github.com>
2025-02-28 15:12:46 +0100

    fix: bring back wdyr

--- src/wdyr.ts
+++ src/wdyr.ts
@@ -1,10 +1,9 @@
-// import React from 'react'
+import React from 'react'
 
-// @TODO: fix this
-// if (import.meta.env.NODE_ENV === 'development') {
-//   const whyDidYouRender = require('@welldone-software/why-did-you-render')
-//   whyDidYouRender(React, {
-//     trackAllPureComponents: true,
-//     exclude: [/withBoundingRects\(\)/, /Portal/],
-//   })
-// }
+if (import.meta.env.NODE_ENV === 'development') {
+  const whyDidYouRender = require('@welldone-software/why-did-you-render')
+  whyDidYouRender(React, {
+    trackAllPureComponents: true,
+    exclude: [/withBoundingRects\(\)/, /Portal/],
+  })
+}

8c397de36624ea090d70e8ab38932ab07a7e2e84
NeOMakinG <14963751+NeOMakinG@users.noreply.github.com>
2025-03-02 17:27:15 +0100

    fix: remove unused import

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,7 +7,7 @@ import { dirname, resolve } from 'path'
 import * as path from 'path'
 import * as ssri from 'ssri'
 import { fileURLToPath } from 'url'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import checker from 'vite-plugin-checker'
 import tsconfigPaths from 'vite-tsconfig-paths'
 

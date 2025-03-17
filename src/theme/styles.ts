2dca40c84905246c21d32dcc853be6ca7c72f3a5
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2025-03-17 14:32:27 -0600

    display block style

--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -1,20 +1,15 @@
 export const clickableLinkSx = {
-  position: 'relative',
   userSelect: 'none',
   '&:after': {
     content: '""',
-    position: 'absolute',
-    bottom: -1,
-    left: 0,
-    right: 0,
+    display: 'block',
     height: '1px',
     borderBottom: '1px dotted',
     borderColor: 'whiteAlpha.500',
   },
   '&:hover': {
     '&:after': {
-      borderBottom: 'none',
-      height: '1px',
+      borderBottom: '1px solid transparent',
     },
   },
 }

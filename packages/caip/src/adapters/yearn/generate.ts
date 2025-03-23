ebb7cd5e5635e5510523783a9bb6af12c0fba5b0
kevin <35275952+kaladinlight@users.noreply.github.com>
2025-03-23 17:24:19 -0600

    chore: remove dead yearn code (#9112)

--- packages/caip/src/adapters/yearn/generate.ts
+++
@@ -1,9 +0,0 @@
-import { fetchData, parseData, writeFiles } from './utils'
-
-const main = async () => {
-  const data = await fetchData()
-  const output = parseData(data)
-  await writeFiles(output)
-}
-
-main()

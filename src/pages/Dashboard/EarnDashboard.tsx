016b95efd6af78ae351873d24b6220ca1aeb212e
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-03-24 19:22:11 +0300

    fix: lint

--- src/pages/Dashboard/EarnDashboard.tsx
+++ src/pages/Dashboard/EarnDashboard.tsx
@@ -1,12 +1,11 @@
 import { ArrowForwardIcon } from '@chakra-ui/icons'
 import { Button, Flex, Heading } from '@chakra-ui/react'
-import { memo, useDeferredValue, useEffect, useState } from 'react'
+import { memo } from 'react'
 import { useTranslate } from 'react-polyglot'
 import { Link as NavLink } from 'react-router-dom'
 
 import { SEO } from '@/components/Layout/Seo'
 import { DeFiEarn } from '@/components/StakingVaults/DeFiEarn'
-import { DeFiEarnSkeleton } from '@/components/StakingVaults/DeFiEarnSkeleton'
 import { RawText } from '@/components/Text'
 
 const alignItems = { base: 'flex-start', md: 'center' }

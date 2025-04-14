65f0ea3c34a47cdde4a96598ffd2306e071ea701
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-04-14 09:55:49 +0200

    fix: shit

--- src/components/NestedList.tsx
+++ src/components/NestedList.tsx
@@ -1,5 +1,6 @@
 import type { ListProps } from '@chakra-ui/react'
-import { forwardRef, List, useColorModeValue } from '@chakra-ui/react'
+import { List } from '@chakra-ui/react'
+import { forwardRef, useColorModeValue } from '@chakra-ui/system'
 
 const listPaddingX = { base: 2, md: 4 }
 const listMarginLeft = { base: 2, md: 8 }
@@ -7,19 +8,15 @@ const listBorderLeftWidth = { base: 0, md: 1 }
 
 export const NestedList = forwardRef<ListProps, 'div'>((props, ref) => {
   const borderColor = useColorModeValue('blackAlpha.100', 'whiteAlpha.100')
-  // Technically, <List /> supports many more props, but trying and spreading the whole props object will result in the tsc error below:
-  // Expression produces a union type that is too complex to represent. [2590]
-  // Since we only consume this component in two places, and pr is the only prop we need from `props`, destructuring it for sanity's sake.
-  const pr = props.pr
-
   return (
     <List
+      // @ts-ignore tsc is drunk with this guy
       px={listPaddingX}
       ml={listMarginLeft}
       borderLeftWidth={listBorderLeftWidth}
       borderColor={borderColor}
       ref={ref}
-      pr={pr}
+      {...props}
     />
   )
 })

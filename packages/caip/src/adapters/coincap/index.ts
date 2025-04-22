166a4e2fbf774d1cffd984aa7deed531c7987867
Apotheosis <0xapotheosis@gmail.com>
2025-04-22 17:12:44 +1000

    fix: use coincap v3

--- packages/caip/src/adapters/coincap/index.ts
+++ packages/caip/src/adapters/coincap/index.ts
@@ -3,7 +3,8 @@ import toLower from 'lodash/toLower'
 
 import * as adapters from './generated'
 
-export const coincapUrl = 'https://api.coincap.io/v2/assets?limit=2000'
+const apiKey = '478b07654ff6dd0716b52e63cb2ad27c2b6aa7b7679d56eda0ea250d8b27dfcd'
+export const coincapUrl = `https://rest.coincap.io/v3/assets?limit=2000&apiKey=${apiKey}`
 
 const generatedAssetIdToCoinCapMap = Object.values(adapters).reduce((acc, cur) => ({
   ...acc,

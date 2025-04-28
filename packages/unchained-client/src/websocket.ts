75308b29078996bac087614fe25fea07818629d1
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2025-04-28 16:23:40 -0600

    reduce changeset by backing out websocket close/cleanup changes

--- packages/unchained-client/src/websocket.ts
+++ packages/unchained-client/src/websocket.ts
@@ -41,8 +41,6 @@ export class Client<T> {
   }
 
   private async initialize(): Promise<void> {
-    this.cleanup()
-
     const ws = new WebSocket(this.url)
 
     this.connections.txs = { ws }
@@ -56,25 +54,6 @@ export class Client<T> {
       ws.onerror = event => this.onError(event)
       ws.onmessage = event => this.onMessage(event)
     })
-  }
-
-  private cleanup(): void {
-    if (!this.connections.txs) return
-
-    if (this.connections.txs.ws) {
-      switch (this.connections.txs.ws.readyState) {
-        case WebSocket.OPEN:
-        case WebSocket.CONNECTING:
-          this.connections.txs.ws.close(NORMAL_CLOSURE_CODE)
-          break
-        default:
-      }
-    }
-
-    clearTimeout(this.connections.txs.pingTimeout)
-    clearInterval(this.connections.txs.interval)
-
-    delete this.connections.txs
   }
 
   private onOpen(
@@ -116,7 +95,8 @@ export class Client<T> {
     resolve: (value: boolean) => void,
     reject: (reason?: unknown) => void,
   ): void {
-    this.cleanup()
+    this.connections.txs?.pingTimeout && clearTimeout(this.connections.txs.pingTimeout)
+    this.connections.txs?.interval && clearInterval(this.connections.txs.interval)
 
     // attempt reconnect logic on non standard exit codes
     if (event.code !== NORMAL_CLOSURE_CODE) {
@@ -272,7 +252,7 @@ export class Client<T> {
   close(topic?: Topics): void {
     const closeTxs = () => {
       this.unsubscribeTxs()
-      this.cleanup()
+      this.connections.txs?.ws?.close(NORMAL_CLOSURE_CODE)
     }
 
     // close all connections if no topic is provided

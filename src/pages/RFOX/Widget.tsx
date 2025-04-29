08de61ddc529fa2f6e634fb83b90a69a0659e0f0
reallybeard <89934888+reallybeard@users.noreply.github.com>
2025-04-28 23:15:51 -0500

    feat: add initial skeleton for tcy page (#9384)
    * add basic skeleton of tcy page
    
    * Update Overview.tsx
    
    * update flag name
    
    * Update Overview.tsx
    
    * Update StakeInput.tsx
    
    * Update store.ts
    
    * comments

--- src/pages/RFOX/Widget.tsx
+++ src/pages/RFOX/Widget.tsx
@@ -1,20 +1,12 @@
-import { Button, Card, Flex, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
-import type { PropsWithChildren } from 'react'
-import { useCallback, useMemo, useState } from 'react'
-import { useTranslate } from 'react-polyglot'
+import { Card, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
+import { useMemo, useState } from 'react'
 
 import { ChangeAddress } from './components/ChangeAddress/ChangeAddress'
 import { Claim } from './components/Claim/Claim'
 import { Stake } from './components/Stake/Stake'
 import { Unstake } from './components/Unstake/Unstake'
 
-type FormHeaderTabProps = {
-  index: number
-  onClick: (index: number) => void
-  isActive?: boolean
-} & PropsWithChildren
-
-const activeStyle = { color: 'text.base' }
+import { FormHeader } from '@/components/FormHeader'
 
 export const RfoxTabIndex = {
   Stake: 0,
@@ -23,75 +15,23 @@ export const RfoxTabIndex = {
   ChangeAddress: 3,
 }
 
-const FormHeaderTab: React.FC<FormHeaderTabProps> = ({ index, onClick, isActive, children }) => {
-  const handleClick = useCallback(() => {
-    onClick(index)
-  }, [index, onClick])
-  return (
-    <Button
-      onClick={handleClick}
-      isActive={isActive}
-      variant='unstyled'
-      color='text.subtle'
-      _active={activeStyle}
-    >
-      {children}
-    </Button>
-  )
-}
-type FormHeaderProps = {
-  setStepIndex: (index: number) => void
-  activeIndex: number
-}
-const FormHeader: React.FC<FormHeaderProps> = ({ setStepIndex, activeIndex }) => {
-  const translate = useTranslate()
-  const handleClick = useCallback(
-    (index: number) => {
-      setStepIndex(index)
-    },
-    [setStepIndex],
-  )
-  return (
-    <Flex px={6} py={4} gap={4} wrap='wrap'>
-      <FormHeaderTab
-        index={RfoxTabIndex.Stake}
-        onClick={handleClick}
-        isActive={activeIndex === RfoxTabIndex.Stake}
-      >
-        {translate('RFOX.stake')}
-      </FormHeaderTab>
-      <FormHeaderTab
-        index={RfoxTabIndex.Unstake}
-        onClick={handleClick}
-        isActive={activeIndex === RfoxTabIndex.Unstake}
-      >
-        {translate('RFOX.unstake')}
-      </FormHeaderTab>
-      <FormHeaderTab
-        index={RfoxTabIndex.Claim}
-        onClick={handleClick}
-        isActive={activeIndex === RfoxTabIndex.Claim}
-      >
-        {translate('RFOX.claim')}
-      </FormHeaderTab>
-      <FormHeaderTab
-        index={RfoxTabIndex.ChangeAddress}
-        onClick={handleClick}
-        isActive={activeIndex === RfoxTabIndex.ChangeAddress}
-      >
-        {translate('RFOX.changeAddress')}
-      </FormHeaderTab>
-    </Flex>
-  )
-}
+const FormHeaderItems = [
+  { label: 'RFOX.stake', index: RfoxTabIndex.Stake },
+  { label: 'RFOX.unstake', index: RfoxTabIndex.Unstake },
+  { label: 'RFOX.claim', index: RfoxTabIndex.Claim },
+  { label: 'RFOX.changeAddress', index: RfoxTabIndex.ChangeAddress },
+]
 
 export const Widget: React.FC = () => {
   const [stepIndex, setStepIndex] = useState(RfoxTabIndex.Stake)
 
   const TabHeader = useMemo(
-    () => <FormHeader setStepIndex={setStepIndex} activeIndex={stepIndex} />,
+    () => (
+      <FormHeader items={FormHeaderItems} setStepIndex={setStepIndex} activeIndex={stepIndex} />
+    ),
     [stepIndex],
   )
+
   return (
     <Card width='full'>
       <Tabs variant='unstyled' index={stepIndex} isLazy>

585057c060bcb62fb5efe897164ad3bf9955cd0c
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-05-05 21:37:12 +0200

    feat: tcy reusable status components (#9472)

--- src/pages/TCY/components/Stake/StakeStatus.tsx
+++ src/pages/TCY/components/Stake/StakeStatus.tsx
@@ -1,23 +1,9 @@
-import { CheckCircleIcon, WarningTwoIcon } from '@chakra-ui/icons'
-import { tcyAssetId } from '@shapeshiftoss/caip'
-import { SwapperName } from '@shapeshiftoss/swapper'
-import { TxStatus } from '@shapeshiftoss/unchained-client'
-import { useCallback, useMemo } from 'react'
 import { useWatch } from 'react-hook-form'
 import { useTranslate } from 'react-polyglot'
-import { useNavigate } from 'react-router'
 
 import { TCYStakeRoute } from '../../types'
+import { ReusableStatus } from '../ReusableStatus'
 import type { StakeFormValues } from './Stake'
-
-import { SlideTransition } from '@/components/SlideTransition'
-import { TransactionStatusDisplay } from '@/components/TransactionStatusDisplay/TransactionStatusDisplay'
-import { useSafeTxQuery } from '@/hooks/queries/useSafeTx'
-import { useTxStatus } from '@/hooks/useTxStatus/useTxStatus'
-import { bnOrZero } from '@/lib/bignumber/bignumber'
-import { getTxLink } from '@/lib/getTxLink'
-import { selectAssetById } from '@/state/slices/selectors'
-import { useAppSelector } from '@/state/store'
 
 type StakeStatusProps = {
   txId: string
@@ -31,112 +17,24 @@ export const StakeStatus: React.FC<StakeStatusProps> = ({
   onTxConfirmed: handleTxConfirmed,
 }) => {
   const translate = useTranslate()
-  const navigate = useNavigate()
-  const tcyAsset = useAppSelector(state => selectAssetById(state, tcyAssetId))
+  const formValues = useWatch<StakeFormValues>()
 
-  const { amountCryptoPrecision, accountId } = useWatch<StakeFormValues>()
-
-  const txStatus = useTxStatus({
-    accountId: accountId ?? '',
-    txHash: txId,
-    onTxStatusConfirmed: handleTxConfirmed,
-  })
-
-  const { data: maybeSafeTx } = useSafeTxQuery({
-    maybeSafeTxHash: txId ?? undefined,
-    accountId,
-  })
-
-  const txLink = useMemo(
-    () =>
-      getTxLink({
-        txId,
-        defaultExplorerBaseUrl: tcyAsset?.explorerTxLink ?? '',
-        accountId,
-        maybeSafeTx,
-        stepSource: SwapperName.Thorchain,
-      }),
-    [accountId, maybeSafeTx, tcyAsset?.explorerTxLink, txId],
-  )
-
-  const handleViewTransaction = useCallback(() => {
-    if (txLink) window.open(txLink, '_blank')
-  }, [txLink])
-
-  const handleGoBack = useCallback(() => {
-    navigate(TCYStakeRoute.Input)
-  }, [navigate])
-
-  const renderStatus = () => {
-    if (!tcyAsset) return null
-
-    if (maybeSafeTx?.isQueuedSafeTx) {
-      return (
-        <TransactionStatusDisplay
-          isLoading
-          title={translate('common.safeProposalQueued', {
-            currentConfirmations: maybeSafeTx?.transaction?.confirmations?.length,
-            confirmationsRequired: maybeSafeTx?.transaction?.confirmationsRequired,
-          })}
-          subtitle={translate('TCY.stakePending', {
-            amount: bnOrZero(amountCryptoPrecision).toFixed(8),
-            symbol: tcyAsset.symbol,
-          })}
-          primaryButtonText={translate('trade.viewTransaction')}
-          onPrimaryClick={handleViewTransaction}
-        />
-      )
-    }
-
-    if (maybeSafeTx?.isExecutedSafeTx && maybeSafeTx?.transaction?.transactionHash) {
-      setStakeTxid(maybeSafeTx.transaction.transactionHash)
-    }
-
-    switch (txStatus) {
-      case undefined:
-      case TxStatus.Pending:
-        return (
-          <TransactionStatusDisplay
-            isLoading
-            title={translate('pools.waitingForConfirmation')}
-            subtitle={translate('TCY.stakeStatus.pendingSubtitle', {
-              amount: bnOrZero(amountCryptoPrecision).toFixed(8),
-              symbol: tcyAsset.symbol,
-            })}
-            primaryButtonText={translate('TCY.stakeStatus.viewTransaction')}
-            onPrimaryClick={handleViewTransaction}
-          />
-        )
-      case TxStatus.Confirmed:
-        return (
-          <TransactionStatusDisplay
-            icon={CheckCircleIcon}
-            iconColor='green.500'
-            title={translate('TCY.stakeStatus.successTitle')}
-            subtitle={translate('TCY.stakeStatus.successSubtitle', {
-              amount: bnOrZero(amountCryptoPrecision).toFixed(8),
-              symbol: tcyAsset.symbol,
-            })}
-            secondaryButtonText={translate('TCY.stakeStatus.viewTransaction')}
-            onSecondaryClick={handleViewTransaction}
-            primaryButtonText={translate('TCY.stakeStatus.goBack')}
-            onPrimaryClick={handleGoBack}
-          />
-        )
-      case TxStatus.Failed:
-        return (
-          <TransactionStatusDisplay
-            icon={WarningTwoIcon}
-            iconColor='red.500'
-            title={translate('TCY.stakeStatus.failedTitle')}
-            subtitle={translate('TCY.stakeStatus.failedSubtitle')}
-            primaryButtonText={translate('TCY.stakeStatus.goBack')}
-            onPrimaryClick={handleGoBack}
-          />
-        )
-      default:
-        return null
-    }
+  if (!formValues.amountCryptoPrecision || !formValues.accountId) {
+    return null
   }
-  return <SlideTransition>{renderStatus()}</SlideTransition>
+
+  return (
+    <ReusableStatus
+      txId={txId}
+      setTxId={setStakeTxid}
+      onTxConfirmed={handleTxConfirmed}
+      translationPrefix='stake'
+      accountId={formValues.accountId}
+      amountCryptoPrecision={formValues.amountCryptoPrecision}
+      isDialog
+      headerText={translate('TCY.stakeConfirm.confirmTitle')}
+      initialRoute={TCYStakeRoute.Input}
+      displayGoBack
+    />
+  )
 }

96b25dd88db5060c233336d79a05973c45d633e2
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-05-05 12:09:16 +0300

    feat: add ResultsEmptyNoWallet

---
+++ src/components/ResultsEmptyNoWallet.tsx
@@ -0,0 +1,40 @@
+import { ArrowForwardIcon } from '@chakra-ui/icons'
+import type { ButtonProps } from '@chakra-ui/react'
+import { useCallback } from 'react'
+
+import type { ResultsEmptyProps } from './ResultsEmpty'
+import { ResultsEmpty } from './ResultsEmpty'
+
+import { WalletActions } from '@/context/WalletProvider/actions'
+import { useWallet } from '@/hooks/useWallet/useWallet'
+
+export const ResultsEmptyNoWallet: React.FC<ResultsEmptyProps> = ({
+  icon,
+  title,
+  body,
+  children,
+}) => {
+  const { dispatch } = useWallet()
+
+  const handleConnect = useCallback(() => {
+    dispatch({ type: WalletActions.SET_WALLET_MODAL, payload: true })
+  }, [dispatch])
+
+  const buttonProps: ButtonProps = {
+    colorScheme: 'blue',
+    onClick: handleConnect,
+    rightIcon: <ArrowForwardIcon />,
+  }
+
+  return (
+    <ResultsEmpty
+      icon={icon}
+      title={title}
+      body={body}
+      ctaText='common.connectWallet'
+      buttonProps={buttonProps}
+    >
+      {children}
+    </ResultsEmpty>
+  )
+}

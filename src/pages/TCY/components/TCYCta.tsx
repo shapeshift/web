9c4af6ca2b6ba2551b58326569b9e208b2c6bd8f
reallybeard <89934888+reallybeard@users.noreply.github.com>
2025-05-08 15:48:22 -0500

    Update TCYCta.tsx

--- src/pages/TCY/components/TCYCta.tsx
+++ src/pages/TCY/components/TCYCta.tsx
@@ -1,12 +1,14 @@
 import { Alert, AlertTitle, Button, Skeleton, SkeletonCircle, Stack } from '@chakra-ui/react'
 import { tcyAssetId } from '@shapeshiftoss/caip'
-import { Suspense, useCallback, useMemo } from 'react'
+import { useCallback, useMemo } from 'react'
 import { useTranslate } from 'react-polyglot'
+import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router'
 
 import { useTCYClaims } from '../queries/useTcyClaims'
 
 import { AssetIcon } from '@/components/AssetIcon'
+import { selectIsPortfolioLoading } from '@/state/slices/selectors'
 
 const margin = { base: 4, md: 0 }
 
@@ -22,39 +24,28 @@ const CtaSkeleton = () => {
   )
 }
 
-const suspenseFallback = <CtaSkeleton />
-
-export const TCYCtaContent = () => {
+export const TCYCta = () => {
   const navigate = useNavigate()
+  const loading = useSelector(selectIsPortfolioLoading)
   const translate = useTranslate()
   const claimsQuery = useTCYClaims('all')
-  const hasClaims = useMemo(() => claimsQuery.some(query => query.data.length), [claimsQuery])
+  const hasClaims = useMemo(() => !claimsQuery.some(query => query.data.length), [claimsQuery])
 
   const handleClick = useCallback(() => {
     navigate('/tcy')
   }, [navigate])
 
-  return (
-    <Suspense fallback={suspenseFallback}>
-      <Alert status='info' m={margin} width='auto'>
-        <AssetIcon assetId={tcyAssetId} />
-        <Stack spacing={0} ml={4}>
-          <AlertTitle>
-            {translate(hasClaims ? 'TCY.cta.hasClaimsTitle' : 'TCY.cta.title')}
-          </AlertTitle>
-        </Stack>
-        <Button ml='auto' flexShrink={0} onClick={handleClick}>
-          {translate('TCY.cta.button')}
-        </Button>
-      </Alert>
-    </Suspense>
-  )
-}
+  if (loading) return <CtaSkeleton />
 
-export const TCYCta = () => {
   return (
-    <Suspense fallback={suspenseFallback}>
-      <TCYCtaContent />
-    </Suspense>
+    <Alert status='info' m={margin} width='auto'>
+      <AssetIcon assetId={tcyAssetId} />
+      <Stack spacing={0} ml={4}>
+        <AlertTitle>{translate(hasClaims ? 'TCY.cta.hasClaimsTitle' : 'TCY.cta.title')}</AlertTitle>
+      </Stack>
+      <Button ml='auto' flexShrink={0} onClick={handleClick}>
+        {translate('TCY.cta.button')}
+      </Button>
+    </Alert>
   )
 }

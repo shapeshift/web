007840d6f5b91b55f964da09016ab9702cc22009
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-05-12 18:38:29 +0300

    feat: cleanup

--- src/lib/utils/thorchain/utils/getMimir.ts
+++
@@ -1,8 +0,0 @@
-import type { ThorchainMimir } from '../types'
-
-export const getMimir = <T extends keyof ThorchainMimir>(
-  mimir: ThorchainMimir,
-  key: T,
-): ThorchainMimir[T] => {
-  return mimir[key]
-}
--- src/lib/utils/thorchain/utils/isChainHalted.ts
+++ src/lib/utils/thorchain/utils/isChainHalted.ts
@@ -2,7 +2,6 @@ import type { ChainId } from '@shapeshiftoss/caip'
 import { assetIdToPoolAssetId } from '@shapeshiftoss/swapper'
 
 import type { ThorchainMimir } from '../types'
-import { getMimir } from './getMimir'
 
 import { getChainAdapterManager } from '@/context/PluginProvider/chainAdapterSingleton'
 import { bn } from '@/lib/bignumber/bignumber'
@@ -22,27 +21,21 @@ export const isChainHalted = ({ mimir, blockHeight, chainId }: IsChainHaltedPara
 
   const chainThorNotation = poolAssetId.split('.')[0]
 
-  const HALTCHAINGLOBAL = getMimir(mimir, 'HALTCHAINGLOBAL')
-
   // Check global halt
-  if (HALTCHAINGLOBAL) return true
-
-  const NODEPAUSECHAINGLOBAL = getMimir(mimir, 'NODEPAUSECHAINGLOBAL')
+  if (mimir.HALTCHAINGLOBAL) return true
 
   // Check node global halt
-  if (bn(NODEPAUSECHAINGLOBAL).gt(blockHeight)) return true
+  if (bn(mimir.NODEPAUSECHAINGLOBAL).gt(blockHeight)) return true
 
   // Check chain-specific halt via admin or double-spend check
   const haltChainKey = `HALT${chainThorNotation}CHAIN`
-  const HALTCHAIN = getMimir(mimir, haltChainKey)
 
-  if (HALTCHAIN) return true
+  if (mimir[haltChainKey]) return true
 
   // Check chain-specific solvency halt
   const solvencyHaltChainKey = `SOLVENCYHALT${chainThorNotation}CHAIN`
-  const SOLVENCYHALTCHAIN = getMimir(mimir, solvencyHaltChainKey)
 
-  if (SOLVENCYHALTCHAIN) return true
+  if (mimir[solvencyHaltChainKey]) return true
 
   return false
 }

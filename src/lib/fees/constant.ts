c3a0d29515e5bf8146ef0d0f23571b1b93cb9057
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-05-13 17:53:58 +0300

    fix: types

--- src/lib/fees/constant.ts
+++ src/lib/fees/constant.ts
@@ -1,4 +1,4 @@
 // This timestamp is the 12 of February 2025 10:30 PST
 export const FOX_WIF_HAT_CAMPAIGN_STARTING_TIME_MS = 1739385000000
 
-export const DEFAULT_FEE_BPS = 55 // basis points
+export const DEFAULT_FEE_BPS = '55' // basis points

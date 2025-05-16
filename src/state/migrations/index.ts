ca8ada90bbce75f3129bbbd1ed234dc6d9bc14ba
GitHub Action <action@github.com>
2025-05-16 09:13:04 +0000

    feat: regenerate asset data 05/16/2025

--- src/state/migrations/index.ts
+++ src/state/migrations/index.ts
@@ -121,6 +121,7 @@ export const clearAssetsMigrations = {
   84: clearAssets,
   85: clearAssets,
   86: clearAssets,
+  87: clearAssets,
 } as unknown as Omit<MigrationManifest, '_persist'>
 
 export const clearMarketDataMigrations = {

c5d606809f1ba614364618c995234c9158bdc562
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-05-22 21:32:37 +0200

    fix: don't display MAYA balance as CACAO (#9611)

--- packages/caip/src/utils.ts
+++ packages/caip/src/utils.ts
@@ -6,6 +6,8 @@ import type { ChainId, ChainNamespace, ChainReference } from './chainId/chainId'
 import * as constants from './constants'
 import { mayaTokenAssetId, tcyAssetId } from './constants'
 
+const mayaTokenAssetId: AssetId = 'cosmos:mayachain-mainnet-v1/slip44:maya'
+
 export const accountIdToChainId = (accountId: AccountId): ChainId =>
   fromAccountId(accountId).chainId
 

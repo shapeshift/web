d9be438efb3d683e8c66689947cdae8d0a3f9de7
kevin <35275952+kaladinlight@users.noreply.github.com>
2025-06-10 11:00:39 -0600

    feat: mayachain swapper package (#9623)

---
+++ packages/swapper/src/thorchain-utils/getQuote.ts
@@ -0,0 +1,119 @@
+import type { AssetId } from '@shapeshiftoss/caip'
+import { bchAssetId } from '@shapeshiftoss/caip'
+import type { Asset } from '@shapeshiftoss/types'
+import { bn, fromBaseUnit, toBaseUnit } from '@shapeshiftoss/utils'
+import type { Result } from '@sniptt/monads'
+import { Err, Ok } from '@sniptt/monads'
+import qs from 'qs'
+
+import type { SwapErrorRight, SwapperDeps, SwapperName } from '../types'
+import { TradeQuoteError } from '../types'
+import { createTradeAmountTooSmallErr, makeSwapErrorRight } from '../utils'
+import { getThresholdedAffiliateBps } from './getThresholdedAffiliateBps/getThresholdedAffiliateBps'
+import { getAffiliate, getDaemonUrl, getNativePrecision, getPoolAssetId } from './index'
+import { thorService } from './service'
+import type { ThornodeQuoteResponse, ThornodeQuoteResponseSuccess } from './types'
+
+type GetQuoteArgs = {
+  sellAsset: Asset
+  buyAssetId: AssetId
+  sellAmountCryptoBaseUnit: string
+  // Receive address is optional for quotes, and will be in case we are getting a quote with a missing manual receive address
+  receiveAddress: string | undefined
+  affiliateBps: string
+  swapperName: SwapperName
+} & (
+  | {
+      streaming: true
+      streamingInterval: number
+    }
+  | {
+      streaming?: false
+      streamingInterval?: never
+    }
+)
+
+export const getQuote = async (
+  input: GetQuoteArgs,
+  deps: SwapperDeps,
+): Promise<Result<ThornodeQuoteResponseSuccess, SwapErrorRight>> => {
+  const {
+    buyAssetId,
+    receiveAddress,
+    sellAsset,
+    sellAmountCryptoBaseUnit,
+    streaming,
+    streamingInterval,
+    affiliateBps,
+    swapperName,
+  } = input
+
+  // don't apply an affiliate fee if it's below the outbound fee for the inbound pool
+  const thresholdedAffiliateBps = await getThresholdedAffiliateBps({
+    sellAsset,
+    sellAmountCryptoBaseUnit,
+    affiliateBps,
+    config: deps.config,
+    swapperName,
+  })
+
+  const buyPoolId = getPoolAssetId({ assetId: buyAssetId, swapperName })
+  const sellPoolId = getPoolAssetId({ assetId: sellAsset.assetId, swapperName })
+
+  const sellAmountCryptoPrecision = fromBaseUnit(sellAmountCryptoBaseUnit, sellAsset.precision)
+
+  // All pool amounts are native precision regardless of token precision
+  const sellAmountCryptoThorBaseUnit = bn(
+    toBaseUnit(sellAmountCryptoPrecision, getNativePrecision(swapperName)),
+  )
+
+  // The swap endpoint expects BCH receiveAddress's to be stripped of the "bitcoincash:" prefix
+  const parsedReceiveAddress =
+    receiveAddress && buyAssetId === bchAssetId
+      ? receiveAddress.replace('bitcoincash:', '')
+      : receiveAddress
+
+  const queryString = qs.stringify({
+    amount: sellAmountCryptoThorBaseUnit.toString(),
+    from_asset: sellPoolId,
+    to_asset: buyPoolId,
+    destination: parsedReceiveAddress,
+    affiliate_bps: thresholdedAffiliateBps,
+    affiliate: getAffiliate(swapperName),
+    ...(streaming && { streaming_interval: streamingInterval }),
+  })
+
+  const daemonUrl = getDaemonUrl(deps.config, swapperName)
+  const res = await thorService.get<ThornodeQuoteResponse>(`${daemonUrl}/quote/swap?${queryString}`)
+  if (res.isErr()) return Err(res.unwrapErr())
+
+  const { data } = res.unwrap()
+  const isError = 'error' in data
+
+  const notEnoughFeeError = isError && /not enough fee/i.test(data.error)
+  const notEnoughToPayTxFeeError = isError && /not enough to pay transaction fee/i.test(data.error)
+  const tradingIsHaltedError = isError && /trading is halted/.test(data.error)
+
+  if (notEnoughFeeError || notEnoughToPayTxFeeError) return Err(createTradeAmountTooSmallErr())
+
+  if (tradingIsHaltedError) {
+    return Err(
+      makeSwapErrorRight({
+        message: `[getQuote]: Trading is halted, cannot process swap`,
+        code: TradeQuoteError.TradingHalted,
+        details: { sellAssetId: sellAsset.assetId, buyAssetId },
+      }),
+    )
+  }
+
+  if (isError) {
+    return Err(
+      makeSwapErrorRight({
+        message: data.error,
+        code: TradeQuoteError.UnknownError,
+      }),
+    )
+  }
+
+  return Ok(data)
+}
---
+++ scripts/generateTradableAssetMap/index.ts
@@ -0,0 +1,84 @@
+import type { AssetId } from '@shapeshiftoss/caip'
+import { mayachainAssetId, tcyAssetId, thorchainAssetId } from '@shapeshiftoss/caip'
+import type { ThornodePoolResponse } from '@shapeshiftoss/swapper'
+import axios from 'axios'
+import fs from 'fs'
+import { isNull } from 'lodash'
+import isUndefined from 'lodash/isUndefined'
+import path from 'path'
+
+import type { AssetIdPair } from './types'
+import { getAssetIdPairFromPool } from './utils'
+
+const axiosConfig = {
+  timeout: 10000,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+}
+
+const isSome = <T>(option: T | null | undefined): option is T =>
+  !isUndefined(option) && !isNull(option)
+
+export const generateTradableAssetMap = async (
+  url: string,
+  nativePairs: AssetIdPair[],
+  swapper: string,
+) => {
+  const thorService = axios.create(axiosConfig)
+
+  const response = await thorService.get<ThornodePoolResponse[]>(url)
+
+  switch (response.status) {
+    case 200:
+      const poolData = response.data
+
+      // native asset is not included in the pools list, so it needs to be manually added as a tradable asset
+      const assetIdPairs: AssetIdPair[] = [
+        ...poolData.map(getAssetIdPairFromPool).filter(isSome),
+        ...nativePairs,
+      ]
+
+      const assetsRecord = assetIdPairs.reduce<Record<string, AssetId>>((acc, [asset, assetId]) => {
+        acc[asset] = assetId
+        return acc
+      }, {})
+
+      await fs.promises.writeFile(
+        path.join(
+          __dirname,
+          `../../packages/swapper/src/swappers/${swapper}/generated/generatedTradableAssetMap.json`,
+        ),
+        // beautify the file for github diff.
+        JSON.stringify(assetsRecord, null, 2),
+      )
+
+      break
+    default:
+      console.error('Network error', response)
+  }
+}
+
+generateTradableAssetMap(
+  'https://daemon.thorchain.shapeshift.com/lcd/thorchain/pools',
+  [
+    ['THOR.RUNE', thorchainAssetId],
+    ['THOR.TCY', tcyAssetId],
+  ],
+  'ThorchainSwapper',
+)
+  .then(() => {
+    console.info('done')
+  })
+  .catch(err => console.info(err))
+
+generateTradableAssetMap(
+  'https://daemon.mayachain.shapeshift.com/lcd/mayachain/pools',
+  [['MAYA.CACAO', mayachainAssetId]],
+  'MayachainSwapper',
+)
+  .then(() => {
+    console.info('done')
+  })
+  .catch(err => console.info(err))

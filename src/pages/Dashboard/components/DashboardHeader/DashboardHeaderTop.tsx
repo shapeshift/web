d14ab5638fd86dfbb6803905eaf4f14fedd403b7
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-06-24 23:59:46 +0200

    [skip ci] wip: mobile wallet buttons

--- src/pages/Dashboard/components/DashboardHeader/DashboardHeaderTop.tsx
+++ src/pages/Dashboard/components/DashboardHeader/DashboardHeaderTop.tsx
@@ -1,8 +1,10 @@
 import { ArrowDownIcon, ArrowUpIcon } from '@chakra-ui/icons'
 import type { ResponsiveValue } from '@chakra-ui/react'
-import { Button, Container, Flex, IconButton, useDisclosure } from '@chakra-ui/react'
+import { Button, Container, Flex, IconButton, Text, useDisclosure, VStack } from '@chakra-ui/react'
 import type { Property } from 'csstype'
-import { memo, useCallback } from 'react'
+import { memo, useCallback, useMemo } from 'react'
+import { isMobile } from 'react-device-detect'
+import { FaRegCreditCard } from 'react-icons/fa'
 import { FiSettings } from 'react-icons/fi'
 import { IoEllipsisHorizontal, IoSwapVerticalSharp } from 'react-icons/io5'
 import { useTranslate } from 'react-polyglot'
@@ -13,12 +15,16 @@ import { DashboardDrawer } from './DashboardDrawer'
 import { WalletBalance } from './WalletBalance'
 
 import { QRCodeIcon } from '@/components/Icons/QRCode'
+import { SendIcon } from '@/components/Icons/SendIcon'
+import { SwapIcon } from '@/components/Icons/SwapIcon'
 import { MobileWalletDialog } from '@/components/MobileWalletDialog/MobileWalletDialog'
+import { FiatRampAction } from '@/components/Modals/FiatRamps/FiatRampsCommon'
 import { useModal } from '@/hooks/useModal/useModal'
 import { useWallet } from '@/hooks/useWallet/useWallet'
-import { isMobile } from '@/lib/globals'
 import { getMixPanel } from '@/lib/mixpanel/mixPanelSingleton'
 import { MixPanelEvent } from '@/lib/mixpanel/types'
+import { selectHighestMarketCapFeeAsset } from '@/state/slices/selectors'
+import { useAppSelector } from '@/state/store'
 
 const qrCodeIcon = <QRCodeIcon />
 const arrowUpIcon = <ArrowUpIcon />
@@ -33,8 +39,34 @@ const containerGap = { base: 6, md: 6 }
 const containerInnerFlexDir: ResponsiveValue<Property.FlexDirection> = { base: 'column', md: 'row' }
 const buttonGroupDisplay = { base: 'none', md: 'flex' }
 
-const profileGridColumn = { base: 2, md: 1 }
 const profileGridTemplate = { base: '1fr 1fr 1fr', md: '1fr 1fr' }
+
+type MobileButtonProps = {
+  icon: React.ReactNode
+  label: string
+  onClick: () => void
+  isDisabled?: boolean
+}
+
+const MobileButton = ({ icon, label, onClick, isDisabled }: MobileButtonProps) => (
+  <Button
+    size='md'
+    width='75px'
+    height='75px'
+    bg='rgba(127, 153, 251, 0.2)'
+    borderRadius='xl'
+    alignItems='center'
+    onClick={onClick}
+    isDisabled={isDisabled}
+  >
+    <VStack spacing={2} justify='center' align='center'>
+      {icon}
+      <Text fontSize='sm' fontWeight='medium'>
+        {label}
+      </Text>
+    </VStack>
+  </Button>
+)
 
 export const DashboardHeaderTop = memo(() => {
   const { isOpen, onClose, onOpen } = useDisclosure()
@@ -48,6 +80,9 @@ export const DashboardHeaderTop = memo(() => {
   const send = useModal('send')
   const receive = useModal('receive')
   const qrCode = useModal('qrCode')
+  const fiatRamps = useModal('fiatRamps')
+
+  const defaultAsset = useAppSelector(selectHighestMarketCapFeeAsset)
 
   const handleQrCodeClick = useCallback(() => {
     qrCode.open({})
@@ -66,6 +101,77 @@ export const DashboardHeaderTop = memo(() => {
     navigate('/trade')
   }, [navigate])
 
+  const handleBuyClick = useCallback(() => {
+    if (!defaultAsset) return
+    fiatRamps.open({ assetId: defaultAsset.assetId, fiatRampAction: FiatRampAction.Buy })
+  }, [fiatRamps, defaultAsset])
+
+  const mobileButtons = useMemo(
+    () => (
+      <Flex mt={4} gap={6}>
+        <MobileButton
+          icon={<SwapIcon boxSize={6} />}
+          label={translate('navBar.tradeShort')}
+          onClick={handleTradeClick}
+        />
+        <MobileButton
+          icon={<FaRegCreditCard size={24} />}
+          label={translate('fiatRamps.buy')}
+          onClick={handleBuyClick}
+          isDisabled={!isConnected}
+        />
+        <MobileButton
+          icon={<SendIcon boxSize='6' />}
+          label={translate('common.send')}
+          onClick={handleSendClick}
+          isDisabled={!isConnected}
+        />
+        <MobileButton
+          icon={<ArrowDownIcon boxSize={6} />}
+          label={translate('common.receive')}
+          onClick={handleReceiveClick}
+          isDisabled={!isConnected}
+        />
+      </Flex>
+    ),
+    [
+      handleTradeClick,
+      handleBuyClick,
+      handleSendClick,
+      handleReceiveClick,
+      isConnected,
+      translate,
+      defaultAsset,
+    ],
+  )
+
+  const desktopButtons = useMemo(
+    () => (
+      <>
+        <Button isDisabled={!isConnected} onClick={handleQrCodeClick} leftIcon={qrCodeIcon}>
+          {translate('modals.send.qrCode')}
+        </Button>
+        <Button isDisabled={!isConnected} onClick={handleSendClick} leftIcon={arrowUpIcon}>
+          {translate('common.send')}
+        </Button>
+        <Button isDisabled={!isConnected} onClick={handleReceiveClick} leftIcon={arrowDownIcon}>
+          {translate('common.receive')}
+        </Button>
+        <Button onClick={handleTradeClick} leftIcon={ioSwapVerticalSharpIcon}>
+          {translate('navBar.tradeShort')}
+        </Button>
+      </>
+    ),
+    [
+      handleQrCodeClick,
+      handleSendClick,
+      handleReceiveClick,
+      handleTradeClick,
+      isConnected,
+      translate,
+    ],
+  )
+
   return (
     <Container
       width='full'
@@ -79,14 +185,10 @@ export const DashboardHeaderTop = memo(() => {
       justifyContent='space-between'
       gap={containerGap}
     >
-      <Flex
-        alignItems='center'
-        flexDir={containerInnerFlexDir}
-        gap={4}
-        gridColumn={profileGridColumn}
-      >
+      <Flex alignItems='center' flexDir={containerInnerFlexDir} gap={4}>
         <ProfileAvatar />
         <WalletBalance />
+        {isMobile && mobileButtons}
       </Flex>
       <Flex
         gridColumn={3}
@@ -95,18 +197,7 @@ export const DashboardHeaderTop = memo(() => {
         justifyContent={'center'}
         display={buttonGroupDisplay}
       >
-        <Button isDisabled={!isConnected} onClick={handleQrCodeClick} leftIcon={qrCodeIcon}>
-          {translate('modals.send.qrCode')}
-        </Button>
-        <Button isDisabled={!isConnected} onClick={handleSendClick} leftIcon={arrowUpIcon}>
-          {translate('common.send')}
-        </Button>
-        <Button isDisabled={!isConnected} onClick={handleReceiveClick} leftIcon={arrowDownIcon}>
-          {translate('common.receive')}
-        </Button>
-        <Button onClick={handleTradeClick} leftIcon={ioSwapVerticalSharpIcon}>
-          {translate('navBar.tradeShort')}
-        </Button>
+        {!isMobile && desktopButtons}
       </Flex>
       <Flex
         justifyContent='flex-end'

79c42a623327e69bfb2160f488ab4401d88aed01
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-06-25 15:17:11 +0200

    fix: fuarkin css mate

--- src/pages/Dashboard/components/DashboardHeader/DashboardHeaderTop.tsx
+++ src/pages/Dashboard/components/DashboardHeader/DashboardHeaderTop.tsx
@@ -12,7 +12,6 @@ import {
 } from '@chakra-ui/react'
 import type { Property } from 'csstype'
 import { memo, useCallback, useMemo } from 'react'
-import { isMobile } from 'react-device-detect'
 import { FaExpand, FaRegCreditCard } from 'react-icons/fa'
 import { FiSettings } from 'react-icons/fi'
 import { IoEllipsisHorizontal, IoSwapVerticalSharp } from 'react-icons/io5'
@@ -23,6 +22,7 @@ import { ProfileAvatar } from '../ProfileAvatar/ProfileAvatar'
 import { DashboardDrawer } from './DashboardDrawer'
 import { WalletBalance } from './WalletBalance'
 
+import { Display } from '@/components/Display'
 import { SendIcon } from '@/components/Icons/SendIcon'
 import { SwapIcon } from '@/components/Icons/SwapIcon'
 import { GlobalSearchModal } from '@/components/Layout/Header/GlobalSearch/GlobalSearchModal'
@@ -36,21 +36,18 @@ import { MixPanelEvent } from '@/lib/mixpanel/types'
 import { selectHighestMarketCapFeeAsset } from '@/state/slices/selectors'
 import { useAppSelector } from '@/state/store'
 
+const mobileButtonRowDisplay = { base: 'flex', md: 'none' }
+const desktopButtonGroupDisplay = { base: 'none', md: 'flex' }
+const containerPadding = { base: 6, '2xl': 8 }
+const containerGap = { base: 6, md: 6 }
+const containerInnerFlexDir: ResponsiveValue<Property.FlexDirection> = { base: 'column', md: 'row' }
+const profileGridColumn = { base: 2, md: 1 }
+const profileGridTemplate = { base: '1fr auto 1fr', md: '1fr 1fr' }
+
 const arrowUpIcon = <ArrowUpIcon />
 const arrowDownIcon = <ArrowDownIcon />
 const ioSwapVerticalSharpIcon = <IoSwapVerticalSharp />
 const moreIcon = isMobileApp ? <FiSettings /> : <IoEllipsisHorizontal />
-
-const mobileButtonRowDisplay = { base: 'flex', md: 'none' }
-const desktopButtonGroupDisplay = { base: 'none', md: 'flex' }
-
-const containerPadding = { base: 6, '2xl': 8 }
-const containerGap = { base: 6, md: 6 }
-const containerInnerFlexDir: ResponsiveValue<Property.FlexDirection> = { base: 'column', md: 'row' }
-
-const profileGridColumn = { base: 2, md: 1 }
-const profileGridTemplate = { base: '1fr 1fr 1fr', md: '1fr 1fr' }
-
 const swapIcon = <SwapIcon boxSize={6} color='blue.200' />
 const buyIcon = (
   <Box color='blue.200'>
@@ -61,6 +58,13 @@ const sendIcon = <SendIcon boxSize='6' color='blue.200' />
 const receiveIcon = <ArrowDownIcon boxSize={6} color='blue.200' />
 const searchIcon = <SearchIcon />
 const qrCodeIcon = <FaExpand />
+
+const netWorth = (
+  <Flex alignItems='center' flexDir={containerInnerFlexDir} gap={4} gridColumn={profileGridColumn}>
+    <ProfileAvatar />
+    <WalletBalance />
+  </Flex>
+)
 
 type MobileButtonProps = {
   icon: React.ReactNode
@@ -166,7 +170,13 @@ export const DashboardHeaderTop = memo(() => {
 
   const desktopButtons = useMemo(
     () => (
-      <>
+      <Flex
+        gridColumn={3}
+        gap={4}
+        flexWrap={'wrap'}
+        justifyContent={'center'}
+        display={desktopButtonGroupDisplay}
+      >
         <Button isDisabled={!isConnected} onClick={handleQrCodeClick} leftIcon={qrCodeIcon}>
           {translate('modals.send.qrCode')}
         </Button>
@@ -179,7 +189,7 @@ export const DashboardHeaderTop = memo(() => {
         <Button onClick={handleTradeClick} leftIcon={ioSwapVerticalSharpIcon}>
           {translate('navBar.tradeShort')}
         </Button>
-      </>
+      </Flex>
     ),
     [
       handleQrCodeClick,
@@ -191,71 +201,86 @@ export const DashboardHeaderTop = memo(() => {
     ],
   )
 
-  const drawer = useMemo(() => {
-    if (!isMobile) return null
-
+  const mobileDrawer = useMemo(() => {
     if (isMobileApp) return <MobileWalletDialog isOpen={isOpen} onClose={onClose} />
     return <DashboardDrawer isOpen={isOpen} onClose={onClose} />
   }, [isOpen, onClose])
 
   return (
     <>
-      <Container
-        width='full'
-        display='grid'
-        gridTemplateColumns={profileGridTemplate}
-        maxWidth='container.4xl'
-        px={containerPadding}
-        pt={4}
-        pb={4}
-        alignItems='flex-start'
-        justifyContent='space-between'
-        gap={containerGap}
-      >
-        <Flex
-          alignItems='center'
-          flexDir={containerInnerFlexDir}
-          gap={4}
-          gridColumn={profileGridColumn}
+      <Display.Mobile>
+        <Container
+          width='full'
+          display='grid'
+          gridTemplateColumns={profileGridTemplate}
+          maxWidth='container.4xl'
+          px={containerPadding}
+          pt={4}
+          pb={4}
+          gap={containerGap}
         >
-          <ProfileAvatar />
-          <WalletBalance />
-        </Flex>
-        <Flex
-          gridColumn={3}
-          gap={4}
-          flexWrap={'wrap'}
-          justifyContent={'center'}
-          display={desktopButtonGroupDisplay}
-        >
+          {netWorth}
           {desktopButtons}
-        </Flex>
-        <Flex justifyContent='flex-end' gap={2} gridColumn={3} display={mobileButtonRowDisplay}>
-          <IconButton
-            icon={searchIcon}
-            aria-label={translate('common.search')}
-            onClick={onSearchOpen}
-            isRound
-          />
-          <IconButton
-            icon={qrCodeIcon}
-            aria-label={translate('modals.send.qrCode')}
-            onClick={handleQrCodeClick}
-            isRound
-          />
-          <IconButton isRound icon={moreIcon} aria-label='Settings' onClick={onOpen} />
-        </Flex>
-      </Container>
-      {mobileButtons}
-      {drawer}
-      {isMobile && (
+          <Flex
+            justifyContent='flex-end'
+            gap={2}
+            gridColumn={3}
+            display={mobileButtonRowDisplay}
+            position='absolute'
+            right={1}
+            top={
+              'calc(env(safe-area-inset-top) + var(--safe-area-inset-top) + var(--chakra-space-4))'
+            }
+          >
+            <IconButton
+              icon={searchIcon}
+              aria-label={translate('common.search')}
+              onClick={onSearchOpen}
+              isRound
+            />
+            <IconButton
+              icon={qrCodeIcon}
+              aria-label={translate('modals.send.qrCode')}
+              onClick={handleQrCodeClick}
+              isRound
+            />
+            <IconButton isRound icon={moreIcon} aria-label='Settings' onClick={onOpen} />
+          </Flex>
+        </Container>
+        {mobileButtons}
+        {mobileDrawer}
         <GlobalSearchModal
           isOpen={isSearchOpen}
           onClose={onSearchClose}
           onOpen={onSearchOpen}
           onToggle={onSearchToggle}
         />
-      )}
+      </Display.Mobile>
+      <Display.Desktop>
+        <Container
+          width='full'
+          display='grid'
+          gridTemplateColumns={profileGridTemplate}
+          maxWidth='container.4xl'
+          px={containerPadding}
+          pt={4}
+          pb={4}
+          alignItems='flex-start'
+          justifyContent='space-between'
+          gap={containerGap}
+        >
+          {netWorth}
+          <Flex
+            gridColumn={3}
+            gap={4}
+            flexWrap={'wrap'}
+            justifyContent={'center'}
+            display={desktopButtonGroupDisplay}
+          >
+            {desktopButtons}
+          </Flex>
+        </Container>
+      </Display.Desktop>
     </>
   )
 })

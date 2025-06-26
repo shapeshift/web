e6a02b3d9d357cd131ffa99aad3a6ad30ad842b2
kevin <35275952+kaladinlight@users.noreply.github.com>
2025-06-26 12:19:02 -0600

    feat: mayachain affiliate (#9762)

--- packages/unchained-client/src/cosmossdk/parser/mayachain.ts
+++ packages/unchained-client/src/cosmossdk/parser/mayachain.ts
@@ -1,18 +1,12 @@
-import type { ChainId } from '@shapeshiftoss/caip'
-
 import * as mayachain from '../../parser/mayachain'
 import * as thormaya from './thormaya'
 
 export interface ParserArgs {
-  chainId: ChainId
   midgardUrl: string
 }
 
 export class Parser extends thormaya.Parser {
   constructor(args: ParserArgs) {
-    super({
-      chainId: args.chainId,
-      parser: new mayachain.Parser({ midgardUrl: args.midgardUrl }),
-    })
+    super({ parser: new mayachain.Parser({ midgardUrl: args.midgardUrl }) })
   }
 }

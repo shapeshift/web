46793c8bfd481fbe17798ff7e8bbf10aaf2d3c8d
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-06-26 14:50:16 +0200

    feat: make search assets only (#9786)

--- src/components/Layout/Header/GlobalSearch/SearchResults.tsx
+++ src/components/Layout/Header/GlobalSearch/SearchResults.tsx
@@ -2,43 +2,26 @@ import { List } from '@chakra-ui/react'
 import { memo, useMemo } from 'react'
 import MultiRef from 'react-multi-ref'
 
-import { ActionResults } from './ActionResults/ActionResults'
 import { AssetResults } from './AssetResults/AssetResults'
-import { TxResults } from './TxResults/TxResults'
 
 import { SearchEmpty } from '@/components/StakingVaults/SearchEmpty'
-import type {
-  AssetSearchResult,
-  GlobalSearchResult,
-  SendResult,
-  TxSearchResult,
-} from '@/state/slices/search-selectors'
+import type { AssetSearchResult, AssetSearchResults } from '@/state/slices/search-selectors'
 
 export type SearchResultsProps = {
-  assetResults: AssetSearchResult[]
-  txResults: TxSearchResult[]
-  sendResults: SendResult[]
+  assetResults: AssetSearchResults
   activeIndex: number
   searchQuery: string
   isSearching: boolean
-  onClickResult: (item: GlobalSearchResult) => void
+  onClickResult: (item: AssetSearchResult) => void
 }
 
 export const SearchResults = memo(
-  ({
-    assetResults,
-    txResults,
-    sendResults,
-    activeIndex,
-    searchQuery,
-    isSearching,
-    onClickResult,
-  }: SearchResultsProps) => {
+  ({ assetResults, activeIndex, searchQuery, isSearching, onClickResult }: SearchResultsProps) => {
     const menuNodes = useMemo(() => new MultiRef<number, HTMLElement>(), [])
 
     const noResults = useMemo(() => {
-      return !assetResults.length && !txResults.length && !sendResults.length
-    }, [assetResults.length, sendResults.length, txResults.length])
+      return !assetResults.length
+    }, [assetResults.length])
 
     if (isSearching && noResults) {
       return <SearchEmpty searchQuery={searchQuery} />
@@ -46,27 +29,11 @@ export const SearchResults = memo(
 
     return (
       <List>
-        <ActionResults
-          onClick={onClickResult}
-          results={sendResults}
-          searchQuery={searchQuery}
-          activeIndex={activeIndex}
-          startingIndex={0}
-          menuNodes={menuNodes}
-        />
         <AssetResults
           onClick={onClickResult}
           results={assetResults}
           activeIndex={activeIndex}
           startingIndex={0}
-          searchQuery={searchQuery}
-          menuNodes={menuNodes}
-        />
-        <TxResults
-          results={txResults}
-          onClick={onClickResult}
-          activeIndex={activeIndex}
-          startingIndex={assetResults.length}
           searchQuery={searchQuery}
           menuNodes={menuNodes}
         />

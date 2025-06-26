0e1c130fcb7a18e08b78423d507e44fa55db94ab
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-06-26 13:53:46 +0200

    feat: notifications center empty state

---
+++ src/components/Layout/Header/ActionCenter/components/EmptyState.tsx
@@ -0,0 +1,62 @@
+import { Box, Button, Card, CardBody, Flex, HStack, Icon, Stack } from '@chakra-ui/react'
+import { useCallback } from 'react'
+import { TbBellFilled } from 'react-icons/tb'
+import { useTranslate } from 'react-polyglot'
+import { useNavigate } from 'react-router'
+
+import { Text } from '@/components/Text/Text'
+
+export const EmptyState = ({ onClose }: { onClose: () => void }) => {
+  const navigate = useNavigate()
+  const translate = useTranslate()
+
+  const handleStartSwappingClick = useCallback(() => {
+    navigate('/trade')
+    onClose()
+  }, [navigate])
+
+  return (
+    <Stack spacing={4} mx={2} borderRadius='lg' transitionProperty='common'>
+      <Flex gap={4} alignItems='flex-start' px={4} py={4}>
+        <Flex
+          align='center'
+          justify='center'
+          minW='45px'
+          minH='45px'
+          borderRadius='50%'
+          bg='blue.500'
+        >
+          <Icon as={TbBellFilled} boxSize={7} color='white' />
+        </Flex>
+        <Stack spacing={0} width='full'>
+          <HStack>
+            <Stack spacing={1} width='full'>
+              <Text fontSize='md' translation='notificationCenter.emptyState.title' />
+              <Box>
+                <Text
+                  fontSize='sm'
+                  color='text.subtle'
+                  translation='notificationCenter.emptyState.body1'
+                />
+                <Text
+                  fontSize='sm'
+                  color='text.subtle'
+                  translation='notificationCenter.emptyState.body2'
+                />
+              </Box>
+            </Stack>
+          </HStack>
+          <Card bg='transparent' mt={4}>
+            <CardBody px={0} py={0}>
+              <Stack gap={4}>
+                <Button colorScheme='blue' width='full' onClick={handleStartSwappingClick}>
+                  {translate('Start Swapping')}
+                </Button>
+              </Stack>
+            </CardBody>
+          </Card>
+        </Stack>
+      </Flex>
+    </Stack>
+  )
+}

bc23abb92c196900d62db0b88436ab0f4de2b3bb
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2025-06-26 19:59:36 +1000

    ButterSwap: implement getUnsignedEvmTransaction and getUnsignedSolanaTransaction for correct chain adapter selection

--- packages/swapper/src/swappers/ButterSwap/endpoints.ts
+++ packages/swapper/src/swappers/ButterSwap/endpoints.ts
@@ -1,4 +1,10 @@
+import { CHAIN_NAMESPACE, fromChainId } from '@shapeshiftoss/caip'
+import { evm } from '@shapeshiftoss/chain-adapters'
+import BigNumber from 'bignumber.js'
+
+import { getUnsignedSolanaTransaction } from '../../solana-utils/getUnsignedSolanaTransaction'
 import type { SwapperApi } from '../../types'
+import { getExecutableTradeStep, isExecutableTradeQuote } from '../../utils'
 import { getTradeQuote } from './swapperApi/getTradeQuote'
 import { getTradeRate } from './swapperApi/getTradeRate'
 
@@ -9,4 +15,36 @@ export const butterSwapApi: SwapperApi = {
     // TODO(gomes): Implement checkTradeStatus
     throw new Error('checkTradeStatus Not implemented')
   },
+  getUnsignedEvmTransaction: async args => {
+    const { from, stepIndex, tradeQuote, supportsEIP1559, assertGetEvmChainAdapter } = args
+    if (!isExecutableTradeQuote(tradeQuote)) throw new Error('Unable to execute a trade rate quote')
+    const step = getExecutableTradeStep(tradeQuote, stepIndex)
+    const { sellAsset, butterSwapTransactionMetadata } = step
+    const { chainNamespace } = fromChainId(sellAsset.chainId)
+    if (chainNamespace !== CHAIN_NAMESPACE.Evm) {
+      throw new Error(`getUnsignedEvmTransaction called for non-EVM chain: ${chainNamespace}`)
+    }
+    const { accountNumber } = step
+    if (!butterSwapTransactionMetadata) throw new Error('Transaction metadata is required')
+    const { to, value, data } = butterSwapTransactionMetadata
+    if (to === undefined || value === undefined || data === undefined) {
+      const undefinedRequiredValues = [to, value, data].filter(v => v === undefined)
+      throw new Error('undefined required values in swap step', {
+        cause: { undefinedRequiredValues },
+      })
+    }
+    const adapter = assertGetEvmChainAdapter(sellAsset.chainId)
+    const feeData = await evm.getFees({ adapter, data, to, value, from, supportsEIP1559 })
+    // Use the higher of the node or API gas limit if available
+    return adapter.buildCustomApiTx({
+      accountNumber,
+      data,
+      from,
+      to,
+      value,
+      ...feeData,
+      gasLimit: BigNumber.max(feeData.gasLimit).toFixed(),
+    })
+  },
+  getUnsignedSolanaTransaction,
 }

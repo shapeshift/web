d7e2c6539c035447f51bc2b8518b622a234be9f4
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2025-06-30 17:31:03 +1000

    chore: unrugaroonio

--- packages/swapper/src/swappers/ButterSwap/ButterSwap.ts
+++ packages/swapper/src/swappers/ButterSwap/ButterSwap.ts
@@ -1,26 +1,10 @@
 import type { Swapper } from '../../types'
 import { executeEvmTransaction, executeSolanaTransaction } from '../../utils'
-import { BUTTERSWAP_SUPPORTED_CHAIN_IDS } from './utils/constants'
 
 export const butterSwap: Swapper = {
   executeEvmTransaction,
   executeSolanaTransaction,
   executeUtxoTransaction: (txToSign, { signAndBroadcastTransaction }) => {
     return signAndBroadcastTransaction(txToSign)
-  },
-  filterAssetIdsBySellable: assets => {
-    return Promise.resolve(
-      assets
-        .filter(asset => BUTTERSWAP_SUPPORTED_CHAIN_IDS.sell.includes(asset.chainId))
-        .map(asset => asset.assetId),
-    )
-  },
-
-  filterBuyAssetsBySellAssetId: input => {
-    return Promise.resolve(
-      input.assets
-        .filter(asset => BUTTERSWAP_SUPPORTED_CHAIN_IDS.buy.includes(asset.chainId))
-        .map(asset => asset.assetId),
-    )
   },
 }

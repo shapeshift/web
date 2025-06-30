cd95d23826675c7c6f6fcbe3373ae847cf7cb98e
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-06-30 18:24:49 +0200

    feat: groundwork

--- src/pages/Dashboard/components/DashboardHeader/DashboardHeaderTop.tsx
+++ src/pages/Dashboard/components/DashboardHeader/DashboardHeaderTop.tsx
@@ -103,6 +103,49 @@ const MobileActionButton = ({ icon, label, onClick, isDisabled }: MobileActionBu
   )
 }
 
+type MobileUserHeaderPrps = {
+  onSearchOpen: () => void
+  handleQrCodeClick: () => void
+  onOpen: () => void
+}
+
+export const MobileUserHeader = ({
+  onSearchOpen,
+  handleQrCodeClick,
+  onOpen,
+}: MobileUserHeaderPrps) => {
+  const translate = useTranslate()
+
+  const {
+    state: { walletInfo },
+  } = useWallet()
+
+  return (
+    <Flex justifyContent='space-between' width='100%' display={mobileButtonRowDisplay}>
+      <Flex align='center' onClick={onOpen}>
+        <ProfileAvatar size='md' borderRadius='full' />
+        <Text ml={2} fontWeight='semibold' fontSize='md'>
+          {(walletInfo?.meta?.label || walletInfo?.name) ?? translate('common.connectWallet')}
+        </Text>
+      </Flex>
+      <Flex gap={2}>
+        <IconButton
+          icon={searchIcon}
+          aria-label={translate('common.search')}
+          onClick={onSearchOpen}
+          isRound
+        />
+        <IconButton
+          icon={qrCodeIcon}
+          aria-label={translate('modals.send.qrCode')}
+          onClick={handleQrCodeClick}
+          isRound
+        />
+      </Flex>
+    </Flex>
+  )
+}
+
 export const DashboardHeaderTop = memo(() => {
   const { isOpen, onClose, onOpen } = useDisclosure()
   const {
@@ -114,7 +157,7 @@ export const DashboardHeaderTop = memo(() => {
   const mixpanel = getMixPanel()
   const translate = useTranslate()
   const {
-    state: { walletInfo, isConnected },
+    state: { isConnected },
   } = useWallet()
 
   const navigate = useNavigate()
@@ -224,29 +267,11 @@ export const DashboardHeaderTop = memo(() => {
       <Display.Mobile>
         <>
           <Container px={6} pt={4}>
-            <Flex justifyContent='space-between' width='100%' display={mobileButtonRowDisplay}>
-              <Flex align='center' onClick={onOpen}>
-                <ProfileAvatar size='md' borderRadius='full' />
-                <Text ml={2} fontWeight='semibold' fontSize='md'>
-                  {(walletInfo?.meta?.label || walletInfo?.name) ??
-                    translate('common.connectWallet')}
-                </Text>
-              </Flex>
-              <Flex gap={2}>
-                <IconButton
-                  icon={searchIcon}
-                  aria-label={translate('common.search')}
-                  onClick={onSearchOpen}
-                  isRound
-                />
-                <IconButton
-                  icon={qrCodeIcon}
-                  aria-label={translate('modals.send.qrCode')}
-                  onClick={handleQrCodeClick}
-                  isRound
-                />
-              </Flex>
-            </Flex>
+            <MobileUserHeader
+              onSearchOpen={onSearchOpen}
+              handleQrCodeClick={handleQrCodeClick}
+              onOpen={onOpen}
+            />
           </Container>
           <Container
             width='100%'

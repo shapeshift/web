6d7cdacc0e5e31088e19520345420d423c2a9606
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-07-01 21:35:28 +0200

    feat: yeah no can't do that

--- src/state/slices/preferencesSlice/preferencesSlice.ts
+++ src/state/slices/preferencesSlice/preferencesSlice.ts
@@ -10,9 +10,6 @@ import { simpleLocale } from '@/lib/browserLocale'
 import type { SupportedFiatCurrencies } from '@/lib/market-service'
 import { getMixPanel } from '@/lib/mixpanel/mixPanelSingleton'
 import { MixPanelEvent } from '@/lib/mixpanel/types'
-import { getMaybeCompositeAssetSymbol } from '@/lib/mixpanel/helpers'
-import { selectAssets } from '@/state/slices/selectors'
-import { store } from '@/state/store'
 
 dayjs.extend(localizedFormat)
 
@@ -258,11 +255,8 @@ export const preferences = createSlice({
         state.watchedAssets = state.watchedAssets.concat(payload)
       }
 
-      const assets = selectAssets(store.getState())
-      const compositeAssetId = getMaybeCompositeAssetSymbol(payload, assets)
-
       getMixPanel()?.track(MixPanelEvent.ToggleWatchAsset, {
-        assetId: compositeAssetId,
+        assetId: payload,
         isAdding: !isWatched,
       })
     }),

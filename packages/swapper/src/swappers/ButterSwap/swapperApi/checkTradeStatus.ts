85f14ed29600292219f6b61c7b2c34a12b99fe08
Apotheosis <0xapotheosis@gmail.com>
2025-07-02 17:26:29 +1000

    feat: optimistic BTC success

--- packages/swapper/src/swappers/ButterSwap/swapperApi/checkTradeStatus.ts
+++ packages/swapper/src/swappers/ButterSwap/swapperApi/checkTradeStatus.ts
@@ -1,6 +1,8 @@
+import { KnownChainIds } from '@shapeshiftoss/types'
 import { TxStatus } from '@shapeshiftoss/unchained-client'
 
 import type { CheckTradeStatusInput, TradeStatus } from '../../../types'
+import { butterSwapChainIdToChainId } from '../utils/helpers'
 import { getBridgeInfoById, getBridgeInfoBySourceHash } from '../xhr'
 
 // See: https://docs.butternetwork.io/butter-swap-integration/butter-api-for-swap-data/get-swap-history-by-source-hash
@@ -59,8 +61,21 @@ export const checkTradeStatus = async (input: CheckTradeStatusInput): Promise<Tr
       return { status: TxStatus.Unknown, buyTxHash: undefined, message: undefined }
     }
 
+    // Use toHash as the destination chain tx hash if present
+    const buyTxHash = detailedInfo.toHash ?? undefined
+    const relayerTxHash = detailedInfo.relayerHash ?? undefined
+    const relayerExplorerTxLink = detailedInfo.relayerChain?.scanUrl ?? undefined
+
     // State mapping: 0 = pending, 1 = complete, 6 = refunded/failed
     let status: TxStatus = (() => {
+      // For BTC, be optimistic on buyTxHash as we update the balance once the TX hits the mempool
+      if (
+        buyTxHash &&
+        butterSwapChainIdToChainId(Number(detailedInfo.toChain.chainId)) ===
+          KnownChainIds.BitcoinMainnet
+      )
+        return TxStatus.Confirmed
+
       switch (detailedInfo.state) {
         case BUTTER_SWAP_STATES.Pending:
           return TxStatus.Pending
@@ -77,11 +92,6 @@ export const checkTradeStatus = async (input: CheckTradeStatusInput): Promise<Tr
     if (status === TxStatus.Confirmed || status === TxStatus.Failed) {
       relayIdCache.delete(txHash)
     }
-
-    // Use toHash as the destination chain tx hash if present
-    const buyTxHash = detailedInfo.toHash ?? undefined
-    const relayerTxHash = detailedInfo.relayerHash ?? undefined
-    const relayerExplorerTxLink = detailedInfo.relayerChain?.scanUrl ?? undefined
 
     return { status, buyTxHash, relayerTxHash, relayerExplorerTxLink, message: undefined }
   } catch (e) {

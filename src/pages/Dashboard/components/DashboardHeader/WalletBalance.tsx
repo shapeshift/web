4aceeb8f9793d68735c8d93f8aa89f41f6e67f56
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-07-03 00:49:01 +0200

    feat: lazy defi tab

--- src/pages/Dashboard/components/DashboardHeader/WalletBalance.tsx
+++ src/pages/Dashboard/components/DashboardHeader/WalletBalance.tsx
@@ -1,18 +1,14 @@
 import type { FlexProps, ResponsiveValue } from '@chakra-ui/react'
 import { Box, Flex, Spinner } from '@chakra-ui/react'
-import { bnOrZero } from '@shapeshiftoss/chain-adapters'
 import type { Property } from 'csstype'
-import { memo, useMemo } from 'react'
+import { memo } from 'react'
 import { useTranslate } from 'react-polyglot'
 
 import { Amount } from '@/components/Amount/Amount'
-import { useFetchOpportunities } from '@/components/StakingVaults/hooks/useFetchOpportunities'
 import { Text } from '@/components/Text'
 import { TooltipWithTouch } from '@/components/TooltipWithTouch'
 import { useAccountsFetchQuery } from '@/context/AppProvider/hooks/useAccountsFetchQuery'
 import {
-  selectClaimableRewards,
-  selectEarnBalancesUserCurrencyAmountFull,
   selectPortfolioAccounts,
   selectPortfolioTotalUserCurrencyBalance,
 } from '@/state/slices/selectors'
@@ -36,50 +32,23 @@ type WalletBalanceProps = {
 export const WalletBalance: React.FC<WalletBalanceProps> = memo(
   ({ label = 'defi.netWorth', alignItems, balanceFontSize }) => {
     const { isFetching: isAccountsMetadataFetching } = useAccountsFetchQuery()
-    const { isLoading: isOpportunitiesLoading, opportunityAccountIdsFetched } =
-      useFetchOpportunities()
-    const claimableRewardsUserCurrencyBalanceFilter = useMemo(() => ({}), [])
-    const claimableRewardsUserCurrencyBalance = useAppSelector(state =>
-      selectClaimableRewards(state, claimableRewardsUserCurrencyBalanceFilter),
-    )
-    const earnUserCurrencyBalance = useAppSelector(
-      selectEarnBalancesUserCurrencyAmountFull,
-    ).toFixed()
     const portfolioTotalUserCurrencyBalance = useAppSelector(
       selectPortfolioTotalUserCurrencyBalance,
     )
 
     const walletAccounts = useAppSelector(selectPortfolioAccounts)
 
-    const walletOpportunityAccountIds = useMemo(
-      () => opportunityAccountIdsFetched.filter(id => walletAccounts[id] !== undefined),
-      [opportunityAccountIdsFetched, walletAccounts],
-    )
     const translate = useTranslate()
-
-    const netWorth = useMemo(
-      () =>
-        bnOrZero(earnUserCurrencyBalance)
-          .plus(portfolioTotalUserCurrencyBalance)
-          .plus(claimableRewardsUserCurrencyBalance)
-          .toFixed(),
-      [
-        claimableRewardsUserCurrencyBalance,
-        earnUserCurrencyBalance,
-        portfolioTotalUserCurrencyBalance,
-      ],
-    )
 
     return (
       <Flex flexDir={balanceFlexDir} alignItems={alignItems ?? portfolioTextAlignment}>
         <Box position='relative'>
           <Text fontWeight='medium' translation={label} color='text.subtle' whiteSpace='nowrap' />
 
-          {(isOpportunitiesLoading || isAccountsMetadataFetching) && (
+          {isAccountsMetadataFetching && (
             <TooltipWithTouch
               label={translate('defi.loadingAccounts', {
                 portfolioAccountsLoaded: Object.keys(walletAccounts).length,
-                opportunityAccountsLoaded: walletOpportunityAccountIds.length,
               })}
             >
               <Spinner color='blue.500' size='sm' position='absolute' right={-8} top={1} />
@@ -88,7 +57,7 @@ export const WalletBalance: React.FC<WalletBalanceProps> = memo(
         </Box>
         <Amount.Fiat
           lineHeight='shorter'
-          value={netWorth}
+          value={portfolioTotalUserCurrencyBalance}
           fontSize={balanceFontSize ?? defaultBalanceFontSize}
           fontWeight='semibold'
         />

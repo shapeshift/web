0cc3c04e23996356a570f0009d6c003705a608ff
reallybeard <89934888+reallybeard@users.noreply.github.com>
2025-07-07 16:51:53 -0500

    feat: remove duplicate asset actions on account page (#9600)
    * parse urls
    
    * update route matching
    
    * fix paths

---
+++ src/hooks/useRouteAccountId/useRouteAccountId.ts
@@ -0,0 +1,40 @@
+import { useMemo } from 'react'
+import { matchPath, useLocation } from 'react-router-dom'
+
+// Define all possible paths that include accountId
+const accountPaths = [
+  '/accounts/:accountId/:assetId',
+  '/accounts/:accountId',
+  '/wallet/accounts/:accountId/:chainId/:assetSubId/:nftId',
+  '/wallet/accounts/:accountId/:chainId/:assetSubId',
+  '/lending/poolAccount/:accountId/:chainId/:assetSubId',
+  '/lending/poolAccount/:accountId/:chainNamespace\\::chainReference/:assetSubId',
+  '/lending/poolAccount/:accountId/:assetId',
+  '/lending/poolAccount/:accountId',
+]
+
+const getRouteAccountId = (pathname: string) => {
+  let match = null
+  for (const path of accountPaths) {
+    match = matchPath({ path, end: true }, pathname)
+    if (match) break
+  }
+
+  if (match?.params) {
+    const { accountId } = match.params
+    return accountId
+  }
+
+  return ''
+}
+
+export const useRouteAccountId = () => {
+  const location = useLocation()
+
+  const accountId = useMemo(() => {
+    const routeAccountId = getRouteAccountId(location.pathname)
+    return decodeURIComponent(routeAccountId ?? '')
+  }, [location.pathname])
+
+  return accountId
+}

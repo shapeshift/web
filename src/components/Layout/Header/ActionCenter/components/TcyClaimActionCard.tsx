eea1125b4f5e331d0e0ff603a0b4011813636b2e
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-07-14 15:21:40 +0200

    feat: more abstract more nice more happy

--- src/components/Layout/Header/ActionCenter/components/TcyClaimActionCard.tsx
+++ src/components/Layout/Header/ActionCenter/components/TcyClaimActionCard.tsx
@@ -2,16 +2,13 @@ import { tcyAssetId } from '@shapeshiftoss/caip'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import { useCallback, useMemo } from 'react'
-import { useTranslate } from 'react-polyglot'
 import { useNavigate } from 'react-router'
 
 import { ClaimActionCard } from './ClaimActionCard'
 
 import { fromThorBaseUnit } from '@/lib/utils/thorchain'
 import type { TcyClaimAction } from '@/state/slices/actionSlice/types'
-import { ActionStatus, GenericTransactionDisplayType } from '@/state/slices/actionSlice/types'
-import { selectAssetById } from '@/state/slices/selectors'
-import { useAppSelector } from '@/state/store'
+import { GenericTransactionDisplayType } from '@/state/slices/actionSlice/types'
 
 dayjs.extend(relativeTime)
 
@@ -20,41 +17,17 @@ type TcyClaimActionCardProps = {
 }
 
 export const TcyClaimActionCard = ({ action }: TcyClaimActionCardProps) => {
-  const translate = useTranslate()
   const navigate = useNavigate()
-
-  const tcyAsset = useAppSelector(state => selectAssetById(state, tcyAssetId))
 
   const handleClaimClick = useCallback(() => {
     const claim = action.tcyClaimActionMetadata.claim
     navigate(`/tcy/claim/${claim.l1_address}`, { state: { selectedClaim: claim } })
   }, [navigate, action.tcyClaimActionMetadata.claim])
 
-  const message = useMemo(() => {
-    if (!tcyAsset) return null
-    const amountCryptoHuman = fromThorBaseUnit(
-      action.tcyClaimActionMetadata.claim.amountThorBaseUnit,
-    )
-    switch (action.status) {
-      case ActionStatus.ClaimAvailable:
-        return translate('actionCenter.tcy.claimReady', {
-          amount: amountCryptoHuman,
-          symbol: tcyAsset.symbol,
-        })
-      case ActionStatus.Pending:
-        return translate('actionCenter.tcy.claimTxPending', {
-          amount: amountCryptoHuman,
-          symbol: tcyAsset.symbol,
-        })
-      case ActionStatus.Claimed:
-        return translate('actionCenter.tcy.claimTxComplete', {
-          amount: amountCryptoHuman,
-          symbol: tcyAsset.symbol,
-        })
-      default:
-        throw new Error(`Unsupported TCY Claim Action status: ${action.status}`)
-    }
-  }, [action.tcyClaimActionMetadata.claim.amountThorBaseUnit, action.status, tcyAsset, translate])
+  const amountCryptoPrecision = useMemo(
+    () => fromThorBaseUnit(action.tcyClaimActionMetadata.claim.amountThorBaseUnit).toFixed(),
+    [action.tcyClaimActionMetadata.claim.amountThorBaseUnit],
+  )
 
   return (
     <ClaimActionCard
@@ -64,7 +37,7 @@ export const TcyClaimActionCard = ({ action }: TcyClaimActionCardProps) => {
       underlyingAssetId={action.tcyClaimActionMetadata.claim.assetId}
       txHash={action.tcyClaimActionMetadata.txHash}
       onClaimClick={handleClaimClick}
-      message={message}
+      amountCryptoPrecision={amountCryptoPrecision}
     />
   )
 }

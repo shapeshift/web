76bcda4d17e6896f2d8b767de9fd2454e5221df5
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-07-16 11:03:58 +0200

    feat: polling too

--- src/hooks/useActionCenterSubscribers/useSendActionSubscriber.tsx
+++ src/hooks/useActionCenterSubscribers/useSendActionSubscriber.tsx
@@ -19,6 +19,7 @@ import { useNotificationToast } from '../useNotificationToast'
 import { useWallet } from '../useWallet/useWallet'
 
 import { useActionCenterContext } from '@/components/Layout/Header/ActionCenter/ActionCenterContext'
+import { SendNotification } from '@/components/Layout/Header/ActionCenter/components/Notifications/SendNotification'
 import { SwapNotification } from '@/components/Layout/Header/ActionCenter/components/Notifications/SwapNotification'
 import { getConfig } from '@/config'
 import { queryClient } from '@/context/QueryClientProvider/queryClient'
@@ -26,7 +27,10 @@ import { getTxLink } from '@/lib/getTxLink'
 import { fetchTradeStatus, tradeStatusQueryKey } from '@/lib/tradeExecution'
 import { actionSlice } from '@/state/slices/actionSlice/actionSlice'
 import { selectPendingSendActions } from '@/state/slices/actionSlice/selectors'
+import { ActionStatus } from '@/state/slices/actionSlice/types'
+import { selectTxs } from '@/state/slices/selectors'
 import { swapSlice } from '@/state/slices/swapSlice/swapSlice'
+import { serializeTxIndex } from '@/state/slices/txHistorySlice/utils'
 import { store, useAppDispatch, useAppSelector } from '@/state/store'
 
 export const useSendActionSubscriber = () => {
@@ -38,13 +42,60 @@ export const useSendActionSubscriber = () => {
   const toast = useNotificationToast({ duration: isDrawerOpen ? 5000 : null })
 
   const pendingSendActions = useAppSelector(selectPendingSendActions)
+  const txs = useAppSelector(selectTxs)
+
+  useEffect(() => {
+    pendingSendActions.forEach(action => {
+      const { accountId, txHash } = action.transactionMetadata
+      const accountAddress = fromAccountId(accountId).account
+
+      const serializedTxIndex = serializeTxIndex(accountId, txHash, accountAddress)
+      const tx = txs[serializedTxIndex]
+
+      if (!tx) return
+      if (tx.status !== TxStatus.Confirmed) return
+
+      dispatch(
+        actionSlice.actions.upsertAction({
+          ...action,
+          status: ActionStatus.Complete,
+          updatedAt: Date.now(),
+          transactionMetadata: {
+            ...action.transactionMetadata,
+            message: 'modals.send.youHaveSent',
+          },
+        }),
+      )
+
+      toast({
+        id: txHash,
+        duration: isDrawerOpen ? 5000 : null,
+        status: 'success',
+        render: ({ onClose, ...props }) => {
+          const handleClick = () => {
+            onClose()
+            openActionCenter()
+          }
+
+          return (
+            <SendNotification
+              // eslint-disable-next-line react-memo/require-usememo
+              handleClick={handleClick}
+              actionId={txHash}
+              onClose={onClose}
+              {...props}
+            />
+          )
+        },
+      })
+    })
+  }, [txs, pendingSendActions])
 
   // TODO(gomes): this log works, which is nice
   // The send card should be the source of truth, which will allow us to very easily do useTxStatus() there and introspect the status
   // Then, the card can automagically update *and* upsert on completion, which will negate the need for this unless closed
   // This SHOULD still exist however, as user may have closed the notification/the app, so we can't rely solely on card updates if mounted,
   // but let's implement this last before opening the PR once card "live notification"'s working
-  console.log({ pendingSendActions })
   const {
     state: { isConnected },
   } = useWallet()

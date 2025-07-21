277807e9300c669b84168fc4e7014a92f70bca27
NeOMakinG <14963751+NeOMakinG@users.noreply.github.com>
2025-07-21 22:57:41 +0800

    fix: go back behavior on account page and account list page (#10039)

--- src/components/AccountDetails.tsx
+++ src/components/AccountDetails.tsx
@@ -2,10 +2,8 @@ import { Flex, Stack } from '@chakra-ui/react'
 import type { AccountId, AssetId } from '@shapeshiftoss/caip'
 import { ethAssetId } from '@shapeshiftoss/caip'
 import { useMemo } from 'react'
-import { useTranslate } from 'react-polyglot'
 
 import { AccountAssets } from './AccountAssets/AccountAssets'
-import { AccountHeader } from './AccountHeader/AccountHeader'
 import { AssetAccounts } from './AssetAccounts/AssetAccounts'
 import { Main } from './Layout/Main'
 import { RelatedAssets } from './RelatedAssets/RelatedAssets'
@@ -27,23 +25,14 @@ const flexMaxWidth = { base: 'full', xl: 'md' }
 const displayMdBlock = { base: 'none', md: 'flex' }
 
 export const AccountDetails = ({ assetId, accountId }: AccountDetailsProps) => {
-  const translate = useTranslate()
-
   // When the asset is ETH, we want to use the built-in default buy asset (FOX)
   const defaultBuyAssetId = useMemo(() => (assetId === ethAssetId ? undefined : assetId), [assetId])
 
-  const header = useMemo(() => <AccountHeader />, [])
-
   if (!accountId || !assetId) return null
   return (
-    <Main width='full' alignItems='flex-start' mx='auto' headerComponent={header}>
+    <Main width='full' alignItems='flex-start' mx='auto'>
       <Stack spacing={4} flex='1 1 0%' width='full'>
-        <AccountBalance
-          assetId={assetId}
-          accountId={accountId}
-          backPath='/wallet/accounts'
-          backLabel={translate('navBar.accounts')}
-        />
+        <AccountBalance assetId={assetId} accountId={accountId} />
         {accountId && <AccountAssets assetId={assetId} accountId={accountId} />}
         <RelatedAssets assetId={assetId} />
         <AssetAccounts assetId={assetId} accountId={isUtxoAccountId(accountId) ? '' : accountId} />

0a58a7d00f88629fd3a04263ffb08bc15e456d0e
kevin <35275952+kaladinlight@users.noreply.github.com>
2025-07-30 16:32:21 -0600

    feat: update estimate gas to post request (#10153)

--- packages/chain-adapters/src/evm/optimism/OptimismChainAdapter.ts
+++ packages/chain-adapters/src/evm/optimism/OptimismChainAdapter.ts
@@ -86,9 +86,10 @@ export class ChainAdapter extends EvmBaseAdapter<KnownChainIds.OptimismMainnet> 
     input: GetFeeDataInput<KnownChainIds.OptimismMainnet>,
   ): Promise<FeeDataEstimate<KnownChainIds.OptimismMainnet>> {
     try {
-      const req = await this.buildEstimateGasRequest(input)
+      const { gasLimit, l1GasLimit } = await this.api.estimateGas({
+        estimateGasBody: this.buildEstimateGasBody(input),
+      })
 
-      const { gasLimit, l1GasLimit } = await this.api.estimateGas(req)
       const { fast, average, slow } = await this.getGasFeeData()
 
       return {

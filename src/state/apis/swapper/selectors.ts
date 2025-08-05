b5bfe2cbc2365301bc9b204911dfee2719712c8a
Jibles <premiumjibles@gmail.com>
2025-08-06 02:37:45 +1000

    feat: combine quote fetches and add new loader (#10154)

--- src/state/apis/swapper/selectors.ts
+++ src/state/apis/swapper/selectors.ts
@@ -1,17 +1,59 @@
 import { QueryStatus } from '@reduxjs/toolkit/query'
+import { solAssetId } from '@shapeshiftoss/caip'
 import type { SwapperName } from '@shapeshiftoss/swapper'
 import type { PartialRecord } from '@shapeshiftoss/types'
 
 import type { TradeQuoteOrRateRequest } from './types'
 
+import { getEnabledSwappers } from '@/state/helpers'
 import type { ReduxState } from '@/state/reducer'
 import { createDeepEqualOutputSelector } from '@/state/selector-utils'
+import { preferences } from '@/state/slices/preferencesSlice/preferencesSlice'
+import { selectInputBuyAsset } from '@/state/slices/tradeInputSlice/selectors'
 
 const selectSwapperApiQueries = (state: ReduxState) => state.swapperApi.queries
 
-export const selectIsTradeQuoteApiQueryPending = createDeepEqualOutputSelector(
+export const selectIsBatchTradeRateQueryLoading = createDeepEqualOutputSelector(
   selectSwapperApiQueries,
   queries => {
+    let latestQueryInfo
+    let latestStartedTimeStamp = 0
+
+    // Find the most recent batch trade rate query
+    for (const [queryKey, queryInfo] of Object.entries(queries)) {
+      if (!queryKey.startsWith('getTradeRates')) continue
+
+      const startedTimeStamp = queryInfo?.startedTimeStamp ?? 0
+      if (startedTimeStamp > latestStartedTimeStamp) {
+        latestStartedTimeStamp = startedTimeStamp
+        latestQueryInfo = queryInfo
+      }
+    }
+
+    if (!latestQueryInfo) return false
+
+    return [QueryStatus.uninitialized, QueryStatus.pending, undefined].includes(
+      latestQueryInfo.status,
+    )
+  },
+)
+
+export const selectIsTradeQuoteApiQueryPending = createDeepEqualOutputSelector(
+  selectSwapperApiQueries,
+  preferences.selectors.selectFeatureFlags,
+  selectInputBuyAsset,
+  selectIsBatchTradeRateQueryLoading,
+  (queries, featureFlags, buyAsset, isBatchTradeRateQueryLoading) => {
+    if (isBatchTradeRateQueryLoading) {
+      const isSolBuyAssetId = buyAsset.assetId === solAssetId
+
+      // No send address so always false
+      const isCrossAccountTrade = false
+
+      // If we're doing a bulk request then everything that's enabled is loading
+      return getEnabledSwappers(featureFlags, isCrossAccountTrade, isSolBuyAssetId)
+    }
+
     const isLoadingBySwapperName: PartialRecord<SwapperName, boolean> = {}
     const latestTimestamps: PartialRecord<SwapperName, number> = {}
 

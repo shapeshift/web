692a62df841cb1dd3efc3606bf9293e8f2a53feb
Apotheosis <0xapotheosis@gmail.com>
2025-08-06 12:34:09 +1000

    feat: enable same-chain butter swaps

--- packages/swapper/src/swappers/ButterSwap/swapperApi/getTradeRate.test.ts
+++ packages/swapper/src/swappers/ButterSwap/swapperApi/getTradeRate.test.ts
@@ -7,6 +7,7 @@ import { describe, expect, it, vi } from 'vitest'
 import type { GetTradeRateInput, SwapperDeps } from '../../../types'
 import { BTC, ETH, USDC_MAINNET, WETH } from '../../utils/test-data/assets'
 import ethBtcRoute from '../test-data/eth-btc.json'
+import { ROUTE_QUOTE } from '../test-data/routeQuote'
 import { butterService } from '../utils/butterSwapService'
 import { getTradeRate } from './getTradeRate'
 
@@ -17,7 +18,7 @@ vi.mock('../utils/butterSwapService', () => ({
 }))
 
 describe('getTradeRate', () => {
-  it('should not return a trade rate for same-chain swaps', async () => {
+  it('should return a trade rate', async () => {
     const deps: SwapperDeps = {
       assetsById: { [ETH.assetId]: ETH },
       assertGetChainAdapter: () => vi.fn() as any,
@@ -45,13 +46,15 @@ describe('getTradeRate', () => {
       supportsEIP1559: false, // TODO - upstream type bug? this is a literal false in the type def
     }
 
+    vi.mocked(butterService.get).mockResolvedValue(
+      Ok({ data: ROUTE_QUOTE } as unknown as AxiosResponse),
+    )
+
     const result = await getTradeRate(input, deps)
 
-    expect(result.isOk()).toBe(false)
-    expect(result.isErr()).toBe(true)
-
-    const error = result.unwrapErr()
-    expect(error.message).toContain('Same-chain swaps are not supported by ButterSwap')
+    expect(result.isOk()).toBe(true)
+    const tradeRate = result.unwrap()
+    expect(tradeRate[0].rate).toBe('2296.409699')
   })
 
   it('should return a correct trade rate and steps for a multi-hop ETH->BTC route', async () => {
@@ -86,6 +89,7 @@ describe('getTradeRate', () => {
     vi.mocked(butterService.get).mockResolvedValue(
       Ok({ data: ethBtcRoute } as unknown as AxiosResponse),
     )
+
     const result = await getTradeRate(input, deps)
 
     expect(result.isOk()).toBe(true)

682cafe3f1b4e7417f03af98bbab73a298378223
NeOMakinG <14963751+NeOMakinG@users.noreply.github.com>
2025-08-13 12:42:24 +0200

    fix: lint

--- src/lib/vibrate.ts
+++ src/lib/vibrate.ts
@@ -1,14 +1,16 @@
-import { HapticLevel, mobileVibrate } from '@/context/WalletProvider/MobileWallet/mobileMessageHandlers'
 import { isMobile } from './globals'
 
+import type { HapticLevel } from '@/context/WalletProvider/MobileWallet/mobileMessageHandlers'
+import { mobileVibrate } from '@/context/WalletProvider/MobileWallet/mobileMessageHandlers'
+
 export const vibrate = (level: HapticLevel): Promise<void> | boolean => {
-	if (isMobile) {
-		return mobileVibrate(level)
-	}
+  if (isMobile) {
+    return mobileVibrate(level)
+  }
 
-	if ('vibrate' in navigator) {
-		return navigator.vibrate(200)
-	}
+  if ('vibrate' in navigator) {
+    return navigator.vibrate(200)
+  }
 
-	return false
+  return false
 }

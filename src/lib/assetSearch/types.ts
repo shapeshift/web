6bffabe011d304692f0427bd17b443f40c043bad
Jibles <premiumjibles@gmail.com>
2025-08-21 09:57:11 +1000

    feat: fix explore weird input behaviour and speed up

--- src/lib/assetSearch/types.ts
+++ src/lib/assetSearch/types.ts
@@ -26,8 +26,8 @@ export interface AssetSearchWorkerMessages {
     payload: {
       searchString: string
       activeChainId: ChainId | 'All'
-      allowWalletUnsupportedAssets: boolean | undefined
-      walletConnectedChainIds: ChainId[]
+      allowWalletUnsupportedAssets?: boolean
+      walletConnectedChainIds?: ChainId[]
     }
   }
   SearchResultMessage: {

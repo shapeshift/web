ab2352ac3472bedc662bda1933fc0d4b5f3d5544
Jibles <premiumjibles@gmail.com>
2025-09-12 08:10:01 +1000

    Convert popover to drawer

--- src/components/Layout/Header/NavBar/WalletManagerPopover.tsx
+++ src/components/Layout/Header/NavBar/WalletManagerPopover.tsx
@@ -1,10 +1,10 @@
-import { Box, Popover, PopoverBody, PopoverContent, PopoverTrigger } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import type { FC } from 'react'
 import { memo, useCallback, useState } from 'react'
 
 import { WalletButton } from './WalletButton'
 
-import { PopoverWallet } from '@/components/Layout/Header/NavBar/PopoverWallet'
+import { DrawerWallet } from '@/components/Layout/Header/NavBar/DrawerWallet'
 import { WalletActions } from '@/context/WalletProvider/actions'
 import { useWallet } from '@/hooks/useWallet/useWallet'
 
@@ -28,25 +28,18 @@ export const WalletManagerPopover: FC = memo(() => {
   const handleClose = useCallback(() => setIsOpen(false), [])
 
   return (
-    <Popover isOpen={isOpen} onOpen={handleOpen} onClose={handleClose} placement='bottom-end'>
-      <PopoverTrigger>
-        <Box>
-          <WalletButton
-            onConnect={handleConnect}
-            walletInfo={walletInfo}
-            isConnected={isConnected && !isLocked}
-            isLoadingLocalWallet={isLoadingLocalWallet}
-            data-test='navigation-wallet-dropdown-button'
-          />
-        </Box>
-      </PopoverTrigger>
-      {isOpen && (
-        <PopoverContent width='430px' bg={'background.surface.base'} p={0}>
-          <PopoverBody p={4} width='430px'>
-            <PopoverWallet onClose={handleClose} />
-          </PopoverBody>
-        </PopoverContent>
-      )}
-    </Popover>
+    <>
+      <Box>
+        <WalletButton
+          onConnect={handleConnect}
+          walletInfo={walletInfo}
+          isConnected={isConnected && !isLocked}
+          isLoadingLocalWallet={isLoadingLocalWallet}
+          onClick={isConnected && !isLocked ? handleOpen : handleConnect}
+          data-test='navigation-wallet-dropdown-button'
+        />
+      </Box>
+      <DrawerWallet isOpen={isOpen} onClose={handleClose} />
+    </>
   )
 })

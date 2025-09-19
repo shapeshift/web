e6cc6b5b4b6059c88523a8607893d8d167017009
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-09-19 13:09:44 +0200

    fix: fml

--- packages/chain-adapters/src/cosmossdk/index.ts
+++ packages/chain-adapters/src/cosmossdk/index.ts
@@ -1,5 +1,6 @@
 export type { CosmosSdkChainAdapter } from './CosmosSdkBaseAdapter'
 export { cosmosSdkChainIds, CosmosSdkBaseAdapter } from './CosmosSdkBaseAdapter'
+export { isThorMayaChainId } from './thorchain/ThorchainChainAdapter'
 
 export * as cosmossdk from './types'
 

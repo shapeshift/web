ea9b86cf3d57dc7984c468aea2c9cd68309e6094
0xean <0xean.eth@gmail.com>
2025-09-25 13:05:25 -0400

    feat: regenerate asset data 09/25/2025 (#10668)

--- src/state/migrations/index.ts
+++ src/state/migrations/index.ts
@@ -235,6 +235,7 @@ export const clearAssetsMigrations = {
   192: clearAssets,
   193: clearAssets,
   194: clearAssets,
+  195: clearAssets,
 } as unknown as Omit<MigrationManifest, '_persist'>
 
 export const clearMarketDataMigrations = {

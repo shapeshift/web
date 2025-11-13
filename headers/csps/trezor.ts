a2a571a77b30914c167555a75264906a62cbe447
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-11-13 01:41:46 +0100

    fix: add Trezor Bridge port 21328 to CSP

--- headers/csps/trezor.ts
+++ headers/csps/trezor.ts
@@ -3,6 +3,8 @@ import type { Csp } from '../types'
 export const csp: Csp = {
   'frame-src': ['https://connect.trezor.io/'],
   'connect-src': [
+    // Trezor Bridge
+    'http://127.0.0.1:21328/',
     // Trezor Suite
     'ws://127.0.0.1:21335/connect-ws',
   ],

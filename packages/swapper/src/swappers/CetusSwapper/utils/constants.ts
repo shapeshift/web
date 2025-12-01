0bbbd0f86de2a8e0d81f7d84077ddd84580d123f
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-12-02 00:44:10 +0300

    chore: two hard things in sw

--- packages/swapper/src/swappers/CetusSwapper/utils/constants.ts
+++ packages/swapper/src/swappers/CetusSwapper/utils/constants.ts
@@ -1,10 +1,10 @@
 import type { ChainId } from '@shapeshiftoss/caip'
 import { suiChainId } from '@shapeshiftoss/caip'
 
-export const SUPPORTED_SUI_CHAIN_IDS: readonly ChainId[] = [suiChainId]
+export const SUPPORTED_CETUS_CHAIN_IDS: readonly ChainId[] = [suiChainId]
 
 export const isSupportedChainId = (
   chainId: ChainId,
-): chainId is (typeof SUPPORTED_SUI_CHAIN_IDS)[number] => {
-  return SUPPORTED_SUI_CHAIN_IDS.includes(chainId)
+): chainId is (typeof SUPPORTED_CETUS_CHAIN_IDS)[number] => {
+  return SUPPORTED_CETUS_CHAIN_IDS.includes(chainId)
 }

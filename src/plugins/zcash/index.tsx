c7484055654539d1fe78440975ba22f661dad1b7
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2025-12-02 15:32:09 -0700

    plugin

---
+++ src/plugins/zcash/index.tsx
@@ -0,0 +1,43 @@
+import { zcash } from '@shapeshiftoss/chain-adapters'
+import { KnownChainIds } from '@shapeshiftoss/types'
+import * as unchained from '@shapeshiftoss/unchained-client'
+
+import { getConfig } from '@/config'
+import type { Plugins } from '@/plugins/types'
+
+// eslint-disable-next-line import/no-default-export
+export default function register(): Plugins {
+  return [
+    [
+      'zcashChainAdapter',
+      {
+        name: 'zcashChainAdapter',
+        providers: {
+          chainAdapters: [
+            [
+              KnownChainIds.ZcashMainnet,
+              () => {
+                const http = new unchained.zcash.V1Api(
+                  new unchained.zcash.Configuration({
+                    basePath: getConfig().VITE_UNCHAINED_ZCASH_HTTP_URL,
+                  }),
+                )
+
+                const ws = new unchained.ws.Client<unchained.utxo.Tx>(
+                  getConfig().VITE_UNCHAINED_ZCASH_WS_URL,
+                )
+
+                return new zcash.ChainAdapter({
+                  providers: { http, ws },
+                  coinName: 'Zcash',
+                  thorMidgardUrl: getConfig().VITE_THORCHAIN_MIDGARD_URL,
+                  mayaMidgardUrl: getConfig().VITE_MAYACHAIN_MIDGARD_URL,
+                })
+              },
+            ],
+          ],
+        },
+      },
+    ],
+  ]
+}

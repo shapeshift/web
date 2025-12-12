b4aeeac4c8f166baa764c5138455e3f62d76fd59
Apotheosis <0xapotheosis@gmail.com>
2025-12-12 13:30:47 +1100

    fix: handle ecosytem disabled

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -113,6 +113,7 @@ export const Header = memo(() => {
   const isWalletConnectToDappsV2Enabled = useFeatureFlag('WalletConnectToDappsV2')
   const isActionCenterEnabled = useFeatureFlag('ActionCenter')
   const isNewWalletManagerEnabled = useFeatureFlag('NewWalletManager')
+  const isRfoxFoxEcosystemPageEnabled = useFeatureFlag('RfoxFoxEcosystemPage')
   const { degradedChainIds } = useDiscoverAccounts()
 
   const hasWallet = Boolean(walletInfo?.deviceId)
@@ -174,7 +175,7 @@ export const Header = memo(() => {
               <NavigationDropdown label='defi.earn' items={earnSubMenuItems} defaultPath='/tcy' />
               <Link
                 as={ReactRouterLink}
-                to='/fox-ecosystem'
+                to={isRfoxFoxEcosystemPageEnabled ? '/fox-ecosystem' : '/fox'}
                 fontWeight='medium'
                 color='text.subtle'
                 _hover={{ color: 'text.base', textDecoration: 'none' }}

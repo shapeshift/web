e1a7904496afca7540d3a3e117a211488883bac1
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-12-18 01:10:15 +0300

    feat: reset --hard over @neomaking 's pr

--- src/state/migrations/index.ts
+++ src/state/migrations/index.ts
@@ -292,6 +292,9 @@ export const clearAssetsMigrations = {
   245: clearAssets,
   246: clearAssets,
   247: clearAssets,
+  248: clearAssets,
+  249: clearAssets,
+  250: clearAssets,
 } as unknown as Omit<MigrationManifest, '_persist'>
 
 export const clearMarketDataMigrations = {

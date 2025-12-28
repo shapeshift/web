7540bd12a6093e214d5c47d2aa5fe43c687ef8c6
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-12-29 00:48:21 +0100

    feat: use @shapeshiftoss/bitcoinjs-lib instead of @bitgo/utxo-lib for Ledger Zcash PSBTs (#11374)

--- packages/chain-adapters/src/utxo/UtxoBaseAdapter.ts
+++ packages/chain-adapters/src/utxo/UtxoBaseAdapter.ts
@@ -352,7 +352,9 @@ export abstract class UtxoBaseAdapter<T extends UtxoChainId> implements IChainAd
               if (!response.ok || data.error) {
                 throw new Error(`Blockchair API error: ${data.error || response.statusText}`)
               }
-              return { hex: data.data[input.txid].raw_transaction }
+              return {
+                hex: data.data[input.txid].raw_transaction,
+              }
             }
             throw error
           }
@@ -372,9 +374,12 @@ export abstract class UtxoBaseAdapter<T extends UtxoChainId> implements IChainAd
           txid: input.txid,
           hex: data.hex,
           // For Zcash, we need to pass the blockHeight and txid of each input transaction
-          // so Ledger can add them to the PSBT and determine the correct consensus branch ID
+          // so Ledger can add them to the PSBT and determine the correct consensus branch ID.
+          // Only pass blockHeight if it's a valid positive number (mempool txs have blockHeight: -1)
           ...(this.coinName === 'Zcash' &&
-            'blockHeight' in data && { blockHeight: data.blockHeight }),
+            'blockHeight' in data &&
+            typeof data.blockHeight === 'number' &&
+            data.blockHeight > 0 && { blockHeight: data.blockHeight }),
         })
       }
 

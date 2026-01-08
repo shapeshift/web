22f96d6bf645b0f9d79f8f70320a52144cb4ca0b
gomes <17035424+gomesalexandre@users.noreply.github.com>
2026-01-08 14:58:12 +0100

    [skip ci] refactor(YieldAccountContext): add memo, useMemo for context value

--- src/pages/Yields/YieldAccountContext.tsx
+++ src/pages/Yields/YieldAccountContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, memo, useCallback, useContext, useMemo, useState } from 'react'
 
 type YieldAccountContextType = {
   accountNumber: number
@@ -7,20 +7,26 @@ type YieldAccountContextType = {
 
 const YieldAccountContext = createContext<YieldAccountContextType | undefined>(undefined)
 
-export const YieldAccountProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [accountNumber, setAccountNumber] = useState(0)
+export const YieldAccountProvider: React.FC<{ children: React.ReactNode }> = memo(
+  ({ children }) => {
+    const [accountNumber, setAccountNumberState] = useState(0)
 
-  return (
-    <YieldAccountContext.Provider value={{ accountNumber, setAccountNumber }}>
-      {children}
-    </YieldAccountContext.Provider>
-  )
-}
+    const setAccountNumber = useCallback((accountNumber: number) => {
+      setAccountNumberState(accountNumber)
+    }, [])
+
+    const value = useMemo(
+      () => ({ accountNumber, setAccountNumber }),
+      [accountNumber, setAccountNumber],
+    )
+
+    return <YieldAccountContext.Provider value={value}>{children}</YieldAccountContext.Provider>
+  },
+)
 
 export const useYieldAccount = () => {
   const context = useContext(YieldAccountContext)
-  if (context === undefined) {
+  if (context === undefined)
     throw new Error('useYieldAccount must be used within a YieldAccountProvider')
-  }
   return context
 }

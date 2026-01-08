4bd7b2f1a96709f57063bb5a52fe0c25118fa06c
gomes <17035424+gomesalexandre@users.noreply.github.com>
2026-01-08 14:56:30 +0100

    [skip ci] refactor(YieldOpportunityCard): add memo, useMemo, useCallback, clean up JSX

--- src/pages/Yields/components/YieldOpportunityCard.tsx
+++ src/pages/Yields/components/YieldOpportunityCard.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Flex, Heading, Text, useColorModeValue } from '@chakra-ui/react'
+import { memo, useCallback, useMemo } from 'react'
 import { useTranslate } from 'react-polyglot'
 
 import { bnOrZero } from '@/lib/bignumber/bignumber'
@@ -9,12 +10,25 @@ type YieldOpportunityCardProps = {
   onClick: (yieldItem: AugmentedYieldDto) => void
 }
 
-export const YieldOpportunityCard = ({ maxApyYield, onClick }: YieldOpportunityCardProps) => {
+const hoverStyle = { bgGradient: 'linear(to-r, blue.600, purple.700)' }
+
+export const YieldOpportunityCard = memo(({ maxApyYield, onClick }: YieldOpportunityCardProps) => {
   const translate = useTranslate()
   const bg = useColorModeValue('gray.50', 'whiteAlpha.100')
   const borderColor = useColorModeValue('gray.100', 'whiteAlpha.100')
 
-  const apy = bnOrZero(maxApyYield.rewardRate.total).times(100).toFixed(2)
+  const apy = useMemo(
+    () => bnOrZero(maxApyYield.rewardRate.total).times(100).toFixed(2),
+    [maxApyYield.rewardRate.total],
+  )
+
+  const earnUpToText = useMemo(() => translate('yieldXYZ.earnUpTo', { apy }), [translate, apy])
+
+  const startEarningText = useMemo(() => translate('yieldXYZ.startEarning'), [translate])
+
+  const handleClick = useCallback(() => {
+    onClick(maxApyYield)
+  }, [onClick, maxApyYield])
 
   return (
     <Box
@@ -34,7 +48,7 @@ export const YieldOpportunityCard = ({ maxApyYield, onClick }: YieldOpportunityC
       >
         <Box>
           <Text color='text.subtle' fontSize='sm' mb={1}>
-            {translate('yieldXYZ.earnUpTo', { apy })}
+            {earnUpToText}
           </Text>
           <Heading size='md' bgClip='text' bgGradient='linear(to-r, blue.400, purple.500)'>
             {apy}% APY
@@ -43,16 +57,14 @@ export const YieldOpportunityCard = ({ maxApyYield, onClick }: YieldOpportunityC
         <Button
           colorScheme='blue'
           size='md'
-          onClick={() => onClick(maxApyYield)}
+          onClick={handleClick}
           px={8}
           bgGradient='linear(to-r, blue.500, purple.600)'
-          _hover={{
-            bgGradient: 'linear(to-r, blue.600, purple.700)',
-          }}
+          _hover={hoverStyle}
         >
-          {translate('yieldXYZ.startEarning')}
+          {startEarningText}
         </Button>
       </Flex>
     </Box>
   )
-}
+})

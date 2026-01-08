187135e20f73534becb454d0dcfbd564ee74b34b
gomes <17035424+gomesalexandre@users.noreply.github.com>
2026-01-08 14:41:56 +0100

    [skip ci] refactor(Yields): add memo, implicit return

--- src/pages/Yields/Yields.tsx
+++ src/pages/Yields/Yields.tsx
@@ -1,3 +1,4 @@
+import { memo } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import { YieldsList } from '@/pages/Yields/components/YieldsList'
@@ -5,16 +6,14 @@ import { YieldAccountProvider } from '@/pages/Yields/YieldAccountContext'
 import { YieldAssetDetails } from '@/pages/Yields/YieldAssetDetails'
 import { YieldDetail } from '@/pages/Yields/YieldDetail'
 
-export const Yields = () => {
-  return (
-    <YieldAccountProvider>
-      <Routes>
-        <Route path='asset/:assetId' element={<YieldAssetDetails />} />
-        <Route index element={<YieldsList />} />
-        <Route path=':yieldId/enter' element={<YieldDetail />} />
-        <Route path=':yieldId/exit' element={<YieldDetail />} />
-        <Route path=':yieldId' element={<YieldDetail />} />
-      </Routes>
-    </YieldAccountProvider>
-  )
-}
+export const Yields = memo(() => (
+  <YieldAccountProvider>
+    <Routes>
+      <Route path='asset/:assetId' element={<YieldAssetDetails />} />
+      <Route index element={<YieldsList />} />
+      <Route path=':yieldId/enter' element={<YieldDetail />} />
+      <Route path=':yieldId/exit' element={<YieldDetail />} />
+      <Route path=':yieldId' element={<YieldDetail />} />
+    </Routes>
+  </YieldAccountProvider>
+))

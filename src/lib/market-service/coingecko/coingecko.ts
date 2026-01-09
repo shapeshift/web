df2e58790619ff4a21b9b4339bef9533d7011c6e
gomes <17035424+gomesalexandre@users.noreply.github.com>
2026-01-09 14:32:40 +0100

    feat: caip katana adapters

--- src/lib/market-service/coingecko/coingecko.ts
+++ src/lib/market-service/coingecko/coingecko.ts
@@ -102,7 +102,9 @@ export class CoinGeckoMarketService implements MarketService {
     // Monkey patch Arb FOX to mainnet FOX until we have market-data for it, similar to
     // what FOXy did in https://github.com/shapeshift/lib/pull/830/files#diff-8d0028d46769c562695ae0eadad8c284637d6a3e45a71a01398c923ae912cf62
     const assetId = _assetId === foxOnArbitrumOneAssetId ? foxAssetId : _assetId
-    if (!adapters.assetIdToCoingecko(assetId)) return null
+
+    const coingeckoId = adapters.assetIdToCoingecko(assetId)
+    if (!coingeckoId) return null
 
     const url = adapters.makeCoingeckoAssetUrl(assetId)
     if (!url) return null

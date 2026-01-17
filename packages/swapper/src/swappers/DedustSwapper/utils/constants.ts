26fe7289a1623b11a7cac2988a06d89c43b2a2b4
Minimoi <mini@Minimois-Mini.lan>
2026-01-17 19:19:05 +0100

    auto-claude: subtask-2-2 - Create utils/constants.ts with DeDust configuration

---
+++ packages/swapper/src/swappers/DedustSwapper/utils/constants.ts
@@ -0,0 +1,7 @@
+import { KnownChainIds } from '@shapeshiftoss/types'
+
+export const DEDUST_SUPPORTED_CHAIN_IDS = [KnownChainIds.TonMainnet] as const
+
+export const DEDUST_DEFAULT_SLIPPAGE_BPS = 100
+
+export const DEDUST_QUOTE_TIMEOUT_MS = 30000

f358e9f48ca5735c4cb5be11ae01ca82749da0d5
NeOMakinG <14963751+NeOMakinG@users.noreply.github.com>
2026-01-22 18:03:34 +0100

    fix: tests

--- src/lib/market-service/coingecko/coingecko.test.ts
+++ src/lib/market-service/coingecko/coingecko.test.ts
@@ -169,7 +169,7 @@ describe('CoinGecko market service', () => {
     it('can flatten multiple responses', async () => {
       mocks.get.mockResolvedValueOnce({ data: [eth] }).mockResolvedValue({ data: [btc] })
       const result = await coinGeckoMarketService.findAll()
-      expect(Object.keys(result).length).toEqual(6)
+      expect(Object.keys(result).length).toEqual(7)
     })
 
     it('can sort by market cap', async () => {
@@ -193,7 +193,7 @@ describe('CoinGecko market service', () => {
     it('can return some results if partially rate limited', async () => {
       mocks.get.mockResolvedValueOnce({ status: 429 }).mockResolvedValue({ data: [eth] })
       const result = await coinGeckoMarketService.findAll()
-      expect(Object.keys(result).length).toEqual(5)
+      expect(Object.keys(result).length).toEqual(6)
     })
 
     it('can use default args', async () => {
@@ -221,8 +221,15 @@ describe('CoinGecko market service', () => {
       const result = await coinGeckoMarketService.findAll()
       const btcAssetId = adapters.coingeckoToAssetIds('bitcoin')
       const ethAssetId = adapters.coingeckoToAssetIds('ethereum')
-      const [btcKey, ethKey, ethOptimismKey, ethOnArbitrumKey, ethOnArbitrumNovaKey, ethOnBaseKey] =
-        Object.keys(result)
+      const [
+        btcKey,
+        ethKey,
+        ethOptimismKey,
+        ethOnArbitrumKey,
+        ethOnArbitrumNovaKey,
+        ethOnBaseKey,
+        ethOnLineaKey,
+      ] = Object.keys(result)
       expect(btcAssetId).toEqual([btcKey])
       expect(ethAssetId).toEqual([
         ethKey,
@@ -230,6 +237,7 @@ describe('CoinGecko market service', () => {
         ethOnArbitrumKey,
         ethOnArbitrumNovaKey,
         ethOnBaseKey,
+        ethOnLineaKey,
       ])
     })
 

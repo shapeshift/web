6194f7645cf1ccf0eaaa3840a54a7a616626daa5
Tim Black <timothy.william.black@gmail.com>
2026-01-23 11:52:42 -0800

    one slight tweak to make sure captchas don't render

--- headers/csps/userback.ts
+++ headers/csps/userback.ts
@@ -1,10 +1,22 @@
 import type { Csp } from '../types'
 
 export const csp: Csp = {
-  'connect-src': ['https://static.userback.io', 'https://api.userback.io', 'https://*.userback.io'],
-  'script-src': ['https://static.userback.io'],
-  'style-src': ['https://static.userback.io', 'https://*.userback.io'],
+  'connect-src': [
+    'https://static.userback.io',
+    'https://api.userback.io',
+    'https://*.userback.io',
+    'https://hcaptcha.com',
+    'https://*.hcaptcha.com',
+  ],
+  'script-src': ['https://static.userback.io', 'https://js.hcaptcha.com', 'https://hcaptcha.com'],
+  'style-src': ['https://static.userback.io', 'https://*.userback.io', 'https://hcaptcha.com'],
   'img-src': ['https://static.userback.io', 'https://*.userback.io'],
-  'frame-src': ['https://static.userback.io', 'https://*.userback.io'],
+  'frame-src': [
+    'https://static.userback.io',
+    'https://*.userback.io',
+    'https://hcaptcha.com',
+    'https://*.hcaptcha.com',
+    'https://newassets.hcaptcha.com',
+  ],
   'font-src': ['https://static.userback.io', 'https://*.userback.io'],
 }

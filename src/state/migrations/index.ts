d9ce587506e2e69215b3d6c38438f89e3467ef7f
Apotheosis <0xapotheosis@gmail.com>
2026-02-10 17:27:36 +1100

    chore: regen assets (#11818)

--- src/state/migrations/index.ts
+++ src/state/migrations/index.ts
@@ -327,6 +327,7 @@ export const clearAssetsMigrations = {
   280: clearAssets,
   281: clearAssets,
   282: clearAssets,
+  283: clearAssets,
 } as unknown as Omit<MigrationManifest, '_persist'>
 
 export const clearMarketDataMigrations = {

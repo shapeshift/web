150c1cb9706931971495217635afe2ad2d8c5368
NeOMakinG <14963751+NeOMakinG@users.noreply.github.com>
2026-02-17 15:38:08 +0100

    fix: add mantle coingecko generated adapter and fix test

--- packages/caip/src/adapters/coingecko/generated/index.ts
+++ packages/caip/src/adapters/coingecko/generated/index.ts
@@ -15,6 +15,7 @@ import hyperevm from "./eip155_999/adapter.json";
 import plasma from "./eip155_9745/adapter.json";
 import katana from "./eip155_747474/adapter.json";
 import megaeth from "./eip155_4326/adapter.json";
+import mantle from "./eip155_5000/adapter.json";
 import cosmos from "./cosmos_cosmoshub-4/adapter.json";
 import thorchain from "./cosmos_thorchain-1/adapter.json";
 import mayachain from "./cosmos_mayachain-mainnet-v1/adapter.json";
@@ -44,6 +45,7 @@ export {
   plasma,
   katana,
   megaeth,
+  mantle,
   cosmos,
   thorchain,
   mayachain,
--- packages/caip/src/adapters/coingecko/utils.test.ts
+++ packages/caip/src/adapters/coingecko/utils.test.ts
@@ -182,6 +182,9 @@ describe('adapters:coingecko:utils', () => {
         'eip155:747474': {
           'eip155:747474/slip44:60': 'katana',
         },
+        'eip155:5000': {
+          'eip155:5000/slip44:60': 'mantle',
+        },
         'near:mainnet': {
           'near:mainnet/slip44:397': 'near',
         },

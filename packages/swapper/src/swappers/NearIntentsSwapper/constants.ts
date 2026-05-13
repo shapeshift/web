1ad879b5675349a50553c885bc937b0969937953
kevin <35275952+kaladinlight@users.noreply.github.com>
2026-05-13 14:41:57 -0600

    fix(swapper): nearIntents allowance contract and btc omni migration (#12338)

--- packages/swapper/src/swappers/NearIntentsSwapper/constants.ts
+++ packages/swapper/src/swappers/NearIntentsSwapper/constants.ts
@@ -3,3 +3,4 @@ export const ONE_CLICK_BASE_URL = 'https://1click.chaindefuser.com'
 export const DEFAULT_SLIPPAGE_BPS = 50
 
 export const DEFAULT_QUOTE_DEADLINE_MS = 30 * 60 * 1000
+export const BTC_QUOTE_DEADLINE_MS = 60 * 60 * 1000

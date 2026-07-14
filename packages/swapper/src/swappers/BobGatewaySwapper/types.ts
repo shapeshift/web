0b8eb75cf56fd4de1cbd2b93350a932dffaf9eab
Slava Startsev <slava@interlay.io>
2026-07-14 19:33:20 +0300

    feat: add bob swapper v3 (#12485)

--- packages/swapper/src/swappers/BobGatewaySwapper/types.ts
+++ packages/swapper/src/swappers/BobGatewaySwapper/types.ts
@@ -10,7 +10,16 @@ type BobGatewayUtxoTxMetadata = {
   opReturnData?: string
 }
 
+type BobGatewayTronTxMetadata = {
+  to: string
+  data: string
+  value: string
+  feeLimit: string
+  chain: string
+}
+
 export type BobGatewayMetadata = { orderId: string } & (
-  | { evmTx: BobGatewayEvmTxMetadata; utxoTx?: never }
-  | { utxoTx: BobGatewayUtxoTxMetadata; evmTx?: never }
+  | { evmTx: BobGatewayEvmTxMetadata; utxoTx?: never; tronTx?: never }
+  | { utxoTx: BobGatewayUtxoTxMetadata; evmTx?: never; tronTx?: never }
+  | { tronTx: BobGatewayTronTxMetadata; evmTx?: never; utxoTx?: never }
 )
